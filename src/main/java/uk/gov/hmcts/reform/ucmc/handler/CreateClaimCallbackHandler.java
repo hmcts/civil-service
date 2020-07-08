f9cfdbc523f4022890347128f6f881467bd3270b
Sam Walker <sam.walker@hmcts.net>
2020-07-08 15:06:00 +0100

    feat: updated existing callbacks to use callback framework (CMC-423) (#36)

--- src/main/java/uk/gov/hmcts/reform/ucmc/handler/CreateClaimCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/ucmc/handler/CreateClaimCallbackHandler.java
@@ -1,38 +1,54 @@
-package uk.gov.hmcts.reform.ucmc.controllers;
+package uk.gov.hmcts.reform.ucmc.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
-import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import uk.gov.hmcts.reform.ucmc.callback.Callback;
+import uk.gov.hmcts.reform.ucmc.callback.CallbackHandler;
+import uk.gov.hmcts.reform.ucmc.callback.CallbackParams;
+import uk.gov.hmcts.reform.ucmc.callback.CallbackType;
+import uk.gov.hmcts.reform.ucmc.callback.CaseEvent;
 import uk.gov.hmcts.reform.ucmc.model.ClaimValue;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import static java.lang.String.format;
+import static uk.gov.hmcts.reform.ucmc.callback.CaseEvent.CREATE_CASE;
 import static uk.gov.hmcts.reform.ucmc.helpers.DateFormatHelper.DATE_TIME_AT;
 import static uk.gov.hmcts.reform.ucmc.helpers.DateFormatHelper.formatLocalDateTime;
 
-@Api
-@RestController
-@RequestMapping("/create-claim")
-public class CreateClaimController {
+@Service
+public class CreateClaimCallbackHandler extends CallbackHandler {
+    private static final List<CaseEvent> EVENTS = Collections.singletonList(CREATE_CASE);
 
-    @Autowired
-    private ObjectMapper mapper;
+    private final ObjectMapper mapper;
 
-    @PostMapping("/mid-event")
-    public AboutToStartOrSubmitCallbackResponse handleMid(@RequestBody CallbackRequest request) {
-        Map<String, Object> data = request.getCaseDetails().getData();
+    public CreateClaimCallbackHandler(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    protected Map<CallbackType, Callback> callbacks() {
+        return Map.of(
+            CallbackType.MID, this::validateClaimValues,
+            CallbackType.SUBMITTED, this::buildConfirmation
+        );
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+
+    private CallbackResponse validateClaimValues(CallbackParams callbackParams) {
+        Map<String, Object> data = callbackParams.getRequest().getCaseDetails().getData();
         List<String> errors = new ArrayList<>();
 
         if (data.get("claimValue") != null) {
@@ -49,8 +65,7 @@ public class CreateClaimController {
             .build();
     }
 
-    @PostMapping("/submitted")
-    public SubmittedCallbackResponse handleSubmitted() {
+    private SubmittedCallbackResponse buildConfirmation(CallbackParams callbackParams) {
         String documentLink = "https://www.google.com";
         String responsePackLink = "https://formfinder.hmctsformfinder.justice.gov.uk/n9-eng.pdf";
         LocalDateTime serviceDeadline = LocalDate.now().plusDays(112).atTime(23, 59);
---
+++ src/test/java/uk/gov/hmcts/reform/ucmc/handler/CreateClaimCallbackHandlerTest.java
@@ -0,0 +1,88 @@
+package uk.gov.hmcts.reform.ucmc.handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import uk.gov.hmcts.reform.ucmc.callback.CallbackParams;
+import uk.gov.hmcts.reform.ucmc.callback.CallbackType;
+import uk.gov.hmcts.reform.ucmc.model.ClaimValue;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.ucmc.helpers.DateFormatHelper.DATE_TIME_AT;
+import static uk.gov.hmcts.reform.ucmc.helpers.DateFormatHelper.formatLocalDateTime;
+
+@SpringBootTest(classes = {CreateClaimCallbackHandler.class, JacksonAutoConfiguration.class})
+class CreateClaimCallbackHandlerTest extends BaseCallbackHandlerTest {
+
+    @Autowired
+    private CreateClaimCallbackHandler handler;
+
+    @Test
+    void shouldReturnExpectedErrorInMidEventWhenValuesAreInvalid() {
+        Map<String, Object> data = new HashMap<>();
+        data.put("claimValue", ClaimValue.builder().higherValue(1).lowerValue(10).build());
+
+        CallbackParams params = callbackParamsOf(data, CallbackType.MID);
+
+        AboutToStartOrSubmitCallbackResponse response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+
+        assertThat(response.getErrors())
+            .containsOnly("CONTENT TBC: Higher value must not be lower than the lower value.");
+    }
+
+    @Test
+    void shouldReturnNoErrorInMidEventWhenValuesAreValid() {
+        Map<String, Object> data = new HashMap<>();
+        data.put("claimValue", ClaimValue.builder().higherValue(10).lowerValue(1).build());
+
+        CallbackParams params = callbackParamsOf(data, CallbackType.MID);
+
+        AboutToStartOrSubmitCallbackResponse response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+
+        assertThat(response.getErrors()).isEmpty();
+    }
+
+    @Test
+    void shouldReturnNoErrorInMidEventWhenNoValues() {
+        CallbackParams params = callbackParamsOf(new HashMap<>(), CallbackType.MID);
+
+        AboutToStartOrSubmitCallbackResponse response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+
+        assertThat(response.getErrors()).isEmpty();
+    }
+
+    @Test
+    void shouldReturnExpectedSubmittedCallbackResponseObject() {
+        CallbackParams params = callbackParamsOf(new HashMap<>(), CallbackType.SUBMITTED);
+
+        SubmittedCallbackResponse response = (SubmittedCallbackResponse) handler.handle(params);
+
+        String documentLink = "https://www.google.com";
+        String responsePackLink = "https://formfinder.hmctsformfinder.justice.gov.uk/n9-eng.pdf";
+        LocalDateTime serviceDeadline = LocalDate.now().plusDays(112).atTime(23, 59);
+        String formattedServiceDeadline = formatLocalDateTime(serviceDeadline, DATE_TIME_AT);
+
+        String body = format(
+            "<br />Follow these steps to serve a claim:"
+                + "\n* [Download the sealed claim form](%s) (PDF, 123KB)"
+                + "\n* Send the form, particulars of claim and [a response pack](%s) (PDF, 266 KB) "
+                + "to the defendant by %s"
+                + "\n* Confirm service online within 21 days of sending the form, particulars and response pack, before"
+                + " 4pm if you're doing this on the due day", documentLink, responsePackLink, formattedServiceDeadline);
+
+        assertThat(response).isEqualToComparingFieldByField(
+            SubmittedCallbackResponse.builder()
+                .confirmationHeader("# Your claim has been issued\n## Claim number: TBC")
+                .confirmationBody(body)
+                .build());
+    }
+}
