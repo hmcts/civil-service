ea934ff43376fcbf28d678d96e548da5a3694533
dharmendra kumar <dharmendra.kumar@hmcts.net>
2020-10-28 14:20:30 +0000

    fix:resolve import bpmn file (#321)

--- bin/import-bpmn-diagram.sh
+++ bin/import-bpmn-diagram.sh
@@ -2,14 +2,16 @@
 
 set -eu
 
-dir=$(dirname ${0})
 filepath="$(realpath ".")/src/main/resources/camunda"
+echo $filepath
+serviceToken=$($(realpath ".")/civil-unspecified-docker/bin/utils/idam-lease-service-token.sh unspec_service $(docker run --rm toolbelt/oathtool --totp -b ${S2S_SECRET:-AABBCCDDEEFFGGHH}))
 
 for file in $(find ${filepath} -name '*.bpmn')
 do
-  uploadResponse=$(curl --insecure --silent -w "\n%{http_code}" --show-error -X POST \
+  uploadResponse=$(curl --insecure -v --silent -w "\n%{http_code}" --show-error -X POST \
     ${CAMUNDA_BASE_URL:-http://localhost:9404}/engine-rest/deployment/create \
     -H "Accept: application/json" \
+    -H "ServiceAuthorization: Bearer ${serviceToken}" \
     -F "deployment-name=$(date +"%Y%m%d-%H%M%S")-$(basename ${file})" \
     -F "file=@${filepath}/$(basename ${file})")
 
--- bin/variables/load-aat-environment-variables.sh
+++ bin/variables/load-aat-environment-variables.sh
@@ -9,6 +9,7 @@ echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider
 echo "export IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net"
 echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect"
 echo "export CCD_DEFINITION_STORE_API_BASE_URL=http://ccd-definition-store-api-aat.service.core-compute-aat.internal"
+echo "export CAMUNDA_BASE_URL=http://camunda-api-aat.service.core-compute-aat.internal"
 
 # definition placeholders
 echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://unspec-service-aat.service.core-compute-aat.internal"
