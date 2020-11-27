9649e8f6c07ec6f1127c1f761754645f0955db90
Piotr Dudkiewicz <58216515+pd-dudkiewicz@users.noreply.github.com>
2020-11-27 15:32:57 +0000

    feat: AOS pdf generation camunda changes (CMC-161) (#420)

--- src/test/java/uk/gov/hmcts/reform/unspec/bpmn/AcknowledgeServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/unspec/bpmn/AcknowledgeServiceTest.java
@@ -9,7 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 class AcknowledgeServiceTest extends BpmnBaseTest {
 
     public static final String NOTIFY_APPLICANT_SOLICITOR_1 = "NOTIFY_APPLICANT_SOLICITOR1_FOR_SERVICE_ACKNOWLEDGEMENT";
+    public static final String GENERATE_ACKNOWLEDGEMENT_OF_SERVICE = "GENERATE_ACKNOWLEDGEMENT_OF_SERVICE";
     public static final String NOTIFICATION_ACTIVITY_ID = "AcknowledgeServiceNotifyApplicantSolicitor1";
+    public static final String GENERATE_CERTIFICATE_ACTIVITY_ID = "AcknowledgeServiceGenerateAcknowledgementOfService";
 
     public AcknowledgeServiceTest() {
         super("acknowledge_service.bpmn", "ACKNOWLEDGE_SERVICE_PROCESS_ID");
@@ -27,6 +29,11 @@ class AcknowledgeServiceTest extends BpmnBaseTest {
         //complete the start business process
         ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
         assertCompleteExternalTask(startBusiness, START_BUSINESS_TOPIC, START_BUSINESS_EVENT, START_BUSINESS_ACTIVITY);
+
+        //complete the certificate generation
+        ExternalTask certificateGeneration = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(certificateGeneration, PROCESS_CASE_EVENT, GENERATE_ACKNOWLEDGEMENT_OF_SERVICE,
+                                   GENERATE_CERTIFICATE_ACTIVITY_ID);
 
         //complete the notification to applicant
         ExternalTask notification = assertNextExternalTask(PROCESS_CASE_EVENT);
