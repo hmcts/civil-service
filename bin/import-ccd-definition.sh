1d9323ccd950a9c925993dca25aa06fdfec663ef
Piotr Dudkiewicz <58216515+pd-dudkiewicz@users.noreply.github.com>
2021-01-27 10:11:59 +0000

    feat: removed definition and camunda files (CMC-1162)

--- bin/import-ccd-definition.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-definition_input_dir=$(realpath 'ccd-definition')
-definition_output_file="$(realpath ".")/build/ccd-development-config/ccd-unspec-dev.xlsx"
-params="$@"
-
-./civil-unspecified-docker/bin/import-ccd-definition.sh "${definition_input_dir}" "${definition_output_file}" "${params}"
