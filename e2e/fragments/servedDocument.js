0b89c80a00e1b17e7c08f6e90199f690c900b676
Sam Walker <sam.walker@hmcts.net>
2021-04-08 11:11:48 +0100

    refactor: added regex to served document file collections (CMC-1444) (#741)

--- e2e/fragments/servedDocument.js
+++ e2e/fragments/servedDocument.js
@@ -2,17 +2,11 @@ const {I} = inject();
 
 module.exports = {
 
-  fields: {
-    servedDocumentFiles: {
-      id: '#servedDocumentFiles_servedDocumentFiles'
-    }
-  },
-
   async upload(file, documents) {
     for (const fileType of documents) {
       await within(fileType, async () => {
         I.click('Add new');
-        await I.attachFile(fileType + '_value', file);
+        await I.attachFile(fileType + '_0_document', file);
         await I.waitForInvisible(locate('.error-message').withText('Uploading...'));
       });
     }
