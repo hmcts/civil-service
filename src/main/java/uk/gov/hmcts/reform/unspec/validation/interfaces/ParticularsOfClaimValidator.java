61f66e2fd401f5c27ba02cc96fefe4e7e92f3f38
devmarcy <marcelmendes@hotmail.co.uk>
2021-04-27 10:28:21 +0100

    further refactoring of ParticularsOfClaimValidator

--- src/main/java/uk/gov/hmcts/reform/unspec/validation/interfaces/ParticularsOfClaimValidator.java
+++ src/main/java/uk/gov/hmcts/reform/unspec/validation/interfaces/ParticularsOfClaimValidator.java
@@ -3,29 +3,33 @@ package uk.gov.hmcts.reform.unspec.validation.interfaces;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 import uk.gov.hmcts.reform.unspec.callback.CallbackParams;
+import uk.gov.hmcts.reform.unspec.model.CaseData;
 import uk.gov.hmcts.reform.unspec.model.ServedDocumentFiles;
 
 import static java.util.Optional.ofNullable;
 
 public interface ParticularsOfClaimValidator {
 
-    private ServedDocumentFiles getServedDocumentFiles(CallbackParams callbackParams) {
+    private ServedDocumentFiles getServedDocumentFiles(CaseData caseData) {
+        ServedDocumentFiles servedDocumentFiles = caseData.getServedDocumentFiles();
 
-        return ofNullable(callbackParams.getCaseData().getServedDocumentFiles())
+        return ofNullable(servedDocumentFiles)
            .orElse(ServedDocumentFiles.builder().build());
     }
 
     default CallbackResponse validateParticularsOfClaim(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
 
         return AboutToStartOrSubmitCallbackResponse.builder()
-            .errors(getServedDocumentFiles(callbackParams).getErrors())
+            .errors(getServedDocumentFiles(caseData).getErrors())
             .build();
     }
 
     default CallbackResponse validateParticularsOfClaimAddOrAmendDocuments(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
 
         return AboutToStartOrSubmitCallbackResponse.builder()
-            .errors(getServedDocumentFiles(callbackParams).getErrorsAddOrAmendDocuments())
+            .errors(getServedDocumentFiles(caseData).getErrorsAddOrAmendDocuments())
             .build();
     }
 }
