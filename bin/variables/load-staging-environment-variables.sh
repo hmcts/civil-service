999ec98537be31a8523f631e03cf0c3f0cb12658
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2021-04-28 15:19:46 +0100

    feat: renamed civil damages to civil (CMC-1541)

--- bin/variables/load-staging-environment-variables.sh
+++ bin/variables/load-staging-environment-variables.sh
@@ -8,8 +8,8 @@ echo 'export ENVIRONMENT=aat'
 echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
 echo "export IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net"
 echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect"
-echo "export CCD_DEFINITION_STORE_API_BASE_URL=http://civil-damages-service-definition-store-staging-aat.service.core-compute-aat.internal"
-echo "export CAMUNDA_BASE_URL=http://civil-damages-service-camunda-staging-aat.service.core-compute-aat.internal"
+echo "export CCD_DEFINITION_STORE_API_BASE_URL=http://civil-service-definition-store-staging-aat.service.core-compute-aat.internal"
+echo "export CAMUNDA_BASE_URL=http://civil-service-camunda-staging-aat.service.core-compute-aat.internal"
 
 # definition placeholders
-echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-damages-service-staging.service.core-compute-aat.internal"
+echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-service-staging.service.core-compute-aat.internal"
