e2682c46a20d411c829b7dc10a1a69076f6fc752
Dharmendra kumar <k_dharmendra@hotmail.com>
2021-05-06 10:50:03 +0100

    copy vault value of s2s

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -92,3 +92,55 @@ resource "azurerm_key_vault_secret" "civil_db_password__v11_secret" {
     module.key-vault
   ]
 }
+
+data "azurerm_key_vault" "ethos_vault" {
+  name                = "ethos-${var.env}"
+  resource_group_name = "ethos-repl-docmosis-backend-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "tornado_access_secret" {
+  key_vault_id = "${data.azurerm_key_vault.ethos_vault.id}"
+  name = "tornado-access-key"
+}
+
+resource "azurerm_key_vault_secret" "civil_docmosis_api_key" {
+  name         = "docmosis-api-key"
+  value        = data.azurerm_key_vault_secret.tornado_access_secret.value
+  key_vault_id = module.key-vault.key_vault_id
+
+  depends_on = [
+    module.key-vault
+  ]
+}
+
+data "azurerm_key_vault" "s2s_vault" {
+  name                = "s2s-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "s2s_secret" {
+  key_vault_id = "${data.azurerm_key_vault.s2s_vault.id}"
+  name = "microservicekey-unspec-service"
+}
+
+resource "azurerm_key_vault_secret" "unspec_s2s_secret" {
+  name         = "microservicekey-civil-service"
+  value        = data.azurerm_key_vault_secret.s2s_secret.value
+  key_vault_id = data.azurerm_key_vault.civil_key_vault.id
+}
+
+data "azurerm_key_vault" "cmc_vault" {
+  name                = "cmc-${var.env}"
+  resource_group_name = "cmc-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "db_password_secret" {
+  key_vault_id = "${data.azurerm_key_vault.cmc_vault.id}"
+  name = "cmc-db-password"
+}
+
+resource "azurerm_key_vault_secret" "unspec_db_password_secret" {
+  name         = "cmc-db-password"
+  value        = data.azurerm_key_vault_secret.db_password_secret.value
+  key_vault_id = data.azurerm_key_vault.civil_key_vault.id
+}
