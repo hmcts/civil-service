479c5ed85b5e6e94f177fcee1118d9d34ff0b17e
Dharmendra kumar <k_dharmendra@hotmail.com>
2021-05-06 10:50:03 +0100

    copy vault value of s2s

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -57,3 +57,35 @@ resource "azurerm_key_vault_secret" "sendgrid_api_key" {
   name         = "sendgrid-api-key"
   value        = data.azurerm_key_vault_secret.send_grid_api_key.value
 }
+
+data "azurerm_key_vault" "s2s_vault" {
+  name                = "s2s-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "s2s_secret" {
+  key_vault_id = "${data.azurerm_key_vault.s2s_vault.id}"
+  name = "microservicekey-unspec-service"
+}
+
+resource "azurerm_key_vault_secret" "unspec_s2s_secret" {
+  name         = "microservicekey-civil-service"
+  value        = data.azurerm_key_vault_secret.s2s_secret.value
+  key_vault_id = data.azurerm_key_vault.civil_key_vault.id
+}
+
+data "azurerm_key_vault" "cmc_vault" {
+  name                = "cmc-${var.env}"
+  resource_group_name = "cmc-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "db_password_secret" {
+  key_vault_id = "${data.azurerm_key_vault.cmc_vault.id}"
+  name = "cmc-db-password"
+}
+
+resource "azurerm_key_vault_secret" "unspec_db_password_secret" {
+  name         = "cmc-db-password"
+  value        = data.azurerm_key_vault_secret.db_password_secret.value
+  key_vault_id = data.azurerm_key_vault.civil_key_vault.id
+}
