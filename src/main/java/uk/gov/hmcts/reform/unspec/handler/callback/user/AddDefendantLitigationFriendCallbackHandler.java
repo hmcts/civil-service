a7838cb2b11f6e7e68a0721888edbbf8b46333cc
Marcel Mendes <marcelmendes@hotmail.co.uk>
2021-05-07 17:40:44 +0100

    feat: creating confirmation screen for litigation friend event (CMC-1482) (1/2) (#100)

--- src/main/java/uk/gov/hmcts/reform/unspec/handler/callback/user/AddDefendantLitigationFriendCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/unspec/handler/callback/user/AddDefendantLitigationFriendCallbackHandler.java
@@ -2,14 +2,17 @@ package uk.gov.hmcts.reform.unspec.handler.callback.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.reform.unspec.callback.Callback;
 import uk.gov.hmcts.reform.unspec.callback.CallbackHandler;
+import uk.gov.hmcts.reform.unspec.callback.CallbackParams;
 import uk.gov.hmcts.reform.unspec.callback.CaseEvent;
 
 import java.util.List;
 import java.util.Map;
 
 import static uk.gov.hmcts.reform.unspec.callback.CallbackType.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.unspec.callback.CallbackType.SUBMITTED;
 import static uk.gov.hmcts.reform.unspec.callback.CaseEvent.ADD_DEFENDANT_LITIGATION_FRIEND;
 
 @Service
@@ -21,12 +24,21 @@ public class AddDefendantLitigationFriendCallbackHandler extends CallbackHandler
     @Override
     protected Map<String, Callback> callbacks() {
         return Map.of(
-            callbackKey(ABOUT_TO_START), this::emptyCallbackResponse
+            callbackKey(ABOUT_TO_START), this::emptyCallbackResponse,
+            callbackKey(SUBMITTED), this::buildConfirmation
         );
     }
 
     @Override
     public List<CaseEvent> handledEvents() {
         return EVENTS;
+    }
+
+    private SubmittedCallbackResponse buildConfirmation(CallbackParams callbackParams) {
+
+        return SubmittedCallbackResponse.builder()
+            .confirmationHeader("# You have added litigation friend details")
+            .confirmationBody("<br />")
+            .build();
     }
 }
--- src/test/java/uk/gov/hmcts/reform/unspec/handler/callback/user/AddDefendantLitigationFriendCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/unspec/handler/callback/user/AddDefendantLitigationFriendCallbackHandlerTest.java
@@ -7,13 +7,17 @@ import org.springframework.boot.autoconfigure.validation.ValidationAutoConfigura
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.reform.unspec.callback.CallbackParams;
 import uk.gov.hmcts.reform.unspec.handler.callback.BaseCallbackHandlerTest;
+import uk.gov.hmcts.reform.unspec.model.CaseData;
 import uk.gov.hmcts.reform.unspec.sampledata.CallbackParamsBuilder;
+import uk.gov.hmcts.reform.unspec.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.unspec.sampledata.CaseDetailsBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.unspec.callback.CallbackType.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.unspec.callback.CallbackType.SUBMITTED;
 
 @SpringBootTest(classes = {
     AddDefendantLitigationFriendCallbackHandler.class,
@@ -36,6 +40,23 @@ class AddDefendantLitigationFriendCallbackHandlerTest extends BaseCallbackHandle
                 .handle(params);
 
             assertThat(response.getErrors()).isNull();
+        }
+    }
+
+    @Nested
+    class SubmittedCallback {
+
+        @Test
+        void shouldReturnExpectedSubmittedCallbackResponse_whenInvoked() {
+            CaseData caseData = CaseDataBuilder.builder().atStateClaimDetailsNotified().build();
+            CallbackParams params = callbackParamsOf(caseData, SUBMITTED);
+            SubmittedCallbackResponse response = (SubmittedCallbackResponse) handler.handle(params);
+
+            assertThat(response).usingRecursiveComparison().isEqualTo(
+                SubmittedCallbackResponse.builder()
+                    .confirmationHeader("# You have added litigation friend details")
+                    .confirmationBody("<br />")
+                    .build());
         }
     }
 }
