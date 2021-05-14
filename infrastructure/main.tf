0ba0367ce5ad588f91025de59b26a405b238a06c
Dharmendra kumar <k_dharmendra@hotmail.com>
2021-05-14 12:07:22 +0100

    refactor:Add docmosis key from ethos

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -92,3 +92,23 @@ resource "azurerm_key_vault_secret" "civil_db_password_secret" {
     module.key-vault
   ]
 }
+
+data "azurerm_key_vault" "ethos_vault" {
+  name                = "ethos-${var.env}"
+  resource_group_name = "ethos-repl-docmosis-backend-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "tornado_access_secret" {
+  key_vault_id = "${data.azurerm_key_vault.ethos_vault.id}"
+  name = "tornado-access-key"
+}
+
+resource "azurerm_key_vault_secret" "civil_docmosis_api_key" {
+  name         = "docmosis-api-key"
+  value        = data.azurerm_key_vault_secret.tornado_access_secret.value
+  key_vault_id = module.key-vault.key_vault_id
+
+  depends_on = [
+    module.key-vault
+  ]
+}
