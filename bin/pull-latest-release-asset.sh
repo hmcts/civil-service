2d05ece3fe2164e57d00b8e6d34ae4923d7452de
Adam Cox <adamjhcox@gmail.com>
2021-06-16 15:03:41 +0100

    fix: Add retries to pull-latest-release-asset.sh (CMC-1688) (#92)

--- bin/pull-latest-release-asset.sh
+++ bin/pull-latest-release-asset.sh
@@ -3,13 +3,26 @@
 repoName=$1
 assetName=$2
 
-latestAssetId=$(curl https://api.github.com/repos/hmcts/${repoName}/releases/latest \
-  | docker run --rm --interactive stedolan/jq ".assets[] | select(.name==\"${assetName}\") | .id")
+retries=0
+until [ -f "$assetName" ]
+do
+  latestAssetId=$(curl https://api.github.com/repos/hmcts/${repoName}/releases/latest \
+   | docker run --rm --interactive stedolan/jq ".assets[] | select(.name==\"${assetName}\") | .id")
 
-curl -L \
-  -H "Accept: application/octet-stream" \
-  --output $assetName \
-  https://api.github.com/repos/hmcts/${repoName}/releases/assets/${latestAssetId} \
+  curl -L \
+    -H "Accept: application/octet-stream" \
+    --output $assetName \
+    https://api.github.com/repos/hmcts/${repoName}/releases/assets/${latestAssetId}
+
+  retries=$((retries+1))
+  echo "Try ${retries}"
+
+  if [ "$retries" -eq 5 ]
+  then
+      echo "Unable to get latest release from GitHub API"
+      exit 1
+  fi
+done
 
 unzip $assetName
 rm $assetName
