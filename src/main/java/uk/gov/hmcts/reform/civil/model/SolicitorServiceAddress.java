3cf9ac85cbe3395bf919b62312ee4dc30fa84813
dharmendra kumar <dharmendra.kumar@hmcts.net>
2021-06-17 12:38:30 +0100

    feat:Provide org service address (CMC-1659) (#96)

---
+++ src/main/java/uk/gov/hmcts/reform/civil/model/SolicitorServiceAddress.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.reform.civil.model;
+
+import lombok.Builder;
+import lombok.Data;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+
+@Data
+@Builder(toBuilder = true)
+public class SolicitorServiceAddress {
+
+    private final YesOrNo required;
+    private final Address address;
+}
