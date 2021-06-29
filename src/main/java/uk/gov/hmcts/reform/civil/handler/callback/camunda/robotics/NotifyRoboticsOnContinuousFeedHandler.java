2177f08f332025837f72493c64248e162c7aa8bc
Sabah Irfan <>
2021-06-29 15:52:13 +0100

    code clean up

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/robotics/NotifyRoboticsOnContinuousFeedHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/robotics/NotifyRoboticsOnContinuousFeedHandler.java
@@ -1,41 +1,32 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.robotics;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.networknt.schema.ValidationMessage;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
-import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.Callback;
-import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
-import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.robotics.RoboticsCaseData;
 import uk.gov.hmcts.reform.civil.service.robotics.JsonSchemaValidationService;
 import uk.gov.hmcts.reform.civil.service.robotics.RoboticsNotificationService;
-import uk.gov.hmcts.reform.civil.service.robotics.exception.JsonSchemaValidationException;
-import uk.gov.hmcts.reform.civil.service.robotics.exception.RoboticsDataException;
 import uk.gov.hmcts.reform.civil.service.robotics.mapper.RoboticsDataMapper;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-import static java.lang.String.format;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_RPA_ON_CONTINUOUS_FEED;
 
 @Service
-@RequiredArgsConstructor
-public class NotifyRoboticsOnContinuousFeedHandler extends CallbackHandler {
+public class NotifyRoboticsOnContinuousFeedHandler extends NotifyRoboticsHandler {
 
     private static final List<CaseEvent> EVENTS = List.of(NOTIFY_RPA_ON_CONTINUOUS_FEED);
     public static final String TASK_ID = "NotifyRoboticsOnContinuousFeed";
 
-    private final RoboticsNotificationService roboticsNotificationService;
-    private final JsonSchemaValidationService jsonSchemaValidationService;
-    private final RoboticsDataMapper roboticsDataMapper;
+    public NotifyRoboticsOnContinuousFeedHandler(
+        RoboticsNotificationService roboticsNotificationService,
+        JsonSchemaValidationService jsonSchemaValidationService,
+        RoboticsDataMapper roboticsDataMapper
+    ) {
+        super(roboticsNotificationService, jsonSchemaValidationService, roboticsDataMapper);
+    }
 
     @Override
     protected Map<String, Callback> callbacks() {
@@ -54,23 +45,4 @@ public class NotifyRoboticsOnContinuousFeedHandler extends CallbackHandler {
         return EVENTS;
     }
 
-    private CallbackResponse notifyRoboticsForCaseHandedOffline(CallbackParams callbackParams) {
-        CaseData caseData = callbackParams.getCaseData();
-
-        try {
-            RoboticsCaseData roboticsCaseData = roboticsDataMapper.toRoboticsCaseData(caseData);
-            Set<ValidationMessage> errors = jsonSchemaValidationService.validate(roboticsCaseData.toJsonString());
-            if (errors.isEmpty()) {
-                roboticsNotificationService.notifyRobotics(caseData);
-            } else {
-                throw new JsonSchemaValidationException(
-                    format("Invalid RPA Json payload for %s", caseData.getLegacyCaseReference()),
-                    errors
-                );
-            }
-        } catch (JsonProcessingException e) {
-            throw new RoboticsDataException(e.getMessage(), e);
-        }
-        return AboutToStartOrSubmitCallbackResponse.builder().build();
-    }
 }
--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/robotics/NotifyRoboticsOnCaseHandedOfflineHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/robotics/NotifyRoboticsOnCaseHandedOfflineHandlerTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.robotics;
 
 import com.networknt.schema.ValidationMessage;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,7 +18,6 @@ import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.sampledata.CallbackParamsBuilder;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.service.OrganisationService;
-import uk.gov.hmcts.reform.civil.service.Time;
 import uk.gov.hmcts.reform.civil.service.flowstate.StateFlowEngine;
 import uk.gov.hmcts.reform.civil.service.robotics.JsonSchemaValidationService;
 import uk.gov.hmcts.reform.civil.service.robotics.RoboticsNotificationService;
@@ -30,10 +28,8 @@ import uk.gov.hmcts.reform.civil.service.robotics.mapper.RoboticsDataMapper;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.prd.client.OrganisationApi;
 
-import java.time.LocalDateTime;
 import java.util.Set;
 
-import static java.time.LocalDateTime.now;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
@@ -56,9 +52,6 @@ import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 class NotifyRoboticsOnCaseHandedOfflineHandlerTest extends BaseCallbackHandlerTest {
 
     @MockBean
-    private Time time;
-
-    @MockBean
     private RoboticsNotificationService roboticsNotificationService;
 
     @MockBean
@@ -72,13 +65,6 @@ class NotifyRoboticsOnCaseHandedOfflineHandlerTest extends BaseCallbackHandlerTe
 
     @Nested
     class ValidJsonPayload {
-
-        private final LocalDateTime takenOfflineDate = now();
-
-        @BeforeEach
-        void setup() {
-            when(time.now()).thenReturn(takenOfflineDate);
-        }
 
         @Autowired
         private NotifyRoboticsOnCaseHandedOfflineHandler handler;
