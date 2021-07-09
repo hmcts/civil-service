f1a7947404faf1650708c7f835e1f4bb0717aedf
devmarcy <marcelmendes@hotmail.co.uk>
2021-07-09 13:34:11 +0100

    actioning feedback

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/AddCaseNoteCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/AddCaseNoteCallbackHandler.java
@@ -2,11 +2,9 @@ package uk.gov.hmcts.reform.civil.handler.callback.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
@@ -23,16 +21,14 @@ import java.util.Map;
 import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
-import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.ADD_CASE_NOTE;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AddCaseNoteCallbackHandler extends CallbackHandler {
 
     private final CaseNoteService caseNoteService;
-    private final ObjectMapper mapper;
+    private final ObjectMapper objectMapper;
 
     private static final List<CaseEvent> EVENTS = Collections.singletonList(ADD_CASE_NOTE);
 
@@ -40,8 +36,7 @@ public class AddCaseNoteCallbackHandler extends CallbackHandler {
     protected Map<String, Callback> callbacks() {
         return Map.of(
             callbackKey(ABOUT_TO_START), this::emptyCallbackResponse,
-            callbackKey(ABOUT_TO_SUBMIT), this::aboutToSubmit,
-            callbackKey(SUBMITTED), this::emptySubmittedCallbackResponse
+            callbackKey(ABOUT_TO_SUBMIT), this::aboutToSubmit
         );
     }
 
@@ -51,19 +46,20 @@ public class AddCaseNoteCallbackHandler extends CallbackHandler {
     }
 
     private CallbackResponse aboutToSubmit(CallbackParams callbackParams) {
-        CaseDetails caseDetails = callbackParams.getRequest().getCaseDetails();
-        CaseData caseData = mapper.convertValue(caseDetails.getData(), CaseData.class);
+        CaseData caseData = callbackParams.getCaseData();
 
         CaseNote caseNote = caseNoteService.buildCaseNote(callbackParams.getParams().get(BEARER_TOKEN).toString(),
                                                           caseData.getCaseNote());
 
         List<Element<CaseNote>> caseNotes = caseNoteService.addNoteToList(caseNote, caseData.getCaseNotes());
 
-        caseDetails.getData().put("caseNotes", caseNotes);
-        caseDetails.getData().remove("caseNote");
+        CaseData updatedCaseData = caseData.toBuilder()
+            .caseNotes(caseNotes)
+            .caseNote(null)
+            .build();
 
         return AboutToStartOrSubmitCallbackResponse.builder()
-            .data(caseDetails.getData())
+            .data(updatedCaseData.toMap(objectMapper))
             .build();
     }
 }
--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/AddCaseNoteCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/AddCaseNoteCallbackHandlerTest.java
@@ -1,46 +1,36 @@
 package uk.gov.hmcts.reform.civil.handler.callback.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
-import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.CaseNote;
+import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.sampledata.CallbackParamsBuilder;
-import uk.gov.hmcts.reform.civil.sampledata.CaseDetailsBuilder;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.service.CaseNoteService;
-import uk.gov.hmcts.reform.idam.client.IdamClient;
-import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
 import java.time.LocalDate;
-import java.util.Map;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
-import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
-import static uk.gov.hmcts.reform.civil.utils.ElementUtils.unwrapElements;
 import static uk.gov.hmcts.reform.civil.utils.ElementUtils.wrapElements;
 
 @SpringBootTest(classes = {
     AddCaseNoteCallbackHandler.class,
     JacksonAutoConfiguration.class,
-    ValidationAutoConfiguration.class,
-    CaseNoteService.class,
-    CaseDetailsConverter.class
 })
 
 class AddCaseNoteCallbackHandlerTest extends BaseCallbackHandlerTest {
@@ -51,19 +41,16 @@ class AddCaseNoteCallbackHandlerTest extends BaseCallbackHandlerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Autowired
-    private CaseDetailsConverter caseDetailsConverter;
-
     @MockBean
-    private IdamClient idamClient;
+    private CaseNoteService caseNoteService;
 
     @Nested
     class AboutToStartCallback {
 
         @Test
         void shouldReturnNoError_WhenAboutToStartIsInvoked() {
-            CaseDetails caseDetails = CaseDetailsBuilder.builder().atStateCaseIssued().build();
-            CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_START, caseDetails).build();
+            CaseData caseData = CaseDataBuilder.builder().atStateClaimIssued().build();
+            CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_START, caseData).build();
 
             AboutToStartOrSubmitCallbackResponse response = (AboutToStartOrSubmitCallbackResponse) handler
                 .handle(params);
@@ -74,14 +61,6 @@ class AddCaseNoteCallbackHandlerTest extends BaseCallbackHandlerTest {
 
     @Nested
     class AboutToSubmit {
-
-        @BeforeEach
-        void setup() {
-            given(idamClient.getUserDetails(any())).willReturn(UserDetails.builder()
-                                                                   .forename("John")
-                                                                   .surname("Smith")
-                                                                   .build());
-        }
 
         private CaseNote caseNote(LocalDate date, String createdBy, String note) {
             return CaseNote.builder()
@@ -94,37 +73,34 @@ class AddCaseNoteCallbackHandlerTest extends BaseCallbackHandlerTest {
         @Test
         void shouldAddCaseNoteToList_whenInvoked() {
 
-            CaseNote caseNote = caseNote(LocalDate.of(2021, 7, 5), "John Doe", "Existing note");
+            CaseNote caseNote = caseNote(LocalDate.of(2021, 7, 5), "John Doe", "Existing case note");
+            CaseNote expectedCaseNote = caseNote(LocalDate.now(), "John Smith", "Example case note");
+            List<Element<CaseNote>> updatedCaseNotes = wrapElements(caseNote, expectedCaseNote);
 
-            CaseDetails caseDetails = CaseDetails.builder()
-                .data(Map.of(
-                    "caseNote", "Example case note",
-                    "caseNotes", wrapElements(caseNote)))
+
+            CaseData caseData = CaseData.builder()
+                .caseNote("Example case note")
+                .caseNotes(wrapElements(caseNote))
                 .build();
-            CaseData data = caseDetailsConverter.toCaseData(caseDetails);
 
-            CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_SUBMIT, data).build();
+            CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+
+            when(caseNoteService.buildCaseNote(params.getParams().get(BEARER_TOKEN).toString(),"Example case note"))
+                .thenReturn(expectedCaseNote);
+            when(caseNoteService.addNoteToList(expectedCaseNote, caseData.getCaseNotes())).thenReturn(updatedCaseNotes);
 
             var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
-            CaseData updatedData = objectMapper.convertValue(response.getData(), CaseData.class);
 
-            assertThat(updatedData.getCaseNote()).isNull();
-            assertThat(unwrapElements(updatedData.getCaseNotes()))
-                .containsExactly(caseNote, caseNote(LocalDate.now(), "John Smith", "Example case note"));
-        }
-    }
+            verify(caseNoteService).buildCaseNote(params.getParams().get(BEARER_TOKEN).toString(),"Example case note");
+            verify(caseNoteService).addNoteToList(expectedCaseNote,caseData.getCaseNotes());
 
-    @Nested
-    class SubmittedCallback {
+            assertThat(response.getData())
+                .extracting("caseNote")
+                .isNull();
 
-        @Test
-        void shouldReturnEmptyResponse_whenInvoked() {
-            CaseDetails caseDetails = CaseDetailsBuilder.builder().atStateCaseIssued().build();
-            CallbackParams params = CallbackParamsBuilder.builder().of(SUBMITTED, caseDetails).build();
-
-            SubmittedCallbackResponse response = (SubmittedCallbackResponse) handler.handle(params);
-
-            assertThat(response).isEqualTo(SubmittedCallbackResponse.builder().build());
+            assertThat(response.getData())
+                .extracting("caseNotes")
+                .isEqualTo(wrapElements(updatedCaseNotes));
         }
     }
 }
