91e79a463bcfdb588290182c3fd050d3c515ca21
Sabah Irfan <>
2021-08-26 12:58:24 +0100

    add tests

--- src/test/java/uk/gov/hmcts/reform/civil/service/robotics/mapper/EventHistorySequencerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/robotics/mapper/EventHistorySequencerTest.java
@@ -22,6 +22,43 @@ class EventHistorySequencerTest {
     EventHistorySequencer eventHistorySequencer;
 
     @Test
+    void shouldSortSequenceBasedOnDateReceived_whenEventHistoryProvided() {
+        Event event = Event.builder()
+            .eventCode("999")
+            .eventDetailsText("RPA Reason: Some event Happened.")
+            .eventDetails(EventDetails.builder()
+                              .miscText("RPA Reason: Some event Happened.")
+                              .build())
+            .build();
+        LocalDateTime now = LocalDateTime.now();
+        Event firstEvent = event.toBuilder()
+            .eventSequence(3)
+            .dateReceived(now.minusDays(2))
+            .build();
+        Event secondEvent = event.toBuilder()
+            .eventSequence(1)
+            .dateReceived(now)
+            .build();
+        Event thirdEvent = event.toBuilder()
+            .eventSequence(2)
+            .dateReceived(now.plusDays(5))
+            .build();
+        EventHistory eventHistory = EventHistory.builder()
+            .miscellaneous(List.of(firstEvent, secondEvent, thirdEvent))
+            .build();
+
+        var result = eventHistorySequencer.sortEvents(eventHistory);
+        assertThat(result).isNotNull();
+        assertThat(result)
+            .extracting(EventHistory::getMiscellaneous)
+            .isEqualTo(List.of(
+                firstEvent.toBuilder().eventSequence(1).build(),
+                secondEvent.toBuilder().eventSequence(2).build(),
+                thirdEvent.toBuilder().eventSequence(3).build()
+            ));
+    }
+
+    @Test
     void shouldAddSequence_whenSingleEventInEventHistory() {
         Event event = Event.builder()
             .eventCode("999")
