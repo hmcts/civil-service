e993623c2fa3b9d48706012c02fce20db9f2840a
Dharmendra Kumar <dharmendra.kumar@hmcts.net>
2021-10-18 10:41:54 +0100

    feat:Add document to ccd case document

--- src/main/java/uk/gov/hmcts/reform/civil/model/documents/Document.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/documents/Document.java
@@ -9,19 +9,29 @@ import lombok.Value;
 @Builder(toBuilder = true)
 public class Document {
 
-    @JsonProperty("document_url")
     String documentUrl;
-
-    @JsonProperty("document_binary_url")
     String documentBinaryUrl;
-
-    @JsonProperty("document_filename")
     String documentFileName;
+    String documentHash;
 
     @JsonCreator
-    public Document(String documentUrl, String documentBinaryUrl, String documentFileName) {
+    public Document(@JsonProperty("document_url") String documentUrl,
+                    @JsonProperty("document_binary_url") String documentBinaryUrl,
+                    @JsonProperty("document_filename") String documentFileName,
+                    @JsonProperty("document_hash") String documentHash) {
         this.documentUrl = documentUrl;
         this.documentBinaryUrl = documentBinaryUrl;
         this.documentFileName = documentFileName;
+        this.documentHash = documentHash;
+    }
+
+    @JsonCreator
+    public Document(@JsonProperty("document_url") String documentUrl,
+                    @JsonProperty("document_binary_url") String documentBinaryUrl,
+                    @JsonProperty("document_filename") String documentFileName) {
+        this.documentUrl = documentUrl;
+        this.documentBinaryUrl = documentBinaryUrl;
+        this.documentFileName = documentFileName;
+        this.documentHash = null;
     }
 }
