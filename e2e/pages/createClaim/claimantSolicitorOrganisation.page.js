383c23edef1d208c42b5995cca3c84db84268679
Doug Rice <douglasrice@yahoo.com>
2021-10-25 09:47:24 +0100

    Remove manual selection of claimant solicitor organisation

--- e2e/pages/createClaim/claimantSolicitorOrganisation.page.js
+++ e2e/pages/createClaim/claimantSolicitorOrganisation.page.js
@@ -13,7 +13,6 @@ module.exports = {
     I.fillField(this.fields.orgPolicyReference, 'Claimant policy reference');
     I.waitForElement(this.fields.searchText);
     I.fillField(this.fields.searchText, 'Civil');
-    I.click('a[title="Select the organisation Civil - Organisation 1"]');
     await I.clickContinue();
   }
 };
