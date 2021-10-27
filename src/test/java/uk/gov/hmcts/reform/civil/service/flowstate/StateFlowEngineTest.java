b63f96f746eb311588a802636bb4e07fea6173c5
Harry Henry <Harry.Henry@version1.com>
2021-10-27 11:57:42 +0100

    Adding @DirtiesContext to recreate context

--- src/test/java/uk/gov/hmcts/reform/civil/service/flowstate/StateFlowEngineTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/flowstate/StateFlowEngineTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.launchdarkly.FeatureToggleService;
@@ -75,6 +76,7 @@ class StateFlowEngineTest {
     }
 
     @Nested
+    @DirtiesContext
     class EvaluateStateFlowEngine {
 
         @Test
