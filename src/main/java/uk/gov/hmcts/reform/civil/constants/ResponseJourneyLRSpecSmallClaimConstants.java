1b13c5828a4e4650c034bb7cb329f58686a1f2c8
jaberelfasi <jaberelfasi@gmail.com>
2021-11-17 18:36:02 +0000

    ROC-9698 added constants class for LR Spec small claims

--- src/main/java/uk/gov/hmcts/reform/civil/constants/ResponseJourneyLRSpecSmallClaimConstants.java
+++ src/main/java/uk/gov/hmcts/reform/civil/constants/ResponseJourneyLRSpecSmallClaimConstants.java
@@ -2,6 +2,10 @@ package uk.gov.hmcts.reform.civil.constants;
 
 public final class ResponseJourneyLRSpecSmallClaimConstants {
 
+    private ResponseJourneyLRSpecSmallClaimConstants(){
+        //To satisfy sonarQube
+    }
+
     public static final String SMALL_CLAIM = "SMALL_CLAIM";
 
 }
