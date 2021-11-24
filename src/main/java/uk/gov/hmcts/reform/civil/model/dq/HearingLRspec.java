af57ca395cdc303fa75049df05bc52236192c693
indu2019 <indu.chaturvedi@hmcts.net>
2021-11-24 09:50:49 +0000

    fix: feat/ROC-9694 Hearing for Fast Track changes after sampan suggestion (after reviewing PR)

--- src/main/java/uk/gov/hmcts/reform/civil/model/dq/HearingLRspec.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/dq/HearingLRspec.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.civil.model.dq;
 
 import lombok.Builder;
-import lombok.Data;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.enums.dq.HearingLength;
 import uk.gov.hmcts.reform.civil.model.UnavailableDateLRspec;
@@ -9,15 +8,18 @@ import uk.gov.hmcts.reform.civil.model.common.Element;
 
 import java.util.List;
 
-@Data
-@Builder(toBuilder = true)
-public class HearingLRspec {
 
-    private final HearingLength hearingLength;
-    private final String hearingLengthHours;
-    private final String hearingLengthDays;
-    private final YesOrNo unavailableDatesRequired;
+public class HearingLRspec extends HearingSuper {
+
     private final List<Element<UnavailableDateLRspec>> unavailableDatesLRspec;
 
+    @Builder
+    public HearingLRspec(HearingLength hearingLength, String hearingLengthHours, String hearingLengthDays, YesOrNo unavailableDatesRequired, List<Element<UnavailableDateLRspec>> unavailableDatesLRspec) {
+        super(hearingLength, hearingLengthHours, hearingLengthDays, unavailableDatesRequired);
+        this.unavailableDatesLRspec = unavailableDatesLRspec;
+    }
 
+    public List<Element<UnavailableDateLRspec>> getUnavailableDatesLRspec() {
+        return unavailableDatesLRspec;
+    }
 }
---
+++ src/main/java/uk/gov/hmcts/reform/civil/model/dq/HearingSuper.java
@@ -0,0 +1,15 @@
+package uk.gov.hmcts.reform.civil.model.dq;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.enums.dq.HearingLength;
+
+@Data
+@AllArgsConstructor
+public class HearingSuper {
+    final HearingLength hearingLength;
+    final String hearingLengthHours;
+    final String hearingLengthDays;
+    final YesOrNo unavailableDatesRequired;
+}
