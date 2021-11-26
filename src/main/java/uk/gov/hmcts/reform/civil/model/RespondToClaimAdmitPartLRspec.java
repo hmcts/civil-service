6efcd1709c4a92f085929f687582e26e2ebc5e6f
jaberelfasi <jaberelfasi@gmail.com>
2021-11-26 14:52:34 +0000

    feat/ROC-9781-whenPartAdmittedToBePaid change completed

--- src/main/java/uk/gov/hmcts/reform/civil/model/RespondToClaimAdmitPartLRspec.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/RespondToClaimAdmitPartLRspec.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.civil.validation.groups.PaymentDateGroup;
@@ -15,4 +16,9 @@ public class RespondToClaimAdmitPartLRspec {
     @FutureOrPresent(message = "Date for when will the amount be paid must be today or in the future.",
         groups = PaymentDateGroup.class)
     private final LocalDate whenWillThisAmountBePaid;
+
+    @JsonCreator
+    public RespondToClaimAdmitPartLRspec(LocalDate whenWillThisAmountBePaid) {
+        this.whenWillThisAmountBePaid = whenWillThisAmountBePaid;
+    }
 }
