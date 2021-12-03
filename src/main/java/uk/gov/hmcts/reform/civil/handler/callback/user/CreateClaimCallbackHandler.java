b928ca79529d0248f04b8e0560185c2f2910b5b1
karthick-mohanasundaram-HMCTS <karthick.mohanasundaram@HMCTS.NET>
2021-12-03 11:29:24 +0000

    squash

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateClaimCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateClaimCallbackHandler.java
@@ -177,7 +177,6 @@ public class CreateClaimCallbackHandler extends CallbackHandler implements Parti
     }
 
     private void addOrgPolicy2ForSameLegalRepresentative(CaseData caseData, CaseData.CaseDataBuilder caseDataBuilder) {
-        if (caseData.getRespondent2SameLegalRepresentative() == YES) {
             OrganisationPolicy respondent1OrganisationPolicy = caseData.getRespondent1OrganisationPolicy();
 
             OrganisationPolicy organisationPolicy2 = OrganisationPolicy.builder()
@@ -187,7 +186,6 @@ public class CreateClaimCallbackHandler extends CallbackHandler implements Parti
                 .build();
 
             caseDataBuilder.respondent2OrganisationPolicy(organisationPolicy2);
-        }
     }
 
     private CallbackResponse calculateFee(CallbackParams callbackParams) {
@@ -270,7 +268,14 @@ public class CreateClaimCallbackHandler extends CallbackHandler implements Parti
         CaseData caseData = callbackParams.getCaseData();
         // second idam call is workaround for null pointer when hiding field in getIdamEmail callback
         CaseData.CaseDataBuilder dataBuilder = getSharedData(callbackParams);
-        addOrgPolicy2ForSameLegalRepresentative(caseData, dataBuilder);
+
+        if (caseData.getRespondent2SameLegalRepresentative() == YES) {
+            addOrgPolicy2ForSameLegalRepresentative(caseData, dataBuilder);
+
+            // Predicate: Def1 registered, Def 2 unregistered.
+            // This is required to ensure mutual exclusion in 1v2 same solicitor case.
+            dataBuilder.respondent2OrgRegistered(YES);
+        }
 
         // moving statement of truth value to correct field, this was not possible in mid event.
         // resetting statement of truth to make sure it's empty the next time it appears in the UI.
