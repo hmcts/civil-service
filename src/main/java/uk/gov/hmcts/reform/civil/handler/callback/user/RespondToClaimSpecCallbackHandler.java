8fd812749fe83e4211dfdb510afdac1ddebc4430
jaberelfasi <jaberelfasi@gmail.com>
2021-12-06 16:30:19 +0000

    feat/CIV-232 change completed

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToClaimSpecCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToClaimSpecCallbackHandler.java
@@ -38,7 +38,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java .util.ArrayList;
+import java.util.ArrayList;
 
 import static java.lang.String.format;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
