25e2ba2af06facc3f76fe40531e3be5da6c55f78
dtortolaV1 <dario.tortolanavarro@hmcts.net>
2021-12-17 13:42:20 +0000

    feat/CIV-236 Use already existing PaymentFrequencyLRspec; use @JsonFormat for BigDecimal

--- src/main/java/uk/gov/hmcts/reform/civil/enums/PaymentFrequencyLRspec.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/PaymentFrequencyLRspec.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum PaymentFrequencyLRspec {
     ONCE_ONE_WEEK("Paid every week"),
     ONCE_TWO_WEEKS("Paid every 2 weeks"),
+    ONCE_THREE_WEEKS("Paid every 3 weeks"),
     ONCE_FOUR_WEEKS("Paid every 4 weeks"),
     ONCE_ONE_MONTH("Paid every month");
 
--- src/main/java/uk/gov/hmcts/reform/civil/model/dq/RecurringIncomeLRspec.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/dq/RecurringIncomeLRspec.java
@@ -1,8 +1,9 @@
 package uk.gov.hmcts.reform.civil.model.dq;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import uk.gov.hmcts.reform.civil.enums.PaymentFrequencyLRspec;
 import uk.gov.hmcts.reform.civil.enums.dq.IncomeTypeLRspec;
-import uk.gov.hmcts.reform.civil.enums.dq.WeekMonthPeriodLRspec;
 
 import java.math.BigDecimal;
 
@@ -11,6 +12,10 @@ public class RecurringIncomeLRspec {
 
     private final IncomeTypeLRspec type;
     private final String typeOtherDetails;
+    /**
+     * amount in pence.
+     */
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private final BigDecimal amount;
-    private final WeekMonthPeriodLRspec frequency;
+    private final PaymentFrequencyLRspec frequency;
 }
