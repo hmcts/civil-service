1ea679cfd2c9dd16034b9eee34a8840b169108c6
Doug Rice <douglasrice@yahoo.com>
2021-12-20 17:01:05 +0000

    Add bash

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # User used during the CCD import and ccd-role creation
-./bin/utils/ccd-add-role.sh "caseworker-civil"
+bash ./bin/utils/ccd-add-role.sh "caseworker-civil"
 ./bin/utils/ccd-add-role.sh "caseworker-caa"
 ./bin/utils/ccd-add-role.sh "caseworker-approver"
 ./bin/utils/ccd-add-role.sh "prd-admin"
