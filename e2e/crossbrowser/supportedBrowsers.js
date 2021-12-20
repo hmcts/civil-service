36525f9e8997ebaafd26a3cdccb764d27864be09
Kingsley  Amadu <kingsleyamadu@Kingsleys-MacBook.local>
2021-12-20 19:35:32 +0000

    disabled but one xbrowserstesting

--- e2e/crossbrowser/supportedBrowsers.js
+++ e2e/crossbrowser/supportedBrowsers.js
@@ -13,54 +13,55 @@ const supportedBrowsers = {
       },
     },
   },
-  safari: {
-    safari_mac_latest: {
-      browserName: 'safari',
-      platformName: 'macOS 11.00',
-      browserVersion: 'latest',
-      'sauce:options': {
-        name: 'Civil: MAC_SAFARI',
-        seleniumVersion: '3.141.59',
-        screenResolution: '1376x1032',
-      },
-    },
-  },
-  chrome: {
-    chrome_win_latest: {
-      browserName: 'chrome',
-      platformName: LATEST_WINDOWS,
-      browserVersion: 'latest',
-      'sauce:options': {
-        name: 'Civil: WIN_CHROME_LATEST',
-      },
-    },
-    chrome_mac_latest: {
-      browserName: 'chrome',
-      platformName: LATEST_MAC,
-      browserVersion: 'latest',
-      'sauce:options': {
-        name: 'Civil: MAC_CHROME_LATEST',
-      },
-    },
-  },
-  firefox: {
-    firefox_win_latest: {
-      browserName: 'firefox',
-      platformName: LATEST_WINDOWS,
-      browserVersion: 'latest',
-      'sauce:options': {
-        name: 'Civil: WIN_FIREFOX_LATEST',
-      },
-    },
-    firefox_mac_latest: {
-      browserName: 'firefox',
-      platformName: LATEST_MAC,
-      browserVersion: 'latest',
-      'sauce:options': {
-        name: 'Civil: MAC_FIREFOX_LATEST',
-      },
-    },
-  },
+//   safari: {
+//     safari_mac_latest: {
+//       browserName: 'safari',
+//       platformName: 'macOS 11.00',
+//       browserVersion: 'latest',
+//       'sauce:options': {
+//         name: 'Civil: MAC_SAFARI',
+//         seleniumVersion: '3.141.59',
+//         screenResolution: '1376x1032',
+//       },
+//     },
+//   },
+//   chrome: {
+//     chrome_win_latest: {
+//       browserName: 'chrome',
+//       platformName: LATEST_WINDOWS,
+//       browserVersion: 'latest',
+//       'sauce:options': {
+//         name: 'Civil: WIN_CHROME_LATEST',
+//       },
+//     },
+//     chrome_mac_latest: {
+//       browserName: 'chrome',
+//       platformName: LATEST_MAC,
+//       browserVersion: 'latest',
+//       'sauce:options': {
+//         name: 'Civil: MAC_CHROME_LATEST',
+//       },
+//     },
+//   },
+//   firefox: {
+//     firefox_win_latest: {
+//       browserName: 'firefox',
+//       platformName: LATEST_WINDOWS,
+//       browserVersion: 'latest',
+//       'sauce:options': {
+//         name: 'Civil: WIN_FIREFOX_LATEST',
+//       },
+//     },
+//     firefox_mac_latest: {
+//       browserName: 'firefox',
+//       platformName: LATEST_MAC,
+//       browserVersion: 'latest',
+//       'sauce:options': {
+//         name: 'Civil: MAC_FIREFOX_LATEST',
+//       },
+//     },
+//   },
 };
+
 
 module.exports = supportedBrowsers;
