c32d05ba5f268a62707d5677a66fa6119b0ae2fe
dtortolaV1 <93722198+dtortolaV1@users.noreply.github.com>
2021-12-21 15:18:58 +0000

    feat/CIV-236 POJOs to hold carer's allowance and credit, recurring income and expenses (#497)
    * feat/CIV-236 POJOs to hold carer's allowance and credit, recurring incomes and recurring expenses
    
    * feat/CIV-236 Unnecessary change
    
    * feat/CIV-236 Use already existing PaymentFrequencyLRspec; use @JsonFormat for BigDecimal
    
    Co-authored-by: dtortolaV1 <dario.tortolanavarro@hmcts.net>

---
+++ src/main/java/uk/gov/hmcts/reform/civil/model/dq/RecurringIncomeLRspec.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.civil.model.dq;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import uk.gov.hmcts.reform.civil.enums.PaymentFrequencyLRspec;
+import uk.gov.hmcts.reform.civil.enums.dq.IncomeTypeLRspec;
+
+import java.math.BigDecimal;
+
+@Data
+public class RecurringIncomeLRspec {
+
+    private final IncomeTypeLRspec type;
+    private final String typeOtherDetails;
+    /**
+     * amount in pence.
+     */
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private final BigDecimal amount;
+    private final PaymentFrequencyLRspec frequency;
+}
