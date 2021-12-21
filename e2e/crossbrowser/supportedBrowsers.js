946d5f5c768d4fe496f894853d511149daea05d6
Kingsley  Amadu <kingsleyamadu@Kingsleys-MacBook.local>
2021-12-21 07:45:49 +0000

    ie11  browsers testing

--- e2e/crossbrowser/supportedBrowsers.js
+++ e2e/crossbrowser/supportedBrowsers.js
@@ -2,50 +2,50 @@ const LATEST_MAC = 'macOS 10.15';
 const LATEST_WINDOWS = 'Windows 10';
 
 const supportedBrowsers = {
-  // microsoft: {
-  //   ie11_win: {
-  //     browserName: 'internet explorer',
-  //     platformName: LATEST_WINDOWS,
-  //     browserVersion: 'latest',
-  //     'sauce:options': {
-  //       name: 'Civil: IE11',
-  //       screenResolution: '1400x1050',
-  //     },
-  //   },
-  // },
-
-  //Safari, all chrome -passed
-
-  safari: {
-    safari_mac_latest: {
-      browserName: 'safari',
-      platformName: 'macOS 11.00',
-      browserVersion: 'latest',
-      'sauce:options': {
-        name: 'Civil: MAC_SAFARI',
-        seleniumVersion: '3.141.59',
-        screenResolution: '1376x1032',
-      },
-    },
-  },
-  chrome: {
-    chrome_win_latest: {
-      browserName: 'chrome',
+  microsoft: {
+    ie11_win: {
+      browserName: 'internet explorer',
       platformName: LATEST_WINDOWS,
       browserVersion: 'latest',
       'sauce:options': {
-        name: 'Civil: WIN_CHROME_LATEST',
-      },
-    },
-    chrome_mac_latest: {
-      browserName: 'chrome',
-      platformName: LATEST_MAC,
-      browserVersion: 'latest',
-      'sauce:options': {
-        name: 'Civil: MAC_CHROME_LATEST',
+        name: 'Civil: IE11',
+        screenResolution: '1400x1050',
       },
     },
   },
+
+  //Safari, all chrome -passed
+  //
+  // safari: {
+  //   safari_mac_latest: {
+  //     browserName: 'safari',
+  //     platformName: 'macOS 11.00',
+  //     browserVersion: 'latest',
+  //     'sauce:options': {
+  //       name: 'Civil: MAC_SAFARI',
+  //       seleniumVersion: '3.141.59',
+  //       screenResolution: '1376x1032',
+  //     },
+  //   },
+  // },
+  // chrome: {
+  //   chrome_win_latest: {
+  //     browserName: 'chrome',
+  //     platformName: LATEST_WINDOWS,
+  //     browserVersion: 'latest',
+  //     'sauce:options': {
+  //       name: 'Civil: WIN_CHROME_LATEST',
+  //     },
+  //   },
+  //   chrome_mac_latest: {
+  //     browserName: 'chrome',
+  //     platformName: LATEST_MAC,
+  //     browserVersion: 'latest',
+  //     'sauce:options': {
+  //       name: 'Civil: MAC_CHROME_LATEST',
+  //     },
+  //   },
+  // },
   // firefox: {
   //   firefox_win_latest: {
   //     browserName: 'firefox',
