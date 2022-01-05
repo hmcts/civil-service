b48df3c35453943b16ee37a29fc14aea1b3e4744
Doug Rice <douglasrice@yahoo.com>
2022-01-05 15:41:57 +0000

    test

--- e2e/pages/login.page.js
+++ e2e/pages/login.page.js
@@ -8,12 +8,12 @@ module.exports = {
   },
   submitButton: 'input[value="Sign in"]',
 
-  signIn(user) {
-    I.retry(5).waitForElement(this.fields.username);
+  async signIn(user) {
+    await I.waitForElement(this.fields.username);
     I.fillField(this.fields.username, user.email);
     I.fillField(this.fields.password, user.password);
 
-    I.retry(5).waitForElement(this.submitButton);
+    await I.waitForElement(this.submitButton);
     I.click(this.submitButton);
   },
 };
--- e2e/steps_file.js
+++ e2e/steps_file.js
@@ -91,7 +91,6 @@ const specConfirmLegalRepDetails = require('./fragments/confirmLegalRepDetailsLR
 const SIGNED_IN_SELECTOR = 'exui-header';
 const SIGNED_OUT_SELECTOR = '#global-header';
 const CASE_HEADER = 'ccd-case-header > h1';
-const REJECT_COOKIES = '#cookie-reject-submit';
 
 const TEST_FILE_PATH = './e2e/fixtures/examplePDF.pdf';
 
@@ -108,6 +107,7 @@ module.exports = function () {
     // Define custom steps here, use 'this' to access default methods of I.
     // It is recommended to place a general 'login' function here.
     async login(user) {
+      console.log('user:', user);
       if (await this.hasSelector(SIGNED_IN_SELECTOR)) {
         await this.signOut();
       }
@@ -123,10 +123,6 @@ module.exports = function () {
 
         if (!config.idamStub.enabled || config.idamStub.enabled === 'false') {
           output.log(`Signing in user: ${user.type}`);
-          if (await this.hasSelector(REJECT_COOKIES)) {
-            this.click('#cookie-reject-submit');
-            this.click('#cookie-reject-all-success-banner-hide');
-          }
           await loginPage.signIn(user);
         }
       }, SIGNED_IN_SELECTOR);
