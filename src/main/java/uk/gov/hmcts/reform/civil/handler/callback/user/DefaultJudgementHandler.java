957af181c0723314b6a0e49c613d7a8639351b1b
skhajuria <sankhajuria@gmail.com>
2022-01-10 12:44:26 +0000

    CIV-111,CIV-114- Legal Rep certifies conditions of CPR 12.3(3) have been satisfied/Not satisfied

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/DefaultJudgementHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/DefaultJudgementHandler.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
@@ -17,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import static java.lang.String.format;
 import static java.util.Objects.nonNull;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.*;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.DEFAULT_JUDGEMENT;
@@ -26,9 +28,15 @@ import static uk.gov.hmcts.reform.civil.helpers.DateFormatHelper.formatLocalDate
 @Service
 @RequiredArgsConstructor
 public class DefaultJudgementHandler extends CallbackHandler {
-    private static final List<CaseEvent> EVENTS = List.of(DEFAULT_JUDGEMENT);
     public static final String NOT_VALID_DJ = "The Claim  is not eligible for Default Judgment util %s";
-
+    public static final String CPR_REQUIRED_INFO = "<br />You can only request default judgment if:"
+        + "%n%n * The time for responding to the claim has expired. "
+        + "%n%n * The Defendant has not responded to the claim."
+        + "%n%n * There is no outstanding application by the Defendant to strike out the claim for summary judgment."
+        + "%n%n * The Defendant has not satisfied the whole claim, including costs."
+        + "%n%n * The Defendant has not filed an admission together with request for time to pay."
+        + "%n%n You can make another default judgment request when you know all these statements have been met.";
+    private static final List<CaseEvent> EVENTS = List.of(DEFAULT_JUDGEMENT);
     private final ObjectMapper objectMapper;
 
     @Override
@@ -36,7 +44,8 @@ public class DefaultJudgementHandler extends CallbackHandler {
         return Map.of(
             callbackKey(ABOUT_TO_START), this::validateDefaultJudgementEligibility,
             callbackKey(MID, "showcertifystatement"), this::checkStatus,
-            callbackKey(SUBMITTED), this::emptySubmittedCallbackResponse
+            callbackKey(ABOUT_TO_SUBMIT), this::emptyCallbackResponse,
+            callbackKey(SUBMITTED), this::buildConfirmation
         );
     }
 
@@ -45,8 +54,37 @@ public class DefaultJudgementHandler extends CallbackHandler {
         return EVENTS;
     }
 
-    private CallbackResponse checkStatus(CallbackParams callbackParams){
-        return null;
+    private SubmittedCallbackResponse buildConfirmation(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        return SubmittedCallbackResponse.builder()
+            .confirmationHeader(getHeader(caseData))
+            .confirmationBody(getBody(caseData))
+            .build();
+    }
+
+    private String getHeader(CaseData caseData) {
+
+        return format("# You cannot request default judgment");
+
+    }
+
+    private String getBody(CaseData caseData) {
+        return format(CPR_REQUIRED_INFO);
+
+    }
+
+    private CallbackResponse checkStatus(CallbackParams callbackParams) {
+        var caseData = callbackParams.getCaseData();
+
+        CaseData.CaseDataBuilder caseDataBuilder = caseData.toBuilder();
+        caseDataBuilder.bothDefendants("One");
+        if (caseData.getDefendantDetails().getValue().getLabel().startsWith("Both")) {
+            caseDataBuilder.bothDefendants(caseData.getDefendantDetails().getValue().getLabel());
+        }
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDataBuilder.build().toMap(objectMapper))
+            .build();
 
     }
 
@@ -62,11 +100,11 @@ public class DefaultJudgementHandler extends CallbackHandler {
         List<String> listData = new ArrayList<>();
         listData.add(caseData.getRespondent1().getIndividualFirstName() + " " + caseData.getRespondent1().getIndividualLastName());
         if (nonNull(caseData.getRespondent2())) {
-            listData.add(caseData.getRespondent2().getIndividualFirstName() + " " + caseData.getRespondent2().getIndividualLastName() );
+            listData.add(caseData.getRespondent2().getIndividualFirstName() + " " + caseData.getRespondent2().getIndividualLastName());
             listData.add("Both Defendants");
             caseDataBuilder.defendantDetails(DynamicList.fromList(listData));
         }
-            caseDataBuilder.defendantDetails(DynamicList.fromList(listData));
+        caseDataBuilder.defendantDetails(DynamicList.fromList(listData));
 
         return AboutToStartOrSubmitCallbackResponse.builder()
             .errors(errors)
