8e6881eda4b8b834db9e2acde5928c334be900a6
laarmada <sergiosalmar@yahoo.es>
2022-01-18 15:00:31 +0000

    modify endpoint getList

--- src/main/java/uk/gov/hmcts/reform/civil/controllers/citizenclaims/ClaimsController.java
+++ src/main/java/uk/gov/hmcts/reform/civil/controllers/citizenclaims/ClaimsController.java
@@ -3,15 +3,23 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.ccd.client.model.SearchResult;
 import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.search.Query;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
+import org.elasticsearch.index.query.QueryBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
 
 @Api
 @Slf4j
@@ -38,13 +46,13 @@ public class ClaimsController {
     }
     @GetMapping(path = "/list")
     @ApiOperation("Handles all callbacks from CCD")
-    public ResponseEntity<CaseData> getList( @RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation){
+    public ResponseEntity<SearchResult> getList( @RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation){
 
         log.info("Received callback from CCD");
-        //modify the service to get all claims instead of 1.
-        long id=1641908933090158L;
-        CaseData caseData = caseDetailsConverter.toCaseData(coreCaseDataService.getCase(id));
 
-        return new ResponseEntity<>(caseData, HttpStatus.OK);
+        Query query = new Query(QueryBuilders.matchQuery("case_type_id", "CIVIL"), emptyList(), 0);
+        SearchResult claims = coreCaseDataService.searchCases(query);
+
+        return new ResponseEntity<>(claims, HttpStatus.OK);
     }
 }
