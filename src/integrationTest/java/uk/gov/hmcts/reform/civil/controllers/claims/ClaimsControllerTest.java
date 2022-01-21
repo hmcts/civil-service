f99707645b432b09835e40aaed489ba3584ed85c
laarmada <sergiosalmar@yahoo.es>
2022-01-21 08:37:22 +0000

    organize imports

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/claims/ClaimsControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/claims/ClaimsControllerTest.java
@@ -3,17 +3,12 @@ package uk.gov.hmcts.reform.civil.controllers.claims;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.civil.controllers.BaseIntegrationTest;
 import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
-
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 public class ClaimsControllerTest extends BaseIntegrationTest {
 
