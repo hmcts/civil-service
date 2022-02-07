609d3e21b308c8d10bc548ee3400ce0ee8113dc3
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2022-02-07 10:14:35 +0000

    removed unused methods

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/cases/CasesControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/cases/CasesControllerTest.java
@@ -3,18 +3,13 @@ package uk.gov.hmcts.reform.civil.controllers.cases;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.civil.controllers.BaseIntegrationTest;
-import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
-import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 import uk.gov.hmcts.reform.civil.service.RoleAssignmentsService;
 import uk.gov.hmcts.reform.ras.model.RasResponse;
 import uk.gov.hmcts.reform.ras.model.RoleAssignmentResponse;
 
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -22,40 +17,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class CasesControllerTest extends BaseIntegrationTest {
 
-    private static final String CASES_URL = "/cases/{cid}";
-    private static final String CASES_LIST_URL = "/cases/";
     private static final String CASES_ACTOR_URL = "/cases/actors/{actorId}";
     private static final String ACTORID = "1111111";
 
-    @MockBean
-    private CoreCaseDataService coreCaseDataService;
-
-    @MockBean
-    private CaseDetailsConverter caseDetailsConverter;
 
     @MockBean
     private RoleAssignmentsService roleAssignmentsService;
 
-    @Test
-    @SneakyThrows
-    public void shouldReturnHttp200() {
-        CaseDetails expectedCaseDetails = CaseDetails.builder().id(1L).build();
-        CaseData expectedCaseData = CaseData.builder().ccdCaseReference(1L).build();
-
-        when(coreCaseDataService.getCase(1L))
-            .thenReturn(expectedCaseDetails);
-        when(caseDetailsConverter.toCaseData(expectedCaseDetails.getData()))
-            .thenReturn(expectedCaseData);
-        doGet(BEARER_TOKEN, CASES_URL, 1L)
-            .andExpect(content().json(toJson(expectedCaseData)))
-            .andExpect(status().isOk());
-    }
-    @Test
-    @SneakyThrows
-    public void shouldReturnOk() {
-        doGet(BEARER_TOKEN, CASES_LIST_URL)
-            .andExpect(status().isOk());
-    }
 
     @Test
     @SneakyThrows
--- src/main/java/uk/gov/hmcts/reform/civil/controllers/cases/CasesController.java
+++ src/main/java/uk/gov/hmcts/reform/civil/controllers/cases/CasesController.java
@@ -4,22 +4,13 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
-import uk.gov.hmcts.reform.ccd.client.model.SearchResult;
-import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.search.Query;
-import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 import uk.gov.hmcts.reform.civil.service.RoleAssignmentsService;
 import uk.gov.hmcts.reform.ras.model.RasResponse;
-import uk.gov.hmcts.reform.ras.model.RoleAssignmentResponse;
-
-import static java.util.Collections.emptyList;
 @Api
 @Slf4j
 @RestController
@@ -30,41 +21,7 @@ import static java.util.Collections.emptyList;
 )
 public class CasesController {
 
-    private final CoreCaseDataService coreCaseDataService;
-    private final CaseDetailsConverter caseDetailsConverter;
     private final RoleAssignmentsService roleAssignmentsService;
-
-    @GetMapping(path = {
-        "/{cid}",
-    })
-    @ApiOperation("get case by id from CCD")
-   public ResponseEntity<CaseData> getClaimById(
-        @PathVariable("cid") Long claimId
-    ) {
-        log.info(
-            "Received CaseId: {}",
-            claimId
-        );
-
-        var caseDataResponse = caseDetailsConverter.toCaseData(coreCaseDataService.getCase(claimId).getData());
-        log.info(
-            "CaseDataResponse : {}",
-            caseDataResponse
-        );
-        return new ResponseEntity<>(caseDataResponse, HttpStatus.OK);
-    }
-
-    @GetMapping(path = "/")
-    @ApiOperation("Handles all callbacks from CCD")
-    public ResponseEntity<SearchResult> getList(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization){
-
-        log.info("Received callback from CCD getting claim list");
-
-        Query query = new Query(QueryBuilders.matchQuery("case_type_id", "CIVIL"), emptyList(), 0);
-        SearchResult claims = coreCaseDataService.searchCases(query);
-
-        return new ResponseEntity<>(claims, HttpStatus.OK);
-    }
 
     @GetMapping(path = "/actors/{actorId}")
     @ApiOperation("Gets credentials for actorId from RAS")
