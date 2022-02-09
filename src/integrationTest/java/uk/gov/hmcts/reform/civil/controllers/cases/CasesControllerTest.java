5c18c13ff71d59960f1a03d18536a70a5700afec
Fergal O'Shea <94917860+osheaf01@users.noreply.github.com>
2022-02-09 11:35:09 +0000

    Civ 845 feature ras api call (#613)
    * adding new folder and claim controller
    
    * add controller to fetch claim by id
    
    * removed consumes from controller
    
    * modify endpoint getClaim
    
    * modify endpoint getList
    
    * added tests
    
    * added tests
    
    * updating endpoint getList
    
    * updating endpoint getList
    
    * adding unit test
    
    * organize imports
    
    * merged with CIV-812
    
    * CIV-845 Initial commit, not working due to HTTP 406 error, can fix with Accept="*/*" but want a better solution
    
    * CIV-845 Fixed HTTP 406 error, consumes, not produces
    
    * CIV-845 Rebased onto Sergio's branch
    
    * CIV-845 Removing redundant ClaimsController class, adding unit test.
    
    * CIV-845 Re-adding unit test after rebase onto CIV640
    
    * CIV-845 Rebased onto Sergio's branch
    
    * CIV-845 Fixing duplicate static String error
    
    * CIV-845 Slight rename from RoleAssignment to RoleAssignments
    
    * CIV-845 Removing redundant shell script
    
    * CIV-845 Adding test assertion to unit test
    
    * Refactoring Ras endpoint
    
    * refactoring
    
    * added model on Ras response
    
    * added model on Ras response
    
    * added unit tests
    
    * added unit tests
    
    * removed unused methods
    
    * fixed merge issues
    
    * fixed checkstyle
    
    * renamed RoleAssignmentServiceResponse
    
    * fixed checkstyle
    
    * fixed checkstyle
    
    * fixed checkstyle
    
    Co-authored-by: laarmada <sergiosalmar@yahoo.es>
    Co-authored-by: Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/cases/CasesControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/cases/CasesControllerTest.java
@@ -9,8 +9,12 @@ import uk.gov.hmcts.reform.civil.controllers.BaseIntegrationTest;
 import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
+import uk.gov.hmcts.reform.civil.service.RoleAssignmentsService;
+import uk.gov.hmcts.reform.ras.model.RoleAssignmentResponse;
+import uk.gov.hmcts.reform.ras.model.RoleAssignmentServiceResponse;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -21,6 +25,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class CasesControllerTest extends BaseIntegrationTest {
 
     private static final String CASES_URL = "/cases/{caseId}";
+    private static final String CASES_ACTOR_URL = "/cases/actors/{actorId}";
+    private static final String ACTORID = "1111111";
     private static final String CLAIMS_LIST_URL = "/cases/";
     private static final String ELASTICSEARCH = "{\n"
         + "\"terms\": {\n"
@@ -35,6 +41,9 @@ public class CasesControllerTest extends BaseIntegrationTest {
     @MockBean
     private CaseDetailsConverter caseDetailsConverter;
 
+    @MockBean
+    private RoleAssignmentsService roleAssignmentsService;
+
     @Test
     @SneakyThrows
     public void shouldReturnHttp200() {
@@ -48,6 +57,28 @@ public class CasesControllerTest extends BaseIntegrationTest {
         doGet(BEARER_TOKEN, CASES_URL, 1L)
             .andExpect(content().json(toJson(expectedCaseData)))
             .andExpect(status().isOk());
+    }
+
+    @Test
+    @SneakyThrows
+    public void shouldReturnRASAssignment() {
+        var rasResponse = RoleAssignmentServiceResponse
+            .builder()
+            .roleAssignmentResponse(
+                List.of(RoleAssignmentResponse
+                            .builder()
+                            .actorId(ACTORID)
+                            .build()
+                )
+            )
+            .build();
+
+        when(roleAssignmentsService.getRoleAssignments(anyString(), anyString()))
+            .thenReturn(rasResponse);
+        doGet(BEARER_TOKEN, CASES_ACTOR_URL, ACTORID)
+            .andExpect(content().json(toJson(rasResponse)))
+            .andExpect(status().isOk());
+
     }
 
     @Test
