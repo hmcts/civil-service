fb78ec1f8855005cf995e7fd74290f571789920c
sankaviv1 <sankavi.mohanraj@version1.com>
2022-02-10 14:36:28 +0000

    add static method to build party references

--- src/main/java/uk/gov/hmcts/reform/civil/utils/PartyUtils.java
+++ src/main/java/uk/gov/hmcts/reform/civil/utils/PartyUtils.java
@@ -1,8 +1,10 @@
 package uk.gov.hmcts.reform.civil.utils;
 
 import org.apache.commons.lang.StringUtils;
+import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.LitigationFriend;
 import uk.gov.hmcts.reform.civil.model.Party;
+import uk.gov.hmcts.reform.civil.model.SolicitorReferences;
 
 import java.time.LocalDate;
 import java.util.Optional;
@@ -78,5 +80,27 @@ public class PartyUtils {
             + party.getIndividualFirstName()
             + " "
             + party.getIndividualLastName();
+    }
+
+    public static String buildPartiesReferences(CaseData caseData){
+        SolicitorReferences solicitorReferences = caseData.getSolicitorReferences();
+        StringBuilder stringBuilder = new StringBuilder("");
+
+        if (solicitorReferences.getApplicantSolicitor1Reference() != null) {
+            stringBuilder.append("Claimant Reference ");
+            stringBuilder.append(solicitorReferences.getApplicantSolicitor1Reference());
+            stringBuilder.append("\\n");
+        }
+        if (solicitorReferences.getRespondentSolicitor1Reference() != null) {
+            stringBuilder.append("Defendant Reference ");
+            stringBuilder.append(solicitorReferences.getRespondentSolicitor1Reference());
+            stringBuilder.append("\\n");
+        }
+        if (solicitorReferences.getRespondentSolicitor2Reference() != null) {
+            stringBuilder.append("Defendant 2 Reference ");
+            stringBuilder.append(solicitorReferences.getRespondentSolicitor2Reference());
+            stringBuilder.append("\\n");
+        }
+        return stringBuilder.toString();
     }
 }
