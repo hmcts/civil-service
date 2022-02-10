b975302724cf751b4cce26d7d443e95623a571d7
smita.talawadekar <smita.talawadekar@HMCTS.NET>
2022-02-10 10:47:24 +0000

    CIV-951 Add fee and PaymentDetails to GAPbaDetails

--- src/main/java/uk/gov/hmcts/reform/civil/model/genapplication/GAPbaDetails.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/genapplication/GAPbaDetails.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Setter;
+import uk.gov.hmcts.reform.civil.model.Fee;
+import uk.gov.hmcts.reform.civil.model.PaymentDetails;
 import uk.gov.hmcts.reform.civil.model.common.DynamicList;
 
 @Setter
@@ -14,11 +16,17 @@ public class GAPbaDetails {
 
     private final DynamicList applicantsPbaAccounts;
     private final String pbaReference;
+    private final Fee fee;
+    private final PaymentDetails paymentDetails;
 
     @JsonCreator
     GAPbaDetails(@JsonProperty("applicantsPbaAccounts") DynamicList applicantsPbaAccounts,
-                 @JsonProperty("pbaReference") String pbaReference) {
+                 @JsonProperty("pbaReference") String pbaReference,
+                 @JsonProperty("fee") Fee fee,
+                 @JsonProperty("paymentDetails") PaymentDetails paymentDetails) {
         this.applicantsPbaAccounts = applicantsPbaAccounts;
         this.pbaReference = pbaReference;
+        this.fee = fee;
+        this.paymentDetails = paymentDetails;
     }
 }
