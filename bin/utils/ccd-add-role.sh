2d176381a26f719f1212a2d4abc43cd3a1bf538b
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2022-02-15 12:31:32 +0000

    updating role logs

--- bin/utils/ccd-add-role.sh
+++ bin/utils/ccd-add-role.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eu
+set -ex
 
 dir=$(dirname ${0})
 
@@ -9,7 +9,7 @@ role=${1}
 userToken=$(${dir}/idam-lease-user-token.sh ${CCD_CONFIGURER_IMPORTER_USERNAME:-ccd.docker.default@hmcts.net} ${CCD_CONFIGURER_IMPORTER_PASSWORD:-Password12!})
 serviceToken=$(${dir}/idam-lease-service-token.sh ccd_gw $(docker run --rm toolbelt/oathtool --totp -b ${CCD_API_GATEWAY_S2S_SECRET:-AAAAAAAAAAAAAAAC}))
 
-echo "Creating CCD role: ${role}"
+echo "Creating CCD role: ${role} using ${CCD_DEFINITION_STORE_API_BASE_URL}"
 
 curl --insecure --fail --show-error --silent --output /dev/null -X PUT \
   ${CCD_DEFINITION_STORE_API_BASE_URL:-http://localhost:4451}/api/user-role \
