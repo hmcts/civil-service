2203c8ddaaa0798edb12161fba4d12c25e917b3d
smita.talawadekar <smita.talawadekar@HMCTS.NET>
2022-02-16 11:19:11 +0000

    CIV-951 Fix style issues

--- src/main/java/uk/gov/hmcts/reform/civil/service/InitiateGeneralApplicationService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/InitiateGeneralApplicationService.java
@@ -6,7 +6,6 @@ import uk.gov.hmcts.reform.civil.enums.MultiPartyScenario;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.Fee;
 import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.model.genapplication.GAApplicationType;
 import uk.gov.hmcts.reform.civil.model.genapplication.GAHearingDetails;
@@ -19,8 +18,6 @@ import uk.gov.hmcts.reform.civil.model.genapplication.GAUrgencyRequirement;
 import uk.gov.hmcts.reform.civil.model.genapplication.GeneralApplication;
 import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
