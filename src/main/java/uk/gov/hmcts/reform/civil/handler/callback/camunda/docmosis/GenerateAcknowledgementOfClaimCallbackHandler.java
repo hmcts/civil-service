a866d4ad27d532cae5aec2fcd3b8c5c409f3ae4b
dtortolaV1 <dario.tortolanavarro@hmcts.net>
2022-02-24 09:59:29 +0000

    feat/CIV-284 removed several duplications from GenerateAcknowledgementOfClaimCallbackHandler and GenerateAcknowledgementOfClaimForSpecCallbackHandler

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateAcknowledgementOfClaimCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateAcknowledgementOfClaimCallbackHandler.java
@@ -1,36 +1,32 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.docmosis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
-import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
-import uk.gov.hmcts.reform.civil.callback.Callback;
-import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.model.documents.CaseDocument;
 import uk.gov.hmcts.reform.civil.service.docmosis.aos.AcknowledgementOfClaimGenerator;
 
 import java.util.List;
-import java.util.Map;
 
-import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
-import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.GENERATE_ACKNOWLEDGEMENT_OF_CLAIM;
-import static uk.gov.hmcts.reform.civil.utils.ElementUtils.element;
 
 @Service
-@RequiredArgsConstructor
-public class GenerateAcknowledgementOfClaimCallbackHandler extends CallbackHandler {
+public class GenerateAcknowledgementOfClaimCallbackHandler
+    extends AbstractGenerateAcknowledgementOfClaimCallbackHandler {
 
     private static final List<CaseEvent> EVENTS = List.of(GENERATE_ACKNOWLEDGEMENT_OF_CLAIM);
     private static final String TASK_ID = "AcknowledgeClaimGenerateAcknowledgementOfClaim";
 
     private final AcknowledgementOfClaimGenerator acknowledgementOfClaimGenerator;
-    private final ObjectMapper objectMapper;
+
+    public GenerateAcknowledgementOfClaimCallbackHandler(
+        AcknowledgementOfClaimGenerator acknowledgementOfClaimGenerator,
+        ObjectMapper objectMapper) {
+        super(objectMapper);
+        this.acknowledgementOfClaimGenerator = acknowledgementOfClaimGenerator;
+    }
 
     @Override
     public String camundaActivityId(CallbackParams callbackParams) {
@@ -38,30 +34,12 @@ public class GenerateAcknowledgementOfClaimCallbackHandler extends CallbackHandl
     }
 
     @Override
-    protected Map<String, Callback> callbacks() {
-        return Map.of(callbackKey(ABOUT_TO_SUBMIT), this::prepareAcknowledgementOfClaim);
-    }
-
-    @Override
     public List<CaseEvent> handledEvents() {
         return EVENTS;
     }
 
-    private CallbackResponse prepareAcknowledgementOfClaim(CallbackParams callbackParams) {
-        CaseData caseData = callbackParams.getCaseData();
-        CaseData.CaseDataBuilder caseDataBuilder = caseData.toBuilder();
-
-        CaseDocument acknowledgementOfClaim = acknowledgementOfClaimGenerator.generate(
-            caseData,
-            callbackParams.getParams().get(BEARER_TOKEN).toString()
-        );
-
-        List<Element<CaseDocument>> systemGeneratedCaseDocuments = caseData.getSystemGeneratedCaseDocuments();
-        systemGeneratedCaseDocuments.add(element(acknowledgementOfClaim));
-        caseDataBuilder.systemGeneratedCaseDocuments(systemGeneratedCaseDocuments);
-
-        return AboutToStartOrSubmitCallbackResponse.builder()
-            .data(caseDataBuilder.build().toMap(objectMapper))
-            .build();
+    @Override
+    protected CaseDocument generateAcknowledgementOfClaim(CaseData caseData, String bearerToken) {
+        return acknowledgementOfClaimGenerator.generate(caseData, bearerToken);
     }
 }
--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateAcknowledgementOfClaimForSpecCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateAcknowledgementOfClaimForSpecCallbackHandler.java
@@ -1,36 +1,32 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.docmosis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
-import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
-import uk.gov.hmcts.reform.civil.callback.Callback;
-import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.model.documents.CaseDocument;
 import uk.gov.hmcts.reform.civil.service.docmosis.aos.AcknowledgementOfClaimGeneratorForSpec;
 
 import java.util.List;
-import java.util.Map;
 
-import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
-import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.GENERATE_ACKNOWLEDGEMENT_OF_CLAIM_SPEC;
-import static uk.gov.hmcts.reform.civil.utils.ElementUtils.element;
 
 @Service
-@RequiredArgsConstructor
-public class GenerateAcknowledgementOfClaimForSpecCallbackHandler extends CallbackHandler {
+public class GenerateAcknowledgementOfClaimForSpecCallbackHandler
+    extends AbstractGenerateAcknowledgementOfClaimCallbackHandler {
 
     private static final List<CaseEvent> EVENTS = List.of(GENERATE_ACKNOWLEDGEMENT_OF_CLAIM_SPEC);
     private static final String TASK_ID = "AcknowledgeClaimGenerateAcknowledgementOfClaimForSpec";
 
     private final AcknowledgementOfClaimGeneratorForSpec acknowledgementOfClaimGenerator;
-    private final ObjectMapper objectMapper;
+
+    public GenerateAcknowledgementOfClaimForSpecCallbackHandler(
+        AcknowledgementOfClaimGeneratorForSpec acknowledgementOfClaimGenerator,
+        ObjectMapper objectMapper) {
+        super(objectMapper);
+        this.acknowledgementOfClaimGenerator = acknowledgementOfClaimGenerator;
+    }
 
     @Override
     public String camundaActivityId(CallbackParams callbackParams) {
@@ -38,30 +34,12 @@ public class GenerateAcknowledgementOfClaimForSpecCallbackHandler extends Callba
     }
 
     @Override
-    protected Map<String, Callback> callbacks() {
-        return Map.of(callbackKey(ABOUT_TO_SUBMIT), this::prepareAcknowledgementOfClaim);
-    }
-
-    @Override
     public List<CaseEvent> handledEvents() {
         return EVENTS;
     }
 
-    private CallbackResponse prepareAcknowledgementOfClaim(CallbackParams callbackParams) {
-        CaseData caseData = callbackParams.getCaseData();
-        CaseData.CaseDataBuilder caseDataBuilder = caseData.toBuilder();
-
-        CaseDocument acknowledgementOfClaim = acknowledgementOfClaimGenerator.generate(
-            caseData,
-            callbackParams.getParams().get(BEARER_TOKEN).toString()
-        );
-
-        List<Element<CaseDocument>> systemGeneratedCaseDocuments = caseData.getSystemGeneratedCaseDocuments();
-        systemGeneratedCaseDocuments.add(element(acknowledgementOfClaim));
-        caseDataBuilder.systemGeneratedCaseDocuments(systemGeneratedCaseDocuments);
-
-        return AboutToStartOrSubmitCallbackResponse.builder()
-            .data(caseDataBuilder.build().toMap(objectMapper))
-            .build();
+    @Override
+    protected CaseDocument generateAcknowledgementOfClaim(CaseData caseData, String bearerToken) {
+        return acknowledgementOfClaimGenerator.generate(caseData, bearerToken);
     }
 }
