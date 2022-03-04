dd69db6612ef93227a789ba949d557238e8a36d7
bhssamray <90403505+bhssamray@users.noreply.github.com>
2022-03-04 16:41:16 +0000

    Feat/cmc 1869 reformat date (#701)

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/DefendantResponseApplicantNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/DefendantResponseApplicantNotificationHandler.java
@@ -94,6 +94,7 @@ public class DefendantResponseApplicantNotificationHandler extends CallbackHandl
             default:
                 throw new CallbackException(String.format("Callback handler received illegal event: %s", caseEvent));
         }
+
         if (SPEC_CLAIM.equals(caseData.getSuperClaimType())) {
             sendNotificationToSolicitorSpec(caseData, recipient, caseEvent);
         } else {
@@ -133,8 +134,13 @@ public class DefendantResponseApplicantNotificationHandler extends CallbackHandl
                 RESPONDENT_NAME, getPartyNameBasedOnType(caseData.getRespondent1())
             );
         } else {
+            //if there are 2 respondents on the case, concatenate the names together for the template subject line
             return Map.of(
-                CLAIM_REFERENCE_NUMBER, caseData.getLegacyCaseReference()
+                CLAIM_REFERENCE_NUMBER, caseData.getLegacyCaseReference(),
+                RESPONDENT_NAME,
+                getPartyNameBasedOnType(caseData.getRespondent1())
+                    .concat(" and ")
+                    .concat(getPartyNameBasedOnType(caseData.getRespondent2()))
             );
         }
     }
