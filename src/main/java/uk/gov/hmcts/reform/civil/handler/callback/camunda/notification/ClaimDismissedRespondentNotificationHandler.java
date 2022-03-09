b8f98e697e2f696005180671b4fcdba66bf48995
bhssamray <90403505+bhssamray@users.noreply.github.com>
2022-03-09 13:29:00 +0000

    Feat/cmc 1948 (#675)

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimDismissedRespondentNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimDismissedRespondentNotificationHandler.java
@@ -17,6 +17,7 @@ import java.util.Map;
 
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_RESPONDENT_SOLICITOR1_FOR_CLAIM_DISMISSED;
+import static uk.gov.hmcts.reform.civil.utils.PartyUtils.buildPartiesReferences;
 
 @Service
 @RequiredArgsConstructor
@@ -67,7 +68,8 @@ public class ClaimDismissedRespondentNotificationHandler extends CallbackHandler
     @Override
     public Map<String, String> addProperties(CaseData caseData) {
         return Map.of(
-            CLAIM_REFERENCE_NUMBER, caseData.getLegacyCaseReference()
+            CLAIM_REFERENCE_NUMBER, caseData.getLegacyCaseReference(),
+            PARTY_REFERENCES, buildPartiesReferences(caseData)
         );
     }
 }
