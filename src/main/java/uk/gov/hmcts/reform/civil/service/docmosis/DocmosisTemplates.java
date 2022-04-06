17d895067df9e0adb911e86dbb58e3db5060429b
sankaviv1 <sankavi.mohanraj@version1.com>
2022-04-06 11:51:08 +0100

    update DQ templates

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/DocmosisTemplates.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/DocmosisTemplates.java
@@ -6,12 +6,12 @@ public enum DocmosisTemplates {
     N10("CV-SPEC-ACK-ENG-00001-v01.docx", "acknowledgement_of_service_form_spec_%s.pdf"),
     N1_MULTIPARTY_SAME_SOL("CV-UNS-CLM-ENG-00762.docx", "sealed_claim_form_%s.pdf"),
     N9("CV-UNS-ACK-ENG-00653.docx", "acknowledgement_of_claim_form_%s.pdf"),
-    N181("CV-UNS-HRN-ENG-00911.docx", "%s_directions_questionnaire_form_%s.pdf"),
-    N181_MULTIPARTY_SAME_SOL("CV-UNS-HRN-ENG-00912.docx", "%s_directions_questionnaire_form_%s.pdf"),
+    N181("CV-UNS-HRN-ENG-00968.docx", "%s_directions_questionnaire_form_%s.pdf"),
+    N181_MULTIPARTY_SAME_SOL("CV-UNS-HRN-ENG-00979.docx", "%s_directions_questionnaire_form_%s.pdf"),
     N9_MULTIPARTY_SAME_SOL("CV-UNS-ACK-ENG-00789.docx", "acknowledgement_of_claim_form_%s.pdf"),
     N11("CV-UNS-ACK-ENG-00799.docx", " acknowledgement_of_claim_form_%s.pdf"),
-    N181_2V1("CV-UNS-HRN-ENG-00913.docx", "%s_directions_questionnaire_form_%s.pdf"),
-    N181_CLAIMANT_MULTIPARTY_DIFF_SOLICITOR("CV-UNS-HRN-ENG-00914.docx",
+    N181_2V1("CV-UNS-HRN-ENG-00970.docx", "%s_directions_questionnaire_form_%s.pdf"),
+    N181_CLAIMANT_MULTIPARTY_DIFF_SOLICITOR("CV-UNS-HRN-ENG-00971.docx",
                                             "%s_directions_questionnaire_form_%s.pdf"),
     DEFENDANT_RESPONSE_SPEC("CV-SPEC-RES-ENG-00001.docx", "%s_directions_questionnaire_form_%s.pdf");
 
