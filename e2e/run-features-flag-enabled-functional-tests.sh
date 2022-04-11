52a938d417cb053d3c7e6a4f5a56ceb71b9e6a96
Doug Rice <douglasrice@yahoo.com>
2022-04-11 11:58:57 +0100

    test

--- e2e/run-features-flag-enabled-functional-tests.sh
+++ e2e/run-features-flag-enabled-functional-tests.sh
@@ -6,5 +6,5 @@ echo "Run Functional tests with all feature flags enabled ccd def file"
 dir=$(dirname ${0})
 
 ${dir}/run-unspec-functional-tests.sh
-${dir}/run-spec-functional-tests.sh
-${dir}/run-sdo-functional-tests.sh
+#${dir}/run-spec-functional-tests.sh
+#${dir}/run-sdo-functional-tests.sh
