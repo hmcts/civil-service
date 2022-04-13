99236c640eaaff29a77df7b76241e4b2503de8a7
dtortolaV1 <dario.tortolanavarro@hmcts.net>
2022-04-13 13:17:53 +0100

    feat/CIV-1684 different confirmation screens for full defence and admit scenarios

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/proceed/confirmation/AdmitProceedConfHeader.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.reform.civil.handler.callback.user.spec.proceed.confirmation;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.civil.handler.callback.user.spec.RespondToResponseConfirmationHeaderGenerator;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+
+import java.util.Optional;
+
+@Component
+public class AdmitProceedConfHeader implements RespondToResponseConfirmationHeaderGenerator {
+
+    @Override
+    public Optional<String> generateTextFor(CaseData caseData) {
+        return Optional.empty();
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/proceed/confirmation/AdmitProceedConfText.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.reform.civil.handler.callback.user.spec.proceed.confirmation;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.civil.handler.callback.user.spec.RespondToResponseConfirmationTextGenerator;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+
+import java.util.Optional;
+
+@Component
+public class AdmitProceedConfText implements RespondToResponseConfirmationTextGenerator {
+
+    @Override
+    public Optional<String> generateTextFor(CaseData caseData) {
+        return Optional.empty();
+    }
+}
