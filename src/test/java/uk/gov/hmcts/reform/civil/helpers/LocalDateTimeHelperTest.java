61a8e01c2e71f04c9c3e843559c693fb20856d7d
Harry H <33700332+HarryH96@users.noreply.github.com>
2022-04-19 11:22:15 +0100

    fix: use docstore response for document created time (#822)

--- src/test/java/uk/gov/hmcts/reform/civil/helpers/LocalDateTimeHelperTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/helpers/LocalDateTimeHelperTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.civil.helpers;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,17 +13,13 @@ class LocalDateTimeHelperTest {
         LocalDateTime utcTime = LocalDateTimeHelper.nowInUTC();
         LocalDateTime localTime = LocalDateTimeHelper.nowInLocalZone();
 
-        long difference = ChronoUnit.HOURS.between(utcTime, localTime);
-
-        assertThat(difference).isEqualTo(1);
+        assertThat(localTime.getHour() - utcTime.getHour()).isEqualTo(1);
     }
 
     @Test
     void utcTimeToLocalTimeConversionShouldIncrementByAnHour() {
         LocalDateTime utcTime = LocalDateTimeHelper.nowInUTC();
 
-        long difference = ChronoUnit.HOURS.between(utcTime, LocalDateTimeHelper.fromUTC(utcTime));
-
-        assertThat(difference).isEqualTo(1);
+        assertThat(LocalDateTimeHelper.fromUTC(utcTime).getHour() - utcTime.getHour()).isEqualTo(1);
     }
 }
