f8536067b9053c7291873104f2244b2f48d0aa32
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-04-19 12:03:19 +0100

    Feat: CIV-1812 Remove empty line

--- src/main/java/uk/gov/hmcts/reform/civil/model/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/CaseData.java
@@ -369,7 +369,6 @@ public class CaseData implements MappableObject {
     private final YesOrNo isRespondent1;
     private final YesOrNo isRespondent2;
     private final YesOrNo isApplicant1;
-
     private final YesOrNo claimStarted;
 
 
