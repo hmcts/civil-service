a4cf9398c8e6998fe1e409ec36a224117b09bafc
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-04-20 18:17:25 +0100

    Feat: CIV-1513 Update after merging with master

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/response/confirmation/CounterClaimConfirmationText.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/response/confirmation/CounterClaimConfirmationText.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.civil.handler.callback.user.spec.response.confirmati
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.civil.enums.RespondentResponseTypeSpec;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.user.spec.RespondToClaimConfirmationTextSpecGenerator;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 
@@ -12,7 +13,7 @@ public class CounterClaimConfirmationText implements RespondToClaimConfirmationT
 
     @Override
     public Optional<String> generateTextFor(CaseData caseData) {
-        if (!RespondentResponseTypeSpec.COUNTER_CLAIM.equals(caseData.getRespondent1ClaimResponseTypeForSpec())) {
+        if (!isCounterClaim(caseData)) {
             return Optional.empty();
         }
 
@@ -21,5 +22,21 @@ public class CounterClaimConfirmationText implements RespondToClaimConfirmationT
         sb.append(" Use form N9B to counterclaim, do not create a new claim.");
         sb.append("<br><br><a href=\"https://www.gov.uk/respond-money-claim\" target=\"_blank\">Download form N9B (opens in a new tab)</a>");
         return Optional.of(sb.toString());
+    }
+
+    private boolean isCounterClaim(CaseData caseData) {
+        if (RespondentResponseTypeSpec.COUNTER_CLAIM.equals(caseData.getRespondent1ClaimResponseTypeForSpec())) {
+            if (caseData.getRespondent2() == null) {
+                return true;
+            } else {
+                if (YesOrNo.YES.equals(caseData.getRespondentResponseIsSame())) {
+                    return true;
+                } else {
+                    return RespondentResponseTypeSpec.COUNTER_CLAIM.equals(
+                        caseData.getRespondent2ClaimResponseTypeForSpec());
+                }
+            }
+        }
+        return false;
     }
 }
