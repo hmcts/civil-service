44a5d77c8d0b0dca735c95cf07d36e5a8ed4a721
Darren Bromwich <darrenb@kainos.com>
2022-04-22 21:37:53 +0100

    replace var.common_tags with local.common_tags from ctags module

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,17 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+  common_tags = module.ctags.common_tags
+}
+
+module "ctags" {
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = var.environment
+  product     = var.product
+  builtFrom   = var.builtFrom
+} 
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
   location = var.location
