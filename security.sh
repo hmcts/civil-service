5ebff9c5eb60e083434f5cae3334eb85289bc46e
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2022-04-22 08:23:40 +0100

    upat

--- security.sh
+++ security.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-echo ${TEST_URL}
+echo ${URL_FOR_SECURITY_SCAN}
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 export PYTHONDONTWRITEBYTECODE=1
 
 echo "Run ZAP scan and generate reports"
-zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL --hook=zap_hooks.py -J report.json -r api-report.html
+zap-api-scan.py -t ${URL_FOR_SECURITY_SCAN}/v2/api-docs -f openapi -S -d -u ${SECURITY_RULES} -P 1001 -l FAIL --hook=zap_hooks.py -J report.json -r api-report.html
 echo "Print alerts"
 zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Informational --exit-code False
 
