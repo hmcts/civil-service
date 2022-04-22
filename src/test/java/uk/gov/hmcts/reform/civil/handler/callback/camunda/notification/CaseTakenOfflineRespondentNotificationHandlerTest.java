876d58f0eb98096365bc9848ad4220e50e9ec14f
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2022-04-22 16:02:05 +0100

    Added missing task id tests for CaseTakenOfflineRespondentNotificationHandler

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/CaseTakenOfflineRespondentNotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/CaseTakenOfflineRespondentNotificationHandlerTest.java
@@ -19,11 +19,14 @@ import uk.gov.hmcts.reform.civil.service.NotificationService;
 
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_RESPONDENT_SOLICITOR1_FOR_CASE_TAKEN_OFFLINE;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_RESPONDENT_SOLICITOR2_FOR_CASE_TAKEN_OFFLINE;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.CaseTakenOfflineRespondentNotificationHandler.TASK_ID_RESPONDENT_ONE;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.CaseTakenOfflineRespondentNotificationHandler.TASK_ID_RESPONDENT_TWO;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIM_REFERENCE_NUMBER;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.PARTY_REFERENCES;
 import static uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder.LEGACY_CASE_REFERENCE;
@@ -83,6 +86,19 @@ class CaseTakenOfflineRespondentNotificationHandlerTest extends BaseCallbackHand
                 getNotificationDataMap(caseData),
                 "case-taken-offline-respondent-notification-000DC001"
             );
+        }
+
+        @Test
+        void shouldReturnCorrectCamundaActivityId_whenInvoked() {
+            assertThat(handler.camundaActivityId(CallbackParamsBuilder.builder().request(
+                CallbackRequest.builder().eventId(
+                NOTIFY_RESPONDENT_SOLICITOR1_FOR_CASE_TAKEN_OFFLINE.name()).build()).build()))
+                .isEqualTo(TASK_ID_RESPONDENT_ONE);
+
+            assertThat(handler.camundaActivityId(CallbackParamsBuilder.builder().request(
+                CallbackRequest.builder().eventId(
+                NOTIFY_RESPONDENT_SOLICITOR2_FOR_CASE_TAKEN_OFFLINE.name()).build()).build()))
+                .isEqualTo(TASK_ID_RESPONDENT_TWO);
         }
 
         @NotNull
