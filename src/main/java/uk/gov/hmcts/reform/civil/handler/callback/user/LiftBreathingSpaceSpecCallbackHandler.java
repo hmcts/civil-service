1f67b11d9e044981f70ca29f475caaded4e2a10a
dtortolaV1 <dario.tortolanavarro@hmcts.net>
2022-04-22 13:48:34 +0100

    feat/CIV-216 feat/CIV-221 ensure all "Breathing Space" are capitalized

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/LiftBreathingSpaceSpecCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/LiftBreathingSpaceSpecCallbackHandler.java
@@ -51,11 +51,11 @@ public class LiftBreathingSpaceSpecCallbackHandler extends CallbackHandler {
             AboutToStartOrSubmitCallbackResponse.builder();
         if (caseData.getBreathing() == null || caseData.getBreathing().getEnter() == null) {
             responseBuilder.errors(Collections.singletonList(
-                "A claim must enter breathing space before it can be lifted."
+                "A claim must enter Breathing Space before it can be lifted."
             ));
         } else if (caseData.getBreathing().getLift() != null) {
             responseBuilder.errors(Collections.singletonList(
-                "This claim is not in breathing space anymore."
+                "This claim is not in Breathing Space anymore."
             ));
         }
 
