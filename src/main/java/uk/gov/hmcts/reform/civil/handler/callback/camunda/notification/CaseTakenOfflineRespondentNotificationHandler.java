eb40fcd6b3cb197f98b52e32169fa8b6adc1152b
Gareth Lancaster <90632240+Gareth40343@users.noreply.github.com>
2022-04-25 17:39:35 +0100

    Added notify respondent 2 solicitor task to case taken offline (#886)

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/CaseTakenOfflineRespondentNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/CaseTakenOfflineRespondentNotificationHandler.java
@@ -17,15 +17,20 @@ import java.util.Map;
 
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_RESPONDENT_SOLICITOR1_FOR_CASE_TAKEN_OFFLINE;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_RESPONDENT_SOLICITOR2_FOR_CASE_TAKEN_OFFLINE;
 import static uk.gov.hmcts.reform.civil.utils.PartyUtils.buildPartiesReferences;
 
 @Service
 @RequiredArgsConstructor
 public class CaseTakenOfflineRespondentNotificationHandler extends CallbackHandler implements NotificationData {
 
-    private static final List<CaseEvent> EVENTS = List.of(NOTIFY_RESPONDENT_SOLICITOR1_FOR_CASE_TAKEN_OFFLINE);
+    private static final List<CaseEvent> EVENTS = List.of(
+        NOTIFY_RESPONDENT_SOLICITOR1_FOR_CASE_TAKEN_OFFLINE,
+        NOTIFY_RESPONDENT_SOLICITOR2_FOR_CASE_TAKEN_OFFLINE
+    );
 
-    public static final String TASK_ID = "TakeCaseOfflineNotifyRespondentSolicitor1";
+    public static final String TASK_ID_RESPONDENT_ONE = "TakeCaseOfflineNotifyRespondentSolicitor1";
+    public static final String TASK_ID_RESPONDENT_TWO = "TakeCaseOfflineNotifyRespondentSolicitor2";
     private static final String REFERENCE_TEMPLATE = "case-taken-offline-respondent-notification-%s";
 
     private final NotificationService notificationService;
@@ -40,7 +45,7 @@ public class CaseTakenOfflineRespondentNotificationHandler extends CallbackHandl
 
     @Override
     public String camundaActivityId(CallbackParams callbackParams) {
-        return TASK_ID;
+        return isForRespondentSolicitor1(callbackParams) ? TASK_ID_RESPONDENT_ONE : TASK_ID_RESPONDENT_TWO;
     }
 
     @Override
@@ -50,9 +55,12 @@ public class CaseTakenOfflineRespondentNotificationHandler extends CallbackHandl
 
     private CallbackResponse notifyRespondentSolicitorForCaseTakenOffline(CallbackParams callbackParams) {
         CaseData caseData = callbackParams.getCaseData();
+        String respondentEmail =  isForRespondentSolicitor1(callbackParams)
+            ? caseData.getRespondentSolicitor1EmailAddress()
+            : caseData.getRespondentSolicitor2EmailAddress();
 
         notificationService.sendMail(
-            caseData.getRespondentSolicitor1EmailAddress(),
+            respondentEmail,
             notificationsProperties.getSolicitorCaseTakenOffline(),
             addProperties(caseData),
             String.format(REFERENCE_TEMPLATE, caseData.getLegacyCaseReference())
@@ -66,5 +74,10 @@ public class CaseTakenOfflineRespondentNotificationHandler extends CallbackHandl
             CLAIM_REFERENCE_NUMBER, caseData.getLegacyCaseReference(),
             PARTY_REFERENCES, buildPartiesReferences(caseData)
         );
+    }
+
+    private boolean isForRespondentSolicitor1(CallbackParams callbackParams) {
+        return callbackParams.getRequest().getEventId()
+            .equals(NOTIFY_RESPONDENT_SOLICITOR1_FOR_CASE_TAKEN_OFFLINE.name());
     }
 }
