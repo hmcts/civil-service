526de61688c61d298f720b2d171ab357aa057ade
Mark Drummond <mark.drummond@version1.com>
2022-05-06 10:56:55 +0100

    New event handler for StandardDirectionOrderDJ and unit tests

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/StandardDirectionOrderDJ.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/StandardDirectionOrderDJ.java
@@ -6,24 +6,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
-import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
-import uk.gov.hmcts.reform.civil.model.BusinessProcess;
+import uk.gov.hmcts.reform.civil.enums.MultiPartyScenario;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.Party;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import static java.lang.String.format;
-import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
-import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
-import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.*;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.STANDARD_DIRECTION_ORDER_DJ;
+import static uk.gov.hmcts.reform.civil.enums.MultiPartyScenario.getMultiPartyScenario;
 
 @Service
 @RequiredArgsConstructor
@@ -31,11 +27,12 @@ public class StandardDirectionOrderDJ extends CallbackHandler {
 
     private static final List<CaseEvent> EVENTS = Collections.singletonList(STANDARD_DIRECTION_ORDER_DJ);
     private final ObjectMapper objectMapper;
+    String participantString;
 
     @Override
     protected Map<String, Callback> callbacks() {
         return new ImmutableMap.Builder<String, Callback>()
-            .put(callbackKey(ABOUT_TO_START), this::emptyCallbackResponse)
+            .put(callbackKey(ABOUT_TO_START), this::initiateSDO)
             .put(callbackKey(ABOUT_TO_SUBMIT), this::emptySubmittedCallbackResponse)
             .put(callbackKey(SUBMITTED), this::emptySubmittedCallbackResponse)
             .build();
@@ -46,5 +43,38 @@ public class StandardDirectionOrderDJ extends CallbackHandler {
         return EVENTS;
     }
 
+    private CallbackResponse initiateSDO(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        CaseData.CaseDataBuilder caseDataBuilder = caseData.toBuilder();
+
+        caseDataBuilder.applicantVRespondentText(caseParticipants(caseData));
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDataBuilder.build().toMap(objectMapper))
+            .build();
+
+    }
+
+    public String caseParticipants(CaseData caseData) {
+        MultiPartyScenario multiPartyScenario = getMultiPartyScenario(caseData);
+        switch (multiPartyScenario) {
+
+            case ONE_V_ONE:
+                participantString = (caseData.getApplicant1().getPartyName() + " v " + caseData.getRespondent1()
+                    .getPartyName());
+                break;
+            case ONE_V_TWO_ONE_LEGAL_REP:
+                participantString = (caseData.getApplicant1().getPartyName() + " v " + caseData.getRespondent1()
+                    .getPartyName() + " and " + caseData.getRespondent2().getPartyName());
+                break;
+
+            case TWO_V_ONE:
+                participantString = (caseData.getApplicant1().getPartyName() + " and " + caseData.getApplicant2()
+                    .getPartyName() + " v " + caseData.getRespondent1().getPartyName());
+                break;
+        }
+        return participantString;
+
+    }
 
 }
