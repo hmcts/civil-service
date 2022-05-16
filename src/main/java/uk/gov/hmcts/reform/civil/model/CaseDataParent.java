3d16fca5222d7b368cffbfb2b4e44f19f5d72a87
Sampan Kumar <sampan.kumar@hmcts.net>
2022-05-16 13:48:40 +0100

    CIV-1015 - checkstyle issues

--- src/main/java/uk/gov/hmcts/reform/civil/model/CaseDataParent.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/CaseDataParent.java
@@ -10,8 +10,8 @@ import uk.gov.hmcts.reform.civil.enums.RespondentResponseTypeSpec;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.model.common.MappableObject;
-import uk.gov.hmcts.reform.civil.model.dq.Witness;
 import uk.gov.hmcts.reform.civil.model.documents.Document;
+import uk.gov.hmcts.reform.civil.model.dq.Witness;
 
 import java.util.ArrayList;
 import java.util.List;
