b8ffaf9de8bf85a964f86bc9335f0a1f9716e581
douglasrice <douglasrice@yahoo.com>
2022-05-27 17:19:06 +0100

    Revert "Changed the content of email and respective test (#1012)" (#1040)
    This reverts commit 8363aa0a656b9509da8fdfad627af11e8fe5a485.

--- src/main/java/uk/gov/hmcts/reform/civil/config/properties/notification/NotificationsProperties.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/properties/notification/NotificationsProperties.java
@@ -16,7 +16,7 @@ public class NotificationsProperties {
     private String respondentSolicitorClaimIssueMultipartyEmailTemplate;
 
     @NotEmpty
-    private String respondentSolicitorClaimDetailsEmailTemplate;
+    private String respondentSolicitorClaimDetailsEmailTemplateMultiParty;
 
     @NotEmpty
     private String solicitorDefendantResponseCaseTakenOffline;
