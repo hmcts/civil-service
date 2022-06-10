f122aca22a9f6f3ba99acf5a47414ba70bc8eeda
Mark Drummond <mark.drummond@version1.com>
2022-06-10 15:42:20 +0100

    default judgment trial/fasttrack screen sdo and tests

---
+++ src/main/java/uk/gov/hmcts/reform/civil/model/defaultjudgment/TrialBuildingDispute.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.civil.model.defaultjudgment;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Future;
+import java.time.LocalDate;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class TrialBuildingDispute {
+
+    private String input1;
+    private String input2;
+    private String input3;
+    @Future(message = "The date entered must be in the future")
+    private LocalDate date1;
+    private String input4;
+    @Future(message = "The date entered must be in the future")
+    private LocalDate date2;
+}
