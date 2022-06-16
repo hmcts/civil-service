98369c7f3820dbd5475f817303c8b53eb3544662
sankaviv1 <sankavi.mohanraj@version1.com>
2022-06-16 14:04:40 +0100

    remove caching

--- src/main/java/uk/gov/hmcts/reform/civil/service/CustomScopeIdamTokenGeneratorService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/CustomScopeIdamTokenGeneratorService.java
@@ -25,7 +25,6 @@ public class CustomScopeIdamTokenGeneratorService {
         this.oauth2Configuration = oauth2Configuration;
     }
 
-    @Cacheable(value = "accessTokenCache")
     public String getAccessToken(String username, String password) {
         return BEARER_AUTH_TYPE + " " + getAccessTokenResponse(username, password).accessToken;
     }
@@ -44,44 +43,4 @@ public class CustomScopeIdamTokenGeneratorService {
                 null
             ));
     }
-
-
-    /* TODO: remove this if above works
-    private final UserIdamTokenGeneratorInfo systemUserIdamInfo;
-    private final IdamWebApi idamWebApi;
-
-    public IdamTokenGenerator(UserIdamTokenGeneratorInfo systemUserIdamInfo,
-                              IdamWebApi idamWebApi) {
-        this.systemUserIdamInfo = systemUserIdamInfo;
-        this.idamWebApi = idamWebApi;
-    }
-
-    @Cacheable(cacheNames = "idam_sys_user_token_cache_generate", key = "'system_user_token'", sync = true)
-    public String generate() {
-        return getUserBearerToken(
-            systemUserIdamInfo.getUserName(),
-            systemUserIdamInfo.getUserPassword()
-        );
-    }
-
-    @Cacheable(value = "idam_sys_user_token_cache", key = "#username", sync = true)
-    public String getUserBearerToken(String username, String password) {
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("grant_type", "password");
-        map.add("redirect_uri", systemUserIdamInfo.getIdamRedirectUrl());
-        map.add("client_id", systemUserIdamInfo.getIdamClientId());
-        map.add("client_secret", systemUserIdamInfo.getIdamClientSecret());
-        map.add("username", username);
-        map.add("password", password);
-        map.add("scope", systemUserIdamInfo.getIdamScope());
-        Token tokenResponse = idamWebApi.token(map);
-
-        return "Bearer " + tokenResponse.getAccessToken();
-    }
-
-    @Cacheable(value = "idam_sys_user_user_info_cache", key = "#bearerAccessToken", sync = true)
-    public UserInfo getUserInfo(String bearerAccessToken) {
-        return idamWebApi.userInfo(bearerAccessToken);
-    }
-    */
 }
