05482033afe4ca6df0c4b90d06228583652d5e44
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-06-23 16:17:28 +0100

    CIV-3120 refactor rename static variable for test

--- src/test/java/uk/gov/hmcts/reform/cmc/model/CmcClaimTest.java
+++ src/test/java/uk/gov/hmcts/reform/cmc/model/CmcClaimTest.java
@@ -7,18 +7,18 @@ import java.util.Collections;
 
 public class CmcClaimTest {
 
-    private static final String PARTY_NAME = "Mr John Clark";
+    private static final String NAME = "Mr John Clark";
 
     @Test
     void shouldReturnClaimantNameWhenClaimantExists() {
         CmcClaim cmcClaim = CmcClaim.builder()
             .claimData(ClaimData.builder().claimants(Arrays.asList(CmcParty.builder()
-                                                                       .name(PARTY_NAME)
+                                                                       .name(NAME)
                                                                        .build()))
                            .build())
             .build();
         String claimantName = cmcClaim.getClaimantName();
-        assert (claimantName).equals(PARTY_NAME);
+        assert (claimantName).equals(NAME);
     }
 
     @Test
@@ -33,12 +33,12 @@ public class CmcClaimTest {
     @Test
     void shouldReturnDefendantNameWhenDefendantExists() {
         CmcClaim cmcClaim = CmcClaim.builder()
-            .claimData(ClaimData.builder().defendants(Arrays.asList(CmcParty.builder().name(PARTY_NAME)
+            .claimData(ClaimData.builder().defendants(Arrays.asList(CmcParty.builder().name(NAME)
                                                                         .build()))
                            .build())
             .build();
         String defendantName = cmcClaim.getDefendantName();
-        assert (defendantName).equals(PARTY_NAME);
+        assert (defendantName).equals(NAME);
     }
 
     @Test
