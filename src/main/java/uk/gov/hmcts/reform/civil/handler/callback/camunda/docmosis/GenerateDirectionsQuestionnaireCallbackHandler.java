66c184bafc3033b2d20ff18ba6c9cd1b7436e7df
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-06-27 12:03:32 +0100

    CIV-2662: Remove TODO

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateDirectionsQuestionnaireCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateDirectionsQuestionnaireCallbackHandler.java
@@ -220,7 +220,6 @@ public class GenerateDirectionsQuestionnaireCallbackHandler extends CallbackHand
                     "TWO"
                 ).ifPresent(document -> {
                     updatedDocuments.add(element(document));
-                    // TODO this field will probably be created during I2P 1v2 different solicitor
                     caseDataBuilder.respondent2GeneratedResponseDocument(document);
                 });
             }
