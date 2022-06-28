104fee1d2de9e250b9a687cfc55647ca6985c5fb
Gulam Hussain <94545342+Gulam-Hussain@users.noreply.github.com>
2022-06-28 12:27:38 +0100

    populate sdo small claims fields (#1148)
    * populate sdo small claims fields
    
    * fix sonar check

--- src/main/java/uk/gov/hmcts/reform/civil/model/sdo/SmallClaimsCreditHire.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/sdo/SmallClaimsCreditHire.java
@@ -21,10 +21,6 @@ public class SmallClaimsCreditHire {
     private String input5;
     private String input6;
     private String input7;
-    private String input8;
-    private String input9;
-    private String input10;
-    private String input11;
     @Future(message = "The date entered must be in the future")
     private LocalDate date1;
     @Future(message = "The date entered must be in the future")
@@ -33,8 +29,4 @@ public class SmallClaimsCreditHire {
     private LocalDate date3;
     @Future(message = "The date entered must be in the future")
     private LocalDate date4;
-    @Future(message = "The date entered must be in the future")
-    private LocalDate date5;
-    @Future(message = "The date entered must be in the future")
-    private LocalDate date6;
 }
