c97792c2813739482bca21b6d7e64f31d8838645
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-06-29 16:10:03 +0100

    CIV-2663: Update I2P document for defendant details

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/dq/DirectionsQuestionnaireGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/dq/DirectionsQuestionnaireGenerator.java
@@ -8,6 +8,7 @@ import uk.gov.hmcts.reform.civil.enums.AllocatedTrack;
 import uk.gov.hmcts.reform.civil.enums.CaseState;
 import uk.gov.hmcts.reform.civil.enums.ExpertReportsSent;
 import uk.gov.hmcts.reform.civil.enums.MultiPartyScenario;
+import uk.gov.hmcts.reform.civil.enums.RespondentResponseTypeSpec;
 import uk.gov.hmcts.reform.civil.enums.SuperClaimType;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.enums.dq.Language;
@@ -42,6 +43,7 @@ import uk.gov.hmcts.reform.civil.service.flowstate.StateFlowEngine;
 import uk.gov.hmcts.reform.civil.utils.DocmosisTemplateDataUtils;
 import uk.gov.hmcts.reform.civil.utils.ElementUtils;
 import uk.gov.hmcts.reform.civil.utils.MonetaryConversions;
+
 
 import java.text.NumberFormat;
 import java.time.LocalDateTime;
@@ -573,8 +575,8 @@ public class DirectionsQuestionnaireGenerator implements TemplateDataGenerator<D
                 if ((ONE_V_TWO_ONE_LEGAL_REP.equals(getMultiPartyScenario(caseData))
                     && YES.equals(caseData.getRespondentResponseIsSame()))
                     || (ONE_V_TWO_TWO_LEGAL_REP.equals(getMultiPartyScenario(caseData))
-                    && FULL_DEFENCE.equals(caseData.getRespondent1ClaimResponseTypeForSpec())
-                    && FULL_DEFENCE.equals(caseData.getRespondent2ClaimResponseTypeForSpec()))
+                    && RespondentResponseTypeSpec.FULL_DEFENCE.equals(caseData.getRespondent1ClaimResponseTypeForSpec())
+                    && RespondentResponseTypeSpec.FULL_DEFENCE.equals(caseData.getRespondent2ClaimResponseTypeForSpec()))
                     ) {
                     respondents.add(Party.builder()
                                         .name(caseData.getRespondent1().getPartyName())
