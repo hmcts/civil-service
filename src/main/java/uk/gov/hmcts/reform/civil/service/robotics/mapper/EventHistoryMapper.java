d27639fe022b895655589177822f6d5e8bbba03e
kenneth-hmcts <91327278+kenneth-hmcts@users.noreply.github.com>
2022-07-01 13:29:35 +0100

    Inform Agree Extension Date For Spec RPA (#1184)
    * Update for Information Agree Extension Date For Spec
    
    * Bumping chart version/ fixing aliases
    
    * Update Suppressions
    
    * Update Suppressions
    
    Co-authored-by: hmcts-jenkins-a-to-c <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/civil/service/robotics/mapper/EventHistoryMapper.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/robotics/mapper/EventHistoryMapper.java
@@ -233,6 +233,7 @@ public class EventHistoryMapper {
                 }
             }
         }
+        buildInformAgreedExtensionDateForSpec(builder, caseData);
 
         return eventHistorySequencer.sortEvents(builder.build());
     }
@@ -1586,6 +1587,14 @@ public class EventHistoryMapper {
         }
 
         builder.consentExtensionFilingDefence(events);
+    }
+
+    private void buildInformAgreedExtensionDateForSpec(EventHistory.EventHistoryBuilder builder, CaseData caseData) {
+        if (SPEC_CLAIM.equals(caseData.getSuperClaimType())
+            && (caseData.getRespondentSolicitor1AgreedDeadlineExtension() != null
+            || caseData.getRespondentSolicitor2AgreedDeadlineExtension() != null)) {
+            buildConsentExtensionFilingDefence(builder, caseData);
+        }
     }
 
     private Event buildConsentExtensionFilingDefenceEvent(
