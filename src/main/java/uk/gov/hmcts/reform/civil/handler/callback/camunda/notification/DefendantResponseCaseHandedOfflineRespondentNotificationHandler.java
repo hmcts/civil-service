df907b78dec9f7f665470725f1d5cc923cc2374d
indu2019 <indu.chaturvedi@hmcts.net>
2022-07-09 18:17:19 +0100

    fix CIV-2649-template-issue, fixed testcases

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/DefendantResponseCaseHandedOfflineRespondentNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/DefendantResponseCaseHandedOfflineRespondentNotificationHandler.java
@@ -82,8 +82,8 @@ public class DefendantResponseCaseHandedOfflineRespondentNotificationHandler ext
         } else {
             //Use Multiparty Template as there are 2 defendant responses
             if (MultiPartyScenario.getMultiPartyScenario(caseData).equals(MultiPartyScenario.ONE_V_TWO_TWO_LEGAL_REP)
-            && (RespondentResponseTypeSpec.COUNTER_CLAIM.equals(caseData.getRespondent1ClaimResponseTypeForSpec()))
-            || RespondentResponseTypeSpec.COUNTER_CLAIM.equals(caseData.getRespondent2ClaimResponseTypeForSpec())) {
+                && (RespondentResponseTypeSpec.COUNTER_CLAIM.equals(caseData.getRespondent1ClaimResponseTypeForSpec()))
+                || RespondentResponseTypeSpec.COUNTER_CLAIM.equals(caseData.getRespondent2ClaimResponseTypeForSpec())) {
                 templateID = notificationsProperties.getRespondentSolicitorDefendantResponseForSpec();
 
             } else {
