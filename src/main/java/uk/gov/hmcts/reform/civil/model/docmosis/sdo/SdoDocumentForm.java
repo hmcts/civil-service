4dc3a28120234d0617184432508632ea9d8d0be0
dtortolaV1 <dario.tortolanavarro@hmcts.net>
2022-07-19 08:32:21 +0100

    CIV-2094 add fields to form, add call to SdoGeneratorService to generate

--- src/main/java/uk/gov/hmcts/reform/civil/model/docmosis/sdo/SdoDocumentForm.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/docmosis/sdo/SdoDocumentForm.java
@@ -4,7 +4,7 @@ import uk.gov.hmcts.reform.civil.model.common.MappableObject;
 
 /**
  * class to hold fields common to sdo document for fast claims, small claims
- * and disposal orders
+ * and disposal orders.
  */
 public abstract class SdoDocumentForm implements MappableObject {
 }
