d31f6b40186d5c783bd4c94051e926774dfd5b34
Sampan Kumar <sampan.kumar@hmcts.net>
2022-07-19 16:01:44 +0100

    Change RPA subject for LR spec journey

--- src/main/java/uk/gov/hmcts/reform/civil/service/robotics/RoboticsNotificationService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/robotics/RoboticsNotificationService.java
@@ -103,12 +103,23 @@ public class RoboticsNotificationService {
     }
 
     private String getSubject(CaseData caseData, String triggerEvent, boolean isMultiParty) {
-        return isMultiParty ? String.format("Multiparty claim data for %s - %s - %s", caseData.getLegacyCaseReference(),
-                                            caseData.getCcdState(), triggerEvent
-        ) : String.format(
-            "Robotics case data for %s",
-            caseData.getLegacyCaseReference()
-        );
+        if(SPEC_CLAIM.equals(caseData.getSuperClaimType())) {
+            return isMultiParty ? String.format("Multiparty LR v LR Case Data for %s - %s - %s", caseData.getLegacyCaseReference(),
+                                                caseData.getCcdState(), triggerEvent
+            ) : String.format(
+                "LR v LR Case Data for %s",
+                caseData.getLegacyCaseReference()
+            );
+        } else {
+            return isMultiParty ? String.format("Multiparty claim data for %s - %s - %s", caseData.getLegacyCaseReference(),
+                                                caseData.getCcdState(), triggerEvent
+            ) : String.format(
+                "Robotics case data for %s",
+                caseData.getLegacyCaseReference()
+            );
+        }
+
+
     }
 
     private String getRoboticsEmailRecipient(boolean isMultiParty, SuperClaimType superClaimType) {
