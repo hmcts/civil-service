a013c90793a0e0829c26917b3b019b7d1f60faf8
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-07-25 09:20:32 +0100

    CIV-3021: Update for PIN generation

--- src/main/java/uk/gov/hmcts/reform/civil/model/CaseToPostLRspec.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/CaseToPostLRspec.java
@@ -6,23 +6,29 @@ import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 
+import java.time.LocalDate;
+
 @Data
 @Builder
 public class CaseToPostLRspec {
 
     private final String accessCode;
     private final String respondentCaseRole;
-    // private final LocalDate expiryDate;
-    // private final String citizenCaseRole;
+    private final LocalDate expiryDate;
+    private final String citizenCaseRole;
     private final YesOrNo pinUsedFlg;
 
     @JsonCreator
     public CaseToPostLRspec(@JsonProperty("accessCode") String accessCode,
                             @JsonProperty("respondentCaseRole") String respondentCaseRole,
+                            @JsonProperty("expiryDate") LocalDate expiryDate,
+                            @JsonProperty("citizenCaseRole") String citizenCaseRole,
                             @JsonProperty("pinUsedFlg") YesOrNo pinUsedFlg
                             ) {
         this.accessCode = accessCode;
         this.respondentCaseRole = respondentCaseRole;
+        this.expiryDate = expiryDate;
+        this.citizenCaseRole = citizenCaseRole;
         this.pinUsedFlg = pinUsedFlg;
     }
 }
