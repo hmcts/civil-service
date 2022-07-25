4808563c4cac1a4b17266e7bf3564ee7e57b4aa3
Sampan Kumar <sampan.kumar@hmcts.net>
2022-07-25 11:27:57 +0100

    LR email for robotics adding logs

--- src/main/java/uk/gov/hmcts/reform/civil/service/robotics/RoboticsNotificationService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/robotics/RoboticsNotificationService.java
@@ -103,14 +103,17 @@ public class RoboticsNotificationService {
     }
 
     private String getSubject(CaseData caseData, String triggerEvent, boolean isMultiParty) {
+        String subject = null;
         if (SPEC_CLAIM.equals(caseData.getSuperClaimType())) {
-            return isMultiParty ? String.format("Multiparty LR v LR Case Data for %s - %s - %s",
+            subject = isMultiParty ? String.format("Multiparty LR v LR Case Data for %s - %s - %s",
                                                 caseData.getLegacyCaseReference(),
                                                 caseData.getCcdState(), triggerEvent
             ) : String.format(
                 "LR v LR Case Data for %s",
                 caseData.getLegacyCaseReference()
             );
+            log.info("Subject--------" + subject);
+            return subject;
         } else {
             return isMultiParty ? String.format("Multiparty claim data for %s - %s - %s",
                                                 caseData.getLegacyCaseReference(),
@@ -124,6 +127,7 @@ public class RoboticsNotificationService {
 
     private String getRoboticsEmailRecipient(boolean isMultiParty, SuperClaimType superClaimType) {
         if (SPEC_CLAIM.equals(superClaimType)) {
+            log.info("EMAIl:---------" + roboticsEmailConfiguration.getSpecRecipient());
             return roboticsEmailConfiguration.getSpecRecipient();
         }
         return isMultiParty ? roboticsEmailConfiguration
