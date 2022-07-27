9485a01cc62f30fc844357069d9400a1e697c82b
Sampan Kumar <sampan.kumar@hmcts.net>
2022-07-27 13:46:44 +0100

    Remove logging case details

--- src/main/java/uk/gov/hmcts/reform/civil/service/stitching/CivilDocumentStitchingService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/stitching/CivilDocumentStitchingService.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.civil.service.stitching;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
