d537ecf627f568e4ff690cf47c265c14cca3dba5
indu2019 <indu.chaturvedi@hmcts.net>
2022-07-28 14:03:28 +0100

    fixed stuck seal claim issue, added testcases for coverage

--- src/main/java/uk/gov/hmcts/reform/civil/service/documentmanagement/ClaimFormService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/documentmanagement/ClaimFormService.java
@@ -49,7 +49,7 @@ public class ClaimFormService {
         if (documentMetaDataList.size() > 1) {
             CaseDocument stitchedDocument = civilDocumentStitchingService.bundle(
                 documentMetaDataList,
-                authorisation,  // access token will be taken from postman
+                authorisation,  //access token will be taken from postman
                 sealClaimForm.getDocumentName(),
                 sealClaimForm.getDocumentName(),
                 caseData
