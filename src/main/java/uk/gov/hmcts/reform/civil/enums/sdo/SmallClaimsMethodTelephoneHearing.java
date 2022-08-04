11e37c7e48d367b3306fe0ecbe24d4341fa31c80
Gulam Hussain <94545342+Gulam-Hussain@users.noreply.github.com>
2022-08-04 16:27:52 +0100

    revise small claims page sdo (#1307)

--- src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/SmallClaimsMethodTelephoneHearing.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/SmallClaimsMethodTelephoneHearing.java
@@ -1,16 +1,7 @@
 package uk.gov.hmcts.reform.civil.enums.sdo;
 
-import lombok.Getter;
-
-@Getter
 public enum SmallClaimsMethodTelephoneHearing {
-    telephoneTheClaimant("the claimant"),
-    telephoneTheDefendant("the defendant"),
-    telephoneTheCourt("the court");
-
-    private final String label;
-
-    SmallClaimsMethodTelephoneHearing(String value) {
-        this.label = value;
-    }
+    telephoneTheClaimant,
+    telephoneTheDefendant,
+    telephoneTheCourt
 }
