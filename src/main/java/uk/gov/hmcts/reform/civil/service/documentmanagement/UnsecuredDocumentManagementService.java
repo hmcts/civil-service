9f22bd85e2e0d89ceefb5f2e62799d1156cf4ea6
indu2019 <indu.chaturvedi@hmcts.net>
2022-08-04 09:10:45 +0100

    fixed stuck seal claim issue, trying to submit the call,committing some code so I dont lose the changes

--- src/main/java/uk/gov/hmcts/reform/civil/service/documentmanagement/UnsecuredDocumentManagementService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/documentmanagement/UnsecuredDocumentManagementService.java
@@ -52,7 +52,6 @@ public class UnsecuredDocumentManagementService implements DocumentManagementSer
     @Override
     public CaseDocument uploadDocument(String authorisation, PDF pdf) {
         String originalFileName = pdf.getFileBaseName();
-        log.info("Inside SecuredDocumentManagementService for uploading document " + originalFileName);
         log.info("Uploading file {}", originalFileName);
         try {
             MultipartFile file
