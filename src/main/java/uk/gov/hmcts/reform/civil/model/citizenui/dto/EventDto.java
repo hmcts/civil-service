0029d70a004a5361ab178e6ac6e643e5749ab125
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-08-10 16:51:13 +0100

    deserealise updated map

--- src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/dto/EventDto.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/dto/EventDto.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.model.citizenui.dto;
 
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,5 +17,10 @@ public class EventDto {
 
     private CaseEvent event;
     private Map<String, Object> caseDataUpdate;
+
+    @JsonAnySetter
+    public void addUpdate(String key, Object value) {
+        caseDataUpdate.put(key, value);
+    }
 
 }
