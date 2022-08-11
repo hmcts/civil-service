47d64dc76218abf91c2c9bcbf582beefd6c4ca7c
dtortolaV1 <dario.tortolanavarro@hmcts.net>
2022-08-11 11:07:25 +0100

    feat/CIV-4106 trigger Camunda process when the event was initiated by a judge

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/NotSuitableSDOCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/NotSuitableSDOCallbackHandler.java
@@ -11,28 +11,24 @@ import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
-import uk.gov.hmcts.reform.civil.enums.YesOrNo;
-import uk.gov.hmcts.reform.civil.enums.sdo.ClaimsTrack;
-import uk.gov.hmcts.reform.civil.enums.sdo.OrderDetailsPagesSectionsToggle;
-import uk.gov.hmcts.reform.civil.enums.sdo.OrderType;
 import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.HearingSupportRequirementsDJ;
-import uk.gov.hmcts.reform.civil.model.Party;
-import uk.gov.hmcts.reform.civil.model.sdo.*;
-import uk.gov.hmcts.reform.civil.service.referencedata.LocationRefDataService;
 import uk.gov.hmcts.reform.civil.model.sdo.ReasonNotSuitableSDO;
+import uk.gov.hmcts.reform.civil.service.RoleAssignmentsService;
+import uk.gov.hmcts.reform.civil.service.UserService;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
+import uk.gov.hmcts.reform.ras.model.RoleAssignmentServiceResponse;
 
-import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import static java.lang.String.format;
 import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
-import static uk.gov.hmcts.reform.civil.callback.CallbackType.*;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.MID;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NotSuitable_SDO;
-import static uk.gov.hmcts.reform.civil.model.common.DynamicList.fromList;
 
 @Service
 @RequiredArgsConstructor
@@ -45,7 +41,8 @@ public class NotSuitableSDOCallbackHandler extends CallbackHandler {
         + "%n%nIf a legal advisor has submitted this information a notification will be sent to a judge for review.";
 
     private final ObjectMapper objectMapper;
-    private final LocationRefDataService locationRefDataService;
+    private final UserService userService;
+    private final RoleAssignmentsService roleAssignmentsService;
 
     @Override
     protected Map<String, Callback> callbacks() {
@@ -73,24 +70,31 @@ public class NotSuitableSDOCallbackHandler extends CallbackHandler {
         CaseData caseData = callbackParams.getCaseData();
         CaseData.CaseDataBuilder<?, ?> updatedData = caseData.toBuilder();
 
-
         ReasonNotSuitableSDO tempReasonNotSuitableSDO = ReasonNotSuitableSDO.builder()
             .input("")
             .build();
 
         updatedData.reasonNotSuitableSDO(tempReasonNotSuitableSDO).build();
 
-
-
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(updatedData.build().toMap(objectMapper))
             .build();
     }
 
-
-
     private CallbackResponse submitNotSuitableSDO(CallbackParams callbackParams) {
         CaseData.CaseDataBuilder dataBuilder = getSharedData(callbackParams);
+
+        String authorisation = callbackParams.getParams().get(BEARER_TOKEN).toString();
+        UserInfo user = userService.getUserInfo(authorisation);
+        RoleAssignmentServiceResponse roleAssignments = roleAssignmentsService.getRoleAssignments(
+            user.getUid(),
+            authorisation
+        );
+
+        if (roleAssignments.getRoleAssignmentResponse().stream()
+            .anyMatch(assignment -> "judge".equalsIgnoreCase(assignment.getRoleName()))) {
+            dataBuilder.businessProcess(BusinessProcess.ready(CaseEvent.UNSUITABLE_FOR_SDO_JUDGE));
+        }
 
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(dataBuilder.build().toMap(objectMapper))
@@ -122,6 +126,5 @@ public class NotSuitableSDOCallbackHandler extends CallbackHandler {
     private String getBody(CaseData caseData) {
         return format(NotSuitableSDO_CONFIRMATION_BODY);
     }
-
 
 }
