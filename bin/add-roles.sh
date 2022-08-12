ae0cbeaf675072779eeff7ce0e2f17604f25442b
Sampan Kumar <sampan.kumar@hmcts.net>
2022-08-12 10:38:40 +0100

    adding new access profile

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -9,6 +9,9 @@
 ./bin/utils/ccd-add-role.sh "basic-access" #remove after GS_profile is added
 ./bin/utils/ccd-add-role.sh "GS_profile"
 ./bin/utils/ccd-add-role.sh "legal-adviser"
+./bin/utils/ccd-add-role.sh "caseworker-ras-validation"
+./bin/utils/ccd-add-role.sh "admin-access"
+./bin/utils/ccd-add-role.sh "full-access"
 
 roles=("solicitor" "systemupdate" "admin" "staff")
 for role in "${roles[@]}"
