166d8decb87dc0e952796e67158797479d79f6a5
OrtizP <pablo.gutierrez@version1.com>
2022-08-12 14:29:56 +0200

    - Refactor of methods from Pin in Post to a Sevice - Remove of pinUsed attribute

--- src/main/java/uk/gov/hmcts/reform/civil/service/DefendantPinToPostLRspecService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/DefendantPinToPostLRspecService.java
@@ -3,28 +3,16 @@ package uk.gov.hmcts.reform.civil.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.enums.CaseRole;
-import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.CaseNote;
 import uk.gov.hmcts.reform.civil.model.DefendantPinToPostLRspec;
-import uk.gov.hmcts.reform.civil.model.common.DynamicList;
-import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.utils.AccessCodeGenerator;
-import uk.gov.hmcts.reform.idam.client.IdamClient;
-import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-
-import static com.google.common.collect.Lists.newArrayList;
-import static java.util.Objects.isNull;
-import static java.util.Optional.ofNullable;
-import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
-import static uk.gov.hmcts.reform.civil.utils.ElementUtils.element;
 
 @Service
 @RequiredArgsConstructor
+
 public class DefendantPinToPostLRspecService {
+
     public DefendantPinToPostLRspec buildDefendantPinToPost() {
         LocalDate expiryDate = LocalDate.now().plusDays(180);
         return DefendantPinToPostLRspec.builder()
@@ -32,7 +20,6 @@ public class DefendantPinToPostLRspecService {
             .respondentCaseRole(
                 CaseRole.RESPONDENTSOLICITORONESPEC.getFormattedName())
             .expiryDate(expiryDate)
-            .pinUsed(NO)
             .build();
     }
 
