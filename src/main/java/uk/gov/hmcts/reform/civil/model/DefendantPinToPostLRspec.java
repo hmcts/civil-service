fbcfddd4d95cc43387492567eb2be12dfa5f5c97
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-08-15 12:19:27 +0100

    CIV-2636: Remove unused import

--- src/main/java/uk/gov/hmcts/reform/civil/model/DefendantPinToPostLRspec.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/DefendantPinToPostLRspec.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
-import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 
 import java.time.LocalDate;
 
