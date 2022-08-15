2c5da9b860fda9ff9f485c6bc1048f4a6b0f38b4
smita.talawadekar <smita.talawadekar@HMCTS.NET>
2022-08-15 15:53:23 +0100

    CIV-2801 checkstyle fixes

--- src/test/java/uk/gov/hmcts/reform/civil/handler/event/DismissClaimEventHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/event/DismissClaimEventHandlerTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import uk.gov.hmcts.reform.civil.event.CloseApplicationsEvent;
 import uk.gov.hmcts.reform.civil.event.DismissClaimEvent;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 
