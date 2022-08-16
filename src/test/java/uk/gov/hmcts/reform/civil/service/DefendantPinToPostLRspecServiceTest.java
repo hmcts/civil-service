b8ec020aa945df917115608d4f354aae51e2447e
OrtizP <pablo.gutierrez@version1.com>
2022-08-16 14:56:38 +0200

    Pin in post Service Test

--- src/test/java/uk/gov/hmcts/reform/civil/service/DefendantPinToPostLRspecServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/DefendantPinToPostLRspecServiceTest.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.reform.civil.enums.CaseRole;
 import uk.gov.hmcts.reform.civil.model.DefendantPinToPostLRspec;
+
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(classes = {
     DefendantPinToPostLRspecService.class,
 })
+
 class DefendantPinToPostLRspecServiceTest {
 
     @Autowired
@@ -22,15 +25,20 @@ class DefendantPinToPostLRspecServiceTest {
 
         @Test
         void shouldBuildDefendantPinToPost_whenInvoked() {
-            DefendantPinToPostLRspec defendantPinToPostLRspec = defendantPinToPostLRspecService.buildDefendantPinToPost();
-            assertThat(defendantPinToPostLRspec.getExpiryDate()).isEqualTo(getDate180days());
-            assertThat(defendantPinToPostLRspec.getRespondentCaseRole()).isEqualTo(CaseRole.RESPONDENTSOLICITORONESPEC.getFormattedName());
-            assertThat(defendantPinToPostLRspec.getAccessCode()).isNotEmpty();
+            DefendantPinToPostLRspec defendantPinToPostLRspec = defendantPinToPostLRspecService
+                .buildDefendantPinToPost();
+            assertThat(defendantPinToPostLRspec.getExpiryDate())
+                .isEqualTo(getDate180days());
+            assertThat(defendantPinToPostLRspec.getRespondentCaseRole())
+                .isEqualTo(CaseRole.RESPONDENTSOLICITORONESPEC.getFormattedName());
+            assertThat(defendantPinToPostLRspec.getAccessCode())
+                .isNotEmpty();
         }
     }
 
-    private LocalDate getDate180days () {
-        return LocalDate.now().plusDays(180);
+    private LocalDate getDate180days() {
+        return LocalDate.now()
+            .plusDays(180);
     }
 
 }
