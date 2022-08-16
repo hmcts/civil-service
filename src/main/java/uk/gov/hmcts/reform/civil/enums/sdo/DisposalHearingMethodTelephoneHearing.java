92ebfce0f0a9be1c9475742b5546d1d9d59e26ca
m-meulendijks-v1 <107135537+m-meulendijks-v1@users.noreply.github.com>
2022-08-16 16:19:44 +0100

    Align disposal hearing page with pdf - CIV3834 (#1314)
    * Changes to be committed:
    	modified:   src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/FlowState.java
    	modified:   src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/FlowStateAllowedEventService.java
    	modified:   src/main/java/uk/gov/hmcts/reform/civil/service/robotics/mapper/EventHistoryMapper.java
    Added method that allows for SDO RPA on unhappy path.
    
    * Fix to translate a LocalDate value to LocalDateTime
    
    * Changes to align PDF & disposal page
    
    * Revisions to disposal hearing page
    
    * Change order of import to appease checkstyle

--- src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/DisposalHearingMethodTelephoneHearing.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/DisposalHearingMethodTelephoneHearing.java
@@ -1,16 +1,7 @@
 package uk.gov.hmcts.reform.civil.enums.sdo;
 
-import lombok.Getter;
-
-@Getter
 public enum DisposalHearingMethodTelephoneHearing {
-    telephoneTheClaimant("the claimant"),
-    telephoneTheDefendant("the defendant"),
-    telephoneTheCourt("the court");
-
-    private final String label;
-
-    DisposalHearingMethodTelephoneHearing(String value) {
-        this.label = value;
-    }
+    telephoneTheClaimant,
+    telephoneTheDefendant,
+    telephoneTheCourt
 }
--- src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/DisposalHearingMethodVideoConferenceHearing.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/DisposalHearingMethodVideoConferenceHearing.java
@@ -1,16 +1,7 @@
 package uk.gov.hmcts.reform.civil.enums.sdo;
 
-import lombok.Getter;
-
-@Getter
 public enum DisposalHearingMethodVideoConferenceHearing {
-    videoTheClaimant("the claimant"),
-    videoTheDefendant("the defendant"),
-    videoTheCourt("the court");
-
-    private final String label;
-
-    DisposalHearingMethodVideoConferenceHearing(String value) {
-        this.label = value;
-    }
+    videoTheClaimant,
+    videoTheDefendant,
+    videoTheCourt
 }
