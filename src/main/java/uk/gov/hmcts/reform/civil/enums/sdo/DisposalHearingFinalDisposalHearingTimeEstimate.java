ca09f4c84fc9c03086c4245b9cba0f90e225fec4
Gulam-Hussain <gulam.hussain@HMCTS.NET>
2022-08-19 15:47:12 +0100

    add sdo pdfs

--- src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/DisposalHearingFinalDisposalHearingTimeEstimate.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/DisposalHearingFinalDisposalHearingTimeEstimate.java
@@ -1,6 +1,15 @@
 package uk.gov.hmcts.reform.civil.enums.sdo;
 
+import lombok.Getter;
+
+@Getter
 public enum DisposalHearingFinalDisposalHearingTimeEstimate {
-    THIRTY_MINUTES,
-    FIFTEEN_MINUTES
+    THIRTY_MINUTES("30 minutes"),
+    FIFTEEN_MINUTES("15 minutes");
+
+    private final String label;
+
+    DisposalHearingFinalDisposalHearingTimeEstimate(String value) {
+        this.label = value;
+    }
 }
