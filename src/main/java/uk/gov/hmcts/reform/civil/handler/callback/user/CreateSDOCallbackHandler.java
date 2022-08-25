27b77f16c296c250ddb3238e18dd0332f89f5631
dtortolaV1 <dario.tortolanavarro@hmcts.net>
2022-08-25 15:27:32 +0100

    feat/CIV-3178 spec claims preferred court is chosen differently from unspec

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateSDOCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateSDOCallbackHandler.java
@@ -61,7 +61,6 @@ import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static java.lang.String.format;
