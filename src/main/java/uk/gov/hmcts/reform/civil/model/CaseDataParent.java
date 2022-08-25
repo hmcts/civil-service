9827ad066015f5f104f7f1034f74b9cf4ba2bd49
dtortolaV1 <93722198+dtortolaV1@users.noreply.github.com>
2022-08-25 11:37:01 +0200

    CIV-3849 added fields for next hearing details (#1311)
    Co-authored-by: dtortolaV1 <dario.tortolanavarro@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/civil/model/CaseDataParent.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/CaseDataParent.java
@@ -275,4 +275,6 @@ public class CaseDataParent implements MappableObject {
      */
     private final DefendantPinToPostLRspec respondent1PinToPostLRspec;
     private final String respondent1EmailAddress;
+
+    private final ScheduledHearing nextHearingDetails;
 }
