3fb389f1033e28f216e1ed4872a3025e42614a06
sherlynkhaw <sherlyn.khaw@evoco.co.uk>
2022-09-08 18:27:34 +0100

    Feat/civ 3278 court location (#1334)

--- src/main/java/uk/gov/hmcts/reform/civil/model/dq/RequestedCourt.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/dq/RequestedCourt.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.reform.civil.model.dq;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.model.common.DynamicList;
+import uk.gov.hmcts.reform.civil.model.defaultjudgment.CaseLocation;
 
 @Data
 @Builder(toBuilder = true)
@@ -11,4 +13,6 @@ public class RequestedCourt {
     private final YesOrNo requestHearingAtSpecificCourt;
     private final String responseCourtCode;
     private final String reasonForHearingAtSpecificCourt;
+    private final DynamicList responseCourtLocations;
+    private final CaseLocation caseLocation;
 }
