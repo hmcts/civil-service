01e90e084ab09f66bff9f03bb16aead9e7134bab
Gulam Hussain <94545342+Gulam-Hussain@users.noreply.github.com>
2022-09-09 16:13:19 +0100

    Generate sdo pdfs final 2 (#1413)
    * add sdo pdfs
    
    * update pdfs and rebase with master
    
    * create new commit and rebase with master
    
    * create new commit and rebase with master

--- src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/DisposalHearingMethodVideoConferenceHearing.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/DisposalHearingMethodVideoConferenceHearing.java
@@ -1,7 +1,16 @@
 package uk.gov.hmcts.reform.civil.enums.sdo;
 
+import lombok.Getter;
+
+@Getter
 public enum DisposalHearingMethodVideoConferenceHearing {
-    videoTheClaimant,
-    videoTheDefendant,
-    videoTheCourt
+    videoTheClaimant("the claimant"),
+    videoTheDefendant("the defendant"),
+    videoTheCourt("the court");
+
+    private final String label;
+
+    DisposalHearingMethodVideoConferenceHearing(String value) {
+        this.label = value;
+    }
 }
