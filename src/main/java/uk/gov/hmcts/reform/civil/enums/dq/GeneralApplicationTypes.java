1d6d42af2d33faae2f46124a8227e8b32061fb79
Deepthi Doppalapudi <deepthi.doppalapudi@version1.com>
2022-09-12 09:30:02 +0100

    New Application Types
    - Unless order
    -Set aside Judgement
    -Vary Order
    -Vary Judgement
    -Adjourn or vacate a hearing
    -Others

--- src/main/java/uk/gov/hmcts/reform/civil/enums/dq/GeneralApplicationTypes.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/dq/GeneralApplicationTypes.java
@@ -11,7 +11,12 @@ public enum GeneralApplicationTypes {
     STAY_THE_CLAIM("Stay the claim"),
     EXTEND_TIME("Extend Time"),
     AMEND_A_STMT_OF_CASE("Amend a statement of case"),
-    RELIEF_FROM_SANCTIONS("Relief from sanctions");
-
+    RELIEF_FROM_SANCTIONS("Relief from sanctions"),
+    UNLESS_ORDER("Unless order"),
+    SET_ASIDE_JUDGEMENT("Set aside judgement"),
+    VARY_ORDER("Vary order"),
+    VARY_JUDGEMENT("Vary Judgement"),
+    ADJOURN_OR_VACATE_HEARING("Adjourn or vacate a hearing"),
+    OTHER("Other");
     private final String displayedValue;
 }
