774c5434c9f6488acb3d94f1bbcfd77808d28156
Mark Drummond <mark.drummond@version1.com>
2022-09-21 16:32:39 +0100

    fixed add-roles

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -6,7 +6,7 @@
 ./bin/utils/ccd-add-role.sh "caseworker-approver"
 ./bin/utils/ccd-add-role.sh "prd-admin"
 ./bin/utils/ccd-add-role.sh "judge-profile"
-./bin/utils/ccd-add-role.sh "basic-access" #remove after GS_profile is added
+./bin/utils/ccd-add-role.sh "basic-access" #remove after GS_profile is added.
 ./bin/utils/ccd-add-role.sh "GS_profile"
 ./bin/utils/ccd-add-role.sh "legal-adviser"
 ./bin/utils/ccd-add-role.sh "caseworker-ras-validation"
