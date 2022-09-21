bdc00e87f1106c7aca9a2c18d3cfc073af322151
Mark Drummond <mark.drummond@version1.com>
2022-09-21 13:17:09 +0100

    test hearing-schedule-access

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -12,7 +12,6 @@
 ./bin/utils/ccd-add-role.sh "caseworker-ras-validation"
 ./bin/utils/ccd-add-role.sh "admin-access"
 ./bin/utils/ccd-add-role.sh "full-access"
-./bin/utils/ccd-add-role.sh "hearing-schedule-access"
 ./bin/utils/ccd-add-role.sh "civil-administrator-standard"
 ./bin/utils/ccd-add-role.sh "civil-administrator-basic"
 
