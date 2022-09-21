b9dbe623c3bc23c3d0148b383defe387a1e542cd
suvarnathekkoot <suvarna.thekkoot@HMCTS.NET>
2022-09-21 16:08:12 +0100

    Adding back disposalHearingApplicationsOrderToggle field

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateSDOCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateSDOCallbackHandler.java
@@ -711,10 +711,12 @@ public class CreateSDOCallbackHandler extends CallbackHandler {
         updatedData.disposalHearingBundleToggle(checkList);
         updatedData.disposalHearingClaimSettlingToggle(checkList);
         updatedData.disposalHearingCostsToggle(checkList);
+        updatedData.disposalHearingApplicationsOrderToggle(checkList);
         updatedData.smallClaimsHearingToggle(checkList);
         updatedData.smallClaimsMethodToggle(checkList);
         updatedData.smallClaimsDocumentsToggle(checkList);
         updatedData.smallClaimsWitnessStatementToggle(checkList);
+        updatedData.disposalHearingApplicationsOrderToggle(checkList);
     }
 
     private List<String> setPreferredLocationFirst(List<LocationRefData> locations, CaseData caseData) {
