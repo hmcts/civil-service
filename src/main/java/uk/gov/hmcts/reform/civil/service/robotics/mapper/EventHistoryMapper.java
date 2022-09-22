451bcd9a14a115cfc2b996ab5831c4d90a7e7d0a
skhajuria <sankhajuria@gmail.com>
2022-09-22 12:15:43 +0100

    RPA changes for  DJ

--- src/main/java/uk/gov/hmcts/reform/civil/service/robotics/mapper/EventHistoryMapper.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/robotics/mapper/EventHistoryMapper.java
@@ -3,8 +3,10 @@ package uk.gov.hmcts.reform.civil.service.robotics.mapper;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.civil.enums.DJPaymentTypeSelection;
 import uk.gov.hmcts.reform.civil.enums.MultiPartyScenario;
 import uk.gov.hmcts.reform.civil.enums.ReasonForProceedingOnPaper;
+import uk.gov.hmcts.reform.civil.enums.RepaymentFrequencyDJ;
 import uk.gov.hmcts.reform.civil.enums.RespondentResponseType;
 import uk.gov.hmcts.reform.civil.enums.RespondentResponseTypeSpec;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
@@ -63,9 +65,11 @@ import static uk.gov.hmcts.reform.civil.model.robotics.EventType.ACKNOWLEDGEMENT
 import static uk.gov.hmcts.reform.civil.model.robotics.EventType.BREATHING_SPACE_ENTERED;
 import static uk.gov.hmcts.reform.civil.model.robotics.EventType.BREATHING_SPACE_LIFTED;
 import static uk.gov.hmcts.reform.civil.model.robotics.EventType.CONSENT_EXTENSION_FILING_DEFENCE;
+import static uk.gov.hmcts.reform.civil.model.robotics.EventType.DEFAULT_JUDGMENT_GRANTED;
 import static uk.gov.hmcts.reform.civil.model.robotics.EventType.DEFENCE_AND_COUNTER_CLAIM;
 import static uk.gov.hmcts.reform.civil.model.robotics.EventType.DEFENCE_FILED;
 import static uk.gov.hmcts.reform.civil.model.robotics.EventType.DIRECTIONS_QUESTIONNAIRE_FILED;
+import static uk.gov.hmcts.reform.civil.model.robotics.EventType.INTERLOCUTORY_JUDGMENT_GRANTED;
 import static uk.gov.hmcts.reform.civil.model.robotics.EventType.MENTAL_HEALTH_BREATHING_SPACE_ENTERED;
 import static uk.gov.hmcts.reform.civil.model.robotics.EventType.MENTAL_HEALTH_BREATHING_SPACE_LIFTED;
 import static uk.gov.hmcts.reform.civil.model.robotics.EventType.MISCELLANEOUS;
@@ -239,9 +243,106 @@ public class EventHistoryMapper {
                 }
             }
         }
-        buildInformAgreedExtensionDateForSpec(builder, caseData);
 
+        buildInterlocutoryJudgment(builder, caseData);
+        buildMiscellaneousIJEvent(builder, caseData);
+        buildDefaultJudgment(builder, caseData);
+        buildMiscellaneousDJEvent(builder, caseData);
+        buildInformAgreedExtensionDateForSpec(builder, caseData);
         return eventHistorySequencer.sortEvents(builder.build());
+    }
+
+    private void buildInterlocutoryJudgment(EventHistory.EventHistoryBuilder builder, CaseData caseData) {
+
+        List<Event> events = new ArrayList<>();
+        Boolean grantedFlag = caseData.getRespondent2() != null
+            && caseData.getDefendantDetails() != null
+            && !caseData.getDefendantDetails().getValue()
+            .getLabel().startsWith("Both");
+        if (!grantedFlag && null != caseData.getHearingSupportRequirementsDJ()) {
+            events.add(prepareInterlocutoryJudgment(builder, caseData, RESPONDENT_ID));
+
+            if (null != caseData.getRespondent2()) {
+                events.add(prepareInterlocutoryJudgment(builder, caseData, RESPONDENT2_ID));
+            }
+            builder.interlocutoryJudgment(events);
+        }
+    }
+
+    private Event prepareInterlocutoryJudgment(EventHistory.EventHistoryBuilder builder, CaseData caseData,
+                                              String litigiousPartyID) {
+        return (Event.builder()
+            .eventSequence(prepareEventSequence(builder.build()))
+            .eventCode(INTERLOCUTORY_JUDGMENT_GRANTED.getCode())
+            .dateReceived(LocalDateTime.now())
+            .litigiousPartyID(litigiousPartyID)
+            .eventDetailsText("")
+            .eventDetails(EventDetails.builder().miscText("")
+                              .build())
+            .build());
+    }
+
+    private void buildDefaultJudgment(EventHistory.EventHistoryBuilder builder, CaseData caseData) {
+
+        List<Event> events = new ArrayList<>();
+        Boolean grantedFlag = caseData.getRespondent2() != null
+            && caseData.getDefendantDetailsSpec() != null
+            && !caseData.getDefendantDetailsSpec().getValue()
+            .getLabel().startsWith("Both");
+
+        if (!grantedFlag && null != caseData.getDefendantDetailsSpec()) {
+            events.add(prepareDefaultJudgment(builder, caseData, RESPONDENT_ID));
+
+            if (null != caseData.getRespondent2()) {
+                events.add(prepareDefaultJudgment(builder, caseData, RESPONDENT2_ID));
+            }
+            builder.defaultJudgment(events);
+        }
+
+    }
+
+    private Event prepareDefaultJudgment(EventHistory.EventHistoryBuilder builder, CaseData caseData,
+                                         String litigiousPartyID) {
+
+        BigDecimal claimInterest = caseData.getTotalInterest() != null
+            ? caseData.getTotalInterest() : BigDecimal.ZERO;
+        BigDecimal amountClaimedWithInterest = caseData.getTotalClaimAmount().add(claimInterest);
+        var partialPaymentPennies = isNotEmpty(caseData.getPartialPaymentAmount())
+            ? new BigDecimal(caseData.getPartialPaymentAmount()) : null;
+        var partialPaymentPounds = isNotEmpty(partialPaymentPennies)
+            ? MonetaryConversions.penniesToPounds(partialPaymentPennies) : null;
+
+        return (Event.builder()
+            .eventSequence(prepareEventSequence(builder.build()))
+            .eventCode(DEFAULT_JUDGMENT_GRANTED.getCode())
+            .dateReceived(LocalDateTime.now())
+            .litigiousPartyID(litigiousPartyID)
+            .eventDetailsText("")
+            .eventDetails(EventDetails.builder().miscText("")
+                              .amountOfJudgment(amountClaimedWithInterest)
+                              .amountOfCosts(getCostOfJudgment(caseData))
+                              .amountPaidBeforeJudgment((caseData.getPartialPayment() == YesOrNo.YES)
+                                                            ? partialPaymentPounds : BigDecimal.ZERO)
+                              .isJudgmentForthwith((caseData.getPaymentTypeSelection()
+                                  .equals(DJPaymentTypeSelection.IMMEDIATELY)) ? true : false)
+                              .paymentInFullDate((caseData.getPaymentTypeSelection()
+                                  .equals(DJPaymentTypeSelection.IMMEDIATELY))
+                                                     ?  LocalDateTime.now()
+                                                     : (caseData.getPaymentTypeSelection()
+                                  .equals(DJPaymentTypeSelection.SET_DATE))
+                                  ? caseData.getPaymentSetDate().atStartOfDay() : null)
+                              .installmentAmount((caseData.getPaymentTypeSelection()
+                                  .equals(DJPaymentTypeSelection.REPAYMENT_PLAN))
+                                                     ? new BigDecimal(caseData.getRepaymentSuggestion())
+                                                     : BigDecimal.ZERO)
+                              .installmentPeriod(getInstallmentPeriod(caseData))
+                              .firstInstallmentDate(caseData.getRepaymentDate())
+                              .dateOfJudgment(LocalDateTime.now())
+                              .jointJudgment(caseData.getRespondent2() != null)
+                              .judgmentToBeRegistered(false)
+                              .build())
+            .build());
+
     }
 
     private void buildBreathingSpaceEvent(EventHistory.EventHistoryBuilder builder, CaseData caseData,
@@ -903,6 +1004,8 @@ public class EventHistoryMapper {
         currentSequence = getCurrentSequence(history.getBreathingSpaceMentalHealthLifted(), currentSequence);
         currentSequence = getCurrentSequence(history.getStatesPaid(), currentSequence);
         currentSequence = getCurrentSequence(history.getDirectionsQuestionnaireFiled(), currentSequence);
+        currentSequence = getCurrentSequence(history.getInterlocutoryJudgment(), currentSequence);
+        currentSequence = getCurrentSequence(history.getDefaultJudgment(), currentSequence);
         return currentSequence + 1;
     }
 
@@ -1829,4 +1932,87 @@ public class EventHistoryMapper {
                 return format("agreed extension date: %s", extensionDate);
         }
     }
+
+    private void buildMiscellaneousIJEvent(EventHistory.EventHistoryBuilder builder, CaseData caseData) {
+        Boolean grantedFlag = caseData.getRespondent2() != null
+            && caseData.getDefendantDetails() != null
+            && !caseData.getDefendantDetails().getValue()
+            .getLabel().startsWith("Both");
+        String miscTextRequested = "RPA Reason: Summary judgment requested and referred to judge.";
+        String miscTextGranted = "RPA Reason: Summary judgment granted and referred to judge.";
+        if (caseData.getDefendantDetails() != null) {
+            builder.miscellaneous(
+                Event.builder()
+                    .eventSequence(prepareEventSequence(builder.build()))
+                    .eventCode(MISCELLANEOUS.getCode())
+                    .dateReceived(LocalDateTime.now())
+                    .eventDetailsText(grantedFlag ? miscTextRequested : miscTextGranted)
+                    .eventDetails(EventDetails.builder()
+                                      .miscText(grantedFlag ? miscTextRequested : miscTextGranted)
+                                      .build())
+                    .build());
+        }
+
+    }
+
+    private void buildMiscellaneousDJEvent(EventHistory.EventHistoryBuilder builder, CaseData caseData) {
+        Boolean grantedFlag = caseData.getRespondent2() != null
+            && caseData.getDefendantDetailsSpec() != null
+            && !caseData.getDefendantDetailsSpec().getValue()
+            .getLabel().startsWith("Both");
+        String miscTextRequested = "RPA Reason: Default Judgment requested and claim moved offline.";
+        String miscTextGranted = "RPA Reason: Default Judgment granted and claim moved offline.";
+        if (caseData.getDefendantDetailsSpec() != null) {
+            builder.miscellaneous(
+                Event.builder()
+                    .eventSequence(prepareEventSequence(builder.build()))
+                    .eventCode(MISCELLANEOUS.getCode())
+                    .dateReceived(LocalDateTime.now())
+                    .eventDetailsText(grantedFlag ? miscTextRequested : miscTextGranted)
+                    .eventDetails(EventDetails.builder()
+                                      .miscText(grantedFlag ? miscTextRequested : miscTextGranted)
+                                      .build())
+                    .build());
+        }
+    }
+
+    private String getInstallmentPeriod(CaseData data) {
+        if (data.getPaymentTypeSelection().equals(DJPaymentTypeSelection.REPAYMENT_PLAN)) {
+            if (data.getRepaymentFrequency().equals(RepaymentFrequencyDJ.ONCE_ONE_WEEK)) {
+
+                return "WK";
+            } else if (data.getRepaymentFrequency().equals(RepaymentFrequencyDJ.ONCE_TWO_WEEKS)) {
+                return "FOR";
+            } else if (data.getRepaymentFrequency().equals(RepaymentFrequencyDJ.ONCE_ONE_MONTH)) {
+                return "MTH";
+            }
+
+        } else if (data.getPaymentTypeSelection().equals(DJPaymentTypeSelection.IMMEDIATELY)) {
+            return "FW";
+        }
+
+        return "FUL";
+    }
+
+    private BigDecimal getCostOfJudgment(CaseData data) {
+
+        String repaymentSummary = data.getRepaymentSummaryObject();
+        BigDecimal fixedCost = null;
+        BigDecimal claimCost = null;
+        if (null != repaymentSummary) {
+            fixedCost = repaymentSummary.contains("Fixed")
+                ? new BigDecimal(repaymentSummary.substring(
+                repaymentSummary.indexOf("Fixed cost amount \n£") + 20,
+                repaymentSummary.indexOf("\n### Claim fee amount ")
+            )) : null;
+            claimCost = new BigDecimal(repaymentSummary.substring(
+                repaymentSummary.indexOf("Claim fee amount \n £") + 20,
+                repaymentSummary.indexOf("\n ## Subtotal")
+            ));
+        }
+
+        return fixedCost != null && claimCost != null ? fixedCost.add(claimCost) : claimCost;
+
+    }
+
 }
