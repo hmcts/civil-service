ae9d012bb5938982e3cff72001937c3b7637e58a
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2022-09-23 16:18:37 +0100

    Update InitiateGeneralApplicationServiceTest.java

--- src/test/java/uk/gov/hmcts/reform/civil/service/InitiateGeneralApplicationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/InitiateGeneralApplicationServiceTest.java
@@ -27,6 +27,7 @@ import uk.gov.hmcts.reform.civil.model.genapplication.GeneralApplication;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.sampledata.GeneralApplicationDetailsBuilder;
 import uk.gov.hmcts.reform.civil.sampledata.LocationRefSampleDataBuilder;
+import uk.gov.hmcts.reform.civil.service.referencedata.LocationRefDataService;
 import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 import uk.gov.hmcts.reform.prd.client.OrganisationApi;
 
@@ -92,6 +93,9 @@ class InitiateGeneralApplicationServiceTest extends LocationRefSampleDataBuilder
 
     @MockBean
     private AuthTokenGenerator authTokenGenerator;
+
+    @MockBean
+    private LocationRefDataService locationRefDataService;
 
     @BeforeEach
     public void setUp() throws IOException {
