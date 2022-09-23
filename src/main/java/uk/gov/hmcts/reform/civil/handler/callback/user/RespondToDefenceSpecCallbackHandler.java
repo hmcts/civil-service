9d89d12e8c07bb9771986657545c4f6bba88f524
Chris Chandler <chris.chandler@hmcts.net>
2022-09-23 13:11:46 +0100

    Pre case mgmt location

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToDefenceSpecCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToDefenceSpecCallbackHandler.java
@@ -15,6 +15,7 @@ import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.user.spec.CaseDataToTextGenerator;
 import uk.gov.hmcts.reform.civil.handler.callback.user.spec.RespondToResponseConfirmationHeaderGenerator;
 import uk.gov.hmcts.reform.civil.handler.callback.user.spec.RespondToResponseConfirmationTextGenerator;
+import uk.gov.hmcts.reform.civil.helpers.LocationHelper;
 import uk.gov.hmcts.reform.civil.launchdarkly.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.CaseData;
@@ -23,6 +24,7 @@ import uk.gov.hmcts.reform.civil.model.dq.Applicant1DQ;
 import uk.gov.hmcts.reform.civil.model.dq.HearingLRspec;
 import uk.gov.hmcts.reform.civil.model.dq.SmallClaimHearing;
 import uk.gov.hmcts.reform.civil.service.Time;
+import uk.gov.hmcts.reform.civil.service.referencedata.LocationRefDataService;
 import uk.gov.hmcts.reform.civil.validation.UnavailableDateValidator;
 import uk.gov.hmcts.reform.civil.validation.interfaces.ExpertsValidator;
 import uk.gov.hmcts.reform.civil.validation.interfaces.WitnessesValidator;
@@ -55,6 +57,8 @@ public class RespondToDefenceSpecCallbackHandler extends CallbackHandler
     private final List<RespondToResponseConfirmationHeaderGenerator> confirmationHeaderGenerators;
     private final List<RespondToResponseConfirmationTextGenerator> confirmationTextGenerators;
     private final FeatureToggleService featureToggleService;
+    private final LocationRefDataService locationRefDataService;
+
 
     @Override
     public List<CaseEvent> handledEvents() {
@@ -130,6 +134,7 @@ public class RespondToDefenceSpecCallbackHandler extends CallbackHandler
 
     private CallbackResponse aboutToSubmit(CallbackParams callbackParams) {
         CaseData caseData = callbackParams.getCaseData();
+        new LocationHelper().setLocationAndCaseManagementLocation(callbackParams, locationRefDataService);
         CaseData.CaseDataBuilder<?, ?> builder = caseData.toBuilder()
             .businessProcess(BusinessProcess.ready(CLAIMANT_RESPONSE_SPEC))
             .applicant1ResponseDate(time.now());
---
+++ src/main/java/uk/gov/hmcts/reform/civil/helpers/LocationHelper.java
@@ -0,0 +1,117 @@
+package uk.gov.hmcts.reform.civil.helpers;
+
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.enums.AllocatedTrack;
+import uk.gov.hmcts.reform.civil.enums.SuperClaimType;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.enums.sdo.OrderType;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.Party;
+import uk.gov.hmcts.reform.civil.model.common.DynamicList;
+import uk.gov.hmcts.reform.civil.model.defaultjudgment.CaseLocation;
+import uk.gov.hmcts.reform.civil.model.dq.Applicant1DQ;
+import uk.gov.hmcts.reform.civil.model.dq.RequestedCourt;
+import uk.gov.hmcts.reform.civil.model.referencedata.response.LocationRefData;
+import uk.gov.hmcts.reform.civil.service.referencedata.LocationRefDataService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
+import static uk.gov.hmcts.reform.civil.enums.SuperClaimType.SPEC_CLAIM;
+
+public class LocationHelper {
+    public void setLocationAndCaseManagementLocation(CallbackParams callbackParams,
+                                                             LocationRefDataService locationRefDataService) {
+        CaseData caseData = callbackParams.getCaseData();
+
+        List<LocationRefData> locations = locationRefDataService.getCourtLocationsForDefaultJudgments(
+            callbackParams.getParams().get(BEARER_TOKEN).toString()
+        );
+
+        String locationLabel;
+        if (caseData.getSuperClaimType() == SuperClaimType.SPEC_CLAIM) {
+            RequestedCourt courtRequest = Optional.ofNullable(caseData.getApplicant1DQ())
+                .map(Applicant1DQ::getRequestedCourt).orElse(null);
+            if (courtRequest != null && courtRequest.getRequestHearingAtSpecificCourt() == YesOrNo.YES) {
+                locationLabel = courtRequest.getResponseCourtCode();
+            } else {
+                return; // getLocationsFromList(locations);
+            }
+        } else {
+            // assume unspec
+            locationLabel = caseData.getCourtLocation().getApplicantPreferredCourt();
+        }
+
+        var preferredLocation = preferredLocation(locationLabel, locations);
+
+        setCaseManagementLocationData(caseData, preferredLocation, locations);
+    }
+
+    public Optional<LocationRefData> preferredLocation(String locationLabel, List<LocationRefData> locations) {
+        return locations
+                .stream()
+                .filter(locationRefData -> checkLocation(locationRefData,
+                                                         locationLabel)).findFirst();
+    }
+
+    public List<String> getLocationsFromList(final List<LocationRefData> locations) {
+        return locations.stream()
+            .map(location -> location.getSiteName()
+                + " - " + location.getCourtAddress()
+                + " - " + location.getPostcode())
+            .collect(Collectors.toList());
+    }
+
+    public Boolean checkLocation(final LocationRefData location,
+                                 String locationTempLabel) {
+        return location.getEpimmsId().equals(locationTempLabel);
+    }
+
+    public void setCaseManagementLocationData(CaseData caseData,
+                                              Optional<LocationRefData> preferredLocation,
+                                              List<LocationRefData> locations) {
+        if (caseData.getOrderType() == OrderType.DECIDE_DAMAGES && preferredLocation.isPresent()) {
+            caseData.toBuilder().caseManagementLocation(
+                CaseLocation.builder()
+                    .region(preferredLocation.get().getRegionId())
+                    .baseLocation(preferredLocation.get().getEpimmsId()).build()
+            ).build().toBuilder().locationName(preferredLocation.get().getSiteName()).build();
+        }
+
+        if (SPEC_CLAIM.equals(caseData.getSuperClaimType())
+            || (caseData.getAllocatedTrack() == AllocatedTrack.MULTI_CLAIM
+            && SPEC_CLAIM.equals(caseData.getSuperClaimType()))) {
+            if (caseData.getApplicant1().getType() == Party.Type.INDIVIDUAL
+                || caseData.getApplicant1().getType() == Party.Type.SOLE_TRADER) {
+                caseData.toBuilder().caseManagementLocation(
+                    CaseLocation.builder()
+                        .region(caseData.getApplicant1DQ().getApplicant1DQRequestedCourt()
+                                    .getCaseLocation().getRegion())
+                        .baseLocation(caseData.getApplicant1DQ().getApplicant1DQRequestedCourt().getCaseLocation()
+                                          .getBaseLocation()).build()
+                ).build();
+            } else if (caseData.getApplicant1().getType() == Party.Type.COMPANY
+                || caseData.getApplicant1().getType() == Party.Type.ORGANISATION) {
+                caseData.toBuilder().caseManagementLocation(
+                    CaseLocation.builder()
+                        .region(caseData.getRespondent1DQ().getRespondent1DQRequestedCourt()
+                                    .getCaseLocation().getRegion())
+                        .baseLocation(caseData.getRespondent1DQ().getRespondent1DQRequestedCourt().getCaseLocation()
+                                          .getBaseLocation()).build()
+                ).build();
+            }
+        }
+
+        if (preferredLocation.isPresent()) {
+            locations.remove(preferredLocation.get());
+            locations.add(0, preferredLocation.get());
+            caseData.toBuilder().caseManagementLocation(
+                CaseLocation.builder()
+                    .region(preferredLocation.get().getRegionId())
+                    .baseLocation(preferredLocation.get().getEpimmsId()).build()
+            ).build().toBuilder().locationName(preferredLocation.get().getSiteName()).build();
+        }
+    }
+}
