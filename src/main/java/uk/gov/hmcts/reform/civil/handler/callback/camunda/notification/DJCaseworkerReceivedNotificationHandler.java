2abd9dc4b26ee900265facc5ee04d513f3ca0b14
Miguel Molina <93722947+miguelMolina3691@users.noreply.github.com>
2022-10-05 13:00:30 +0100

    Update DJCaseworkerReceivedNotificationHandler.java

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/DJCaseworkerReceivedNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/DJCaseworkerReceivedNotificationHandler.java
@@ -1,8 +1,8 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.notification;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
