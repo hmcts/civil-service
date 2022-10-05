e35646829fdb9a4878e0dff815fcc86f11938393
indu2019 <indu.chaturvedi@hmcts.net>
2022-10-05 18:14:32 +0100

    CIV-4673-changing-response-deadline to 28 days, fixing unit tests

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToClaimSpecCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToClaimSpecCallbackHandlerTest.java
@@ -531,7 +531,7 @@ class RespondToClaimSpecCallbackHandlerTest extends BaseCallbackHandlerTest {
                 .build();
 
             CallbackParams params = callbackParamsOf(V_1, caseData, ABOUT_TO_SUBMIT);
-            when(deadlinesCalculator.calculateApplicantResponseDeadline(any(), any())).thenReturn(LocalDateTime.now());
+            when(deadlinesCalculator.calculateApplicantResponseDeadlineSpec(any(), any())).thenReturn(LocalDateTime.now());
 
             AboutToStartOrSubmitCallbackResponse response = (AboutToStartOrSubmitCallbackResponse) handler
                 .handle(params);
@@ -568,7 +568,7 @@ class RespondToClaimSpecCallbackHandlerTest extends BaseCallbackHandlerTest {
                 .build();
 
             CallbackParams params = callbackParamsOf(V_1, caseData, ABOUT_TO_SUBMIT);
-            when(deadlinesCalculator.calculateApplicantResponseDeadline(any(), any())).thenReturn(LocalDateTime.now());
+            when(deadlinesCalculator.calculateApplicantResponseDeadlineSpec(any(), any())).thenReturn(LocalDateTime.now());
 
             AboutToStartOrSubmitCallbackResponse response = (AboutToStartOrSubmitCallbackResponse) handler
                 .handle(params);
