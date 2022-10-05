bb1c9671fbba279ef60b649f9265c12c0453002d
suvarnathekkoot <suvarna.thekkoot@HMCTS.NET>
2022-10-05 14:27:12 +0100

    stylecheck

--- src/main/java/uk/gov/hmcts/reform/civil/config/ServiceRequestConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/ServiceRequestConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Data
 @Configuration
 public class ServiceRequestConfiguration {
+    
     private final String siteId;
     private final String service;
 
