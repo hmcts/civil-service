ac9e12135617265d8592c0cefd0141286517f434
indu2019 <indu.chaturvedi@hmcts.net>
2022-10-07 18:01:59 +0100

    CIV-4673-changing-response-deadline to 28 days, fixing respond deadline for seal claim form

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/sealedclaim/SealedClaimFormGeneratorForSpec.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/sealedclaim/SealedClaimFormGeneratorForSpec.java
@@ -193,7 +193,7 @@ public class SealedClaimFormGeneratorForSpec implements TemplateDataGenerator<Se
     private String getResponseDedline(CaseData caseData) {
         var notificationDeadline = formatLocalDate(
             deadlinesCalculator
-                .calculateFirstWorkingDay(caseData.getIssueDate().plusDays(28)),
+                .calculateFirstWorkingDay(caseData.getIssueDate().plusDays(286)),
             DATE
         );
         return END_OF_BUSINESS_DAY + notificationDeadline;
