494b65fe6a97558cc4bf44af03a20cc62acb12b4
Mark Drummond <mark.drummond@version1.com>
2022-10-07 15:25:54 +0100

    screens for uploading document and note evidence judge

--- src/main/java/uk/gov/hmcts/reform/civil/model/documents/DocumentAndNote.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/documents/DocumentAndNote.java
@@ -1,11 +1,9 @@
 package uk.gov.hmcts.reform.civil.model.documents;
 
-import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
-
 
 @Data
 @Builder(toBuilder = true)
@@ -13,7 +11,6 @@ import lombok.Getter;
 @Getter
 public class DocumentAndNote {
 
+    private final Document document;
 
-    private final Document DOCUMENT;
-    private final String DOCUMENT_NOTE;
 }
