762763c46579ed7d3d6f06686c751a3227e5b899
Sampan Kumar <sampan.kumar@hmcts.net>
2022-10-07 11:17:47 +0100

    Moving SDO code to V1

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToDefenceCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToDefenceCallbackHandler.java
@@ -45,6 +45,7 @@ import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.MID;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
+import static uk.gov.hmcts.reform.civil.callback.CallbackVersion.V_1;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CLAIMANT_RESPONSE;
 import static uk.gov.hmcts.reform.civil.enums.MultiPartyScenario.ONE_V_TWO_ONE_LEGAL_REP;
 import static uk.gov.hmcts.reform.civil.enums.MultiPartyScenario.ONE_V_TWO_TWO_LEGAL_REP;
@@ -84,6 +85,7 @@ public class RespondToDefenceCallbackHandler extends CallbackHandler implements 
             callbackKey(MID, "validate-unavailable-dates"), this::validateUnavailableDates,
             callbackKey(MID, "statement-of-truth"), this::resetStatementOfTruth,
             callbackKey(ABOUT_TO_SUBMIT), this::aboutToSubmit,
+            callbackKey(V_1, ABOUT_TO_SUBMIT), this::aboutToSubmit_v1,
             callbackKey(SUBMITTED), this::buildConfirmation
         );
     }
@@ -190,6 +192,59 @@ public class RespondToDefenceCallbackHandler extends CallbackHandler implements 
     }
 
     private CallbackResponse aboutToSubmit(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        MultiPartyScenario multiPartyScenario = getMultiPartyScenario(caseData);
+        LocalDateTime currentTime = time.now();
+
+        CaseData.CaseDataBuilder builder = caseData.toBuilder()
+            .businessProcess(BusinessProcess.ready(CLAIMANT_RESPONSE))
+            .applicant1ResponseDate(currentTime);
+
+        if (multiPartyScenario == TWO_V_ONE) {
+            builder.applicant2ResponseDate(currentTime);
+        }
+
+        if (anyApplicantDecidesToProceedWithClaim(caseData)) {
+            // moving statement of truth value to correct field, this was not possible in mid event.
+            StatementOfTruth statementOfTruth = caseData.getUiStatementOfTruth();
+
+            if (caseData.getApplicant1DQ() != null
+                && caseData.getApplicant1DQ().getApplicant1DQFileDirectionsQuestionnaire() != null) {
+                Applicant1DQ dq = caseData.getApplicant1DQ().toBuilder()
+                    .applicant1DQStatementOfTruth(statementOfTruth)
+                    .build();
+
+                builder.applicant1DQ(dq);
+            }
+
+            if (caseData.getApplicant2DQ() != null
+                && caseData.getApplicant2DQ().getApplicant2DQFileDirectionsQuestionnaire() != null) {
+                Applicant2DQ dq = caseData.getApplicant2DQ().toBuilder()
+                    .applicant2DQStatementOfTruth(statementOfTruth)
+                    .build();
+
+                builder.applicant2DQ(dq);
+            }
+
+            // resetting statement of truth to make sure it's empty the next time it appears in the UI.
+            builder.uiStatementOfTruth(StatementOfTruth.builder().build());
+        }
+
+        assembleResponseDocuments(caseData, builder);
+
+        if (multiPartyScenario == ONE_V_TWO_ONE_LEGAL_REP) {
+            builder.respondentSharedClaimResponseDocument(null);
+        }
+
+        //Set to null because there are no more deadlines
+        builder.nextDeadline(null);
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(builder.build().toMap(objectMapper))
+            .build();
+    }
+
+    private CallbackResponse aboutToSubmit_v1(CallbackParams callbackParams) {
         CaseData caseData = callbackParams.getCaseData();
 
         LocalDateTime currentTime = time.now();
