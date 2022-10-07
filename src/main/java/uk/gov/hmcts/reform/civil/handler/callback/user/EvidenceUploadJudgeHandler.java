9570e99bf4a0160a7eee68dad25a5c21a61a2497
Mark Drummond <mark.drummond@version1.com>
2022-10-07 09:17:10 +0100

    initial document work

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/EvidenceUploadJudgeHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/EvidenceUploadJudgeHandler.java
@@ -4,14 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.model.CaseData;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
+import static java.lang.String.format;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
@@ -23,19 +28,47 @@ public class EvidenceUploadJudgeHandler extends CallbackHandler {
 
     private static final List<CaseEvent> EVENTS = Collections.singletonList(EVIDENCE_UPLOAD_JUDGE);
     private final ObjectMapper objectMapper;
+    public static final String EVIDENCE_UPLOAD_HEADER_ONE = "# Document uploaded and note added \n # %s";
+    public static final String EVIDENCE_UPLOAD_BODY_ONE = "## You have uploaded: \n %s";
 
     @Override
     protected Map<String, Callback> callbacks() {
         return new ImmutableMap.Builder<String, Callback>()
             .put(callbackKey(ABOUT_TO_START), this::emptyCallbackResponse)
             .put(callbackKey(ABOUT_TO_SUBMIT), this::emptyCallbackResponse)
-            .put(callbackKey(SUBMITTED), this::emptySubmittedCallbackResponse)
+            .put(callbackKey(SUBMITTED), this::buildConfirmation)
             .build();
     }
 
     @Override
     public List<CaseEvent> handledEvents() {
         return EVENTS;
+    }
+
+    private SubmittedCallbackResponse buildConfirmation(CallbackParams callbackParams) {
+        var caseData = callbackParams.getCaseData();
+
+        return SubmittedCallbackResponse.builder()
+            .confirmationHeader(getHeader(caseData))
+            .confirmationBody(getBody(caseData))
+            .build();
+    }
+
+    private String getHeader(CaseData caseData) {
+        return format(EVIDENCE_UPLOAD_HEADER_ONE, caseData.getLegacyCaseReference());
+    }
+
+
+    private String getBody(CaseData caseData) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        IntStream.range(0, caseData.getDocumentAndNote().size()).forEachOrdered(i -> stringBuilder.append("* ").append(
+            caseData.getDocumentAndNote().get(i)
+                .getValue()
+                .getDOCUMENT()
+                .getDocumentFileName()).append("\n"));
+
+        return format(EVIDENCE_UPLOAD_BODY_ONE, stringBuilder);
     }
 
 }
