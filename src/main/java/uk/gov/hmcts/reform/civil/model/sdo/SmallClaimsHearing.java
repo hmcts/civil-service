2bcf4c79d1c3350c5d86abaf10fa178a17b653d1
Sherlyn Khaw <sherlyn.khaw@evoco.co.uk>
2022-10-11 13:29:09 +0100

    added date

--- src/main/java/uk/gov/hmcts/reform/civil/model/sdo/SmallClaimsHearing.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/sdo/SmallClaimsHearing.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.civil.enums.sdo.SmallClaimsTimeEstimate;
 
 import java.math.BigDecimal;
-
+import java.time.LocalDate;
 @Data
 @Builder(toBuilder = true)
 @NoArgsConstructor
@@ -20,4 +20,6 @@ public class SmallClaimsHearing {
     private BigDecimal otherHours;
     private BigDecimal otherMinutes;
     private String input2;
+    private LocalDate dateFrom;
+    private LocalDate dateTo;
 }
