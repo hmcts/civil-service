3b8eaa5fa714e6cbd4918fe2c48e4ae2bbdefa5d
meulendijksm <max.meulendijks@hmcts.net>
2022-10-12 17:18:23 +0100

    removed more logs

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/robotics/NotifyRoboticsHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/robotics/NotifyRoboticsHandler.java
@@ -22,7 +22,6 @@ import java.util.Set;
 
 import static java.lang.String.format;
 import static uk.gov.hmcts.reform.civil.enums.MultiPartyScenario.isMultiPartyScenario;
-import static uk.gov.hmcts.reform.civil.handler.tasks.BaseExternalTaskHandler.log;
 import static uk.gov.hmcts.reform.civil.utils.CaseCategoryUtils.isSpecCaseCategory;
 
 @RequiredArgsConstructor
@@ -44,9 +43,7 @@ public abstract class NotifyRoboticsHandler extends CallbackHandler {
         try {
 
             if (isSpecCaseCategory(caseData, toggleService.isAccessProfilesEnabled())) {
-                log.info("Access Profiles enabled");
                 if (toggleService.isLrSpecEnabled()) {
-                    log.info("LR Spec Enabled");
                     roboticsCaseDataSpec = roboticsDataMapperForSpec.toRoboticsCaseData(caseData);
                     log.info(roboticsCaseDataSpec.toJsonString());
                     errors = jsonSchemaValidationService.validate(roboticsCaseDataSpec.toJsonString());
@@ -54,9 +51,7 @@ public abstract class NotifyRoboticsHandler extends CallbackHandler {
                     throw new UnsupportedOperationException("Specified claims are not enabled");
                 }
             } else {
-                log.info("Access Profiles disabled");
                 roboticsCaseData = roboticsDataMapper.toRoboticsCaseData(caseData);
-                log.info(roboticsCaseData.toJsonString());
                 errors = jsonSchemaValidationService.validate(roboticsCaseData.toJsonString());
             }
 
