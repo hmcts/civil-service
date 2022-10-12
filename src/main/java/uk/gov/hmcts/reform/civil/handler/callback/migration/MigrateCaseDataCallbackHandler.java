0cba5ff1be6fed5db7bf35e5cb9fa72ab8ffc52d
sankaviv1 <sankavi.mohanraj@version1.com>
2022-10-12 15:30:14 +0100

    update caseRole for spec cases

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/migration/MigrateCaseDataCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/migration/MigrateCaseDataCallbackHandler.java
@@ -7,10 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.ccd.model.OrganisationPolicy;
 import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.enums.CaseRole;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 
 import java.util.Collections;
@@ -40,10 +42,11 @@ public class MigrateCaseDataCallbackHandler extends CallbackHandler {
         CaseData oldCaseData = callbackParams.getCaseData();
         CaseData.CaseDataBuilder<?, ?> caseDataBuilder = oldCaseData.toBuilder();
 
-        log.info("Migrating case: {}", oldCaseData.getCcdCaseReference());
+        log.info("Migrating data for case: {}", oldCaseData.getCcdCaseReference());
 
         //if (SuperClaimType.SPEC_CLAIM.equals(oldCaseData.getSuperClaimType())) {
         //    caseDataBuilder.caseAccessCategory(CaseCategory.SPEC_CLAIM);
+        //    updateOrgPolicyCaseRole(oldCaseData, caseDataBuilder);
         //} else {
         //    caseDataBuilder.caseAccessCategory(CaseCategory.UNSPEC_CLAIM);
         //}
@@ -51,6 +54,34 @@ public class MigrateCaseDataCallbackHandler extends CallbackHandler {
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(caseDataBuilder.build().toMap(objectMapper))
             .build();
+    }
+
+    private void updateOrgPolicyCaseRole(CaseData oldCaseData,
+                                         CaseData.CaseDataBuilder<?, ?> caseDataBuilder) {
+        OrganisationPolicy applicant1OrganisationPolicy = oldCaseData.getApplicant1OrganisationPolicy();
+        caseDataBuilder.applicant1OrganisationPolicy(OrganisationPolicy.builder()
+                             .orgPolicyReference(applicant1OrganisationPolicy.getOrgPolicyReference())
+                             .orgPolicyCaseAssignedRole(CaseRole.APPLICANTSOLICITORONE.getFormattedName())
+                             .organisation(applicant1OrganisationPolicy.getOrganisation())
+                             .build());
+
+        OrganisationPolicy respondent1OrganisationPolicy = oldCaseData.getRespondent1OrganisationPolicy();
+        if (respondent1OrganisationPolicy != null) {
+            caseDataBuilder.respondent1OrganisationPolicy(OrganisationPolicy.builder()
+                              .organisation(respondent1OrganisationPolicy.getOrganisation())
+                              .orgPolicyReference(respondent1OrganisationPolicy.getOrgPolicyReference())
+                              .orgPolicyCaseAssignedRole(CaseRole.RESPONDENTSOLICITORONE.getFormattedName())
+                              .build());
+        }
+
+        OrganisationPolicy respondent2OrganisationPolicy = oldCaseData.getRespondent2OrganisationPolicy();
+        if (respondent2OrganisationPolicy != null) {
+            caseDataBuilder.respondent2OrganisationPolicy(OrganisationPolicy.builder()
+                            .organisation(respondent2OrganisationPolicy.getOrganisation())
+                            .orgPolicyReference(respondent1OrganisationPolicy.getOrgPolicyReference())
+                            .orgPolicyCaseAssignedRole(CaseRole.RESPONDENTSOLICITORTWO.getFormattedName())
+                            .build());
+        }
     }
 
     @Override
