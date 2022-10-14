c518184e28870c1db37a0ec10f77df3e5274d6dd
indu2019 <indu.chaturvedi@hmcts.net>
2022-10-14 16:23:32 +0100

    CIV-5261, fixing Response Deadline for Sealed claim form

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/sealedclaim/SealedClaimFormGeneratorForSpec.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/sealedclaim/SealedClaimFormGeneratorForSpec.java
@@ -33,6 +33,7 @@ import uk.gov.hmcts.reform.civil.utils.MonetaryConversions;
 
 import java.math.BigDecimal;
 import java.net.URI;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -200,9 +201,13 @@ public class SealedClaimFormGeneratorForSpec implements TemplateDataGenerator<Se
     }
 
     private String getResponseDedline(CaseData caseData) {
+        LocalDate issueDate = caseData.getIssueDate();
+        if(isAfterFourPM()) {
+          issueDate = issueDate.plusDays(1);
+        }
         var notificationDeadline = formatLocalDate(
             deadlinesCalculator
-                .calculateFirstWorkingDay(caseData.getIssueDate().plusDays(28)),
+                .calculateFirstWorkingDay(issueDate.plusDays(28)),
             DATE
         );
         return END_OF_BUSINESS_DAY + notificationDeadline;
