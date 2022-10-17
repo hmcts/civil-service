1475a6341821e89714582752d38a3c6ae8ad3bac
Astha Malviya <astha.malviya@HMCTS.NET>
2022-10-17 17:45:58 +0100

    changes for all three scenerios

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimDismissedApplicantNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimDismissedApplicantNotificationHandler.java
@@ -9,15 +9,26 @@ import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.config.properties.notification.NotificationsProperties;
+import uk.gov.hmcts.reform.civil.enums.CaseState;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.NotificationService;
+import uk.gov.hmcts.reform.civil.service.flowstate.StateFlowEngine;
+import uk.gov.hmcts.reform.civil.stateflow.StateFlow;
+import uk.gov.hmcts.reform.civil.stateflow.model.State;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_APPLICANT_SOLICITOR1_FOR_CLAIM_DISMISSED;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.AWAITING_CASE_DETAILS_NOTIFICATION;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.AWAITING_RESPONDENT_ACKNOWLEDGEMENT;
+import static uk.gov.hmcts.reform.civil.service.flowstate.FlowPredicate.*;
+import static uk.gov.hmcts.reform.civil.service.flowstate.FlowState.Main.*;
 import static uk.gov.hmcts.reform.civil.utils.PartyUtils.buildPartiesReferences;
+
 
 @Service
 @RequiredArgsConstructor
@@ -32,6 +43,7 @@ public class ClaimDismissedApplicantNotificationHandler extends CallbackHandler
 
     private final NotificationService notificationService;
     private final NotificationsProperties notificationsProperties;
+    private final StateFlowEngine stateFlowEngine;
 
     @Override
     protected Map<String, Callback> callbacks() {
@@ -54,53 +66,10 @@ public class ClaimDismissedApplicantNotificationHandler extends CallbackHandler
     private CallbackResponse notifyApplicantSolicitorForClaimDismissed(
         CallbackParams callbackParams) {
         CaseData caseData = callbackParams.getCaseData();
-        String solicitorClaimDismissedProperty = null;
-
+        String solicitorClaimDismissedProperty = getSolicitorClaimDismissedProperty(callbackParams.getCaseData());
         notificationService.sendMail(
             caseData.getApplicantSolicitor1UserDetails().getEmail(),
-            notificationsProperties.getSolicitorClaimDismissed(),
-            addProperties(caseData),
-            String.format(REFERENCE_TEMPLATE, caseData.getLegacyCaseReference())
-        );
-
-        return AboutToStartOrSubmitCallbackResponse.builder().build();
-    }
-
-    private CallbackResponse notifyApplicantSolicitorForClaimDismissedWithin4Months(
-        CallbackParams callbackParams) {
-        CaseData caseData = callbackParams.getCaseData();
-
-        notificationService.sendMail(
-            caseData.getApplicantSolicitor1UserDetails().getEmail(),
-            notificationsProperties.getSolicitorClaimDismissed(),
-            addProperties(caseData),
-            String.format(REFERENCE_TEMPLATE, caseData.getLegacyCaseReference())
-        );
-
-        return AboutToStartOrSubmitCallbackResponse.builder().build();
-    }
-
-    private CallbackResponse notifyApplicantSolicitorForClaimDismissedWithin14Days(
-        CallbackParams callbackParams) {
-        CaseData caseData = callbackParams.getCaseData();
-
-        notificationService.sendMail(
-            caseData.getApplicantSolicitor1UserDetails().getEmail(),
-            notificationsProperties.getSolicitorClaimDismissed(),
-            addProperties(caseData),
-            String.format(REFERENCE_TEMPLATE, caseData.getLegacyCaseReference())
-        );
-
-        return AboutToStartOrSubmitCallbackResponse.builder().build();
-    }
-
-    private CallbackResponse notifyApplicantSolicitorForClaimDismissedWithinDeadline(
-        CallbackParams callbackParams) {
-        CaseData caseData = callbackParams.getCaseData();
-
-        notificationService.sendMail(
-            caseData.getApplicantSolicitor1UserDetails().getEmail(),
-            notificationsProperties.getSolicitorClaimDismissed(),
+            solicitorClaimDismissedProperty,
             addProperties(caseData),
             String.format(REFERENCE_TEMPLATE, caseData.getLegacyCaseReference())
         );
@@ -115,4 +84,53 @@ public class ClaimDismissedApplicantNotificationHandler extends CallbackHandler
             PARTY_REFERENCES, buildPartiesReferences(caseData)
         );
     }
+
+
+    private String getSolicitorClaimDismissedProperty(CaseData caseData) {
+
+        StateFlow stateFlow = stateFlowEngine.evaluate(caseData);
+        List<String> stateHistoryNameList = stateFlow.getStateHistory().stream().map(State::getName).collect(Collectors.toList());
+
+        //scenerio 1: Claim notification does not happen within 4 months of issue
+        if (stateHistoryNameList.contains(CLAIM_ISSUED.fullName())
+            && !stateHistoryNameList.contains(CLAIM_NOTIFIED.fullName())
+            && ifPastClaimNotificationDeadline(caseData)) {
+            return notificationsProperties.getSolicitorClaimDismissedWithin4Months();
+        }//scenerio 2: Claims details notification is not completed within 14 days of the claim notification step
+        else if (stateHistoryNameList.contains(CLAIM_NOTIFIED.fullName())
+            && stateHistoryNameList.contains(AWAITING_CASE_DETAILS_NOTIFICATION.name())
+            && !stateHistoryNameList.contains(CLAIM_DETAILS_NOTIFIED.fullName())
+            && ifPastClaimDetailsNotificationDeadline(caseData)) {
+            return notificationsProperties.getSolicitorClaimDismissedWithin4Months();
+        }//scenerio 3 Claimant does not give their intention by the given deadline
+        else if(stateHistoryNameList.contains(CLAIM_DETAILS_NOTIFIED.fullName())
+            && stateHistoryNameList.contains(AWAITING_RESPONDENT_ACKNOWLEDGEMENT.name())
+            && ifPastClaimDismissedDeadline(caseData)){
+            return notificationsProperties.getSolicitorClaimDismissedWithinDeadline();
+        }
+        else{
+            return notificationsProperties.getSolicitorClaimDismissedWithinDeadline();
+        }
+    }
+
+    private boolean ifPastClaimDismissedDeadline(CaseData caseData) {
+        return caseData.getClaimDismissedDate() != null
+            && caseData.getClaimDismissedDeadline().isBefore(LocalDateTime.now());
+    }
+
+    private boolean ifPastClaimNotificationDeadline(CaseData caseData) {
+        return caseData.getClaimDismissedDate() != null
+            && caseData.getClaimNotificationDeadline() != null
+            && caseData.getClaimNotificationDeadline().isBefore(LocalDateTime.now())
+            && caseData.getClaimNotificationDate() == null;
+    }
+
+    private boolean ifPastClaimDetailsNotificationDeadline(CaseData caseData) {
+        return caseData.getClaimDismissedDate() != null
+            && caseData.getClaimDetailsNotificationDeadline() != null
+            && caseData.getClaimDetailsNotificationDeadline().isBefore(LocalDateTime.now())
+            && caseData.getClaimDetailsNotificationDate() == null
+            && caseData.getClaimNotificationDate() != null;
+    }
+
 }
--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimDismissedRespondentNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimDismissedRespondentNotificationHandler.java
@@ -11,13 +11,22 @@ import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.config.properties.notification.NotificationsProperties;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.NotificationService;
+import uk.gov.hmcts.reform.civil.service.flowstate.StateFlowEngine;
+import uk.gov.hmcts.reform.civil.stateflow.StateFlow;
+import uk.gov.hmcts.reform.civil.stateflow.model.State;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_RESPONDENT_SOLICITOR1_FOR_CLAIM_DISMISSED;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_RESPONDENT_SOLICITOR2_FOR_CLAIM_DISMISSED;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.AWAITING_CASE_DETAILS_NOTIFICATION;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.AWAITING_RESPONDENT_ACKNOWLEDGEMENT;
+import static uk.gov.hmcts.reform.civil.service.flowstate.FlowState.Main.*;
+import static uk.gov.hmcts.reform.civil.service.flowstate.FlowState.Main.CLAIM_DETAILS_NOTIFIED;
 import static uk.gov.hmcts.reform.civil.utils.NotificationUtils.is1v1Or2v1Case;
 import static uk.gov.hmcts.reform.civil.utils.NotificationUtils.isRespondent1;
 import static uk.gov.hmcts.reform.civil.utils.PartyUtils.buildPartiesReferences;
@@ -38,6 +47,7 @@ public class ClaimDismissedRespondentNotificationHandler extends CallbackHandler
 
     private final NotificationService notificationService;
     private final NotificationsProperties notificationsProperties;
+    private final StateFlowEngine stateFlowEngine;
 
     @Override
     protected Map<String, Callback> callbacks() {
@@ -65,9 +75,11 @@ public class ClaimDismissedRespondentNotificationHandler extends CallbackHandler
             && !isRespondent1(callbackParams, NOTIFY_RESPONDENT_SOLICITOR1_FOR_CLAIM_DISMISSED) ? caseData
                 .getRespondentSolicitor2EmailAddress() : caseData.getRespondentSolicitor1EmailAddress();
 
+        String solicitorClaimDismissedProperty = getSolicitorClaimDismissedProperty(callbackParams.getCaseData());
+
         notificationService.sendMail(
             recipient,
-            notificationsProperties.getSolicitorClaimDismissed(),
+            solicitorClaimDismissedProperty,
             addProperties(caseData),
             String.format(REFERENCE_TEMPLATE, caseData.getLegacyCaseReference())
         );
@@ -81,5 +93,52 @@ public class ClaimDismissedRespondentNotificationHandler extends CallbackHandler
             CLAIM_REFERENCE_NUMBER, caseData.getLegacyCaseReference(),
             PARTY_REFERENCES, buildPartiesReferences(caseData)
         );
+    }
+
+    private String getSolicitorClaimDismissedProperty(CaseData caseData) {
+
+        StateFlow stateFlow = stateFlowEngine.evaluate(caseData);
+        List<String> stateHistoryNameList = stateFlow.getStateHistory().stream().map(State::getName).collect(Collectors.toList());
+
+        //scenerio 1: Claim notification does not happen within 4 months of issue
+        if (stateHistoryNameList.contains(CLAIM_ISSUED.fullName())
+            && !stateHistoryNameList.contains(CLAIM_NOTIFIED.fullName())
+            && ifPastClaimNotificationDeadline(caseData)) {
+            return notificationsProperties.getSolicitorClaimDismissedWithin4Months();
+        }//scenerio 2: Claims details notification is not completed within 14 days of the claim notification step
+        else if (stateHistoryNameList.contains(CLAIM_NOTIFIED.fullName())
+            && stateHistoryNameList.contains(AWAITING_CASE_DETAILS_NOTIFICATION.name())
+            && !stateHistoryNameList.contains(CLAIM_DETAILS_NOTIFIED.fullName())
+            && ifPastClaimDetailsNotificationDeadline(caseData)) {
+            return notificationsProperties.getSolicitorClaimDismissedWithin4Months();
+        }//scenerio 3 Claimant does not give their intention by the given deadline
+        else if(stateHistoryNameList.contains(CLAIM_DETAILS_NOTIFIED.fullName())
+            && stateHistoryNameList.contains(AWAITING_RESPONDENT_ACKNOWLEDGEMENT.name())
+            && ifPastClaimDismissedDeadline(caseData)){
+            return notificationsProperties.getSolicitorClaimDismissedWithinDeadline();
+        }
+        else{
+            return notificationsProperties.getSolicitorClaimDismissedWithinDeadline();
+        }
+    }
+
+    private boolean ifPastClaimDismissedDeadline(CaseData caseData) {
+        return caseData.getClaimDismissedDate() != null
+            && caseData.getClaimDismissedDeadline().isBefore(LocalDateTime.now());
+    }
+
+    private boolean ifPastClaimNotificationDeadline(CaseData caseData) {
+        return caseData.getClaimDismissedDate() != null
+            && caseData.getClaimNotificationDeadline() != null
+            && caseData.getClaimNotificationDeadline().isBefore(LocalDateTime.now())
+            && caseData.getClaimNotificationDate() == null;
+    }
+
+    private boolean ifPastClaimDetailsNotificationDeadline(CaseData caseData) {
+        return caseData.getClaimDismissedDate() != null
+            && caseData.getClaimDetailsNotificationDeadline() != null
+            && caseData.getClaimDetailsNotificationDeadline().isBefore(LocalDateTime.now())
+            && caseData.getClaimDetailsNotificationDate() == null
+            && caseData.getClaimNotificationDate() != null;
     }
 }
