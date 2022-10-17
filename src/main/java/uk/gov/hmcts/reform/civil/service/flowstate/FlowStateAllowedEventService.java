75f94bb130a757a7342bb0d08e5ee4884ac97270
Mark Drummond <mark.drummond@version1.com>
2022-10-17 14:33:38 +0100

    update FlowStateAllowedEventService

--- src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/FlowStateAllowedEventService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/FlowStateAllowedEventService.java
@@ -174,7 +174,8 @@ public class FlowStateAllowedEventService {
 
                 INITIATE_GENERAL_APPLICATION,
                 CREATE_SDO,
-                NotSuitable_SDO
+                NotSuitable_SDO,
+                EVIDENCE_UPLOAD_JUDGE
             )
         ),
 
@@ -559,7 +560,8 @@ public class FlowStateAllowedEventService {
                 INITIATE_GENERAL_APPLICATION,
                 CREATE_SDO,
                 HEARING_SCHEDULED,
-                NotSuitable_SDO
+                NotSuitable_SDO,
+                EVIDENCE_UPLOAD_JUDGE
             )
         ),
         entry(
