22c12e2509ebb3d0a477c009be336f2c254bd5c9
Filippo Possenti <filippo.possenti@version1.com>
2022-10-19 15:41:01 +0100

    Checkstyle
    - Satisfying pedantic checkstyle requirement

--- src/main/java/uk/gov/hmcts/reform/civil/config/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/JacksonConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JacksonConfiguration {
+
     public static final String DATE_FORMAT = "yyyy-MM-dd";
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
 
