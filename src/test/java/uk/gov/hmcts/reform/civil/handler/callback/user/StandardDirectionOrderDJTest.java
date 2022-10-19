2dac638fcfc349ec60570801dc6e10d1903d3bb8
TurkingtonL <lillie.turkington@HMCTS.net>
2022-10-19 17:09:10 +0100

    updating test

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/StandardDirectionOrderDJTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/StandardDirectionOrderDJTest.java
@@ -467,7 +467,7 @@ public class StandardDirectionOrderDJTest extends BaseCallbackHandlerTest {
 
             assertThat(response.getData()).extracting("disposalHearingOrderMadeWithoutHearingDJ").extracting("input")
                 .isEqualTo(String.format("This order has been made without a hearing. Each party "
-                                             + "has the right to apply to have this Order "
+                                             + "has the right to apply to have this order "
                                              + "set aside or varied. Any such application must be "
                                              + "received by the Court "
                                              + "(together with the appropriate fee) by 4pm on %s.",
