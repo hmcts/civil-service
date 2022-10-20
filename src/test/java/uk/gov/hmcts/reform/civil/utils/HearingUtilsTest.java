4e9c9016f279d01e8ccd7500af7505f69a279a22
Filippo Possenti <filippo.possenti@version1.com>
2022-10-20 11:52:23 +0100

    Checkstyle and test improvements
    - Improves on unit-tests to reduce code
    - Adds description on some unit-tests
    - Fixes pedantic checkstyle complaints

--- src/test/java/uk/gov/hmcts/reform/civil/utils/HearingUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/utils/HearingUtilsTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.utils;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -32,67 +33,26 @@ public class HearingUtilsTest {
         assertThat(days).isNotNull();
     }
 
-    @Test
-    void shouldReturnFee1_whenGivenAnyClaimFee() {
-        assertThat(HearingUtils.getFastTrackFee(0)).isEqualTo(new BigDecimal(0));
+    @ParameterizedTest
+    @CsvSource(value = {"0;0", "15000;2700", "35000;5900", "90000;8500", "140000;12300",
+        "290000;18100", "500000;34600"}, delimiter = ';')
+    @DisplayName("HearingUtils.getFastTrackFee should return the appropriate fast-track fee" +
+        " based on the claim fee amount.")
+    void shouldReturnFee_whenGivenAnyClaimFee(int intClaimFee, int expectedFastTrackFee) {
+        assertThat(HearingUtils.getFastTrackFee(intClaimFee)).isEqualTo(new BigDecimal(expectedFastTrackFee));
     }
 
     @Test
-    void shouldReturnFee2_whenGivenAnyClaimFee() {
-        assertThat(HearingUtils.getFastTrackFee(15000)).isEqualTo(new BigDecimal(2700));
-    }
-
-    @Test
-    void shouldReturnFee3_whenGivenAnyClaimFee() {
-        assertThat(HearingUtils.getFastTrackFee(35000)).isEqualTo(new BigDecimal(5900));
-    }
-
-    @Test
-    void shouldReturnFee4_whenGivenAnyClaimFee() {
-        assertThat(HearingUtils.getFastTrackFee(90000)).isEqualTo(new BigDecimal(8500));
-    }
-
-    @Test
-    void shouldReturnFee5_whenGivenAnyClaimFee() {
-        assertThat(HearingUtils.getFastTrackFee(140000)).isEqualTo(new BigDecimal(12300));
-    }
-
-    @Test
-    void shouldReturnFee6_whenGivenAnyClaimFee() {
-        assertThat(HearingUtils.getFastTrackFee(290000)).isEqualTo(new BigDecimal(18100));
-    }
-
-    @Test
-    void shouldReturnFee7_whenGivenAnyClaimFee() {
-        assertThat(HearingUtils.getFastTrackFee(500000)).isEqualTo(new BigDecimal(34600));
-    }
-
-    @Test
-    void shouldReturnFormattedFee_whenGivenMidClaimFee() {
-        assertThat(HearingUtils.formatHearingFee(
-            Fee.builder().calculatedAmountInPence(new BigDecimal(34600)).build())).isEqualTo("£346");
-    }
-
-    @Test
-    void shouldReturnFormattedFee_whenGivenBigClaimFee() {
-        assertThat(HearingUtils.formatHearingFee(
-            Fee.builder().calculatedAmountInPence(new BigDecimal(132000)).build())).isEqualTo("£1,320");
-    }
-
-    @Test
-    void shouldReturnFormattedFee_whenGivenSmallClaimFee() {
-        assertThat(HearingUtils.formatHearingFee(
-            Fee.builder().calculatedAmountInPence(new BigDecimal(5000)).build())).isEqualTo("£50");
-    }
-
-    @Test
+    @DisplayName("HearingUtils.formatHearingFee should return <null> when the hearing fee is zero.")
     void shouldReturnNull_when0ClaimFee() {
         assertThat(HearingUtils.formatHearingFee(
             Fee.builder().calculatedAmountInPence(new BigDecimal(0)).build())).isNull();
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"34600;£346", "132000;£1,320", "5000;£50"}, delimiter=';')
+    @CsvSource(value = {"34600;£346", "132000;£1,320", "5000;£50"}, delimiter = ';')
+    @DisplayName("HearingUtils.formatHearingFee should format an amount in pence into a pound value." +
+        " Fractional values can be discarded.")
     void shouldReturnFormattedFee_whenGivenAnyClaimFee(int amount, String expectedOutput) {
         BigDecimal feeAmount = new BigDecimal(amount);
         assertThat(HearingUtils.formatHearingFee(
@@ -107,17 +67,23 @@ public class HearingUtilsTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"000", "50000", "08:00", "8:00", "12:00", "23:00"})
+    @DisplayName("HearingUtils.getHearingTimeFormatted should not allow invalid values. Valid values" +
+        " are composed of 4 numerical digits only.")
     void shouldReturnNull_whenNotAllowedTime(String input) {
         assertThat(HearingUtils.getHearingTimeFormatted(input)).isNull();
     }
 
     @Test
+    @DisplayName("HearingUtils.getHearingTimeFormatted should return <null> when an empty string is passed.")
     void shouldReturnNull_whenGivenEmptyTime() {
         assertThat(HearingUtils.getHearingTimeFormatted("")).isNull();
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"0000;00:00", "0500;05:00", "1200;12:00", "2300;23:00", "1230;12:30", "2318;23:18"}, delimiter=';')
+    @CsvSource(value = {"0000;00:00", "0500;05:00", "1200;12:00", "2300;23:00",
+        "1230;12:30", "2318;23:18"}, delimiter = ';')
+    @DisplayName("HearingUtils.getHearingTimeFormatted should put a ':' between a 4-digit value" +
+        " as they are considered to be hours.")
     void shouldReturnTimedFormatted_whenGivenAnyTime(String input, String expectedOutput) {
         assertThat(HearingUtils.getHearingTimeFormatted(input)).isEqualTo(expectedOutput);
     }
