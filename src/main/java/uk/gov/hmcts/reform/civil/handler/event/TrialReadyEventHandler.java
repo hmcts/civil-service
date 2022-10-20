9ea7ff63c6bd5a650d65005115643eb294f91fb6
meulendijksm <max.meulendijks@hmcts.net>
2022-10-20 17:46:11 +0100

    readded trial ready event handler

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/event/TrialReadyEventHandler.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.civil.handler.event;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.civil.event.TrialReadyEvent;
+import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.TRIAL_READY;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TrialReadyEventHandler {
+
+    private final CoreCaseDataService coreCaseDataService;
+
+    @EventListener
+    public void sendTrialReadyNotification(TrialReadyEvent event) {
+        coreCaseDataService.triggerEvent(event.getCaseId(), TRIAL_READY);
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/civil/service/tasklisteners/TrialReadyExternalTaskListener.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.civil.service.tasklisteners;
+
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.client.topic.TopicSubscriptionBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.civil.handler.tasks.TakeCaseOfflineHandler;
+import uk.gov.hmcts.reform.civil.handler.tasks.TrialReadyCheckHandler;
+
+@Component
+public class TrialReadyExternalTaskListener {
+
+    private static final String TOPIC = "TRIAL_READY_CHECK";
+
+    @Autowired
+    private TrialReadyExternalTaskListener(TrialReadyCheckHandler handler, ExternalTaskClient client) {
+        TopicSubscriptionBuilder subscriptionBuilder = client.subscribe(TOPIC);
+        subscriptionBuilder.handler(handler).open();
+    }
+}
