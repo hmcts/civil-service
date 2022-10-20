2423809a510fa09cfc272c799a3f3a7e521c2236
Filippo Possenti <filippo.possenti@version1.com>
2022-10-20 11:58:09 +0100

    Checkstyle
    - Fixes more pedantic checkstyle complaints

--- src/test/java/uk/gov/hmcts/reform/civil/utils/HearingUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/utils/HearingUtilsTest.java
@@ -36,8 +36,8 @@ public class HearingUtilsTest {
     @ParameterizedTest
     @CsvSource(value = {"0;0", "15000;2700", "35000;5900", "90000;8500", "140000;12300",
         "290000;18100", "500000;34600"}, delimiter = ';')
-    @DisplayName("HearingUtils.getFastTrackFee should return the appropriate fast-track fee" +
-        " based on the claim fee amount.")
+    @DisplayName("HearingUtils.getFastTrackFee should return the appropriate fast-track fee"
+        + " based on the claim fee amount.")
     void shouldReturnFee_whenGivenAnyClaimFee(int intClaimFee, int expectedFastTrackFee) {
         assertThat(HearingUtils.getFastTrackFee(intClaimFee)).isEqualTo(new BigDecimal(expectedFastTrackFee));
     }
@@ -51,8 +51,8 @@ public class HearingUtilsTest {
 
     @ParameterizedTest
     @CsvSource(value = {"34600;£346", "132000;£1,320", "5000;£50"}, delimiter = ';')
-    @DisplayName("HearingUtils.formatHearingFee should format an amount in pence into a pound value." +
-        " Fractional values can be discarded.")
+    @DisplayName("HearingUtils.formatHearingFee should format an amount in pence into a pound value."
+        + " Fractional values can be discarded.")
     void shouldReturnFormattedFee_whenGivenAnyClaimFee(int amount, String expectedOutput) {
         BigDecimal feeAmount = new BigDecimal(amount);
         assertThat(HearingUtils.formatHearingFee(
@@ -67,8 +67,8 @@ public class HearingUtilsTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"000", "50000", "08:00", "8:00", "12:00", "23:00"})
-    @DisplayName("HearingUtils.getHearingTimeFormatted should not allow invalid values. Valid values" +
-        " are composed of 4 numerical digits only.")
+    @DisplayName("HearingUtils.getHearingTimeFormatted should not allow invalid values. Valid values"
+        + " are composed of 4 numerical digits only.")
     void shouldReturnNull_whenNotAllowedTime(String input) {
         assertThat(HearingUtils.getHearingTimeFormatted(input)).isNull();
     }
@@ -82,8 +82,8 @@ public class HearingUtilsTest {
     @ParameterizedTest
     @CsvSource(value = {"0000;00:00", "0500;05:00", "1200;12:00", "2300;23:00",
         "1230;12:30", "2318;23:18"}, delimiter = ';')
-    @DisplayName("HearingUtils.getHearingTimeFormatted should put a ':' between a 4-digit value" +
-        " as they are considered to be hours.")
+    @DisplayName("HearingUtils.getHearingTimeFormatted should put a ':' between a 4-digit value"
+        + " as they are considered to be hours.")
     void shouldReturnTimedFormatted_whenGivenAnyTime(String input, String expectedOutput) {
         assertThat(HearingUtils.getHearingTimeFormatted(input)).isEqualTo(expectedOutput);
     }
