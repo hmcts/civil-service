652b2973ae6c3844f6bc5778f2a91ae69a677fa0
Pablo Ortiz <pablo.gutierrez@version1.com>
2022-10-25 11:08:40 +0200

    CIV-5217: Claim Fee and Hearing Fee (#1664)

--- src/test/java/uk/gov/hmcts/reform/civil/service/FeesServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/FeesServiceTest.java
@@ -19,7 +19,6 @@ import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -28,6 +27,7 @@ class FeesServiceTest {
 
     private static final String CHANNEL = "channel";
     private static final String EVENT = "event";
+    private static final String HEARING_EVENT = "hearingEvent";
     private static final BigDecimal TEST_FEE_AMOUNT_POUNDS = new BigDecimal("1.00");
     private static final BigDecimal TEST_FEE_AMOUNT_PENCE = new BigDecimal("100");
     private static final BigDecimal MIN_RANGE = new BigDecimal("0.01");
@@ -44,7 +44,7 @@ class FeesServiceTest {
 
     @BeforeEach
     void setUp() {
-        given(feesClient.lookupFee(any(), any(), eq(new BigDecimal("50.00"))))
+        given(feesClient.lookupFee(any(), any(), any()))
             .willReturn(FeeLookupResponseDto.builder()
                             .feeAmount(TEST_FEE_AMOUNT_POUNDS)
                             .code("test_fee_code")
@@ -53,6 +53,7 @@ class FeesServiceTest {
         given(feesClient.findRangeGroup(any(), any())).willReturn(buildFeeRangeResponse());
         given(feesConfiguration.getChannel()).willReturn(CHANNEL);
         given(feesConfiguration.getEvent()).willReturn(EVENT);
+        given(feesConfiguration.getHearingEvent()).willReturn(HEARING_EVENT);
     }
 
     @Test
@@ -70,6 +71,35 @@ class FeesServiceTest {
         Fee feeDto = feesService.getFeeDataByClaimValue(claimValue);
 
         verify(feesClient).lookupFee(CHANNEL, EVENT, new BigDecimal("50.00"));
+        assertThat(feeDto).isEqualTo(expectedFeeDto);
+    }
+
+    @Test
+    void shouldReturnFeeDataByTotalClaimAmount() {
+
+        Fee expectedFeeDto = Fee.builder()
+            .calculatedAmountInPence(TEST_FEE_AMOUNT_PENCE)
+            .code("test_fee_code")
+            .version("1")
+            .build();
+
+        Fee feeDto = feesService.getFeeDataByTotalClaimAmount(new BigDecimal("7000.00"));
+
+        verify(feesClient).lookupFee(CHANNEL, EVENT, new BigDecimal("7000.00"));
+        assertThat(feeDto).isEqualTo(expectedFeeDto);
+    }
+
+    @Test
+    void shouldReturnHearingFeeData() {
+        Fee expectedFeeDto = Fee.builder()
+            .calculatedAmountInPence(TEST_FEE_AMOUNT_PENCE)
+            .code("test_fee_code")
+            .version("1")
+            .build();
+
+        Fee feeDto = feesService.getHearingFeeDataByTotalClaimAmount(new BigDecimal("7000.00"));
+
+        verify(feesClient).lookupFee(CHANNEL, HEARING_EVENT, new BigDecimal("7000.00"));
         assertThat(feeDto).isEqualTo(expectedFeeDto);
     }
 
