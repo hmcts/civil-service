d1ca2eb259e413e47b5905d34735cd17c0e3eb42
= <gareth.lancaster1@hmcts.net>
2022-10-27 11:36:10 +0100

    Removed unecessary previous orgs sorting

--- src/main/java/uk/gov/hmcts/reform/civil/utils/OrgPolicyUtils.java
+++ src/main/java/uk/gov/hmcts/reform/civil/utils/OrgPolicyUtils.java
@@ -1,15 +1,7 @@
 package uk.gov.hmcts.reform.civil.utils;
 
 import uk.gov.hmcts.reform.ccd.model.OrganisationPolicy;
-import uk.gov.hmcts.reform.ccd.model.PreviousOrganisation;
-import uk.gov.hmcts.reform.ccd.model.PreviousOrganisationCollectionItem;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Stream;
 
 public class OrgPolicyUtils {
 
@@ -32,34 +24,5 @@ public class OrgPolicyUtils {
             && orgPolicy.getOrganisation() != null
             && orgPolicy.getOrganisation().getOrganisationID() != null
             ? orgPolicy.getOrganisation().getOrganisationID() : null;
-    }
-
-    private static Comparator<PreviousOrganisationCollectionItem> sortPreviousOrgs() {
-        return (nocEvent1, nocEvent2) -> {
-            if (nocEvent1.getValue().getToTimestamp().isAfter(nocEvent2.getValue().getToTimestamp())) {
-                return -1;
-            } else if (nocEvent1.getValue().getToTimestamp().isBefore(nocEvent2.getValue().getToTimestamp())) {
-                return 1;
-            } else {
-                return 0;
-            }
-        };
-    }
-
-    private static List<PreviousOrganisationCollectionItem> getPreviousOrganisations(OrganisationPolicy orgPolicy) {
-        return orgPolicy != null
-            && orgPolicy.getPreviousOrganisations() != null
-            ? orgPolicy.getPreviousOrganisations() : new ArrayList<>();
-    }
-
-    public static PreviousOrganisation getLatestNoCEvent(CaseData caseData) {
-        var mostRecentOrganisation = Stream.of(
-                getPreviousOrganisations(caseData.getApplicant1OrganisationPolicy()),
-                getPreviousOrganisations(caseData.getRespondent1OrganisationPolicy()),
-                getPreviousOrganisations(caseData.getRespondent2OrganisationPolicy())
-            ).flatMap(Collection::stream)
-            .sorted(sortPreviousOrgs())
-            .findFirst();
-        return mostRecentOrganisation.isPresent() ? mostRecentOrganisation.get().getValue() : null;
     }
 }
