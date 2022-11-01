6ed5858d3a73189bab0ac90e787bd739c015e91c
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2022-11-01 07:45:18 +0000

    temp commit

--- src/test/java/uk/gov/hmcts/reform/civil/helpers/LocalDateTimeHelperTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/helpers/LocalDateTimeHelperTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.TimeZone;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,18 +15,31 @@ class LocalDateTimeHelperTest {
     void utcTimeShouldBeBehindByAnHour() {
         LocalDateTime utcTime = LocalDateTimeHelper.nowInUTC();
         LocalDateTime localTime = LocalDateTimeHelper.nowInLocalZone();
+        TimeZone localTimeZone = TimeZone.getTimeZone(LocalDateTimeHelper.LOCAL_ZONE);
 
         long difference = ChronoUnit.HOURS.between(utcTime, localTime);
-
-        assertThat(difference).isEqualTo(1);
+        if (localTimeZone.inDaylightTime(Date
+                                             .from(localTime.atZone(LocalDateTimeHelper.LOCAL_ZONE)
+                                                       .toInstant()))) {
+            assertThat(difference).isEqualTo(1);
+        } else {
+            assertThat(difference).isEqualTo(0);
+        }
     }
 
     @Test
     void utcTimeToLocalTimeConversionShouldIncrementByAnHour() {
         LocalDateTime utcTime = LocalDateTimeHelper.nowInUTC();
+        LocalDateTime localTime = LocalDateTimeHelper.fromUTC(utcTime);
+        long difference = ChronoUnit.HOURS.between(utcTime, localTime);
+        TimeZone localTimeZone = TimeZone.getTimeZone(LocalDateTimeHelper.LOCAL_ZONE);
 
-        long difference = ChronoUnit.HOURS.between(utcTime, LocalDateTimeHelper.fromUTC(utcTime));
-
-        assertThat(difference).isEqualTo(1);
+        if (localTimeZone.inDaylightTime(Date
+                                             .from(localTime.atZone(LocalDateTimeHelper.LOCAL_ZONE)
+                                                       .toInstant()))) {
+            assertThat(difference).isEqualTo(1);
+        } else {
+            assertThat(difference).isEqualTo(0);
+        }
     }
 }
