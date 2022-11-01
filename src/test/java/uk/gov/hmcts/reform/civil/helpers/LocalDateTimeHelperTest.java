80ee221385e76a357b4cba252b664517cc565d51
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2022-11-01 16:14:29 +0000

    Revert "temp commit"
    This reverts commit 6ed5858d3a73189bab0ac90e787bd739c015e91c.

--- src/test/java/uk/gov/hmcts/reform/civil/helpers/LocalDateTimeHelperTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/helpers/LocalDateTimeHelperTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Date;
-import java.util.TimeZone;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,31 +13,18 @@ class LocalDateTimeHelperTest {
     void utcTimeShouldBeBehindByAnHour() {
         LocalDateTime utcTime = LocalDateTimeHelper.nowInUTC();
         LocalDateTime localTime = LocalDateTimeHelper.nowInLocalZone();
-        TimeZone localTimeZone = TimeZone.getTimeZone(LocalDateTimeHelper.LOCAL_ZONE);
 
         long difference = ChronoUnit.HOURS.between(utcTime, localTime);
-        if (localTimeZone.inDaylightTime(Date
-                                             .from(localTime.atZone(LocalDateTimeHelper.LOCAL_ZONE)
-                                                       .toInstant()))) {
-            assertThat(difference).isEqualTo(1);
-        } else {
-            assertThat(difference).isEqualTo(0);
-        }
+
+        assertThat(difference).isEqualTo(1);
     }
 
     @Test
     void utcTimeToLocalTimeConversionShouldIncrementByAnHour() {
         LocalDateTime utcTime = LocalDateTimeHelper.nowInUTC();
-        LocalDateTime localTime = LocalDateTimeHelper.fromUTC(utcTime);
-        long difference = ChronoUnit.HOURS.between(utcTime, localTime);
-        TimeZone localTimeZone = TimeZone.getTimeZone(LocalDateTimeHelper.LOCAL_ZONE);
 
-        if (localTimeZone.inDaylightTime(Date
-                                             .from(localTime.atZone(LocalDateTimeHelper.LOCAL_ZONE)
-                                                       .toInstant()))) {
-            assertThat(difference).isEqualTo(1);
-        } else {
-            assertThat(difference).isEqualTo(0);
-        }
+        long difference = ChronoUnit.HOURS.between(utcTime, LocalDateTimeHelper.fromUTC(utcTime));
+
+        assertThat(difference).isEqualTo(1);
     }
 }
