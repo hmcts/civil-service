cb60785e0b65cf5dbd103bc7db4ce584114ea761
Filippo Possenti <filippo.possenti@version1.com>
2022-11-02 17:37:50 +0000

    Accept multiple input date/time formats
    - Accept yyyy-MM-dd for LocalDateTime jackson fields (previously not accepted, believe to be the source of some random failures)
    - Accept multiple formats for LocalDateTime jackson fields

--- src/main/java/uk/gov/hmcts/reform/civil/config/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/JacksonConfiguration.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.reform.civil.config;
 
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.util.Locale;
+
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 @Configuration
 public class JacksonConfiguration {
@@ -19,12 +23,24 @@ public class JacksonConfiguration {
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter
         .ofPattern(DATE_TIME_FORMAT, Locale.UK);
 
+    public static final DateTimeFormatter INPUT_DATE_TIME_FORMATTER =
+        new DateTimeFormatterBuilder()
+            .parseLenient()
+            .appendPattern("yyyy-MM-dd['T'HH:mm[:ss[.SSSSSSSSS]]]")
+            .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+            .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
+            .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
+            .parseDefaulting(ChronoField.NANO_OF_SECOND, 0)
+        .toFormatter(Locale.UK);
+
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jsonDateTimeFormatCustomizer() {
         return builder -> {
             builder.simpleDateFormat(DATE_TIME_FORMAT);
             builder.serializers(new LocalDateSerializer(DATE_FORMATTER));
             builder.serializers(new LocalDateTimeSerializer(DATE_TIME_FORMATTER));
+            builder.deserializers(new LocalDateDeserializer(DATE_FORMATTER));
+            builder.deserializers(new LocalDateTimeDeserializer(INPUT_DATE_TIME_FORMATTER));
         };
     }
 }
---
+++ src/test/java/uk/gov/hmcts/reform/civil/config/JacksonConfigurationTest.java
@@ -0,0 +1,49 @@
+package uk.gov.hmcts.reform.civil.config;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = {JacksonAutoConfiguration.class, JacksonConfiguration.class})
+class JacksonConfigurationTest {
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @ParameterizedTest()
+    @CsvSource({
+        "2022-11-02T16:25:37.123456789,2022-11-02T16:25:37.123456789",
+        "2022-11-02T16:25:37.123456,2022-11-02T16:25:37.123456000",
+        "2022-11-02T16:25:37.123,2022-11-02T16:25:37.123000000",
+        "2022-11-02T16:25:37,2022-11-02T16:25:37.000000000",
+        "2022-11-02T16:25,2022-11-02T16:25:00.000000000",
+        "2022-11-02,2022-11-02T00:00:00.000000000"})
+    @SneakyThrows
+    void shouldProcessInputDateTimeInATolerantWay(String inputValue, String expectedResult) {
+        // When
+        Map<String, LocalDateTime> parsed = mapper.readValue(
+            "{ \"date\": \"" + inputValue + "\" }",
+            new TypeReference<Map<String, LocalDateTime>>() {}
+        );
+        LocalDateTime actualResult = parsed.get("date");
+
+        // Then
+        assertThat(actualResult.format(DateTimeFormatter
+                                           .ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS", Locale.UK)))
+            .isEqualTo(expectedResult);
+
+
+    }
+}
