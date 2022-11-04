2851f8e02da9f5d2cefe4714f013fe01b0c38429
Sampan Kumar <sampan.kumar@hmcts.net>
2022-11-04 13:26:34 +0000

    Reverting 1716 changes

--- infrastructure/tf-kv-secrets-ethos.tf
+++ infrastructure/tf-kv-secrets-ethos.tf
@@ -1,6 +1,6 @@
 data "azurerm_key_vault" "ethos_vault" {
-  name                = "et-cos-${var.env}"
-  resource_group_name = "et-cos-${var.env}"
+    name                = "ethos-${var.env}"
+    resource_group_name = "ethos-repl-docmosis-backend-${var.env}"
 }
 
 data "azurerm_key_vault_secret" "tornado_access_secret" {
