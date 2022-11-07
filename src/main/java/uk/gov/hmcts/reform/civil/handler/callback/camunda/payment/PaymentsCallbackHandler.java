5650c2a46f0f0db993d842474e2716e6397a2dfe
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2022-11-07 16:22:54 +0000

    State changed for post payment

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/payment/PaymentsCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/payment/PaymentsCallbackHandler.java
@@ -12,6 +12,7 @@ import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.enums.CaseState;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.PaymentDetails;
 import uk.gov.hmcts.reform.civil.service.PaymentsService;
@@ -94,6 +95,7 @@ public class PaymentsCallbackHandler extends CallbackHandler {
             caseData = caseData.toBuilder()
                 .claimIssuedPaymentDetails(paymentDetails)
                 .paymentSuccessfulDate(time.now())
+                .ccdState(CaseState.CASE_ISSUED)
                 .build();
 
         } catch (FeignException e) {
