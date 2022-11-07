559dbfa79960494aaf857582006c4296340bdf48
Pijian Liao <Pijian.Liao@HMCTS.NET>
2022-11-07 14:14:24 +0000

    CIV-4113 CoS pojo

--- src/main/java/uk/gov/hmcts/reform/civil/model/CertificateOfService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/CertificateOfService.java
@@ -7,8 +7,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import uk.gov.hmcts.reform.civil.enums.CoSRecipientServeLocationOwnerType;
+import uk.gov.hmcts.reform.civil.enums.CoSRecipientServeType;
+import uk.gov.hmcts.reform.civil.enums.CosRecipientServeLocationType;
+import uk.gov.hmcts.reform.civil.model.common.Element;
+import uk.gov.hmcts.reform.civil.model.documents.Document;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Builder
@@ -19,4 +25,24 @@ import java.time.LocalDate;
 public class CertificateOfService {
     @JsonProperty("cosDateOfServiceForDefendant")
     private LocalDate cosDateOfServiceForDefendant;
+    @JsonProperty("cosServedDocumentFiles")
+    private String cosServedDocumentFiles;
+    @JsonProperty("cosEvidenceDocument")
+    List<Element<Document>> cosEvidenceDocument;
+    @JsonProperty("cosRecipient")
+    private String cosRecipient;
+    @JsonProperty("cosRecipientServeType")
+    private CoSRecipientServeType cosRecipientServeType;
+    @JsonProperty("cosRecipientServeLocation")
+    private String cosRecipientServeLocation;
+    @JsonProperty("cosRecipientServeLocationOwnerType")
+    private CoSRecipientServeLocationOwnerType cosRecipientServeLocationOwnerType;
+    @JsonProperty("cosRecipientServeLocationType")
+    private CosRecipientServeLocationType cosRecipientServeLocationType;
+    @JsonProperty("cosRecipientServeLocationTypeOther")
+    private String cosRecipientServeLocationTypeOther;
+    @JsonProperty("cosSender")
+    private String cosSender;
+    @JsonProperty("cosSenderFirm")
+    private String cosSenderFirm;
 }
