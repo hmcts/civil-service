55b917a2f5ce0f850c3e215ed7c99e53f661886d
Sampan Kumar <sampan.kumar@hmcts.net>
2022-11-10 23:16:21 +0000

    CIV-5174 - checking MULTI_CLAIM allocated track

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToDefenceCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToDefenceCallbackHandler.java
@@ -50,6 +50,7 @@ import static uk.gov.hmcts.reform.civil.callback.CallbackType.MID;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
 import static uk.gov.hmcts.reform.civil.callback.CallbackVersion.V_1;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CLAIMANT_RESPONSE;
+import static uk.gov.hmcts.reform.civil.enums.AllocatedTrack.getAllocatedTrack;
 import static uk.gov.hmcts.reform.civil.enums.MultiPartyScenario.ONE_V_ONE;
 import static uk.gov.hmcts.reform.civil.enums.MultiPartyScenario.ONE_V_TWO_ONE_LEGAL_REP;
 import static uk.gov.hmcts.reform.civil.enums.MultiPartyScenario.ONE_V_TWO_TWO_LEGAL_REP;
@@ -280,8 +281,11 @@ public class RespondToDefenceCallbackHandler extends CallbackHandler implements 
         builder.nextDeadline(null);
         AboutToStartOrSubmitCallbackResponse response = null;
 
+        AllocatedTrack allocatedTrack =
+            getAllocatedTrack(caseData.getClaimValue().toPounds(), caseData.getClaimType());
+
         if (v1 && featureToggleService.isSdoEnabled()
-            && !AllocatedTrack.MULTI_CLAIM.equals(caseData.getAllocatedTrack())) {
+            && !AllocatedTrack.MULTI_CLAIM.equals(allocatedTrack)) {
             if (caseData.getRespondent1ClaimResponseType().equals(RespondentResponseType.FULL_DEFENCE)) {
                 if ((multiPartyScenario.equals(ONE_V_ONE) || multiPartyScenario.equals(TWO_V_ONE))
                     || multiPartyScenario.equals(ONE_V_TWO_ONE_LEGAL_REP)) {
