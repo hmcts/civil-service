9b0d87c57c3b5e2a4d2ac22060779d21ec5a2c33
Azam <muhammad.ghulam@hmcts.net>
2022-11-15 23:08:23 +0000

    cosintegration: Modified CCD pull asset way

--- bin/pull-latest-ccd-files.sh
+++ bin/pull-latest-ccd-files.sh
@@ -13,4 +13,8 @@ cd ..
 #Copy ccd to civil-ccd-def
 cp -r ./civil-ccd-definition/ccd-definition .
 cp -r ./civil-ccd-definition/e2e .
+cp -r ./civil-ccd-definition/package.json .
+cp -r ./civil-ccd-definition/yarn.lock .
+cp -r ./civil-ccd-definition/codecept.conf.js .
+echo *
 rm -rf ./civil-ccd-definition
