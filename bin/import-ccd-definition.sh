914387a069d5d717fb58cd22b3e9d96511c984ab
Sampan Kumar <sampan.kumar@hmcts.net>
2022-11-15 14:52:56 +0000

    taking latest of ccd

--- bin/import-ccd-definition.sh
+++ bin/import-ccd-definition.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 
-params="$1"
-branchName="$2"
-
-git clone https://github.com/hmcts/civil-ccd-definition.git
-cd civil-ccd-definition
-
-echo "Switch to ${branchName} branch on civil-ccd-definition"
-git checkout ${branchName}
-
 definition_input_dir=$(realpath 'ccd-definition')
 definition_output_file="$(realpath ".")/build/ccd-development-config/ccd-civil-dev.xlsx"
+params="$@"
 
 ./bin/utils/import-ccd-definition.sh "${definition_input_dir}" "${definition_output_file}" "${params}"
