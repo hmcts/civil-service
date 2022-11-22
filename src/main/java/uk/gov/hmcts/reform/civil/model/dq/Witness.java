cd714be5528f2fac440f0268cc5280c902a97541
Sherlyn Khaw <sherlyn.khaw@evoco.co.uk>
2022-11-22 12:40:14 +0000

    feat/civ-5557 damages dq docmosis changes for hearing and listing uplift

--- src/main/java/uk/gov/hmcts/reform/civil/model/dq/Witness.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/dq/Witness.java
@@ -7,6 +7,12 @@ import lombok.Data;
 @Builder
 public class Witness {
 
+    // CIV-5557 to be removed
+    @Deprecated
     private final String name;
+    private final String firstName;
+    private final String lastName;
+    private final String emailAddress;
+    private final String phoneNumber;
     private final String reasonForWitness;
 }
