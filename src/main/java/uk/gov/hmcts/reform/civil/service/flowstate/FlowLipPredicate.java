ea7dea94ecc7d213c8e268cff38aaa51548be31c
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-11-23 09:21:32 +0000

    CIV-5336 private constructor without lombok

--- src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/FlowLipPredicate.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/FlowLipPredicate.java
@@ -1,13 +1,14 @@
 package uk.gov.hmcts.reform.civil.service.flowstate;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 
 import java.util.function.Predicate;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FlowLipPredicate {
+
+    private FlowLipPredicate(){
+
+    }
 
     public static final Predicate<CaseData> isLipCase = caseData ->
         caseData.isApplicantNotRepresented();
