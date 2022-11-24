ae46e0831e798d2a999d61c2a152f5dbb602caf6
Sampan Kumar <sampan.kumar@hmcts.net>
2022-11-24 16:25:38 +0000

    Pointing PR to a particular ccd branch

--- bin/import-ccd-definition.sh
+++ bin/import-ccd-definition.sh
@@ -3,5 +3,14 @@
 definition_input_dir=$(realpath 'ccd-definition')
 definition_output_file="$(realpath ".")/build/ccd-development-config/ccd-civil-dev.xlsx"
 params="$@"
+params="$1"
+branchName="$2"
+
+git clone https://github.com/hmcts/civil-ccd-definition.git
+cd civil-ccd-definition
+
+echo "Switch to ${branchName} branch on civil-ccd-definition"
+git checkout ${branchName}
+
 
 ./bin/utils/import-ccd-definition.sh "${definition_input_dir}" "${definition_output_file}" "${params}"
