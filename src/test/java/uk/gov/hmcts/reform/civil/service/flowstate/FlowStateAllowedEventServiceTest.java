c42f5fb09ff85da6b9ecef58d89cc5250fa06f69
bhagyashreesharma90 <114094319+bhagyashreesharma90@users.noreply.github.com>
2022-11-25 13:23:23 +0000

    Feat/civ 2691 civ 2898 evidence upload (#1851)
    * CIV-2691 CIV-2898
    
    * elastic search changes for evidence upload
    
    * removed camunda changes for notification
    
    * remove extra file
    
    * addressed review comments
    
    Co-authored-by: Filippo Possenti <filippo.possenti@version1.com>

--- src/test/java/uk/gov/hmcts/reform/civil/service/flowstate/FlowStateAllowedEventServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/flowstate/FlowStateAllowedEventServiceTest.java
@@ -53,7 +53,6 @@ import static uk.gov.hmcts.reform.civil.callback.CaseEvent.EVIDENCE_UPLOAD_APPLI
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.EVIDENCE_UPLOAD_JUDGE;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.EVIDENCE_UPLOAD_RESPONDENT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.HEARING_FEE_UNPAID;
-import static uk.gov.hmcts.reform.civil.callback.CaseEvent.EVIDENCE_UPLOAD_NOTIFICATION;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.HEARING_SCHEDULED;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.INFORM_AGREED_EXTENSION_DATE;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.INITIATE_GENERAL_APPLICATION;
@@ -226,8 +225,7 @@ class FlowStateAllowedEventServiceTest {
                         EVIDENCE_UPLOAD_JUDGE,
                         EVIDENCE_UPLOAD_APPLICANT,
                         migrateCase,
-                        EVIDENCE_UPLOAD_RESPONDENT,
-                        EVIDENCE_UPLOAD_NOTIFICATION
+                        EVIDENCE_UPLOAD_RESPONDENT
                     }
                 ),
                 of(
@@ -252,8 +250,7 @@ class FlowStateAllowedEventServiceTest {
                         EVIDENCE_UPLOAD_JUDGE,
                         EVIDENCE_UPLOAD_APPLICANT,
                         migrateCase,
-                        EVIDENCE_UPLOAD_RESPONDENT,
-                        EVIDENCE_UPLOAD_NOTIFICATION
+                        EVIDENCE_UPLOAD_RESPONDENT
                     }
                 ),
                 of(
@@ -283,8 +280,7 @@ class FlowStateAllowedEventServiceTest {
                         HEARING_SCHEDULED,
                         EVIDENCE_UPLOAD_APPLICANT,
                         migrateCase,
-                        EVIDENCE_UPLOAD_RESPONDENT,
-                        EVIDENCE_UPLOAD_NOTIFICATION
+                        EVIDENCE_UPLOAD_RESPONDENT
                     }
                 ),
                 of(
