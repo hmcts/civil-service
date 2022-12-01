f3f8186fe8bb2e128235c8070e2cdbd0967e43b3
Sampan Kumar <sampan.kumar@hmcts.net>
2022-12-01 22:22:31 +0000

    Making civil-service branch to talk to publishAfterJudicialReferalState ccd branch

--- bin/import-ccd-definition.sh
+++ bin/import-ccd-definition.sh
@@ -3,6 +3,7 @@
 definition_input_dir=$(realpath 'ccd-definition')
 definition_output_file="$(realpath ".")/build/ccd-development-config/ccd-civil-dev.xlsx"
 params="$@"
+
 params="$1"
 branchName="$2"
 
@@ -12,5 +13,8 @@ cd civil-ccd-definition
 echo "Switch to ${branchName} branch on civil-ccd-definition"
 git checkout ${branchName}
 
+
+definition_input_dir=$(realpath './ccd-definition')
+definition_output_file="$(realpath ".")/ccd-definition/build/ccd-development-config/ccd-civil-dev.xlsx"
 
 ./bin/utils/import-ccd-definition.sh "${definition_input_dir}" "${definition_output_file}" "${params}"
