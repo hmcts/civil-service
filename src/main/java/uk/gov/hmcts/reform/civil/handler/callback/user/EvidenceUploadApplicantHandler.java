9b8bdafdd92f658419b18ae6497234256d212990
Mark Drummond <mark.drummond@version1.com>
2022-12-02 17:04:09 +0000

    evidence upload multiparty

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/EvidenceUploadApplicantHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/EvidenceUploadApplicantHandler.java
@@ -8,20 +8,23 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.service.CoreCaseUserService;
 import uk.gov.hmcts.reform.civil.service.Time;
+import uk.gov.hmcts.reform.civil.service.UserService;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
 
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.EVIDENCE_UPLOAD_APPLICANT;
 
 @Service
 public class EvidenceUploadApplicantHandler extends EvidenceUploadHandlerBase {
 
-    public EvidenceUploadApplicantHandler(ObjectMapper objectMapper, Time time) {
-        super(objectMapper, time, Collections.singletonList(EVIDENCE_UPLOAD_APPLICANT), "validateValuesApplicant");
+    public EvidenceUploadApplicantHandler(UserService userService, CoreCaseUserService coreCaseUserService, ObjectMapper objectMapper, Time time) {
+        super(userService, coreCaseUserService, objectMapper, time, Collections.singletonList(EVIDENCE_UPLOAD_APPLICANT), "validateValuesApplicant");
     }
 
     @Override
-    CallbackResponse caseType(CaseData caseData) {
-        return caseTypeDetermine(caseData);
+    CallbackResponse caseType(CaseData caseData, CallbackParams callbackParams) {
+        return caseTypeDetermine(caseData, callbackParams);
     }
 
     @Override
--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/EvidenceUploadRespondentHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/EvidenceUploadRespondentHandler.java
@@ -6,16 +6,20 @@ import java.util.Collections;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.service.CoreCaseUserService;
 import uk.gov.hmcts.reform.civil.service.Time;
+import uk.gov.hmcts.reform.civil.service.UserService;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
 
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.EVIDENCE_UPLOAD_RESPONDENT;
 
 @Service
 public class EvidenceUploadRespondentHandler extends EvidenceUploadHandlerBase {
 
-    public EvidenceUploadRespondentHandler(ObjectMapper objectMapper, Time time) {
-        super(objectMapper, time, Collections.singletonList(EVIDENCE_UPLOAD_RESPONDENT), "validateValuesRespondent");
+    public EvidenceUploadRespondentHandler(UserService userService, CoreCaseUserService coreCaseUserService, ObjectMapper objectMapper, Time time) {
+        super(userService, coreCaseUserService, objectMapper, time, Collections.singletonList(EVIDENCE_UPLOAD_RESPONDENT), "validateValuesRespondent");
     }
 
     @Override
@@ -30,7 +34,7 @@ public class EvidenceUploadRespondentHandler extends EvidenceUploadHandlerBase {
     }
 
     @Override
-    CallbackResponse caseType(CaseData caseData) {
+    CallbackResponse caseType(CaseData caseData, CallbackParams callbackParams) {
         return null;
     }
 
