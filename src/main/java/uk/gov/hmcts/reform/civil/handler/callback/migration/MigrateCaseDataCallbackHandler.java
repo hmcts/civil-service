4f6eabd5f36194eccb00288db81ad0e87cf20cfa
skhajuria <sankhajuria@gmail.com>
2022-12-14 12:04:20 +0000

    Added more logs for migration utility

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/migration/MigrateCaseDataCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/migration/MigrateCaseDataCallbackHandler.java
@@ -46,40 +46,45 @@ public class MigrateCaseDataCallbackHandler extends CallbackHandler {
             .build();
     }
 
-    private CallbackResponse migrateCaseData(CallbackParams callbackParams) {
+    private CallbackResponse migrateCaseData(CallbackParams callbackParams)  {
         CaseData oldCaseData = callbackParams.getCaseData();
         log.info("Migrating data for case: {}", oldCaseData.getCcdCaseReference());
         CaseData.CaseDataBuilder<?, ?> caseDataBuilder = oldCaseData.toBuilder();
-        String authToken = callbackParams.getParams().get(BEARER_TOKEN).toString();
-        log.info("After getting auth token");
-        if (CaseCategory.SPEC_CLAIM.equals(oldCaseData.getCaseAccessCategory())) {
-            log.info("Process SPEC claim");
+        try {
+            String authToken = callbackParams.getParams().get(BEARER_TOKEN).toString();
+            log.info("After getting auth token");
+            if (CaseCategory.SPEC_CLAIM.equals(oldCaseData.getCaseAccessCategory())) {
+                log.info("Process SPEC claim");
 
-            caseMigrationUtility.migrateGS(oldCaseData, caseDataBuilder
-            );
+                caseMigrationUtility.migrateGS(oldCaseData, caseDataBuilder
+                );
 
-            caseMigrationUtility.migrateCaseManagementLocation(
+                caseMigrationUtility.migrateCaseManagementLocation(
+                    caseDataBuilder,
+                    CaseLocation.builder().baseLocation("420219").region("2").build()
+                );
+            } else {
+                log.info("Process UNSPEC claim");
+                caseMigrationUtility.migrateCaseManagementLocation(
+                    caseDataBuilder,
+                    CaseLocation.builder().baseLocation("192280").region("4").build()
+                );
+                caseMigrationUtility.migrateGS(oldCaseData, caseDataBuilder);
+                caseMigrationUtility.migrateUnspecCourtLocation(authToken, oldCaseData, caseDataBuilder);
+            }
+            log.info("Start DQ migration");
+            caseMigrationUtility.migrateRespondentAndApplicantDQUnSpec(
+                authToken,
+                oldCaseData,
                 caseDataBuilder,
                 CaseLocation.builder().baseLocation("420219").region("2").build()
             );
-        } else {
-            log.info("Process UNSPEC claim");
-            caseMigrationUtility.migrateCaseManagementLocation(
-                caseDataBuilder,
-                CaseLocation.builder().baseLocation("192280").region("4").build()
-            );
-            caseMigrationUtility.migrateGS(oldCaseData, caseDataBuilder);
-            caseMigrationUtility.migrateUnspecCourtLocation(authToken, oldCaseData, caseDataBuilder);
+            log.info("Add migration ID");
+            caseDataBuilder.migrationId(MIGRATION_ID_VALUE);
+        }catch (Exception exception) {
+            log.error("Exception during migration about to submit event- " + exception.getMessage(), exception);
+            throw exception;
         }
-        log.info("Start DQ migration");
-        caseMigrationUtility.migrateRespondentAndApplicantDQUnSpec(
-            authToken,
-            oldCaseData,
-            caseDataBuilder,
-            CaseLocation.builder().baseLocation("420219").region("2").build()
-        );
-        log.info("Add migration ID");
-        caseDataBuilder.migrationId(MIGRATION_ID_VALUE);
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(caseDataBuilder.build().toMap(objectMapper))
             .build();
