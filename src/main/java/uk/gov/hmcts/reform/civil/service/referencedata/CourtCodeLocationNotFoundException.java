1e01daff283b7342bbe71a903bf5fc23f2799c0c
skhajuria <sankhajuria@gmail.com>
2022-12-16 09:30:39 +0000

    Filter LocationRefdata based on court location

---
+++ src/main/java/uk/gov/hmcts/reform/civil/service/referencedata/CourtCodeLocationNotFoundException.java
@@ -0,0 +1,9 @@
+package uk.gov.hmcts.reform.civil.service.referencedata;
+
+public class CourtCodeLocationNotFoundException extends RuntimeException {
+
+    public CourtCodeLocationNotFoundException(Exception cause) {
+        super(cause);
+    }
+
+}
