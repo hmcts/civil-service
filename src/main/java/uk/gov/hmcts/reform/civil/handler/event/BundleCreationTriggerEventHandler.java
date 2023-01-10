3cc4c9ed1b57ca9b38bc9dfa515619a157b001bd
suvarnathekkoot <95478835+suvarnathekkoot@users.noreply.github.com>
2023-01-10 09:59:06 +0000

    CIV-2293 (#1918)
    * Bundle Creation
    
    * TaskListener
    
    * BundleTrigger

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandler.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.civil.handler.event;
+
+    import lombok.RequiredArgsConstructor;
+    import lombok.extern.slf4j.Slf4j;
+    import org.springframework.context.event.EventListener;
+    import org.springframework.stereotype.Service;
+    import uk.gov.hmcts.reform.civil.event.BundleCreationTriggerEvent;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BundleCreationTriggerEventHandler {
+
+     /**
+     * This method will send notification to applicant and respondent solicitors.
+     * This method will not throw any exception but log warning if there is any error while
+     * sending notification because these email notifications are not business critical
+     * and provided as a courtesy, as the user can log in and see the new uploads.
+     *
+     * @param event EvidenceUploadNotificationEvent
+     */
+    @EventListener
+    public void sendBundleCreationTrigger(BundleCreationTriggerEvent event) {
+
+    }
+}
