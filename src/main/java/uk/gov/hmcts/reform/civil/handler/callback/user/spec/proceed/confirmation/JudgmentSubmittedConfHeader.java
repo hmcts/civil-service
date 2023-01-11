3cb9a800339fc8165c472e6afaf94adf9e9f824c
neeta-hmcts <neeta.shinde@hmcts.net>
2023-01-11 16:11:38 +0000

    fixed style check error.

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/proceed/confirmation/JudgmentSubmittedConfHeader.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/proceed/confirmation/JudgmentSubmittedConfHeader.java
@@ -12,7 +12,6 @@ import java.util.Set;
 
 import static java.lang.String.format;
 
-
 @Component
 public class JudgmentSubmittedConfHeader  implements RespondToResponseConfirmationHeaderGenerator {
 
