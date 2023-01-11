ca125704a5102b8116c76d000484ebff620b58a1
neeta-hmcts <neeta.shinde@hmcts.net>
2023-01-11 16:01:15 +0000

    Legal Rep ITP - CCJ to Formalise payment CYA and Confirmation Screen.

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/RespondToResponseConfirmationHeaderGeneratorTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/RespondToResponseConfirmationHeaderGeneratorTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.civil.handler.callback.user.spec;
 
 import org.apache.commons.lang3.tuple.Pair;
+import uk.gov.hmcts.reform.civil.enums.RespondentResponsePartAdmissionPaymentTimeLRspec;
 import uk.gov.hmcts.reform.civil.enums.RespondentResponseTypeSpec;
 import uk.gov.hmcts.reform.civil.enums.SuperClaimType;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
@@ -8,6 +9,7 @@ import uk.gov.hmcts.reform.civil.handler.callback.user.spec.proceed.confirmation
 import uk.gov.hmcts.reform.civil.handler.callback.user.spec.proceed.confirmation.AdmitProceedConfHeader;
 import uk.gov.hmcts.reform.civil.handler.callback.user.spec.proceed.confirmation.DefendNotProceedConfHeader;
 import uk.gov.hmcts.reform.civil.handler.callback.user.spec.proceed.confirmation.DefendProceedConfHeader;
+import uk.gov.hmcts.reform.civil.handler.callback.user.spec.proceed.confirmation.JudgmentSubmittedConfHeader;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 
 import java.util.List;
@@ -30,7 +32,8 @@ public class RespondToResponseConfirmationHeaderGeneratorTest implements CaseDat
             Pair.of(buildPartAdmitProceedCaseData(), AdmitProceedConfHeader.class),
             Pair.of(buildPartAdmitNotProceedCaseData(), AdmitNotProceedConfHeader.class),
             Pair.of(buildFullDefenceProceedCaseData(), DefendProceedConfHeader.class),
-            Pair.of(buildFullDefenceNotProceedCaseData(), DefendNotProceedConfHeader.class)
+            Pair.of(buildFullDefenceNotProceedCaseData(), DefendNotProceedConfHeader.class),
+            Pair.of(buildJudgmentSubmitProceedCaseData(), JudgmentSubmittedConfHeader.class)
         );
     }
 
@@ -40,6 +43,7 @@ public class RespondToResponseConfirmationHeaderGeneratorTest implements CaseDat
             .legacyCaseReference("claimNumber")
             .applicant1ProceedWithClaim(YesOrNo.YES)
             .respondent1ClaimResponseTypeForSpec(RespondentResponseTypeSpec.FULL_DEFENCE)
+            .applicant1AcceptFullAdmitPaymentPlanSpec(YesOrNo.NO)
             .build();
     }
 
@@ -49,6 +53,7 @@ public class RespondToResponseConfirmationHeaderGeneratorTest implements CaseDat
             .legacyCaseReference("claimNumber")
             .applicant1ProceedWithClaim(YesOrNo.NO)
             .respondent1ClaimResponseTypeForSpec(RespondentResponseTypeSpec.FULL_DEFENCE)
+            .applicant1AcceptFullAdmitPaymentPlanSpec(YesOrNo.NO)
             .build();
     }
 
@@ -58,6 +63,7 @@ public class RespondToResponseConfirmationHeaderGeneratorTest implements CaseDat
             .legacyCaseReference("claimNumber")
             .applicant1ProceedWithClaim(YesOrNo.YES)
             .respondent1ClaimResponseTypeForSpec(RespondentResponseTypeSpec.FULL_ADMISSION)
+            .applicant1AcceptFullAdmitPaymentPlanSpec(YesOrNo.NO)
             .build();
     }
 
@@ -67,6 +73,7 @@ public class RespondToResponseConfirmationHeaderGeneratorTest implements CaseDat
             .legacyCaseReference("claimNumber")
             .applicant1ProceedWithClaim(YesOrNo.NO)
             .respondent1ClaimResponseTypeForSpec(RespondentResponseTypeSpec.FULL_ADMISSION)
+            .applicant1AcceptFullAdmitPaymentPlanSpec(YesOrNo.NO)
             .build();
     }
 
@@ -76,6 +83,7 @@ public class RespondToResponseConfirmationHeaderGeneratorTest implements CaseDat
             .legacyCaseReference("claimNumber")
             .applicant1ProceedWithClaim(YesOrNo.YES)
             .respondent1ClaimResponseTypeForSpec(RespondentResponseTypeSpec.PART_ADMISSION)
+            .applicant1AcceptPartAdmitPaymentPlanSpec(YesOrNo.NO)
             .build();
     }
 
@@ -85,6 +93,18 @@ public class RespondToResponseConfirmationHeaderGeneratorTest implements CaseDat
             .legacyCaseReference("claimNumber")
             .applicant1ProceedWithClaim(YesOrNo.NO)
             .respondent1ClaimResponseTypeForSpec(RespondentResponseTypeSpec.PART_ADMISSION)
+            .applicant1AcceptPartAdmitPaymentPlanSpec(YesOrNo.NO)
+            .build();
+    }
+
+    public static CaseData buildJudgmentSubmitProceedCaseData(){
+        return CaseData.builder()
+            .superClaimType(SuperClaimType.SPEC_CLAIM)
+            .legacyCaseReference("claimNumber")
+            .applicant1ProceedWithClaim(null)
+            .respondent1ClaimResponseTypeForSpec(RespondentResponseTypeSpec.FULL_ADMISSION)
+            .applicant1AcceptFullAdmitPaymentPlanSpec(YesOrNo.YES)
+            .defenceAdmitPartPaymentTimeRouteRequired(RespondentResponsePartAdmissionPaymentTimeLRspec.SUGGESTION_OF_REPAYMENT_PLAN)
             .build();
     }
 }
