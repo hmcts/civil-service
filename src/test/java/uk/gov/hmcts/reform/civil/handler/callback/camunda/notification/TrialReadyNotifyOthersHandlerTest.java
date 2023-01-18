b020f52baaaacd1213a1596b723bbcae624e3c2a
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-01-18 09:52:21 +0000

    Tests ready

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/TrialReadyNotifyOthersHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/TrialReadyNotifyOthersHandlerTest.java
@@ -19,12 +19,18 @@ import uk.gov.hmcts.reform.civil.service.NotificationService;
 
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_APPLICANT_SOLICITOR_FOR_OTHER_TRIAL_READY;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_RESPONDENT_SOLICITOR1_FOR_OTHER_TRIAL_READY;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_RESPONDENT_SOLICITOR2_FOR_OTHER_TRIAL_READY;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIM_REFERENCE_NUMBER;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.HEARING_DATE;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.TrialReadyNotifyOthersHandler.TASK_ID_APPLICANT;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.TrialReadyNotifyOthersHandler.TASK_ID_RESPONDENT_ONE;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.TrialReadyNotifyOthersHandler.TASK_ID_RESPONDENT_TWO;
 import static uk.gov.hmcts.reform.civil.helpers.DateFormatHelper.DATE;
 import static uk.gov.hmcts.reform.civil.helpers.DateFormatHelper.formatLocalDate;
 
@@ -58,11 +64,63 @@ public class TrialReadyNotifyOthersHandlerTest extends BaseCallbackHandlerTest {
             handler.handle(params);
 
             verify(notificationService).sendMail(
+                "hmcts.civil@gmail.com",
+                "template-id",
+                getNotificationDataMap(caseData),
+                "other-party-trial-ready-notification-000DC001"
+            );
+        }
+
+        @Test
+        void shouldNotifyRespondentSolicitor1_whenInvoked() {
+            CaseData caseData = CaseDataBuilder.builder().atStateTrialReadyCheck().build();
+            CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_SUBMIT, caseData).request(
+                CallbackRequest.builder().eventId(NOTIFY_RESPONDENT_SOLICITOR1_FOR_OTHER_TRIAL_READY.name()).build()
+            ).build();
+
+            handler.handle(params);
+
+            verify(notificationService).sendMail(
                 "respondentsolicitor@example.com",
                 "template-id",
                 getNotificationDataMap(caseData),
                 "other-party-trial-ready-notification-000DC001"
             );
+        }
+
+        @Test
+        void shouldNotifyRespondentSolicitor2_whenInvoked() {
+            CaseData caseData = CaseDataBuilder.builder().atStateTrialReadyCheck().build();
+            CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_SUBMIT, caseData).request(
+                CallbackRequest.builder().eventId(NOTIFY_RESPONDENT_SOLICITOR2_FOR_OTHER_TRIAL_READY.name()).build()
+            ).build();
+
+            handler.handle(params);
+
+            verify(notificationService).sendMail(
+                "respondentsolicitor2@example.com",
+                "template-id",
+                getNotificationDataMap(caseData),
+                "other-party-trial-ready-notification-000DC001"
+            );
+        }
+
+        @Test
+        void shouldReturnCorrectCamundaActivityId_whenInvoked() {
+            assertThat(handler.camundaActivityId(CallbackParamsBuilder.builder().request(
+                CallbackRequest.builder().eventId(
+                    NOTIFY_APPLICANT_SOLICITOR_FOR_OTHER_TRIAL_READY.name()).build()).build()))
+                .isEqualTo(TASK_ID_APPLICANT);
+
+            assertThat(handler.camundaActivityId(CallbackParamsBuilder.builder().request(
+                CallbackRequest.builder().eventId(
+                    NOTIFY_RESPONDENT_SOLICITOR1_FOR_OTHER_TRIAL_READY.name()).build()).build()))
+                .isEqualTo(TASK_ID_RESPONDENT_ONE);
+
+            assertThat(handler.camundaActivityId(CallbackParamsBuilder.builder().request(
+                CallbackRequest.builder().eventId(
+                    NOTIFY_RESPONDENT_SOLICITOR2_FOR_OTHER_TRIAL_READY.name()).build()).build()))
+                .isEqualTo(TASK_ID_RESPONDENT_TWO);
         }
 
         @NotNull
