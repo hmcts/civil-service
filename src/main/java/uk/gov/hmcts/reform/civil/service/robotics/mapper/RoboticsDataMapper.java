1523015a8cf3bb15e44b6aa7cdde3ca407c70845
jeejavr <jeejamol.vijayarajan@HMCTS.net>
2023-01-18 16:07:41 +0000

    CIV-6144: resolve conflict merge master

--- src/main/java/uk/gov/hmcts/reform/civil/service/robotics/mapper/RoboticsDataMapper.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/robotics/mapper/RoboticsDataMapper.java
@@ -20,6 +20,7 @@ import uk.gov.hmcts.reform.civil.model.robotics.RoboticsAddresses;
 import uk.gov.hmcts.reform.civil.model.robotics.RoboticsCaseData;
 import uk.gov.hmcts.reform.civil.model.robotics.Solicitor;
 import uk.gov.hmcts.reform.civil.service.OrganisationService;
+import uk.gov.hmcts.reform.civil.service.robotics.utils.RoboticsDataUtil;
 import uk.gov.hmcts.reform.civil.service.referencedata.LocationRefDataService;
 import uk.gov.hmcts.reform.civil.utils.OrgPolicyUtils;
 import uk.gov.hmcts.reform.civil.utils.PartyUtils;
@@ -37,23 +38,23 @@ import static io.jsonwebtoken.lang.Collections.isEmpty;
 import static java.time.format.DateTimeFormatter.ISO_DATE;
 import static java.util.Objects.requireNonNull;
 import static java.util.Optional.ofNullable;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.PROCEEDS_IN_HERITAGE_SYSTEM;
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.YES;
+import static uk.gov.hmcts.reform.civil.service.robotics.utils.RoboticsDataUtil.APPLICANT_ID;
+import static uk.gov.hmcts.reform.civil.service.robotics.utils.RoboticsDataUtil.APPLICANT2_ID;
+import static uk.gov.hmcts.reform.civil.service.robotics.utils.RoboticsDataUtil.APPLICANT_SOLICITOR_ID;
+import static uk.gov.hmcts.reform.civil.service.robotics.utils.RoboticsDataUtil.CIVIL_COURT_TYPE_ID;
+import static uk.gov.hmcts.reform.civil.service.robotics.utils.RoboticsDataUtil.RESPONDENT_ID;
+import static uk.gov.hmcts.reform.civil.service.robotics.utils.RoboticsDataUtil.RESPONDENT2_ID;
+import static uk.gov.hmcts.reform.civil.service.robotics.utils.RoboticsDataUtil.RESPONDENT_SOLICITOR_ID;
+import static uk.gov.hmcts.reform.civil.service.robotics.utils.RoboticsDataUtil.RESPONDENT2_SOLICITOR_ID;
 import static uk.gov.hmcts.reform.civil.utils.MonetaryConversions.penniesToPounds;
 
 @Service
 @RequiredArgsConstructor
 public class RoboticsDataMapper {
 
-    public static final String APPLICANT_SOLICITOR_ID = "001";
-    public static final String RESPONDENT_SOLICITOR_ID = "002";
-    public static final String APPLICANT_ID = "001";
-    public static final String RESPONDENT_ID = "002";
-
-    public static final String RESPONDENT2_ID = "003";
-    public static final String APPLICANT2_ID = "004";
-    public static final String RESPONDENT2_SOLICITOR_ID = "003";
-    public static final String CIVIL_COURT_TYPE_ID = "10";
     private final RoboticsAddressMapper addressMapper;
     private final EventHistoryMapper eventHistoryMapper;
     private final OrganisationService organisationService;
@@ -62,13 +63,19 @@ public class RoboticsDataMapper {
 
     public RoboticsCaseData toRoboticsCaseData(CaseData caseData, String authToken) {
         requireNonNull(caseData);
-        return RoboticsCaseData.builder()
+        var roboticsBuilder = RoboticsCaseData.builder()
             .header(buildCaseHeader(caseData, authToken))
             .litigiousParties(buildLitigiousParties(caseData))
             .solicitors(buildSolicitors(caseData))
             .claimDetails(buildClaimDetails(caseData))
-            .events(eventHistoryMapper.buildEvents(caseData))
-            .build();
+            .events(eventHistoryMapper.buildEvents(caseData));
+
+        if (featureToggleService.isNoticeOfChangeEnabled()
+            && caseData.getCcdState() == PROCEEDS_IN_HERITAGE_SYSTEM) {
+            roboticsBuilder.noticeOfChange(RoboticsDataUtil.buildNoticeOfChange(caseData));
+        }
+
+        return roboticsBuilder.build();
     }
 
     private ClaimDetails buildClaimDetails(CaseData caseData) {
