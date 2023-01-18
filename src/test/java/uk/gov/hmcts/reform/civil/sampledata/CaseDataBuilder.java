20e4910f8866b86f293718925cb7db606a614b9c
jeejavr <jeejamol.vijayarajan@HMCTS.net>
2023-01-18 16:06:10 +0000

    CIV-6144: resolve conflict merge master

--- src/test/java/uk/gov/hmcts/reform/civil/sampledata/CaseDataBuilder.java
+++ src/test/java/uk/gov/hmcts/reform/civil/sampledata/CaseDataBuilder.java
@@ -4102,7 +4102,6 @@ public class CaseDataBuilder {
         cosUIStatement.add("CERTIFIED");
         if (setCos1) {
             CertificateOfService.CertificateOfServiceBuilder cos1Builder = CertificateOfService.builder()
-                .cosUISenderStatementOfTruthLabel(cosUIStatement)
                 .cosDateOfServiceForDefendant(cos1Date);
             if (file1) {
                 cos1Builder.cosEvidenceDocument(files);
@@ -4111,7 +4110,6 @@ public class CaseDataBuilder {
         }
         if (setCos2) {
             CertificateOfService.CertificateOfServiceBuilder cos2Builder = CertificateOfService.builder()
-                .cosUISenderStatementOfTruthLabel(cosUIStatement)
                 .cosDateOfServiceForDefendant(cos2Date);
             if (file2) {
                 cos2Builder.cosEvidenceDocument(files2);
