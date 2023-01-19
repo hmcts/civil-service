fc09f3fabdf7ad51874fd04b63bd564d7d487cca
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2023-01-19 17:42:20 +0000

    CIV-5337: Update Test

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimContinuingOnlineApplicantPartyForSpecNotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimContinuingOnlineApplicantPartyForSpecNotificationHandlerTest.java
@@ -3,11 +3,12 @@ package uk.gov.hmcts.reform.civil.handler.callback.camunda.notification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
@@ -43,7 +44,7 @@ import static uk.gov.hmcts.reform.civil.helpers.DateFormatHelper.DATE;
 import static uk.gov.hmcts.reform.civil.helpers.DateFormatHelper.formatLocalDate;
 import static uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder.LEGACY_CASE_REFERENCE;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {
     ClaimContinuingOnlineApplicantPartyForSpecNotificationHandler.class,
     JacksonAutoConfiguration.class,
@@ -66,7 +67,7 @@ public class ClaimContinuingOnlineApplicantPartyForSpecNotificationHandlerTest e
 
     public static final String TASK_ID_Applicant1 = "CreateClaimContinuingOnlineNotifyApplicant1ForSpec";
 
-    @org.junit.Test
+    @Test
     public void ldBlock() {
         when(toggleService.isLrSpecEnabled()).thenReturn(false, true);
         assertTrue(handler.handledEvents().isEmpty());
