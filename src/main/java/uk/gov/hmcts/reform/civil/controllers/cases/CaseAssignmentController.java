71c8e6c21af67c20d617099d6310525c398039ff
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2023-01-24 12:34:40 +0000

    CIV-6434 assign case to defendant

--- src/main/java/uk/gov/hmcts/reform/civil/controllers/cases/CaseAssignmentController.java
+++ src/main/java/uk/gov/hmcts/reform/civil/controllers/cases/CaseAssignmentController.java
@@ -6,21 +6,27 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.civil.enums.CaseRole;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.citizenui.dto.PinDto;
+import uk.gov.hmcts.reform.civil.service.AssignCaseService;
 import uk.gov.hmcts.reform.civil.service.docmosis.pip.PiPLetterGenerator;
 import uk.gov.hmcts.reform.civil.service.pininpost.DefendantPinToPostLRspecService;
 import uk.gov.hmcts.reform.civil.service.search.CaseLegacyReferenceSearchService;
+
+import java.util.Optional;
 
 @Api
 @Slf4j
@@ -35,6 +41,7 @@ public class CaseAssignmentController {
     private final CaseLegacyReferenceSearchService caseByLegacyReferenceSearchService;
     private final DefendantPinToPostLRspecService defendantPinToPostLRspecService;
     private final PiPLetterGenerator piPLetterGenerator;
+    private final AssignCaseService assignCaseService;
 
     @PostMapping(path = {
         "/reference/{caseReference}"
@@ -50,6 +57,22 @@ public class CaseAssignmentController {
         CaseDetails caseDetails = caseByLegacyReferenceSearchService.getCaseDataByLegacyReference(caseReference);
         defendantPinToPostLRspecService.validatePin(caseDetails, pin.getPin());
         return new ResponseEntity<>(caseDetails, HttpStatus.OK);
+    }
+
+    @PostMapping(path = {
+        "/case/{caseId}/{caseRole}"
+    })
+    @ApiOperation("Assigns case to defendant")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK"),
+        @ApiResponse(code = 401, message = "Not Authorized"),
+        @ApiResponse(code = 400, message = "Bad Request")})
+    public ResponseEntity<String> assignCaseToDefendant(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
+                                      @PathVariable("caseId") String caseId,
+                                      @PathVariable("caseRole") Optional<CaseRole> caseRole){
+        log.info("hit the assign case controller - case reference {}", caseId);
+        assignCaseService.assignCase(authorisation, caseId, caseRole);
+        return new ResponseEntity<>("ok", HttpStatus.OK);
     }
 
     @PostMapping(value = "/pipLetter", produces = MediaType.APPLICATION_PDF_VALUE)
