5f995d708afc7d3a3946aa6e2b6927c4b6439192
Nagaraju Deekonda <nagaraju.deekonda@version1.com>
2023-01-26 21:35:09 +0000

    updated from master

--- src/main/java/uk/gov/hmcts/reform/civil/model/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/CaseData.java
@@ -511,6 +511,7 @@ public class CaseData extends CaseDataParent implements MappableObject {
     private List<Element<TrialHearingAddNewDirectionsDJ>> trialHearingAddNewDirectionsDJ;
     private HearingMethodTelephoneHearingDJ disposalHearingMethodTelephoneHearingDJ;
     private HearingMethodVideoConferenceDJ disposalHearingMethodVideoConferenceHearingDJ;
+
     private String featureToggleWA;
 
     //Hearing Scheduled
