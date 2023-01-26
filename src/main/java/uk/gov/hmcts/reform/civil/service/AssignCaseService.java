0f2f62dc2a07e25a835a2f1c40ce403b528e1896
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2023-01-26 15:03:51 +0000

    CIV-6434 remove unused imports

--- src/main/java/uk/gov/hmcts/reform/civil/service/AssignCaseService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/AssignCaseService.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.civil.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.civil.controllers.testingsupport.CaseAssignmentSupportService;
 import uk.gov.hmcts.reform.civil.enums.CaseRole;
 import uk.gov.hmcts.reform.prd.model.Organisation;
 
