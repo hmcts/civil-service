d1ef590d82632a3e43baa62602117636ab47919e
Astha Malviya <astha.malviya@HMCTS.NET>
2023-01-27 15:42:06 +0000

    fixing null pointer exception

---
+++ src/main/java/uk/gov/hmcts/reform/civil/utils/LocationRefDataUtil.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.reform.civil.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.referencedata.response.LocationRefData;
+import uk.gov.hmcts.reform.civil.service.referencedata.LocationRefDataService;
+
+import java.util.List;
+
+import static uk.gov.hmcts.reform.civil.service.robotics.utils.RoboticsDataUtil.CIVIL_COURT_TYPE_ID;
+
+@Slf4j
+@Component
+public class LocationRefDataUtil {
+
+    private final LocationRefDataService locationRefDataService;
+
+    @Autowired
+    public LocationRefDataUtil(LocationRefDataService locationRefDataService) {
+        this.locationRefDataService = locationRefDataService;
+    }
+
+    public String getPreferredCourtCode(CaseData caseData, String authToken) {
+        if (caseData.getCourtLocation().getCaseLocation() == null) {
+            return caseData.getCourtLocation().getApplicantPreferredCourt();
+        } else {
+            List<LocationRefData> courtLocations = locationRefDataService.getCourtLocationsByEpimmsId(
+                authToken, caseData.getCourtLocation().getCaseLocation().getBaseLocation());
+            if (!courtLocations.isEmpty()) {
+                return courtLocations.stream()
+                    .filter(id -> id.getCourtTypeId().equals(CIVIL_COURT_TYPE_ID))
+                    .findFirst()
+                    .map(LocationRefData::getCourtLocationCode)
+                    .orElse("");
+            } else {
+                log.info("Court location not found");
+                return "";
+            }
+        }
+    }
+}
--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/robotics/NotifyRoboticsOnCaseHandedOfflineHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/robotics/NotifyRoboticsOnCaseHandedOfflineHandlerTest.java
@@ -31,6 +31,7 @@ import uk.gov.hmcts.reform.civil.service.robotics.mapper.EventHistorySequencer;
 import uk.gov.hmcts.reform.civil.service.robotics.mapper.RoboticsAddressMapper;
 import uk.gov.hmcts.reform.civil.service.robotics.mapper.RoboticsDataMapper;
 import uk.gov.hmcts.reform.civil.service.robotics.mapper.RoboticsDataMapperForSpec;
+import uk.gov.hmcts.reform.civil.utils.LocationRefDataUtil;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.prd.client.OrganisationApi;
 
@@ -56,7 +57,8 @@ import static uk.gov.hmcts.reform.civil.enums.MultiPartyScenario.isMultiPartySce
     JacksonAutoConfiguration.class,
     CaseDetailsConverter.class,
     StateFlowEngine.class,
-    OrganisationService.class
+    OrganisationService.class,
+    LocationRefDataUtil.class
 })
 @ExtendWith(SpringExtension.class)
 class NotifyRoboticsOnCaseHandedOfflineHandlerTest extends BaseCallbackHandlerTest {
@@ -72,8 +74,6 @@ class NotifyRoboticsOnCaseHandedOfflineHandlerTest extends BaseCallbackHandlerTe
     FeatureToggleService featureToggleService;
     @MockBean
     PrdAdminUserConfiguration userConfig;
-    @MockBean
-    LocationRefDataService locationRefDataService;
 
     @Nested
     class ValidJsonPayload {
@@ -118,6 +118,7 @@ class NotifyRoboticsOnCaseHandedOfflineHandlerTest extends BaseCallbackHandlerTe
 
         @Test
         void shouldThrowJsonSchemaValidationException_whenSchemaErrors() {
+            when(validationService.validate(anyString())).thenReturn(Set.of(new ValidationMessage.Builder().build()));
             when(validationService.validate(anyString())).thenReturn(Set.of(new ValidationMessage.Builder().build()));
             CaseData caseData = CaseDataBuilder.builder().atStateProceedsOfflineAdmissionOrCounterClaim().build();
             CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_SUBMIT, caseData).build();
