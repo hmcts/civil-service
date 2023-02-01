4b020ec9bf573f09137202bf181fb35e961d8522
Douglas Rice <douglas.rice@hmcts.net>
2023-02-01 18:17:54 +0000

    test

--- src/main/java/uk/gov/hmcts/reform/civil/launchdarkly/FeatureToggleService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/launchdarkly/FeatureToggleService.java
@@ -74,10 +74,6 @@ public class FeatureToggleService {
         return false;
     }
 
-    //public boolean isHearingAndListingSDOEnabled() {
-    //    return false;
-    //}
-
     public boolean isCourtLocationDynamicListEnabled() {
         return true;
     }
