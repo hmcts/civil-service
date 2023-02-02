e7e5eb7b833b6a51210f353a827989b9e02c3f29
jeejavr <jeejamol.vijayarajan@HMCTS.net>
2023-02-02 17:12:37 +0000

    CIV-6892: adding flag

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/sealedclaim/SealedClaimFormGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/sealedclaim/SealedClaimFormGenerator.java
@@ -68,7 +68,7 @@ public class SealedClaimFormGenerator implements TemplateDataGenerator<SealedCla
         MultiPartyScenario multiPartyScenario = getMultiPartyScenario(caseData);
         String hearingCourtLocation = locationRefDataUtil.getPreferredCourtData(
             caseData,
-            CallbackParams.Params.BEARER_TOKEN.toString(), false);
+            CallbackParams.Params.BEARER_TOKEN.toString(), true);
         SealedClaimForm.SealedClaimFormBuilder sealedClaimFormBuilder = SealedClaimForm.builder()
             .applicants(getApplicants(caseData, multiPartyScenario))
             .respondents(getRespondents(caseData, multiPartyScenario))
