f057351f01d1eade071a88438a3197a827135377
skhajuria <sankhajuria@gmail.com>
2023-02-02 14:42:30 +0000

    Remove migraiton related code

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/migration/MigrateCaseDataCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/migration/MigrateCaseDataCallbackHandlerTest.java
@@ -1,9 +1,17 @@
 package uk.gov.hmcts.reform.civil.handler.callback.migration;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.enums.CaseCategory;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.utils.CaseMigrationUtility;
+
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
 
 @SpringBootTest(classes = {
     MigrateCaseDataCallbackHandler.class,
@@ -11,5 +19,33 @@ import uk.gov.hmcts.reform.civil.utils.CaseMigrationUtility;
     JacksonAutoConfiguration.class})
 public class MigrateCaseDataCallbackHandlerTest extends BaseCallbackHandlerTest {
 
+    @Test
+    void shouldReturnNoError_whenAboutToSubmitIsInvoked_UnSpec() {
+        CaseData caseData = CaseDataBuilder.builder()
+            .atStatePendingClaimIssued()
+            .courtLocation()
+            .caseAccessCategory(CaseCategory.UNSPEC_CLAIM)
+            .respondent1DQWithLocation()
+            .applicant1DQWithLocation()
+            .build();
+        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+
+
+    }
+
+    @Test
+    void shouldReturnNoError_whenSUbmittedIsInvoked_UnSpec() {
+        CaseData caseData = CaseDataBuilder.builder()
+            .atStatePendingClaimIssued()
+            .courtLocation()
+            .caseAccessCategory(CaseCategory.UNSPEC_CLAIM)
+            .respondent1DQWithLocation()
+            .applicant1DQWithLocation()
+            .build();
+        CallbackParams params = callbackParamsOf(caseData, SUBMITTED);
+
+
+    }
 
 }
+
