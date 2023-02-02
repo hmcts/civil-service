545f67b0e8c55c04f2b2f0769f031c64454a253a
skhajuria <sankhajuria@gmail.com>
2023-02-02 14:54:14 +0000

    Remove migraiton related code

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/migration/MigrateCaseDataCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/migration/MigrateCaseDataCallbackHandlerTest.java
@@ -1,8 +1,11 @@
 package uk.gov.hmcts.reform.civil.handler.callback.migration;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.enums.CaseCategory;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
@@ -10,6 +13,7 @@ import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.utils.CaseMigrationUtility;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
 
@@ -18,6 +22,9 @@ import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
     CaseMigrationUtility.class,
     JacksonAutoConfiguration.class})
 public class MigrateCaseDataCallbackHandlerTest extends BaseCallbackHandlerTest {
+
+    @Autowired
+    private MigrateCaseDataCallbackHandler handler;
 
     @Test
     void shouldReturnNoError_whenAboutToSubmitIsInvoked_UnSpec() {
@@ -29,6 +36,9 @@ public class MigrateCaseDataCallbackHandlerTest extends BaseCallbackHandlerTest 
             .applicant1DQWithLocation()
             .build();
         CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+        AboutToStartOrSubmitCallbackResponse response = (AboutToStartOrSubmitCallbackResponse) handler
+            .handle(params);
+        assertThat(response.getErrors()).isNull();
 
     }
 
@@ -36,14 +46,12 @@ public class MigrateCaseDataCallbackHandlerTest extends BaseCallbackHandlerTest 
     void shouldReturnNoError_whenSUbmittedIsInvoked_UnSpec() {
         CaseData caseData = CaseDataBuilder.builder()
             .atStatePendingClaimIssued()
-            .courtLocation()
-            .caseAccessCategory(CaseCategory.UNSPEC_CLAIM)
-            .respondent1DQWithLocation()
-            .applicant1DQWithLocation()
             .build();
+
         CallbackParams params = callbackParamsOf(caseData, SUBMITTED);
 
-
+        SubmittedCallbackResponse response = (SubmittedCallbackResponse) handler
+            .handle(params);
     }
 
 }
