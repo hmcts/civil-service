9036310d093cb8f9a04fdd6dee1a8808f29296ee
m-meulendijks-v1 <107135537+m-meulendijks-v1@users.noreply.github.com>
2023-02-08 13:53:56 +0000

    CIV-5052 Hearing fee paid journey (also CIV-2727, CIV-2736, CIV-4480) (#2024)
    * first changes to schedule
    
    * created necessary classes for hearing fee check (without logic)
    
    * checkstyle fixes
    
    * test classes
    
    * Changes to logic for hearing fee due date
    
    * logic for searching hearing readiness cases
    
    * Checking for relistings
    
    * fix checkstyle errors
    
    * change from less than to less than or equal to
    
    * test fix
    
    * updated handler tests
    
    * Fixed checkstyle errors
    
    * hearingFeeHandler increased coverage for sonarcloud
    
    * positive outcome
    
    * first changes to schedule
    
    * created necessary classes for hearing fee check (without logic)
    
    * checkstyle fixes
    
    * test classes
    
    * Changes to logic for hearing fee due date
    
    * logic for searching hearing readiness cases
    
    * Checking for relistings
    
    * fix checkstyle errors
    
    * change from less than to less than or equal to
    
    * test fix
    
    * updated handler tests
    
    * Fixed checkstyle errors
    
    * hearingFeeHandler increased coverage for sonarcloud
    
    * created necessary classes for hearing fee check (without logic)
    
    * checkstyle fixes
    
    * test classes
    
    * added most classes
    
    * changes to classes for hearing fee check unhappy path
    
    * fix to HearingFeeDueCallbackHandler class
    
    * Rebase
    
    * Rebase
    
    * Fixed checkstyle errors
    
    * Rebase
    
    * Rebase
    
    * Rebase
    
    * Rebase
    
    * Positive outcome
    
    * Rebasing
    
    * Check for trial ready notification
    
    * tests for notification trigger
    
    * added test for trial ready check handler
    
    * removed checkstyle errors
    
    * classes for notification
    
    * notifications for trial ready
    
    * fixes
    
    * case data builder addition
    
    * Decision outcome classes added - no tests
    
    * trial ready
    
    * removed error in test
    
    * fix checkstyle error
    
    * change to test for trial ready
    
    * party utils tests added
    
    * Added tests and changed event name
    
    * added event handler for decision outcome
    
    * readded trial ready event handler
    
    * checkstyle fixes
    
    * Changed date format
    
    * main classes
    
    * tests and checkstyle fixes
    
    * search service test fixes
    
    * Add missing tests
    
    - New unit tests on code added as part of this ticket
    
    * Remove unused imports
    
    - Remove unused imports
    
    * Excessively pedantic checkstyle fix
    
    - Removed excessively pedantic checkstyle fix
    
    * Revert "Excessively pedantic checkstyle fix"
    
    This reverts commit 698c8cfbe7225866ec00088eb7b045e0dfb4e28b.
    
    * Fixing pedantic checkstyle complaints
    
    - non-standard import ordering.......
    
    * Fixing pedantic checkstyle complaints
    
    - non-standard import ordering.......
    
    * changed name to trial-ready-notification throughout.
    
    * changes for hearing fee paid path
    
    * added hearingFeePaid details
    
    * changes to journey
    
    * fixed tests and journey for fee paid
    
    * test fixes
    
    * increased code coverage
    
    * checkstyle fixes
    
    * fix more checkstyle errors
    
    * separated solicitor email into applicant & respondent for hearing fee unpaid
    
    * hearing fee paid event test added
    
    * different template for defendant tests
    
    * update chart
    
    * fixes
    
    * test fixes
    
    * small changes to handlers to avoid null pointers
    
    * removed unused import
    
    * remove variables no longer used in template
    
    * fixed checkstyle error
    
    * checkstyle fix in tests
    
    * removed duplicate event
    
    * test fix after latest master pull
    
    ---------
    
    Co-authored-by: suvarnathekkoot <suvarna.thekkoot@HMCTS.NET>
    Co-authored-by: suvarnathekkoot <95478835+suvarnathekkoot@users.noreply.github.com>
    Co-authored-by: Filippo Possenti <filippo.possenti@version1.com>
    Co-authored-by: Kurt <kurtplaschke@hotmail.co.uk>
    Co-authored-by: filippo-possenti-version1 <109352283+filippo-possenti-version1@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/TrialReadyCheckHandler.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.reform.civil.handler.tasks;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.civil.event.TrialReadyCheckEvent;
+import uk.gov.hmcts.reform.civil.service.search.TrialReadyCheckSearchService;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class TrialReadyCheckHandler implements BaseExternalTaskHandler {
+
+    private final TrialReadyCheckSearchService caseSearchService;
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    @Override
+    public void handleTask(ExternalTask externalTask) {
+        List<CaseDetails> cases = caseSearchService.getCases();
+        log.info("Job '{}' found {} case(s)", externalTask.getTopicName(), cases.size());
+
+        cases.forEach(caseDetails -> {
+            try {
+                applicationEventPublisher.publishEvent(new TrialReadyCheckEvent(caseDetails.getId()));
+            } catch (Exception e) {
+                log.error("Updating case with id: '{}' failed", caseDetails.getId(), e);
+            }
+        });
+    }
+}
