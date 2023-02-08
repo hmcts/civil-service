1bbf466d03f0c6aa0e52d98122e3ed1371828d76
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2023-02-08 08:41:22 +0000

    Test's Regex correction

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/DismissClaimCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/DismissClaimCallbackHandlerTest.java
@@ -64,7 +64,7 @@ class DismissClaimCallbackHandlerTest extends BaseCallbackHandlerTest {
                 .extracting("createdOn")
                 .asString().matches(
                     "((19|2[0-9])[0-9]{2})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])T" +
-                        "(([0-1][0-9])|([2][0-3])):([0-5][0-9]):([0-5][0-9]).\\d{5,}");
+                        "(([0-1][0-9])|([2][0-3])):([0-5][0-9]):([0-5][0-9]).\\d+");
 
             assertThat(response.getData())
                 .containsEntry("claimDismissedDate", localDateTime.format(ISO_DATE_TIME));
--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/HearingFeeUnpaidCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/HearingFeeUnpaidCallbackHandlerTest.java
@@ -66,7 +66,7 @@ class HearingFeeUnpaidCallbackHandlerTest extends BaseCallbackHandlerTest {
                 .extracting("createdOn")
                 .asString().matches(
                     "((19|2[0-9])[0-9]{2})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])T" +
-                        "(([0-1][0-9])|([2][0-3])):([0-5][0-9]):([0-5][0-9]).\\d{5,}");
+                        "(([0-1][0-9])|([2][0-3])):([0-5][0-9]):([0-5][0-9]).\\d+");
 
             assertThat(response.getData())
                 .containsEntry("caseDismissedHearingFeeDueDate", localDateTime.format(ISO_DATE_TIME));
