6d172aa1e81ffd5c09cfae12deea2671deaa91cd
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2023-02-08 08:41:22 +0000

    Test's Regex correction

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ApplyNoticeOfChangeDecisionCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ApplyNoticeOfChangeDecisionCallbackHandlerTest.java
@@ -613,7 +613,7 @@ public class ApplyNoticeOfChangeDecisionCallbackHandlerTest extends BaseCallback
                 .extracting("createdOn")
                 .asString().matches(
                     "((19|2[0-9])[0-9]{2})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])T" +
-                        "(([0-1][0-9])|([2][0-3])):([0-5][0-9]):([0-5][0-9]).\\d{5,}");
+                        "(([0-1][0-9])|([2][0-3])):([0-5][0-9]):([0-5][0-9]).\\d+");
         }
     }
 }
