ea9ea5dc67d6154e1e9921f50972e246db5a8325
pliao-hmcts <113367232+pliao-hmcts@users.noreply.github.com>
2023-02-10 08:40:20 +0000

    CIV-6796 revert (#2260)
    Co-authored-by: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/civil/model/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/CaseData.java
@@ -592,7 +592,7 @@ public class CaseData extends CaseDataParent implements MappableObject {
     private final List<Element<CaseDocument>> generalOrderDocument;
     private final List<Element<CaseDocument>> dismissalOrderDocument;
     private final List<Element<CaseDocument>> directionOrderDocument;
-    private final List<Element<CaseDocument>> hearingOrderDocument;
+    private final List<Element<CaseDocument>> hearingNoticeDocument;
 
     @Builder.Default
     private final List<Element<CaseDocument>> hearingDocuments = new ArrayList<>();
