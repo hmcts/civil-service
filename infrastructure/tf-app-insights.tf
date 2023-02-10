3ce5d48c4671f9ddf24083ca68a666ab0ec3b4a8
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-02-10 12:08:59 +0000

    AppInsights updated

--- infrastructure/tf-app-insights.tf
+++ infrastructure/tf-app-insights.tf
@@ -6,9 +6,9 @@ resource "azurerm_application_insights" "appinsights" {
   tags                = var.common_tags
 }
 
-resource "azurerm_key_vault_secret" "app_insights_key" {
-  name         = "appinsights-instrumentation-key"
-  value        = azurerm_application_insights.appinsights.instrumentation_key
+resource "azurerm_key_vault_secret" "app_insights_connection_string" {
+  name         = "app-insights-connection-string"
+  value        = azurerm_application_insights.appinsights.connection_string
   key_vault_id = module.key-vault.key_vault_id
 
   content_type = "secret"
