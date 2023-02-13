92955797246264e6383f220bc6af74c0f88d9190
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2023-02-13 10:04:39 +0000

    Reset Toggle

--- src/main/java/uk/gov/hmcts/reform/civil/launchdarkly/FeatureToggleService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/launchdarkly/FeatureToggleService.java
@@ -83,7 +83,7 @@ public class FeatureToggleService {
     }
 
     public boolean isCourtLocationDynamicListEnabled() {
-        return internalClient.boolVariation("court-location-dynamic-list", createLDUser().build(), true);
+        return internalClient.boolVariation("court-location-dynamic-list", createLDUser().build(), false);
     }
 
     public boolean isCaseFlagsEnabled() {
@@ -95,7 +95,7 @@ public class FeatureToggleService {
     }
 
     public boolean isAccessProfilesEnabled() {
-        return internalClient.boolVariation("access-profiles", createLDUser().build(), true);
+        return internalClient.boolVariation("access-profiles", createLDUser().build(), false);
     }
 
     public boolean isPbaV3Enabled() {
