7ccae5f022a346105c4d22d76317a1a59ffddee5
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2023-02-13 13:08:11 +0000

    CIV-5829: Adding Field not exist in CUI

--- src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/RespondentResponseCui.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/RespondentResponseCui.java
@@ -4,16 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class RespondentResponseCui {
-
-    private YesOrNo partnerPensionCui;
-    private YesOrNo partnerDisabilityCui;
-    private YesOrNo partnerSevereDisabilityCui;
-    private String childrenEducationCui;
+    private FinancialDetailsCui respondent1FinancialDetailsFromCui;
 }
