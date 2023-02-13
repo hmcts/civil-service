65aed6e18ab6f3045c1c3d29f99316a150a3b6d8
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2023-02-13 15:37:13 +0000

    fix unit tests

--- src/test/java/uk/gov/hmcts/reform/civil/service/citizenui/DashboardClaimInfoServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/citizenui/DashboardClaimInfoServiceTest.java
@@ -12,7 +12,9 @@ import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.ClaimValue;
 import uk.gov.hmcts.reform.civil.model.Party;
+import uk.gov.hmcts.reform.civil.model.citizenui.CcdClaimStatusDashboardFactory;
 import uk.gov.hmcts.reform.civil.model.citizenui.DashboardClaimInfo;
+import uk.gov.hmcts.reform.civil.model.citizenui.DashboardClaimStatus;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 import uk.gov.hmcts.reform.civil.service.claimstore.ClaimStoreService;
 
@@ -43,6 +45,9 @@ public class DashboardClaimInfoServiceTest {
     @Mock
     private CaseDetailsConverter caseDetailsConverter;
 
+    @Mock
+    private CcdClaimStatusDashboardFactory ccdClaimStatusDashboardFactory;
+
     @InjectMocks
     private DashboardClaimInfoService dashboardClaimInfoService;
 
@@ -50,6 +55,7 @@ public class DashboardClaimInfoServiceTest {
     void setUp() {
         given(claimStoreService.getClaimsForClaimant(any(), any())).willReturn(CLAIM_STORE_SERVICE_RESULTS);
         given(claimStoreService.getClaimsForDefendant(any(), any())).willReturn(CLAIM_STORE_SERVICE_RESULTS);
+        given(ccdClaimStatusDashboardFactory.getDashboardClaimStatus(any())).willReturn(DashboardClaimStatus.NO_STATUS);
         CaseDetails caseDetails = CaseDetails.builder().id(1L).build();
         List<CaseDetails> cases = List.of(caseDetails);
         SearchResult searchResult = SearchResult.builder().total(1).cases(cases).build();
--- src/test/java/uk/gov/hmcts/reform/cmc/model/CmcClaimTest.java
+++ src/test/java/uk/gov/hmcts/reform/cmc/model/CmcClaimTest.java
@@ -1,16 +1,8 @@
 package uk.gov.hmcts.reform.cmc.model;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import uk.gov.hmcts.reform.civil.model.citizenui.DashboardClaimStatus;
-import uk.gov.hmcts.reform.civil.enums.RespondentResponseType;
-import uk.gov.hmcts.reform.civil.enums.YesOrNo;
-import uk.gov.hmcts.reform.civil.model.CountyCourtJudgment;
-import uk.gov.hmcts.reform.civil.model.PaymentDetails;
-import uk.gov.hmcts.reform.civil.model.dq.Respondent1DQ;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 import java.util.Collections;
 
