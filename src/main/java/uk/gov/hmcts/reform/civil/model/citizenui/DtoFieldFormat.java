c305e243095560e4b16225174fc29d523e9d05b7
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2023-02-16 11:45:00 +0000

    add private constructor to utility

--- src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/DtoFieldFormat.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/DtoFieldFormat.java
@@ -3,4 +3,8 @@ package uk.gov.hmcts.reform.civil.model.citizenui;
 public class DtoFieldFormat {
 
     public static final String DATE_FORMAT = "yyyy-MM-dd";
+
+    private DtoFieldFormat() {
+
+    }
 }
