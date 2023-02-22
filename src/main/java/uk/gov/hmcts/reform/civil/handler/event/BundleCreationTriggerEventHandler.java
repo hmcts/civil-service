fbb9ecedcb9a21b0b34e44e5a319ae3905aa7b2d
bhagyashreesharma90 <114094319+bhagyashreesharma90@users.noreply.github.com>
2023-02-22 11:26:28 +0000

    CIV-5801 BundleEndpoint (#2181)
    * initial commit for bundle endpoint
    
    * updated case
    
    * removed duplicate event
    
    * added doc for evidence other
    
    * updated changes
    
    * updated config
    
    * updated model
    
    * updated bundle details
    
    * Update BundleCreationTriggerEventHandler.java
    
    * remove historical bundles
    
    * updated logs
    
    * updated model
    
    * updated caseBundlesInfo to caseBundles
    
    * updated AppInsight version
    
    * revert java17 changes
    
    * issue fix
    
    * update the filename
    
    * updated flow state
    
    * updated for display filename
    
    * added createddatetime
    
    * addressed review comments
    
    * Addressed review comments
    
    * added missing comment
    
    * CIV-5801 Code review
    
    - Modified some areas of code to eliminate SonarLint complaints
    - Separated a method in two different ones
    - Added skeleton unit-tests to be added
    
    * Fixed compilation  issue
    
    * udpated test
    
    * upated test
    
    ---------
    
    Co-authored-by: sankhajuria <sankhajuria@gmail.com>
    Co-authored-by: mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
    Co-authored-by: Filippo Possenti <filippo.possenti@version1.com>

--- src/main/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandler.java
@@ -1,26 +1,84 @@
 package uk.gov.hmcts.reform.civil.handler.event;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
+import uk.gov.hmcts.reform.ccd.client.model.Event;
+import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 import uk.gov.hmcts.reform.civil.event.BundleCreationTriggerEvent;
+import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
+import uk.gov.hmcts.reform.civil.model.Bundle;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.IdValue;
+import uk.gov.hmcts.reform.civil.model.bundle.BundleCreateResponse;
+import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
+import uk.gov.hmcts.reform.civil.service.bundle.BundleCreationService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_BUNDLE;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class BundleCreationTriggerEventHandler {
 
+    @Autowired
+    private BundleCreationService bundleCreationService;
+    private final CoreCaseDataService coreCaseDataService;
+    private final CaseDetailsConverter caseDetailsConverter;
+
     /**
-    * This method will send notification to applicant and respondent solicitors.
-    * This method will not throw any exception but log warning if there is any error while
-    * sending notification because these email notifications are not business critical
-    * and provided as a courtesy, as the user can log in and see the new uploads.
-    *
-    * @param event EvidenceUploadNotificationEvent
-    */
+     * This method will call bundle API and save required details in case data.
+     * If there is any existing bundle then new bundle will be added to existing list of bundles.
+     * @param event BundleCreationTriggerEvent.
+     */
     @EventListener
     public void sendBundleCreationTrigger(BundleCreationTriggerEvent event) {
+        BundleCreateResponse bundleCreateResponse = bundleCreationService.createBundle(event);
+        String caseId = event.getCaseId().toString();
+        StartEventResponse startEventResponse = coreCaseDataService.startUpdate(caseId, CREATE_BUNDLE);
+        CaseData caseData = caseDetailsConverter.toCaseData(startEventResponse.getCaseDetails().getData());
 
+        List<IdValue<Bundle>> caseBundles = caseData.getCaseBundles();
+        caseBundles.addAll(bundleCreateResponse.getData().getCaseBundles()
+                               .stream().map(bundle -> prepareNewBundle(bundle, caseData)
+            ).collect(Collectors.toList()));
+        CaseDataContent caseContent = prepareCaseContent(caseBundles, startEventResponse);
+        coreCaseDataService.submitUpdate(caseId, caseContent);
+    }
+
+    IdValue<Bundle> prepareNewBundle(uk.gov.hmcts.reform.civil.model.bundle.Bundle bundle, CaseData caseData) {
+        Bundle result = Bundle.builder()
+            .bundleHearingDate(Optional.of(caseData.getHearingDate()))
+            .stitchedDocument(Optional.ofNullable(bundle.getValue().getStitchedDocument()))
+            .filename(bundle.getValue().getFileName())
+            .title(bundle.getValue().getTitle())
+            .description(Optional.ofNullable(bundle.getValue().getDescription()).isPresent()
+                             ? Optional.ofNullable(bundle.getValue().getDescription()).get() : "")
+            .stitchStatus(Optional.ofNullable(bundle.getValue().getStitchStatus()))
+            .createdOn(Optional.of(LocalDateTime.now()))
+            .id(bundle.getValue().getId()).build();
+        return new IdValue<>(result.getId(), result);
+    }
+
+    CaseDataContent prepareCaseContent(List<IdValue<Bundle>> caseBundles, StartEventResponse startEventResponse) {
+        Map<String, Object> data = startEventResponse.getCaseDetails().getData();
+        data.put("caseBundles", caseBundles);
+        return CaseDataContent.builder()
+            .eventToken(startEventResponse.getToken())
+            .event(Event.builder()
+                       .id(startEventResponse.getEventId())
+                       .summary("bundle created")
+                       .build())
+            .data(data)
+            .build();
     }
 }
