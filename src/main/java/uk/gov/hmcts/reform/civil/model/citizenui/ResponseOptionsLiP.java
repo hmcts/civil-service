cb7371466e08d3e7005e68a96f2376d313e8d7f5
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2023-02-23 12:20:57 +0000

    CIV-5788: Update Enum

--- src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/ResponseOptionsLiP.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/ResponseOptionsLiP.java
@@ -3,13 +3,9 @@ package uk.gov.hmcts.reform.civil.model.citizenui;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
 public enum ResponseOptionsLiP {
-    ALREADY_AGREED("already-agreed"),
-    NO("no"),
-    REQUEST_REFUSED("request-refused"),
-    YES("yes");
-
-    private final String value;
+    ALREADY_AGREED,
+    NO,
+    REQUEST_REFUSED,
+    YES;
 }
