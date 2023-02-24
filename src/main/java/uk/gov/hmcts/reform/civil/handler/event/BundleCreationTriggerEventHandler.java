69af469a6d5828c289f010eca43eda88d8b765fb
bhagyashreesharma90 <bhagyashree.sharma@hmcts.net>
2023-02-24 09:13:55 +0000

    fix sonar

--- src/main/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandler.java
@@ -65,8 +65,8 @@ public class BundleCreationTriggerEventHandler {
             .stitchedDocument(Optional.ofNullable(bundle.getValue().getStitchedDocument()))
             .filename(bundle.getValue().getFileName())
             .title(bundle.getValue().getTitle())
-            .description(Optional.ofNullable(bundle.getValue().getDescription()).isPresent()
-                             ? Optional.ofNullable(bundle.getValue().getDescription()).get() : "")
+            .description(null != bundle.getValue().getDescription()
+                             ? Optional.of(bundle.getValue().getDescription()).get() : "")
             .stitchStatus(Optional.ofNullable(bundle.getValue().getStitchStatus()))
             .createdOn(Optional.of(LocalDateTime.now()))
             .id(bundle.getValue().getId()).build();
