4b4755b46686684e445b207925ac637d50d7155d
dtortolaV1 <dario.tortolanavarro@hmcts.net>
2023-02-27 15:14:52 +0000

    CIV-5793 most fields but some from financial details

--- src/main/java/uk/gov/hmcts/reform/civil/model/docmosis/sealedclaim/SealedClaimLipResponseForm.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/docmosis/sealedclaim/SealedClaimLipResponseForm.java
@@ -7,10 +7,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import uk.gov.hmcts.reform.civil.model.Party;
-import uk.gov.hmcts.reform.civil.model.SolicitorReferences;
-import uk.gov.hmcts.reform.civil.model.StatementOfTruth;
+import uk.gov.hmcts.reform.civil.enums.RespondentResponsePartAdmissionPaymentTimeLRspec;
+import uk.gov.hmcts.reform.civil.enums.RespondentResponseTypeSpec;
+import uk.gov.hmcts.reform.civil.model.EmployerDetailsLRspec;
+import uk.gov.hmcts.reform.civil.model.PartnerAndDependentsLRspec;
+import uk.gov.hmcts.reform.civil.model.RepaymentPlanLRspec;
+import uk.gov.hmcts.reform.civil.model.Respondent1SelfEmploymentLRspec;
+import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.model.common.MappableObject;
+import uk.gov.hmcts.reform.civil.model.docmosis.LipDefenceFormParty;
+import uk.gov.hmcts.reform.civil.model.docmosis.common.EventTemplateData;
+import uk.gov.hmcts.reform.civil.model.docmosis.common.EvidenceTemplateData;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -20,23 +27,44 @@ import java.util.List;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class SealedClaimLipResponseForm implements MappableObject {
-    private final String referenceNumber;
-    private final String caseName;
-    private final SolicitorReferences solicitorReferences;
+
+    private final String claimReferenceNumber;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     @JsonSerialize(using = LocalDateSerializer.class)
-    private final LocalDate submittedOn;
-    private final Party respondent1;
-    private final String defendantResponse;
-    private final String whyDisputeTheClaim;
-    private final boolean timelineUploaded;
-    private final String specResponseTimelineDocumentFiles;
-    private final List<TimelineEventDetailsDocmosis> timeline;
-    private final String respondent1SpecDefenceResponseDocument;
-    private final String poundsPaid;
+    private final LocalDate generationDate;
+    private final String amountToPay;
+    private final String howMuchWasPaid;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     @JsonSerialize(using = LocalDateSerializer.class)
     private final LocalDate paymentDate;
-    private final String paymentMethod;
-    private final StatementOfTruth statementOfTruth;
+    private final String paymentHow;
+    private final RespondentResponsePartAdmissionPaymentTimeLRspec howToPay;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private final LocalDate payBy;
+    private final String whyNotPayImmediately;
+    private final RepaymentPlanLRspec repaymentPlan;
+
+    private final RespondentResponseTypeSpec responseType;
+    // TODO enum, ALREADY_PAID, DISPUTE, COUNTER_CLAIM
+    private final String whyReject;
+    private final LipDefenceFormParty claimant1;
+    private final LipDefenceFormParty defendant1;
+    private final LipDefenceFormParty defendant2;
+    private final List<EventTemplateData> timelineEventList;
+    private final String timelineComments;
+    private final List<EvidenceTemplateData> evidenceList;
+    private final String evidenceComments;
+    private final boolean mediation;
+    private final String whereTheyLive;
+    private final PartnerAndDependentsLRspec partnerAndDependent;
+    private final boolean currentlyWorking;
+    private final List<EmployerDetailsLRspec> employerDetails;
+    private final Respondent1SelfEmploymentLRspec selfEmployment;
+
+    public String getResponseTypeDisplay() {
+        // TODO localization?
+        return responseType.getDisplayedValue();
+    }
+
 }
--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/sealedclaim/SealedClaimLipResponseFormGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/sealedclaim/SealedClaimLipResponseFormGenerator.java
@@ -2,19 +2,29 @@ package uk.gov.hmcts.reform.civil.service.docmosis.sealedclaim;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.civil.enums.RespondentResponsePartAdmissionPaymentTimeLRspec;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.Party;
+import uk.gov.hmcts.reform.civil.model.Respondent1EmployerDetailsLRspec;
 import uk.gov.hmcts.reform.civil.model.docmosis.DocmosisDocument;
+import uk.gov.hmcts.reform.civil.model.docmosis.LipDefenceFormParty;
 import uk.gov.hmcts.reform.civil.model.docmosis.sealedclaim.SealedClaimLipResponseForm;
 import uk.gov.hmcts.reform.civil.model.documents.CaseDocument;
 import uk.gov.hmcts.reform.civil.model.documents.DocumentType;
 import uk.gov.hmcts.reform.civil.model.documents.PDF;
+import uk.gov.hmcts.reform.civil.model.dq.HomeDetails;
+import uk.gov.hmcts.reform.civil.model.dq.Respondent1DQ;
 import uk.gov.hmcts.reform.civil.service.docmosis.DocumentGeneratorService;
 import uk.gov.hmcts.reform.civil.service.docmosis.TemplateDataGenerator;
 import uk.gov.hmcts.reform.civil.service.documentmanagement.DocumentManagementService;
-import uk.gov.hmcts.reform.civil.utils.DocmosisTemplateDataUtils;
-import uk.gov.hmcts.reform.civil.utils.MonetaryConversions;
+import uk.gov.hmcts.reform.civil.utils.ElementUtils;
 
+import java.time.LocalDate;
+import java.util.EnumSet;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.DEFENDANT_RESPONSE_LIP_SPEC;
@@ -26,23 +36,102 @@ public class SealedClaimLipResponseFormGenerator implements TemplateDataGenerato
     private final DocumentGeneratorService documentGeneratorService;
     private final DocumentManagementService documentManagementService;
 
+    private static final Set<Party.Type> INDIVIDUAL_TYPES = EnumSet.of(Party.Type.INDIVIDUAL, Party.Type.SOLE_TRADER);
+    // TODO look for existing constant
+    private static final long DAYS_TO_PAY_IMMEDIATELY = 5;
+
     @Override
     public SealedClaimLipResponseForm getTemplateData(CaseData caseData) {
-        var builder = SealedClaimLipResponseForm.builder()
-            .respondent1(caseData.getRespondent1())
-            .referenceNumber(caseData.getLegacyCaseReference())
-            .caseName(DocmosisTemplateDataUtils.toCaseName.apply(caseData))
-            .whyDisputeTheClaim(caseData.getDetailsOfWhyDoesYouDisputeTheClaim())
-            .statementOfTruth(caseData.getRespondent1DQ().getRespondent1DQStatementOfTruth());
+        SealedClaimLipResponseForm.SealedClaimLipResponseFormBuilder builder = SealedClaimLipResponseForm.builder()
+            .generationDate(LocalDate.now())
+            .responseType(caseData.getRespondent1ClaimResponseTypeForSpec())
+            .claimReferenceNumber(caseData.getLegacyCaseReference())
+            .claimant1(getPartyData(caseData.getApplicant1()))
+            .defendant1(getPartyData(caseData.getRespondent1()))
+            .defendant2(getPartyData(caseData.getRespondent2()))
+            .mediation(caseData.getResponseClaimMediationSpecRequired() == YesOrNo.YES)
+            .whyNotPayImmediately(caseData.getResponseToClaimAdmitPartWhyNotPayLRspec())
+            .partnerAndDependent(caseData.getRespondent1PartnerAndDependent())
+            .currentlyWorking(caseData.getDefenceAdmitPartEmploymentTypeRequired() == YesOrNo.YES)
+            .selfEmployment(caseData.getSpecDefendant1SelfEmploymentDetails());
 
-        Stream.of(caseData.getRespondToClaim(), caseData.getRespondToAdmittedClaim())
-            .filter(Objects::nonNull)
-            .findFirst()
-            .ifPresent(response -> builder.poundsPaid(MonetaryConversions
-                                                          .penniesToPounds(response.getHowMuchWasPaid()).toString())
-                .paymentDate(response.getWhenWasThisAmountPaid()));
+
+        Optional.ofNullable(caseData.getResponseClaimAdmitPartEmployer())
+            .map(Respondent1EmployerDetailsLRspec::getEmployerDetails)
+            .map(ElementUtils::unwrapElements)
+            .ifPresent(builder::employerDetails);
 
 
+        if (caseData.getDefenceAdmitPartPaymentTimeRouteRequired()
+            == RespondentResponsePartAdmissionPaymentTimeLRspec.SUGGESTION_OF_REPAYMENT_PLAN
+            || caseData.getDefenceAdmitPartPaymentTimeRouteRequired()
+            == RespondentResponsePartAdmissionPaymentTimeLRspec.BY_SET_DATE) {
+            Optional.ofNullable(caseData.getRespondent1DQ())
+                .map(Respondent1DQ::getRespondent1DQHomeDetails)
+                .map(HomeDetails::getType)
+                .ifPresent(type -> {
+                    switch (type) {
+                        case OWNED_HOME:
+                            builder.whereTheyLive("Home they own or pay a mortgage on");
+                            break;
+                        case PRIVATE_RENTAL:
+                            builder.whereTheyLive("Private rental");
+                            break;
+                        case ASSOCIATION_HOME:
+                            builder.whereTheyLive("Council or housing association home");
+                            break;
+                        case JOINTLY_OWNED_HOME:
+                            builder.whereTheyLive("Jointly-owned home (or jointly mortgaged home)");
+                            break;
+                        case OTHER:
+                            builder.whereTheyLive("Other");
+                            break;
+                    }
+                });
+        }
+
+        if (caseData.getRespondent1ClaimResponseTypeForSpec() != null) {
+            builder.howToPay(caseData.getDefenceAdmitPartPaymentTimeRouteRequired());
+            switch (caseData.getRespondent1ClaimResponseTypeForSpec()) {
+                case FULL_ADMISSION:
+                    if (caseData.getDefenceAdmitPartPaymentTimeRouteRequired()
+                        == RespondentResponsePartAdmissionPaymentTimeLRspec.IMMEDIATELY) {
+                        builder.payBy(LocalDate.now().plusDays(DAYS_TO_PAY_IMMEDIATELY))
+                            .amountToPay(caseData.getTotalClaimAmount() + "");
+                    } else if (caseData.getDefenceAdmitPartPaymentTimeRouteRequired()
+                        == RespondentResponsePartAdmissionPaymentTimeLRspec.SUGGESTION_OF_REPAYMENT_PLAN) {
+                        builder.repaymentPlan(caseData.getRespondent1RepaymentPlan())
+                            .payBy(caseData.getRespondent1RepaymentPlan()
+                                       .finalPaymentBy(caseData.getTotalClaimAmount()));
+                    } else if (caseData.getDefenceAdmitPartPaymentTimeRouteRequired()
+                        == RespondentResponsePartAdmissionPaymentTimeLRspec.BY_SET_DATE) {
+                        // TODO
+                    }
+                    break;
+            }
+        }
+
+        return builder.build();
+    }
+
+    private LipDefenceFormParty getPartyData(Party party) {
+        if (party == null) {
+            return null;
+        }
+        LipDefenceFormParty.LipDefenceFormPartyBuilder builder = LipDefenceFormParty.builder()
+            .name(party.getPartyName())
+            .phone(party.getPartyPhone())
+            .email(party.getPartyEmail())
+            .primaryAddress(party.getPrimaryAddress());
+        if (INDIVIDUAL_TYPES.contains(party.getType())) {
+            builder.isIndividual(true);
+            Stream.of(party.getIndividualDateOfBirth(), party.getSoleTraderDateOfBirth())
+                .filter(Objects::nonNull)
+                .findFirst()
+                .ifPresent(builder::dateOfBirth);
+        } else {
+            builder.isIndividual(false);
+        }
         return builder.build();
     }
 
