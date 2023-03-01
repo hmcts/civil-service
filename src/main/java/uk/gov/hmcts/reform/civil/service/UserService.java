bcde6938850dc7b868b4821d7c90027e2fe52ed4
Filippo Possenti <filippo.possenti@version1.com>
2023-03-01 18:33:27 +0000

    CIV-7263 Checkstyle...

--- src/main/java/uk/gov/hmcts/reform/civil/service/UserService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/UserService.java
@@ -1,9 +1,5 @@
 package uk.gov.hmcts.reform.civil.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 
 public interface UserService {
