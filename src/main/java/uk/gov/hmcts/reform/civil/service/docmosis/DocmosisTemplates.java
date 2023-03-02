9e8f3a49636a7f72f025153673c0137d6a473d92
jeejavr <jeejamol.vijayarajan@HMCTS.net>
2023-03-02 16:15:53 +0000

    CIV-6422: Updating docmosis templates HNL

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/DocmosisTemplates.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/DocmosisTemplates.java
@@ -46,7 +46,7 @@ public enum DocmosisTemplates {
     PIN_IN_THE_POST_LETTER("CV-CMC-LET-ENG-PIP0001.docx", "PIP_letter.pdf"),
 
     //HNL uplifted docs to be replaced with original once ready
-    DEFENDANT_RESPONSE_SPEC_HNL("CV-SPC-HRN-ENG-01208.docx", "%s_response_sealed_form.pdf"),
+    DEFENDANT_RESPONSE_SPEC_HNL("CV-SPC-HRN-ENG-01209.docx", "%s_response_sealed_form.pdf"),
     HNL_DQ_RESPONSE_1V1("CV-UNS-HRN-ENG-01215.docx", "%s_directions_questionnaire_form_%s.pdf"),
     HNL_DQ_RESPONSE_1V2_DS("CV-UNS-HRN-ENG-01212.docx", "%s_directions_questionnaire_form_%s.pdf"),
     HNL_DQ_RESPONSE_1V2_SS("CV-UNS-HRN-ENG-01214.docx", "%s_directions_questionnaire_form_%s.pdf"),
