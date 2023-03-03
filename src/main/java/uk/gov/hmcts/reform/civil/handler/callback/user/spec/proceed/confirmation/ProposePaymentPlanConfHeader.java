c60f8f5ea1c6618af018d2e2260705b677a5397a
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2023-03-03 14:54:57 +0000

    CIV-5462: Update From CheckStyle

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/proceed/confirmation/ProposePaymentPlanConfHeader.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/proceed/confirmation/ProposePaymentPlanConfHeader.java
@@ -1,24 +1,15 @@
 package uk.gov.hmcts.reform.civil.handler.callback.user.spec.proceed.confirmation;
 
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.civil.enums.RespondentResponsePartAdmissionPaymentTimeLRspec;
-import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.user.spec.RespondToResponseConfirmationHeaderGenerator;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 
-import java.util.EnumSet;
 import java.util.Optional;
-import java.util.Set;
 
 import static java.lang.String.format;
 
 @Component
 public class ProposePaymentPlanConfHeader implements RespondToResponseConfirmationHeaderGenerator {
-
-    private static final Set<RespondentResponsePartAdmissionPaymentTimeLRspec> PAYMENT_PLAN = EnumSet.of(
-        RespondentResponsePartAdmissionPaymentTimeLRspec.SUGGESTION_OF_REPAYMENT_PLAN,
-        RespondentResponsePartAdmissionPaymentTimeLRspec.BY_SET_DATE
-    );
 
     @Override
     public Optional<String> generateTextFor(CaseData caseData) {
