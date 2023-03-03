fb4fd6c51607f0a1ca75459dc2dc3f2914a5fa4a
indu2019 <indu.chaturvedi@hmcts.net>
2023-03-03 12:37:04 +0000

    fix: CIV-6963, fixes test cases

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateResponseSealedSpec.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateResponseSealedSpec.java
@@ -29,6 +29,7 @@ import java.util.Map;
 import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CallbackVersion.V_1;
+import static uk.gov.hmcts.reform.civil.utils.ElementUtils.wrapElements;
 
 @Service
 @RequiredArgsConstructor
@@ -68,7 +69,11 @@ public class GenerateResponseSealedSpec extends CallbackHandler {
         );
 
         if (stitchEnabled) {
+
             List<DocumentMetaData> documentMetaDataList = fetchDocumentsToStitch(caseData, sealedForm);
+            System.out.println(" documentMetaDataList list here " + documentMetaDataList.size());
+            if (documentMetaDataList.size() > 1) {
+                System.out.println(" inside if condition ");
             CaseDocument stitchedDocument = civilDocumentStitchingService.bundle(
                 documentMetaDataList,
                 callbackParams.getParams().get(CallbackParams.Params.BEARER_TOKEN).toString(),
@@ -76,10 +81,16 @@ public class GenerateResponseSealedSpec extends CallbackHandler {
                 sealedForm.getDocumentName(),
                 caseData
             );
+                System.out.println(" inside if condition 2 ");
             if (V_1.equals(callbackParams.getVersion()) && toggleService.isPinInPostEnabled()) {
+                System.out.println(" inside if condition 3 ");
                 builder.respondent1ClaimResponseDocumentSpec(stitchedDocument);
             }
+                System.out.println(" before adding this list to system ");
             caseData.getSystemGeneratedCaseDocuments().add(ElementUtils.element(stitchedDocument));
+                System.out.println(" after adding this list to system ");
+           }
+            //caseDataBuilder.systemGeneratedCaseDocuments(wrapElements(stitchedDocument));
         } else {
             if (V_1.equals(callbackParams.getVersion()) && toggleService.isPinInPostEnabled()) {
                 builder.respondent1ClaimResponseDocumentSpec(sealedForm);
