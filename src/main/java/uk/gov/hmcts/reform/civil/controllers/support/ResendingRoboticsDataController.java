14b1410600675ce2acfe7d06f73e3eb823459fe1
Astha Malviya <astha.malviya@HMCTS.NET>
2023-03-06 15:14:21 +0000

    retriggering RPA event

---
+++ src/main/java/uk/gov/hmcts/reform/civil/controllers/support/ResendingRoboticsDataController.java
@@ -0,0 +1,66 @@
+package uk.gov.hmcts.reform.civil.controllers.support;
+
+import feign.FeignException;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.web.bind.annotation.*;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
+import uk.gov.hmcts.reform.ccd.client.model.Event;
+import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
+
+import java.util.Map;
+
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.*;
+
+@Api
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class ResendingRoboticsDataController {
+
+    private final CoreCaseDataService coreCaseDataService;
+
+    @PostMapping("/support/rpacontinuousfeed/{caseId}")
+    public void updateCaseDataForRPAContinuousfeed(@PathVariable("caseId") Long caseId) {
+        try {
+            var startEventResponse = coreCaseDataService.startUpdate(caseId.toString(), NOTIFY_RPA_ON_CONTINUOUS_FEED);
+
+            Map<String, Object> caseDataMap  = coreCaseDataService.getCase(caseId).getData();
+
+            coreCaseDataService.submitUpdate(caseId.toString(), caseDataContent(startEventResponse, caseDataMap));
+
+        } catch (FeignException e) {
+            log.error(String.format("Updating case data failed: %s", e.contentUTF8()));
+            throw e;
+        }
+    }
+
+    @PostMapping("/support/rpafeedonoffline/{caseId}")
+    public void updateCaseDataForRPAFeedOnOffline(@PathVariable("caseId") Long caseId) {
+        try {
+            var startEventResponse = coreCaseDataService.startUpdate(caseId.toString(), NOTIFY_RPA_ON_CASE_HANDED_OFFLINE);
+
+            Map<String, Object> caseDataMap  = coreCaseDataService.getCase(caseId).getData();
+
+            coreCaseDataService.submitUpdate(caseId.toString(), caseDataContent(startEventResponse, caseDataMap));
+
+        } catch (FeignException e) {
+            log.error(String.format("Updating case data failed: %s", e.contentUTF8()));
+            throw e;
+        }
+    }
+
+    private CaseDataContent caseDataContent(StartEventResponse startEventResponse, Map<String, Object> caseDataMap) {
+        Map<String, Object> data = startEventResponse.getCaseDetails().getData();
+        data.putAll(caseDataMap);
+
+        return CaseDataContent.builder()
+            .eventToken(startEventResponse.getToken())
+            .event(Event.builder().id(startEventResponse.getEventId()).build())
+            .data(data)
+            .build();
+    }
+}
