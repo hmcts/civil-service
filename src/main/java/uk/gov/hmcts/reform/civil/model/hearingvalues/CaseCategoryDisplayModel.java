7d1154fcd9f6521ac3e95e1fe5bb89b09ef1545e
ThomasMitchellA <adam.thomas-mitchell@hmcts.net>
2023-03-06 12:38:50 +0000

    Fixed checkstyle errors

--- src/main/java/uk/gov/hmcts/reform/civil/model/hearingvalues/CaseCategoryDisplayModel.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/hearingvalues/CaseCategoryDisplayModel.java
@@ -14,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CaseCategoryDisplayModel {
+
     private String categoryDisplayValue;
     private List<CaseCategoryDisplayModel> childNodes;
 }
