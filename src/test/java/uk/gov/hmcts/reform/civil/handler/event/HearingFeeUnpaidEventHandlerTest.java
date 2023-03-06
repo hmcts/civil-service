ee909b7ee114c803ac8101438dc70864b6c1c752
bhagyashreesharma90 <bhagyashree.sharma@hmcts.net>
2023-03-06 15:04:38 +0000

    CIV-7409 Added test for HearingFeeUnpaid

---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/event/HearingFeeUnpaidEventHandlerTest.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.reform.civil.handler.event;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.civil.event.HearingFeePaidEvent;
+import uk.gov.hmcts.reform.civil.event.HearingFeeUnpaidEvent;
+import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
+
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.HEARING_FEE_PAID;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.HEARING_FEE_UNPAID;
+
+@ExtendWith(SpringExtension.class)
+class HearingFeeUnpaidEventHandlerTest {
+
+    @Mock
+    private CoreCaseDataService coreCaseDataService;
+
+    @InjectMocks
+    private HearingFeeUnpaidEventHandler handler;
+
+    @Test
+    void shouldCallTriggerEventWithExpectedParams_WhenDismissClaimEvent() {
+        HearingFeeUnpaidEvent event = new HearingFeeUnpaidEvent(1L);
+
+        handler.moveCaseToStruckOut(event);
+
+        verify(coreCaseDataService).triggerEvent(event.getCaseId(), HEARING_FEE_UNPAID);
+    }
+
+}
