d787e6408c886488fa671fcb31b9871b67b21ef8
Astha Malviya <astha.malviya@HMCTS.NET>
2023-03-06 15:53:19 +0000

    Fixing test

--- src/main/java/uk/gov/hmcts/reform/civil/controllers/support/ResendingRoboticsDataController.java
+++ src/main/java/uk/gov/hmcts/reform/civil/controllers/support/ResendingRoboticsDataController.java
@@ -4,8 +4,9 @@ import feign.FeignException;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.Event;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
@@ -13,7 +14,8 @@ import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 
 import java.util.Map;
 
-import static uk.gov.hmcts.reform.civil.callback.CaseEvent.*;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_RPA_ON_CONTINUOUS_FEED;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_RPA_ON_CASE_HANDED_OFFLINE;
 
 @Api
 @Slf4j
