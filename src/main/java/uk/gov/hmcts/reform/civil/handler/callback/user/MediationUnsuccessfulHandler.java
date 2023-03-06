a854aac79ba43b4a0b5d865e36108f47fa0d222f
CalderonF <felipe.calderon@hmcts.net>
2023-03-06 14:09:31 +0100

    Fix: individual imports

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/MediationUnsuccessfulHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/MediationUnsuccessfulHandler.java
@@ -5,7 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
-import uk.gov.hmcts.reform.civil.callback.*;
+import uk.gov.hmcts.reform.civil.callback.Callback;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CallbackType;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 
 import java.util.List;
 import java.util.Map;
