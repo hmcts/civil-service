a4c121bfdaa4acf1830b828d5777a805f9599972
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2023-03-07 17:48:43 +0000

    upload handler to upload translation documents

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/UploadTranslatedDocumentHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/UploadTranslatedDocumentHandler.java
@@ -3,8 +3,11 @@ package uk.gov.hmcts.reform.civil.handler.callback.user;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 
 import java.util.List;
@@ -21,11 +24,17 @@ public class UploadTranslatedDocumentHandler extends CallbackHandler {
 
     @Override
     protected Map<String, Callback> callbacks() {
-        return Map.of(callbackKey(ABOUT_TO_SUBMIT), this::emptyCallbackResponse);
+        return Map.of(callbackKey(ABOUT_TO_SUBMIT), this::uploadDocument);
     }
 
     @Override
     public List<CaseEvent> handledEvents() {
         return List.of(UPLOAD_TRANSLATED_DOCUMENT);
+    }
+
+    private CallbackResponse uploadDocument(CallbackParams callbackParams) {
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(callbackParams.getCaseData().toMap(objectMapper))
+            .build();
     }
 }
