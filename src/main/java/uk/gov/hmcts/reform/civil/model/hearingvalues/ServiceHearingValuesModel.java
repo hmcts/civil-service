0abacb6d058b2a6d28c84dc5f434ad8e5e58c7fb
ThomasMitchellA <adam.thomas-mitchell@hmcts.net>
2023-03-08 14:02:05 +0000

    Remove unused import

--- src/main/java/uk/gov/hmcts/reform/civil/model/hearingvalues/ServiceHearingValuesModel.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/hearingvalues/ServiceHearingValuesModel.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.civil.model.hearingvalues;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
