aba0fd1b64a4074adab32101dad6b82ee225f4ce
meulendijksm <max.meulendijks@hmcts.net>
2023-03-09 12:45:28 +0000

    CIV-TASK enable PBAv3 toggle in demo

--- src/test/java/uk/gov/hmcts/reform/civil/launchdarkly/FeatureToggleServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/launchdarkly/FeatureToggleServiceTest.java
@@ -190,15 +190,6 @@ class FeatureToggleServiceTest {
     }
 
     @Test
-    void shouldCallBoolVariation_whenIsPbaV3EnabledInvoked() {
-        var pbaV3Key = "pba-version-3-ways-to-pay";
-        givenToggle(pbaV3Key, true);
-
-        assertThat(featureToggleService.isPbaV3Enabled()).isTrue();
-        verifyBoolVariationCalled(pbaV3Key, List.of("timestamp", "environment"));
-    }
-
-    @Test
     void shouldCallBoolVariation_whenHmcIsEnabled() {
         var hmcKey = "hmc";
         givenToggle(hmcKey, true);
