18fa81c99c540a0770bac9c6da61ca1738c6c6fc
Sabah Irfan <sabah.irfan@gmail.com>
2023-03-09 18:05:23 +0000

    clean up model class

--- commons-civil/src/main/java/uk/gov/hmcts/reform/ccd/model/Address.java
+++ commons-civil/src/main/java/uk/gov/hmcts/reform/ccd/model/Address.java
@@ -1,15 +1,11 @@
 package uk.gov.hmcts.reform.ccd.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.reform.civil.utils.ObjectUtils;
-
-import static uk.gov.hmcts.reform.civil.utils.StringUtils.joinNonNull;
 
 @Data
 @Builder(toBuilder = true)
@@ -25,60 +21,4 @@ public class Address {
     private String county;
     private String country;
     private String postCode;
-
-    @JsonIgnore
-    public String firstNonNull() {
-        return ObjectUtils.firstNonNull(
-            addressLine1,
-            addressLine2,
-            addressLine3,
-            postTown,
-            joinNonNull(", ", county, country)
-        );
-    }
-
-    @JsonIgnore
-    public String secondNonNull() {
-        return ObjectUtils.secondNonNull(
-            addressLine1,
-            addressLine2,
-            addressLine3,
-            postTown,
-            joinNonNull(", ", county, country)
-        );
-    }
-
-    @JsonIgnore
-    public String thirdNonNull() {
-        return ObjectUtils.thirdNonNull(
-            addressLine1,
-            addressLine2,
-            addressLine3,
-            postTown,
-            joinNonNull(", ", county, country)
-        );
-    }
-
-    @JsonIgnore
-    public String fourthNonNull() {
-        return ObjectUtils.fourthNonNull(
-            addressLine1,
-            addressLine2,
-            addressLine3,
-            postTown,
-            joinNonNull(", ", county, country)
-        );
-    }
-
-    @JsonIgnore
-    public String fifthNonNull() {
-        return ObjectUtils.fifthNonNull(
-            addressLine1,
-            addressLine2,
-            addressLine3,
-            postTown,
-            joinNonNull(", ", county, country)
-        );
-    }
-
 }
