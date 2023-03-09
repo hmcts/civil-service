f98f7b20107aa98a9c6cb2ad01effe2f7d7a97f8
MMNycz <maciej.nycz@version1.com>
2023-03-09 16:08:00 +0000

    CIV-6295 fix spelling mistake

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/proceed/confirmation/PayImmidietelyConfText.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/proceed/confirmation/PayImmidietelyConfText.java
@@ -27,13 +27,13 @@ public class PayImmidietelyConfText implements RespondToResponseConfirmationText
         String formattedWhenBePaid = formatLocalDate(whenBePaid, DATE);
         return Optional.of(format(
             "They must make sure you have the money by %s. "
-                + "Any cheques or transfers should be clear in your account."
-                + "<p>You need to tell us if you've settled the claim, for example because the defenrant has paid you.</p>"
+                + "Any cheques or transfers should be clear in your account"
+                + "<p>You need to tell us if you’ve settled the claim, for example because the defendant has paid you.</p>"
                 + "<p>You can settle for less than the full claim amount.</p>"
-                + "<p><h3>If you haven't been paid.</h3></p>"
-                + "<p>If the defentand has not paid you, you can request a County Court Judgement"
+                + "<p><h3>If you haven’t been paid.</h3></p>"
+                + "<p>If the defendant has not paid you, you can request a County Court Judgement "
                 + "by completing the following form and sending it to the email address below.</p>"
-                + "<p><li><a href=\"%s\" target=\"_blank\">N225 </a>- Ask for judgement on a claim for a specified amount of money</li></ul></p>"
+                + "<p><li><a href=\"%s\" target=\"_blank\">N225 </a>- Ask for judgment on a claim for a specified amount of money</li></ul></p>"
                 + "<p>Email: <a href=\"mailto:contactocmc@justice.gov.uk\">contactocmc@justice.gov.uk</a></p>",
             formattedWhenBePaid,
             "https://formfinder.hmctsformfinder.justice.gov.uk/n225-eng.pdf"
