57a499997f06ed5a336a002a6f1378fcd7179c58
Douglas Rice <douglas.rice@hmcts.net>
2023-03-09 19:05:00 +0000

    testing on Demo

--- src/main/java/uk/gov/hmcts/reform/civil/launchdarkly/FeatureToggleService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/launchdarkly/FeatureToggleService.java
@@ -32,31 +32,23 @@ public class FeatureToggleService {
     }
 
     public boolean isRpaContinuousFeedEnabled() {
-        return internalClient.boolVariation("rpaContinuousFeed", createLDUser().build(), false);
+        return true;
     }
 
     public boolean isSpecRpaContinuousFeedEnabled() {
-        return internalClient.boolVariation(
-            "specified-rpa-continuous-feed",
-            createLDUser().build(),
-            false
-        );
+        return true;
     }
 
     public boolean isGlobalSearchEnabled() {
-        return internalClient.boolVariation(
-            "global-search-specified",
-            createLDUser().build(),
-            false
-        );
+        return true;
     }
 
     public boolean isSdoEnabled() {
-        return isFeatureEnabled("enableSDO");
+        return true;
     }
 
     public boolean isGeneralApplicationsEnabled() {
-        return internalClient.boolVariation("general_applications_enabled", createLDUser().build(), false);
+        return  false;
     }
 
     public LDUser.Builder createLDUser() {
@@ -66,44 +58,43 @@ public class FeatureToggleService {
     }
 
     public boolean isNoticeOfChangeEnabled() {
-        return internalClient.boolVariation("notice-of-change", createLDUser().build(), false);
+        return false;
     }
 
     public boolean isHearingAndListingSDOEnabled() {
-        return internalClient.boolVariation("hearing-and-listing-sdo", createLDUser().build(), false);
+        return false;
     }
 
     public boolean isHearingAndListingLegalRepEnabled() {
-        return internalClient.boolVariation("hearing-and-listing-legal-rep", createLDUser().build(), false);
+        return true;
     }
 
     public boolean isCourtLocationDynamicListEnabled() {
-        return internalClient.boolVariation("court-location-dynamic-list", createLDUser().build(), false);
+        return true;
     }
 
     public boolean isCaseFlagsEnabled() {
-        return internalClient.boolVariation("case-flags", createLDUser().build(), false);
+        return false;
     }
 
     public boolean isPinInPostEnabled() {
-        return internalClient.boolVariation("pin-in-post", createLDUser().build(), false);
+        return false;
     }
 
     public boolean isPbaV3Enabled() {
-        return internalClient.boolVariation("pba-version-3-ways-to-pay", createLDUser().build(), false);
+        return false;
     }
 
     public boolean isSDOEnabled() {
-        return internalClient.boolVariation("enableSDO", createLDUser().build(), false);
+        return true;
     }
 
     public boolean isCertificateOfServiceEnabled() {
-        return internalClient.boolVariation("isCertificateOfServiceEnabled",
-                                            createLDUser().build(), false);
+        return false;
     }
 
     public boolean isHmcEnabled() {
-        return internalClient.boolVariation("hmc", createLDUser().build(), false);
+        return false;
     }
 
     private void close() {
