266080958bb2680eff39436fba73cffc32e0c0df
jeejavr <jeejamol.vijayarajan@HMCTS.net>
2023-03-14 14:45:08 +0000

    CIV-6809: fixing predicate

--- src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/FlowPredicate.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/FlowPredicate.java
@@ -114,8 +114,9 @@ public class FlowPredicate {
     public static final Predicate<CaseData> claimSubmittedBothUnregisteredSolicitors = caseData ->
             caseData.getSubmittedDate() != null
                     && caseData.getRespondent1OrgRegistered() == NO
-                    && (caseData.getAddRespondent2() == YES && caseData.getRespondent2OrgRegistered() == NO)
-                    && caseData.getRespondent2SameLegalRepresentative() == NO;
+                    && (caseData.getAddRespondent2() == YES && caseData.getRespondent2OrgRegistered() == NO
+                    && (caseData.getRespondent2SameLegalRepresentative() == NO
+                    || caseData.getRespondent2SameLegalRepresentative() == null));
 
     public static final Predicate<CaseData> respondent1NotRepresented = caseData ->
         caseData.getIssueDate() != null && caseData.getRespondent1Represented() == NO;
