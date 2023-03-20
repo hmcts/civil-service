fd4f62043928811b8bddb770b8606db9cac9464f
indu2019 <indu.chaturvedi@hmcts.net>
2023-03-20 09:58:08 +0000

    fix: CIV-7442, saving the initial changes

--- src/main/java/uk/gov/hmcts/reform/civil/Application.java
+++ src/main/java/uk/gov/hmcts/reform/civil/Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = {"uk.gov.hmcts.ccd.sdk", "uk.gov.hmcts.reform.civil.model"}
+)
 @EnableCamundaRestClient
 @EnableFeignClients(basePackages = {
     "uk.gov.hmcts.reform.idam.client",
