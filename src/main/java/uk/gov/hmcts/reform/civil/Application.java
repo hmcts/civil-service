70acfc895906ac677f9e1a0bba0c52cdc17ab16c
indu2019 <indu.chaturvedi@hmcts.net>
2023-03-20 15:46:45 +0000

    fix: CIV-7442, saving the initial changes

--- src/main/java/uk/gov/hmcts/reform/civil/Application.java
+++ src/main/java/uk/gov/hmcts/reform/civil/Application.java
@@ -4,6 +4,9 @@ import org.camunda.bpm.extension.rest.EnableCamundaRestClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 
 @SpringBootApplication(
     scanBasePackages = {"uk.gov.hmcts.ccd.sdk", "uk.gov.hmcts.reform.civil.model"}
@@ -18,10 +21,32 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
     "uk.gov.hmcts.reform.cmc",
     "uk.gov.hmcts.reform.crd"
 })
+//@ComponentScan({"uk.gov.hmcts.reform.authorisation"})
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    /*@Bean
+    public AuthTokenGenerator generator() {
+        return new AuthTokenGenerator() {
+            @Override
+            public String generate() {
+                return null;
+            }
+        };
+    }*/
+
+    @Bean
+    public AuthTokenGenerator generator() {
+        return () -> null;
+    }
+
 }
