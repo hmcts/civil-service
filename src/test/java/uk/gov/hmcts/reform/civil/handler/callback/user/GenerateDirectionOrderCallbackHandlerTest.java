3c96e27cccd3b886a55ed5fc8c3e58a14ca76c36
Mark Drummond <mark.drummond@version1.com>
2023-03-21 15:41:43 +0000

    CIV-3107 docmosis template changes

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/GenerateDirectionOrderCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/GenerateDirectionOrderCallbackHandlerTest.java
@@ -6,17 +6,25 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.enums.caseprogression.FinalOrderSelection;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.documents.CaseDocument;
+import uk.gov.hmcts.reform.civil.model.documents.Document;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+import uk.gov.hmcts.reform.civil.service.docmosis.DocumentGeneratorService;
+import uk.gov.hmcts.reform.civil.service.docmosis.caseProgression.JudgeFinalOrderGenerator;
 
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.MID;
@@ -32,6 +40,12 @@ public class GenerateDirectionOrderCallbackHandlerTest extends BaseCallbackHandl
 
     @Autowired
     private GenerateDirectionOrderCallbackHandler handler;
+
+    @MockBean
+    private DocumentGeneratorService documentGeneratorService;
+
+    @MockBean
+    private JudgeFinalOrderGenerator judgeFinalOrderGenerator;
 
     private static final String ON_INITIATIVE_SELECTION_TEST = "As this order was made on the court's own initiative "
         + "any party affected by the order may apply to set aside, vary or stay the order. Any such application must "
@@ -71,6 +85,40 @@ public class GenerateDirectionOrderCallbackHandlerTest extends BaseCallbackHandl
         assertThat(response.getData()).extracting("orderWithoutNotice").extracting("withoutNoticeSelectionDate")
             .isEqualTo(LocalDate.now().toString());
 
+    }
+
+    @Test
+    void shouldGenerateFreeFormOrder_onMidEventCallback() {
+        // Given
+        CaseDocument freeFormOrder = CaseDocument.builder().documentLink(
+                Document.builder().documentUrl("url").build())
+            .build();
+        CaseData caseData = CaseDataBuilder.builder().atStateNotificationAcknowledged().build().toBuilder()
+            .finalOrderSelection(FinalOrderSelection.FREE_FORM_ORDER)
+            .build();
+        CallbackParams params = callbackParamsOf(caseData, MID, "generate-document-preview");
+        // When
+        when(judgeFinalOrderGenerator.generate(any(), any())).thenReturn(freeFormOrder);
+        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+        // Then
+        assertThat(response.getData()).extracting("freeFormOrderDocument").isNotNull();
+    }
+
+    @Test
+    void shouldGenerateAssistedOrder_onMidEventCallback() {
+        // Given
+        CaseDocument freeFormOrder = CaseDocument.builder().documentLink(
+                Document.builder().documentUrl("url").build())
+            .build();
+        CaseData caseData = CaseDataBuilder.builder().atStateNotificationAcknowledged().build().toBuilder()
+            .finalOrderSelection(FinalOrderSelection.ASSISTED_ORDER)
+            .build();
+        CallbackParams params = callbackParamsOf(caseData, MID, "generate-document-preview");
+        // When
+        when(judgeFinalOrderGenerator.generate(any(), any())).thenReturn(freeFormOrder);
+        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+        // Then
+        assertThat(response.getData()).extracting("assistedOrderDocument").isNotNull();
     }
 
     @Nested
