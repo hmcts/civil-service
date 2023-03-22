9236527d0619b8b3e09b6c9f20fd644a85067de6
indu2019 <indu.chaturvedi@hmcts.net>
2023-03-22 11:58:10 +0000

    saving it so i dont lose it

--- src/main/java/uk/gov/hmcts/reform/civil/stateflow/grammar/SetNext.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.civil.stateflow.grammar;
-
-/**
- * This specifies what can come after a SET clause.
- */
-public interface SetNext<S> extends TransitionTo<S>, State<S>, Subflow<S>, Build {
-
-}
