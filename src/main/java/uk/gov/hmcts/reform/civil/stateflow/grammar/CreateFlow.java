5983e0b401f02ff7ce339432c584365cb901239e
indu2019 <indu.chaturvedi@hmcts.net>
2023-03-22 11:18:31 +0000

    saving it so i dont lose it

--- src/main/java/uk/gov/hmcts/reform/civil/stateflow/grammar/CreateFlow.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.civil.stateflow.grammar;
-
-/**
- * Represents the CREATE_FLOW clause.
- */
-public interface CreateFlow<S> {
-
-    CreateFlowNext<S> createFlow();
-}
--- src/main/java/uk/gov/hmcts/reform/civil/stateflow/grammar/CreateFlowNext.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.civil.stateflow.grammar;
-
-/**
- * This specifies what can come after a CREATE_FLOW clause.
- */
-public interface CreateFlowNext<S>
-
-    extends Initial<S> {
-}
