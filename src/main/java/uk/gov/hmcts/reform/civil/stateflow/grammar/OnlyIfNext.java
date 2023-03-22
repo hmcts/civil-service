5656ac71fbf659daa7c4ff8dab302702817c9265
indu2019 <indu.chaturvedi@hmcts.net>
2023-03-22 11:38:26 +0000

    saving it so i dont lose it

--- src/main/java/uk/gov/hmcts/reform/civil/stateflow/grammar/OnlyIfNext.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.civil.stateflow.grammar;
-
-/**
- * This specifies what can come after a ONLY_IF clause.
- */
-public interface OnlyIfNext<S> extends TransitionTo<S>, State<S>, Subflow<S>, Set<S>, Build {
-
-}
-
