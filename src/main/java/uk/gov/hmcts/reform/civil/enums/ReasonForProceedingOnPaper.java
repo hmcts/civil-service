75cb24ba45412d32a5c05aee88cc7a9a3db03046
indu2019 <indu.chaturvedi@hmcts.net>
2023-03-22 11:41:59 +0000

    saving it so i dont lose it

--- src/main/java/uk/gov/hmcts/reform/civil/enums/ReasonForProceedingOnPaper.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.civil.enums;
-
-public enum ReasonForProceedingOnPaper {
-    APPLICATION,
-    JUDGEMENT_REQUEST,
-    DEFENDANT_DOES_NOT_CONSENT,
-    CASE_SETTLED,
-    OTHER;
-}
