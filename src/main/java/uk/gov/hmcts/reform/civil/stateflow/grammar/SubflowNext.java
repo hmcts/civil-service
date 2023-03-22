492c393add061d9f2bbbee505eade1c12d9a25e8
indu2019 <indu.chaturvedi@hmcts.net>
2023-03-22 11:49:26 +0000

    saving it so i dont lose it

--- src/main/java/uk/gov/hmcts/reform/civil/stateflow/grammar/SubflowNext.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.civil.stateflow.grammar;
-
-/**
- * This specifies what can come after a SUBFLOW clause.
- */
-public interface SubflowNext<S> extends State<S>, TransitionTo<S>, Subflow<S>, Build {
-
-}
