7106198d77e820eb9ac1bf93f2e815d223c2b3cc
indu2019 <indu.chaturvedi@hmcts.net>
2023-03-22 11:37:49 +0000

    saving it so i dont lose it

--- src/main/java/uk/gov/hmcts/reform/civil/enums/MultiPartyResponseTypeFlags.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.civil.enums;
-
-public enum MultiPartyResponseTypeFlags {
-    FULL_DEFENCE,
-    NOT_FULL_DEFENCE,
-    COUNTER_ADMIT_OR_ADMIT_PART,
-    FULL_ADMISSION,
-    PART_ADMISSION
-}
