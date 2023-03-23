dd1ee95ca5543d277b37afd36ef761f2f687f658
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-03-23 11:19:33 +0000

    CIV-6214 import document

--- src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/TranslatedDocument.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/TranslatedDocument.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.reform.civil.model.documents.Document;
+import uk.gov.hmcts.reform.civil.documentmanagement.model.Document;
 
 @Data
 @AllArgsConstructor
