ef62228782843e163e9ebf276745107e07c4c149
meulendijksm <max.meulendijks@hmcts.net>
2023-03-23 11:19:53 +0000

    CIV-6647 remove unused import

--- src/main/java/uk/gov/hmcts/reform/civil/controllers/testingsupport/TestingSupportController.java
+++ src/main/java/uk/gov/hmcts/reform/civil/controllers/testingsupport/TestingSupportController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.civil.event.HearingFeePaidEvent;
 import uk.gov.hmcts.reform.civil.handler.event.HearingFeePaidEventHandler;
 import uk.gov.hmcts.reform.civil.handler.tasks.ClaimDismissedHandler;
-import uk.gov.hmcts.reform.civil.handler.tasks.HearingFeeDueHandler;
 import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.model.BusinessProcess;
