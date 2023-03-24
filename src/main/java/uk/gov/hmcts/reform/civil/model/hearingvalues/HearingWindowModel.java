75274944b57774a93e3f23b20df5cc53364bcb4b
sankaviv1 <sankavi.mohanraj@version1.com>
2023-03-24 16:13:18 +0000

    remove unnecessary annotation

--- src/main/java/uk/gov/hmcts/reform/civil/model/hearingvalues/HearingWindowModel.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/hearingvalues/HearingWindowModel.java
@@ -1,14 +1,12 @@
 package uk.gov.hmcts.reform.civil.model.hearingvalues;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude
 @Builder(toBuilder = true)
 @Data
 @NoArgsConstructor
