8fa8ad4a3934908bee73801936391d29f52bb392
Astha Malviya <astha.malviya@HMCTS.NET>
2023-03-27 11:31:42 +0100

    fixing utc issue with more permanent fix

--- commons-civil/src/main/java/uk/gov/hmcts/reform/civil/helpers/LocalDateTimeHelper.java
+++ commons-civil/src/main/java/uk/gov/hmcts/reform/civil/helpers/LocalDateTimeHelper.java
@@ -17,11 +17,4 @@ public class LocalDateTimeHelper {
             .toLocalDateTime();
     }
 
-    public static LocalDateTime nowInLocalZone() {
-        return LocalDateTime.now(LOCAL_ZONE);
-    }
-
-    public static LocalDateTime nowInUTC() {
-        return LocalDateTime.now(UTC_ZONE);
-    }
 }
--- commons-civil/src/test/java/uk/gov/hmcts/reform/civil/helpers/LocalDateTimeHelperTest.java
+++ commons-civil/src/test/java/uk/gov/hmcts/reform/civil/helpers/LocalDateTimeHelperTest.java
@@ -1,30 +1,40 @@
 package uk.gov.hmcts.reform.civil.helpers;
 
-import org.junit.jupiter.api.Test;
-
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
+
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LocalDateTimeHelperTest {
 
     @Test
-    void utcTimeShouldBeBehindByAnHour() {
-        LocalDateTime utcTime = LocalDateTimeHelper.nowInUTC();
-        LocalDateTime localTime = LocalDateTimeHelper.nowInLocalZone();
+    void fromUTC_shouldReturnTimeInLocalZoneWinterTime() {
+        // Given
+        LocalDateTime farAwayLocalDateTime = LocalDateTime.of(2022,12,30,12,30,5)
+            .atZone(ZoneId.of("America/New_York")).toLocalDateTime();
 
-        long difference = ChronoUnit.HOURS.between(utcTime, localTime);
+        // when
+        LocalDateTime expectedDateTime = LocalDateTimeHelper.fromUTC(farAwayLocalDateTime);
 
-        assertThat(difference).isEqualTo(1);
+        // then
+        assertThat(farAwayLocalDateTime.atZone(LocalDateTimeHelper.UTC_ZONE).toLocalDateTime())
+            .isEqualTo(expectedDateTime.atZone(LocalDateTimeHelper.UTC_ZONE).toLocalDateTime());
     }
 
     @Test
-    void utcTimeToLocalTimeConversionShouldIncrementByAnHour() {
-        LocalDateTime utcTime = LocalDateTimeHelper.nowInUTC();
+    void fromUTC_shouldReturnTimeInLocalZoneSummerTime() {
+        // Given
+        LocalDateTime farAwayLocalDateTime = LocalDateTime.of(2022,6,30,12,30,5)
+            .atZone(ZoneId.of("America/New_York")).toLocalDateTime();
 
-        long difference = ChronoUnit.HOURS.between(utcTime, LocalDateTimeHelper.fromUTC(utcTime));
+        // when
+        LocalDateTime expectedDateTime = LocalDateTimeHelper.fromUTC(farAwayLocalDateTime);
 
-        assertThat(difference).isEqualTo(1);
+        // then
+        assertThat(farAwayLocalDateTime.atZone(LocalDateTimeHelper.UTC_ZONE).toLocalDateTime())
+            .isEqualTo(expectedDateTime.atZone(LocalDateTimeHelper.UTC_ZONE).minus(1, ChronoUnit.HOURS).toLocalDateTime());
     }
 }
