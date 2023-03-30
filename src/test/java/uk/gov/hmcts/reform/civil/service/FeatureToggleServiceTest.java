9c3e09ea3c9be99b180b8a6171115903c18cbaa8
Deepthi Doppalapudi <deepthi.doppalapudi@version1.com>
2023-03-30 13:57:15 +0100

    removed unused import

--- src/test/java/uk/gov/hmcts/reform/civil/service/FeatureToggleServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/FeatureToggleServiceTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.civil.service;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
