74d9be3a4b907d6b00e33d85558d1279b9993947
meulendijksm <max.meulendijks@hmcts.net>
2023-03-30 11:54:00 +0100

    CIV-2732 fix typo in Not Trial Ready body

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/TrialReadinessCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/TrialReadinessCallbackHandler.java
@@ -49,7 +49,7 @@ public class TrialReadinessCallbackHandler extends CallbackHandler {
             + "If there are any additional changes between now and the hearing date, "
             + "you will need to make an application as soon as possible and pay the appropriate fee.\n\n"
             + "The trial will go ahead on the specified date "
-            + "unless a judge makes an order changing the date of the hearing"
+            + "unless a judge makes an order changing the date of the hearing. "
             + "If you want the date of the hearing to be changed (or any other order to make the case ready for trial)"
             + "you will need to make an application to the court and pay the appropriate fee.";
     private final ObjectMapper objectMapper;
