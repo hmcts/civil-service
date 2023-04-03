39ac43f7d6215d0e465b61ddc84e1164d6da4ba4
neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
2023-04-03 10:57:01 +0100

    CIV-6238 - Welsh - Bilingual language preference  (#2485)
    * CIV-6238 Added defenant response lang changes.
    
    * CIV-6238 fixed indentation.
    
    * added unit test.
    
    * Fixed indentation.
    
    * Fixed review comments.
    
    * updated method name.
    
    * CIV-6238 Fixed review comments.
    
    * fixed unit test failure.
    
    * CIV-6238 fixed review comment.
    
    ---------
    
    Co-authored-by: nishm <mnbdba@gmail.com>

--- src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/RespondentLiPResponse.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/RespondentLiPResponse.java
@@ -5,11 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.civil.model.Address;
+import net.minidev.json.annotate.JsonIgnore;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.civil.enums.dq.Language;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+
+import java.util.Optional;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Component
 public class RespondentLiPResponse {
 
     private MediationLiP respondent1MediationLiPResponse;
@@ -18,4 +25,16 @@ public class RespondentLiPResponse {
     private Address respondent1LiPCorrespondenceAddress;
     private DQExtraDetailsLip respondent1DQExtraDetails;
     private HearingSupportLip respondent1DQHearingSupportLip;
+    private String respondent1ResponseLanguage;
+
+    @JsonIgnore
+    public boolean isRespondentResponseBilingual(CaseData caseData) {
+        boolean responseLanguage = Optional.ofNullable(caseData.getCaseDataLiP())
+            .map(CaseDataLiP::getRespondent1LiPResponse)
+            .map(RespondentLiPResponse::getRespondent1ResponseLanguage)
+            .filter(Language.BOTH.toString()::equals)
+            .isPresent();
+
+        return responseLanguage;
+    }
 }
