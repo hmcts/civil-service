25b65bb85ced41758e49c2dfe3b36e9197b770e6
suvarnathekkoot <suvarna.thekkoot@HMCTS.NET>
2023-04-03 14:41:29 +0100

    CIV-7689 Including Unavailability dates in Party and utils

--- src/main/java/uk/gov/hmcts/reform/civil/utils/UnavailabilityDatesUtils.java
+++ src/main/java/uk/gov/hmcts/reform/civil/utils/UnavailabilityDatesUtils.java
@@ -26,25 +26,25 @@ public class UnavailabilityDatesUtils {
 
         if (defendantResponse) {
             if (caseData.getRespondent1DQ() != null && caseData.getRespondent1DQ().getHearing() != null
-                && caseData.getRespondent1DQ().getHearing().getUnavailableDates() != null) {
-                List<Element<UnavailableDate>> respondent1DQUnavailableDates = caseData.getRespondent1DQ().getHearing().getUnavailableDates();
-                getUnavailableDatesForParty(unavailabilityDates, respondent1DQUnavailableDates);
+                && caseData.getApplicant1().getUnavailableDates() != null) {
+                List<Element<UnavailableDate>> respondent1UnavailableDates = caseData.getRespondent1().getUnavailableDates();
+                getUnavailableDatesForParty(unavailabilityDates, respondent1UnavailableDates);
             }
             if (caseData.getRespondent2DQ() != null && caseData.getRespondent2DQ().getHearing() != null
-                && caseData.getRespondent2DQ().getHearing().getUnavailableDates() != null) {
-                List<Element<UnavailableDate>> respondent2DQUnavailableDates = caseData.getRespondent2DQ().getHearing().getUnavailableDates();
-                getUnavailableDatesForParty(unavailabilityDates, respondent2DQUnavailableDates);
+                && caseData.getRespondent2().getUnavailableDates() != null) {
+                List<Element<UnavailableDate>> respondent2UnavailableDates = caseData.getRespondent2().getUnavailableDates();
+                getUnavailableDatesForParty(unavailabilityDates, respondent2UnavailableDates);
             }
         } else {
             if (caseData.getApplicant1DQ() != null && caseData.getApplicant1DQ().getHearing() != null
-                && caseData.getApplicant1DQ().getHearing().getUnavailableDates() != null) {
-                List<Element<UnavailableDate>> applicant1DQUnavailableDates = caseData.getApplicant1DQ().getHearing().getUnavailableDates();
-                getUnavailableDatesForParty(unavailabilityDates, applicant1DQUnavailableDates);
+                && caseData.getApplicant1().getUnavailableDates() != null) {
+                List<Element<UnavailableDate>> applicant1UnavailableDates = caseData.getApplicant1().getUnavailableDates();
+                getUnavailableDatesForParty(unavailabilityDates, applicant1UnavailableDates);
             }
             if (caseData.getApplicant2DQ() != null && caseData.getApplicant2DQ().getHearing() != null
-                && caseData.getApplicant2DQ().getHearing().getUnavailableDates() != null) {
-                List<Element<UnavailableDate>> applicant2DQUnavailableDates = caseData.getApplicant2DQ().getHearing().getUnavailableDates();
-                getUnavailableDatesForParty(unavailabilityDates, applicant2DQUnavailableDates);
+                && caseData.getApplicant2().getUnavailableDates() != null) {
+                List<Element<UnavailableDate>> applicant2UnavailableDates = caseData.getApplicant2().getUnavailableDates();
+                getUnavailableDatesForParty(unavailabilityDates, applicant2UnavailableDates);
             }
         }
         return unavailabilityDates;
