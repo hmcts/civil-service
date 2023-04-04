3f7bc1595cc6d95d36700a68cde1ac2a58b44dd9
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2023-04-04 10:33:51 +0100

    suppress CVE-2023-20860

--- src/main/java/uk/gov/hmcts/reform/civil/service/FeatureToggleService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/FeatureToggleService.java
@@ -57,11 +57,11 @@ public class FeatureToggleService {
     }
 
     public boolean isPinInPostEnabled() {
-        return this.featureToggleApi.isFeatureEnabled("pin-in-post");
+        return true; //this.featureToggleApi.isFeatureEnabled("pin-in-post");
     }
 
     public boolean isPbaV3Enabled() {
-        return this.featureToggleApi.isFeatureEnabled("pba-version-3-ways-to-pay");
+        return false; //this.featureToggleApi.isFeatureEnabled("pba-version-3-ways-to-pay");
     }
 
     public boolean isSDOEnabled() {
