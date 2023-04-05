49f77a626146534fda4ac401bde6daf9d28a9156
Nagaraju Deekonda <nagaraju.deekonda@version1.com>
2023-04-05 09:13:23 +0100

    CIV-6540 added dummy document

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToClaimCuiCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToClaimCuiCallbackHandlerTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.handler.callback.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
+import uk.gov.hmcts.reform.civil.documentmanagement.model.Document;
 import uk.gov.hmcts.reform.civil.enums.CaseState;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
@@ -26,6 +29,7 @@ import static org.mockito.BDDMockito.given;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.DEFENDANT_RESPONSE_CUI;
+import static uk.gov.hmcts.reform.civil.documentmanagement.model.DocumentType.RESPONDENT1_GENERATED_RESPONSE;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {
@@ -34,6 +38,8 @@ import static uk.gov.hmcts.reform.civil.callback.CaseEvent.DEFENDANT_RESPONSE_CU
 })
 class RespondToClaimCuiCallbackHandlerTest extends BaseCallbackHandlerTest {
 
+    CaseDocument dummyDocument = new CaseDocument(null, null, null, 0, null, null);
+
     @MockBean
     private Time time;
     @MockBean
@@ -41,6 +47,9 @@ class RespondToClaimCuiCallbackHandlerTest extends BaseCallbackHandlerTest {
 
     @Autowired
     private RespondToClaimCuiCallbackHandler handler;
+
+    @Autowired
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Nested
     class AboutToStartCallback {
@@ -90,6 +99,9 @@ class RespondToClaimCuiCallbackHandlerTest extends BaseCallbackHandlerTest {
                 .extracting("status")
                 .isEqualTo("READY");
             assertThat(response.getState()).isEqualTo(CaseState.AWAITING_APPLICANT_INTENTION.name());
+            CaseData updatedData = mapper.convertValue(response.getData(), CaseData.class);
+            assertThat(updatedData.getSystemGeneratedCaseDocuments()).hasSize(2);
+            assertThat(updatedData.getSystemGeneratedCaseDocuments().get(1).getValue()).isEqualTo(dummyDocument);
         }
     }
 }
