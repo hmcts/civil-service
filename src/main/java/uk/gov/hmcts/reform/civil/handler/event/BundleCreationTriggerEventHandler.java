e2422f83b32037f1b5e839a2adc6efc971d1482d
bhagyashreesharma90 <bhagyashree.sharma@hmcts.net>
2023-04-05 14:50:47 +0100

    CIV-7646 add loggers

--- src/main/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandler.java
@@ -48,25 +48,30 @@ public class BundleCreationTriggerEventHandler {
      */
     @EventListener
     public void sendBundleCreationTrigger(BundleCreationTriggerEvent event) {
-        boolean isBundleCreated = getIsBundleCreatedForHearingDate(event.getCaseId());
-        if (isBundleCreated) {
-            log.info("Trial Bundle already exists for case {}", event.getCaseId());
-            return;
+        try {
+            boolean isBundleCreated = getIsBundleCreatedForHearingDate(event.getCaseId());
+            if (isBundleCreated) {
+                log.info("Trial Bundle already exists for case {}", event.getCaseId());
+                return;
+            }
+
+            BundleCreateResponse bundleCreateResponse = bundleCreationService.createBundle(event);
+
+            String caseId = event.getCaseId().toString();
+            StartEventResponse startEventResponse = coreCaseDataService.startUpdate(caseId, CREATE_BUNDLE);
+            CaseData caseData = caseDetailsConverter.toCaseData(startEventResponse.getCaseDetails().getData());
+
+            List<IdValue<Bundle>> caseBundles = caseData.getCaseBundles();
+            caseBundles.addAll(bundleCreateResponse.getData().getCaseBundles()
+                                   .stream().map(bundle -> prepareNewBundle(bundle, caseData)
+                ).collect(Collectors.toList()));
+            CaseDataContent caseContent = prepareCaseContent(caseBundles, startEventResponse);
+            coreCaseDataService.submitUpdate(caseId, caseContent);
+            coreCaseDataService.triggerEvent(event.getCaseId(), BUNDLE_CREATION_NOTIFICATION);
+        } catch (Exception e) {
+            log.error("Error while creating bundle for case {}", event.getCaseId());
+            throw e;
         }
-
-        BundleCreateResponse bundleCreateResponse = bundleCreationService.createBundle(event);
-
-        String caseId = event.getCaseId().toString();
-        StartEventResponse startEventResponse = coreCaseDataService.startUpdate(caseId, CREATE_BUNDLE);
-        CaseData caseData = caseDetailsConverter.toCaseData(startEventResponse.getCaseDetails().getData());
-
-        List<IdValue<Bundle>> caseBundles = caseData.getCaseBundles();
-        caseBundles.addAll(bundleCreateResponse.getData().getCaseBundles()
-                               .stream().map(bundle -> prepareNewBundle(bundle, caseData)
-            ).collect(Collectors.toList()));
-        CaseDataContent caseContent = prepareCaseContent(caseBundles, startEventResponse);
-        coreCaseDataService.submitUpdate(caseId, caseContent);
-        coreCaseDataService.triggerEvent(event.getCaseId(), BUNDLE_CREATION_NOTIFICATION);
     }
 
     boolean getIsBundleCreatedForHearingDate(Long caseId) {
