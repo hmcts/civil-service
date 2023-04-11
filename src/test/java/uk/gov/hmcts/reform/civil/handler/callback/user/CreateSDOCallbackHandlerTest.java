539567bd8789b90296754f6cb11bbed1dff209a2
Mark Drummond <mark.drummond@version1.com>
2023-04-11 15:58:37 +0100

    CIV-5486 checkstyle

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateSDOCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateSDOCallbackHandlerTest.java
@@ -1197,7 +1197,9 @@ public class CreateSDOCallbackHandlerTest extends BaseCallbackHandlerTest {
             when(sdoGeneratorService.generate(any(), any())).thenReturn(order);
             var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
             CaseData updatedData = objectMapper.convertValue(response.getData(), CaseData.class);
-            assertThat(updatedData.getSdoOrderDocument().getDocumentLink().getCategoryID().equals("sdo"));     }
+            assertThat(updatedData.getSdoOrderDocument().getDocumentLink().getCategoryID().equals("sdo"));
+        }
+
     }
 
     @Nested
