07706fcf34d0a3f345902fe587bc93ce2adb1fbc
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-04-13 10:03:52 +0100

    CIV-7140 add unit test for claimant agreed to free mediation method

--- src/test/java/uk/gov/hmcts/reform/civil/model/CaseDataTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/model/CaseDataTest.java
@@ -2,7 +2,12 @@ package uk.gov.hmcts.reform.civil.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.civil.enums.MediationDecision;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.model.citizenui.CaseDataLiP;
+import uk.gov.hmcts.reform.civil.model.citizenui.ClaimantMediationLip;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CaseDataTest {
 
@@ -20,5 +25,47 @@ public class CaseDataTest {
             .applicant1ProceedWithClaimSpec2v1(YesOrNo.YES)
             .build();
         Assertions.assertEquals(YesOrNo.YES, caseData.getApplicant1ProceedsWithClaimSpec());
+    }
+
+    @Test
+    void givenApplicantAgreedToMediation_whenHasClaimantAgreedToFreeMediation_thenTrue(){
+        //Given
+        CaseData caseData = CaseData.builder()
+            .caseDataLiP(CaseDataLiP.builder()
+                             .applicant1ClaimMediationSpecRequiredLip(ClaimantMediationLip.builder()
+                                                                          .hasAgreedFreeMediation(MediationDecision.Yes)
+                                                                          .build())
+                             .build())
+            .build();
+        //When
+        boolean result = caseData.hasClaimantAgreedToFreeMediation();
+        //Then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void givenNoDataForAgreedToMediation_whenHasClaimantAgreedToFeeMediation_thenFalse(){
+        //Given
+        CaseData caseData = CaseData.builder().build();
+        //When
+        boolean result = caseData.hasClaimantAgreedToFreeMediation();
+        //Then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void givenApplicantDidNotAgreeToFreeMediation_whenHasClaimantAgreedToFeeMediation_thenFalse(){
+        //Given
+        CaseData caseData = CaseData.builder()
+            .caseDataLiP(CaseDataLiP.builder()
+                             .applicant1ClaimMediationSpecRequiredLip(ClaimantMediationLip.builder()
+                                                                          .hasAgreedFreeMediation(MediationDecision.No)
+                                                                          .build())
+                             .build())
+            .build();
+        //When
+        boolean result = caseData.hasClaimantAgreedToFreeMediation();
+        //Then
+        assertThat(result).isFalse();
     }
 }
