f39115f937125bde55992f9139fe555511e0e454
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-04-17 08:52:21 +0100

    CIV-7140 add correct check for agreeing to free mediation

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/proceed/confirmation/AdmitProceedConfHeader.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/proceed/confirmation/AdmitProceedConfHeader.java
@@ -25,8 +25,7 @@ public class AdmitProceedConfHeader implements RespondToResponseConfirmationHead
         if (caseData.getApplicant1ProceedsWithClaimSpec() == null
             || YesOrNo.NO.equals(caseData.getApplicant1ProceedsWithClaimSpec())
             || !ADMISSION.contains(caseData.getRespondent1ClaimResponseTypeForSpec())
-            || (caseData.getApplicant1ClaimMediationSpecRequired() != null
-            && YesOrNo.YES.equals(caseData.getApplicant1ClaimMediationSpecRequired().getHasAgreedFreeMediation()))) {
+            || (caseData.hasClaimantAgreedToFreeMediation())) {
             return Optional.empty();
         }
         String claimNumber = caseData.getLegacyCaseReference();
