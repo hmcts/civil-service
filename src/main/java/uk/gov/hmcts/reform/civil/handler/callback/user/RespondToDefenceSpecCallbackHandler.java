e961f3f039ab8918515dac94be909720d82a58c3
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2023-04-25 10:18:03 +0100

    CIV-7137 Update From Merging

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToDefenceSpecCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToDefenceSpecCallbackHandler.java
@@ -336,7 +336,7 @@ public class RespondToDefenceSpecCallbackHandler extends CallbackHandler
             && isOneVOne(caseData)) {
             if (caseData.hasClaimantAgreedToFreeMediation()) {
                 response.state(CaseState.IN_MEDIATION.name());
-            } else if (caseData.isRejectDefendantPaymentPlanNo()) {
+            } else if (caseData.hasApplicantRejectedRepaymentPlan()) {
                 response.state(CaseState.PROCEEDS_IN_HERITAGE_SYSTEM.name());
             } else if (caseData.isPartAdmitClaimSettled()) {
                 response.state(CaseState.CASE_SETTLED.name());
