b488ce9254f602692d68884e03a78f56542f1cfc
sankaviv1 <sankavi.mohanraj@version1.com>
2023-04-25 11:53:41 +0100

    add annotation

--- src/main/java/uk/gov/hmcts/reform/civil/model/caseflags/Flags.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/caseflags/Flags.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.model.caseflags;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
 import uk.gov.hmcts.reform.civil.model.common.Element;
@@ -8,10 +9,10 @@ import java.util.List;
 
 @Data
 @SuperBuilder(toBuilder = true)
+@AllArgsConstructor
 public class Flags {
 
     private final String partyName;
     private final String roleOnCase;
     private final List<Element<FlagDetail>> details;
-
 }
