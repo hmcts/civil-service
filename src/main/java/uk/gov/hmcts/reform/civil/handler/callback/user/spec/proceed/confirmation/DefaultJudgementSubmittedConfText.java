d9783021cbc0da9c3011b59ba12c21cc7d8a70a3
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-05-03 15:10:02 +0100

    CIV-7357 add default judgement text

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/proceed/confirmation/DefaultJudgementSubmittedConfText.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.civil.handler.callback.user.spec.proceed.confirmation;
+
+import uk.gov.hmcts.reform.civil.handler.callback.user.spec.RespondToResponseConfirmationTextGenerator;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+
+import java.util.Optional;
+
+import static java.lang.String.format;
+
+public class DefaultJudgementSubmittedConfText implements RespondToResponseConfirmationTextGenerator {
+    @Override
+    public Optional<String> generateTextFor(CaseData caseData) {
+        if(!caseData.hasClaimantAgreedToFreeMediation() && caseData.hasApplicantRejectedRepaymentPlan()) {
+            return Optional.of(format(
+                "<br />A request for default judgement has been sent to the court for review.<br>" +
+                    "<br>The claim will now progress offline<br>"
+            ));
+        }
+        return Optional.empty();
+    }
+}
