d9237d1ed5b48bfaf0215cba6f66f006b2a48dee
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-05-05 17:53:57 +0100

    CIV-6299 upload translated document and add it to system generated documents. change status to awaitng applicant intention

--- commons-civil/src/main/java/uk/gov/hmcts/reform/civil/documentmanagement/model/CaseDocument.java
+++ commons-civil/src/main/java/uk/gov/hmcts/reform/civil/documentmanagement/model/CaseDocument.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.documentmanagement.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,4 +18,14 @@ public class CaseDocument {
     private final long documentSize;
     private final LocalDateTime createdDatetime;
     private final String createdBy;
+
+    @JsonIgnore
+    public static CaseDocument toCaseDocument(Document document, DocumentType documentType){
+        return CaseDocument.builder()
+            .documentLink(document)
+            .documentName(document.documentFileName)
+            .documentType(documentType)
+            .createdDatetime(LocalDateTime.now())
+            .build();
+    }
 }
--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/UploadTranslatedDocumentHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/UploadTranslatedDocumentHandler.java
@@ -9,18 +9,29 @@ import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.citizenui.CaseDataLiP;
+import uk.gov.hmcts.reform.civil.model.citizenui.TranslatedDocument;
+import uk.gov.hmcts.reform.civil.model.common.Element;
+import uk.gov.hmcts.reform.civil.service.SystemGeneratedDocumentService;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UPLOAD_TRANSLATED_DOCUMENT;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.AWAITING_APPLICANT_INTENTION;
+import static uk.gov.hmcts.reform.civil.utils.ElementUtils.element;
 
 @Service
 @RequiredArgsConstructor
 public class UploadTranslatedDocumentHandler extends CallbackHandler {
 
-    private static final List<CaseEvent> EVENTS =  List.of(UPLOAD_TRANSLATED_DOCUMENT);
+    private static final List<CaseEvent> EVENTS = List.of(UPLOAD_TRANSLATED_DOCUMENT);
+    private final SystemGeneratedDocumentService systemGeneratedDocumentService;
     private final ObjectMapper objectMapper;
 
     private Map<String, Callback> callbackMap = Map.of(callbackKey(ABOUT_TO_SUBMIT), this::uploadDocument);
@@ -36,8 +47,28 @@ public class UploadTranslatedDocumentHandler extends CallbackHandler {
     }
 
     private CallbackResponse uploadDocument(CallbackParams callbackParams) {
+        List<Element<CaseDocument>> updatedDocumentList = updateSystemGeneratedDocumentsWithTranslationDocument(
+            callbackParams);
+        CaseData updatedCaseData = callbackParams.getCaseData().builder().systemGeneratedCaseDocuments(
+            updatedDocumentList).build();
         return AboutToStartOrSubmitCallbackResponse.builder()
-            .data(callbackParams.getCaseData().toMap(objectMapper))
+            .state(AWAITING_APPLICANT_INTENTION.name())
+            .data(updatedCaseData.toMap(objectMapper))
             .build();
+    }
+
+    private List<Element<CaseDocument>> updateSystemGeneratedDocumentsWithTranslationDocument(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        TranslatedDocument translatedDocument = Optional.ofNullable(caseData.getCaseDataLiP())
+            .map(CaseDataLiP::getTranslatedDocument)
+            .orElse(null);
+        if (translatedDocument != null) {
+            return systemGeneratedDocumentService.getSystemGeneratedDocumentsWithAddedDocument(
+                translatedDocument.getFile(),
+                translatedDocument.getCorrespondingDocumentType(),
+                callbackParams
+            );
+        }
+        return caseData.getSystemGeneratedCaseDocuments();
     }
 }
