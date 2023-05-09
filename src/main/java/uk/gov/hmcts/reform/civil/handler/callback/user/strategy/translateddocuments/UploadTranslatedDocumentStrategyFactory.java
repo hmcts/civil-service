061c3805d20d32225dfdc0703253fb634bf9588e
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-05-09 15:39:05 +0100

    CIV-6299 format

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/strategy/translateddocuments/UploadTranslatedDocumentStrategyFactory.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/strategy/translateddocuments/UploadTranslatedDocumentStrategyFactory.java
@@ -7,6 +7,7 @@ import uk.gov.hmcts.reform.civil.callback.CallbackVersion;
 @Component
 @AllArgsConstructor
 public class UploadTranslatedDocumentStrategyFactory {
+
     private UploadTranslatedDocumentDefaultStrategy uploadTranslatedDocumentDefaultStrategy;
     private UploadTranslatedDocumentV1Strategy uploadTranslatedDocumentV1Strategy;
 
