106f20a9802b42ecbf55f5b03a449fd72844a04e
Pijian Liao <Pijian.Liao@HMCTS.NET>
2023-05-10 09:56:32 +0100

    CIV-8316 fix documents update from ga

--- src/main/java/uk/gov/hmcts/reform/civil/service/InitiateGeneralApplicationServiceHelper.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/InitiateGeneralApplicationServiceHelper.java
@@ -18,7 +18,6 @@ import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
