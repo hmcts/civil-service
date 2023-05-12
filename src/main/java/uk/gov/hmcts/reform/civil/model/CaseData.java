e7304c6cfb4a776a2bb6b3fa1f85bfb71188568f
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2023-05-12 15:37:49 +0100

    CIV-7155 Remove Unused Import

--- src/main/java/uk/gov/hmcts/reform/civil/model/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/CaseData.java
@@ -140,7 +140,6 @@ import static java.util.Objects.nonNull;
 import static uk.gov.hmcts.reform.civil.enums.AllocatedTrack.FAST_CLAIM;
 import static uk.gov.hmcts.reform.civil.enums.AllocatedTrack.SMALL_CLAIM;
 import static uk.gov.hmcts.reform.civil.enums.BusinessProcessStatus.FINISHED;
-import static uk.gov.hmcts.reform.civil.enums.MultiPartyScenario.ONE_V_ONE;
 import static uk.gov.hmcts.reform.civil.enums.MultiPartyScenario.TWO_V_ONE;
 import static uk.gov.hmcts.reform.civil.enums.MultiPartyScenario.isOneVOne;
 import static uk.gov.hmcts.reform.civil.enums.MultiPartyScenario.isOneVTwoTwoLegalRep;
