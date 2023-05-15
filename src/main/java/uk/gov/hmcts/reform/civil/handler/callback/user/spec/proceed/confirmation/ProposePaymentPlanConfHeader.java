5ce9ebd317d90d76bb962804f022c845ae10b75e
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-05-15 13:07:13 +0100

    CIV-7790 confirmation default judgement header (#2672)
    * CIV-7357 add default judgement header
    
    * CIV-7357 add default judgement text
    
    * CIV-7357 add default judgement text and header components
    
    * CIV-7790 revert changes for feature toggle
    
    * CIV-7790 change header on default judgment handler and add unit tests
    
    * CIV-7790 add on paper text
    
    * CIV-7790 supress dependecy volnarabilities
    
    * CIV-7790 remove unused imports
    
    * CIV-7790 formatting
    
    * CIV-7790 fix compilation error due to merge
    
    ---------
    
    Co-authored-by: Raja Mani <rajakm@gmail.com>

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/proceed/confirmation/ProposePaymentPlanConfHeader.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/proceed/confirmation/ProposePaymentPlanConfHeader.java
@@ -13,7 +13,7 @@ public class ProposePaymentPlanConfHeader implements RespondToResponseConfirmati
 
     @Override
     public Optional<String> generateTextFor(CaseData caseData) {
-        if (caseData.isRejectDefendantPaymentPlanYes() || caseData.hasClaimantAgreedToFreeMediation()) {
+        if (caseData.isDefendantPaymentPlanYes() || caseData.hasClaimantAgreedToFreeMediation()) {
             return Optional.empty();
         }
 
