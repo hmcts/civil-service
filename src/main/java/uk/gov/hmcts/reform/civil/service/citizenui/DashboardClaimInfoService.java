d2b115fa7314ea25bc864e0a1999eedf69548abd
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2023-05-17 12:09:23 +0100

    remove unusded imports

--- src/main/java/uk/gov/hmcts/reform/civil/service/citizenui/DashboardClaimInfoService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/citizenui/DashboardClaimInfoService.java
@@ -16,8 +16,6 @@ import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 import uk.gov.hmcts.reform.civil.service.claimstore.ClaimStoreService;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
