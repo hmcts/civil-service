89483cdf18ee019926336589982226493fa5fe0e
skhajuria <sankhajuria@gmail.com>
2023-05-23 08:59:04 +0100

    CIV-8604 Change the date format to fix docmosis issue

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/hearing/HearingFormGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/hearing/HearingFormGenerator.java
@@ -95,7 +95,7 @@ public class HearingFormGenerator implements TemplateDataGenerator<HearingForm> 
         if (isNull(date)) {
             return null;
         }
-        return DateFormatHelper.formatLocalDate(date, "dd/MMM/yyyy");
+        return DateFormatHelper.formatLocalDate(date, "dd/MM/yyyy");
     }
 
     private boolean checkReference(CaseData caseData) {
