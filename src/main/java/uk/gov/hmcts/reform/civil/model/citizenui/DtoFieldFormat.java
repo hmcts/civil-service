fab310ba7405bfce62f4ee171bcdca927227f54e
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2023-05-26 17:48:55 +0100

    remove unused date format

--- src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/DtoFieldFormat.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/DtoFieldFormat.java
@@ -4,7 +4,6 @@ public class DtoFieldFormat {
 
     public static final String DATE_FORMAT = "yyyy-MM-dd";
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
-    public static final String DATE_TIME_FORMAT_CMC = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS";
 
     private DtoFieldFormat() {
 
