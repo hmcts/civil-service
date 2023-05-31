e9885ff08e2e41ccddde77a1f1c417f31711e820
bhagyashreesharma90 <bhagyashree.sharma@hmcts.net>
2023-05-31 11:37:11 +0100

    CIV-000 revert changes

--- src/main/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandler.java
@@ -57,16 +57,16 @@ public class BundleCreationTriggerEventHandler {
         BundleCreateResponse bundleCreateResponse = bundleCreationService.createBundle(event);
 
         String caseId = event.getCaseId().toString();
-            StartEventResponse startEventResponse = coreCaseDataService.startUpdate(caseId, CREATE_BUNDLE);
-            CaseData caseData = caseDetailsConverter.toCaseData(startEventResponse.getCaseDetails().getData());
+        StartEventResponse startEventResponse = coreCaseDataService.startUpdate(caseId, CREATE_BUNDLE);
+        CaseData caseData = caseDetailsConverter.toCaseData(startEventResponse.getCaseDetails().getData());
 
-            List<IdValue<Bundle>> caseBundles = caseData.getCaseBundles();
-            caseBundles.addAll(bundleCreateResponse.getData().getCaseBundles()
-                                   .stream().map(bundle -> prepareNewBundle(bundle, caseData)
-                ).collect(Collectors.toList()));
-            CaseDataContent caseContent = prepareCaseContent(caseBundles, startEventResponse);
-            coreCaseDataService.submitUpdate(caseId, caseContent);
-            coreCaseDataService.triggerEvent(event.getCaseId(), BUNDLE_CREATION_NOTIFICATION);
+        List<IdValue<Bundle>> caseBundles = caseData.getCaseBundles();
+        caseBundles.addAll(bundleCreateResponse.getData().getCaseBundles()
+                               .stream().map(bundle -> prepareNewBundle(bundle, caseData)
+            ).collect(Collectors.toList()));
+        CaseDataContent caseContent = prepareCaseContent(caseBundles, startEventResponse);
+        coreCaseDataService.submitUpdate(caseId, caseContent);
+        coreCaseDataService.triggerEvent(event.getCaseId(), BUNDLE_CREATION_NOTIFICATION);
     }
 
     boolean getIsBundleCreatedForHearingDate(Long caseId) {
