ba74a44b3d228f28356c5a27e804a9d129f17f8a
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-06-01 12:18:28 +0100

    CIV-8782 Flow state added to prevent unexpected flow state error

--- src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/CaseEventTaskHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/CaseEventTaskHandler.java
@@ -125,7 +125,7 @@ public class CaseEventTaskHandler implements BaseExternalTaskHandler {
                     "RPA Reason: Only one of the defendants is notified.";
                 case TAKEN_OFFLINE_BY_STAFF -> "RPA Reason: Case taken offline by staff.";
                 case CLAIM_DETAILS_NOTIFIED, NOTIFICATION_ACKNOWLEDGED_TIME_EXTENSION,
-                    CLAIM_DETAILS_NOTIFIED_TIME_EXTENSION ->
+                    CLAIM_DETAILS_NOTIFIED_TIME_EXTENSION, NOTIFICATION_ACKNOWLEDGED ->
                     "RPA Reason: Not suitable for SDO.";
                 default -> throw new IllegalStateException("Unexpected flow state " + flowState.fullName());
             };
