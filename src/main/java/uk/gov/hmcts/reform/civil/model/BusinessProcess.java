bc657b9a74f070f339a748b65c514418aba381a7
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2023-06-02 13:52:33 +0100

    CIV-TASK Merge master

--- src/main/java/uk/gov/hmcts/reform/civil/model/BusinessProcess.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/BusinessProcess.java
@@ -1,10 +1,7 @@
 package uk.gov.hmcts.reform.civil.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Builder;
 import lombok.Data;
