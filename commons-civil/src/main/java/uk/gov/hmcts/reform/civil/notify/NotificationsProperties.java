d7502135358b9d6f8a3bcf269e1724dd13ebd470
hmcts-version1-domini <domini.mattos@version1.com>
2023-06-05 13:13:14 +0200

    Add Welsh Template and Unit Test

--- commons-civil/src/main/java/uk/gov/hmcts/reform/civil/notify/NotificationsProperties.java
+++ commons-civil/src/main/java/uk/gov/hmcts/reform/civil/notify/NotificationsProperties.java
@@ -287,4 +287,7 @@ public class NotificationsProperties {
     @NotEmpty
     private String notifyRespondentLiPMediationSuccessfulTemplate;
 
+    @NotEmpty
+    private String notifyRespondentLiPMediationSuccessfulTemplateWelsh;
+
 }
