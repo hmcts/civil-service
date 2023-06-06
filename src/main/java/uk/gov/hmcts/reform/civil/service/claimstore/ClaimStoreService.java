4e75d42a1c513bd4f6251e49ef973c582f47729b
Jeswanth Reddy <134285996+jeswanth-hmcts@users.noreply.github.com>
2023-06-06 13:24:14 +0100

    code for Part Admit Accept

--- src/main/java/uk/gov/hmcts/reform/civil/service/claimstore/ClaimStoreService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/claimstore/ClaimStoreService.java
@@ -9,7 +9,6 @@ import uk.gov.hmcts.reform.cmc.client.ClaimStoreApi;
 import uk.gov.hmcts.reform.cmc.model.CmcClaim;
 import uk.gov.hmcts.reform.cmc.model.Response;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
