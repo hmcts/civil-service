1272224b51b49b3797d1e62b750c526e7e0ad17b
dtortolaV1 <dario.tortolanavarro@hmcts.net>
2023-06-07 11:56:31 +0100

    CIV-6804 filtering cases so that the same event is not triggered twice for the same case

--- src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/PollingEventEmitterHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/PollingEventEmitterHandler.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.civil.handler.tasks;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
 import org.camunda.bpm.client.task.ExternalTask;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,9 @@ import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.service.EventEmitterService;
 import uk.gov.hmcts.reform.civil.service.search.CaseReadyBusinessProcessSearchService;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static java.lang.String.format;
 
@@ -28,13 +31,27 @@ public class PollingEventEmitterHandler implements BaseExternalTaskHandler {
     public void handleTask(ExternalTask externalTask) {
         List<CaseDetails> cases = caseSearchService.getCases();
         log.info("Job '{}' found {} case(s)", externalTask.getTopicName(), cases.size());
+        Set<Pair<Long, String>> caseEvent = new HashSet<>();
         cases.stream()
             .map(caseDetailsConverter::toCaseData)
             .forEach(mappedCase -> {
-                log.info(format("Emitting %s camunda event for case through poller: %d",
-                                mappedCase.getBusinessProcess().getCamundaEvent(),
-                                mappedCase.getCcdCaseReference()));
-                eventEmitterService.emitBusinessProcessCamundaEvent(mappedCase, true);
+                if (caseEvent.add(Pair.of(
+                    mappedCase.getCcdCaseReference(),
+                    mappedCase.getBusinessProcess().getCamundaEvent()
+                ))) {
+                    log.info(format(
+                        "Emitting %s camunda event for case through poller: %d",
+                        mappedCase.getBusinessProcess().getCamundaEvent(),
+                        mappedCase.getCcdCaseReference()
+                    ));
+                    eventEmitterService.emitBusinessProcessCamundaEvent(mappedCase, true);
+                } else {
+                    log.info(
+                        "Camunda {} event stopped for case {} because it is already in the list",
+                        mappedCase.getBusinessProcess().getCamundaEvent(),
+                        mappedCase.getCcdCaseReference()
+                    );
+                }
             });
     }
 
