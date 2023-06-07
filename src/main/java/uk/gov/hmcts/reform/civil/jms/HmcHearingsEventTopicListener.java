e6cd52dd48befe3e7b22a2e19d95b8c425c231ec
sankaviv1 <sankavi.mohanraj@version1.com>
2023-06-07 14:23:32 +0100

    update config

--- src/main/java/uk/gov/hmcts/reform/civil/jms/HmcHearingsEventTopicListener.java
+++ src/main/java/uk/gov/hmcts/reform/civil/jms/HmcHearingsEventTopicListener.java
@@ -26,8 +26,8 @@ public class HmcHearingsEventTopicListener {
     private final CoreCaseDataService coreCaseDataService;
 
     @JmsListener(
-        destination = "hmc-to-cft-demo",
-        subscription = "hmc-to-civil-subscription-demo",
+        destination = "${azure.service-bus.hmc-to-hearings-api.topicName}",
+        subscription = "${azure.service-bus.hmc-to-hearings-api.subscriptionName}",
         containerFactory = "hmcHearingsEventTopicContainerFactory")
     public void onMessage(JmsBytesMessage message) throws JMSException, HmcTopicEventProcessingException {
 
