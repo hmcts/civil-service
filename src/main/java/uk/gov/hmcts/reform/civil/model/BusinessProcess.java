7a881c2a47e020d7c60aee6c35c9992c9d56e411
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2023-06-08 09:09:28 +0100

    CIV-5365 Test add variables null values

--- src/main/java/uk/gov/hmcts/reform/civil/model/BusinessProcess.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/BusinessProcess.java
@@ -41,6 +41,7 @@ public class BusinessProcess {
     public BusinessProcess start() {
         this.status = BusinessProcessStatus.STARTED;
         this.activityId = null;
+        this.readyOn = null;
         return this;
     }
 
@@ -61,6 +62,7 @@ public class BusinessProcess {
         this.activityId = null;
         this.processInstanceId = null;
         this.status = BusinessProcessStatus.FINISHED;
+        this.readyOn = null;
         return this;
     }
 
