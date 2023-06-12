4d2ea62b01d22041dc7c37a8c16d7f1a00976304
sankaviv1 <sankavi.mohanraj@version1.com>
2023-06-12 11:35:55 +0100

    revert previous commit

--- src/main/java/uk/gov/hmcts/reform/civil/config/HearingsJmsConfig.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/HearingsJmsConfig.java
@@ -54,7 +54,6 @@ public class HearingsJmsConfig {
         factory.setSubscriptionDurable(Boolean.TRUE);
         factory.setSessionTransacted(Boolean.TRUE);
         factory.setSessionAcknowledgeMode(Session.SESSION_TRANSACTED);
-        factory.setConcurrency("5-10");
 
         configurer.configure(factory, hmcHearingJmsConnectionFactory);
         return factory;
