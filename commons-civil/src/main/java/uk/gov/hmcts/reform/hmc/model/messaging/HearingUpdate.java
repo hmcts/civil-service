11d8ebb4e265caff42e9efe454f8e6b0415916bd
sankaviv1 <sankavi.mohanraj@version1.com>
2023-06-13 15:25:42 +0100

    use azure library

---
+++ commons-civil/src/main/java/uk/gov/hmcts/reform/hmc/model/messaging/HearingUpdate.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.reform.hmc.model.messaging;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.reform.hmc.model.hearing.ListAssistCaseStatus;
+import uk.gov.hmcts.reform.hmc.model.hearing.ListingStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HearingUpdate {
+
+    private LocalDateTime hearingResponseReceivedDateTime;
+
+    private LocalDateTime hearingEventBroadcastDateTime;
+
+    @JsonProperty("HMCStatus")
+    private HmcStatus hmcStatus;
+
+    @JsonProperty("hearingListingStatus")
+    private ListingStatus listingStatus;
+
+    private LocalDateTime nextHearingDate;
+
+    @JsonProperty("ListAssistCaseStatus")
+    private ListAssistCaseStatus listAssistCaseStatus;
+
+    private String listAssistSessionID;
+
+    @JsonProperty("hearingVenueId")
+    private String hearingEpimsId;
+
+    private String hearingRoomId;
+
+    private String hearingJudgeId;
+}
---
+++ src/main/java/uk/gov/hmcts/reform/civil/config/ServiceBusConfiguration.java
@@ -0,0 +1,116 @@
+package uk.gov.hmcts.reform.civil.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.azure.servicebus.ExceptionPhase;
+import com.microsoft.azure.servicebus.IMessage;
+import com.microsoft.azure.servicebus.IMessageHandler;
+import com.microsoft.azure.servicebus.MessageHandlerOptions;
+import com.microsoft.azure.servicebus.ReceiveMode;
+import com.microsoft.azure.servicebus.SubscriptionClient;
+import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
+import com.microsoft.azure.servicebus.primitives.ServiceBusException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import uk.gov.hmcts.reform.hmc.model.messaging.HmcMessage;
+import uk.gov.hmcts.reform.hmc.model.messaging.HmcStatus;
+
+@Configuration
+public class ServiceBusConfiguration {
+
+    public static final String AMQP_CONNECTION_STRING_TEMPLATE = "amqps://%1s";
+
+    @Value("${azure.service-bus.hmc-to-hearings-api.namespace}")
+    private String namespace;
+
+    @Value("${azure.service-bus.connection-postfix}")
+    private String connectionPostfix;
+
+    @Value("${azure.service-bus.hmc-to-hearings-api.username}")
+    private String username;
+
+    @Value("${azure.service-bus.hmc-to-hearings-api.password}")
+    private String password;
+
+    @Value("${azure.service-bus.hmc-to-hearings-api.topicName}")
+    private String topicName;
+
+    @Value("${azure.service-bus.hmc-to-hearings-api.subscriptionName}")
+    private String subscriptionName;
+
+    // @Value("${thread.count}")
+    // private int threadCount;
+
+
+    private static Logger log = LoggerFactory.getLogger(ServiceBusConfiguration.class);
+
+    @Bean
+    public SubscriptionClient receiveClient()
+        throws URISyntaxException, ServiceBusException, InterruptedException {
+        URI endpoint = new URI("sb://" + namespace + connectionPostfix);
+
+        String destination = topicName.concat("/subscriptions/").concat(subscriptionName);
+
+        ConnectionStringBuilder connectionStringBuilder =
+            new ConnectionStringBuilder(
+                endpoint, destination, username, password);
+        connectionStringBuilder.setOperationTimeout(Duration.ofMinutes(10));
+        return new SubscriptionClient(connectionStringBuilder, ReceiveMode.PEEKLOCK);
+    }
+
+    @Bean
+    CompletableFuture<Void> registerMessageHandlerOnClient(
+        @Autowired SubscriptionClient receiveClient)
+        throws ServiceBusException, InterruptedException {
+
+        IMessageHandler messageHandler =
+            new IMessageHandler() {
+
+                @SneakyThrows
+                @Override
+                public CompletableFuture<Void> onMessageAsync(IMessage message) {
+                    log.info("message received {}", LocalDateTime.now());
+                    List<byte[]> body = message.getMessageBody().getBinaryData();
+                    ObjectMapper mapper = new ObjectMapper();
+
+                    HmcMessage hearing = mapper.readValue(body.get(0), HmcMessage.class);
+                    String listAssistSessionID = hearing.getHearingUpdate().getListAssistSessionID();
+                    log.info("message received: {}", listAssistSessionID);
+                    if (HmcStatus.EXCEPTION.equals(hearing.getHearingUpdate().getHmcStatus())) {
+                        log.info("triggering WA event");
+                        // trigger ccd event for WA
+                        // if event triggered successfully
+                        return receiveClient.completeAsync(message.getLockToken());
+                    }
+                    return receiveClient.abandonAsync(message.getLockToken());
+                }
+
+                @Override
+                public void notifyException(
+                    Throwable throwable, ExceptionPhase exceptionPhase) {
+                    log.error("Exception occurred.");
+                    log.error(exceptionPhase + "-" + throwable.getMessage());
+                }
+            };
+
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        receiveClient.registerMessageHandler(
+            messageHandler,
+            new MessageHandlerOptions(
+                4, false, Duration.ofHours(1), Duration.ofMinutes(5)),
+            executorService);
+        return null;
+    }
+}
