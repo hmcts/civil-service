374b57f66567ea7de711cb48d8530da3de497fd8
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-06-14 12:24:13 +0200

    CIV-5345 : Test for new service

---
+++ src/test/java/uk/gov/hmcts/reform/civil/service/PaymentDateServiceTest.java
@@ -0,0 +1,80 @@
+package uk.gov.hmcts.reform.civil.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.RespondToClaim;
+import uk.gov.hmcts.reform.civil.model.RespondToClaimAdmitPartLRspec;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class PaymentDateServiceTest {
+
+    @Mock
+    private DeadlinesCalculator calculator;
+
+    private PaymentDateService paymentDateService;
+
+    @Test
+    void shouldGetWhenWillThisAmountBePaid_whenWhenWillThisAmountBePaidArePresent() {
+        //Given
+        LocalDate whenWillPay = LocalDate.now().plusDays(5);
+        CaseData caseData = CaseData.builder()
+            .respondToClaimAdmitPartLRspec(
+                RespondToClaimAdmitPartLRspec.builder()
+                    .whenWillThisAmountBePaid(whenWillPay)
+                    .build()
+            )
+            .build();
+        //When
+        LocalDate payDate = paymentDateService.getPaymentDateAdmittedClaim(caseData);
+        //Then
+        org.assertj.core.api.Assertions.assertThat(payDate).isEqualTo(whenWillPay);
+    }
+
+    @Test
+    void shouldGetWhenWasThisAmountPaid_whenWhenWasThisAmountPaidArePresent() {
+        //Given
+        LocalDate whenWasPaid = LocalDate.now().plusDays(-5);
+        CaseData caseData = CaseData.builder()
+            .respondToAdmittedClaim(RespondToClaim.builder()
+                                        .whenWasThisAmountPaid(whenWasPaid).build()
+            )
+            .build();
+        //When
+        LocalDate result = paymentDateService.getPaymentDateAdmittedClaim(caseData);
+        //Then
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(whenWasPaid);
+    }
+
+    @Test
+    void shouldReturRespondent1ResponseDatePlus5Days_whenRespondent1ResponseDateArePresent() {
+        //Given
+        LocalDate whenWillPay = LocalDate.now().plusDays(5);
+        when(calculator.calculateRespondentPaymentDateAdmittedClaim(any())).thenReturn(whenWillPay);
+        CaseData caseData = CaseData.builder()
+            .respondent1ResponseDate(LocalDateTime.now())
+            .build();
+        //When
+        LocalDate result = paymentDateService.getPaymentDateAdmittedClaim(caseData);
+        //Then
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(whenWillPay);
+    }
+
+    @Test
+    void shouldReturNull_whenAnythingIsSettled() {
+        //Given
+        CaseData caseData = CaseData.builder().build();
+        //When
+        LocalDate result = paymentDateService.getPaymentDateAdmittedClaim(caseData);
+        //Then
+        org.assertj.core.api.Assertions.assertThat(result).isNull();
+    }
+
+}
