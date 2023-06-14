c1555c689beb9f452526fdf8d008c34312027531
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-06-14 14:23:36 +0100

    CIV-5793 fix bug with displaying where the defendant lives

--- src/main/java/uk/gov/hmcts/reform/civil/model/dq/HomeDetails.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/dq/HomeDetails.java
@@ -1,17 +1,16 @@
 package uk.gov.hmcts.reform.civil.model.dq;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Builder;
-import lombok.Data;
+
 import uk.gov.hmcts.reform.civil.enums.HomeTypeOptionLRspec;
 
 /**
  * @param type             type of home.
  * @param typeOtherDetails home description when type is "other".
  */
-@Data
+
 @Builder
-@AllArgsConstructor
 public record HomeDetails(HomeTypeOptionLRspec type, String typeOtherDetails) {
 
 }
