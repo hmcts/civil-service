8e8112ba2196e70e66b1dd173ae1e2fd4356ad7e
sankaviv1 <sankavi.mohanraj@version1.com>
2023-06-19 11:18:10 +0100

    add conditional prop to listener bean

--- src/main/java/uk/gov/hmcts/reform/civil/config/ServiceBusConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/ServiceBusConfiguration.java
@@ -21,6 +21,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.hmc.model.messaging.HmcMessage;
