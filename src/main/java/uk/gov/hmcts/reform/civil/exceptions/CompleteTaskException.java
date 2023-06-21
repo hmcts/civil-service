76fa361008b4ffe08238cca704d1e8a1c7a2fa24
asthamalviya <104994907+asthamalviya@users.noreply.github.com>
2023-06-21 16:57:50 +0100

    CIV-6804 throwing exception from complete task (#2865)

---
+++ src/main/java/uk/gov/hmcts/reform/civil/exceptions/CompleteTaskException.java
@@ -0,0 +1,9 @@
+package uk.gov.hmcts.reform.civil.exceptions;
+
+public class CompleteTaskException extends Exception {
+
+    public CompleteTaskException(Throwable cause) {
+        super(cause);
+    }
+
+}
