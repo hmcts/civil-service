dd17075eb176ddad68fa5a2fb38545da5bf40a35
Astha Malviya <astha.malviya@HMCTS.NET>
2023-06-21 13:07:49 +0100

    handleFailureNoRetryable added fixing test9

--- src/test/java/uk/gov/hmcts/reform/civil/handler/tasks/UpdateFromGACaseEventTaskHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/tasks/UpdateFromGACaseEventTaskHandlerTest.java
@@ -44,7 +44,6 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.ADD_PDF_TO_MAIN_CASE;
