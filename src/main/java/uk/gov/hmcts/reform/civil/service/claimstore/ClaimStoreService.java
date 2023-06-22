0a858f9bc739dab16e36b26b852e52d17a270d7d
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-06-22 14:19:39 +0100

    CIV-6632 remove unused imports

--- src/main/java/uk/gov/hmcts/reform/civil/service/claimstore/ClaimStoreService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/claimstore/ClaimStoreService.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.civil.service.claimstore;
 
-import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
