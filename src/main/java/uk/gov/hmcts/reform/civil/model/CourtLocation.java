780999ba42c3fa4e67604061b6cb0bad26c8ed46
dtortolaV1 <dario.tortolanavarro@hmcts.net>
2023-06-28 14:50:10 +0100

    CIV-3883 added remote hearing option to claimant unspec

--- src/main/java/uk/gov/hmcts/reform/civil/model/CourtLocation.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/CourtLocation.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.model.common.DynamicList;
 import uk.gov.hmcts.reform.civil.model.defaultjudgment.CaseLocationCivil;
 
@@ -14,13 +15,22 @@ public class CourtLocation {
     private final String applicantPreferredCourt;
     private final DynamicList applicantPreferredCourtLocationList;
     private final CaseLocationCivil caseLocation;
+    private final String reasonForHearingAtSpecificCourt;
+    private final YesOrNo remoteHearingRequested;
+    private final String reasonForRemoteHearing;
 
     @JsonCreator
     CourtLocation(@JsonProperty("applicantPreferredCourt") String applicantPreferredCourt,
                   @JsonProperty("applicantPreferredCourtLocationList") DynamicList applicantPreferredCourtLocationList,
-                  @JsonProperty("caseLocation") CaseLocationCivil caseLocation) {
+                  @JsonProperty("caseLocation") CaseLocationCivil caseLocation,
+                  @JsonProperty("reasonForHearingAtSpecificCourt") String reasonForHearingAtSpecificCourt,
+                  @JsonProperty("remoteHearingRequested") YesOrNo remoteHearingRequested,
+                  @JsonProperty("reasonForRemoteHearing") String reasonForRemoteHearing) {
         this.applicantPreferredCourt = applicantPreferredCourt;
         this.applicantPreferredCourtLocationList = applicantPreferredCourtLocationList;
         this.caseLocation = caseLocation;
+        this.reasonForHearingAtSpecificCourt = reasonForHearingAtSpecificCourt;
+        this.remoteHearingRequested = remoteHearingRequested;
+        this.reasonForRemoteHearing = reasonForRemoteHearing;
     }
 }
