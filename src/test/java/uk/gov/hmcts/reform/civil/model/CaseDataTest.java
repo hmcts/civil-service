22da30ee4721f56d5b52947a3c98b1a1a2738ba9
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-06-28 12:01:45 +0100

    CIV-5793 remove unused import

--- src/test/java/uk/gov/hmcts/reform/civil/model/CaseDataTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/model/CaseDataTest.java
@@ -22,7 +22,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static uk.gov.hmcts.reform.civil.enums.RespondentResponseTypeSpec.FULL_ADMISSION;
 import static uk.gov.hmcts.reform.civil.enums.RespondentResponseTypeSpec.FULL_DEFENCE;
