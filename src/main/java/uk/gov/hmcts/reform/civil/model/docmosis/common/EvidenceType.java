a7c01e6929f81a2c2123cefbdfb2a00675a6ccc3
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-06-28 08:26:44 +0100

    CIV-5793 fix reciepts evidence type

--- src/main/java/uk/gov/hmcts/reform/civil/model/docmosis/common/EvidenceType.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/docmosis/common/EvidenceType.java
@@ -9,7 +9,8 @@ public enum EvidenceType {
     CONTRACTS_AND_AGREEMENTS("Contracts and agreements"),
     EXPERT_WITNESS("Expert witness"),
     LETTERS_EMAILS_AND_OTHER_CORRESPONDENCE("Letters, emails and other correspondence"),
-    PHOTO_EVIDENCE("Receipts"),
+    PHOTO_EVIDENCE("Photo Evidence"),
+    RECEIPTS("Receipts"),
     STATEMENT_OF_ACCOUNT("Statement of account"),
     OTHER("Other");
 
