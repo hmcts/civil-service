a57f27e37d78d56fb0ec11a50b43e4f9597876a6
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-07-10 15:10:19 +0100

    CIV-8957 fix revert lip expert details changes

--- src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/ExpertReportLiP.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/ExpertReportLiP.java
@@ -1,8 +1,5 @@
 package uk.gov.hmcts.reform.civil.model.citizenui;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,8 +14,6 @@ import java.time.LocalDate;
 public class ExpertReportLiP {
 
     private String expertName;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
-    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate reportDate;
 }
 
