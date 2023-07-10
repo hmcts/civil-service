8e4219a248f936c4a3ac04f7877e7b3f42c7cc62
hmcts-version1-domini <domini.mattos@version1.com>
2023-07-10 13:19:57 +0200

    Update add-roles.sh

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -27,6 +27,7 @@
 ./bin/utils/ccd-add-role.sh "caseworker-wa-task-configuration"
 ./bin/utils/ccd-add-role.sh "CITIZEN-CLAIMANT-PROFILE"
 ./bin/utils/ccd-add-role.sh "CITIZEN-DEFENDANT-PROFILE"
+./bin/utils/ccd-add-role.sh "cui-admin-profile"
 
 roles=("solicitor" "systemupdate" "admin" "staff")
 for role in "${roles[@]}"
