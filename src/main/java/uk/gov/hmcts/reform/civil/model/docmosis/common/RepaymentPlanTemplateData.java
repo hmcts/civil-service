98c4d37230e4b054dba185a869407eb4eeb28c54
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-07-11 15:38:22 +0100

    CIV-8957 repayment date format

--- src/main/java/uk/gov/hmcts/reform/civil/model/docmosis/common/RepaymentPlanTemplateData.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/docmosis/common/RepaymentPlanTemplateData.java
@@ -17,7 +17,7 @@ public class RepaymentPlanTemplateData {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     private final BigDecimal paymentAmount;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @JsonSerialize(using = LocalDateSerializer.class)
     private final LocalDate firstRepaymentDate;
 }
