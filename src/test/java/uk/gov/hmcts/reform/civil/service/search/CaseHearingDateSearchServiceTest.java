e125161545c522c1bd268e3d516ae297e28e0296
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2023-07-14 00:10:21 +0100

    Fixed flakey date unit test

--- src/test/java/uk/gov/hmcts/reform/civil/service/search/CaseHearingDateSearchServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/search/CaseHearingDateSearchServiceTest.java
@@ -1,20 +1,37 @@
 package uk.gov.hmcts.reform.civil.service.search;
 
 import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import uk.gov.hmcts.reform.civil.model.search.Query;
 
-import java.time.Clock;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
 import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static org.mockito.Mockito.mockStatic;
 
+@SuppressWarnings("unchecked")
 class CaseHearingDateSearchServiceTest extends ElasticSearchServiceTest {
+
+    private static final LocalDate CURRENT_DATE = LocalDate.of(2023, 7, 10);
+    private static MockedStatic currentDateMock;
+
+    @BeforeAll
+    static void setupSuite() {
+        currentDateMock = mockStatic(LocalDate.class, Mockito.CALLS_REAL_METHODS);
+        currentDateMock.when(LocalDate::now).thenReturn(CURRENT_DATE);
+    }
+
+    @AfterAll
+    static void tearDown() {
+        currentDateMock.reset();
+    }
 
     @BeforeEach
     void setup() {
@@ -23,11 +40,7 @@ class CaseHearingDateSearchServiceTest extends ElasticSearchServiceTest {
 
     @Override
     protected Query buildQuery(int fromValue) {
-        Clock clock = Clock.fixed(Instant.parse("2023-07-10T10:15:30Z"), ZoneId.of("UTC"));
-        Instant.now(clock);
-
-        String targetDate = LocalDate.of(2023, 07, 26).format(DateTimeFormatter.ISO_DATE);
-
+        String targetDate = LocalDate.of(2023, 07, 24).format(DateTimeFormatter.ISO_DATE);
         BoolQueryBuilder query = boolQuery()
             .minimumShouldMatch(1)
             .should(boolQuery()
