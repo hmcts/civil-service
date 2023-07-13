63a30a916384910c5e8d449ef38de1b1a8f78f5e
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2023-07-13 13:05:46 +0100

    fix checkstyle

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/NotificationDefendantOfHearingHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/NotificationDefendantOfHearingHandler.java
@@ -8,7 +8,6 @@ import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
-import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.notify.NotificationService;
@@ -66,7 +65,7 @@ public class NotificationDefendantOfHearingHandler extends CallbackHandler imple
             return AboutToStartOrSubmitCallbackResponse.builder().build();
         }
 
-        if (caseData.getRespondent1Represented() == YesOrNo.NO){
+        if (caseData.isLRvLipOneVOne()) {
             return AboutToStartOrSubmitCallbackResponse.builder()
                 .build();
         }
