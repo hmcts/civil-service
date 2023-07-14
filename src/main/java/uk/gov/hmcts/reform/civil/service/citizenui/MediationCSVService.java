925f2f1cf179183ddee638059391cfa35058b5aa
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-07-14 15:48:31 +0100

    CIV-9525 add lr organisaiton name, phone number and email for applicant represented

--- src/main/java/uk/gov/hmcts/reform/civil/service/citizenui/MediationCSVService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/citizenui/MediationCSVService.java
@@ -1,12 +1,18 @@
 package uk.gov.hmcts.reform.civil.service.citizenui;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.CorrectEmail;
 import uk.gov.hmcts.reform.civil.model.Party;
+import uk.gov.hmcts.reform.civil.prd.model.Organisation;
+import uk.gov.hmcts.reform.civil.service.OrganisationService;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class MediationCSVService {
 
     private static final String SITE_ID = "5";
@@ -14,22 +20,25 @@ public class MediationCSVService {
     private static final String CHECK_LIST = "4";
     private static final String PARTY_STATUS = "5";
 
+    private final OrganisationService organisationService;
+
     public String generateCSVContent(CaseData data) {
         String [] headers = {"SITE_ID", "CASE_NUMBER", "CASE_TYPE", "AMOUNT", "PARTY_TYPE", "COMPANY_NAME",
             "CONTACT_NAME", "CONTACT_NUMBER", "CHECK_LIST", "PARTY_STATUS", "CONTACT_EMAIL", "PILOT"};
 
+        Optional<Organisation> claimantRepresentativeOrganisation = organisationService.findOrganisationById(data.getApplicantOrganisationId());
         String [] claimantData = {
             SITE_ID, data.getLegacyCaseReference(), CASE_TYPE, data.getTotalClaimAmount().toString(),
-            data.getApplicant1().getType().toString(), setUpCsvCompanyName(data.getApplicant1()),
-            setUpCsvIndividualName(data.getApplicant1()), data.getApplicant1().getPartyPhone(),
-            CHECK_LIST, PARTY_STATUS, data.getApplicant1().getPartyEmail(),
+            data.getApplicant1().getType().toString(), getApplicantSolicitorCompanyName(claimantRepresentativeOrganisation,data),
+            data.getApplicant1().getPartyName(), getRepresentativeContactNumber(claimantRepresentativeOrganisation),
+            CHECK_LIST, PARTY_STATUS, getApplicantEmailAddress(data),
             isPilot(data.getTotalClaimAmount())
         };
 
         String [] respondentData = {
             SITE_ID, data.getLegacyCaseReference(), CASE_TYPE, data.getTotalClaimAmount().toString(),
-            data.getRespondent1().getType().toString(), setUpCsvCompanyName(data.getRespondent1()),
-            setUpCsvIndividualName(data.getRespondent1()), data.getRespondent1().getPartyPhone(),
+            data.getRespondent1().getType().toString(), getCsvCompanyNameForDefendant(data.getRespondent1()),
+            getCsvIndividualName(data.getRespondent1()), data.getRespondent1().getPartyPhone(),
             CHECK_LIST, PARTY_STATUS, data.getRespondent1().getPartyEmail(),
             isPilot(data.getTotalClaimAmount())
         };
@@ -54,37 +63,26 @@ public class MediationCSVService {
         return builder.toString();
     }
 
-    private String setUpCsvCompanyName(Party party) {
-        switch (party.getType()) {
-            case COMPANY:
-                return party.getCompanyName();
-            case ORGANISATION:
-                return party.getOrganisationName();
-            default:
-                return null;
+    private String getApplicantSolicitorCompanyName(Optional<Organisation> organisation, CaseData caseData) {
+        return organisation.map(Organisation::getName).orElse(caseData.getApplicantSolicitor1ClaimStatementOfTruth().getName());
+    }
+
+    private String getRepresentativeContactNumber(Optional<Organisation> organisation) {
+       return organisation.map(Organisation::getCompanyNumber).orElse("");
+    }
+
+    private String getApplicantEmailAddress(CaseData caseData) {
+        return Optional.ofNullable(caseData.getApplicantSolicitor1CheckEmail()).map(CorrectEmail::getEmail).orElse("");
+    }
+
+    private String getCsvCompanyNameForDefendant(CaseData caseData) {
+        if(caseData.isRespondent1LiP()){
+            return caseData.getRespondent1().getPartyName();
         }
+
     }
 
-    private String setUpCsvIndividualName(Party party) {
-        switch (party.getType()) {
-            case INDIVIDUAL:
-                return getIndividualName(party);
-            case SOLE_TRADER:
-                return getSoleTraderName(party);
-            default:
-                return null;
-        }
-    }
-
-    private static String getSoleTraderName(Party party) {
-        return party.getSoleTraderFirstName()
-            + " "
-            + party.getSoleTraderLastName();
-    }
-
-    private static String getIndividualName(Party party) {
-        return party.getIndividualFirstName()
-            + " "
-            + party.getIndividualLastName();
+    private String getCsvIndividualName(Party party) {
+       return party.getPartyName();
     }
 }
