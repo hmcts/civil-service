600037fa43952ce27991e6a739aad38012d18558
jeswanth <jeswanth.reddy@hmcts.net>
2023-07-18 14:29:22 +0100

    code for updating list of claims based on the page no

--- src/main/java/uk/gov/hmcts/reform/civil/service/citizenui/DashboardClaimInfoService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/citizenui/DashboardClaimInfoService.java
@@ -21,7 +21,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 import static java.util.Objects.nonNull;
 
