714597638b82c30ccee8878c132f9dc466c665d8
dtortolaV1 <dario.tortolanavarro@hmcts.net>
2023-07-19 13:17:54 +0100

    CIV-8923 style

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToClaimSpecCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToClaimSpecCallbackHandlerTest.java
@@ -95,7 +95,6 @@ import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
