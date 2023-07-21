deafc8230037e1c8fbb8f1ccec7690c36b2abb62
neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
2023-07-21 16:56:11 +0100

    Update DocumentType.java

--- commons-civil/src/main/java/uk/gov/hmcts/reform/civil/documentmanagement/model/DocumentType.java
+++ commons-civil/src/main/java/uk/gov/hmcts/reform/civil/documentmanagement/model/DocumentType.java
@@ -16,7 +16,6 @@ public enum DocumentType {
     HEARING_FORM,
     PIP_LETTER,
     JUDGE_FINAL_ORDER,
-    TRANSLATED_DEFENCE_DOCUMENT,
 
     //General Application Document Type
     GENERAL_ORDER,
