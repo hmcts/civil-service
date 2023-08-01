10623a54077d676b808b9924073dca01f9a85770
suvarnathekkoot <suvarna.thekkoot@HMCTS.NET>
2023-08-01 17:10:56 +0100

    CIV-8878 logging import DQ

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/dq/DirectionsQuestionnaireGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/dq/DirectionsQuestionnaireGenerator.java
@@ -129,6 +129,7 @@ public class DirectionsQuestionnaireGenerator implements TemplateDataGeneratorWi
 
     private DocmosisTemplates getDocmosisTemplate(CaseData caseData) {
         DocmosisTemplates templateId = DQ_RESPONSE_1V1;
+        log.info("DQ_RESPONSE_1V1", templateId);
         switch (getMultiPartyScenario(caseData)) {
             case ONE_V_TWO_TWO_LEGAL_REP:
                 if (isClaimantResponse(caseData) && isClaimantMultipartyProceed(caseData)) {
