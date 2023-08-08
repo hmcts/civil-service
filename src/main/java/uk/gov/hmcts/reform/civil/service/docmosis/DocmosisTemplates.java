cfb98e9f82e2a79add4f53e45fc9a3f3f9e4c71c
indu2019 <indu.chaturvedi@hmcts.net>
2023-08-08 11:03:02 +0100

    CIV-9864-changing court name to venue_name

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/DocmosisTemplates.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/DocmosisTemplates.java
@@ -25,7 +25,7 @@ public enum DocmosisTemplates {
     DJ_SDO_DISPOSAL("CV-UNS-DEC-ENG-01323.docx", "Order_disposal_%s.pdf"),
     DJ_SDO_TRIAL("CV-UNS-DEC-ENG-01322.docx", "Order_trial_%s.pdf"),
     SDO_SMALL("CV-UNS-STD-ENG-01325.docx", "small_claims_sdo_%s.pdf"),
-    SDO_FAST("CV-UNS-STD-ENG-013326.docx", "fast_track_sdo_%s.pdf"),
+    SDO_FAST("CV-UNS-STD-ENG-01326.docx", "fast_track_sdo_%s.pdf"),
     DEFENDANT_RESPONSE_LIP_SPEC("CV-SPC-CLM-ENG-01064-LIP.docx", "response_sealed_form_%s.pdf"),
     HEARING_SMALL_CLAIMS("CV-UNS-HNO-ENG-01197.docx", "hearing_small_claim_%s.pdf"),
     HEARING_FAST_TRACK("CV-UNS-HNO-ENG-01198.docx", "hearing_fast_track_%s.pdf"),
