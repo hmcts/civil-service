af40b3b6c53c13bbf0ffa71abc443713893a8c3d
neeta-hmcts <neeta.shinde@hmcts.net>
2023-08-14 17:58:25 +0100

    fixed testing.

--- src/main/java/uk/gov/hmcts/reform/cmc/model/FormaliseOptionPlan.java
+++ src/main/java/uk/gov/hmcts/reform/cmc/model/FormaliseOptionPlan.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.reform.cmc.model;
 
 public enum FormaliseOptionPlan {
-    SIGN_SETTLEMENT_AGREEMENT,
-    REQUEST_COUNTY_COURT_JUDGEMENT,
+    SETTLEMENT,
+    CCJ,
     REFER_TO_JUDGE
 }
