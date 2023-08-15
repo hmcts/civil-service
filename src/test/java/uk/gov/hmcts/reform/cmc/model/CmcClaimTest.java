db178ef9b85318559db231fe3a7ddc107ef7773f
neeta-hmcts <neeta.shinde@hmcts.net>
2023-08-15 15:49:11 +0100

    removed unwanted code.

--- src/test/java/uk/gov/hmcts/reform/cmc/model/CmcClaimTest.java
+++ src/test/java/uk/gov/hmcts/reform/cmc/model/CmcClaimTest.java
@@ -11,7 +11,6 @@ import uk.gov.hmcts.reform.civil.model.citizenui.DashboardClaimStatus;
 import uk.gov.hmcts.reform.civil.model.citizenui.DashboardClaimStatusFactory;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
 
