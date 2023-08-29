9ee45f8e20d34512539043429cd2759fd084bc31
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2023-08-29 13:01:07 +0100

    create UserInformationService and unit tests

---
+++ src/main/java/uk/gov/hmcts/reform/civil/service/user/UserInformationService.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.reform.civil.service.user;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.civil.exceptions.CaseNotFoundException;
+import uk.gov.hmcts.reform.civil.service.CoreCaseUserService;
+import uk.gov.hmcts.reform.civil.service.UserService;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserInformationService {
+    Logger log = LoggerFactory.getLogger(UserInformationService.class);
+
+    private final UserService userService;
+    private final CoreCaseUserService coreCaseUserService;
+
+    public List<String> getUserCaseRoles(String caseId, String authorization) {
+        try {
+            UserInfo userInfo = userService.getUserInfo(authorization);
+            List<String> roles = coreCaseUserService.getUserCaseRoles(
+                caseId,
+                userInfo.getUid()
+            );
+            return roles;
+        } catch (Exception e) {
+            log.error(String.format("No case found for %s", caseId));
+            throw new CaseNotFoundException();
+        }
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/civil/service/user/UserInformationServiceTest.java
@@ -0,0 +1,60 @@
+package uk.gov.hmcts.reform.civil.service.user;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.civil.exceptions.CaseNotFoundException;
+import uk.gov.hmcts.reform.civil.service.CoreCaseUserService;
+import uk.gov.hmcts.reform.civil.service.UserService;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+class UserInformationServiceTest {
+    public static final UserInfo USER_INFO = UserInfo.builder().uid("uid").build();
+    public static final String AUTHORIZATION = "token";
+    public static final String CASE_ID = "123";
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private CoreCaseUserService coreCaseUserService;
+
+    private UserInformationService userInformationService;
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        userInformationService = new UserInformationService(userService, coreCaseUserService);
+    }
+
+    @Test
+    public void should_find_Success() {
+        List<String> expectedRoles = List.of("role1", "role2");
+
+        when(userService.getUserInfo(AUTHORIZATION)).thenReturn(USER_INFO);
+        when(coreCaseUserService.getUserCaseRoles(CASE_ID, USER_INFO.getUid())).thenReturn(expectedRoles);
+
+        List<String> actualRoles = userInformationService.getUserCaseRoles(CASE_ID, AUTHORIZATION);
+
+        assertEquals(expectedRoles, actualRoles);
+        verify(userService, times(1)).getUserInfo(AUTHORIZATION);
+        verify(coreCaseUserService, times(1)).getUserCaseRoles(CASE_ID, USER_INFO.getUid());
+    }
+
+    @Test
+    public void testGetUserCaseRoles_CaseNotFound() {
+
+        when(userService.getUserInfo(AUTHORIZATION)).thenThrow(new CaseNotFoundException());
+
+        assertThrows(CaseNotFoundException.class, () -> userInformationService.getUserCaseRoles(CASE_ID, AUTHORIZATION));
+        verify(userService, times(1)).getUserInfo(AUTHORIZATION);
+        verify(coreCaseUserService, never()).getUserCaseRoles(anyString(), anyString());
+    }
+}
