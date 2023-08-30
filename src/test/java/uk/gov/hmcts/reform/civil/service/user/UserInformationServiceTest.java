80e26467bbd6d78ef2e02d4860345e37d3c5971e
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2023-08-30 10:49:39 +0100

    fix checkstyle

--- src/test/java/uk/gov/hmcts/reform/civil/service/user/UserInformationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/user/UserInformationServiceTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.civil.service.user;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
