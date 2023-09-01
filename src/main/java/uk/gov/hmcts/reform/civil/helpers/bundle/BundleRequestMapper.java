7c9a3228e89bda24ea84365b6f2f15a99d0f0da9
bhagyashreesharma90 <bhagyashree.sharma@hmcts.net>
2023-09-01 17:38:24 +0100

    CIV-7950 fix witness statment

--- src/main/java/uk/gov/hmcts/reform/civil/helpers/bundle/BundleRequestMapper.java
+++ src/main/java/uk/gov/hmcts/reform/civil/helpers/bundle/BundleRequestMapper.java
@@ -648,8 +648,8 @@ public class BundleRequestMapper {
                     : fileNamePrefix.equals(DOC_FILE_NAME_WITH_DATE)
                     ?
                     generateDocName(uploadEvidenceDocumentTypeElement.getValue().getDocumentUpload().getDocumentFileName(), null, null,
-                                    documentType.equals(EvidenceUploadFiles.COSTS.name()) ?
-                                        uploadEvidenceDocumentTypeElement.getValue().getCreatedDatetime().toLocalDate() :
+                                    documentType.equals(EvidenceUploadFiles.COSTS.name())
+                                        ? uploadEvidenceDocumentTypeElement.getValue().getCreatedDatetime().toLocalDate() :
                                         uploadEvidenceDocumentTypeElement.getValue().getDocumentIssuedDate())
                     : generateDocName(fileNamePrefix, party.getDisplayName(), null,
                                                 documentType.equals(EvidenceUploadFiles.CASE_SUMMARY.name())
