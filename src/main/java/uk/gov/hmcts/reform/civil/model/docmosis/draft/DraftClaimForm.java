0cf421669af382ebc7bde917c607391c2fb3e5fa
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-09-05 14:27:08 +0100

    CIV-8897 timeline and evidence

--- src/main/java/uk/gov/hmcts/reform/civil/model/docmosis/draft/DraftClaimForm.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/docmosis/draft/DraftClaimForm.java
@@ -7,6 +7,7 @@ import uk.gov.hmcts.reform.civil.model.Address;
 import uk.gov.hmcts.reform.civil.model.ClaimAmountBreakupDetails;
 import uk.gov.hmcts.reform.civil.model.common.MappableObject;
 import uk.gov.hmcts.reform.civil.model.docmosis.common.EventTemplateData;
+import uk.gov.hmcts.reform.civil.model.docmosis.common.EvidenceTemplateData;
 import uk.gov.hmcts.reform.civil.model.docmosis.lip.LipFormParty;
 
 import java.util.List;
@@ -22,6 +23,7 @@ public class DraftClaimForm implements MappableObject {
     private final Address defendantCorrespondenceAddress;
     private final String descriptionOfClaim;
     private final List<EventTemplateData> timelineEvents;
+    private final List<EvidenceTemplateData> evidenceList;
     private final List<ClaimAmountBreakupDetails> claimAmount;
     private final String totalClaimAmount;
 
