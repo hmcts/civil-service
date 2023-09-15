fb11433ba2d4b7e3d91a34eea6fb28c91ba4d53a
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-09-15 13:44:12 +0100

    CIV-1038 add more coverage on new changes

--- src/test/java/uk/gov/hmcts/reform/civil/service/docmosis/sealedclaim/SealedClaimResponseFormGeneratorForSpecTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/docmosis/sealedclaim/SealedClaimResponseFormGeneratorForSpecTest.java
@@ -254,6 +254,41 @@ public class SealedClaimResponseFormGeneratorForSpecTest {
         assertThat(docmosisTemplatesArgumentCaptor.getValue()).isEqualTo(DocmosisTemplates.DEFENDANT_RESPONSE_SPEC_SEALED_1V1_INSTALLMENTS);
     }
 
+    @Test
+    void shouldSelectTemplateWithoutRepaymentPlan_whenPinAndPostDisabled() {
+        //Given
+        DocmosisDocument docmosisDocument = DocmosisDocument.builder().build();
+        given(featureToggleService.isPinInPostEnabled()).willReturn(false);
+        given(documentGeneratorService.generateDocmosisDocument(any(MappableObject.class), any()))
+            .willReturn(docmosisDocument);
+        //When
+        generator.generate(CASE_DATA_WITH_RESPONDENT1, BEARER_TOKEN);
+        //Then
+        verify(documentGeneratorService).generateDocmosisDocument(templateDataCaptor.capture(), docmosisTemplatesArgumentCaptor.capture());
+        assertThat(docmosisTemplatesArgumentCaptor.getValue()).isEqualTo(DocmosisTemplates.DEFENDANT_RESPONSE_SPEC_SEALED_1v1);
+    }
+
+    @Test
+    void shouldSelectMultipartyTemplate_whenMultipartyCase() {
+        //Given
+        DocmosisDocument docmosisDocument = DocmosisDocument.builder().build();
+        given(documentGeneratorService.generateDocmosisDocument(any(MappableObject.class), any()))
+            .willReturn(docmosisDocument);
+        CaseData multipartyCaseData = CASE_DATA_WITH_RESPONDENT1.toBuilder()
+            .respondent2(Party.builder()
+                             .type(Party.Type.COMPANY)
+                             .companyName("defendant2 name")
+                             .build())
+            .respondentResponseIsSame(YesOrNo.YES)
+            .build();
+        //When
+        generator.generate(multipartyCaseData, BEARER_TOKEN);
+        //Then
+        verify(documentGeneratorService).generateDocmosisDocument(templateDataCaptor.capture(), docmosisTemplatesArgumentCaptor.capture());
+        assertThat(docmosisTemplatesArgumentCaptor.getValue()).isEqualTo(DocmosisTemplates.DEFENDANT_RESPONSE_SPEC_SEALED_1v2);
+    }
+
+
     private static CaseData getCaseDataWithRespondent1Data() {
         return CaseData.builder()
             .legacyCaseReference("case reference")
