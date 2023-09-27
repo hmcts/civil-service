b553010d2efa2ca808599ec290a180e0f8d6d5b4
KumarSam <sampank@gmail.com>
2023-09-27 14:14:41 +0100

    CIV-9406 - Use caching to cache user role for logged in user - checkstyle fixes

--- src/main/java/uk/gov/hmcts/reform/civil/utils/UserRoleCaching.java
+++ src/main/java/uk/gov/hmcts/reform/civil/utils/UserRoleCaching.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
-import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.service.CoreCaseUserService;
 import uk.gov.hmcts.reform.civil.service.UserService;
 import uk.gov.hmcts.reform.idam.client.models.UserInfo;
