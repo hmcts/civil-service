c6512c99fb310e0d823c403f4de632480a5a32b0
MMNycz <maciej.nycz@version1.com>
2023-09-27 13:26:57 +0100

    Update RepresentativeService.java

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/RepresentativeService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/RepresentativeService.java
@@ -24,8 +24,9 @@ public class RepresentativeService {
     private final FeatureToggleService featureToggleService;
 
     private boolean doesOrganisationPolicyExist(OrganisationPolicy organisationPolicy, String organisationIDCopy) {
-        return organisationPolicy != null && organisationPolicy.getOrganisation() != null
-            && (organisationPolicy.getOrganisation().getOrganisationID() != null || organisationIDCopy != null);
+        return (organisationPolicy != null && organisationPolicy.getOrganisation() != null
+            && organisationPolicy.getOrganisation().getOrganisationID() != null)
+            || organisationIDCopy != null;
     }
 
     public Representative getRespondent1Representative(CaseData caseData) {
