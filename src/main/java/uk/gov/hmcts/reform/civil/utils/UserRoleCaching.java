5b64513aa188c476e9853adb4a1732bc90655607
KumarSam <sampank@gmail.com>
2023-09-28 14:09:13 +0100

    CIV-9406 - Use caching to cache user role for logged in user - adding cache evict

--- src/main/java/uk/gov/hmcts/reform/civil/utils/UserRoleCaching.java
+++ src/main/java/uk/gov/hmcts/reform/civil/utils/UserRoleCaching.java
@@ -1,10 +1,13 @@
 package uk.gov.hmcts.reform.civil.utils;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.service.CoreCaseUserService;
 import uk.gov.hmcts.reform.civil.service.UserService;
@@ -14,12 +17,13 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserRoleCaching {
 
     private final UserService userService;
     private final CoreCaseUserService coreCaseUserService;
 
-    @Cacheable(cacheNames = "UserCache", cacheManager = "userCacheManager", key = "#userInfo.uid")
+    @Cacheable(cacheNames = "UserCache", cacheManager = "userCacheManager", key = "bearerToken")
     public List<String> getUserRoles(String bearerToken, String ccdCaseRef) {
         UserInfo userInfo = userService.getUserInfo(bearerToken);
         List<String> roles = coreCaseUserService.getUserCaseRoles(ccdCaseRef, userInfo.getUid());
@@ -29,6 +33,12 @@ public class UserRoleCaching {
     @Bean(name = "userCacheManager")
     public CacheManager getCacheManager() {
         return new ConcurrentMapCacheManager();
+    }
+
+    @CacheEvict(value = "UserCache", allEntries = true)
+    @Scheduled(fixedRateString = "3600000")
+    public void emptyUserCache() {
+        log.debug("Cache removed");
     }
 
 }
