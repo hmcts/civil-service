f325732c4bd5caa03a60aedd6af3019a5c92e4de
Pijian Liao <Pijian.Liao@HMCTS.NET>
2023-10-04 15:46:59 +0100

    CIV-10789 bundle

--- src/test/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandlerTest.java
@@ -219,6 +219,7 @@ class BundleCreationTriggerEventHandlerTest {
                                 generatedBundle.getValue().getStitchedDocument().get().getDocumentFileName());
         Assertions.assertEquals(caseData.getHearingDate(), generatedBundle.getValue().getBundleHearingDate().get());
         Assertions.assertNotNull(generatedBundle.getValue().getCreatedOn());
+        Assertions.assertEquals(bundle.getValue().getStitchedDocument().getCategoryID(), "bundles");
     }
 
     @Test
