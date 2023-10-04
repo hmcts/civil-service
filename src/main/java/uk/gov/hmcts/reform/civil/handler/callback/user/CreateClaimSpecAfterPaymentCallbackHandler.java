517dcdca077d34b092b50df7258bb8163965bb02
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2023-10-04 16:01:17 +0100

    CIV-10391 changes

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateClaimSpecAfterPaymentCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateClaimSpecAfterPaymentCallbackHandler.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.civil.handler.callback.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
@@ -12,9 +11,12 @@ import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.service.DeadlinesCalculator;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import static java.util.Collections.singletonList;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
@@ -22,7 +24,6 @@ import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
 import static uk.gov.hmcts.reform.civil.callback.CallbackVersion.V_1;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_CLAIM_SPEC_AFTER_PAYMENT;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CreateClaimSpecAfterPaymentCallbackHandler extends CallbackHandler {
@@ -30,6 +31,7 @@ public class CreateClaimSpecAfterPaymentCallbackHandler extends CallbackHandler 
     // This should be created by the handler/service that receives payment info via our endpoint.
     private static final List<CaseEvent> EVENTS = singletonList(CREATE_CLAIM_SPEC_AFTER_PAYMENT);
     private final ObjectMapper objectMapper;
+    private final DeadlinesCalculator deadlinesCalculator;
 
     @Override
     protected Map<String, Callback> callbacks() {
@@ -45,14 +47,16 @@ public class CreateClaimSpecAfterPaymentCallbackHandler extends CallbackHandler 
     }
 
     private CallbackResponse changeStateToCaseIssued(CallbackParams callbackParams) {
-        Long caseId = callbackParams.getCaseData().getCcdCaseReference();
         CaseData caseData = callbackParams.getCaseData();
         CaseData.CaseDataBuilder dataBuilder = caseData.toBuilder();
         dataBuilder.businessProcess(BusinessProcess.ready(CREATE_CLAIM_SPEC_AFTER_PAYMENT));
-
+        if (Objects.isNull(caseData.getRespondent1ResponseDeadline())) {
+            dataBuilder.respondent1ResponseDeadline(deadlinesCalculator.plus28DaysAt4pmDeadline(
+                LocalDateTime.now()));
+        }
         return AboutToStartOrSubmitCallbackResponse.builder()
-                .data(dataBuilder.build().toMap(objectMapper))
-                .build();
+            .data(dataBuilder.build().toMap(objectMapper))
+            .build();
     }
 
 }
--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateClaimSpecAfterPaymentCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateClaimSpecAfterPaymentCallbackHandlerTest.java
@@ -1,19 +1,27 @@
 package uk.gov.hmcts.reform.civil.handler.callback.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+import uk.gov.hmcts.reform.civil.service.DeadlinesCalculator;
+
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CallbackVersion.V_1;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_CLAIM_SPEC_AFTER_PAYMENT;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.CASE_ISSUED;
 
 @SpringBootTest(classes = {
     CreateClaimSpecAfterPaymentCallbackHandler.class,
@@ -23,6 +31,12 @@ class CreateClaimSpecAfterPaymentCallbackHandlerTest extends BaseCallbackHandler
 
     @Autowired
     private CreateClaimSpecAfterPaymentCallbackHandler handler;
+
+    @Autowired
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @MockBean
+    private DeadlinesCalculator deadlinesCalculator;
 
     @Test
     void shouldRespondWithStateChanged() {
@@ -37,5 +51,23 @@ class CreateClaimSpecAfterPaymentCallbackHandlerTest extends BaseCallbackHandler
     @Test
     void handleEventsReturnsTheExpectedCallbackEvent() {
         assertThat(handler.handledEvents()).contains(CREATE_CLAIM_SPEC_AFTER_PAYMENT);
+    }
+
+    @Test
+    void shouldUpdateRespondent1ResponseDeadlineTo28days_whenClaimIssued() {
+
+        LocalDateTime localDateTime = LocalDateTime.of(2023, 10, 4, 12, 0, 0);
+        when(deadlinesCalculator.plus28DaysAt4pmDeadline(any())).thenReturn(localDateTime);
+
+        CaseData caseData = CaseDataBuilder.builder().build();
+        caseData = caseData.toBuilder().ccdState(CASE_ISSUED).build();
+        CallbackParams params = callbackParamsOf(V_1, caseData, ABOUT_TO_SUBMIT);
+
+        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+
+        CaseData updatedData = mapper.convertValue(response.getData(), CaseData.class);
+
+        assertThat(updatedData.getRespondent1ResponseDeadline()).isEqualTo(localDateTime);
+
     }
 }
