2affc315dc0bf63b50f61e9c835a7c92b3acb4da
sankaviv1 <sankavi.mohanraj@version1.com>
2023-10-10 18:54:27 +0100

    fix test

--- src/test/java/uk/gov/hmcts/reform/civil/service/FeatureToggleServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/FeatureToggleServiceTest.java
@@ -146,7 +146,7 @@ class FeatureToggleServiceTest {
     void shouldReturnCorrectValue_whenLocationWhiteListed(Boolean toggleStat) {
         final String feature = "case-progression-location-whitelist";
         String location = "000000";
-        when(featureToggleApi.isFeatureEnabledForLocation(eq(feature), eq(location), eq(false)))
+        when(featureToggleApi.isFeatureEnabledForLocation(eq(feature), eq(location), eq(true)))
             .thenReturn(toggleStat);
 
         assertThat(featureToggleService.isLocationWhiteListedForCaseProgression(location)).isEqualTo(toggleStat);
