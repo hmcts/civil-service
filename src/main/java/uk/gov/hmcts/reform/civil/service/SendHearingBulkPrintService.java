7a14c7ac1aaaa916687368f31791e1099aacd17d
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2023-10-20 10:55:00 +0100

    CIV-10782 update service

--- src/main/java/uk/gov/hmcts/reform/civil/service/SendHearingBulkPrintService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/SendHearingBulkPrintService.java
@@ -21,22 +21,27 @@ public class SendHearingBulkPrintService {
     private static final String HEARING_PACK_LETTER_TYPE = "hearing-document-pack";
 
     public void sendHearingToDefendantLIP(String authorisation, CaseData caseData) {
-        if (caseData.getSystemGeneratedCaseDocuments() != null && !caseData.getSystemGeneratedCaseDocuments().isEmpty()) {
-            if (nonNull(caseData.getHearingDocuments()) && !caseData.getHearingDocuments().isEmpty()) {
-                CaseDocument caseDocument = caseData.getHearingDocuments().get(0).getValue();
-                String documentUrl = caseDocument.getDocumentLink().getDocumentUrl();
-                String documentId = documentUrl.substring(documentUrl.lastIndexOf("/") + 1);
-                byte[] letterContent;
-                try {
-                    letterContent = documentDownloadService.downloadDocument(authorisation, documentId).file().getInputStream().readAllBytes();
-                } catch (Exception e) {
-                    log.error("Failed getting letter content for Hearing ");
-                    throw new DocumentDownloadException(caseDocument.getDocumentName(), e);
-                }
-                List<String> recipients = List.of(caseData.getRespondent1().getPartyName());
-                bulkPrintService.printLetter(letterContent, caseData.getLegacyCaseReference(),
-                                             caseData.getLegacyCaseReference(), HEARING_PACK_LETTER_TYPE, recipients);
+        if (checkHearingDocumentAvailable(caseData)) {
+            CaseDocument caseDocument = caseData.getHearingDocuments().get(0).getValue();
+            String documentUrl = caseDocument.getDocumentLink().getDocumentUrl();
+            String documentId = documentUrl.substring(documentUrl.lastIndexOf("/") + 1);
+            byte[] letterContent;
+            try {
+                letterContent = documentDownloadService.downloadDocument(authorisation, documentId).file().getInputStream().readAllBytes();
+            } catch (Exception e) {
+                log.error("Failed getting letter content for Hearing ");
+                throw new DocumentDownloadException(caseDocument.getDocumentName(), e);
             }
+            List<String> recipients = List.of(caseData.getRespondent1().getPartyName());
+            bulkPrintService.printLetter(letterContent, caseData.getLegacyCaseReference(),
+                                         caseData.getLegacyCaseReference(), HEARING_PACK_LETTER_TYPE, recipients);
         }
+    }
+
+    private boolean checkHearingDocumentAvailable(CaseData caseData) {
+        return caseData.getSystemGeneratedCaseDocuments() != null
+            && !caseData.getSystemGeneratedCaseDocuments().isEmpty()
+            && nonNull(caseData.getHearingDocuments())
+            && !caseData.getHearingDocuments().isEmpty();
     }
 }
