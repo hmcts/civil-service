df6f833f2c9d136cccfc0f2c9cdfb4bd64337715
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2023-10-30 10:35:38 +0000

    CIV-9025: Fix unused import

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimantLipRequestJudgementByAdmissionApplicantNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimantLipRequestJudgementByAdmissionApplicantNotificationHandler.java
@@ -9,7 +9,6 @@ import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.config.PinInPostConfiguration;
-import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.notify.NotificationService;
 import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
