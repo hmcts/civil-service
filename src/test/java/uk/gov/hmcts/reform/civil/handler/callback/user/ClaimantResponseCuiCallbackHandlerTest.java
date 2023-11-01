8849afd5c2fe770fcb12711d74bbbdf5df82c76f
dharmendra kumar <dharmendra.kumar@hmcts.net>
2023-11-01 14:45:03 +0000

    Correct court location for LIPVLIP

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ClaimantResponseCuiCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ClaimantResponseCuiCallbackHandlerTest.java
@@ -1,36 +1,56 @@
 package uk.gov.hmcts.reform.civil.handler.callback.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.enums.CaseState;
 import uk.gov.hmcts.reform.civil.enums.MediationDecision;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
+import uk.gov.hmcts.reform.civil.helpers.LocationHelper;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.citizenui.CaseDataLiP;
 import uk.gov.hmcts.reform.civil.model.citizenui.ClaimantMediationLip;
+import uk.gov.hmcts.reform.civil.referencedata.LocationRefDataService;
+import uk.gov.hmcts.reform.civil.referencedata.model.LocationRefData;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CLAIMANT_RESPONSE_CUI;
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
+import uk.gov.hmcts.reform.civil.utils.CourtLocationUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {
     ClaimantResponseCuiCallbackHandler.class,
-    JacksonAutoConfiguration.class
+    JacksonAutoConfiguration.class,
+    CourtLocationUtils.class,
+    LocationRefDataService.class,
+    LocationHelper.class
 })
 class ClaimantResponseCuiCallbackHandlerTest extends BaseCallbackHandlerTest {
 
+    @Autowired
+    private CourtLocationUtils courtLocationUtility;
+    @Autowired
+    private LocationHelper locationHelper;
+    @MockBean
+    private LocationRefDataService locationRefDataService;
     @Autowired
     private ClaimantResponseCuiCallbackHandler handler;
 
@@ -54,9 +74,16 @@ class ClaimantResponseCuiCallbackHandlerTest extends BaseCallbackHandlerTest {
     @Nested
     class AboutToSubmitCallback {
 
+        @BeforeEach
+        void before() {
+            given(locationRefDataService.getCourtLocationsForDefaultJudgments(any()))
+                .willReturn(getSampleCourLocationsRefObject());
+        }
+
         @Test
         void shouldUpdateBusinessProcess() {
             CaseData caseData = CaseDataBuilder.builder()
+                .atStateClaimIssued1v1LiP()
                 .caseDataLip(
                     CaseDataLiP.builder()
                         .applicant1ClaimMediationSpecRequiredLip(
@@ -119,6 +146,20 @@ class ClaimantResponseCuiCallbackHandlerTest extends BaseCallbackHandlerTest {
             var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
 
             assertThat(response.getState()).isEqualTo(CaseState.IN_MEDIATION.name());
+        }
+
+        protected List<LocationRefData> getSampleCourLocationsRefObject() {
+            return new ArrayList<>(List.of(
+                LocationRefData.builder()
+                    .epimmsId("111").siteName("Site 1").courtAddress("Adr 1").postcode("AAA 111")
+                    .courtLocationCode("court1").build(),
+                LocationRefData.builder()
+                    .epimmsId("222").siteName("Site 2").courtAddress("Adr 2").postcode("BBB 222")
+                    .courtLocationCode("court2").build(),
+                LocationRefData.builder()
+                    .epimmsId("333").siteName("Site 3").courtAddress("Adr 3").postcode("CCC 333")
+                    .courtLocationCode("court3").build()
+            ));
         }
     }
 }
