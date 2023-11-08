0c31ee447290204acf7e0f2eec7ad2e4b611ed24
Manish Garg <manish.garg@hmcts.net>
2023-11-08 11:49:59 +0000

    CIV-8147 Rebase with master

--- src/main/java/uk/gov/hmcts/reform/civil/model/Party.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/Party.java
@@ -112,9 +112,4 @@ public class Party {
     public LocalDate getDateOfBirth() {
         return Optional.ofNullable(individualDateOfBirth).orElse(soleTraderDateOfBirth);
     }
-
-    @JsonIgnore
-    public boolean isCompanyOROrganisation() {
-        return this.isCompany() || this.isOrganisation();
-    }
 }
