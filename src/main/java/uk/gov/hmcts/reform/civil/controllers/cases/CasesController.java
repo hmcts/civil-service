01bb8558a9e15481201ecd9bfa1323bec7c48ad9
neeta-hmcts <neeta.shinde@hmcts.net>
2023-11-10 14:51:13 +0000

    added dto

--- src/main/java/uk/gov/hmcts/reform/civil/controllers/cases/CasesController.java
+++ src/main/java/uk/gov/hmcts/reform/civil/controllers/cases/CasesController.java
@@ -24,6 +24,7 @@ import uk.gov.hmcts.reform.ccd.client.model.SearchResult;
 import uk.gov.hmcts.reform.civil.exceptions.CaseDataInvalidException;
 import uk.gov.hmcts.reform.civil.model.bulkclaims.CaseworkerSubmitEventDTo;
 import uk.gov.hmcts.reform.civil.model.citizenui.DashboardResponse;
+import uk.gov.hmcts.reform.civil.model.citizenui.dto.DeadlineDateDetails;
 import uk.gov.hmcts.reform.civil.model.citizenui.dto.EventDto;
 import uk.gov.hmcts.reform.civil.model.citizenui.dto.RepaymentDecisionType;
 import uk.gov.hmcts.reform.civil.model.repaymentplan.ClaimantProposedPlan;
@@ -174,9 +175,9 @@ public class CasesController {
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "OK"),
         @ApiResponse(responseCode = "401", description = "Not Authorized")})
-    public ResponseEntity<LocalDate> calculateNewResponseDeadline(@RequestBody LocalDate extendedDeadline,
-                                                                  @RequestBody int plusDays) {
-        LocalDate calculatedDeadline = deadlineExtensionCalculatorService.calculateExtendedDeadline(extendedDeadline, plusDays);
+    public ResponseEntity<LocalDate> calculateNewResponseDeadline(@RequestBody DeadlineDateDetails deadlineDateDetails) {
+        LocalDate calculatedDeadline = deadlineExtensionCalculatorService.calculateExtendedDeadline(
+            deadlineDateDetails.getResponseDate(), deadlineDateDetails.getPlusDays());
         return new ResponseEntity<>(calculatedDeadline, HttpStatus.OK);
     }
 
---
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/dto/DeadlineDateDetails.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.civil.model.citizenui.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeadlineDateDetails {
+    private LocalDate responseDate;
+    private Integer plusDays;
+}
