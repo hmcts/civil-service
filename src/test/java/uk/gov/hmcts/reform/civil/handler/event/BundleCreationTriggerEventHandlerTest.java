4aceb9f043a80e7206732aa73f5c6d6c9f9003e3
sankaviv1 <sankavi.mohanraj@version1.com>
2023-11-13 17:17:03 +0000

    fix build

--- src/test/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandlerTest.java
@@ -191,8 +191,9 @@ class BundleCreationTriggerEventHandlerTest {
         // Given: Case details with all type of documents require for bundles
         BundleCreationTriggerEvent event = new BundleCreationTriggerEvent(1L);
         when(coreCaseDataService.getCase(1L)).thenReturn(caseDetails);
-        when(coreCaseDataService.startUpdate(event.getCaseId().toString(), CREATE_BUNDLE)).thenReturn(
-            StartEventResponse.builder().caseDetails(CaseDetailsBuilder.builder().data(caseData).build()).eventId("event1").token("test").build());
+        StartEventResponse response = StartEventResponse.builder()
+            .caseDetails(CaseDetailsBuilder.builder().data(caseData).build()).eventId("event1").token("test").build();
+        when(coreCaseDataService.startUpdate(event.getCaseId().toString(), CREATE_BUNDLE)).thenReturn(response);
         when(bundleCreationService.createBundle(event)).thenReturn(bundleCreateResponse);
         when(caseDetailsConverter.toCaseData(anyMap())).thenReturn(caseData);
 
@@ -279,8 +280,9 @@ class BundleCreationTriggerEventHandlerTest {
         // Given: Case details with all type of documents require for bundles
         BundleCreationTriggerEvent event = new BundleCreationTriggerEvent(1L);
         when(coreCaseDataService.getCase(1L)).thenReturn(caseDetails);
-        when(coreCaseDataService.startUpdate(event.getCaseId().toString(), CREATE_BUNDLE))
-            .thenReturn(StartEventResponse.builder().caseDetails(CaseDetailsBuilder.builder().data(caseData).build()).eventId("event1").token("test").build());
+        StartEventResponse response = StartEventResponse.builder()
+            .caseDetails(CaseDetailsBuilder.builder().data(caseData).build()).eventId("event1").token("test").build();
+        when(coreCaseDataService.startUpdate(event.getCaseId().toString(), CREATE_BUNDLE)).thenReturn(response);
         when(bundleCreationService.createBundle(event)).thenReturn(bundleCreateResponse);
         when(caseDetailsConverter.toCaseData(anyMap())).thenReturn(caseData);
 
