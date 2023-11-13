e3851f09da9a8575ee449275c0779debd3b0c902
sankaviv1 <95748224+sankaviv1@users.noreply.github.com>
2023-11-13 13:40:41 +0000

    CIV-10094 Case flags updates for new parties (#3229)
    * Added new ManageContactInformation callback handler and event and whitelisted it within the AllowedFlowState service.
    
    * Added new ManageContactInformation callback handler and event and whitelisted it within the AllowedFlowState service.
    
    * CIV-9992 add options for manage contact information
    
    * revert accidental commit
    
    * add utils test
    
    * update utils test
    
    * add test scenarios for handler
    
    * update handler tests
    
    * add IDs to party options
    
    * changed name of some variables
    
    * added chosenpartytype and chosenpartyid to determine what to show on page 2, wip on applicant/defendant pages
    
    * adding tests ManageContactInformationUtilsTest
    
    * changed some keys, updated tests
    
    * experts and witnesses form wip
    
    * show experts on ui
    
    * make id fields public
    
    * CIV-10094 create/update case flags names for new/edited individuals
    
    * add unit tests
    
    * Cherry picked the 3 main commits from CIV-3216
    Added partyID to dq witness and expert objects. Enabled the initialisation on party Ids in DQ witnesses and experts and copied partyIDs into their relative case level counterparts
    
    Moved changes behind hmc toggle
    
    Fixed duplicate append call
    
    * Updated experts and witnesses to have mappings
    
    * changed utils class back to static and removed mocks
    
    * Fixed witnesses
    
    * fixed litigation friend
    
    * append with party ID
    
    * remove unnecessary changes
    
    * remove unnecessary tests
    
    * update tests for party ids
    
    * fixed capability to delete all experts/witnesses and add new expert/witnesses when never existed before
    
    * postcode
    
    * removed unnecessary lines and imports
    
    * fix conflicts
    
    * fixed data going missing for applicant 1 for party id
    
    * fixed claim details tab
    
    * added ccd data store image in charts
    
    * fixed test
    
    * sonar fix
    
    ---------
    
    Co-authored-by: GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
    Co-authored-by: sherlynkhaw <sherlyn.khaw@evoco.co.uk>
    Co-authored-by: Gareth Lancaster <90632240+Gareth40343@users.noreply.github.com>
    Co-authored-by: TurkingtonL <lillie.turkington@HMCTS.net>
    Co-authored-by: AhsanZX97 <ahsan.zia1@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/civil/utils/CaseFlagsInitialiser.java
+++ src/main/java/uk/gov/hmcts/reform/civil/utils/CaseFlagsInitialiser.java
@@ -5,16 +5,27 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.service.OrganisationService;
 
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.YES;
+import static uk.gov.hmcts.reform.civil.utils.CaseFlagUtils.APPLICANT_ONE;
+import static uk.gov.hmcts.reform.civil.utils.CaseFlagUtils.APPLICANT_ONE_LITIGATION_FRIEND;
+import static uk.gov.hmcts.reform.civil.utils.CaseFlagUtils.APPLICANT_TWO;
+import static uk.gov.hmcts.reform.civil.utils.CaseFlagUtils.APPLICANT_TWO_LITIGATION_FRIEND;
+import static uk.gov.hmcts.reform.civil.utils.CaseFlagUtils.RESPONDENT_ONE;
+import static uk.gov.hmcts.reform.civil.utils.CaseFlagUtils.RESPONDENT_ONE_LITIGATION_FRIEND;
+import static uk.gov.hmcts.reform.civil.utils.CaseFlagUtils.RESPONDENT_TWO;
+import static uk.gov.hmcts.reform.civil.utils.CaseFlagUtils.RESPONDENT_TWO_LITIGATION_FRIEND;
 import static uk.gov.hmcts.reform.civil.utils.CaseFlagUtils.addApplicantExpertAndWitnessFlagsStructure;
 import static uk.gov.hmcts.reform.civil.utils.CaseFlagUtils.addRespondentDQPartiesFlagStructure;
+import static uk.gov.hmcts.reform.civil.utils.CaseFlagUtils.createOrUpdateFlags;
 
 @Component
 @AllArgsConstructor
 public class CaseFlagsInitialiser {
 
     private final FeatureToggleService featureToggleService;
+    private final OrganisationService organisationService;
 
     public void initialiseCaseFlags(CaseEvent caseEvent, CaseData.CaseDataBuilder dataBuilder) {
         if (!featureToggleService.isCaseFlagsEnabled()) {
@@ -42,6 +53,10 @@ public class CaseFlagsInitialiser {
                 addApplicantExpertAndWitnessFlagsStructure(dataBuilder, caseData);
                 break;
             }
+            case MANAGE_CONTACT_INFORMATION: {
+                createOrUpdateFlags(dataBuilder, caseData, organisationService);
+                break;
+            }
             default:
         }
     }
@@ -62,26 +77,26 @@ public class CaseFlagsInitialiser {
         dataBuilder
             .respondent1LitigationFriend(
                 CaseFlagUtils.updateLitFriend(
-                    "Respondent 1 Litigation Friend",
+                    RESPONDENT_ONE_LITIGATION_FRIEND,
                     caseData.getRespondent1LitigationFriend()
                 ))
             .respondent2LitigationFriend(
                 CaseFlagUtils.updateLitFriend(
-                    "Respondent 2 Litigation Friend",
+                    RESPONDENT_TWO_LITIGATION_FRIEND,
                     caseData.getRespondent2LitigationFriend()
                 ));
     }
 
     private void initialiseApplicantAndRespondentFlags(CaseData.CaseDataBuilder dataBuilder, CaseData caseData) {
         dataBuilder
-            .applicant1(CaseFlagUtils.updateParty("Applicant 1", caseData.getApplicant1()))
-            .applicant2(CaseFlagUtils.updateParty("Applicant 2", caseData.getApplicant2()))
-            .respondent1(CaseFlagUtils.updateParty("Respondent 1", caseData.getRespondent1()))
-            .respondent2(CaseFlagUtils.updateParty("Respondent 2", caseData.getRespondent2()))
+            .applicant1(CaseFlagUtils.updateParty(APPLICANT_ONE, caseData.getApplicant1()))
+            .applicant2(CaseFlagUtils.updateParty(APPLICANT_TWO, caseData.getApplicant2()))
+            .respondent1(CaseFlagUtils.updateParty(RESPONDENT_ONE, caseData.getRespondent1()))
+            .respondent2(CaseFlagUtils.updateParty(RESPONDENT_TWO, caseData.getRespondent2()))
             .applicant1LitigationFriend(CaseFlagUtils.updateLitFriend(
-                "Applicant 1 Litigation Friend", caseData.getApplicant1LitigationFriend()))
+                APPLICANT_ONE_LITIGATION_FRIEND, caseData.getApplicant1LitigationFriend()))
             .applicant2LitigationFriend(CaseFlagUtils.updateLitFriend(
-                "Applicant 2 Litigation Friend", caseData.getApplicant2LitigationFriend()));
+                APPLICANT_TWO_LITIGATION_FRIEND, caseData.getApplicant2LitigationFriend()));
     }
 
     private boolean shouldReinitialiseRespondentDQFlags(CaseData caseData) {
