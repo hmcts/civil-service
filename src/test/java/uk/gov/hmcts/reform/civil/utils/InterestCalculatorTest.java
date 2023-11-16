8f4334b740873119e055afe0b00e2027a9c91dbe
Mark Drummond <mark.drummond@version1.com>
2023-11-16 10:46:15 +0000

    tests bulk claim interest calculation

--- src/test/java/uk/gov/hmcts/reform/civil/utils/InterestCalculatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/utils/InterestCalculatorTest.java
@@ -1,6 +1,10 @@
 package uk.gov.hmcts.reform.civil.utils;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.interestcalc.InterestClaimFromType;
@@ -9,18 +13,27 @@ import uk.gov.hmcts.reform.civil.model.interestcalc.InterestClaimUntilType;
 import uk.gov.hmcts.reform.civil.model.interestcalc.SameRateInterestSelection;
 import uk.gov.hmcts.reform.civil.model.interestcalc.SameRateInterestType;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+import uk.gov.hmcts.reform.civil.service.Time;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
+@ExtendWith(MockitoExtension.class)
 public class InterestCalculatorTest {
 
-    private final InterestCalculator interestCalculator = new InterestCalculator();
+    @Mock
+    private Time time;
+    @InjectMocks
+    private InterestCalculator interestCalculator;
 
     @Test
     public void shouldReturnValidInterestAmountByDate() {
+        given(time.now()).willReturn(LocalDateTime.now().withHour(13).withMinute(59));
         assertThat(interestCalculator.calculateInterestByDate(
             new BigDecimal("1000"),
             BigDecimal.valueOf(8),
@@ -38,8 +51,9 @@ public class InterestCalculatorTest {
         assertThat(actual).isZero();
     }
 
-    // @Test
+    @Test
     public void shouldReturnZeroInterestRateWhenSameRateInterestAndSubmitDateIsChoosen() {
+        given(time.now()).willReturn(LocalDateTime.of(2022, 11, 15, 13, 0));
         CaseData caseData = new CaseDataBuilder().atStateClaimDraft()
             .claimInterest(YesOrNo.YES)
             .interestClaimOptions(InterestClaimOptions.SAME_RATE_INTEREST)
@@ -53,8 +67,9 @@ public class InterestCalculatorTest {
         assertThat(actual).isZero();
     }
 
-    // @Test
+    @Test
     public void shouldReturnZeroInterestRateWhenSameRateInterestDifferentRateAndSubmitDateIsChoosen() {
+        given(time.now()).willReturn(LocalDateTime.of(2022, 11, 15, 13, 0));
         CaseData caseData = new CaseDataBuilder().atStateClaimDraft()
             .claimInterest(YesOrNo.YES)
             .interestClaimOptions(InterestClaimOptions.SAME_RATE_INTEREST)
@@ -70,8 +85,9 @@ public class InterestCalculatorTest {
         assertThat(actual).isZero();
     }
 
-    // @Test
+    @Test
     public void shouldReturnZeroInterestRateWhenSameRateInterestDifferentRateAndSpecificDateIsChoosen() {
+        given(time.now()).willReturn(LocalDateTime.now().withHour(13).withMinute(59));
         CaseData caseData = new CaseDataBuilder().atStateClaimDraft()
             .claimInterest(YesOrNo.YES)
             .interestClaimOptions(InterestClaimOptions.SAME_RATE_INTEREST)
@@ -81,16 +97,17 @@ public class InterestCalculatorTest {
                                            .differentRate(BigDecimal.valueOf(10)).build())
             .interestClaimFrom(InterestClaimFromType.FROM_A_SPECIFIC_DATE)
             .interestClaimUntil(InterestClaimUntilType.UNTIL_CLAIM_SUBMIT_DATE)
-            .interestFromSpecificDate(LocalDate.now().minusDays(1))
+            .interestFromSpecificDate(LocalDate.now())
             .totalClaimAmount(BigDecimal.valueOf(5000))
             .build();
 
         BigDecimal actual = interestCalculator.calculateInterest(caseData);
-        assertThat(actual).isEqualTo(BigDecimal.valueOf(1.37));
+        assertThat(actual).isZero();
     }
 
     @Test
     public void shouldReturnValidInterestRateWhenSameRateInterestAndSpecificDateIsChoosen() {
+        given(time.now()).willReturn(LocalDateTime.now().withHour(13).withMinute(59));
         CaseData caseData = new CaseDataBuilder().atStateClaimDraft()
             .claimInterest(YesOrNo.YES)
             .interestClaimOptions(InterestClaimOptions.SAME_RATE_INTEREST)
@@ -98,12 +115,12 @@ public class InterestCalculatorTest {
                                            .sameRateInterestType(SameRateInterestType.SAME_RATE_INTEREST_8_PC).build())
             .interestClaimFrom(InterestClaimFromType.FROM_A_SPECIFIC_DATE)
             .interestClaimUntil(InterestClaimUntilType.UNTIL_CLAIM_SUBMIT_DATE)
-            .interestFromSpecificDate(LocalDate.now().minusDays(1))
+            .interestFromSpecificDate(LocalDate.now().minusDays(6))
             .totalClaimAmount(BigDecimal.valueOf(5000))
             .build();
 
         BigDecimal actual = interestCalculator.calculateInterest(caseData);
-        assertThat(actual).isGreaterThanOrEqualTo(BigDecimal.valueOf(1.10));
+        assertThat(actual).isEqualTo(BigDecimal.valueOf(6.60).setScale(2, RoundingMode.UNNECESSARY));
     }
 
     @Test
@@ -116,5 +133,51 @@ public class InterestCalculatorTest {
 
         BigDecimal actual = interestCalculator.calculateInterest(caseData);
         assertThat(actual).isGreaterThanOrEqualTo(BigDecimal.valueOf(500));
+    }
+
+    @Test
+    public void shouldReturnInterestRateBulkClaim_InterestSelectedBefore4pm() {
+        // when before 4pm interest will be days multiplied by daily rate of interest. 5 * 6 = 30
+        given(time.now()).willReturn(LocalDateTime.of(2023, 11, 15, 15, 0));
+        CaseData caseData = new CaseDataBuilder().atStateClaimDraft()
+            .claimInterest(YesOrNo.YES)
+            .interestFromSpecificDate(LocalDate.of(2023, 11, 20))
+            .sameRateInterestSelection(SameRateInterestSelection.builder()
+                                           .differentRate(BigDecimal.valueOf(6L))
+                                           .build())
+            .build();
+
+        BigDecimal result = interestCalculator.calculateBulkInterest(caseData);
+
+        assertThat(result).isEqualTo(BigDecimal.valueOf(30));
+    }
+
+    @Test
+    public void shouldReturnInterestRateBulkClaim_InterestSelectedAfter4pm() {
+        // when after 4pm interest will be days +1, multiplied by daily rate of interest. (5+1) * 6 = 36
+        given(time.now()).willReturn(LocalDateTime.of(2023, 11, 15, 18, 0));
+        CaseData caseData = new CaseDataBuilder().atStateClaimDraft()
+            .claimInterest(YesOrNo.YES)
+            .interestFromSpecificDate(LocalDate.of(2023, 11, 20))
+            .sameRateInterestSelection(SameRateInterestSelection.builder()
+                                           .differentRate(BigDecimal.valueOf(6L))
+                                           .build())
+            .build();
+
+        BigDecimal result = interestCalculator.calculateBulkInterest(caseData);
+
+        assertThat(result).isEqualTo(BigDecimal.valueOf(36));
+    }
+
+    @Test
+    public void shouldReturnZeroInterestRateBulkClaim_noInterestSelected() {
+        CaseData caseData = new CaseDataBuilder().atStateClaimDraft()
+            .claimInterest(YesOrNo.NO)
+            .interestFromSpecificDate(null)
+            .sameRateInterestSelection(null)
+            .build();
+
+        BigDecimal result = interestCalculator.calculateBulkInterest(caseData);
+        assertThat(result).isZero();
     }
 }
