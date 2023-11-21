5210db0332b8f68e289c663363c15b21cbe3de48
Astha Malviya <astha.malviya@HMCTS.NET>
2023-11-21 14:57:53 +0000

    Adding values to bootstrap

--- infrastructure/tf-kv-secrets-cmc.tf
+++ infrastructure/tf-kv-secrets-cmc.tf
@@ -3,22 +3,19 @@ data "azurerm_key_vault" "cmc_vault" {
   resource_group_name = "cmc-${var.env}"
 }
 
-data "azurerm_key_vault_secret" "db_password_v11_secret" {
+resource "azurerm_key_vault_secret" "cmc-db-password-v15" {
+  name         = "cmc-db-password-v15"
+  value        = module.db-v15.password
   key_vault_id = data.azurerm_key_vault.cmc_vault.id
-  name         = "cmc-db-password-v11"
 }
 
-resource "azurerm_key_vault_secret" "civil_db_password__v11_secret" {
-  name         = "cmc-db-password-v11"
-  value        = data.azurerm_key_vault_secret.db_password_v11_secret.value
-  key_vault_id = module.key-vault.key_vault_id
+resource "azurerm_key_vault_secret" "cmc-db-username-v15" {
+  name         = "cmc-db-username-v15"
+  value        = module.db-v15.username
+  key_vault_id = data.azurerm_key_vault.cmc_vault.id}
 
-  content_type = "secret"
-  tags = merge(var.common_tags, {
-    "source" : "Vault ${data.azurerm_key_vault.cmc_vault.name}"
-  })
-
-  depends_on = [
-    module.key-vault
-  ]
+resource "azurerm_key_vault_secret" "cmc-db-host-v15" {
+  name         = "cmc-db-host-v15"
+  value        = module.db-v15.fqdn
+  key_vault_id = data.azurerm_key_vault.cmc_vault.id
 }
