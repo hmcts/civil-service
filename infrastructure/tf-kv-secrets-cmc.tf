0d73216149d4c9ba06dbe757c941359d23a6b398
Astha Malviya <astha.malviya@HMCTS.NET>
2023-11-21 15:14:59 +0000

    fixing terraform

--- infrastructure/tf-kv-secrets-cmc.tf
+++ infrastructure/tf-kv-secrets-cmc.tf
@@ -3,6 +3,26 @@ data "azurerm_key_vault" "cmc_vault" {
   resource_group_name = "cmc-${var.env}"
 }
 
+data "azurerm_key_vault_secret" "db_password_v11_secret" {
+  key_vault_id = data.azurerm_key_vault.cmc_vault.id
+  name         = "cmc-db-password-v11"
+}
+
+resource "azurerm_key_vault_secret" "civil_db_password__v11_secret" {
+  name         = "cmc-db-password-v11"
+  value        = data.azurerm_key_vault_secret.db_password_v11_secret.value
+  key_vault_id = module.key-vault.key_vault_id
+
+  content_type = "secret"
+  tags = merge(var.common_tags, {
+    "source" : "Vault ${data.azurerm_key_vault.cmc_vault.name}"
+  })
+
+  depends_on = [
+    module.key-vault
+  ]
+}
+
 resource "azurerm_key_vault_secret" "cmc-db-password-v15" {
   name         = "cmc-db-password-v15"
   value        = module.db-v15.password
