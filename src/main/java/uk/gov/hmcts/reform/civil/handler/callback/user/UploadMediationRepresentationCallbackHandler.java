8249302b761ae2a85b35c5c72a8bc949ce420f01
sankaviv1 <sankavi.mohanraj@version1.com>
2023-11-23 12:05:08 +0000

    CIV-11591 Update mediation representation event

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/UploadMediationRepresentationCallbackHandler.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.reform.civil.handler.callback.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.civil.callback.Callback;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UPLOAD_MEDIATION_REPRESENTATION;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UploadMediationRepresentationCallbackHandler extends CallbackHandler {
+
+    private static final List<CaseEvent> EVENTS = List.of(UPLOAD_MEDIATION_REPRESENTATION);
+
+    @Override
+    protected Map<String, Callback> callbacks() {
+        return Map.of(
+            callbackKey(ABOUT_TO_START), this::emptyCallbackResponse,
+            callbackKey(ABOUT_TO_SUBMIT), this::emptyCallbackResponse
+        );
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+}
