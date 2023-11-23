240272d1491fc5b2e26bdc10dac617b47a47c1a3
Astha Malviya <astha.malviya@HMCTS.NET>
2023-11-23 12:24:20 +0000

    removing password secret too

--- infrastructure/tf-kv-secrets-cmc.tf
+++ infrastructure/tf-kv-secrets-cmc.tf
@@ -23,26 +23,6 @@ resource "azurerm_key_vault_secret" "civil_db_password__v11_secret" {
   ]
 }
 
-data "azurerm_key_vault_secret" "db_password_v15_secret" {
-  key_vault_id = data.azurerm_key_vault.cmc_vault.id
-  name         = "cmc-db-password-v15"
-}
-
-resource "azurerm_key_vault_secret" "civil_db_password__v15_secret" {
-  name         = "cmc-db-password-v15"
-  value        = data.azurerm_key_vault_secret.db_password_v15_secret.value
-  key_vault_id = module.key-vault.key_vault_id
-
-  content_type = "secret"
-  tags = merge(var.common_tags, {
-    "source" : "Vault ${data.azurerm_key_vault.cmc_vault.name}"
-  })
-
-  depends_on = [
-    module.key-vault
-  ]
-}
-
 data "azurerm_key_vault_secret" "db_username_v15_secret" {
   key_vault_id = data.azurerm_key_vault.cmc_vault.id
   name         = "cmc-db-username-v15"
