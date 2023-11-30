4aa19640bd52ed69102485fd87a4ab8834eab15a
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2023-11-30 14:52:21 +0000

    CIV-11161 test code updated

--- src/test/java/uk/gov/hmcts/reform/civil/service/docmosis/utils/ClaimantResponseUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/docmosis/utils/ClaimantResponseUtilsTest.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.civil.service.docmosis.utils;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
 import uk.gov.hmcts.reform.civil.enums.PaymentFrequencyClaimantResponseLRspec;
 import uk.gov.hmcts.reform.civil.enums.PaymentType;
 import uk.gov.hmcts.reform.civil.model.CaseData;
@@ -8,7 +10,7 @@ import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-
+import org.junit.jupiter.params.ParameterizedTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.civil.service.docmosis.utils.ClaimantResponseUtils.getClaimantFinalRepaymentDate;
 import static uk.gov.hmcts.reform.civil.service.docmosis.utils.ClaimantResponseUtils.getClaimantRepaymentType;
@@ -30,20 +32,15 @@ public class ClaimantResponseUtilsTest {
         assertThat(finalDate).isNotNull();
     }
 
-    @Test
-    void shouldReturnRepaymentTypeDescForDefendant() {
-        CaseData caseData = CaseDataBuilder.builder()
-                .applicant1SuggestInstalmentsPaymentAmountForDefendantSpec(BigDecimal.valueOf(100))
-                .issueDate(LocalDate.now())
-                .applicant1RepaymentOptionForDefendantSpec(PaymentType.REPAYMENT_PLAN)
-                .applicant1SuggestInstalmentsFirstRepaymentDateForDefendantSpec(LocalDate.now())
-                .applicant1SuggestInstalmentsRepaymentFrequencyForDefendantSpec(PaymentFrequencyClaimantResponseLRspec.ONCE_TWO_WEEKS)
-                .totalClaimAmount(BigDecimal.valueOf(1000))
+    @ParameterizedTest
+    @CsvSource({"IMMEDIATELY,Immediately", "SET_DATE,By a set date", "REPAYMENT_PLAN,By installments"})
+    void ShouldReturnClaimantRepaymentOption(PaymentType input, String expectedOutput) {
+        CaseData caseData = CaseData.builder()
+                .applicant1RepaymentOptionForDefendantSpec(input)
                 .build();
 
-        String repaymentTypeDesc = getClaimantRepaymentType(caseData);
-        assertThat(repaymentTypeDesc).isNotNull();
-        assertThat(repaymentTypeDesc).isEqualTo("By installments");
+        String actualOutput = getClaimantRepaymentType(caseData);
+        Assertions.assertEquals(expectedOutput,actualOutput);
     }
 
     @Test
