7cb013367fa99975199d61a689a0f87e0450b9b8
sankaviv1 <sankavi.mohanraj@version1.com>
2023-11-30 09:31:55 +0000

    tweak config

--- src/main/java/uk/gov/hmcts/reform/civil/config/properties/mediation/MediationCSVEmailConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/properties/mediation/MediationCSVEmailConfiguration.java
@@ -1,20 +1,14 @@
 package uk.gov.hmcts.reform.civil.config.properties.mediation;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
+@ConfigurationProperties(prefix = "mediation.emails")
 public class MediationCSVEmailConfiguration {
 
-    private final String sender;
-    private  final String recipient;
-
-    public MediationCSVEmailConfiguration(
-        @Value("${mediation.emails.sender}") String sender,
-        @Value("${mediation.emails.recipient}") String recipient) {
-        this.sender = sender;
-        this.recipient = recipient;
-    }
+    private String sender;
+    private String recipient;
 }
