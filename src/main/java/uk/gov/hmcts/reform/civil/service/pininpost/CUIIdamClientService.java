f97a67c04d0e40d42188aecb1a1d5f8e54e25487
neeta-hmcts <neeta.shinde@hmcts.net>
2023-12-01 15:00:38 +0000

    fixed integration test.

--- src/main/java/uk/gov/hmcts/reform/civil/service/pininpost/CUIIdamClientService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/pininpost/CUIIdamClientService.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.reform.civil.service.pininpost;
 
 import feign.Response;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.config.CMCPinVerifyConfiguration;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
@@ -10,16 +10,27 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 @Service
-@AllArgsConstructor
 public class CUIIdamClientService {
 
     private final IdamApi idamApi;
     private final CMCPinVerifyConfiguration cmcPinVerifyConfiguration;
 
+    @Autowired
+    public CUIIdamClientService(IdamApi idamApi, CMCPinVerifyConfiguration cmcPinVerifyConfiguration) {
+        this.idamApi = idamApi;
+        this.cmcPinVerifyConfiguration = cmcPinVerifyConfiguration;
+    }
+
     public int authenticatePinUser(String pin, String state) {
 
         final String encodedRedirectUrl = URLEncoder.encode(cmcPinVerifyConfiguration.getRedirectUrl() + "/receiver", StandardCharsets.UTF_8);
-        final Response response = idamApi.authenticatePinUser(pin, cmcPinVerifyConfiguration.getClientId(), encodedRedirectUrl, state);
-        return response.status();
+        try (Response response = idamApi.authenticatePinUser(
+            pin,
+            cmcPinVerifyConfiguration.getClientId(),
+            encodedRedirectUrl,
+            state
+        )) {
+            return response.status();
+        }
     }
 }
