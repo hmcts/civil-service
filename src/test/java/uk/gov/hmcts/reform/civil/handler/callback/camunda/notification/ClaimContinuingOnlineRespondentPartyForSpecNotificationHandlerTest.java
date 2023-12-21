a9fe9febec06ceb564e0963a1e0291adce907d5c
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-12-21 11:48:59 +0100

    CIV-12028 : Fix tests

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimContinuingOnlineRespondentPartyForSpecNotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimContinuingOnlineRespondentPartyForSpecNotificationHandlerTest.java
@@ -20,11 +20,8 @@ import uk.gov.hmcts.reform.civil.model.DefendantPinToPostLRspec;
 import uk.gov.hmcts.reform.civil.sampledata.CallbackParamsBuilder;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.sampledata.PartyBuilder;
-import uk.gov.hmcts.reform.civil.service.BulkPrintService;
-import uk.gov.hmcts.reform.civil.service.DeadlinesCalculator;
+import uk.gov.hmcts.reform.civil.service.*;
 import uk.gov.hmcts.reform.civil.notify.NotificationService;
-import uk.gov.hmcts.reform.civil.service.OrganisationService;
-import uk.gov.hmcts.reform.civil.service.Time;
 import uk.gov.hmcts.reform.civil.service.docmosis.pip.PiPLetterGenerator;
 import uk.gov.hmcts.reform.civil.prd.model.Organisation;
 
@@ -77,7 +74,8 @@ public class ClaimContinuingOnlineRespondentPartyForSpecNotificationHandlerTest 
     private BulkPrintService bulkPrintService;
     @MockBean
     private PiPLetterGenerator pipLetterGenerator;
-
+    @MockBean
+    private SealedClaimFromDownloadService sealedClaimFromDownloadService;
     @MockBean
     private Time time;
 
@@ -138,6 +136,7 @@ public class ClaimContinuingOnlineRespondentPartyForSpecNotificationHandlerTest 
         void shouldGenerateAndPrintLetterSuccessfully() {
             // Given
             given(pipLetterGenerator.downloadLetter(any())).willReturn(LETTER_CONTENT);
+            given(sealedClaimFromDownloadService.downloadDocument(any(),any())).willReturn(LETTER_CONTENT);
             CaseData caseData = getCaseData("testorg@email.com");
             CallbackParams params = getCallbackParams(caseData);
 
