38b6e3ddd9605a1c8d4d08c3bda6ed056b7ebcaa
Manish Garg <manish.garg@hmcts.net>
2023-12-21 11:18:23 +0000

    CIV-8903 Revert unwanted change in DATE_TIME_FORMAT_CMC

--- src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/DtoFieldFormat.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/DtoFieldFormat.java
@@ -1,12 +1,10 @@
 package uk.gov.hmcts.reform.civil.model.citizenui;
 
-import java.time.LocalDateTime;
-
 public class DtoFieldFormat {
 
     public static final String DATE_FORMAT = "yyyy-MM-dd";
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
-    public static final String DATE_TIME_FORMAT_CMC = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS";
+    public static final String DATE_TIME_FORMAT_CMC = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS";
 
     private DtoFieldFormat() {
 
