6ff760f19edd2e65b7144f4792f935bb4c932452
Astha Malviya <astha.malviya@HMCTS.NET>
2024-01-03 10:28:13 +0000

    checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/civil/utils/PredicateUtils.java
+++ src/main/java/uk/gov/hmcts/reform/civil/utils/PredicateUtils.java
@@ -1,14 +1,10 @@
 package uk.gov.hmcts.reform.civil.utils;
 
-import uk.gov.hmcts.reform.civil.enums.AllocatedTrack;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.SmallClaimMedicalLRspec;
 
-import java.util.Optional;
 import java.util.function.Predicate;
 
-import static uk.gov.hmcts.reform.civil.enums.CaseCategory.SPEC_CLAIM;
 import static uk.gov.hmcts.reform.civil.enums.CaseCategory.UNSPEC_CLAIM;
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.YES;
@@ -34,20 +30,21 @@ public class PredicateUtils {
         caseData.getRespondent2AcknowledgeNotificationDate() != null;
 
     public static final Predicate<CaseData> defendant1ResponseExists = caseData ->
-        UNSPEC_CLAIM.equals(caseData.getCaseAccessCategory()) ? (caseData.getRespondent1ResponseDate() != null && caseData.getRespondent1ClaimResponseType() != null) : caseData.getRespondent1ResponseDate() != null;
-
+        UNSPEC_CLAIM.equals(caseData.getCaseAccessCategory())
+            ? (caseData.getRespondent1ResponseDate() != null && caseData.getRespondent1ClaimResponseType() != null)
+            : caseData.getRespondent1ResponseDate() != null;
 
     public static final Predicate<CaseData> defendant2ResponseExists = caseData ->
         caseData.getRespondent2() != null && caseData.getRespondent2ResponseDate() != null
-        && caseData.getRespondent2SameLegalRepresentative() != YesOrNo.YES;
+            && caseData.getRespondent2SameLegalRepresentative() != YesOrNo.YES;
 
     public static final Predicate<CaseData> defendant2DivergentResponseExists = caseData ->
         caseData.getRespondent2() != null && (caseData.getRespondent2ResponseDate() != null
             || (caseData.getRespondent2SameLegalRepresentative() == YesOrNo.YES
-            && caseData.getRespondent1ResponseDate() != null && caseData.getRespondentResponseIsSame()  == NO));
+            && caseData.getRespondent1ResponseDate() != null && caseData.getRespondentResponseIsSame() == NO));
 
-    public static final Predicate<CaseData> defendant1v2SameSolicitorSameResponse =  caseData ->
+    public static final Predicate<CaseData> defendant1v2SameSolicitorSameResponse = caseData ->
         caseData.getRespondent1ResponseDate() != null && caseData.getRespondent2() != null
             && caseData.getRespondent2SameLegalRepresentative() == YES
-            && caseData.getRespondentResponseIsSame()  == YES;
+            && caseData.getRespondentResponseIsSame() == YES;
 }
