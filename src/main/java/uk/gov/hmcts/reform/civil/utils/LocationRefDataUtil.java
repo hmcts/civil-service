87a65eb7e21e4182158be2899a30786ab822c354
TortolaNavarroD <dario.tortolanavarro@version1.com>
2024-01-03 16:20:55 +0100

    SNI-5142 added login for court location not found

--- src/main/java/uk/gov/hmcts/reform/civil/utils/LocationRefDataUtil.java
+++ src/main/java/uk/gov/hmcts/reform/civil/utils/LocationRefDataUtil.java
@@ -4,10 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.CourtLocation;
+import uk.gov.hmcts.reform.civil.model.defaultjudgment.CaseLocationCivil;
 import uk.gov.hmcts.reform.civil.referencedata.LocationRefDataService;
 import uk.gov.hmcts.reform.civil.referencedata.model.LocationRefData;
 
 import java.util.List;
+import java.util.Optional;
 
 import static uk.gov.hmcts.reform.civil.enums.CaseCategory.SPEC_CLAIM;
 import static uk.gov.hmcts.reform.civil.service.robotics.utils.RoboticsDataUtil.CIVIL_COURT_TYPE_ID;
@@ -41,7 +44,12 @@ public class LocationRefDataUtil {
                         ? locationRefData.getCourtLocationCode() : locationRefData.getCourtName())
                     .orElse("");
             } else {
-                log.info("Court location not found");
+                log.info("Case {}, court location not found for base location {}",
+                         caseData.getLegacyCaseReference(),
+                         Optional.ofNullable(caseData.getCourtLocation())
+                             .map(CourtLocation::getCaseLocation)
+                             .map(CaseLocationCivil::getBaseLocation)
+                             .orElse("null"));
                 return "";
             }
         }
