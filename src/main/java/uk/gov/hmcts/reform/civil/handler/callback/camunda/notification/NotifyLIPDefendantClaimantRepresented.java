01d489bc223c26dfbafa13d7fa982c8db441a4c6
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2024-01-05 14:03:31 +0000

    CIV-11697 Set Up Case Event, Handler

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/NotifyLIPDefendantClaimantRepresented.java
@@ -0,0 +1,74 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.notification;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.Callback;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.notify.NotificationService;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_DEFENDANT_LIP_CLAIMANT_REPRESENTED;
+
+@Service
+@RequiredArgsConstructor
+public class NotifyLIPDefendantClaimantRepresented extends CallbackHandler implements NotificationData {
+
+    private final NotificationService notificationService;
+    private final NotificationsProperties notificationsProperties;
+    private static final String REFERENCE_TEMPLATE = "notify-defendant-lip-claimant-represented-notification-%s";
+    public static final String TASK_ID = "NotifyDefendantLipClaimantRepresented";
+    private final Map<String, Callback> callbackMap = Map.of(
+        callbackKey(ABOUT_TO_SUBMIT), this::notifyDefendantLIP
+    );
+
+    @Override
+    protected Map<String, Callback> callbacks() {
+        return callbackMap;
+    }
+
+    @Override
+    public String camundaActivityId(CallbackParams callbackParams) {
+        return TASK_ID;
+    }
+
+    private CallbackResponse notifyDefendantLIP(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+
+        if (StringUtils.isNotEmpty(caseData.getRespondent1().getPartyEmail())) {
+            notificationService.sendMail(
+                caseData.getRespondent1().getPartyEmail(),
+                notificationsProperties.getNotifyRespondentLipForClaimantRepresentedTemplate(),
+                addProperties(caseData),
+                String.format(REFERENCE_TEMPLATE, caseData.getLegacyCaseReference())
+            );
+        }
+        return AboutToStartOrSubmitCallbackResponse.builder().build();
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return Collections.singletonList(
+            NOTIFY_DEFENDANT_LIP_CLAIMANT_REPRESENTED
+        );
+    }
+
+    @Override
+    public Map<String, String> addProperties(CaseData caseData) {
+        return Map.of(
+            RESPONDENT_NAME, caseData.getRespondent1().getPartyName(),
+            CLAIM_REFERENCE_NUMBER, caseData.getLegacyCaseReference(),
+            CLAIMANT_NAME, caseData.getApplicant1().getPartyName()
+        );
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/NotifyLIPDefendantClaimantRepresentedTest.java
@@ -0,0 +1,91 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.notification;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.Party;
+import uk.gov.hmcts.reform.civil.notify.NotificationService;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+import uk.gov.hmcts.reform.civil.sampledata.CallbackParamsBuilder;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_DEFENDANT_LIP_CLAIMANT_REPRESENTED;
+
+@SpringBootTest(classes = {
+    NotifyLIPDefendantClaimantRepresented.class,
+    JacksonAutoConfiguration.class,
+})
+public class NotifyLIPDefendantClaimantRepresentedTest extends BaseCallbackHandlerTest {
+
+    @Autowired
+    private NotifyLIPDefendantClaimantRepresented notificationHandler;
+    @MockBean
+    private NotificationService notificationService;
+    @MockBean
+    NotificationsProperties notificationsProperties;
+    @Captor
+    private ArgumentCaptor<String> targetEmail;
+    @Captor
+    private ArgumentCaptor<String> emailTemplate;
+    @Captor
+    private ArgumentCaptor<Map<String, String>> notificationDataMap;
+    @Captor
+    private ArgumentCaptor<String> reference;
+
+    @Nested
+    class AboutToSubmitCallback {
+
+        private static final String DEFENDANT_EMAIL_ADDRESS = "defendantmail@hmcts.net";
+        private static final String DEFENDANT_PARTY_NAME = "ABC ABC";
+        private static final String REFERENCE_NUMBER = "8372942374";
+        private static final String EMAIL_TEMPLATE = "test-notification-id";
+        private static final String CLAIMANT_ORG_NAME = "Org Name";
+
+        @BeforeEach
+        void setUp() {
+            given(notificationsProperties.getNotifyRespondentLipForClaimantRepresentedTemplate()).willReturn(EMAIL_TEMPLATE);
+        }
+
+        @Test
+        void shouldSendNotificationToDefendantLip_whenEventIsCalledAndDefendantHasEmail() {
+            //Given
+            CaseData caseData = CaseData.builder()
+                .respondent1(Party.builder().type(Party.Type.COMPANY).companyName(DEFENDANT_PARTY_NAME).partyEmail(
+                    DEFENDANT_EMAIL_ADDRESS).build())
+                .applicant1(Party.builder().type(Party.Type.COMPANY).companyName(CLAIMANT_ORG_NAME).build())
+                .legacyCaseReference(REFERENCE_NUMBER)
+                .addApplicant2(YesOrNo.NO)
+                .addRespondent2(YesOrNo.NO)
+                .build();
+            CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_SUBMIT, caseData)
+                .request(CallbackRequest.builder().eventId(NOTIFY_DEFENDANT_LIP_CLAIMANT_REPRESENTED.name()).build()).build();
+            //When
+            notificationHandler.handle(params);
+            //Then
+            verify(notificationService, times(1)).sendMail(targetEmail.capture(),
+                                                           emailTemplate.capture(),
+                                                           notificationDataMap.capture(), reference.capture()
+            );
+            assertThat(targetEmail.getAllValues().get(0)).isEqualTo(DEFENDANT_EMAIL_ADDRESS);
+            assertThat(emailTemplate.getAllValues().get(0)).isEqualTo(EMAIL_TEMPLATE);
+        }
+
+    }
+}
