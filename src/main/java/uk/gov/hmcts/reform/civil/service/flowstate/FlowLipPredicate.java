f014510384e005114976bb6f12dfe8075dbd3814
jeswanth-hmcts <134285996+jeswanth-hmcts@users.noreply.github.com>
2024-01-11 14:07:14 +0000

    CIV-11523  Revoking access to claim for claimant LiP after NoC and representative assigned (#3834)

--- src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/FlowLipPredicate.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/FlowLipPredicate.java
@@ -11,7 +11,7 @@ public class FlowLipPredicate {
     }
 
     public static final Predicate<CaseData> isLipCase = caseData ->
-        caseData.isApplicantNotRepresented();
+        caseData.isApplicantNotRepresented() || caseData.isApplicantRepresented();
 
     public static final Predicate<CaseData> agreedToMediation = CaseData::hasClaimantAgreedToFreeMediation;
 
