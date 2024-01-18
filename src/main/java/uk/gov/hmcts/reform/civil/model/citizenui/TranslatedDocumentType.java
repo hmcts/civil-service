8e9bd8fd3e6654b14479b5a4e376067cc3ae9c30
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-01-18 18:14:54 +0000

    CIV-0000 translated Doc type change

--- src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/TranslatedDocumentType.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/TranslatedDocumentType.java
@@ -1,10 +1,17 @@
 package uk.gov.hmcts.reform.civil.model.citizenui;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum TranslatedDocumentType {
-    @JsonProperty("DEFENDANT_RESPONSE")
     DEFENDANT_RESPONSE,
-    @JsonProperty("CLAIM_ISSUE")
-    CLAIM_ISSUE
+    CLAIM_ISSUE;
+    @JsonValue
+    public static TranslatedDocumentType fromString(String translatedDocument) {
+        for (TranslatedDocumentType doc : TranslatedDocumentType.values()) {
+            if (doc.name().equalsIgnoreCase(translatedDocument)) {
+                return doc;
+            }
+        }
+        return null;
+    }
 }
