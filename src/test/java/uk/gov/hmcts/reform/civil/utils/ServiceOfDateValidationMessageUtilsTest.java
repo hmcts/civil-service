855b00cd62b1d3e9ca9099bed65bbc667a047097
MMNycz <94067802+MMNycz@users.noreply.github.com>
2024-01-23 11:13:55 +0000

    Update ServiceOfDateValidationMessageUtilsTest.java

--- src/test/java/uk/gov/hmcts/reform/civil/utils/ServiceOfDateValidationMessageUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/utils/ServiceOfDateValidationMessageUtilsTest.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 class ServiceOfDateValidationMessageUtilsTest {
