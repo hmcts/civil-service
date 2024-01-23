10f6d26ca131600ba0a191b58e3b353dc9f0429c
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2024-01-23 08:56:01 +0000

    Added the updating and clearing of next hearing details logic in UpdateNextHearingDetailsCallbackHandler

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/hearings/UpdateNextHearingDetailsCallbackHandler.java
@@ -0,0 +1,104 @@
+package uk.gov.hmcts.reform.civil.handler.callback.user.hearings;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.Callback;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.NextHearingDetails;
+import uk.gov.hmcts.reform.civil.service.Time;
+import uk.gov.hmcts.reform.hmc.model.hearing.HearingDaySchedule;
+import uk.gov.hmcts.reform.hmc.model.hearings.CaseHearing;
+import uk.gov.hmcts.reform.hmc.model.hearings.HearingsResponse;
+import uk.gov.hmcts.reform.hmc.model.messaging.HmcStatus;
+import uk.gov.hmcts.reform.hmc.service.HearingsService;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UPDATE_NEXT_HEARING_DETAILS;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UpdateNextHearingInfo;
+import static uk.gov.hmcts.reform.hmc.model.messaging.HmcStatus.ADJOURNED;
+import static uk.gov.hmcts.reform.hmc.model.messaging.HmcStatus.AWAITING_ACTUALS;
+import static uk.gov.hmcts.reform.hmc.model.messaging.HmcStatus.CANCELLED;
+import static uk.gov.hmcts.reform.hmc.model.messaging.HmcStatus.COMPLETED;
+import static uk.gov.hmcts.reform.hmc.model.messaging.HmcStatus.LISTED;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateNextHearingDetailsCallbackHandler extends CallbackHandler {
+
+    private static final List<CaseEvent> EVENTS = List.of(
+        UPDATE_NEXT_HEARING_DETAILS,
+        UpdateNextHearingInfo
+    );
+
+    private static final List<String> UPDATE_HEARING_DATE_STATUSES = List.of(LISTED.name(), AWAITING_ACTUALS.name());
+    private static final List<HmcStatus> CLEAR_HEARING_DATE_STATUSES = List.of(COMPLETED, CANCELLED, ADJOURNED);
+
+    private final HearingsService hearingService;
+    private final Time datetime;
+    private final ObjectMapper objectMapper;
+
+    private Map<String, Callback> callbackMap = Map.of(callbackKey(ABOUT_TO_SUBMIT), this::aboutToSubmit);
+
+    @Override
+    protected Map<String, Callback> callbacks() {
+        return callbackMap;
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+
+    private CallbackResponse aboutToSubmit(CallbackParams callbackParams) {
+        Long caseId = callbackParams.getRequest().getCaseDetails().getId();
+        CaseData.CaseDataBuilder caseDataBuilder = callbackParams.getCaseData().toBuilder();
+        String authToken = callbackParams.getParams().get(BEARER_TOKEN).toString();
+        HearingsResponse hearingsResponse = hearingService.getHearings(authToken, caseId, null);
+
+        CaseHearing latestHearing = getLatestHearing(hearingsResponse);
+
+        if (UPDATE_HEARING_DATE_STATUSES.contains(latestHearing.getHmcStatus())) {
+            LocalDateTime nextHearingDate = getNextHearingDate(latestHearing);
+            caseDataBuilder.nextHearingDetails(
+                    nextHearingDate != null
+                        ? NextHearingDetails.builder()
+                            .hearingID(latestHearing.getHearingId().toString())
+                            .hearingDateTime(nextHearingDate)
+                            .build() : null)
+                .build();
+        }
+
+        if (CLEAR_HEARING_DATE_STATUSES.contains(latestHearing.getHmcStatus())) {
+            caseDataBuilder.nextHearingDetails(null);
+        }
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDataBuilder.build().toMap(objectMapper)).build();
+    }
+
+    public CaseHearing getLatestHearing(HearingsResponse hearingsResponse) {
+        return hearingsResponse.getCaseHearings().stream()
+            .max(Comparator.comparing(CaseHearing::getHearingRequestDateTime)).orElse(null);
+    }
+
+    public LocalDateTime getNextHearingDate(CaseHearing hearing) {
+        return hearing.getHearingDaySchedule()
+            .stream()
+            .filter(day -> day.getHearingStartDateTime().isAfter(datetime.now().withHour(0).withMinute(0).withSecond(0)))
+            .min(Comparator.comparing(HearingDaySchedule::getHearingStartDateTime))
+            .orElse(HearingDaySchedule.builder().build())
+            .getHearingStartDateTime();
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/HmcMessageHandlerTest.java
@@ -0,0 +1,268 @@
+package uk.gov.hmcts.reform.civil.handler;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.civil.config.PaymentsConfiguration;
+import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
+import uk.gov.hmcts.reform.hmc.model.messaging.HearingUpdate;
+import uk.gov.hmcts.reform.hmc.model.messaging.HmcMessage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.REVIEW_HEARING_EXCEPTION;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UPDATE_NEXT_HEARING_DETAILS;
+import static uk.gov.hmcts.reform.hmc.model.messaging.HmcStatus.ADJOURNED;
+import static uk.gov.hmcts.reform.hmc.model.messaging.HmcStatus.AWAITING_ACTUALS;
+import static uk.gov.hmcts.reform.hmc.model.messaging.HmcStatus.CANCELLED;
+import static uk.gov.hmcts.reform.hmc.model.messaging.HmcStatus.COMPLETED;
+import static uk.gov.hmcts.reform.hmc.model.messaging.HmcStatus.EXCEPTION;
+import static uk.gov.hmcts.reform.hmc.model.messaging.HmcStatus.HEARING_REQUESTED;
+import static uk.gov.hmcts.reform.hmc.model.messaging.HmcStatus.LISTED;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {
+    HmcMessageHandler.class
+})
+class HmcMessageHandlerTest {
+
+    @Autowired
+    private HmcMessageHandler handler;
+    @MockBean
+    private CoreCaseDataService coreCaseDataService;
+    @MockBean
+    private PaymentsConfiguration paymentsConfiguration;
+
+    @BeforeEach
+    void setUp() {
+        when(paymentsConfiguration.getSpecSiteId()).thenReturn("AAA6");
+        when(paymentsConfiguration.getSiteId()).thenReturn("AAA7");
+    }
+
+    @Test
+    void shouldReturnTrue_whenMessageRelevantForServiceAndHearingException() {
+        HmcMessage hmcMessage = HmcMessage.builder()
+            .caseId(1234L)
+            .hearingId("HER1234")
+            .hmctsServiceCode("AAA7")
+            .hearingUpdate(HearingUpdate.builder()
+                               .hmcStatus(EXCEPTION)
+                               .build())
+            .build();
+
+        boolean actual = handler.handleMessage(hmcMessage);
+
+        verify(coreCaseDataService).triggerEvent(1234L, REVIEW_HEARING_EXCEPTION);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrue_whenMessageRelevantForServiceNoHearingException() {
+        HmcMessage hmcMessage = HmcMessage.builder()
+            .caseId(1234L)
+            .hearingId("HER1234")
+            .hmctsServiceCode("AAA7")
+            .hearingUpdate(HearingUpdate.builder()
+                               .hmcStatus(HEARING_REQUESTED)
+                               .build())
+            .build();
+
+        boolean actual = handler.handleMessage(hmcMessage);
+
+        assertThat(actual).isTrue();
+        verifyNoInteractions(coreCaseDataService);
+    }
+
+    @Test
+    void shouldReturnTrue_whenServiceIdIsAAA6AndHMCStatusIsListed() {
+        HmcMessage hmcMessage = HmcMessage.builder()
+                .caseId(1234L)
+                .hearingId("HER1234")
+                .hmctsServiceCode("AAA6")
+                .hearingUpdate(HearingUpdate.builder()
+                        .hmcStatus(LISTED)
+                        .build())
+                .build();
+
+        boolean actual = handler.handleMessage(hmcMessage);
+
+        verify(coreCaseDataService).triggerEvent(1234L, UPDATE_NEXT_HEARING_DETAILS);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrue_whenServiceIdIsAAA7AndHMCStatusIsListed() {
+        HmcMessage hmcMessage = HmcMessage.builder()
+                .caseId(1234L)
+                .hearingId("HER1234")
+                .hmctsServiceCode("AAA7")
+                .hearingUpdate(HearingUpdate.builder()
+                        .hmcStatus(LISTED)
+                        .build())
+                .build();
+
+        boolean actual = handler.handleMessage(hmcMessage);
+
+        verify(coreCaseDataService).triggerEvent(1234L, UPDATE_NEXT_HEARING_DETAILS);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrue_whenServiceIdIsAAA6AndHMCStatusIsAwaitingActuals() {
+        HmcMessage hmcMessage = HmcMessage.builder()
+                .caseId(1234L)
+                .hearingId("HER1234")
+                .hmctsServiceCode("AAA6")
+                .hearingUpdate(HearingUpdate.builder()
+                        .hmcStatus(AWAITING_ACTUALS)
+                        .build())
+                .build();
+
+        boolean actual = handler.handleMessage(hmcMessage);
+
+        verify(coreCaseDataService).triggerEvent(1234L, UPDATE_NEXT_HEARING_DETAILS);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrue_whenServiceIdIsAAA7AndHMCStatusIsAwaitingActuals() {
+        HmcMessage hmcMessage = HmcMessage.builder()
+                .caseId(1234L)
+                .hearingId("HER1234")
+                .hmctsServiceCode("AAA7")
+                .hearingUpdate(HearingUpdate.builder()
+                        .hmcStatus(AWAITING_ACTUALS)
+                        .build())
+                .build();
+
+        boolean actual = handler.handleMessage(hmcMessage);
+
+        verify(coreCaseDataService).triggerEvent(1234L, UPDATE_NEXT_HEARING_DETAILS);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrue_whenServiceIdIsAAA6AndHMCStatusIsCompleted() {
+        HmcMessage hmcMessage = HmcMessage.builder()
+                .caseId(1234L)
+                .hearingId("HER1234")
+                .hmctsServiceCode("AAA6")
+                .hearingUpdate(HearingUpdate.builder()
+                        .hmcStatus(COMPLETED)
+                        .build())
+                .build();
+
+        boolean actual = handler.handleMessage(hmcMessage);
+
+        verify(coreCaseDataService).triggerEvent(1234L, UPDATE_NEXT_HEARING_DETAILS);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrue_whenServiceIdIsAAA7AndHMCStatusIsCompleted() {
+        HmcMessage hmcMessage = HmcMessage.builder()
+                .caseId(1234L)
+                .hearingId("HER1234")
+                .hmctsServiceCode("AAA7")
+                .hearingUpdate(HearingUpdate.builder()
+                        .hmcStatus(COMPLETED)
+                        .build())
+                .build();
+
+        boolean actual = handler.handleMessage(hmcMessage);
+
+        verify(coreCaseDataService).triggerEvent(1234L, UPDATE_NEXT_HEARING_DETAILS);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrue_whenServiceIdIsAAA6AndHMCStatusIsCenceled() {
+        HmcMessage hmcMessage = HmcMessage.builder()
+                .caseId(1234L)
+                .hearingId("HER1234")
+                .hmctsServiceCode("AAA6")
+                .hearingUpdate(HearingUpdate.builder()
+                        .hmcStatus(CANCELLED)
+                        .build())
+                .build();
+
+        boolean actual = handler.handleMessage(hmcMessage);
+
+        verify(coreCaseDataService).triggerEvent(1234L, UPDATE_NEXT_HEARING_DETAILS);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrue_whenServiceIdIsAAA7AndHMCStatusIsCanceled() {
+        HmcMessage hmcMessage = HmcMessage.builder()
+                .caseId(1234L)
+                .hearingId("HER1234")
+                .hmctsServiceCode("AAA7")
+                .hearingUpdate(HearingUpdate.builder()
+                        .hmcStatus(CANCELLED)
+                        .build())
+                .build();
+
+        boolean actual = handler.handleMessage(hmcMessage);
+
+        verify(coreCaseDataService).triggerEvent(1234L, UPDATE_NEXT_HEARING_DETAILS);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrue_whenServiceIdIsAAA6AndHMCStatusIsAdjourned() {
+        HmcMessage hmcMessage = HmcMessage.builder()
+                .caseId(1234L)
+                .hearingId("HER1234")
+                .hmctsServiceCode("AAA6")
+                .hearingUpdate(HearingUpdate.builder()
+                        .hmcStatus(ADJOURNED)
+                        .build())
+                .build();
+
+        boolean actual = handler.handleMessage(hmcMessage);
+
+        verify(coreCaseDataService).triggerEvent(1234L, UPDATE_NEXT_HEARING_DETAILS);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrue_whenServiceIdIsAAA7AndHMCStatusIsAdjourned() {
+        HmcMessage hmcMessage = HmcMessage.builder()
+                .caseId(1234L)
+                .hearingId("HER1234")
+                .hmctsServiceCode("AAA7")
+                .hearingUpdate(HearingUpdate.builder()
+                        .hmcStatus(ADJOURNED)
+                        .build())
+                .build();
+
+        boolean actual = handler.handleMessage(hmcMessage);
+
+        verify(coreCaseDataService).triggerEvent(1234L, UPDATE_NEXT_HEARING_DETAILS);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrue_whenMessageServiceIdNotRelevant() {
+        HmcMessage hmcMessage = HmcMessage.builder()
+            .caseId(1234L)
+            .hearingId("HER1234")
+            .hmctsServiceCode("AAA8")
+            .hearingUpdate(HearingUpdate.builder()
+                               .hmcStatus(EXCEPTION)
+                               .build())
+            .build();
+
+        boolean actual = handler.handleMessage(hmcMessage);
+
+        assertThat(actual).isTrue();
+        verifyNoInteractions(coreCaseDataService);
+    }
+}
