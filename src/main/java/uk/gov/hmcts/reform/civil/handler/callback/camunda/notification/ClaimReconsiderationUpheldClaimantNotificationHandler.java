3aa332197ae439c12cec771cb03b8e494ce705f7
Omaira-Melo-Hmcts <148855113+Omaira-Melo-Hmcts@users.noreply.github.com>
2024-01-24 11:50:40 +0000

    CIV-10909 request for reconsideration notification (#3843)
    * CIV-10909
    Created the handlers to notify Claimant and defendant on 1v1 scenario
    Test classes for check it.
    
    * CIV-10909 Pending to update the civil-common to the tag version
    
    CIV-10909 Pending to update the civil-common to the tag version
    
    * CIV-10909 Updating civil- commons version and the test for defendant
    
    * CIV-8336 Judge decision to reconsider request changes
    
    * Style modifications
    
    * CIV-8336 fix checkstyle
    
    * CIV-10909
    Adding the event call into submit event for Reconsideration
    
    * Update JudgeDecisionOnReconsiderationRequestCallbackHandler.java
    
    * Update JudgeDecisionOnReconsiderationRequestCallbackHandlerTest.java
    
    * Update ClaimReconsiderationUpheldClaimantNotificationHandler.java
    
    CIV-10909 Modifiying the information related to the applicant
    
    * CIV-10909
    Adding the event call into submit event for Reconsideration
    
    * CIV-10909
    Changing the event list to only triigger DECISION_ON_RECONSIDERATION_REQUEST
    
    * CIV-10909 Refactoring the business process to notify the claim changes
    
    * CIV-10909 Adding the property for ClaimentLipHelpWithFees
    
    * CIV-10909 Refactoring respondant information from the casedata
    
    * CIV-10909 Refactoring respondant information from the casedata in the test
    
    * CIV-10909 Refactoring some text issues.
    
    * CIV-10909 Testing purpose
    
    * CIV-10909 Refactoring and fixing some comments on the PR
    
    * CIV-10909 Refactoring and fixing some comments on the PR
    
    * CIV-10909 Pointing to Camunda branch
    
    * CIV-10909 Fixing retriving email respondant1 solicitor email
    
    * CIV-10909 Fixing retriving email respondant1 solicitor email
    
    * CIV-10909 Fixing retriving email respondant1 solicitor email
    
    * CIV-10909 update civil-commons to updated version
    
    * CIV-10909 refactoring the branchs to master
    
    * CIV-10909 refactoring the branchs to master
    
    * CIV-10909 Civil-commons version changed
    
    * CIV-10909 Civil-commons version changed
    Added properties for the current version of civil-commons
    
    ---------
    
    Co-authored-by: bhagyashreesharma90 <bhagyashree.sharma@hmcts.net>
    Co-authored-by: bhagyashreesharma90 <114094319+bhagyashreesharma90@users.noreply.github.com>
    Co-authored-by: Azam <106387766+Azam-Hmcts@users.noreply.github.com>
    Co-authored-by: krishnanuthalapati <32389208+krishnanuthalapati@users.noreply.github.com>
    Co-authored-by: Azam <muhammad.ghulam@hmcts.net>

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimReconsiderationUpheldClaimantNotificationHandler.java
@@ -0,0 +1,84 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.notification;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.Callback;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.notify.NotificationService;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_CLAIM_RECONSIDERATION_UPHELD_CLAIMANT;
+import static uk.gov.hmcts.reform.civil.utils.HearingUtils.getClaimantVDefendant;
+
+@Service
+@RequiredArgsConstructor
+public class ClaimReconsiderationUpheldClaimantNotificationHandler extends CallbackHandler
+    implements NotificationData {
+
+    private static final List<CaseEvent> EVENTS = List.of(NOTIFY_CLAIM_RECONSIDERATION_UPHELD_CLAIMANT);
+    public static final String TASK_ID = "NotifyDecisionOnReconsiderationRequest";
+    private static final String REFERENCE_TEMPLATE =
+        "reconsideration-upheld-applicant-notification-%s";
+
+    private final NotificationService notificationService;
+    private final NotificationsProperties notificationsProperties;
+
+    @Override
+    protected Map<String, Callback> callbacks() {
+        return Map.of(
+            callbackKey(ABOUT_TO_SUBMIT),
+            this::notifyClaimReconsiderationUpheldToClaimant
+        );
+    }
+
+    @Override
+    public String camundaActivityId(CallbackParams callbackParams) {
+        return TASK_ID;
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+
+    private CallbackResponse notifyClaimReconsiderationUpheldToClaimant(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        if (caseData.getApplicantSolicitor1UserDetails().getEmail() != null) {
+            notificationService.sendMail(
+                caseData.getApplicantSolicitor1UserDetails().getEmail(),
+                getTemplate(),
+                addProperties(caseData),
+                getReferenceTemplate(caseData)
+            );
+        }
+
+        return AboutToStartOrSubmitCallbackResponse.builder().build();
+    }
+
+    @Override
+    public Map<String, String> addProperties(CaseData caseData) {
+        return Map.of(
+                CLAIM_REFERENCE_NUMBER, caseData.getLegacyCaseReference(),
+                CLAIMANT_V_DEFENDANT, getClaimantVDefendant(caseData),
+                PARTY_NAME, caseData.getApplicant1().getPartyName()
+            );
+    }
+
+    private String getTemplate() {
+        return notificationsProperties.getNotifyUpdateTemplate();
+    }
+
+    private String getReferenceTemplate(CaseData caseData) {
+        return String.format(REFERENCE_TEMPLATE, caseData.getLegacyCaseReference());
+    }
+
+}
