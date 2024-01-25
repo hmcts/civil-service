c5100d608a60473b98d9c0ce737743375c9d3916
mfallonhmcts <114912573+mfallonhmcts@users.noreply.github.com>
2024-01-25 16:05:16 +0000

    Update GenerateCsvAndTransferTaskHandler.java

--- src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/GenerateCsvAndTransferTaskHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/GenerateCsvAndTransferTaskHandler.java
@@ -83,7 +83,7 @@ public class GenerateCsvAndTransferTaskHandler implements BaseExternalTaskHandle
         InputStreamSource inputSource = new ByteArrayResource(generateCsvData.getBytes(StandardCharsets.UTF_8));
 
         return Optional.of(EmailData.builder()
-                               .to("hestermctester90@gmail.com")
+                               .to(mediationCSVEmailConfiguration.getRecipient())
                                .subject(subject)
                                .attachments(List.of(new EmailAttachment(inputSource, "text/csv", filename)))
                                .build());
