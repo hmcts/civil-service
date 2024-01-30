f30c4e1441855f45348fef854952aa768a18c347
Omaira-Melo-Hmcts <148855113+Omaira-Melo-Hmcts@users.noreply.github.com>
2024-01-30 12:31:29 +0000

    CIV-8306

--- src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/SmallTrack.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/SmallTrack.java
@@ -1,6 +1,11 @@
 package uk.gov.hmcts.reform.civil.enums.sdo;
 
 public enum SmallTrack {
+    smallClaimBuildingDispute,
     smallClaimCreditHire,
-    smallClaimRoadTrafficAccident
+    smallClaimHousingDisrepair,
+    smallClaimRoadTrafficAccident,
+    smallClaimDisputeResolutionHearing,
+    smallClaimPPI,
+    smallClaimFlightDelay
 }
