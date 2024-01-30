bf6c0f60b5ad0695ddb5a196d3c87dbcd0bd27d3
laarmada <sergiosalmar@yahoo.es>
2024-01-30 15:33:34 +0000

    basic structure

---
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/model/Notification.java
@@ -0,0 +1,132 @@
+package uk.gov.hmcts.reform.civil.model.citizenui.model;
+
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@EqualsAndHashCode
+@Table(name = "notifications")
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String templateId;
+    private String caseReference;
+    private String role;
+    private String enHTML;
+    private String cyHTML;
+    private String reference;
+    private String createdBy;
+    private Date createdAt;
+    private String modifiedBy;
+    private Date modifiedAt;
+
+    public Notification() {}
+
+    public Notification(String templateId,String caseReference,String role, String enHTML,String cyHTML, String reference, String createdBy, Date createdAt, String modifiedBy, Date modifiedAt) {
+        this.templateId = templateId;
+        this.caseReference = caseReference;
+        this.role = role;
+        this.enHTML = enHTML;
+        this.cyHTML = cyHTML;
+        this.reference = reference;
+        this.createdBy = createdBy;
+        this.createdAt = createdAt;
+        this.modifiedBy= modifiedBy;
+        this.modifiedAt= modifiedAt;
+
+    }
+    public String getTemplateId() {
+        return templateId;
+    }
+
+    public void setTemplateId(String templateId) {
+        this.templateId = templateId;
+    }
+
+    public String getCaseReference() {
+        return caseReference;
+    }
+
+    public void setCaseReference(String caseReference) {
+        this.caseReference = caseReference;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEnHTML() {
+        return enHTML;
+    }
+
+    public void setEnHTML(String enHTML) {
+        this.enHTML = enHTML;
+    }
+
+    public String getCyHTML() {
+        return cyHTML;
+    }
+
+    public void setCyHTML(String cyHTML) {
+        this.cyHTML = cyHTML;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(Date modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
