05c01b75b1ab1b6527e3204223333b0bbadb4f8e
paul-pearson <paul.pearson@version1.com>
2024-02-06 16:16:45 +0000

    CIV-12045 Create test PR

--- src/test/java/uk/gov/hmcts/reform/civil/service/FeatureToggleServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/FeatureToggleServiceTest.java
@@ -188,6 +188,13 @@ class FeatureToggleServiceTest {
         assertThat(featureToggleService.isCarmEnabledForCase(LocalDateTime.now())).isEqualTo(toggleStat);
     }
 
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void shouldLipVLipEnabledReturnTrue() {
+
+        assertThat(featureToggleService.isLipVLipEnabled()).isEqualTo(true);
+    }
+
     private void givenToggle(String feature, boolean state) {
         when(featureToggleApi.isFeatureEnabled(eq(feature)))
             .thenReturn(state);
