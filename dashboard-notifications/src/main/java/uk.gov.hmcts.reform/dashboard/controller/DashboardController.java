92eec63a9ee11c1dc5ab7b167898ca029165808a
meulendijksm <max.meulendijks@hmcts.net>
2024-02-07 14:12:58 +0000

    CIV-12358 - added annotation import

--- dashboard-notifications/src/main/java/uk.gov.hmcts.reform/dashboard/controller/DashboardController.java
+++ dashboard-notifications/src/main/java/uk.gov.hmcts.reform/dashboard/controller/DashboardController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
