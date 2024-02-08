1f438671fea218f2068850e53727e5e53dbccd04
neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
2024-02-08 13:59:57 +0000

    Update UpdateClaimantIntentionClaimStateCallbackHandlerTests.java

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/caseevents/UpdateClaimantIntentionClaimStateCallbackHandlerTests.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/caseevents/UpdateClaimantIntentionClaimStateCallbackHandlerTests.java
@@ -10,18 +10,12 @@ import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.citizenui.CaseDataLiP;
-import uk.gov.hmcts.reform.civil.model.citizenui.ChooseHowToProceed;
-import uk.gov.hmcts.reform.civil.model.citizenui.ClaimantLiPResponse;
-import uk.gov.hmcts.reform.civil.model.citizenui.dto.ClaimantResponseOnCourtDecisionType;
-import uk.gov.hmcts.reform.civil.model.citizenui.dto.RepaymentDecisionType;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.service.UpdateClaimStateService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 
 @ExtendWith(MockitoExtension.class)
