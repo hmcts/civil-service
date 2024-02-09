0eac419e255fe41156270c58c87ebc0463381f3d
neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
2024-02-09 12:35:10 +0000

    Update UpdateClaimantIntentionClaimStateCallbackHandler.java

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/caseevents/UpdateClaimantIntentionClaimStateCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/caseevents/UpdateClaimantIntentionClaimStateCallbackHandler.java
@@ -9,7 +9,6 @@ import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
-import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.UpdateClaimStateService;
 
@@ -18,7 +17,6 @@ import java.util.List;
 import java.util.Map;
 
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
-import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NotSuitable_SDO;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UPDATE_CLAIMANT_INTENTION_CLAIM_STATE;
 
 @Service
@@ -58,7 +56,6 @@ public class UpdateClaimantIntentionClaimStateCallbackHandler extends CallbackHa
         if (!updatedData.isBilingual()) {
             response.state(updateClaimStateService.setUpCaseState(updatedData));
         }
-
         return response.build();
     }
 }
