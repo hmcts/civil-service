6b92f2125037906bcbf48de784f4d66190142cbb
jeswanth <jeswanth.reddy@hmcts.net>
2024-02-09 16:08:32 +0000

    CIV-10058 code handler for no remission

---
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/HelpWithFeesDetails.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.civil.model.citizenui;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.reform.civil.enums.FeeType;
+import uk.gov.hmcts.reform.civil.enums.HWFFeeDetailsSummary;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelpWithFeesDetails {
+
+    private String noRemissionDetails;
+    private HWFFeeDetailsSummary noRemissionDetailsSummary;
+    private FeeType hwfFeeType;
+
+}
