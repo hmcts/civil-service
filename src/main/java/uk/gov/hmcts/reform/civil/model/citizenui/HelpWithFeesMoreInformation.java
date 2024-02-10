c4336e382eb07bafaff8215967c4a59227efa5d8
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2024-02-10 15:51:48 +0000

    CIV-10054 build fix

--- src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/HelpWithFeesMoreInformation.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/HelpWithFeesMoreInformation.java
@@ -14,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class HelpWithFeesMoreInformation {
+
     private List<HwFMoreInfoRequiredDocuments> hwFMoreInfoRequiredDocuments;
     private String hwFMoreInfoDetails;
     private LocalDate hwFMoreInfoDocumentDate;
