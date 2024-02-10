7b6010dda5221a72bfea4b3be5dd0fd242ee6c85
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2024-02-10 15:36:14 +0000

    CIV-10054 Set Up Unit Test

---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/MoreInformationHwfCallbackHandlerTest.java
@@ -0,0 +1,85 @@
+package uk.gov.hmcts.reform.civil.handler.callback.user;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.citizenui.HelpWithFeesDetails;
+import uk.gov.hmcts.reform.civil.model.citizenui.HelpWithFeesMoreInformation;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.MID;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {
+    MoreInformationHwfCallbackHandler.class,
+    JacksonAutoConfiguration.class,
+})
+class MoreInformationHwfCallbackHandlerTest extends BaseCallbackHandlerTest {
+
+    @Autowired
+    private MoreInformationHwfCallbackHandler handler;
+
+    @MockBean
+    private FeatureToggleService featureToggleService;
+
+    @Nested
+    class AboutToStartCallback {
+
+        @Test
+        void shouldValidationMoreInformation_withInvalidDate() {
+            HelpWithFeesDetails hwFeesDetails = HelpWithFeesDetails.builder()
+                .helpWithFeesMoreInformation(
+                    HelpWithFeesMoreInformation.builder()
+                        .hwFMoreInfoDocumentDate(LocalDate.now())
+                        .build()
+                )
+                .build();
+
+            CaseData caseData = CaseDataBuilder.builder()
+                .atStatePendingClaimIssued()
+                .build()
+                .builder()
+                .hwFeesDetails(hwFeesDetails)
+                .build();
+
+            CallbackParams params = callbackParamsOf(caseData, MID, "more-information-hwf");
+
+            AboutToStartOrSubmitCallbackResponse response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+
+            Assertions.assertThat(response.getErrors()).containsExactly("Documents date must be future date");
+        }
+    }
+
+    @Nested
+    class AboutToSubmitCallback {
+
+        @Test
+        void shouldCallSubmitMoreInformationHwfAboutToSubmit() {
+            CaseData caseData = CaseDataBuilder.builder()
+                .atStatePendingClaimIssued()
+                .build();
+            CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+
+            var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+
+            assertThat(response).isNotNull();
+        }
+    }
+}
