aff10b7a286f3123912810c76e78f1dc336e5a27
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-02-11 13:44:48 +0100

    CIV-10056 : Handler

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/FullRemissionHWFCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/FullRemissionHWFCallbackHandler.java
@@ -9,23 +9,25 @@ import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.enums.FeeType;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
-import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NO_REMISSION_HWF;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.FULL_REMISSION_HWF;
 
 @Service
 @RequiredArgsConstructor
-public class NoRemissionHWFCallbackHandler extends CallbackHandler {
+public class FullRemissionHWFCallbackHandler extends CallbackHandler {
 
-    private static final List<CaseEvent> EVENTS = List.of(NO_REMISSION_HWF);
+    private static final List<CaseEvent> EVENTS = List.of(FULL_REMISSION_HWF);
     private final ObjectMapper objectMapper;
     private final Map<String, Callback> callbackMap = Map.of(
         callbackKey(ABOUT_TO_SUBMIT),
-        this::noRemssionHWF,
+        this::fullRemissionHWF,
         callbackKey(SUBMITTED), this::emptySubmittedCallbackResponse
     );
 
@@ -39,7 +41,25 @@ public class NoRemissionHWFCallbackHandler extends CallbackHandler {
         return EVENTS;
     }
 
-    private CallbackResponse noRemssionHWF(CallbackParams callbackParams) {
+    private CallbackResponse fullRemissionHWF(CallbackParams callbackParams) {
+        var caseData = callbackParams.getCaseData();
+        var updatedData = caseData.toBuilder();
+        var claimFeeAmount = caseData.getCalculatedClaimFeeInPence();
+        var hearingFeeAmount = caseData.getHearingFeeAmount();
+        var feeType = caseData.getHwfFeeType();
+
+        if (FeeType.CLAIMISSUED == feeType && claimFeeAmount != null) {
+            var claimFee = caseData.getClaimFee();
+
+            claimFee.setCalculatedAmountInPence(BigDecimal.ZERO);
+            updatedData.claimFee(claimFee);
+        } else if (FeeType.HEARING == feeType && hearingFeeAmount != null) {
+            var hearingFee = caseData.getHearingFee();
+
+            hearingFee.setCalculatedAmountInPence(BigDecimal.ZERO);
+            updatedData.hearingFee(hearingFee);
+        }
+
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(callbackParams.getCaseData().toMap(objectMapper))
             .build();
