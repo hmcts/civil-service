a8c09efb91031f233244ef09a7bd187815c41c70
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-02-11 14:04:20 +0100

    CIV-10056 : Remove CIV-10058 changes from this branch

--- src/main/java/uk/gov/hmcts/reform/civil/enums/HWFFeeDetailsSummary.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.civil.enums;
-
-public enum HWFFeeDetailsSummary {
-    NOT_QUALIFY_FEE_ASSISTANCE,
-    INCORRECT_EVIDENCE,
-    INSUFFICIENT_EVIDENCE,
-    FEES_REQUIREMENT_NOT_MET,
-}
