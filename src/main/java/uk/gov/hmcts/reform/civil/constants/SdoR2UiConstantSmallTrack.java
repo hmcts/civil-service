4046ea634464b652a1faa79f853e78c59c20ec3f
Omaira-Melo-Hmcts <148855113+Omaira-Melo-Hmcts@users.noreply.github.com>
2024-02-12 12:23:21 +0000

    CIV-8306 Adding time to screen

---
+++ src/main/java/uk/gov/hmcts/reform/civil/constants/SdoR2UiConstantSmallTrack.java
@@ -0,0 +1,9 @@
+package uk.gov.hmcts.reform.civil.constants;
+
+public class SdoR2UiConstantSmallTrack {
+    public SdoR2UiConstantSmallTrack() {
+        //To satisfy sonarQube
+    }
+    public static final String JUDGE_RECITAL = "Upon considering the statements of case and the information provided" +
+        " by the parties.";
+}
