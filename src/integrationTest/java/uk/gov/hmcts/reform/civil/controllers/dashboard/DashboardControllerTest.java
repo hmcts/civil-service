ab3d962c7369f8574917f500ed4b6a3dca9f4334
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-02-12 22:26:41 +0000

    Adding Integration Tests

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/DashboardControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/DashboardControllerTest.java
@@ -3,12 +3,18 @@ package uk.gov.hmcts.reform.civil.controllers.dashboard;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.jdbc.Sql;
 import uk.gov.hmcts.reform.civil.controllers.BaseIntegrationTest;
 import uk.gov.hmcts.reform.dashboard.entities.NotificationEntity;
 import uk.gov.hmcts.reform.dashboard.entities.NotificationTemplateEntity;
+import uk.gov.hmcts.reform.dashboard.model.TaskList;
 import uk.gov.hmcts.reform.dashboard.repositories.NotificationRepository;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -18,12 +24,16 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+@Sql("/scripts/dashboardNotifications/get_task_list_data.sql")
 public class DashboardControllerTest extends BaseIntegrationTest {
 
     @MockBean
     private NotificationRepository notificationRepository;
 
     private final String endPointUrl = "/dashboard/notifications/{uuid}";
+
+    private final String getTaskListUrl = "/dashboard/taskList/{ccd-case-identifier}/role/{role-type}";
 
     @Test
     @SneakyThrows
@@ -56,5 +66,23 @@ public class DashboardControllerTest extends BaseIntegrationTest {
         doGet(BEARER_TOKEN, endPointUrl,  "1234")
             .andExpect(status().isBadRequest());
     }
+
+    @Test
+    @SneakyThrows
+    void shouldReturnTaskListForGiveCaseReferenceAndRole() {
+        String timeStamp = "2024-02-12 21:32:49";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime dateTime = LocalDateTime.parse(timeStamp, formatter);
+
+        List<TaskList> taskList = new ArrayList<>();
+        taskList.add(TaskList.builder().id(UUID.fromString("8c2712da-47ce-4050-bbee-650134a7b9e5")).taskNameCy("task_name_cy").taskNameEn("task_name_en").taskOrder(0).categoryCy("category_cy").categoryEn("category_en")
+                             .role("defendant").currentStatus(0).nextStatus(1).hintTextCy("hint_text_cy").hintTextEn("hint_text_en").reference("123")
+                             .createdAt(dateTime).updatedAt(dateTime).updatedBy("Test").build());
+        doGet(BEARER_TOKEN, getTaskListUrl,  "123","defendant")
+            .andExpect(status().isOk())
+            .andExpect(content().json(toJson(taskList)));
+    }
+
+
 
 }
