3052a5fd60b0f18e801400889628cd52cd77845f
MMNycz <94067802+MMNycz@users.noreply.github.com>
2024-02-12 14:51:34 +0000

    CIV-12378 first claim to recreate task

--- src/main/java/uk/gov/hmcts/reform/civil/handler/RetriggerCasesEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/RetriggerCasesEventHandler.java
@@ -51,6 +51,7 @@ public class RetriggerCasesEventHandler implements BaseExternalTaskHandler {
                 } catch (Exception e) {
                     throw e;
                 }
+                
                 log.info("Retrigger cases Finished for event: {}", caseEvent);
             });
         } else {
