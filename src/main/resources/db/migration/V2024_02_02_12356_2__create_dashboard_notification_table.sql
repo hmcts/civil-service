514f35d2779b9ad6fc1df4660e013ef37710b366
laarmada <sergiosalmar@yahoo.es>
2024-02-13 09:55:25 +0000

    change to camel case in order to respect the good practise.

--- src/main/resources/db/migration/V2024_02_02_12356_2__create_dashboard_notification_table.sql
+++ src/main/resources/db/migration/V2024_02_02_12356_2__create_dashboard_notification_table.sql
@@ -3,14 +3,14 @@ CREATE TABLE IF NOT EXISTS dbs.dashboard_notifications (
  dashboard_notifications_Templates_id INTEGER REFERENCES dbs.dashboard_notifications_Templates(id),
  reference VARCHAR(256),
  notification_name VARCHAR(256),
- citizen_Role VARCHAR(256),
- title_En VARCHAR(256),
- title_Cy VARCHAR(256),
- description_En VARCHAR(256),
- description_Cy VARCHAR(256),
+ citizen_role VARCHAR(256),
+ title_en VARCHAR(256),
+ title_cy VARCHAR(256),
+ description_en VARCHAR(256),
+ description_cy VARCHAR(256),
  message_param VARCHAR(256),
- created_By VARCHAR(256),
- created_At timestamp without time zone DEFAULT now() NOT NULL,
- updated_By VARCHAR(256),
- updated_On timestamp without time zone DEFAULT now() NOT NULL
+ created_by VARCHAR(256),
+ created_at timestamp without time zone DEFAULT now() NOT NULL,
+ updated_by VARCHAR(256),
+ updated_on timestamp without time zone DEFAULT now() NOT NULL
 );
