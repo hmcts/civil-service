976a4bf57553039a46fe19f04fb739f1e96d8daf
Manish Garg <manish.garg@hmcts.net>
2024-02-13 22:44:13 +0000

    CIV-10057 HWF partial remission functionality

--- src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/HelpWithFeesDetails.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/HelpWithFeesDetails.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.reform.civil.enums.FeeType;
 
 import java.math.BigDecimal;
 
