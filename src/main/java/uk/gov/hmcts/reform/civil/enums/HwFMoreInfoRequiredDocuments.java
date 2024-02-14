927d5c3670e0c3d2d4ba008e845bbfe8d5a5e77e
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2024-02-14 11:53:21 +0000

    CIV-10054 Update from review

--- src/main/java/uk/gov/hmcts/reform/civil/enums/HwFMoreInfoRequiredDocuments.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/HwFMoreInfoRequiredDocuments.java
@@ -6,13 +6,13 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum HwFMoreInfoRequiredDocuments {
+    ANY_OTHER_INCOME,
     BANK_STATEMENTS,
-    WAGE_SLIPS,
-    CHILD_MAINTENANCE,
     BENEFITS_AND_TAX_CREDITS,
-    PENSIONS,
-    RENTAL_INCOME,
+    CHILD_MAINTENANCE,
     INCOME_FROM_SELLING_GOODS,
+    PENSIONS,
     PRISONERS_INCOME,
-    ANY_OTHER_INCOME
+    RENTAL_INCOME,
+    WAGE_SLIPS
 }
