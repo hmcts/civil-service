707ae8415091318e17618bdef58acd03c16c0d2c
Mark Drummond <mark.drummond@version1.com>
2024-02-14 15:08:18 +0000

    remove logging

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/hearing/HearingFormGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/hearing/HearingFormGenerator.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.civil.service.docmosis.hearing;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import uk.gov.hmcts.reform.civil.enums.DocCategory;
@@ -43,7 +42,6 @@ import static uk.gov.hmcts.reform.civil.utils.HearingUtils.getHearingType;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class HearingFormGenerator implements TemplateDataGenerator<HearingForm> {
 
     private final DocumentManagementService documentManagementService;
@@ -59,7 +57,6 @@ public class HearingFormGenerator implements TemplateDataGenerator<HearingForm> 
         HearingForm templateData = getTemplateData(caseData, authorisation);
         DocmosisTemplates template = getTemplate(caseData);
 
-        log.info("template is " + template);
         DocmosisDocument document =
             documentGeneratorService.generateDocmosisDocument(templateData, template);
         CaseDocument caseDocument = documentManagementService.uploadDocument(
