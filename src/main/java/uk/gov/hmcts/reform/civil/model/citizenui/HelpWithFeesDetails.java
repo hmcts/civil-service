40c2c8f05d12f84a36fdfe16acf3771f9438595b
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2024-02-15 14:29:03 +0000

    CIV-10053 Invalid HwF

--- src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/HelpWithFeesDetails.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/HelpWithFeesDetails.java
@@ -1,21 +1,11 @@
 package uk.gov.hmcts.reform.civil.model.citizenui;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.reform.civil.enums.FeeType;
-
-import java.math.BigDecimal;
 
 @Data
 @Builder(toBuilder = true)
 @NoArgsConstructor
-@AllArgsConstructor
 public class HelpWithFeesDetails {
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
-    private BigDecimal remissionAmount;
-    private FeeType hwfFeeType;
 }
