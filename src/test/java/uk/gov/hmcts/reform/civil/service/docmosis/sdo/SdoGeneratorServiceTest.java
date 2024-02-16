6d096c8445b9309b993b1bcb03e36a1514bdaa34
Mark Drummond <mark.drummond@version1.com>
2024-02-16 16:43:15 +0000

    Updated missed tests

--- src/test/java/uk/gov/hmcts/reform/civil/service/docmosis/sdo/SdoGeneratorServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/docmosis/sdo/SdoGeneratorServiceTest.java
@@ -39,7 +39,11 @@ import java.time.LocalDate;
 import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.documentmanagement.model.DocumentType.SDO_ORDER;
