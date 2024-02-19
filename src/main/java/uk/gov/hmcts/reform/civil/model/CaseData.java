c18ebdf8f4ceee1dd6120a9b4cf20fbc5e77e926
paul-pearson <paul.pearson@version1.com>
2024-02-19 14:54:33 +0000

    CIV-12722 Merge changes from CIV-10055

--- src/main/java/uk/gov/hmcts/reform/civil/model/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/CaseData.java
@@ -1326,14 +1326,4 @@ public class CaseData extends CaseDataParent implements MappableObject {
             .map(CaseDataLiP::getApplicant1LiPResponse)
             .filter(ClaimantLiPResponse::hasCourtDecisionInFavourOfClaimant).isPresent();
     }
-
-    @JsonIgnore
-    public boolean isHWFTypeHearing() {
-        return getHwfFeeType() == FeeType.HEARING;
-    }
-
-    @JsonIgnore
-    public boolean isHWFTypeClaimIssued() {
-        return getHwfFeeType() == FeeType.CLAIMISSUED;
-    }
 }
