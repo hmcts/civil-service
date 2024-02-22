cbce4f9d60bfb0b6e904e57e79d2ba450ec409ca
sankaviv1 <sankavi.mohanraj@version1.com>
2024-02-22 13:21:53 +0000

    checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/StateFlowEngine.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/StateFlowEngine.java
@@ -485,8 +485,8 @@ public class StateFlowEngine {
                 .transitionTo(TAKEN_OFFLINE_SDO_NOT_DRAWN).onlyIf(takenOfflineSDONotDrawnAfterNotificationAcknowledgedTimeExtension)
             .state(FULL_DEFENCE)
                 .transitionTo(IN_MEDIATION).onlyIf((agreedToMediation.and(allAgreedToLrMediationSpec.negate()))
-                                                        // for carm cases, fullDefenceProcced is tracked with lipFullDefenceProceed
-                                                        // and move to in mediation if applicant does not settle
+                                                       // for carm cases, fullDefenceProcced is tracked with lipFullDefenceProceed
+                                                       // and move to in mediation if applicant does not settle
                                                        .or(lipFullDefenceProceed.and(isCarmApplicableLipCase).and(not(fullDefenceProceed))))
                 .transitionTo(FULL_DEFENCE_PROCEED)
                 .onlyIf(fullDefenceProceed.and(allAgreedToLrMediationSpec).and(agreedToMediation.negate()).and(declinedMediation.negate()))
