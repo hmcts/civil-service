e0c9e27bc5dbd00f085a6756e36046d752577bd8
ShwetaTandel-hmcts <shweta.tandel@hmcts.net>
2024-02-23 16:57:42 +0000

    fix

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateSDOCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateSDOCallbackHandlerTest.java
@@ -392,59 +392,6 @@ public class CreateSDOCallbackHandlerTest extends BaseCallbackHandlerTest {
         }
 
         @Test
-        void shouldPopulateHearingCourtLocationForNihl() {
-
-            when(featureToggleService.isSdoR2Enabled()).thenReturn(true);
-
-            String preSelectedCourt = "214320";
-            List<LocationRefData> locations = List.of(
-                LocationRefData.builder().epimmsId("00001").courtLocationCode("00001")
-                    .siteName("court 1").courtAddress("1 address").postcode("Y01 7RB").build(),
-                LocationRefData.builder().epimmsId(preSelectedCourt).courtLocationCode(preSelectedCourt)
-                    .siteName("court 2").courtAddress("2 address").postcode("Y02 7RB").build(),
-                LocationRefData.builder().epimmsId("00003").courtLocationCode("00003")
-                    .siteName("court 3").courtAddress("3 address").postcode("Y03 7RB").build()
-            );
-            when(locationRefDataService.getHearingCourtLocations(anyString())).thenReturn(locations);
-            CaseData caseData = CaseDataBuilder.builder().atStateClaimDraft()
-                .build()
-                .toBuilder()
-                .drawDirectionsOrderRequired(YesOrNo.NO)
-                .claimsTrack(ClaimsTrack.fastTrack)
-                .fastClaims(List.of(FastTrack.fastClaimNoiseInducedHearingLoss))
-                .build();
-
-            CallbackParams params = callbackParamsOf(CallbackVersion.V_1, caseData, ABOUT_TO_START);
-
-            var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
-
-            CaseData responseCaseData = objectMapper.convertValue(response.getData(), CaseData.class);
-
-            DynamicList altExpected = DynamicList.builder()
-                .listItems(List.of(
-                               DynamicListElement.builder().code("00001").label("court 1 - 1 address - Y01 7RB").build(),
-                               DynamicListElement.builder().code(preSelectedCourt).label("court 2 - 2 address - Y02 7RB").build(),
-                               DynamicListElement.builder().code("00003").label("court 3 - 3 address - Y03 7RB").build()
-                           )
-                )
-                .build();
-
-            DynamicList expected = DynamicList.builder()
-                .value(
-                    DynamicListElement.builder().code(preSelectedCourt).label("court 2 - 2 address - Y02 7RB").build()
-                )
-                .listItems(List.of(
-                               DynamicListElement.builder().code(preSelectedCourt).label("court 2 - 2 address - Y02 7RB").build(),
-                               DynamicListElement.builder().code("OTHER_LOCATION").label("Other location").build()
-                           )
-                )
-                .build();
-
-            assertThat(responseCaseData.getSdoR2Trial().getHearingCourtLocationList()).isEqualTo(expected);
-            assertThat(responseCaseData.getSdoR2Trial().getAltHearingCourtLocationList()).isEqualTo(altExpected);
-        }
-
-        @Test
         void shouldPopulateDefaultFieldsForNihl() {
 
             when(featureToggleService.isSdoR2Enabled()).thenReturn(true);
