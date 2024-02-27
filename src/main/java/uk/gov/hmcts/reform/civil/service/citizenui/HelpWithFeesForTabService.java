7db9fd8d1da2b1f53feaa180ff81fe554a00b53c
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2024-02-27 17:04:45 +0000

    CIV-12702 Change from review

--- src/main/java/uk/gov/hmcts/reform/civil/service/citizenui/HelpWithFeesForTabService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/citizenui/HelpWithFeesForTabService.java
@@ -8,11 +8,15 @@ import uk.gov.hmcts.reform.civil.model.citizenui.HelpWithFeesDetails;
 import uk.gov.hmcts.reform.civil.model.citizenui.HelpWithFeesForTab;
 import uk.gov.hmcts.reform.civil.utils.MonetaryConversions;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class HelpWithFeesForTabService {
+
+    private String HEARING_FEE = "Hearing Fee";
+    private String CLAIM_ISSUED_FEE = "Claim Fee";
 
     public void setUpHelpWithFeeTab(CaseData.CaseDataBuilder caseDataBuilder) {
         CaseData caseData = caseDataBuilder.build();
@@ -21,16 +25,20 @@ public class HelpWithFeesForTabService {
         } else if (FeeType.HEARING == caseData.getHwfFeeType()) {
             caseDataBuilder.hearingHwfForTab(setUpHearingHelpWithFeeTab(caseData));
         }
-        return;
     }
 
     private HelpWithFeesForTab setUpHearingHelpWithFeeTab(CaseData caseData) {
         return HelpWithFeesForTab.builder()
-            .remissionAmount(caseData.getHearingHwfDetails().getRemissionAmount())
-            .applicantMustPay(caseData.getHearingHwfDetails().getOutstandingFeeInPounds())
+            .remissionAmount(Optional.ofNullable(caseData.getHearingHwfDetails())
+                                 .map(HelpWithFeesDetails::getRemissionAmount).isPresent()
+                                 ? MonetaryConversions.penniesToPounds(caseData.getClaimIssuedHwfDetails().getRemissionAmount())
+                                 : BigDecimal.ZERO)
+            .applicantMustPay(Optional.ofNullable(caseData.getHearingHwfDetails())
+                                  .map(HelpWithFeesDetails::getOutstandingFeeInPounds)
+                                  .orElse(BigDecimal.ZERO))
             .claimFee(MonetaryConversions.penniesToPounds(caseData.getCalculatedHearingFeeInPence()))
             .feeCode(caseData.getHearingFee().getCode())
-            .hwfType("Hearing Fee")
+            .hwfType(HEARING_FEE)
             .hwfReferenceNumber(caseData.getHelpWithFeesReferenceNumber())
             .build();
     }
@@ -40,13 +48,13 @@ public class HelpWithFeesForTabService {
             .remissionAmount(Optional.ofNullable(caseData.getClaimIssuedHwfDetails())
                                  .map(HelpWithFeesDetails::getRemissionAmount).isPresent()
                                  ? MonetaryConversions.penniesToPounds(caseData.getClaimIssuedHwfDetails().getRemissionAmount())
-                                 : null)
+                                 : BigDecimal.ZERO)
             .applicantMustPay(Optional.ofNullable(caseData.getClaimIssuedHwfDetails())
                                   .map(HelpWithFeesDetails::getOutstandingFeeInPounds)
-                                  .orElse(null))
+                                  .orElse(BigDecimal.ZERO))
             .claimFee(MonetaryConversions.penniesToPounds(caseData.getCalculatedClaimFeeInPence()))
             .feeCode(caseData.getClaimFee().getCode())
-            .hwfType("Claim Fee")
+            .hwfType(CLAIM_ISSUED_FEE)
             .hwfReferenceNumber(caseData.getHelpWithFeesReferenceNumber())
             .build();
     }
