d436e4680f96ad68f6d3cf52068130856dcdbf55
meulendijksm <max.meulendijks@hmcts.net>
2024-02-28 16:34:31 +0000

    CIV-12654 - updated link colour

--- src/main/resources/db/migration/V2024_02_20_1020__CIV-12654-claim-fee-required-scenario.sql
+++ src/main/resources/db/migration/V2024_02_20_1020__CIV-12654-claim-fee-required-scenario.sql
@@ -11,8 +11,8 @@ VALUES ('Scenario.AAA7.ClaimIssue.ClaimFee.Required', '{"Notice.AAA7.ClaimIssue.
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
 VALUES ('Notice.AAA7.ClaimIssue.ClaimFee.Required', 'You need to pay your claim fee', 'You need to pay your claim fee',
-        'Your claim has not yet been issued, in order to proceed you must pay the claim fee of ${claimFee}. <a href={CLAIM_FEE_URL}>Pay the claim fee</a>.',
-        'Your claim has not yet been issued, in order to proceed you must pay the claim fee of ${claimFee}. <a href={CLAIM_FEE_URL}>Pay the claim fee</a>.',
+        'Your claim has not yet been issued, in order to proceed you must pay the claim fee of ${claimFee}. <a href={CLAIM_FEE_URL}  rel="noopener noreferrer" class="govuk-link">Pay the claim fee</a>.',
+        'Your claim has not yet been issued, in order to proceed you must pay the claim fee of ${claimFee}. <a href={CLAIM_FEE_URL}  rel="noopener noreferrer" class="govuk-link">Pay the claim fee</a>.',
         'CLAIMANT');
 
 /**
@@ -20,11 +20,11 @@ VALUES ('Notice.AAA7.ClaimIssue.ClaimFee.Required', 'You need to pay your claim 
  */
 INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
                                     scenario_name, task_status_sequence, role,task_order)
-values ('<a href={VIEW_CLAIM_URL}>View the claim</a>', 'The claim',
-        '<a href={VIEW_CLAIM_URL}>View the claim</a>',
+values ('<a href={VIEW_CLAIM_URL}  rel="noopener noreferrer" class="govuk-link">View the claim</a>', 'The claim',
+        '<a href={VIEW_CLAIM_URL}  rel="noopener noreferrer" class="govuk-link">View the claim</a>',
         'The claim', 'Claim.View', 'Scenario.AAA7.ClaimIssue.ClaimFee.Required', '{3, 3}', 'CLAIMANT', 1),
-       ('<a href={VIEW_INFO_ABOUT_CLAIMANT_URL}>View information about the claimant</a>', 'The claim',
-        '<a href={VIEW_INFO_ABOUT_CLAIMANT_URL}>View information about the claimant</a>',
+       ('<a href={VIEW_INFO_ABOUT_CLAIMANT_URL}  rel="noopener noreferrer" class="govuk-link">View information about the claimant</a>', 'The claim',
+        '<a href={VIEW_INFO_ABOUT_CLAIMANT_URL}  rel="noopener noreferrer" class="govuk-link">View information about the claimant</a>',
         'The claim', 'Claim.Claimant.Info', 'Scenario.AAA7.ClaimIssue.ClaimFee.Required', '{3, 3}', 'CLAIMANT', 2),
        ('<a>View the response to the claim</a>', 'The response','<a>View the response to the claim</a>',
         'The response', 'Response.View', 'Scenario.AAA7.ClaimIssue.ClaimFee.Required', '{1, 1}', 'CLAIMANT', 3),
@@ -40,8 +40,8 @@ values ('<a href={VIEW_CLAIM_URL}>View the claim</a>', 'The claim',
         'Hearings', 'Hearing.Fee.Pay', 'Scenario.AAA7.ClaimIssue.ClaimFee.Required', '{1, 1}', 'CLAIMANT', 8),
        ('<a>View the bundle</a>', 'Hearings' ,'<a>View the bundle</a>',
         'Hearings', 'Hearing.Bundle.View', 'Scenario.AAA7.ClaimIssue.ClaimFee.Required', '{1, 1}', 'CLAIMANT', 9),
-       ('<a href={VIEW_ORDERS_AND_NOTICES_URL}>View orders and notices</a>', 'Orders and notices from the court' ,
-        '<a href={VIEW_ORDERS_AND_NOTICES_URL}>View orders and notices</a>',
+       ('<a href={VIEW_ORDERS_AND_NOTICES_URL}  rel="noopener noreferrer" class="govuk-link">View orders and notices</a>', 'Orders and notices from the court' ,
+        '<a href={VIEW_ORDERS_AND_NOTICES_URL}  rel="noopener noreferrer" class="govuk-link">View orders and notices</a>',
         'Orders and notices from the court', 'Order.View', 'Scenario.AAA7.ClaimIssue.ClaimFee.Required', '{3, 3}', 'CLAIMANT', 10),
        ('<a>View the judgment</a>', 'Judgments from the court' ,'<a>View the judgment</a>',
         'Judgments from the court', 'Judgment.View', 'Scenario.AAA7.ClaimIssue.ClaimFee.Required', '{1, 1}', 'CLAIMANT', 11),
