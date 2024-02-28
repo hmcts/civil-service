6960abef4d6eab0f8a82a8aafb7fafa741d9b072
Omaira-Melo-Hmcts <148855113+Omaira-Melo-Hmcts@users.noreply.github.com>
2024-02-28 15:13:33 +0000

    CIV-8308 Fixing document generation error

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/sdo/SdoGeneratorService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/sdo/SdoGeneratorService.java
@@ -53,7 +53,7 @@ public class SdoGeneratorService {
                 .anyMatch(s -> s != null && s.toLowerCase().contains("judge"));
         }
 
-        if (SdoHelper.isSmallClaimsTrack(caseData) && SdoHelper.hasSmallClaimsVariable(caseData, "smallClaimsFlightDelayToggle")) {
+        if (SdoHelper.isSmallClaimsTrack(caseData) && SdoHelper.hasSmallAdditionalDirections(caseData, "smallClaimFlightDelay")) {
             docmosisTemplate = DocmosisTemplates.SDO_SMALL_FLIGHT_DELAY;
             templateData = getTemplateDataSmall(caseData, judgeName, isJudge, authorisation, true);
         } else if (SdoHelper.isSmallClaimsTrack(caseData)) {
