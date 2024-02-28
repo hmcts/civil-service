766c6aa8cdd050ac68c68404d4f0a99488c3ffe2
meulendijksm <max.meulendijks@hmcts.net>
2024-02-28 16:55:45 +0000

    CIV-12693 - update url colour

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/ClaimIssuedScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/ClaimIssuedScenarioTest.java
@@ -52,13 +52,13 @@ public class ClaimIssuedScenarioTest extends BaseIntegrationTest {
             .andExpectAll(
                 status().is(HttpStatus.OK.value()),
                 jsonPath("$[0].reference").value(caseId.toString()),
-                jsonPath("$[0].taskNameEn").value("<a href={VIEW_CLAIM_URL}>View the claim</a>"),
+                jsonPath("$[0].taskNameEn").value("<a href={VIEW_CLAIM_URL}  rel=\"noopener noreferrer\" class=\"govuk-link\">View the claim</a>"),
                 jsonPath("$[0].currentStatusEn").value("Available"),
-                jsonPath("$[1].taskNameEn").value("<a href={VIEW_INFO_ABOUT_CLAIMANT}>View information about the claimant</a>"),
+                jsonPath("$[1].taskNameEn").value("<a href={VIEW_INFO_ABOUT_CLAIMANT}  rel=\"noopener noreferrer\" class=\"govuk-link\">View information about the claimant</a>"),
                 jsonPath("$[1].currentStatusEn").value("Available"),
                 jsonPath("$[2].taskNameEn").value("<a>View the response to the claim</a>"),
                 jsonPath("$[2].currentStatusEn").value("Not available yet"),
-                jsonPath("$[3].taskNameEn").value("<a href={VIEW_INFO_ABOUT_DEFENDANT}>View information about the defendant</a>"),
+                jsonPath("$[3].taskNameEn").value("<a href={VIEW_INFO_ABOUT_DEFENDANT}  rel=\"noopener noreferrer\" class=\"govuk-link\">View information about the defendant</a>"),
                 jsonPath("$[3].currentStatusEn").value("Available"),
                 jsonPath("$[4].taskNameEn").value("<a>View hearings</a>"),
                 jsonPath("$[4].currentStatusEn").value("Not available yet"),
@@ -68,7 +68,7 @@ public class ClaimIssuedScenarioTest extends BaseIntegrationTest {
                 jsonPath("$[6].currentStatusEn").value("Not available yet"),
                 jsonPath("$[7].taskNameEn").value("<a>View the bundle</a>"),
                 jsonPath("$[7].currentStatusEn").value("Not available yet"),
-                jsonPath("$[8].taskNameEn").value("<a href={VIEW_ORDERS_AND_NOTICES}>View orders and notices</a>"),
+                jsonPath("$[8].taskNameEn").value("<a href={VIEW_ORDERS_AND_NOTICES}  rel=\"noopener noreferrer\" class=\"govuk-link\">View orders and notices</a>"),
                 jsonPath("$[8].currentStatusEn").value("Available"),
                 jsonPath("$[9].taskNameEn").value("<a>View the judgment</a>"),
                 jsonPath("$[9].currentStatusEn").value("Not available yet"),
@@ -84,7 +84,7 @@ public class ClaimIssuedScenarioTest extends BaseIntegrationTest {
                 status().is(HttpStatus.OK.value()),
                 jsonPath("$[0].titleEn").value("You haven´t responded to the claim"),
                 jsonPath("$[0].descriptionEn")
-                    .value("You need to respond before 4 pm on " + hearingFeeByDate + ". There are {daysLeftToRespond} days remaining. <a href=\"{RESPONSE_TASK_LIST_URL}\">Respond to the claim.</a>.")
+                    .value("You need to respond before 4 pm on " + hearingFeeByDate + ". There are {daysLeftToRespond} days remaining. <a href=\"{RESPONSE_TASK_LIST_URL}\"  rel=\"noopener noreferrer\" class=\"govuk-link\">Respond to the claim.</a>.")
             );
     }
 }
--- src/main/resources/db/migration/V2024_02_22_1135__CIV_12693_create_scenario_for_claim_issue_defendant.sql
+++ src/main/resources/db/migration/V2024_02_22_1135__CIV_12693_create_scenario_for_claim_issue_defendant.sql
@@ -10,8 +10,8 @@ VALUES ('Scenario.AAA7.ClaimIssue.Response.Required', '{}', '{"Notice.AAA7.Claim
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
 VALUES ('Notice.AAA7.ClaimIssue.Response.Required', 'You haven´t responded to the claim', 'You haven´t responded to the claim (CY)',
-        'You need to respond before ${defaultRespondTime} on ${responseDeadline}. There are {daysLeftToRespond} days remaining. <a href="{RESPONSE_TASK_LIST_URL}">Respond to the claim.</a>.',
-        'You need to respond before ${defaultRespondTime} on ${responseDeadline}. There are {daysLeftToRespond} days remaining. <a href="{RESPONSE_TASK_LIST_URL}">Respond to the claim.</a>.',
+        'You need to respond before ${defaultRespondTime} on ${responseDeadline}. There are {daysLeftToRespond} days remaining. <a href="{RESPONSE_TASK_LIST_URL}"  rel="noopener noreferrer" class="govuk-link">Respond to the claim.</a>.',
+        'You need to respond before ${defaultRespondTime} on ${responseDeadline}. There are {daysLeftToRespond} days remaining. <a href="{RESPONSE_TASK_LIST_URL}"  rel="noopener noreferrer" class="govuk-link">Respond to the claim.</a>.',
         'DEFENDANT');
 
 /**
@@ -20,13 +20,13 @@ VALUES ('Notice.AAA7.ClaimIssue.Response.Required', 'You haven´t responded to t
  */
 INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
                                     scenario_name, task_status_sequence, role, task_order)
-values ('<a href={VIEW_CLAIM_URL}>View the claim</a>', 'The claim','<a href={VIEW_CLAIM_URL}>View the claim</a>',
+values ('<a href={VIEW_CLAIM_URL}  rel="noopener noreferrer" class="govuk-link">View the claim</a>', 'The claim','<a href={VIEW_CLAIM_URL}>View the claim</a>',
         'The claim', 'Claim.View', 'Scenario.AAA7.ClaimIssue.Response.Required', '{3, 3}', 'DEFENDANT', 1),
-       ('<a href={VIEW_INFO_ABOUT_CLAIMANT}>View information about the claimant</a>', 'The claim','<a href={VIEW_INFO_ABOUT_CLAIMANT_URL}>View information about the claimant</a>',
+       ('<a href={VIEW_INFO_ABOUT_CLAIMANT}  rel="noopener noreferrer" class="govuk-link">View information about the claimant</a>', 'The claim','<a href={VIEW_INFO_ABOUT_CLAIMANT_URL}>View information about the claimant</a>',
         'The claim', 'Claim.Claimant.Info', 'Scenario.AAA7.ClaimIssue.Response.Required', '{3, 3}', 'DEFENDANT', 2),
        ('<a>View the response to the claim</a>', 'The response','<a>View the response to the claim</a>',
         'The response', 'Response.View', 'Scenario.AAA7.ClaimIssue.Response.Required', '{1, 1}', 'DEFENDANT', 3),
-       ('<a href={VIEW_INFO_ABOUT_DEFENDANT}>View information about the defendant</a>', 'The response','<a href={VIEW_INFO_ABOUT_DEFENDANT_URL}>View information about the defendant</a>',
+       ('<a href={VIEW_INFO_ABOUT_DEFENDANT}  rel="noopener noreferrer" class="govuk-link">View information about the defendant</a>', 'The response','<a href={VIEW_INFO_ABOUT_DEFENDANT_URL}>View information about the defendant</a>',
         'The response', 'Response.Defendant.Info', 'Scenario.AAA7.ClaimIssue.Response.Required', '{3, 3}', 'DEFENDANT', 4),
        ('<a>View hearings</a>', 'Hearings','<a>View hearings</a>',
         'Hearings', 'Hearing.View', 'Scenario.AAA7.ClaimIssue.Response.Required', '{1, 1}', 'DEFENDANT', 5),
@@ -36,7 +36,7 @@ values ('<a href={VIEW_CLAIM_URL}>View the claim</a>', 'The claim','<a href={VIE
         'Hearings', 'Hearing.Arrangements.Add', 'Scenario.AAA7.ClaimIssue.Response.Required', '{1, 1}', 'DEFENDANT', 7),
        ('<a>View the bundle</a>', 'Hearings' ,'<a>View the bundle</a>',
         'Hearings', 'Hearing.Bundle.View', 'Scenario.AAA7.ClaimIssue.Response.Required', '{1, 1}', 'DEFENDANT', 8),
-       ('<a href={VIEW_ORDERS_AND_NOTICES}>View orders and notices</a>', 'Orders and notices from the court' ,'<a href={VIEW_ORDERS_AND_NOTICES_URL}>View orders and notices</a>',
+       ('<a href={VIEW_ORDERS_AND_NOTICES}  rel="noopener noreferrer" class="govuk-link">View orders and notices</a>', 'Orders and notices from the court' ,'<a href={VIEW_ORDERS_AND_NOTICES_URL}>View orders and notices</a>',
         'Orders and notices from the court', 'Order.View', 'Scenario.AAA7.ClaimIssue.Response.Required', '{3, 3}', 'DEFENDANT', 9),
        ('<a>View the judgment</a>', 'Judgments from the court' ,'<a>View the judgment</a>',
         'Judgments from the court', 'Judgment.View', 'Scenario.AAA7.ClaimIssue.Response.Required', '{1, 1}', 'DEFENDANT', 10),
