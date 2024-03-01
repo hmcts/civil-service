84a035d7c052fead95b754049395412e798617ba
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-03-01 08:51:53 +0000

    CIV-12776 change

--- src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/StateFlowEngine.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/StateFlowEngine.java
@@ -507,8 +507,7 @@ public class StateFlowEngine {
                          .or(declinedMediation).and(applicantOutOfTime.negate()).and(demageMultiClaim.negate()))
             .setDynamic(Map.of(FlowFlag.SDO_ENABLED.name(),
                                JudicialReferralUtils::shouldMoveToJudicialReferral))
-            .transitionTo(FULL_DEFENCE_PROCEED).onlyIf(isClaimantNotSettleFullDefenceClaim.and(lipFullDefenceProceed)
-                        .and(not(agreedToMediation)))
+            .transitionTo(FULL_DEFENCE_PROCEED).onlyIf(isClaimantNotSettleFullDefenceClaim.and(not(agreedToMediation)))
                 .set((c, flags) -> {
                     flags.put(FlowFlag.AGREED_TO_MEDIATION.name(), false);
                 })
