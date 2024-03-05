e3958312fbe52f52d59dc6d7626f1a6b04b65dfe
Manish Garg <manish.garg@hmcts.net>
2024-03-05 12:29:18 +0000

    CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimIssueHwfInvalidRefScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimIssueHwfInvalidRefScenarioTest.java
@@ -36,11 +36,11 @@ public class ClaimIssueHwfInvalidRefScenarioTest extends BaseIntegrationTest {
             jsonPath("$[0].titleEn").value("You've provided an invalid help with fees reference number"),
             jsonPath("$[0].descriptionEn").value(
                 "You've applied for help with the claim fee, but the reference number is invalid.<br>You've been sent an email with instructions on what to do next." +
-                    " If you've already read the email and taken action, disregard this message.<br>You can pay by phone by calling 0300 123 7050."),
+                    " If you've already read the email and taken action, disregard this message.<br>You can pay by phone by calling {civilMoneyClaimsTelephone}."),
             jsonPath("$[0].titleCy").value("You've provided an invalid help with fees reference number"),
             jsonPath("$[0].descriptionCy").value(
                 "You've applied for help with the claim fee, but the reference number is invalid.<br>You've been sent an email with instructions on what to do next." +
-                    " If you've already read the email and taken action, disregard this message.<br>You can pay by phone by calling 0300 123 7050.")
+                    " If you've already read the email and taken action, disregard this message.<br>You can pay by phone by calling {civilMoneyClaimsTelephone}.")
 
         );
     }
