3b0bec7fbe8d9d55540252f48535e33498d1ded1
EllisD-B <ellis.domengerboyce@hmcts.net>
2024-03-06 10:06:49 +0000

    add wiremock docker image

---
+++ bin/add-services.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Hello"
+
+IFS=','; read -ra FILES <<< "${ADDITIONAL_COMPOSE_FILES}"
+for FILE in "${FILES[@]}"; do
+  # process "$i"
+  echo "$FILE"
+  docker compose -f "${BASE_COMPOSE_FILES_PATH}$FILE" up -d
+done
