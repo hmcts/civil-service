d376b7f5daa64436193fb961a31f79a1cf70d31d
neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
2024-03-06 16:16:34 +0000

    Update FlowLipPredicate.java

--- src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/FlowLipPredicate.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/FlowLipPredicate.java
@@ -10,8 +10,7 @@ public class FlowLipPredicate {
 
     }
 
-    public static final Predicate<CaseData> isLipCase = caseData ->
-        caseData.isApplicantNotRepresented() || caseData.isApplicantRepresented();
+    public static final Predicate<CaseData> isLipCase = CaseData::isLipvLipOneVOne();
 
     public static final Predicate<CaseData> nocSubmittedForLiPApplicant = caseData ->
         caseData.isLRvLipOneVOne() && caseData.claimIssueFeePaymentDoneWithHWF();
