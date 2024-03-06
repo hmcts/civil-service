24560bea118ea7a55663f37a21108a1b6196f38b
paul-pearson <paul.pearson@version1.com>
2024-03-06 09:41:18 +0000

    CIV-12792 Fix handler test

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/GoToHearingNotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/GoToHearingNotificationHandlerTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotification
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_DASHBOARD_NOTIFICATION_GO_TO_HEARING_FOR_APPLICANT1;
@@ -10,7 +11,6 @@ import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_DASHBOARD_NOTI
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -40,32 +40,45 @@ public class GoToHearingNotificationHandlerTest extends BaseCallbackHandlerTest 
 
     @Nested
     class AboutToSubmitCallback {
-        @BeforeEach
-        void setup() {
-            when(dashboardApiClient.recordScenario(any(), any(), anyString(), any())).thenReturn(ResponseEntity.of(
-                Optional.empty()));
-        }
 
         @Test
-        void shouldRecordScenario_whenInvoked() {
-            CaseData caseData = CaseDataBuilder.builder().atStateTrialReadyCheck().build();
+        void shouldRecordScenario_whenInvokedInJudicialReferralState() {
+            // Given
+            when(dashboardApiClient.recordScenario(any(), any(), anyString(), any())).thenReturn(ResponseEntity.of(
+                Optional.empty()));
+            CaseData caseData = CaseDataBuilder.builder().atStateBeforeTakenOfflineSDONotDrawn().build();
             CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_SUBMIT, caseData).request(
                 CallbackRequest.builder().eventId(CREATE_DASHBOARD_NOTIFICATION_GO_TO_HEARING_FOR_APPLICANT1.name()).build()
             ).build();
-
             Map<String, Object> scenarioParams = new HashMap<>();
-            scenarioParams.put("defendant", "Defendant Name");
-
+            scenarioParams.put("defendantName", "Defendant Name");
             when(mapper.mapCaseDataToParams(any())).thenReturn(scenarioParams);
 
+            // When
             handler.handle(params);
 
+            // Then
             verify(dashboardApiClient).recordScenario(
                 caseData.getCcdCaseReference().toString(),
                 "Scenario.AAA7.ClaimantIntent.GoToHearing.Claimant",
                 "BEARER_TOKEN",
                 ScenarioRequestParams.builder().params(scenarioParams).build()
             );
+        }
+
+        @Test
+        void shouldNotRecordScenario_whenInvokedNotInJudicialReferralStatey() {
+            // Given
+            CaseData caseData = CaseDataBuilder.builder().atStateTrialReadyCheck().build();
+            CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_SUBMIT, caseData).request(
+                CallbackRequest.builder().eventId(CREATE_DASHBOARD_NOTIFICATION_GO_TO_HEARING_FOR_APPLICANT1.name()).build()
+            ).build();
+
+            // When
+            handler.handle(params);
+
+            // Then
+            verifyNoInteractions(dashboardApiClient);
         }
     }
 }
