d68f926688163b713977c5dde2682bac2596d3ee
Manish Garg <manish.garg@hmcts.net>
2024-03-07 11:29:01 +0000

    CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number" (#4139)
    * CIV-12757 Dashboard Notification HWF
    
    * CIV-12757 integration test modified
    
    * CIV-12757 remove typeOfFee param
    
    * CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"
    
    * CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"
    
    * CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"
    
    * CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"
    
    * CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"
    
    * CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"
    
    * CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"
    
    * CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"
    
    * CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"
    
    * CIV-12757 update script
    
    * CIV-12757 update test
    
    * CIV-12757 fix db issue
    
    * CIV-12757 fix db issue task
    
    ---------
    
    Co-authored-by: miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
    Co-authored-by: dharmendra kumar <dharmendra.kumar@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardScenarios.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardScenarios.java
@@ -7,7 +7,8 @@ public enum DashboardScenarios {
     SCENARIO_AAA7_CLAIM_ISSUE_CLAIM_FEE_REQUIRED("Scenario.AAA7.ClaimIssue.ClaimFee.Required"),
     SCENARIO_AAA7_CLAIM_ISSUE_RESPONSE_AWAIT("Scenario.AAA7.ClaimIssue.Response.Await"),
     SCENARIO_AAA7_CLAIM_ISSUE_RESPONSE_REQUIRED("Scenario.AAA7.ClaimIssue.Response.Required"),
-    SCENARIO_AAA7_CLAIM_ISSUE_HWF_REQUESTED("Scenario.AAA7.ClaimIssue.HWF.Requested");
+    SCENARIO_AAA7_CLAIM_ISSUE_HWF_REQUESTED("Scenario.AAA7.ClaimIssue.HWF.Requested"),
+    SCENARIO_AAA7_CLAIM_ISSUE_HWF_INVALID_REF("Scenario.AAA7.ClaimIssue.HWF.InvalidRef");
 
     private final String scenario;
 
