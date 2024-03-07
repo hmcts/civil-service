964163b6eee789846d65146219841623bf1ede21
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-03-07 11:51:31 +0000

    CIV-12792 Refactoring

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/ClaimantResponseNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/ClaimantResponseNotificationHandler.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.claimant;
 
+import com.google.common.base.Strings;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
@@ -9,7 +10,6 @@ import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.client.DashboardApiClient;
-import uk.gov.hmcts.reform.civil.enums.CaseState;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.DashboardNotificationsParamsMapper;
 import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
@@ -20,6 +20,8 @@ import java.util.Map;
 import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_CLAIMANT_DASHBOARD_NOTIFICATION_FOR_CLAIMANT_RESPONSE;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.CASE_SETTLED;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.JUDICIAL_REFERRAL;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_CLAIMANT_INTENT_CLAIM_SETTLED_CLAIMANT;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_CLAIMANT_INTENT_GO_TO_HEARING;
 
@@ -54,19 +56,25 @@ public class ClaimantResponseNotificationHandler extends CallbackHandler {
         CaseData caseData = callbackParams.getCaseData();
         String authToken = callbackParams.getParams().get(BEARER_TOKEN).toString();
 
-        if (caseData.getCcdState() == CaseState.JUDICIAL_REFERRAL) {
-
-            dashboardApiClient.recordScenario(caseData.getCcdCaseReference().toString(),
-                                              SCENARIO_AAA7_CLAIMANT_INTENT_GO_TO_HEARING.getScenario(), authToken,
-                                              ScenarioRequestParams.builder().params(mapper.mapCaseDataToParams(caseData)).build()
-            );
-        } else if (caseData.getCcdState() == CaseState.CASE_SETTLED) {
-
-            dashboardApiClient.recordScenario(caseData.getCcdCaseReference().toString(),
-                                              SCENARIO_AAA7_CLAIMANT_INTENT_CLAIM_SETTLED_CLAIMANT.getScenario(), authToken,
-                                              ScenarioRequestParams.builder().params(mapper.mapCaseDataToParams(caseData)).build()
+        String scenario = getScenario(caseData);
+        if (!Strings.isNullOrEmpty(scenario)) {
+            dashboardApiClient.recordScenario(
+                caseData.getCcdCaseReference().toString(),
+                scenario,
+                authToken,
+                ScenarioRequestParams.builder().params(mapper.mapCaseDataToParams(
+                    caseData)).build()
             );
         }
         return AboutToStartOrSubmitCallbackResponse.builder().build();
+    }
+
+    private String getScenario(CaseData caseData) {
+        if (caseData.getCcdState() == CASE_SETTLED) {
+            return SCENARIO_AAA7_CLAIMANT_INTENT_CLAIM_SETTLED_CLAIMANT.getScenario();
+        } else if (caseData.getCcdState() == JUDICIAL_REFERRAL) {
+            return SCENARIO_AAA7_CLAIMANT_INTENT_GO_TO_HEARING.getScenario();
+        }
+        return null;
     }
 }
