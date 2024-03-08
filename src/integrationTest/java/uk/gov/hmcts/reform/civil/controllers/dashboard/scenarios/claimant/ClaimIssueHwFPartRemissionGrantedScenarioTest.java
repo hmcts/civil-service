719cb203d1ae2802eb2bf9ee5e49baff80a5980c
jeswanth <jeswanth.reddy@hmcts.net>
2024-03-08 18:01:10 +0000

    CIV-12800 Dashboard notifications part remission

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimIssueHwFPartRemissionGrantedScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimIssueHwFPartRemissionGrantedScenarioTest.java
@@ -40,10 +40,12 @@ public class ClaimIssueHwFPartRemissionGrantedScenarioTest extends BaseIntegrati
             status().is(HttpStatus.OK.value()),
             jsonPath("$[0].titleEn").value("Your help with fees application has been reviewed"),
             jsonPath("$[0].descriptionEn").value(
-                "You'll get help with the claim fee. You'll receive 1000 towards it. <br> You must still pay the remaining fee of 25.You can pay by phone by calling {civilMoneyClaimsTelephone}."),
+                "You'll get help with the claim fee. You'll receive 1000 towards it. <br>" +
+                    " You must still pay the remaining fee of 25.You can pay by phone by calling {civilMoneyClaimsTelephone}."),
             jsonPath("$[0].titleCy").value("Your help with fees application has been reviewed"),
             jsonPath("$[0].descriptionCy").value(
-                "You'll get help with the claim fee. You'll receive 1000 towards it. <br> You must still pay the remaining fee of 25.You can pay by phone by calling {civilMoneyClaimsTelephone}.")
+                "You'll get help with the claim fee. You'll receive 1000 towards it. <br>" +
+                    " You must still pay the remaining fee of 25.You can pay by phone by calling {civilMoneyClaimsTelephone}.")
 
         );
     }
