ac050c1f7bc12c4f309c08c99952ddfa03ab7043
dharmendra kumar <dharmendra.kumar@hmcts.net>
2024-03-08 13:49:01 +0000

    Add tests for handlers

--- src/main/java/uk/gov/hmcts/reform/civil/handler/event/SettlementNoResponseFromDefendantEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/event/SettlementNoResponseFromDefendantEventHandler.java
@@ -4,29 +4,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.ccd.client.model.Event;
-import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 import uk.gov.hmcts.reform.civil.client.DashboardApiClient;
 import uk.gov.hmcts.reform.civil.config.SystemUpdateUserConfiguration;
-import uk.gov.hmcts.reform.civil.event.EvidenceUploadNotificationEvent;
 import uk.gov.hmcts.reform.civil.event.SettlementNoResponseFromDefendantEvent;
 import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios;
 import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.notification.EvidenceUploadApplicantNotificationHandler;
-import uk.gov.hmcts.reform.civil.notification.EvidenceUploadRespondentNotificationHandler;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 import uk.gov.hmcts.reform.civil.service.DashboardNotificationsParamsMapper;
 import uk.gov.hmcts.reform.civil.service.UserService;
 import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
-
-import java.util.Map;
-
-import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
-import static uk.gov.hmcts.reform.civil.callback.CaseEvent.EVIDENCE_UPLOAD_CHECK;
 
 @Slf4j
 @Service
@@ -41,7 +29,7 @@ public class SettlementNoResponseFromDefendantEventHandler {
     private final UserService userService;
 
     @EventListener
-    public void sendEvidenceUploadNotification(SettlementNoResponseFromDefendantEvent event) {
+    public void createClaimantDashboardScenario(SettlementNoResponseFromDefendantEvent event) {
         CaseDetails caseDetails = coreCaseDataService.getCase(event.caseId());
         CaseData caseData = caseDetailsConverter.toCaseData(caseDetails);
         dashboardApiClient.recordScenario(
