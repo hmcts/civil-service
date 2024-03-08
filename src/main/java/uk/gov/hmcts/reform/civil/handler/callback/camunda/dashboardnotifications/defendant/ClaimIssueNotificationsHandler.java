70b2fa1d6937f98e1ddce6c973f6e46ebb182103
laarmada <sergiosalmar@yahoo.es>
2024-03-08 08:38:37 +0000

    remove line separator for checkstyle

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/ClaimIssueNotificationsHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/ClaimIssueNotificationsHandler.java
@@ -35,7 +35,6 @@ public class ClaimIssueNotificationsHandler extends CallbackHandler {
     private final DashboardNotificationsParamsMapper dashboardNotificationsParamsMapper;
     private final FeatureToggleService toggleService;
 
-
     @Override
     protected Map<String, Callback> callbacks() {
         return toggleService.isDashboardServiceEnabled()
