ecdc85c5268d36844e1b70cd010c88fdb682aa59
laarmada <sergiosalmar@yahoo.es>
2024-03-11 11:31:11 +0000

    adding handler, sql, event..

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardScenarios.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardScenarios.java
@@ -9,7 +9,8 @@ public enum DashboardScenarios {
     SCENARIO_AAA7_CLAIM_ISSUE_RESPONSE_REQUIRED("Scenario.AAA7.ClaimIssue.Response.Required"),
     SCENARIO_AAA7_CLAIM_ISSUE_HWF_REQUESTED("Scenario.AAA7.ClaimIssue.HWF.Requested"),
     SCENARIO_AAA7_CLAIM_ISSUE_HWF_INVALID_REF("Scenario.AAA7.ClaimIssue.HWF.InvalidRef"),
-    SCENARIO_AAA7_CLAIM_ISSUE_HWF_INFO_REQUIRED("Scenario.AAA7.ClaimIssue.HWF.InfoRequired");
+    SCENARIO_AAA7_CLAIM_ISSUE_HWF_INFO_REQUIRED("Scenario.AAA7.ClaimIssue.HWF.InfoRequired"),
+    SCENARIO_AAA7_CLAIMANT_INTENT_CCJ_REQUESTED_DEFENDANT("Scenario.AAA7.ClaimantIntent.CCJ.Requested.Defendant");
 
     private final String scenario;
 
