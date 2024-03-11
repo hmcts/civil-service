92eeabeb7b52dca8884e0f0517dd294fbab39353
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-03-11 11:47:42 +0100

    CIV-12782 change url

--- src/main/resources/db/migration/V2024_03_08_0916__CIV_12782_create_pay_immediately_accepted_for_defendant.sql
+++ src/main/resources/db/migration/V2024_03_08_0916__CIV_12782_create_pay_immediately_accepted_for_defendant.sql
@@ -10,8 +10,8 @@ VALUES ('Scenario.AAA7.ClaimantIntent.PartAdmit.Defendant', '{"Notice.AAA7.DefRe
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
 VALUES ('Notice.AAA7.ClaimantIntent.PartAdmit.Defendant', 'Immediate payment', 'Immediate payment',
-        'The claimant has accepted your plan to pay £${defendantAdmittedAmount} immediately. Funds must clear <a href={CLAIMANT_DETAILS_URL} class="govuk-link">their account</a> by ${defendantAdmittedAmountPaymentDeadline}. If they don´t receive the money by then, they can request a County Court Judgment.',
-        'The claimant has accepted your plan to pay £${defendantAdmittedAmount} immediately. Funds must clear <a href={CLAIMANT_DETAILS_URL} class="govuk-link">their account</a> by ${defendantAdmittedAmountPaymentDeadline}. If they don´t receive the money by then, they can request a County Court Judgment.',
+        'The claimant has accepted your plan to pay £${defendantAdmittedAmount} immediately. Funds must clear <a href={VIEW_INFO_ABOUT_CLAIMANT} class="govuk-link">their account</a> by ${defendantAdmittedAmountPaymentDeadline}. If they don´t receive the money by then, they can request a County Court Judgment.',
+        'The claimant has accepted your plan to pay £${defendantAdmittedAmount} immediately. Funds must clear <a href={VIEW_INFO_ABOUT_CLAIMANT} class="govuk-link">their account</a> by ${defendantAdmittedAmountPaymentDeadline}. If they don´t receive the money by then, they can request a County Court Judgment.',
         'DEFENDANT');
 
 /**
