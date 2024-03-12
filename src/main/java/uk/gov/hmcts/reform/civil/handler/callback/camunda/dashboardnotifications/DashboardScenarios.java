729acdd91e9b1c00890844cac9fe96d265b2d333
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-03-12 16:32:46 +0000

    merge conflict fixed

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardScenarios.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardScenarios.java
@@ -15,10 +15,9 @@ public enum DashboardScenarios {
     SCENARIO_AAA7_CLAIM_ISSUE_HWF_INFO_REQUIRED("Scenario.AAA7.ClaimIssue.HWF.InfoRequired"),
     SCENARIO_AAA7_CLAIM_ISSUE_HWF_UPDATED("Scenario.AAA7.ClaimIssue.HWF.Updated"),
     SCENARIO_AAA7_CLAIMANT_INTENT_CCJ_REQUESTED_CLAIMANT("Scenario.AAA7.ClaimantIntent.CCJ.Requested.Claimant"),
-    SCENARIO_AAA7_CLAIM_PART_ADMIT_CLAIMANT("Scenario.AAA7.ClaimantIntent.PartAdmit.Claimant"),
-    SCENARIO_AAA7_CLAIMANT_INTENT_GO_TO_HEARING("Scenario.AAA7.ClaimantIntent.GoToHearing.Claimant"),
     SCENARIO_AAA7_CLAIMANT_INTENT_CLAIM_SETTLED_CLAIMANT("Scenario.AAA7.ClaimantIntent.ClaimSettled.Claimant"),
     SCENARIO_AAA7_CLAIMANT_INTENT_SETTLEMENT_AGREEMENT("Scenario.AAA7.ClaimantIntent.SettlementAgreement.AcceptOrRejectDefPlan.Claimant");
+
 
     private final String scenario;
 
