0af74163630d815c2ffce06d5d8ec9a9fc07b419
neeta-hmcts <neeta.shinde@hmcts.net>
2024-03-12 10:43:00 +0000

    reverted es query changes.

--- src/test/java/uk/gov/hmcts/reform/civil/service/search/FullAdmitPayImmediatelyNoPaymentFromDefendantSearchServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/search/FullAdmitPayImmediatelyNoPaymentFromDefendantSearchServiceTest.java
@@ -3,15 +3,20 @@ package uk.gov.hmcts.reform.civil.service.search;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import uk.gov.hmcts.reform.civil.model.search.Query;
+import uk.gov.hmcts.reform.civil.utils.DateUtils;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
 import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
-import static org.elasticsearch.index.query.QueryBuilders.rangeQuery;
 
 class FullAdmitPayImmediatelyNoPaymentFromDefendantSearchServiceTest extends ElasticSearchServiceTest {
+
+    private static final int BUSINESS_DAYS_FROM_NOW = 0;
+    public static final LocalTime END_OF_BUSINESS_DAY = LocalTime.of(16, 0, 0);
 
     @BeforeEach
     void setup() {
@@ -20,11 +25,15 @@ class FullAdmitPayImmediatelyNoPaymentFromDefendantSearchServiceTest extends Ela
 
     @Override
     protected Query buildQuery(int fromValue) {
+        String expectedDate = DateUtils.addDaysSkippingWeekends(
+                LocalDate.now().minusDays(1), BUSINESS_DAYS_FROM_NOW).atTime(END_OF_BUSINESS_DAY)
+            .format(DateTimeFormatter.ISO_DATE);
 
         BoolQueryBuilder query = boolQuery()
             .minimumShouldMatch(1)
             .should(boolQuery()
-                        .must(rangeQuery("data.respondToClaimAdmitPartLRspec.whenWillThisAmountBePaid").lt("now"))
+                        .must(matchQuery("data.respondToClaimAdmitPartLRspec.whenWillThisAmountBePaid",
+                                         expectedDate))
                         .must(boolQuery().must(matchQuery("state", "AWAITING_APPLICANT_INTENTION"))));
         return new Query(query, List.of("reference"), fromValue);
     }
