79192cfca6141215f626364733e8145bf1d82614
paul-pearson <paul.pearson@version1.com>
2024-03-12 12:22:31 +0000

    CIV-12831 Fix checkstyle

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/CourtFavoursClaimantSettlementAgreementScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/CourtFavoursClaimantSettlementAgreementScenarioTest.java
@@ -40,14 +40,18 @@ public class CourtFavoursClaimantSettlementAgreementScenarioTest extends BaseInt
                 jsonPath("$[0].titleCy").value("Settlement agreement"),
                 jsonPath("$[0].descriptionEn")
                     .value("<p class=\"govuk-body\">The claimant has rejected your plan and asked you to sign a settlement agreement.</p>"
-                               + "<p class=\"govuk-body\">The claimant has proposed a new repayment plan and the court has agreed with it, based on the financial details you provided.</p>"
-                               + "<p class=\"govuk-body\">You must respond by 16 March 2024. If you do not respond by then, or reject the agreement, they can request a County Court Judgment.</p>"
+                               + "<p class=\"govuk-body\">The claimant has proposed a new repayment plan and the court has agreed with it, "
+                               + "based on the financial details you provided.</p>"
+                               + "<p class=\"govuk-body\">You must respond by 16 March 2024. "
+                               + "If you do not respond by then, or reject the agreement, they can request a County Court Judgment.</p>"
                                + "<p class=\"govuk-body\"><a href=\"{VIEW_REPAYMENT_PLAN}\"  rel=\"noopener noreferrer\" class=\"govuk-link\">View the repayment plan</a><br>"
                                + "<a href=\"{VIEW_RESPONSE_TO_CLAIM}\"  rel=\"noopener noreferrer\" class=\"govuk-link\">View your response</a></p>"),
                 jsonPath("$[0].descriptionCy")
                     .value("<p class=\"govuk-body\">The claimant has rejected your plan and asked you to sign a settlement agreement.</p>"
-                               + "<p class=\"govuk-body\">The claimant has proposed a new repayment plan and the court has agreed with it, based on the financial details you provided.</p>"
-                               + "<p class=\"govuk-body\">You must respond by 16 March 2024. If you do not respond by then, or reject the agreement, they can request a County Court Judgment.</p>"
+                               + "<p class=\"govuk-body\">The claimant has proposed a new repayment plan and the court has agreed with it, "
+                               + "based on the financial details you provided.</p>"
+                               + "<p class=\"govuk-body\">You must respond by 16 March 2024. "
+                               + "If you do not respond by then, or reject the agreement, they can request a County Court Judgment.</p>"
                                + "<p class=\"govuk-body\"><a href=\"{VIEW_REPAYMENT_PLAN}\"  rel=\"noopener noreferrer\" class=\"govuk-link\">View the repayment plan</a><br>"
                                + "<a href=\"{VIEW_RESPONSE_TO_CLAIM}\"  rel=\"noopener noreferrer\" class=\"govuk-link\">View your response</a></p>")
             );
