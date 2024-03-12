8cbe9d1abf985b3226db2dfee6e0b7d26d61f553
kalachandrasekar1 <114995593+kalachandrasekar1@users.noreply.github.com>
2024-03-12 11:51:50 +0000

    CIV-13012 and CIV-13013 settlement agreement (#4171)
    * CIV-13012 Retain case state if signed settlement agreement
    
    * CIV-13012 Update tests after changes
    
    * CIV-13012 Add test for AWAITING_APPLICANT_INTENTION state
    
    * CIV-13013 changes
    
    ---------
    
    Co-authored-by: paul-pearson <paul.pearson@version1.com>
    Co-authored-by: Paul Pearson <46532983+paul-pearson@users.noreply.github.com>
    Co-authored-by: Pablo Ortiz <pablo.gutierrez@version1.com>
    Co-authored-by: Raja Mani <rajakm@gmail.com>
    Co-authored-by: jarekPierchala <118526007+jarekPierchala@users.noreply.github.com>
    Co-authored-by: sankhajuria <sankhajuria@gmail.com>

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/caseevents/UpdateClaimantIntentionClaimStateCallbackHandlerTests.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/caseevents/UpdateClaimantIntentionClaimStateCallbackHandlerTests.java
@@ -9,14 +9,18 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.config.ToggleConfiguration;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.citizenui.CaseDataLiP;
+import uk.gov.hmcts.reform.civil.model.citizenui.ClaimantLiPResponse;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.service.UpdateClaimStateService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 
 @ExtendWith(MockitoExtension.class)
@@ -54,5 +58,27 @@ public class UpdateClaimantIntentionClaimStateCallbackHandlerTests extends BaseC
         // then
         assertThat(response.getErrors()).isNull();
         verify(updateClaimStateService, times(1)).setUpCaseState(caseData);
+    }
+
+    @Test
+    void shouldNotChangeCaseStateIfClaimantSignedSettlementAgreement() {
+        // given
+        CaseData caseData = CaseDataBuilder.builder().build();
+        caseData = caseData.toBuilder()
+            .caseDataLiP(CaseDataLiP.builder()
+                .applicant1LiPResponse(ClaimantLiPResponse.builder()
+                    .applicant1SignedSettlementAgreement(YesOrNo.YES)
+                    .build())
+                .build())
+            .build();
+
+        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+
+        // when
+        var response = (AboutToStartOrSubmitCallbackResponse)handler.handle(params);
+
+        // then
+        assertThat(response.getErrors()).isNull();
+        verifyNoInteractions(updateClaimStateService);
     }
 }
