649b76ff0844a14252fafaf2644523e063d4ed7b
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-03-12 09:53:23 +0000

    checkstyle fixed

--- src/main/java/uk/gov/hmcts/reform/civil/utils/DateUtils.java
+++ src/main/java/uk/gov/hmcts/reform/civil/utils/DateUtils.java
@@ -23,6 +23,7 @@ public class DateUtils {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy");
         return date.format(formatter);
     }
+
     public static LocalDateTime convertFromUTC(LocalDateTime utcDate) {
         ZonedDateTime utcZonedDateTime = utcDate.atZone(ZoneId.of("UTC"));
         ZonedDateTime ukZonedDateTime = utcZonedDateTime.withZoneSameInstant(ZoneId.of("Europe/London"));
