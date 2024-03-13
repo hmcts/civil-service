eb82595c494731d06b31e7476be8787c13740335
laarmada <sergiosalmar@yahoo.es>
2024-03-13 15:20:06 +0000

    fix lint error

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/CCJRequestedScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/CCJRequestedScenarioTest.java
@@ -46,7 +46,9 @@ public class CCJRequestedScenarioTest extends BaseIntegrationTest {
                 jsonPath("$[0].descriptionEn").value(
                     "<p class=\"govuk-body\">" + claimantName + "has requested CCJ against you, because the response deadline has passed.</p>"
                         + "<p class=\"govuk-body\">Your online account will not be updated with the progress of the claim, and any further updates will be by post.</p>"
-                        + "<p class=\"govuk-body\">If your deadline has passed, but the CCJ has not been issued, you can still respond. Get in touch with HMCTS on {civilMoneyClaimsTelephone} if you are in England and Wales, or 0300 790 6234 if you are in Scotland. You can call from Monday to Friday, between 8.30am to 5pm.</p>"
+                        + "<p class=\"govuk-body\">If your deadline has passed, but the CCJ has not been issued, you can still respond. " +
+                        "Get in touch with HMCTS on {civilMoneyClaimsTelephone} if you are in England and Wales, or 0300 790 6234 if you are in Scotland. " +
+                        "You can call from Monday to Friday, between 8.30am to 5pm.</p>"
                         + "<p class=\"govuk-body\"><a href=\"{callChargesUrl}\" rel=\"noopener noreferrer\" class=\"govuk-link\">Find out about call charges (opens in new tab).</a></p>"
                         + "<p class=\"govuk-body\">If you do not get in touch, we will post a CCJ to you and <Name> and explain what to do next.</p>"));
 
