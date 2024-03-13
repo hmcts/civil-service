f30828afa902500d177f06ba38f94643ceae20ad
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-03-13 14:27:43 +0100

    CIV-12821 : Fix tests

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/ClaimantAcceptDefendantSettlementAgreementScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/ClaimantAcceptDefendantSettlementAgreementScenarioTest.java
@@ -1,28 +1,21 @@
 package uk.gov.hmcts.reform.civil.controllers.dashboard.scenarios.defendant;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.http.HttpStatus;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import uk.gov.hmcts.reform.civil.controllers.BaseIntegrationTest;
 import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
-
 import java.util.Map;
 import java.util.UUID;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_CLAIMANT_INTENT_SETTLEMENT_AGREEMENT_CLAIMANT_ACCEPTS_DEFENDANT;
-
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Testcontainers
 public class ClaimantAcceptDefendantSettlementAgreementScenarioTest extends BaseIntegrationTest {
-
     @Test
     void should_create_scenario_for_claimant_accept_defendant_plan_settlement_agreement() throws Exception {
-
         UUID caseId = UUID.randomUUID();
-
         doPost(
             BEARER_TOKEN,
             ScenarioRequestParams.builder()
@@ -34,7 +27,6 @@ public class ClaimantAcceptDefendantSettlementAgreementScenarioTest extends Base
             caseId
         )
             .andExpect(status().isOk());
-
         //Verify Notification is created
         doGet(BEARER_TOKEN, GET_NOTIFICATIONS_URL, caseId, "DEFENDANT")
             .andExpect(status().isOk())
@@ -45,17 +37,17 @@ public class ClaimantAcceptDefendantSettlementAgreementScenarioTest extends Base
                 jsonPath("$[0].descriptionEn")
                     .value(
                         "<p class=\"govuk-body\">The claimant has accepted your plan and asked you to sign a settlement agreement."
-                            + " You must respond by 16 March 2024.</p><p class=\"govuk-body\">If you do not respond by then, or reject the agreement," +
-                            " they can request a County Court Judgment.</p>"
-                            + "<p class=\"govuk-body\"><a href=\"{VIEW_REPAYMENT_PLAN}\" rel=\"noopener noreferrer\" class=\"govuk-link\">View the repayment plan</a>"
-                            + "<br><a href=\"{VIEW_RESPONSE_TO_CLAIM}\" rel=\"noopener noreferrer\" class=\"govuk-link\">View your response</a></p>"),
+                            + " You must respond by 16 March 2024.</p><p class=\"govuk-body\">If you do not respond by then, or reject the agreement,"
+                            + " they can request a County Court Judgment.</p><p class=\"govuk-body\"><a href=\"{VIEW_REPAYMENT_PLAN}\""
+                            + " rel=\"noopener noreferrer\" class=\"govuk_link\">View the repayment plan</a><br><a href=\"{VIEW_RESPONSE_TO_CLAIM}\" "
+                            + "rel=\"noopener noreferrer\" class=\"govuk_link\">View your response</a></p>"),
                 jsonPath("$[0].descriptionCy")
                     .value(
                         "<p class=\"govuk-body\">The claimant has accepted your plan and asked you to sign a settlement agreement."
-                            + " You must respond by 16 March 2024.</p><p class=\"govuk-body\">If you do not respond by then, or reject the agreement," +
-                            " they can request a County Court Judgment.</p>"
-                            + "<p class=\"govuk-body\"><a href=\"{VIEW_REPAYMENT_PLAN}\" rel=\"noopener noreferrer\" class=\"govuk-link\">View the repayment plan</a>"
-                            + "<br><a href=\"{VIEW_RESPONSE_TO_CLAIM}\" rel=\"noopener noreferrer\" class=\"govuk-link\">View your response</a></p>")
+                            + " You must respond by 16 March 2024.</p><p class=\"govuk-body\">If you do not respond by then, or reject the agreement,"
+                            + " they can request a County Court Judgment.</p><p class=\"govuk-body\"><a href=\"{VIEW_REPAYMENT_PLAN}\""
+                            + " rel=\"noopener noreferrer\" class=\"govuk_link\">View the repayment plan</a><br><a href=\"{VIEW_RESPONSE_TO_CLAIM}\" "
+                            + "rel=\"noopener noreferrer\" class=\"govuk_link\">View your response</a></p>")
             );
     }
 }
