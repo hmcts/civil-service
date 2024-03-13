58ca29f94bb8908077b2e778745a498ad2c89b3d
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2024-03-13 11:17:37 +0000

    CIV-12801 No remission dashboard notification (#4138)
    * CIV-11104 Auth vulnerability assigning cases
    
    * CIV-12800 common template for notifications part remission
    
    * CIV-12757 Dashboard Notification HWF
    
    * CIV-12757 integration test modified
    
    * CIV-12757 remove typeOfFee param
    
    * CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"
    
    * CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"
    
    * CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"
    
    * CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"
    
    * CIV-12784 Dashboard Notification - HwF - When Case worker selects "Invalid HwF Number"
    
    * CIV-12801 No remission dashboard -claimant
    
    * CIV-12757 update script
    
    * CIV-12757 update test
    
    * CIV-12801 No remission dashboard -claimant
    
    * CIV-12801 No remission dashboard -claimant
    
    * CIV-12801 No remission dashboard -claimant
    
    * CIV-12757 fix db issue
    
    * CIV-12757 fix db issue task
    
    * CIV-12801 rebase 12757
    
    * CIV-12801 rebase team2b
    
    * CIV-12801 notification formatting
    
    * CIV-12801 notification formatting
    
    * Use response deadline for eng and welsh
    
    ---------
    
    Co-authored-by: jeswanth <jeswanth.reddy@hmcts.net>
    Co-authored-by: miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
    Co-authored-by: Manish Garg <manish.garg@hmcts.net>
    Co-authored-by: dharmendra kumar <dharmendra.kumar@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/HwFDashboardNotificationsHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/HwFDashboardNotificationsHandler.java
@@ -19,13 +19,15 @@ import java.util.Map;
 import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CLAIMANT1_HWF_DASHBOARD_NOTIFICATION;
-import static uk.gov.hmcts.reform.civil.callback.CaseEvent.PARTIAL_REMISSION_HWF_GRANTED;
-import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_CLAIM_ISSUE_HWF_PART_REMISSION;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.INVALID_HWF_REFERENCE;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.MORE_INFORMATION_HWF;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NO_REMISSION_HWF;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.PARTIAL_REMISSION_HWF_GRANTED;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UPDATE_HELP_WITH_FEE_NUMBER;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_CLAIM_ISSUE_HWF_INFO_REQUIRED;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_CLAIM_ISSUE_HWF_INVALID_REF;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_CLAIM_ISSUE_HWF_NO_REMISSION;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_CLAIM_ISSUE_HWF_PART_REMISSION;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_CLAIM_ISSUE_HWF_UPDATED;
 
 @Service
@@ -37,6 +39,7 @@ public class HwFDashboardNotificationsHandler extends CallbackHandler {
     private final DashboardApiClient dashboardApiClient;
     private final DashboardNotificationsParamsMapper mapper;
     private final Map<CaseEvent, String> dashboardScenarios = Map.of(
+        NO_REMISSION_HWF, SCENARIO_AAA7_CLAIM_ISSUE_HWF_NO_REMISSION.getScenario(),
         INVALID_HWF_REFERENCE, SCENARIO_AAA7_CLAIM_ISSUE_HWF_INVALID_REF.getScenario(),
         MORE_INFORMATION_HWF, SCENARIO_AAA7_CLAIM_ISSUE_HWF_INFO_REQUIRED.getScenario(),
         UPDATE_HELP_WITH_FEE_NUMBER, SCENARIO_AAA7_CLAIM_ISSUE_HWF_UPDATED.getScenario(),
