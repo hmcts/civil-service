c5b30ae2d62242851fc6932e8f70786509ab008f
dharmendra kumar <dharmendra.kumar@hmcts.net>
2024-03-13 17:05:51 +0000

    Correct param name for claimant settle

--- src/main/resources/db/migration/V2024_03_05_1500__CIV-12753_creat_claimant_claim_settled_scenario.sql
+++ src/main/resources/db/migration/V2024_03_05_1500__CIV-12753_creat_claimant_claim_settled_scenario.sql
@@ -1,6 +1,6 @@
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
 VALUES ('Scenario.AAA7.ClaimantIntent.ClaimSettled.Claimant',
-        '{"Notice.AAA7.DefResponse.PartAdmit.AlreadyPaid.Claimant", "Notice.AAA7.DefResponse.Full Defence.AlreadyPaid.Claimant"}', '{"Notice.AAA7.ClaimantIntent.ClaimSettled.Claimant":["claimSettledAmount", "claimSettledDate"]}');
+        '{"Notice.AAA7.DefResponse.PartAdmit.AlreadyPaid.Claimant", "Notice.AAA7.DefResponse.Full Defence.AlreadyPaid.Claimant"}', '{"Notice.AAA7.ClaimantIntent.ClaimSettled.Claimant":["claimSettledAmount", "claimSettledDateEn", "claimSettledDateCy"]}');
 
 /**
  * Add notification template
@@ -8,6 +8,6 @@ VALUES ('Scenario.AAA7.ClaimantIntent.ClaimSettled.Claimant',
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
 VALUES ('Notice.AAA7.ClaimantIntent.ClaimSettled.Claimant', 'The claim is settled', 'The claim is settled',
-        'You have confirmed that the defendant paid ${claimSettledAmount} on ${claimSettledDate}.',
-        'You have confirmed that the defendant paid ${claimSettledAmount} on ${claimSettledDate}.',
+        'You have confirmed that the defendant paid ${claimSettledAmount} on ${claimSettledDateEn}.',
+        'You have confirmed that the defendant paid ${claimSettledAmount} on ${claimSettledDateCy}.',
         'CLAIMANT');
