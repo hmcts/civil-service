9c59ead8fa6c63a080abe8783ca8fe34c028d955
laarmada <sergiosalmar@yahoo.es>
2024-03-14 14:20:07 +0000

    PR review changes

--- src/main/resources/db/migration/V2024_03_08_1630__CIV-12747_claimantIntent_ccj_requested_scenario.sql
+++ src/main/resources/db/migration/V2024_03_08_1630__CIV-12747_claimantIntent_ccj_requested_scenario.sql
@@ -14,13 +14,11 @@ VALUES ('Notice.AAA7.ClaimantIntent.CCJ.Requested.Defendant', 'Claimant has requ
         '<p class="govuk-body">${applicant1PartyName} has requested CCJ against you, because the response deadline has passed.</p>'
         '<p class="govuk-body">Your online account will not be updated with the progress of the claim, and any further updates will be by post.</p>'
         '<p class="govuk-body">If your deadline has passed, but the CCJ has not been issued, you can still respond. Get in touch with HMCTS on {civilMoneyClaimsTelephone} if you are in England and Wales, or 0300 790 6234 if you are in Scotland. ' ||
-        'You can call from Monday to Friday, between 8.30am to 5pm.</p>'||
-        '<p class="govuk-body"><a href="https://www.gov.uk/call-charges" rel="noopener noreferrer" class="govuk-link">Find out about call charges (opens in new tab).</a></p>'
+        'You can call from Monday to Friday, between 8.30am to 5pm. <a href="https://www.gov.uk/call-charges" target="_blank" rel="noopener noreferrer" class="govuk-link">Find out about call charges (opens in new tab).</a></p>'
         '<p class="govuk-body">If you do not get in touch, we will post a CCJ to you and <Name> and explain what to do next.</p>',
         '<p class="govuk-body">${applicant1PartyName} has requested CCJ against you, because the response deadline has passed.</p>'
         '<p class="govuk-body">Your online account will not be updated with the progress of the claim, and any further updates will be by post.</p>'
         '<p class="govuk-body">If your deadline has passed, but the CCJ has not been issued, you can still respond. Get in touch with HMCTS on {civilMoneyClaimsTelephone} if you are in England and Wales, or 0300 790 6234 if you are in Scotland. ' ||
-        'You can call from Monday to Friday, between 8.30am to 5pm.</p>'||
-        '<p class="govuk-body"><a href="https://www.gov.uk/call-charges" rel="noopener noreferrer" class="govuk-link">Find out about call charges (opens in new tab).</a></p>'
+        'You can call from Monday to Friday, between 8.30am to 5pm. <a href="https://www.gov.uk/call-charges" target="_blank" rel="noopener noreferrer" class="govuk-link">Find out about call charges (opens in new tab).</a></p>'
         '<p class="govuk-body">If you do not get in touch, we will post a CCJ to you and <Name> and explain what to do next.</p>',
 'DEFENDANT');
