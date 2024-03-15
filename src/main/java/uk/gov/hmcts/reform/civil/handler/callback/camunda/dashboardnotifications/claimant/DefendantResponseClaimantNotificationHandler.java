b7983b02e5fe65c0a006cb6508cd9828c7cba7e4
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2024-03-15 16:44:14 +0000

    CIV-13115 Set Up Handler

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/DefendantResponseClaimantNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/DefendantResponseClaimantNotificationHandler.java
@@ -22,7 +22,7 @@ import java.util.Map;
 import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_CLAIMANT_DASHBOARD_NOTIFICATION_FOR_DEFENDANT_RESPONSE;
-import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_DEFENDANT_ADMIT_PAY_IMMEDIATELY_DEFENDANT;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_DEFENDANT_ADMIT_PAY_IMMEDIATELY_CLAIMANT;
 
 @Service
 @RequiredArgsConstructor
@@ -51,7 +51,12 @@ public class DefendantResponseClaimantNotificationHandler extends CallbackHandle
     }
 
     private String getScenario(CaseData caseData) {
-
+        if (caseData.getRespondentClaimResponseTypeForSpecGeneric()
+            .equals(RespondentResponseTypeSpec.FULL_ADMISSION)
+            && caseData.getDefenceAdmitPartPaymentTimeRouteRequired()
+            .equals(RespondentResponsePartAdmissionPaymentTimeLRspec.IMMEDIATELY)) {
+            return SCENARIO_AAA7_DEFENDANT_ADMIT_PAY_IMMEDIATELY_CLAIMANT.getScenario();
+        }
         return null;
     }
 
