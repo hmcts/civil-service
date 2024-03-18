b56d212b44c700720297d1851d568cd3652bf7c9
dharmendra kumar <dharmendra.kumar@hmcts.net>
2024-03-18 18:44:11 +0000

    Add full remission in delete

--- src/main/resources/db/migration/V2024_02_29_1207__CIV-12653_claim_issued_claimant_scenario.sql
+++ src/main/resources/db/migration/V2024_02_29_1207__CIV-12653_claim_issued_claimant_scenario.sql
@@ -3,7 +3,7 @@
  */
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
 VALUES ('Scenario.AAA7.ClaimIssue.Response.Await',
-        '{"Notice.AAA7.ClaimIssue.ClaimFee.Required", "Notice.AAA7.ClaimIssue.HWF.Requested", "Notice.AAA7.ClaimIssue.HWF.InvalidRef", "Notice.AAA7.ClaimIssue.HWF.InfoRequired", "Notice.AAA7.ClaimIssue.HWF.Updated", "Notice.AAA7.ClaimIssue.HWF.PartRemission", "Notice.AAA7.ClaimIssue.HWF.Rejected", "Notice.AAA7.ClaimIssue.HWF.PhonePayment"}',
+        '{"Notice.AAA7.ClaimIssue.ClaimFee.Required", "Notice.AAA7.ClaimIssue.HWF.Requested", "Notice.AAA7.ClaimIssue.HWF.InvalidRef", "Notice.AAA7.ClaimIssue.HWF.InfoRequired", "Notice.AAA7.ClaimIssue.HWF.Updated", "Notice.AAA7.ClaimIssue.HWF.PartRemission", "Notice.AAA7.ClaimIssue.HWF.Rejected", "Notice.AAA7.ClaimIssue.HWF.PhonePayment", "Notice.AAA7.ClaimIssue.HWF.FullRemission"}',
         '{"Notice.AAA7.ClaimIssue.Response.Await":["respondent1ResponseDeadlineEn", "respondent1ResponseDeadlineCy", "respondent1PartyName"]}');
 
 /**
