ce3ef3374312ac5870b7a2f6613219149c1c69eb
kenneth-hmcts <91327278+kenneth-hmcts@users.noreply.github.com>
2024-03-19 13:15:47 +0000

    CIV-12808 Dashboard notification Defendant states paid - claimant rejects (#4216)
    * CIV-12808 Set Up civil service changes
    
    * CIV-12808 Set Up unit test
    
    * CIV-12808 Add Unit Test
    
    * CIV-12808 Update check style
    
    * CIV-12808 Update check style
    
    * CIV-12808 Update check style
    
    * CIV-12808 Update check style
    
    * CIV-12808 Update check style
    
    * CIV-12808 Update StateFlowEngine
    
    * CIV-12808 Update StateFlowEngine
    
    * CIV-12808 Update StateFlowEngine
    
    * CIV-12808 Update StateFlowEngine

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardScenarios.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardScenarios.java
@@ -35,7 +35,8 @@ public enum DashboardScenarios {
             "Scenario.AAA7.ClaimantIntent.GoToHearing.DefPartAdmit.FullDefence.StatesPaid.ClaimantConfirms.Defendant"
     ),
     SCENARIO_AAA7_CLAIMANT_INTENT_GO_TO_HEARING_DEF_FULL_DEFENCE_CLAIMANT_DISPUTES_DEFENDANT("Scenario.AAA7.ClaimantIntent.GoToHearing.DefFullDefence.ClaimantDisputes.Defendant"),
-    SCENARIO_AAA7_CLAIMANT_INTENT_SETTLEMENT_AGREEMENT("Scenario.AAA7.ClaimantIntent.SettlementAgreement.AcceptOrRejectDefPlan.Claimant");
+    SCENARIO_AAA7_CLAIMANT_INTENT_SETTLEMENT_AGREEMENT("Scenario.AAA7.ClaimantIntent.SettlementAgreement.AcceptOrRejectDefPlan.Claimant"),
+    SCENARIO_AAA7_CLAIMANT_REJECTED_NOT_PAID_DEFENDANT("Scenario.AAA7.ClaimantIntent.GoToHearing.DefPartAdmit.FullDefence.StatesPaid.PartOrFull.ClaimantDisputes.Defendant");
 
     private final String scenario;
 
