959f298d832956dbb991c94e215515a45d225ece
paul-pearson <paul.pearson@version1.com>
2024-03-19 14:41:22 +0000

    CIV-13701 Use doPost in test

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/DefendantRequestMoreTimeScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/DefendantRequestMoreTimeScenarioTest.java
@@ -2,7 +2,11 @@ package uk.gov.hmcts.reform.civil.controllers.dashboard.scenarios.claimant;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_CLAIM_ISSUE_HWF_INFO_REQUIRED;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_DEFENDANT_RESPONSE_MORE_TIME_REQUESTED_CLAIMANT;
 
+import java.util.Map;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +15,7 @@ import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.defendant.ResponseDeadlineExtendedDashboardNotificationHandler;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
 
 public class DefendantRequestMoreTimeScenarioTest extends DashboardBaseIntegrationTest {
 
@@ -20,12 +25,16 @@ public class DefendantRequestMoreTimeScenarioTest extends DashboardBaseIntegrati
     @Test
     void should_create_more_time_requested_scenario() throws Exception {
 
-        String caseId = "31415926";
-        CaseData caseData = CaseDataBuilder.builder().atStateClaimIssued().build()
-            .toBuilder().respondent1Represented(YesOrNo.NO)
-            .build();
-
-        handler.handle(callbackParams(caseData));
+        UUID caseId = UUID.randomUUID();
+        doPost(
+            BEARER_TOKEN,
+            ScenarioRequestParams.builder().params(Map.of("defaultRespondTime", "4pm",
+                                                          "respondent1ResponseDeadlineEn", "1 April 2024",
+                                                          "respondent1ResponseDeadlineCy", "1 April 2024")).build(),
+            DASHBOARD_CREATE_SCENARIO_URL,
+            SCENARIO_AAA7_DEFENDANT_RESPONSE_MORE_TIME_REQUESTED_CLAIMANT.getScenario(),
+            caseId
+        ).andExpect(status().isOk());
 
         //Verify Notification is created
         doGet(BEARER_TOKEN, GET_NOTIFICATIONS_URL, caseId, "CLAIMANT")
@@ -38,10 +47,10 @@ public class DefendantRequestMoreTimeScenarioTest extends DashboardBaseIntegrati
                 status().is(HttpStatus.OK.value()),
                 jsonPath("$[0].titleEn").value("More time requested"),
                 jsonPath("$[0].descriptionEn").value(
-                    "<p class=\"govuk-body\">The response deadline for the defendant is now ${defaultRespondTime} on ${respondent1ResponseDeadlineEn} ({daysLeftToRespond} days remaining).</p>"),
+                    "<p class=\"govuk-body\">The response deadline for the defendant is now 4pm on 1 April 2024 ({daysLeftToRespond} days remaining).</p>"),
                 jsonPath("$[0].titleCy").value("More time requested"),
                 jsonPath("$[0].descriptionCy").value(
-                    "<p class=\"govuk-body\">The response deadline for the defendant is now ${defaultRespondTime} on ${respondent1ResponseDeadlineEn} ({daysLeftToRespond} days remaining).</p>")
+                    "<p class=\"govuk-body\">The response deadline for the defendant is now 4pm on 1 April 2024 ({daysLeftToRespond} days remaining).</p>")
             );
     }
 }
