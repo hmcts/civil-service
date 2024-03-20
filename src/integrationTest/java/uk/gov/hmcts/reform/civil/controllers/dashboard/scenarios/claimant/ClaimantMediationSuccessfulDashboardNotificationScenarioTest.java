36a42a2db6d94678ca0285ad391e847159cd8971
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-03-20 11:33:09 +0000

    fix checkstyle

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimantMediationSuccessfulDashboardNotificationScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimantMediationSuccessfulDashboardNotificationScenarioTest.java
@@ -15,7 +15,6 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 public class ClaimantMediationSuccessfulDashboardNotificationScenarioTest extends DashboardBaseIntegrationTest {
 
     @Autowired
