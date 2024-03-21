5c45fd54699344269367879f37a827ea1a89eeb9
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2024-03-21 10:28:58 +0000

    CIV-13116 fix script

--- src/main/resources/db/migration/V2024_03_18_1002__CIV-13116_defendant_response_part_admit_claimant.sql
+++ src/main/resources/db/migration/V2024_03_18_1002__CIV-13116_defendant_response_part_admit_claimant.sql
@@ -13,6 +13,6 @@ INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, titl
                                                   ,notification_role)
 VALUES ('Notice.AAA7.DefResponse.PartAdmit.PayImmediately.Claimant',
         'Response to the claim', 'Response to the claim',
-        '<p class="govuk-body">The defendant has offered to pay ${defendantAdmittedAmount} by ${respondent1AdmittedAmountPaymentDeadlineEn}.<br>The payment must clear the account by then, if not you can request a county court judgment.<br><a href="{CLAIMANT_RESPONSE_TASK_LIST}" class="govuk-link">View and respond</a>.</p>',
-        '<p class="govuk-body">The defendant has offered to pay ${defendantAdmittedAmount} by ${respondent1AdmittedAmountPaymentDeadlineCy}.<br>The payment must clear the account by then, if not you can request a county court judgment.<br><a href="{CLAIMANT_RESPONSE_TASK_LIST}" class="govuk-link">View and respond</a>.</p>',
+        '<p class="govuk-body">The defendant has offered to pay £${defendantAdmittedAmount} by ${respondent1AdmittedAmountPaymentDeadlineEn}.<br>The payment must clear the account by then, if not you can request a county court judgment.<br><a href="{CLAIMANT_RESPONSE_TASK_LIST}" class="govuk-link">View and respond</a>.</p>',
+        '<p class="govuk-body">The defendant has offered to pay £${defendantAdmittedAmount} by ${respondent1AdmittedAmountPaymentDeadlineCy}.<br>The payment must clear the account by then, if not you can request a county court judgment.<br><a href="{CLAIMANT_RESPONSE_TASK_LIST}" class="govuk-link">View and respond</a>.</p>',
         'CLAIMANT');
