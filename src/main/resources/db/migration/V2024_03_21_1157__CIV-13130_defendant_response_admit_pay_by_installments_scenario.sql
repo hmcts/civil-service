197578b499bebb3d6b48584b38b2dbfd0ea64f45
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-03-21 15:37:30 +0000

    CIV-13130 fixes

--- src/main/resources/db/migration/V2024_03_21_1157__CIV-13130_defendant_response_admit_pay_by_installments_scenario.sql
+++ src/main/resources/db/migration/V2024_03_21_1157__CIV-13130_defendant_response_admit_pay_by_installments_scenario.sql
@@ -8,6 +8,6 @@ VALUES ('Scenario.AAA6.DefResponse.FullOrPartAdmit.PayByInstalments.Claimant',
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
 VALUES ('Notice.AAA6.DefResponse.FullOrPartAdmit.PayByInstalments.Claimant', 'Response to the claim', 'The claim is settled',
-        '<p class="govuk-body">The defendant has offered to pay ${defendantAdmittedAmount} in installments of ${installmentAmount} {installmentTimePeriod} starting ${installmentStartDateEn}.</p><p class="govuk-body"><a href="{VIEW_AND_RESPOND}"  rel="noopener noreferrer" class="govuk-link">View and respond</a></p>',
-        '<p class="govuk-body">The defendant has offered to pay ${defendantAdmittedAmount} in installments of ${installmentAmount} {installmentTimePeriod} starting ${installmentStartDateCy}.</p><p class="govuk-body"><a href="{VIEW_AND_RESPOND}"  rel="noopener noreferrer" class="govuk-link">View and respond</a></p>',
+        '<p class="govuk-body">The defendant has offered to pay ${defendantAdmittedAmount} in installments of ${installmentAmount} ${installmentTimePeriod} starting ${installmentStartDateEn}.</p><p class="govuk-body"><a href="{VIEW_AND_RESPOND}"  rel="noopener noreferrer" class="govuk-link">View and respond</a></p>',
+        '<p class="govuk-body">The defendant has offered to pay ${defendantAdmittedAmount} in installments of ${installmentAmount} ${installmentTimePeriod} starting ${installmentStartDateCy}.</p><p class="govuk-body"><a href="{VIEW_AND_RESPOND}"  rel="noopener noreferrer" class="govuk-link">View and respond</a></p>',
         'CLAIMANT');
