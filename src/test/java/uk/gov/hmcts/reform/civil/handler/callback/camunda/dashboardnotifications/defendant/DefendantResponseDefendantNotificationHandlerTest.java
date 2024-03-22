ece91fec77b6efeeaeebe0429a023e554863faf7
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2024-03-22 10:56:40 +0000

    CIV-13115 Set up feature toggle service

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/DefendantResponseDefendantNotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/DefendantResponseDefendantNotificationHandlerTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.defendant;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,6 +17,7 @@ import uk.gov.hmcts.reform.civil.model.RespondToClaimAdmitPartLRspec;
 import uk.gov.hmcts.reform.civil.sampledata.CallbackParamsBuilder;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.service.DashboardNotificationsParamsMapper;
+import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
 
 import java.math.BigDecimal;
@@ -45,7 +47,15 @@ public class DefendantResponseDefendantNotificationHandlerTest extends BaseCallb
     @Mock
     private DashboardNotificationsParamsMapper dashboardNotificationsParamsMapper;
 
+    @Mock
+    private FeatureToggleService featureToggleService;
+
     public static final String TASK_ID = "GenerateDefendantDashboardNotificationDefendantResponse";
+
+    @BeforeEach
+    void setup() {
+        when(featureToggleService.isDashboardServiceEnabled()).thenReturn(true);
+    }
 
     @Test
     void handleEventsReturnsTheExpectedCallbackEvent() {
