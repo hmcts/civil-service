d6392adefe35de9165dfcbdba6df9b54ef0de08d
Sherlyn Khaw <sherlyn.khaw@evoco.co.uk>
2024-03-25 12:14:30 +0000

    test

--- src/main/java/uk/gov/hmcts/reform/civil/service/OrganisationDetailsService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/OrganisationDetailsService.java
@@ -15,6 +15,9 @@ public class OrganisationDetailsService {
 
     public String getApplicantLegalOrganisationName(CaseData caseData) {
         Optional<Organisation> organisation = organisationService.findOrganisationById(caseData.getApplicantOrganisationId());
+        System.out.println(organisation);
+        System.out.println(organisation.map(Organisation::getName).orElse("somehow this is very broken"));
+        System.out.println(organisation.get().getName());
         return organisation.map(Organisation::getName).orElse(caseData.getApplicantSolicitor1ClaimStatementOfTruth().getName());
     }
 
