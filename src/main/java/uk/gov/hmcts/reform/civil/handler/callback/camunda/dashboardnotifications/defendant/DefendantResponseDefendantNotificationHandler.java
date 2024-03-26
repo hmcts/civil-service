1f2e115c869894e98d3f1880c09a571d495ad9c8
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-03-26 12:04:01 +0100

    CIV-13140 fixes

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/DefendantResponseDefendantNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/DefendantResponseDefendantNotificationHandler.java
@@ -22,7 +22,6 @@ import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_DEFENDANT_DASHBOARD_NOTIFICATION_FOR_DEFENDANT_RESPONSE;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_DEFENDANT_ALREADY_PAID;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_DEFENDANT_ADMIT_PAY_IMMEDIATELY_DEFENDANT;
-import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA7_DEFENDANT_FULL_OR_PART_ADMIT_PAY_SET_DATE_ORG_DEFENDANT;
 
 @Service
 @RequiredArgsConstructor
@@ -51,11 +50,6 @@ public class DefendantResponseDefendantNotificationHandler extends CallbackHandl
     }
 
     private String getScenario(CaseData caseData) {
-
-        if (caseData.getRespondent1().isCompanyOROrganisation()
-            && caseData.isPayBySetDate()) {
-            return SCENARIO_AAA7_DEFENDANT_FULL_OR_PART_ADMIT_PAY_SET_DATE_ORG_DEFENDANT.getScenario();
-        }
 
         if ((caseData.isRespondentResponseFullDefence() && caseData.hasDefendantPayedTheAmountClaimed())
             || (caseData.isPartAdmitClaimSpec() && caseData.isPartAdmitAlreadyPaid())) {
