beb526aa2d4be9923390641e9724aa89ad0ade40
jeswanth-hmcts <134285996+jeswanth-hmcts@users.noreply.github.com>
2024-03-27 10:41:59 +0000

    CIV-12943 claim settled defendant notification (#4262)
    * draft changes for CIV-12942
    
    * refactored the code.
    
    * fixed checkstyle error.
    
    * test error ficed.
    
    * refactored the new field changes.
    
    * Update V2024_03_18_1315__CIV-12942_claim_settled_scenario.sql
    
    * fixed unit test.
    
    * CIV-12943 defendant claim settle notification
    
    * CIV-12943 defendant claim settle notification
    
    * CIV-12943 defendant claim settle notification
    
    * CIV-12493 rebased the branch
    
    * CIV-12493 rebased the branch
    
    * Rebase with team2b
    
    ---------
    
    Co-authored-by: neeta-hmcts <neeta.shinde@hmcts.net>
    Co-authored-by: neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
    Co-authored-by: dharmendra kumar <dharmendra.kumar@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardScenarios.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardScenarios.java
@@ -53,7 +53,8 @@ public enum DashboardScenarios {
     SCENARIO_AAA6_DEFENDANT_ADMIT_AND_PAID_PARTIAL_ALREADY_CLAIMANT("Scenario.AAA6.DefResponse.PartAdmit.AlreadyPaid.Claimant"),
     SCENARIO_AA6_DEFENDANT_RESPONSE_PAY_BY_INSTALLMENTS_CLAIMANT("Scenario.AAA6.DefResponse.FullOrPartAdmit.PayByInstalments.Claimant"),
     SCENARIO_AAA6_DEFENDANT_RESPONSE_MORE_TIME_REQUESTED_CLAIMANT("Scenario.AAA6.DefResponse.MoreTimeRequested.Claimant"),
-    SCENARIO_AAA6_DEFENDANT_FULL_OR_PART_ADMIT_PAY_SET_DATE_CLAIMANT("Scenario.AAA6.DefResponse.FullOrPartAdmit.PayBySetDate.Claimant");
+    SCENARIO_AAA6_DEFENDANT_FULL_OR_PART_ADMIT_PAY_SET_DATE_CLAIMANT("Scenario.AAA6.DefResponse.FullOrPartAdmit.PayBySetDate.Claimant"),
+    SCENARIO_AAA6_CLAIMANT_INTENT_CLAIM_SETTLE_EVENT_DEFENDANT("Scenario.AAA6.ClaimantIntent.ClaimSettleEvent.Defendant");
 
     private final String scenario;
 
