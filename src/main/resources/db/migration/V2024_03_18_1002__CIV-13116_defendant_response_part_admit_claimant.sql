965bebe866af6bcec81fa02e81887670c55d4c8e
dharmendra kumar <dharmendra.kumar@hmcts.net>
2024-03-28 09:30:01 +0000

    Add more time request for deletion

--- src/main/resources/db/migration/V2024_03_18_1002__CIV-13116_defendant_response_part_admit_claimant.sql
+++ src/main/resources/db/migration/V2024_03_18_1002__CIV-13116_defendant_response_part_admit_claimant.sql
@@ -3,7 +3,7 @@
  */
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
 VALUES ('Scenario.AAA6.DefResponse.PartAdmit.PayImmediately.Claimant',
-        '{"Notice.AAA6.ClaimIssue.Response.Await", "Notice.AAA6.DefResponse.MoretimeRequested.Claimant"}',
+        '{"Notice.AAA6.ClaimIssue.Response.Await", "Notice.AAA6.DefResponse.MoretimeRequested.Claimant", "Notice.AAA6.DefResponse.MoreTimeRequested.Claimant"}',
         '{"Notice.AAA6.DefResponse.PartAdmit.PayImmediately.Claimant" : ["defendantAdmittedAmount","respondent1AdmittedAmountPaymentDeadlineEn","respondent1AdmittedAmountPaymentDeadlineCy"]}');
 
 /**
