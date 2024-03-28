77647f86675a8aa54421ee1c2956f206038fb456
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2024-03-28 11:49:07 +0000

    CIV-13073 Dashboard notification - Judgment Requested (where court agrees with claimant) (By Admission -Defendant) (#4292)
    * CIV-13070 ccj dashboard notifications
    
    * CIV-13070 added testcases for handlers
    
    * CIV-13073 Dashboard notification - Judgment Requested (where court agrees with claimant) (By Admission -Defendant)
    
    * CIV-13073 Dashboard notification - Judgment Requested (where court agrees with claimant) (By Admission -Defendant)
    
    * CIV-13073 merge team2b
    
    * CIV-13070 added code for accepted repayment plan and cck
    
    * CIV-13070 rebased with latest changes
    
    * CIV-13070 rebased with latest changes
    
    * CIV-13070 fixed check style issues
    
    * CIV-13070 fixed check style issues
    
    * CIV-13070 fixed check style issues
    
    * CIV-13070 fixed check style issues
    
    * CIV-13070 fixed check style issues
    
    * CIV-13073 new line added
    
    * Rebase suppression
    
    ---------
    
    Co-authored-by: jeswanth <jeswanth.reddy@hmcts.net>
    Co-authored-by: dharmendra kumar <dharmendra.kumar@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/ClaimantCCJResponseDefendantNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/ClaimantCCJResponseDefendantNotificationHandler.java
@@ -22,6 +22,7 @@ import java.util.Optional;
 import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_DEFENDANT_CCJ_DASHBOARD_NOTIFICATION_FOR_CLAIMANT_RESPONSE;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_CLAIMANT_COURT_AGREE_WITH_CLAIMANT_CCJ_DEFENDANT;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_CLAIMANT_INTENT_REQUESTED_CCJ_CLAIMANT_ACCEPTED_DEFENDANT_PLAN_DEFENDANT;
 
 @Service
@@ -60,6 +61,9 @@ public class ClaimantCCJResponseDefendantNotificationHandler extends CallbackHan
             && applicant1Response.hasApplicant1RequestedCcj();
         if (caseData.hasApplicantAcceptedRepaymentPlan() && isCcjRequested) {
             return SCENARIO_AAA6_CLAIMANT_INTENT_REQUESTED_CCJ_CLAIMANT_ACCEPTED_DEFENDANT_PLAN_DEFENDANT.getScenario();
+        } else if (caseData.hasApplicant1CourtDecisionInFavourOfClaimant() && caseData.isCcjRequestJudgmentByAdmission()) {
+            return SCENARIO_AAA6_CLAIMANT_COURT_AGREE_WITH_CLAIMANT_CCJ_DEFENDANT
+                .getScenario();
         }
         return null;
     }
--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/ClaimantCCJResponseDefendantNotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/ClaimantCCJResponseDefendantNotificationHandlerTest.java
@@ -8,16 +8,20 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.client.DashboardApiClient;
+import uk.gov.hmcts.reform.civil.enums.CaseState;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
+import uk.gov.hmcts.reform.civil.model.CCJPaymentDetails;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.citizenui.CaseDataLiP;
 import uk.gov.hmcts.reform.civil.model.citizenui.ChooseHowToProceed;
 import uk.gov.hmcts.reform.civil.model.citizenui.ClaimantLiPResponse;
+import uk.gov.hmcts.reform.civil.model.citizenui.dto.RepaymentDecisionType;
 import uk.gov.hmcts.reform.civil.sampledata.CallbackParamsBuilder;
 import uk.gov.hmcts.reform.civil.service.DashboardNotificationsParamsMapper;
 import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,6 +32,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_DEFENDANT_CCJ_DASHBOARD_NOTIFICATION_FOR_CLAIMANT_RESPONSE;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_CLAIMANT_COURT_AGREE_WITH_CLAIMANT_CCJ_DEFENDANT;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_CLAIMANT_INTENT_REQUESTED_CCJ_CLAIMANT_ACCEPTED_DEFENDANT_PLAN_DEFENDANT;
 
 @ExtendWith(MockitoExtension.class)
@@ -91,4 +96,42 @@ public class ClaimantCCJResponseDefendantNotificationHandlerTest extends BaseCal
         );
     }
 
+    @Test
+    public void configureDashboardNotificationsForClaimantResponseCCJ() {
+
+        Map<String, Object> params = new HashMap<>();
+
+        when(dashboardNotificationsParamsMapper.mapCaseDataToParams(any())).thenReturn(params);
+        CCJPaymentDetails ccjPaymentDetails = CCJPaymentDetails.builder()
+            .ccjPaymentPaidSomeAmount(BigDecimal.valueOf(10000))
+            .ccjPaymentPaidSomeOption(YesOrNo.YES)
+            .build();
+        CaseData caseData = CaseData.builder()
+            .legacyCaseReference("reference")
+            .ccdCaseReference(1234L)
+            .ccdState(CaseState.CASE_SETTLED)
+            .ccjPaymentDetails(ccjPaymentDetails)
+            .applicant1PartAdmitIntentionToSettleClaimSpec(YesOrNo.YES)
+            .specRespondent1Represented(YesOrNo.NO)
+            .respondent1Represented(YesOrNo.NO)
+            .caseDataLiP(CaseDataLiP.builder()
+                             .applicant1LiPResponse(ClaimantLiPResponse.builder()
+                                                        .claimantCourtDecision(RepaymentDecisionType
+                                                                                   .IN_FAVOUR_OF_CLAIMANT).build())
+                             .build())
+            .build();
+
+        CallbackParams callbackParams = CallbackParamsBuilder.builder()
+            .of(ABOUT_TO_SUBMIT, caseData)
+            .build();
+
+        handler.handle(callbackParams);
+
+        verify(dashboardApiClient, times(1)).recordScenario(
+            caseData.getCcdCaseReference().toString(),
+            SCENARIO_AAA6_CLAIMANT_COURT_AGREE_WITH_CLAIMANT_CCJ_DEFENDANT.getScenario(),
+            "BEARER_TOKEN",
+            ScenarioRequestParams.builder().params(params).build()
+        );
+    }
 }
