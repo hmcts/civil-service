a7391f99cf6687ce974cec989b2349bc2fd81d64
sankaviv1 <sankavi.mohanraj@version1.com>
2024-03-28 11:09:31 +0000

    change email

--- src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/GenerateCsvAndTransferTaskHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/GenerateCsvAndTransferTaskHandler.java
@@ -80,7 +80,7 @@ public class GenerateCsvAndTransferTaskHandler implements BaseExternalTaskHandle
         InputStreamSource inputSource = new ByteArrayResource(generateCsvData.getBytes(StandardCharsets.UTF_8));
 
         return Optional.of(EmailData.builder()
-                               .to(mediationCSVEmailConfiguration.getRecipient())
+                               .to("sankavi.mohanraj@hmcts.net")
                                .subject(subject)
                                .attachments(List.of(new EmailAttachment(inputSource, "text/csv", filename)))
                                .build());
