1a27e156cbeb1b26e0b30f0ed9a81d1506226c94
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-04-01 13:34:36 +0200

    CIV-12812 : Fix typo error

--- src/main/resources/db/migration/V2024_03_21_1415__CIV-12812_mediation_unsuccessful_defendant_scenario.sql
+++ src/main/resources/db/migration/V2024_03_21_1415__CIV-12812_mediation_unsuccessful_defendant_scenario.sql
@@ -12,6 +12,6 @@ VALUES ('Scenario.AAA6.ClaimantIntent.MediationUnsuccessful.Defendant',
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
 VALUES ('Notice.AAA6.ClaimantIntent.MediationUnsuccessful.Defendant', 'Mediation was unsuccessful', 'Mediation was unsuccessful',
-        '<p class="govuk-body">You weren''t able to resolve ${applicant1PartyName}''s claim against you using mediation. The court will review the case. We''ll contact you to tell you what to do next. <a href={VIEW_CLAIMANT_HEARING_REQS} target="_blank" class="govuk-link">View ${applicant1PartyName}''s hearing requirements</a></p>',
-        '<p class="govuk-body">You weren''t able to resolve ${applicant1PartyName}''s claim against you using mediation. The court will review the case. We''ll contact you to tell you what to do next. <a href={VIEW_CLAIMANT_HEARING_REQS} target="_blank" class="govuk-link">View ${applicant1PartyName}''s hearing requirements</a></p>',
+        '<p class="govuk-body">You weren''t able to resolve ${applicant1PartyName}''s claim against you using mediation. The court will review the case. We''ll contact you to tell you what to do next. <a href={VIEW_CLAIMANT_HEARING_REQS} target="_blank" class="govuk-link">View ${applicant1PartyName}''s hearing requirements.</a></p>',
+        '<p class="govuk-body">You weren''t able to resolve ${applicant1PartyName}''s claim against you using mediation. The court will review the case. We''ll contact you to tell you what to do next. <a href={VIEW_CLAIMANT_HEARING_REQS} target="_blank" class="govuk-link">View ${applicant1PartyName}''s hearing requirements.</a></p>',
         'DEFENDANT');
