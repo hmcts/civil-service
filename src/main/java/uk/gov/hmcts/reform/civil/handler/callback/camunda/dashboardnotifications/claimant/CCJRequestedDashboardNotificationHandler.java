7d9b23ba88b78bee62b42d097f78d9fbad01aa33
Manish Garg <manish.garg@hmcts.net>
2024-04-04 17:53:40 +0100

    CIV-13035 updated dashboard notification for judgment by admission

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/CCJRequestedDashboardNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/CCJRequestedDashboardNotificationHandler.java
@@ -42,7 +42,8 @@ public class CCJRequestedDashboardNotificationHandler extends DashboardCallbackH
     @Override
     public String getScenario(CaseData caseData) {
         CaseDataLiP caseDataLiP = caseData.getCaseDataLiP();
-        if ((caseDataLiP != null && YesOrNo.NO == caseDataLiP.getRespondentSignSettlementAgreement()) || caseData.isSettlementAgreementDeadlineExpired() || !caseData.isJudgementDateNotPermitted()) {
+        if ((caseDataLiP != null && YesOrNo.NO == caseDataLiP.getRespondentSignSettlementAgreement()) || caseData.isSettlementAgreementDeadlineExpired()
+            || !caseData.isJudgementDateNotPermitted()) {
             return SCENARIO_AAA6_CLAIMANT_INTENT_REQUESTED_CCJ_CLAIMANT.getScenario();
         }
 
