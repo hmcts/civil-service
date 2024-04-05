fa2478d5b5ecdd76f7ce99e77eb197d22bcb1075
hmcts-version1-ignacio <107860328+hmcts-version1-ignacio@users.noreply.github.com>
2024-04-05 17:57:09 +0100

    CIV-13110 Set aside notification LiP (#4316)
    * Initial Commit
    
    * Code review fixes
    
    * CIV-13109 Initial Commit
    
    * CIV-13109 Initial Commit - Template id
    
    * CIV-13109: Few more changes
    
    * CIV-13109: Few more changes by pointing to civil-commons tag
    
    * CIV-13109: Pointing to CCD branch of CIV-13109
    
    * CIV-13109: Fix tests
    
    * CIV-13109: Fix tests
    
    * CIV-13109: Pointing master ccd branch
    
    * CIV-13109: Pointing to 1.0.65 version
    
    * CIV-13109: Pointing to old tag version
    
    * CIV-13109: Updated logic as suggested by Bhagyasree
    
    * CIV-13109: Triggering the correct Camunda event
    
    * CIV-13109: Updated reference template name for Defendant
    
    * CIV-13109: Updated Judgement to Judgment. Also updated the tag version.
    
    * changes
    
    * CIV-13109: Renamed to add prefix as NOTIFY
    
    * CIV-13109: Merge issue resolved
    
    * CIV-13109: Updated logic for NotifyClaimSetAsideJudgement logic for Defendant
    
    * CIV-13109: Fixed test
    
    * CIV-13109: Remove unused imports
    
    * CIV-13109: Added tests for NotificationUtils and removed unused method.
    
    * CIV-13109: Added suppression as a temporary fix for build
    
    * CIV-13110 set aside notification for LiP
    
    * CIV-13110 set aside notification for LiP
    
    * CIV-13110 set aside notification for LiP
    
    * CIV-13110 NotificationUtils fix
    
    * CIV-13110 remove blank line
    
    * CIV-13110 fix defendant lip check
    
    ---------
    
    Co-authored-by: ShwetaTandel-hmcts <shweta.tandel@hmcts.net>
    Co-authored-by: ShwetaTandel-hmcts <130586258+ShwetaTandel-hmcts@users.noreply.github.com>
    Co-authored-by: JamiS <santoshini.jami@hmcts.net>
    Co-authored-by: sjamihmcts <110464983+sjamihmcts@users.noreply.github.com>
    Co-authored-by: bhagyashreesharma90 <114094319+bhagyashreesharma90@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimSetAsideJudgmentClaimantNotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimSetAsideJudgmentClaimantNotificationHandlerTest.java
@@ -11,8 +11,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.enums.CaseCategory;
+import uk.gov.hmcts.reform.civil.enums.CaseState;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.Party;
 import uk.gov.hmcts.reform.civil.notify.NotificationService;
 import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
 import uk.gov.hmcts.reform.civil.prd.model.Organisation;
@@ -27,10 +31,13 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIMANT_V_DEFENDANT;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIM_REFERENCE_NUMBER;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.DEFENDANT_NAME_INTERIM;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.LEGAL_ORG_NAME;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.PARTY_NAME;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.REASON_FROM_CASEWORKER;
+import static uk.gov.hmcts.reform.civil.utils.PartyUtils.getAllPartyNames;
 
 @SpringBootTest(classes = {
     ClaimSetAsideJudgmentClaimantNotificationHandler.class,
@@ -40,6 +47,7 @@ import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.No
 public class ClaimSetAsideJudgmentClaimantNotificationHandlerTest extends BaseCallbackHandlerTest {
 
     public static final String TEMPLATE_ID = "template-id";
+    public static final String TEMPLATE_ID_LIP = "template-id-lip";
 
     @MockBean
     private NotificationService notificationService;
@@ -59,6 +67,7 @@ public class ClaimSetAsideJudgmentClaimantNotificationHandlerTest extends BaseCa
         @BeforeEach
         void setup() {
             when(notificationsProperties.getNotifySetAsideJudgmentTemplate()).thenReturn(TEMPLATE_ID);
+            when(notificationsProperties.getNotifyUpdateTemplate()).thenReturn(TEMPLATE_ID_LIP);
             when(organisationService.findOrganisationById(anyString()))
                 .thenReturn(Optional.of(Organisation.builder().name("Test Org Name").build()));
         }
@@ -87,6 +96,40 @@ public class ClaimSetAsideJudgmentClaimantNotificationHandlerTest extends BaseCa
                 "set-aside-judgment-applicant-notification-000DC001"
             );
         }
+
+        @Test
+        void shouldNotifyApplicantLipSolicitor_whenInvoked() {
+            CaseData caseData = CaseDataBuilder.builder().buildJudgmentOnlineCaseDataWithPaymentByDate().toBuilder()
+                .applicant1(Party.builder()
+                                .individualFirstName("Applicant1")
+                                .individualLastName("ApplicantLastName").partyName("Applicant1")
+                                .type(Party.Type.INDIVIDUAL).partyEmail("applicantLip@example.com").build())
+                .respondent1(Party.builder().partyName("Respondent1")
+                                 .individualFirstName("Respondent1").individualLastName("RespondentLastName")
+                                 .type(Party.Type.INDIVIDUAL).partyEmail("respondentLip@example.com").build())
+                .applicant1Represented(YesOrNo.NO)
+                .legacyCaseReference("000DC001")
+                .caseAccessCategory(CaseCategory.SPEC_CLAIM)
+                .ccdState(CaseState.All_FINAL_ORDERS_ISSUED)
+                .build();
+
+            CallbackParams params = CallbackParams.builder()
+                .caseData(caseData)
+                .type(ABOUT_TO_SUBMIT)
+                .request(CallbackRequest.builder()
+                             .eventId(CaseEvent.SET_ASIDE_JUDGMENT.name())
+                             .build())
+                .build();
+
+            handler.handle(params);
+
+            verify(notificationService, times(1)).sendMail(
+                "applicantLip@example.com",
+                TEMPLATE_ID_LIP,
+                getNotificationDataMapLip(caseData),
+                "set-aside-judgment-applicant-notification-lip-000DC001"
+            );
+        }
     }
 
     @NotNull
@@ -96,6 +139,14 @@ public class ClaimSetAsideJudgmentClaimantNotificationHandlerTest extends BaseCa
             LEGAL_ORG_NAME, "Test Org Name",
             REASON_FROM_CASEWORKER, "test error",
             DEFENDANT_NAME_INTERIM, "Mr. Sole Trader"
+        );
+    }
+
+    private Map<String, String> getNotificationDataMapLip(CaseData caseData) {
+        return Map.of(
+            CLAIM_REFERENCE_NUMBER, caseData.getLegacyCaseReference(),
+            CLAIMANT_V_DEFENDANT, getAllPartyNames(caseData),
+            PARTY_NAME, caseData.getApplicant1().getPartyName()
         );
     }
 }
