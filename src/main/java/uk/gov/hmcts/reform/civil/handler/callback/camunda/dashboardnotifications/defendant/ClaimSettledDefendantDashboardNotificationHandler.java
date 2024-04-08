1fb7dd8e4e8754237a064fb66a324950a173d3c2
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-04-08 12:02:00 +0100

    CIV-13283 changes

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/ClaimSettledDefendantDashboardNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/ClaimSettledDefendantDashboardNotificationHandler.java
@@ -1,44 +1,30 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.defendant;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
-import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
-import uk.gov.hmcts.reform.civil.callback.Callback;
-import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.callback.DashboardCallbackHandler;
 import uk.gov.hmcts.reform.civil.client.DashboardApiClient;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.DashboardNotificationsParamsMapper;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
-import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
 
 import java.util.List;
-import java.util.Map;
 
-import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
-import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_DASHBOARD_NOTIFICATION_FOR_CLAIM_SETTLED_FOR_DEFENDANT1;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_CLAIMANT_INTENT_CLAIM_SETTLE_EVENT_DEFENDANT;
 
 @Service
-@RequiredArgsConstructor
-public class ClaimSettledDefendantDashboardNotificationHandler extends CallbackHandler {
+public class ClaimSettledDefendantDashboardNotificationHandler extends DashboardCallbackHandler {
 
     private static final List<CaseEvent> EVENTS =
         List.of(CREATE_DASHBOARD_NOTIFICATION_FOR_CLAIM_SETTLED_FOR_DEFENDANT1);
     public static final String TASK_ID = "CreateClaimSettledDashboardNotificationsForDefendant1";
-    private final DashboardApiClient dashboardApiClient;
-    private final FeatureToggleService featureToggleService;
-    private final DashboardNotificationsParamsMapper mapper;
 
-    @Override
-    protected Map<String, Callback> callbacks() {
-        return featureToggleService.isDashboardServiceEnabled()
-            ? Map.of(callbackKey(ABOUT_TO_SUBMIT), this::configureScenarioForClaimSubmission)
-            : Map.of(callbackKey(ABOUT_TO_SUBMIT), this::emptyCallbackResponse
-        );
+    public ClaimSettledDefendantDashboardNotificationHandler(DashboardApiClient dashboardApiClient,
+                                          DashboardNotificationsParamsMapper mapper,
+                                          FeatureToggleService featureToggleService) {
+        super(dashboardApiClient, mapper, featureToggleService);
     }
 
     @Override
@@ -51,17 +37,8 @@ public class ClaimSettledDefendantDashboardNotificationHandler extends CallbackH
         return EVENTS;
     }
 
-    private CallbackResponse configureScenarioForClaimSubmission(CallbackParams callbackParams) {
-        CaseData caseData = callbackParams.getCaseData();
-        String authToken = callbackParams.getParams().get(BEARER_TOKEN).toString();
-
-        dashboardApiClient.recordScenario(
-            caseData.getCcdCaseReference().toString(),
-            SCENARIO_AAA6_CLAIMANT_INTENT_CLAIM_SETTLE_EVENT_DEFENDANT.getScenario(),
-            authToken,
-            ScenarioRequestParams.builder().params(mapper.mapCaseDataToParams(caseData))
-                .build()
-        );
-        return AboutToStartOrSubmitCallbackResponse.builder().build();
+    @Override
+    public String getScenario(CaseData caseData) {
+        return SCENARIO_AAA6_CLAIMANT_INTENT_CLAIM_SETTLE_EVENT_DEFENDANT.getScenario();
     }
 }
--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/ClaimantCCJResponseDefendantNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/ClaimantCCJResponseDefendantNotificationHandler.java
@@ -1,13 +1,9 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.defendant;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
-import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
-import uk.gov.hmcts.reform.civil.callback.Callback;
-import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.callback.DashboardCallbackHandler;
 import uk.gov.hmcts.reform.civil.client.DashboardApiClient;
 import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios;
 import uk.gov.hmcts.reform.civil.model.CaseData;
@@ -15,34 +11,25 @@ import uk.gov.hmcts.reform.civil.model.citizenui.CaseDataLiP;
 import uk.gov.hmcts.reform.civil.model.citizenui.ClaimantLiPResponse;
 import uk.gov.hmcts.reform.civil.service.DashboardNotificationsParamsMapper;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
-import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
-import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_DEFENDANT_CCJ_DASHBOARD_NOTIFICATION_FOR_CLAIMANT_RESPONSE;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_CLAIMANT_COURT_AGREE_WITH_CLAIMANT_CCJ_DEFENDANT;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_CLAIMANT_INTENT_REQUESTED_CCJ_CLAIMANT_ACCEPTED_DEFENDANT_PLAN_DEFENDANT;
 
 @Service
-@RequiredArgsConstructor
-public class ClaimantCCJResponseDefendantNotificationHandler extends CallbackHandler {
+public class ClaimantCCJResponseDefendantNotificationHandler extends DashboardCallbackHandler {
 
     private static final List<CaseEvent> EVENTS =
         List.of(CREATE_DEFENDANT_CCJ_DASHBOARD_NOTIFICATION_FOR_CLAIMANT_RESPONSE);
     public static final String TASK_ID = "GenerateDefendantCCJDashboardNotificationForClaimantResponse";
-    private final DashboardApiClient dashboardApiClient;
-    private final DashboardNotificationsParamsMapper mapper;
-    private final FeatureToggleService featureToggleService;
 
-    @Override
-    protected Map<String, Callback> callbacks() {
-        return featureToggleService.isDashboardServiceEnabled()
-            ? Map.of(callbackKey(ABOUT_TO_SUBMIT), this::configureScenarioForClaimantResponse)
-            : Map.of(callbackKey(ABOUT_TO_SUBMIT), this::emptyCallbackResponse);
+    public ClaimantCCJResponseDefendantNotificationHandler(DashboardApiClient dashboardApiClient,
+                                                             DashboardNotificationsParamsMapper mapper,
+                                                             FeatureToggleService featureToggleService) {
+        super(dashboardApiClient, mapper, featureToggleService);
     }
 
     @Override
@@ -55,7 +42,8 @@ public class ClaimantCCJResponseDefendantNotificationHandler extends CallbackHan
         return EVENTS;
     }
 
-    private String getScenario(CaseData caseData) {
+    @Override
+    public String getScenario(CaseData caseData) {
         ClaimantLiPResponse applicant1Response = Optional.ofNullable(caseData.getCaseDataLiP())
             .map(CaseDataLiP::getApplicant1LiPResponse)
             .orElse(null);
@@ -73,20 +61,8 @@ public class ClaimantCCJResponseDefendantNotificationHandler extends CallbackHan
         return null;
     }
 
-    private CallbackResponse configureScenarioForClaimantResponse(CallbackParams callbackParams) {
-        CaseData caseData = callbackParams.getCaseData();
-        String authToken = callbackParams.getParams().get(BEARER_TOKEN).toString();
-        String scenario = getScenario(caseData);
-        if (scenario != null && caseData.isRespondent1NotRepresented()) {
-            dashboardApiClient.recordScenario(
-                caseData.getCcdCaseReference().toString(),
-                scenario,
-                authToken,
-                ScenarioRequestParams.builder().params(mapper.mapCaseDataToParams(
-                    caseData)).build()
-            );
-        }
-
-        return AboutToStartOrSubmitCallbackResponse.builder().build();
+    @Override
+    public boolean shouldRecordScenario(CaseData caseData) {
+        return caseData.isRespondent1NotRepresented();
     }
 }
