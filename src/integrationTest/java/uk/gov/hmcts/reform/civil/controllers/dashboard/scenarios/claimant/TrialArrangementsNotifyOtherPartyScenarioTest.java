96d9a1f222222128709e8f5d5eb599357e9a80e3
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-04-09 15:59:05 +0100

    CIV-13259 adding test cases

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/TrialArrangementsNotifyOtherPartyScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/TrialArrangementsNotifyOtherPartyScenarioTest.java
@@ -9,6 +9,7 @@ import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.Party;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+import uk.gov.hmcts.reform.dashboard.data.TaskStatus;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -45,6 +46,18 @@ public class TrialArrangementsNotifyOtherPartyScenarioTest extends DashboardBase
                 jsonPath("$[0].descriptionCy")
                     .value(
                         "<p class=\"govuk-body\">You can <a href=\"{VIEW_ORDERS_AND_NOTICES_REDIRECT}\" class=\"govuk-link\">view the arrangements that they’ve confirmed.</a></p>")
+            );
+
+        doGet(BEARER_TOKEN, GET_TASKS_ITEMS_URL, caseId, "DEFENDANT")
+            .andExpectAll(
+                status().is(HttpStatus.OK.value()),
+                jsonPath("$[0].reference").value(caseId.toString()),
+                jsonPath("$[0].taskNameEn").value(
+                    "<a>Add the trial arrangements</a>"),
+                jsonPath("$[0].currentStatusEn").value(TaskStatus.DONE.getName()),
+                jsonPath("$[0].taskNameCy").value(
+                    "<a>Add the trial arrangements</a>"),
+                jsonPath("$[0].currentStatusCy").value(TaskStatus.DONE.getName())
             );
     }
 }
