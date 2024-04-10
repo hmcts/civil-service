3be2f6b6c474f78b03b74767e0721de2d7f5c8b2
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2024-04-10 16:27:30 +0100

    CIV-11304 Order made

---
+++ src/main/resources/db/migration/V2024_04_08_1221__CIV-11304-order_made_defendant.sql
@@ -0,0 +1,24 @@
+/**
+ * Add scenario
+ */
+INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
+VALUES ('Scenario.AAA6.CP.OrderMade.Defendant', '{}', '{"Notice.AAA6.CP.OrderMade.Defendant" : []}');
+
+/**
+ * Add notification template
+ */
+INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
+                                                  ,notification_role, time_to_live)
+VALUES ('Scenario.AAA6.CP.OrderMade.Defendant', 'An order has been made', 'An order has been made',
+        '<p class="govuk-body">The judge has made an order on your claim. <a href="{VIEW_FINAL_ORDER}" rel="noopener noreferrer" target="_blank" class="govuk-link">View the order</a>.</p>',
+        '<p class="govuk-body">The judge has made an order on your claim. <a href="{VIEW_FINAL_ORDER}" rel="noopener noreferrer" target="_blank" class="govuk-link">View the order</a>.</p>',
+        'DEFENDANT', 'Session');
+
+/**
+ * Add task list items
+ */
+INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
+                                    scenario_name, task_status_sequence, role, task_order)
+values ('<a href={VIEW_ORDERS_AND_NOTICES}  rel="noopener noreferrer" class="govuk-link">View orders and notices</a>', 'Orders and notices from the court' ,
+        '<a href={VIEW_ORDERS_AND_NOTICES}  rel="noopener noreferrer" class="govuk-link">View orders and notices</a>',
+        'Orders and notices from the court', 'Order.View', 'Scenario.AAA6.CP.OrderMade.Defendant', '{3, 3}', 'DEFENDANT', 10);
