e2f1798094b12f8701e0c199ea00c9c42921f8a5
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-04-11 10:54:12 +0100

    CIV-13260 adding test

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/TrialArrangementsNotifyOtherPartyDefendantScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/TrialArrangementsNotifyOtherPartyDefendantScenarioTest.java
@@ -7,7 +7,6 @@ import uk.gov.hmcts.reform.civil.controllers.DashboardBaseIntegrationTest;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.defendant.TrialArrangementsNotifyOtherPartyDefendantNotificationHandler;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.Party;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.dashboard.data.TaskStatus;
 
