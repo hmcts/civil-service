5b9e525c8b5c0c5f2994a1c0588028f3a31ca0b5
Miguel Molina <93722947+miguelMolina3691@users.noreply.github.com>
2024-04-11 18:05:19 +0200

    CIV-11300 Dashboard Notification Hearing Scheduled  (#4432)
    * CIV-11300 hearing notification
    
    * CIV-11300 split scenarios
    
    * CIV-113000 Test
    
    * CIV-11300 Test
    
    * CIV-11300 fix test
    
    * CIV-11300 update script
    
    * CIV-11300 update script
    
    * CIV-11300 modify court name
    
    * CIV-11300 lint
    
    * CIV-11300 test
    
    * CIV-11300 test
    
    * CIV-11300 update dashboard notif conf
    
    * CIV-11300 update dashboard notification
    
    * CIV-11300 update dashboard notification
    
    * CIV-11300 fix test
    
    ---------
    
    Co-authored-by: dharmendra kumar <dharmendra.kumar@hmcts.net>

---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/HearingScheduledClaimantNotificationHandlerTest.java
@@ -0,0 +1,100 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.claimant;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.client.DashboardApiClient;
+import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
+import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.claimant.HearingScheduledClaimantNotificationHandler;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.common.DynamicList;
+import uk.gov.hmcts.reform.civil.model.common.DynamicListElement;
+import uk.gov.hmcts.reform.civil.referencedata.LocationRefDataService;
+import uk.gov.hmcts.reform.civil.referencedata.model.LocationRefData;
+import uk.gov.hmcts.reform.civil.sampledata.CallbackParamsBuilder;
+import uk.gov.hmcts.reform.civil.service.DashboardNotificationsParamsMapper;
+import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
+import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_DASHBOARD_NOTIFICATION_HEARING_SCHEDULED_CLAIMANT;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_CP_HEARING_SCHEDULED_CLAIMANT;
+
+@ExtendWith(MockitoExtension.class)
+public class HearingScheduledClaimantNotificationHandlerTest extends BaseCallbackHandlerTest {
+
+    @InjectMocks
+    private HearingScheduledClaimantNotificationHandler handler;
+
+    @Mock
+    private DashboardApiClient dashboardApiClient;
+
+    @Mock
+    private DashboardNotificationsParamsMapper dashboardNotificationsParamsMapper;
+
+    @Mock
+    private FeatureToggleService featureToggleService;
+
+    @Mock
+    private LocationRefDataService locationRefDataService;
+
+    public static final String TASK_ID = "GenerateDashboardNotificationHearingScheduledClaimant";
+
+    HashMap<String, Object> params = new HashMap<>();
+
+    @Test
+    void handleEventsReturnsTheExpectedCallbackEvent() {
+        assertThat(handler.handledEvents()).contains(CREATE_DASHBOARD_NOTIFICATION_HEARING_SCHEDULED_CLAIMANT);
+    }
+
+    @Test
+    void shouldReturnCorrectCamundaActivityId_whenInvoked() {
+        assertThat(handler.camundaActivityId(
+            CallbackParamsBuilder.builder()
+                .request(CallbackRequest.builder()
+                             .eventId(CREATE_DASHBOARD_NOTIFICATION_HEARING_SCHEDULED_CLAIMANT.name())
+                             .build())
+                .build()))
+            .isEqualTo(TASK_ID);
+    }
+
+    @Test
+    public void createDashboardNotifications() {
+        List<LocationRefData> locations = new ArrayList<>();
+        locations.add(LocationRefData.builder().siteName("Name").courtAddress("Loc").postcode("1").build());
+        when(dashboardNotificationsParamsMapper.mapCaseDataToParams(any())).thenReturn(params);
+        when(featureToggleService.isDashboardServiceEnabled()).thenReturn(true);
+        when(locationRefDataService.getCourtLocationsForDefaultJudgments(any())).thenReturn(locations);
+
+        DynamicListElement location = DynamicListElement.builder().label("Name - Loc - 1").build();
+        DynamicList list = DynamicList.builder().value(location).listItems(List.of(location)).build();
+        CaseData caseData = CaseData.builder()
+            .legacyCaseReference("reference")
+            .ccdCaseReference(1234L)
+            .build().toBuilder().hearingLocation(list).build();
+
+        CallbackParams callbackParams = CallbackParamsBuilder.builder()
+            .of(ABOUT_TO_SUBMIT, caseData)
+            .build();
+
+        handler.handle(callbackParams);
+        verify(dashboardApiClient).recordScenario(
+            caseData.getCcdCaseReference().toString(),
+            SCENARIO_AAA6_CP_HEARING_SCHEDULED_CLAIMANT.getScenario(),
+            "BEARER_TOKEN",
+            ScenarioRequestParams.builder().params(params).build()
+        );
+    }
+}
