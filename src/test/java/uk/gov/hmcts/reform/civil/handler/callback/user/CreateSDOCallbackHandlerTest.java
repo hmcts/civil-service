97c397d4fe6d0a2fb59849fc0554b54657181d2e
bhagyashreesharma90 <bhagyashree.sharma@hmcts.net>
2024-04-12 17:42:56 +0100

    CIV-11475 fix tests

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateSDOCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateSDOCallbackHandlerTest.java
@@ -684,6 +684,7 @@ public class CreateSDOCallbackHandlerTest extends BaseCallbackHandlerTest {
             CallbackParams params = callbackParamsOf(CallbackVersion.V_1, caseData, ABOUT_TO_START);
 
             var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+            CaseData responseCaseData = objectMapper.convertValue(response.getData(), CaseData.class);
             if (valid) {
                 assertThat(response.getData()).extracting("sdoR2FastTrackUseOfWelshLanguage")
                     .extracting("description").isEqualTo(WELSH_LANG_DESCRIPTION);
@@ -696,11 +697,11 @@ public class CreateSDOCallbackHandlerTest extends BaseCallbackHandlerTest {
                 assertThat(response.getData()).extracting("sdoR2NihlUseOfWelshLanguage")
                     .extracting("description").isEqualTo(WELSH_LANG_DESCRIPTION);
             } else {
-                assertThat(response.getData()).extracting("sdoR2FastTrackUseOfWelshLanguage").isNull();
-                assertThat(response.getData()).extracting("sdoR2SmallClaimsUseOfWelshLanguage").isNull();
-                assertThat(response.getData()).extracting("sdoR2DisposalHearingUseOfWelshLanguage").isNull();
-                assertThat(response.getData()).extracting("sdoR2DrhUseOfWelshLanguage").isNull();
-                assertThat(response.getData()).extracting("sdoR2NihlUseOfWelshLanguage").isNull();
+                assertThat(responseCaseData.getSdoR2FastTrackUseOfWelshLanguage()).isNull();
+                assertThat(responseCaseData.getSdoR2SmallClaimsUseOfWelshLanguage()).isNull();
+                assertThat(responseCaseData.getSdoR2DisposalHearingUseOfWelshLanguage()).isNull();
+                assertThat(responseCaseData.getSdoR2DrhUseOfWelshLanguage()).isNull();
+                assertThat(responseCaseData.getSdoR2NihlUseOfWelshLanguage()).isNull();
             }
         }
     }
