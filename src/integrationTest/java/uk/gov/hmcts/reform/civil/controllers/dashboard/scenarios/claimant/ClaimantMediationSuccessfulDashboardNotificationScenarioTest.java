bb8529ea51a36f458d2256f04142b1066fccb772
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-04-15 20:08:42 +0100

    add claimant tasklist and sql

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimantMediationSuccessfulDashboardNotificationScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimantMediationSuccessfulDashboardNotificationScenarioTest.java
@@ -4,10 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import uk.gov.hmcts.reform.civil.controllers.DashboardBaseIntegrationTest;
+import uk.gov.hmcts.reform.civil.controllers.dashboard.mock.MockTaskList;
 import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.claimant.ClaimantMediationSuccessfulDashboardNotificationHandler;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+import uk.gov.hmcts.reform.dashboard.data.TaskList;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -15,25 +23,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class ClaimantMediationSuccessfulDashboardNotificationScenarioTest extends DashboardBaseIntegrationTest {
 
+
     @Autowired
     private ClaimantMediationSuccessfulDashboardNotificationHandler handler;
 
+    public static final String CLAIMANT = "CLAIMANT";
     @Test
-    void should_create_mediation_scenario_for_carm() throws Exception {
-
+    void should_create_mediation_scenario_for_carm_claimant() throws Exception {
+        //given
         String caseId = String.valueOf(System.currentTimeMillis());
         CaseData caseData = CaseDataBuilder.builder().atStateTrialReadyCheck().build()
             .toBuilder()
             .legacyCaseReference("reference")
             .ccdCaseReference(Long.valueOf(caseId))
             .build();
-
+        List<TaskList> taskListExpected = MockTaskList.getMediationTaskListMock(CLAIMANT, caseId);
         when(featureToggleService.isCarmEnabledForCase(any())).thenReturn(true);
 
+        //When
         handler.handle(callbackParams(caseData));
 
-        //Verify Notification is created
-        doGet(BEARER_TOKEN, GET_NOTIFICATIONS_URL, caseId, "CLAIMANT")
+        //then
+        // Verify Notification is created
+        doGet(BEARER_TOKEN, GET_NOTIFICATIONS_URL, caseId, CLAIMANT)
             .andExpect(status().isOk())
             .andExpectAll(
                 status().is(HttpStatus.OK.value()),
@@ -52,6 +64,13 @@ public class ClaimantMediationSuccessfulDashboardNotificationScenarioTest extend
                             + "<p class=\"govuk-body\">You can view your mediation agreement <a href=\"{MEDIATION_SUCCESSFUL_URL}\" "
                             + "rel=\"noopener noreferrer\" class=\"govuk-link\" target=\"_blank\">here</a>.</p>")
             );
+        //Verify dashboard information
+        String result = doGet(BEARER_TOKEN, GET_TASKS_ITEMS_URL, caseId, CLAIMANT)
+            .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+
+        List<TaskList> response = Arrays.asList(objectMapper.readValue(result, TaskList[].class));
+        assertThat(response.size()).isEqualTo(taskListExpected.size());
+        evaluateTasklist(response, taskListExpected);
     }
 
     @Test
@@ -69,7 +88,7 @@ public class ClaimantMediationSuccessfulDashboardNotificationScenarioTest extend
         handler.handle(callbackParams(caseData));
 
         //Verify Notification is created
-        doGet(BEARER_TOKEN, GET_NOTIFICATIONS_URL, caseId, "CLAIMANT")
+        doGet(BEARER_TOKEN, GET_NOTIFICATIONS_URL, caseId, CLAIMANT)
             .andExpect(status().isOk())
             .andExpectAll(
                 status().is(HttpStatus.OK.value()),
@@ -82,5 +101,9 @@ public class ClaimantMediationSuccessfulDashboardNotificationScenarioTest extend
                     .value(
                         "<p class=\"govuk-body\">You made an agreement which means the claim is now ended and sets out the terms of how Mr. Sole Trader must repay you.</p><p class=\"govuk-body\"><a href=\"{MEDIATION_SUCCESSFUL_URL}\" rel=\"noopener noreferrer\" class=\"govuk-link\" target=\"_blank\">Download the agreement (PDF).</a></p>")
             );
+    }
+    private static void evaluateTasklist(List<TaskList> response, List<TaskList> taskListExpected) {
+        assertThat(response).usingRecursiveFieldByFieldElementComparatorIgnoringFields("id", "createdAt", "updatedAt", "messageParams").isEqualTo(
+            taskListExpected);
     }
 }
