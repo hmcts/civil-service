5f9a7e5d699b798dc6eb7166a8a825e862363094
Mike Lemos <gobii@me.com>
2024-04-15 12:26:19 +0100

    DTSCCI-17 fixing the retry process

--- src/main/java/uk/gov/hmcts/reform/civil/exceptions/CompleteTaskException.java
+++ src/main/java/uk/gov/hmcts/reform/civil/exceptions/CompleteTaskException.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.civil.exceptions;
 
-public class CompleteTaskException extends RuntimeException {
+public class CompleteTaskException extends Throwable {
 
     public CompleteTaskException(Throwable cause) {
         super(cause);
