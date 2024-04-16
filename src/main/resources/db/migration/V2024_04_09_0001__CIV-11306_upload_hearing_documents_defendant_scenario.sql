50445401bd66f8c7890c159c0a4e77eda8ddc751
TortolaNavarroD <dario.tortolanavarro@version1.com>
2024-04-16 08:34:07 +0100

    CIV-11305 and 11306 fixed: anchor, notification title

--- src/main/resources/db/migration/V2024_04_09_0001__CIV-11306_upload_hearing_documents_defendant_scenario.sql
+++ src/main/resources/db/migration/V2024_04_09_0001__CIV-11306_upload_hearing_documents_defendant_scenario.sql
@@ -11,7 +11,7 @@ VALUES ('Scenario.AAA6.CP.HearingDocuments.Upload.Defendant',
  */
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
-VALUES ('Notice.AAA6.CP.HearingDocuments.Upload.Defendant', 'An order has been made', 'An order has been made',
+VALUES ('Notice.AAA6.CP.HearingDocuments.Upload.Defendant', 'Upload documents', 'Upload documents',
         '<p class="govuk-body">You can <a href="{UPLOAD_HEARING_DOCUMENTS}" class="govuk-link">upload and submit documents.</a> to support your defence. Follow the instructions set out in the directions order. You must submit all documents by ${sdoDocumentUploadRequestedDate}. Any documents submitted after the deadline may not be considered by the judge.</p>',
         '<p class="govuk-body">You can <a href="{UPLOAD_HEARING_DOCUMENTS}" class="govuk-link">upload and submit documents.</a> to support your defence. Follow the instructions set out in the directions order. You must submit all documents by ${sdoDocumentUploadRequestedDate}. Any documents submitted after the deadline may not be considered by the judge.</p>',
         'DEFENDANT');
