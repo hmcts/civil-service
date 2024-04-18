68b4dda8f81ff600619e0051effb12247759d662
jeswanth <jeswanth.reddy@hmcts.net>
2024-04-18 16:15:46 +0100

    CIV-13444 content changes

--- src/main/resources/db/migration/V2024_04_02_1429__CIV-13035-claimant-requested-judgment-by-admission.sql
+++ src/main/resources/db/migration/V2024_04_02_1429__CIV-13035-claimant-requested-judgment-by-admission.sql
@@ -16,6 +16,6 @@ VALUES ('Scenario.AAA6.ClaimantIntent.RequestedCCJ.Claimant','{"Notice.AAA6.DefR
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
 VALUES ('Notice.AAA6.ClaimantIntent.RequestedCCJ.Claimant', 'You requested a County Court Judgment against ${respondent1PartyName}', 'You requested a County Court Judgment against ${respondent1PartyName}',
-        '<p class="govuk-body">You ${claimantRepaymentPlanDecision} the <a href="{VIEW_CCJ_REPAYMENT_PLAN_CLAIMANT}" class="govuk-link">repayment plan.</a></p><p class="govuk-body">When we''ve processed the request, we''ll post a copy of the judgment to you.</p><p class="govuk-body"></p>',
-        '<p class="govuk-body">You ${claimantRepaymentPlanDecision} the <a href="{VIEW_CCJ_REPAYMENT_PLAN_CLAIMANT}" class="govuk-link">repayment plan.</a></p><p class="govuk-body">When we''ve processed the request, we''ll post a copy of the judgment to you.</p><p class="govuk-body"></p>',
+        '<p class="govuk-body">You ${claimantRepaymentPlanDecision} the <a href="{VIEW_CCJ_REPAYMENT_PLAN_CLAIMANT}" class="govuk-link">repayment plan.</a></p><p class="govuk-body">When we''ve processed the request, we''ll post a copy of the judgment to you.</p>',
+        '<p class="govuk-body">You ${claimantRepaymentPlanDecision} the <a href="{VIEW_CCJ_REPAYMENT_PLAN_CLAIMANT}" class="govuk-link">repayment plan.</a></p><p class="govuk-body">When we''ve processed the request, we''ll post a copy of the judgment to you.</p>',
         'CLAIMANT');
