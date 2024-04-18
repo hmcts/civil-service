2b5fa7f944c891112b4fb306b3f6e6863ecc5821
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-04-18 10:22:13 +0100

    CIV-11355 updating hint text to null once the status is done

--- src/main/resources/db/migration/V2024_04_02_1500__CIV-11302_trial_arrangements_notify_other_party_claimant_scenario.sql
+++ src/main/resources/db/migration/V2024_04_02_1500__CIV-11302_trial_arrangements_notify_other_party_claimant_scenario.sql
@@ -18,6 +18,6 @@ VALUES ('Notice.AAA6.CP.Trial.Arrangements.Finalised.NotifyOtherParty.Claimant',
         'CLAIMANT','Session');
 
 INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
-                                    scenario_name, task_status_sequence, role, task_order)
+                                    scenario_name, task_status_sequence, role, task_order, hint_text_en, hint_text_cy)
 values ('<a>Add the trial arrangements</a>', 'Hearings' ,'<a>Add the trial arrangements</a>',
-        'Hearings', 'Hearing.Arrangements.Add', 'Scenario.AAA6.CP.Trial.Arrangements.Finalised.NotifyOtherParty.Claimant', '{7, 7}', 'DEFENDANT', 7);
+        'Hearings', 'Hearing.Arrangements.Add', 'Scenario.AAA6.CP.Trial.Arrangements.Finalised.NotifyOtherParty.Claimant', '{7, 7}', 'DEFENDANT', 7, null, null);
