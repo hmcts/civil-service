ac8d2e1da05923490f78b49ebbb9b9d13e87f26f
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2024-04-22 21:03:40 +0100

    Revert Accidental changes

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/InitiateGeneralApplicationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/InitiateGeneralApplicationHandler.java
@@ -98,6 +98,13 @@ public class InitiateGeneralApplicationHandler extends CallbackHandler {
         String authToken = callbackParams.getParams().get(BEARER_TOKEN).toString();
         List<String> errors = new ArrayList<>();
         CaseData caseData = callbackParams.getCaseData();
+        if (featureToggleService.isEarlyAdoptersEnabled()
+            && (Objects.isNull(caseData.getCaseManagementLocation())
+                || !(featureToggleService.isLocationWhiteListedForCaseProgression(caseData.getCaseManagementLocation()
+                                                                                  .getBaseLocation()))
+                )) {
+            errors.add(NOT_IN_EA_REGION);
+        }
 
         if (!initiateGeneralApplicationService.respondentAssigned(caseData, authToken)) {
             errors.add(RESP_NOT_ASSIGNED_ERROR);
