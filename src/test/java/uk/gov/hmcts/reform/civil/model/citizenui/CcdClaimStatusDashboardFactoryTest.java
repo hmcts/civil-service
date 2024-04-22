6eed7c84d354e8a5ec7ef5af6ba5b383ab7d27c1
paul-pearson <paul.pearson@version1.com>
2024-04-22 14:14:39 +0100

    CIV-9120 Fix test

--- src/test/java/uk/gov/hmcts/reform/civil/model/citizenui/CcdClaimStatusDashboardFactoryTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/model/citizenui/CcdClaimStatusDashboardFactoryTest.java
@@ -170,11 +170,9 @@ class CcdClaimStatusDashboardFactoryTest {
     }
 
     @Test
-    void given_defendantPayedInFull_whenGetStatus_thenReturnSettled() {
+    void given_caseStateSettled_whenGetStatus_thenReturnSettledClaimant() {
         CaseData claim = CaseData.builder()
-            .respondent1ResponseDeadline(LocalDate.now().plusDays(10).atTime(16, 0, 0))
-            .respondent1ResponseDate(LocalDateTime.now())
-            .respondent1ClaimResponsePaymentAdmissionForSpec(RespondentResponseTypeSpecPaidStatus.PAID_FULL_OR_MORE_THAN_CLAIMED_AMOUNT)
+            .ccdState(CaseState.CASE_SETTLED)
             .build();
 
         DashboardClaimStatus status = ccdClaimStatusDashboardFactory.getDashboardClaimStatus(new CcdDashboardDefendantClaimMatcher(
@@ -183,11 +181,9 @@ class CcdClaimStatusDashboardFactoryTest {
     }
 
     @Test
-    void given_claimantAcceptedDefendantResponse_whenGetStatus_thenReturnSettled() {
+    void given_caseStateSettled_whenGetStatus_thenReturnSettledDefendant() {
         CaseData claim = CaseData.builder()
-            .respondent1ResponseDeadline(LocalDate.now().plusDays(10).atTime(16, 0, 0))
-            .respondent1ResponseDate(LocalDateTime.now())
-            .applicant1AcceptAdmitAmountPaidSpec(YesOrNo.YES)
+            .ccdState(CaseState.CASE_SETTLED)
             .build();
 
         DashboardClaimStatus status = ccdClaimStatusDashboardFactory.getDashboardClaimStatus(new CcdDashboardDefendantClaimMatcher(
