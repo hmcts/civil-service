a492a9f7f545660d8b0ec262f113d88417ecca3b
neeta-hmcts <neeta.shinde@hmcts.net>
2024-04-24 12:27:51 +0100

    fixed checkstyle errors.

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimIssueHwfInfoRequiredScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimIssueHwfInfoRequiredScenarioTest.java
@@ -41,7 +41,8 @@ public class ClaimIssueHwfInfoRequiredScenarioTest extends BaseIntegrationTest {
             jsonPath("$[0].titleCy").value("Mae angen i chi ddarparu mwy o wybodaeth am eich cais am help i dalu ffioedd"),
             jsonPath("$[0].descriptionCy").value(
                 "<p class=\"govuk-body\">Mae arnom angen mwy o wybodaeth am eich cais am help i dalu ffi’r hawliad.<br>Anfonwyd e-bost atoch gyda mwy o fanylion." +
-                    " Os ydych eisoes wedi darllen yr e-bost ac wedi gweithredu, gallwch anwybyddu'r neges hon.<br>Gallwch dalu dros y ffôn drwy ffonio {civilMoneyClaimsTelephone}.</p>")
+                    " Os ydych eisoes wedi darllen yr e-bost ac wedi gweithredu, gallwch anwybyddu'r neges hon.<br>" +
+                    "Gallwch dalu dros y ffôn drwy ffonio {civilMoneyClaimsTelephone}.</p>")
 
         );
     }
--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimIssueHwfNumUpdatedScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimIssueHwfNumUpdatedScenarioTest.java
@@ -38,9 +38,11 @@ public class ClaimIssueHwfNumUpdatedScenarioTest extends BaseIntegrationTest {
                         status().is(HttpStatus.OK.value()),
                         jsonPath("$[0].titleEn").value("Your help with fees application has been updated"),
                         jsonPath("$[0].descriptionEn")
-                                .value("<p class=\"govuk-body\">You've applied for help with the claim fee. You'll receive an update from us within 5 to 10 working days.</p>"),
+                                .value("<p class=\"govuk-body\">You've applied for help with the claim fee. " +
+                                           "You'll receive an update from us within 5 to 10 working days.</p>"),
                         jsonPath("$[0].titleCy").value("Mae eich cais am help i dalu ffioedd wedi cael ei ddiweddaru"),
                         jsonPath("$[0].descriptionCy")
-                                .value("<p class=\"govuk-body\">Fe wnaethoch gais am help i dalu ffi’r hawliad. Byddwch yn cael diweddariad gennym mewn 5 i 10 diwrnod gwaith.</p>"));
+                                .value("<p class=\"govuk-body\">Fe wnaethoch gais am help i dalu ffi’r hawliad." +
+                                           " Byddwch yn cael diweddariad gennym mewn 5 i 10 diwrnod gwaith.</p>"));
     }
 }
