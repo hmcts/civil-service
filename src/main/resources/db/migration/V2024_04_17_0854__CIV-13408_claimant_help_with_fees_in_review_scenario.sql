e1ed58ca0272c178dc016612e30ebfc057c5fb11
kannan-v-hmcts <148557022+kannan-v-hmcts@users.noreply.github.com>
2024-04-25 20:44:07 +0100

    Update V2024_04_17_0854__CIV-13408_claimant_help_with_fees_in_review_scenario.sql
    removing space

--- src/main/resources/db/migration/V2024_04_17_0854__CIV-13408_claimant_help_with_fees_in_review_scenario.sql
+++ src/main/resources/db/migration/V2024_04_17_0854__CIV-13408_claimant_help_with_fees_in_review_scenario.sql
@@ -23,7 +23,7 @@ VALUES ('Notice.AAA6.CP.HearingFee.HWF.AppliedFor.Claimant',
 INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy,
                                     template_name, scenario_name, task_status_sequence, role, task_order,
                                     hint_text_en, hint_text_cy)
-values ('<a>Pay the hearing fee</a>', 'Hearing' ,
+values ('<a>Pay the hearing fee</a>', 'Hearing',
         '<a>Pay the hearing fee</a>', 'Hearing',
         'Hearing.Fee.Pay', 'Scenario.AAA6.CP.HearingFee.HWF.AppliedFor.Claimant',
         '{6, 6}', 'CLAIMANT', 11,
