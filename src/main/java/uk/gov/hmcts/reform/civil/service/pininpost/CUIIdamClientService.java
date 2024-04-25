e53e831037a501cc34f0fe65207037aefc502161
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2024-04-25 11:37:28 +0100

    DTSCCI-220: Remove RetryablePinException

--- src/main/java/uk/gov/hmcts/reform/civil/service/pininpost/CUIIdamClientService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/pininpost/CUIIdamClientService.java
@@ -1,18 +1,15 @@
 package uk.gov.hmcts.reform.civil.service.pininpost;
 
-import feign.Response;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
-import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.config.CMCPinVerifyConfiguration;
-import uk.gov.hmcts.reform.civil.exceptions.RetryablePinException;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+
+import feign.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
@@ -27,20 +24,14 @@ public class CUIIdamClientService {
         this.cmcPinVerifyConfiguration = cmcPinVerifyConfiguration;
     }
 
-    @Retryable(value = RetryablePinException.class, backoff = @Backoff(delay = 500))
     public Response authenticatePinUser(String pin, String state) {
         final String encodedRedirectUrl = URLEncoder.encode(cmcPinVerifyConfiguration.getRedirectUrl() + "/receiver", StandardCharsets.UTF_8);
         log.info("Redirect URL: " + encodedRedirectUrl);
-
-        Response response = idamApi.authenticatePinUser(
+        return idamApi.authenticatePinUser(
             pin,
             cmcPinVerifyConfiguration.getClientId(),
             encodedRedirectUrl,
             state
         );
-        if (response != null && response.status() == HttpStatus.UNAUTHORIZED.value()) {
-            throw new RetryablePinException();
-        }
-        return response;
     }
 }
