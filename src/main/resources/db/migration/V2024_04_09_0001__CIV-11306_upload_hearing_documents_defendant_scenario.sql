8833ee9a0cf586d5ae3b5c295009a56aadaa6fb7
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-04-25 17:51:31 +0100

    CIV-11306 fixing link color issue

--- src/main/resources/db/migration/V2024_04_09_0001__CIV-11306_upload_hearing_documents_defendant_scenario.sql
+++ src/main/resources/db/migration/V2024_04_09_0001__CIV-11306_upload_hearing_documents_defendant_scenario.sql
@@ -12,8 +12,8 @@ VALUES ('Scenario.AAA6.CP.HearingDocuments.Upload.Defendant',
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
 VALUES ('Notice.AAA6.CP.HearingDocuments.Upload.Defendant', 'Upload documents', 'Upload documents',
-        '<p class="govuk-body">You can <a href="{UPLOAD_HEARING_DOCUMENTS}" class="govuk-link">upload and submit documents.</a> to support your defence. Follow the instructions set out in the directions order. You must submit all documents by ${sdoDocumentUploadRequestedDateEn}. Any documents submitted after the deadline may not be considered by the judge.</p>',
-        '<p class="govuk-body">You can <a href="{UPLOAD_HEARING_DOCUMENTS}" class="govuk-link">upload and submit documents.</a> to support your defence. Follow the instructions set out in the directions order. You must submit all documents by ${sdoDocumentUploadRequestedDateCy}. Any documents submitted after the deadline may not be considered by the judge.</p>',
+        '<p class="govuk-body">You can <a href="{UPLOAD_HEARING_DOCUMENTS}" class="govuk-link">upload and submit documents</a> to support your defence. Follow the instructions set out in the directions order. You must submit all documents by ${sdoDocumentUploadRequestedDateEn}. Any documents submitted after the deadline may not be considered by the judge.</p>',
+        '<p class="govuk-body">You can <a href="{UPLOAD_HEARING_DOCUMENTS}" class="govuk-link">upload and submit documents</a> to support your defence. Follow the instructions set out in the directions order. You must submit all documents by ${sdoDocumentUploadRequestedDateCy}. Any documents submitted after the deadline may not be considered by the judge.</p>',
         'DEFENDANT');
 
 /**
@@ -21,8 +21,8 @@ VALUES ('Notice.AAA6.CP.HearingDocuments.Upload.Defendant', 'Upload documents', 
  */
 INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
                                     scenario_name, task_status_sequence, role, task_order, hint_text_en, hint_text_cy)
-values ('<a href="{UPLOAD_HEARING_DOCUMENTS}">Upload hearing documents</a>', 'Hearing',
-        '<a href="{UPLOAD_HEARING_DOCUMENTS}">Upload hearing documents</a>', 'Hearing',
+values ('<a href="{UPLOAD_HEARING_DOCUMENTS} class="govuk-link"">Upload hearing documents</a>', 'Hearing',
+        '<a href="{UPLOAD_HEARING_DOCUMENTS} class="govuk-link"">Upload hearing documents</a>', 'Hearing',
         'Hearing.Document.Upload', 'Scenario.AAA6.CP.HearingDocuments.Upload.Defendant',
         '{5, 5}', 'DEFENDANT', 2,
         'Deadline is 4pm on ${sdoDocumentUploadRequestedDateEn}',
