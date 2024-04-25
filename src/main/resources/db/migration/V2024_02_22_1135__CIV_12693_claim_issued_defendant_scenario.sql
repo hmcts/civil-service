60a5c668fb7dfb5f125fab4c4b16defd6acaba02
Felipe <36081861+GorilaNaranja@users.noreply.github.com>
2024-04-25 19:03:35 +0200

    CIV-13374 add welsh translations (#4593)
    Co-authored-by: jarekPierchala <118526007+jarekPierchala@users.noreply.github.com>
    Co-authored-by: dharmendra kumar <dharmendra.kumar@hmcts.net>

--- src/main/resources/db/migration/V2024_02_22_1135__CIV_12693_claim_issued_defendant_scenario.sql
+++ src/main/resources/db/migration/V2024_02_22_1135__CIV_12693_claim_issued_defendant_scenario.sql
@@ -9,9 +9,9 @@ VALUES ('Scenario.AAA6.ClaimIssue.Response.Required', '{}', '{"Notice.AAA6.Claim
  */
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
-VALUES ('Notice.AAA6.ClaimIssue.Response.Required', 'You haven´t responded to the claim', 'You haven´t responded to the claim',
+VALUES ('Notice.AAA6.ClaimIssue.Response.Required', 'You haven´t responded to the claim', 'Nid ydych wedi ymateb i''r hawliad',
         '<p class="govuk-body">You need to respond before ${defaultRespondTime} on ${respondent1ResponseDeadlineEn}. There are {daysLeftToRespond} days remaining. <a href="{RESPONSE_TASK_LIST_URL}"  rel="noopener noreferrer" class="govuk-link">Respond to the claim</a>.</p>',
-        '<p class="govuk-body">You need to respond before ${defaultRespondTime} on ${respondent1ResponseDeadlineCy}. There are {daysLeftToRespond} days remaining. <a href="{RESPONSE_TASK_LIST_URL}"  rel="noopener noreferrer" class="govuk-link">Respond to the claim</a>.</p>',
+        '<p class="govuk-body">Mae angen i chi ymateb cyn ${defaultRespondTime} ar ${respondent1ResponseDeadlineCy}. Mae yna {daysLeftToRespond} diwrnod yn weddill. <a href="{RESPONSE_TASK_LIST_URL}"  rel="noopener noreferrer" class="govuk-link">Ymateb i''r hawliad</a>.</p>',
         'DEFENDANT');
 
 /**
