08683a32f958ef9a5d244446a7172f5fd7d8b736
TortolaNavarroD <dario.tortolanavarro@version1.com>
2024-04-25 13:28:10 +0100

    CIV-11306 hint text added

--- src/main/resources/db/migration/V2024_04_09_0001__CIV-11306_upload_hearing_documents_defendant_scenario.sql
+++ src/main/resources/db/migration/V2024_04_09_0001__CIV-11306_upload_hearing_documents_defendant_scenario.sql
@@ -20,10 +20,16 @@ VALUES ('Notice.AAA6.CP.HearingDocuments.Upload.Defendant', 'Upload documents', 
  * Add task list items
  */
 INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
-                                    scenario_name, task_status_sequence, role, task_order)
-values ('<a href="{UPLOAD_HEARING_DOCUMENTS}">Upload hearing documents</a>', 'Hearings' ,'<a>Upload hearing documents</a>', 'Hearings',
+                                    scenario_name, task_status_sequence, role, task_order, hint_text_en, hint_text_cy)
+values ('<a href="{UPLOAD_HEARING_DOCUMENTS}">Upload hearing documents</a>', 'Hearings',
+        '<a href="{UPLOAD_HEARING_DOCUMENTS}">Upload hearing documents</a>', 'Hearings',
         'Hearing.Document.Upload', 'Scenario.AAA6.CP.HearingDocuments.Upload.Defendant',
-        '{5, 5}', 'DEFENDANT', 2),
-       ('<a>View documents</a>', 'Hearings' ,'<a>View documents</a>', 'Hearings',
-  'Hearing.Document.View', 'Scenario.AAA6.CP.HearingDocuments.Upload.Defendant',
-  '{1, 1}', 'DEFENDANT', 3);
+        '{5, 5}', 'DEFENDANT', 2,
+        'Deadline is 4pm on ${sdoDocumentUploadRequestedDateEn}',
+        'Deadline is 4pm on ${sdoDocumentUploadRequestedDateCy}');
+
+INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
+                                    scenario_name, task_status_sequence, role, task_order)
+values ('<a>View documents</a>', 'Hearings' ,'<a>View documents</a>', 'Hearings',
+        'Hearing.Document.View', 'Scenario.AAA6.CP.HearingDocuments.Upload.Defendant',
+        '{1, 1}', 'DEFENDANT', 3);
