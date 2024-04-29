c8c9d7b3d702482323a2935f7f552934177fd839
GermanAmoros <German.Amoros@HMCTS.NET>
2024-04-29 15:36:09 +0200

    CIV-13272 Fix checkstyle

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/caseevents/RequestJudgementByAdmissionNonDivergentSpecCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/caseevents/RequestJudgementByAdmissionNonDivergentSpecCallbackHandlerTest.java
@@ -18,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
     JacksonAutoConfiguration.class
 })
 public class RequestJudgementByAdmissionNonDivergentSpecCallbackHandlerTest extends BaseCallbackHandlerTest {
+
     @Autowired
     RequestJudgementByAdmissionNonDivergentSpecCallbackHandler handler;
 
