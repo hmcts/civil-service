56dbbe31400867af0d4dc44f655e3eec877f8ff9
EllisD-B <ellis.domengerboyce@hmcts.net>
2024-05-01 12:24:22 +0100

    Checkstyle

--- src/main/java/uk/gov/hmcts/reform/civil/exceptions/RetryableStitchingException.java
+++ src/main/java/uk/gov/hmcts/reform/civil/exceptions/RetryableStitchingException.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.civil.exceptions;
 
 public class RetryableStitchingException extends RuntimeException {
+
     private static final String ERROR_MESSAGE = "Stitching failed, retrying...";
 
     public RetryableStitchingException() {
