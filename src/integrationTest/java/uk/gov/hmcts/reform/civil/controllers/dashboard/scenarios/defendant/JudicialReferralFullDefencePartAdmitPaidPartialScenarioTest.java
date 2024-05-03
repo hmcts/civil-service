f1c5e4667567d80b66d99acc785c67945ce66828
jeswanth <jeswanth.reddy@hmcts.net>
2024-05-03 17:59:23 +0100

    CIV-13448 added suggested content changes

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/JudicialReferralFullDefencePartAdmitPaidPartialScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/JudicialReferralFullDefencePartAdmitPaidPartialScenarioTest.java
@@ -48,7 +48,7 @@ public class JudicialReferralFullDefencePartAdmitPaidPartialScenarioTest extends
                 jsonPath("$[0].titleEn").value("Wait for the court to review the case"),
                 jsonPath("$[0].titleCy").value("Wait for the court to review the case"),
                 jsonPath("$[0].descriptionEn")
-                    .value("<p class=\"govuk-body\">Mr Claim Claimant wants to proceed with the claim.</p>"
+                    .value("<p class=\"govuk-body\">Mr. John Rambo wants to proceed with the claim.</p>"
                                + "<p class=\"govuk-body\">They rejected your admission of £3000 although they accept you have already paid it.</p>"
                                +
                                "<p class=\"govuk-body\">The case will be referred to a judge who will decide what should happen next.</p>"
@@ -57,7 +57,7 @@ public class JudicialReferralFullDefencePartAdmitPaidPartialScenarioTest extends
                                + "<a target=\"_blank\" href=\"{VIEW_CLAIMANT_HEARING_REQS}\"  rel=\"noopener noreferrer\" "
                                + "class=\"govuk-link\">view the claimant's hearing requirements</a>.</p>"),
                 jsonPath("$[0].descriptionCy")
-                    .value("<p class=\"govuk-body\">Mr Claim Claimant wants to proceed with the claim.</p>"
+                    .value("<p class=\"govuk-body\">Mr. John Rambo wants to proceed with the claim.</p>"
                                + "<p class=\"govuk-body\">They rejected your admission of £3000 although they accept you have already paid it.</p>"
                                +
                                "<p class=\"govuk-body\">The case will be referred to a judge who will decide what should happen next.</p>"
