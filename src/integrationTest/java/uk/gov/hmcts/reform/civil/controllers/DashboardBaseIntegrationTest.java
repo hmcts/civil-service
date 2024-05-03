8b5845a920ed490caf7fd7246f65962639a99a22
Ruban <ruban72@hotmail.com>
2024-05-03 16:26:13 +0100

    DTSCCI-202: Tidy up PR for review

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/DashboardBaseIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/DashboardBaseIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
@@ -29,6 +30,7 @@ import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 @Testcontainers
 @RunWith(SpringRunner.class)
 @EnableFeignClients(defaultConfiguration = DashboardBaseIntegrationTest.MockMvcFeignConfiguration.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class DashboardBaseIntegrationTest extends BaseIntegrationTest {
 
     @MockBean
