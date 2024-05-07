b2b0842dc6b963d5fb38601876edfd1e83b33115
sankaviv1 <sankavi.mohanraj@version1.com>
2024-05-07 16:55:14 +0100

    revert email changes

--- src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/GenerateMediationJsonAndTransferTaskHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/GenerateMediationJsonAndTransferTaskHandler.java
@@ -80,7 +80,7 @@ public class GenerateMediationJsonAndTransferTaskHandler implements BaseExternal
 
     private Optional<EmailData> prepareEmail(MediationDTO mediationDTO) {
         return Optional.of(EmailData.builder()
-                               .to("madhan.mahadevan@hmcts.net")
+                               .to(mediationCSVEmailConfiguration.getJsonRecipient())
                                .subject(subject)
                                .attachments(of(json(mediationDTO.getJsonRawData(), filename)))
                                .build());
