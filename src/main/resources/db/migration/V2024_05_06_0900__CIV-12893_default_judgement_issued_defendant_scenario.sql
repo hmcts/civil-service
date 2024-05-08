5fc549ad599f11416619d38d80982c2b252ff807
GermanAmoros <German.Amoros@HMCTS.NET>
2024-05-08 12:40:52 +0200

    CIV-12893 Added task items

--- src/main/resources/db/migration/V2024_05_06_0900__CIV-12893_default_judgement_issued_defendant_scenario.sql
+++ src/main/resources/db/migration/V2024_05_06_0900__CIV-12893_default_judgement_issued_defendant_scenario.sql
@@ -17,3 +17,30 @@ VALUES ('Notice.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant',
         '<p class="govuk-body">The exact details of what you need to pay, and by when, are stated on the judgment. <br> If you want to dispute the judgment, or ask to change how and when you pay back the claim amount, you can <a href="{VIEW_AND_RESPOND}" class="govuk-link">make an application to set aside (remove) or vary the judgment</a>.</p>',
         '<p class="govuk-body">The exact details of what you need to pay, and by when, are stated on the judgment. <br> If you want to dispute the judgment, or ask to change how and when you pay back the claim amount, you can <a href="{VIEW_AND_RESPOND}" class="govuk-link">make an application to set aside (remove) or vary the judgment</a>.</p>',
         'DEFENDANT');
+
+/**
+ * Add task list items claimant
+ */
+INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
+                                    scenario_name, task_status_sequence, role, task_order)
+VALUES ('<a>View the response to the claim</a>', 'The response',
+        '<a>View the response to the claim</a>', 'The response',
+        'Response.View', 'Scenario.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant', '{2, 2}', 'DEFENDANT', 3),
+       ('<a>View hearings</a>', 'Hearing',
+        '<a>View hearings</a>', 'Hearing',
+        'Hearing.View', 'Scenario.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant', '{2, 2}', 'DEFENDANT', 5),
+       ('<a>Upload hearing documents</a>', 'Hearing',
+        '<a>Upload hearing documents</a>', 'Hearing',
+        'Hearing.Document.Upload', 'Scenario.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant', '{2, 2}', 'DEFENDANT', 6),
+       ('<a>View documents</a>', 'Hearing',
+        '<a>View documents</a>', 'Hearing',
+        'Hearing.Document.View', 'Scenario.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant', '{2, 2}', 'DEFENDANT', 7),
+       ('<a>Add the trial arrangements</a>', 'Hearing',
+        '<a>Add the trial arrangements</a>', 'Hearing',
+        'Hearing.Arrangements.Add', 'Scenario.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant', '{2, 2}', 'DEFENDANT', 8),
+       ('<a>View the bundle</a>', 'Hearing',
+        '<a>View the bundle</a>', 'Hearing',
+        'Hearing.Bundle.View', 'Scenario.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant', '{2, 2}', 'DEFENDANT', 10),
+       ('<a>View the judgment</a>', 'Judgments from the court',
+        '<a>View the judgment</a>', 'Judgments from the court',
+        'Judgment.View', 'Scenario.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant', '{3, 3}', 'DEFENDANT', 12);
