2b27ff3ed0eac4d4848700645e7af127ee88a44b
GermanAmoros <German.Amoros@HMCTS.NET>
2024-05-09 12:24:11 +0200

    CIV-12893 Fix notification flag

--- src/main/resources/db/migration/V2024_05_06_0900__CIV-12893_default_judgement_issued_defendant_scenario.sql
+++ src/main/resources/db/migration/V2024_05_06_0900__CIV-12893_default_judgement_issued_defendant_scenario.sql
@@ -4,7 +4,7 @@
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
 VALUES ('Scenario.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant',
         '{"Notice.AAA6.ClaimantIntent.CCJ.Requested.Defendant"}',
-        '{"Notice.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant": []}');
+        '{"Notice.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant": ["djDefendantNotificationMessage"]}');
 
 /**
  * Add notification template
@@ -14,8 +14,8 @@ INSERT INTO dbs.dashboard_notifications_templates ( template_name, title_En, tit
 VALUES ('Notice.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant',
         'A judgment has been made against you',
         'A judgment has been made against you',
-        '<p class="govuk-body">The exact details of what you need to pay, and by when, are stated on the judgment. <br> If you want to dispute the judgment, or ask to change how and when you pay back the claim amount, you can <a href="{VIEW_AND_RESPOND}" class="govuk-link">make an application to set aside (remove) or vary the judgment</a>.</p>',
-        '<p class="govuk-body">The exact details of what you need to pay, and by when, are stated on the judgment. <br> If you want to dispute the judgment, or ask to change how and when you pay back the claim amount, you can <a href="{VIEW_AND_RESPOND}" class="govuk-link">make an application to set aside (remove) or vary the judgment</a>.</p>',
+        '<p class="govuk-body">The exact details of what you need to pay, and by when, are stated on the judgment. <br> If you want to dispute the judgment, or ask to change how and when you pay back the claim amount, you can ${djDefendantNotificationMessage}.</p>',
+        '<p class="govuk-body">The exact details of what you need to pay, and by when, are stated on the judgment. <br> If you want to dispute the judgment, or ask to change how and when you pay back the claim amount, you can ${djDefendantNotificationMessage}.</p>',
         'DEFENDANT');
 
 /**
@@ -41,6 +41,6 @@ VALUES ('<a>View the response to the claim</a>', 'The response',
        ('<a>View the bundle</a>', 'Hearing',
         '<a>View the bundle</a>', 'Hearing',
         'Hearing.Bundle.View', 'Scenario.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant', '{2, 2}', 'DEFENDANT', 10),
-       ('<a>View the judgment</a>', 'Judgments from the court',
-        '<a>View the judgment</a>', 'Judgments from the court',
+       ('<a href={VIEW_JUDGEMENT} class="govuk-link">View the judgment</a>', 'Judgments from the court',
+        '<a href={VIEW_JUDGEMENT} class="govuk-link">View the judgment</a>', 'Judgments from the court',
         'Judgment.View', 'Scenario.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant', '{3, 3}', 'DEFENDANT', 12);
