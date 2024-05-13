dd48185474f2c3666d91fc212ba423002173f347
jeswanth-hmcts <134285996+jeswanth-hmcts@users.noreply.github.com>
2024-05-13 12:38:04 +0100

    Update V2024_05_09_1429__CIV-13483-claimant-requested-judge-to-make-new-plan.sql

--- src/main/resources/db/migration/V2024_05_09_1429__CIV-13483-claimant-requested-judge-to-make-new-plan.sql
+++ src/main/resources/db/migration/V2024_05_09_1429__CIV-13483-claimant-requested-judge-to-make-new-plan.sql
@@ -3,7 +3,7 @@
  */
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
 VALUES ('Scenario.AAA6.ClaimantIntent.RequestJudgePlan.RequestedCCJ.Claimant',
-        '{"Notice.AAA6.DefResponse.FullOrPartAdmit.PayBySetDate.Claimant","Notice.AAA6.DefResponse.FullOrPartAdmit.PayByInstallments.Claimant"}',
+        '{"Notice.AAA6.DefResponse.FullOrPartAdmit.PayBySetDate.Claimant","Notice.AAA6.DefResponse.FullOrPartAdmit.PayByInstalments.Claimant"}',
         '{"Notice.AAA6.ClaimantIntent.RequestJudgePlan.RequestedCCJ.Claimant" : ["respondent1PartyName"]}');
 
 /**
