4b54b1cf9ab0c22bf288f64725e6530c73840470
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2024-05-14 12:12:36 +0100

    Reverted nhd handler

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/hearings/UpdateNextHearingDetailsCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/hearings/UpdateNextHearingDetailsCallbackHandler.java
@@ -26,7 +26,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
-import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UPDATE_NEXT_HEARING_DETAILS;
@@ -74,7 +73,6 @@ public class UpdateNextHearingDetailsCallbackHandler extends CallbackHandler {
     private CallbackResponse updateNextHearingDetails(CallbackParams callbackParams) {
         Long caseId = callbackParams.getRequest().getCaseDetails().getId();
         CaseData.CaseDataBuilder<?, ?> caseDataBuilder = callbackParams.getCaseData().toBuilder();
-        String authToken = callbackParams.getParams().get(BEARER_TOKEN).toString();
         HearingsResponse hearingsResponse = getHearings(caseId);
 
         CaseHearing latestHearing = getLatestHearing(hearingsResponse);
