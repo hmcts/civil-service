3e995d0d8797a17944a1ca5d58417f8f428b0879
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-05-16 14:22:16 +0200

    CIV-8154 delete import

--- src/main/java/uk/gov/hmcts/reform/civil/controllers/airlines/FlightController.java
+++ src/main/java/uk/gov/hmcts/reform/civil/controllers/airlines/FlightController.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
