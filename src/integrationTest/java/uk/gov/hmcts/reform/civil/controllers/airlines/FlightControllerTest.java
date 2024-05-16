10b4e7ac549c46e96af3313a9298b371b5e23fc7
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-05-16 11:55:54 +0200

    CIV-8154 delete imports

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/airlines/FlightControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/airlines/FlightControllerTest.java
@@ -1,24 +1,15 @@
 package uk.gov.hmcts.reform.civil.controllers.airlines;
 
-import com.opencsv.CSVReader;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.gov.hmcts.reform.civil.controllers.BaseIntegrationTest;
 import uk.gov.hmcts.reform.civil.model.AirlineEpimsId;
-import uk.gov.hmcts.reform.civil.model.common.DynamicList;
-import uk.gov.hmcts.reform.civil.model.common.DynamicListElement;
 import uk.gov.hmcts.reform.civil.service.AirlineEpimsDataLoader;
-import uk.gov.hmcts.reform.civil.utils.CourtLocationUtils;
 
-import javax.annotation.PostConstruct;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
