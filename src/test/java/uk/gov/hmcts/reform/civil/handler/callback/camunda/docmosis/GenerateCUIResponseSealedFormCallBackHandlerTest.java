957fb55436e802df961f261597058851d226be5e
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-05-17 15:41:33 +0100

    CIV-13703 Testcase updated

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateCUIResponseSealedFormCallBackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateCUIResponseSealedFormCallBackHandlerTest.java
@@ -14,7 +14,6 @@ import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
 import uk.gov.hmcts.reform.civil.documentmanagement.model.Document;
-import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.common.Element;
@@ -36,7 +35,9 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
-import static uk.gov.hmcts.reform.civil.documentmanagement.model.DocumentType.*;
+import static uk.gov.hmcts.reform.civil.documentmanagement.model.DocumentType.DEFENDANT_DEFENCE;
+import static uk.gov.hmcts.reform.civil.documentmanagement.model.DocumentType.DIRECTIONS_QUESTIONNAIRE;
+import static uk.gov.hmcts.reform.civil.documentmanagement.model.DocumentType.SEALED_CLAIM;
 import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.N1;
 import static uk.gov.hmcts.reform.civil.utils.ElementUtils.element;
 
@@ -65,18 +66,19 @@ public class GenerateCUIResponseSealedFormCallBackHandlerTest extends BaseCallba
     @MockBean
     private AssignCategoryId assignCategoryId;
 
-    private static final CaseDocument FORM = CaseDocument.builder()
-        .createdBy("John")
-        .documentName("document name")
-        .documentSize(0L)
-        .documentType(DEFENDANT_DEFENCE)
-        .createdDatetime(LocalDateTime.now())
-        .documentLink(Document.builder()
-                          .documentUrl("fake-url")
-                          .documentFileName("file-name")
-                          .documentBinaryUrl("binary-url")
-                          .build())
-        .build();
+    private static final CaseDocument FORM =
+            CaseDocument.builder()
+                    .createdBy("John")
+                    .documentName("document name")
+                    .documentSize(0L)
+                    .documentType(DEFENDANT_DEFENCE)
+                    .createdDatetime(LocalDateTime.now())
+                    .documentLink(Document.builder()
+                            .documentUrl("fake-url")
+                            .documentFileName("file-name")
+                            .documentBinaryUrl("binary-url")
+                            .build())
+                    .build();
     private static final CaseDocument DIRECTIONS_QUESTIONNAIRE_DOC =
             CaseDocument.builder()
                     .createdBy("John")
@@ -105,34 +107,39 @@ public class GenerateCUIResponseSealedFormCallBackHandlerTest extends BaseCallba
                     .build();
     private static final String BEARER_TOKEN = "BEARER_TOKEN";
 
-    @Test
-    void shouldGenerateForm_whenAboutToSubmitCalled() {
+    @BeforeEach
+    void setup() {
         when(featureToggleService.isLipVLipEnabled()).thenReturn(false);
         ReflectionTestUtils.setField(handler, "stitchEnabled", false);
+    }
+
+    @Test
+    void shouldGenerateForm_whenAboutToSubmitCalled() {
         given(formGenerator.generate(any(CaseData.class), anyString())).willReturn(FORM);
         CaseData caseData = CaseData.builder().build();
-
         handler.handle(callbackParamsOf(caseData, ABOUT_TO_SUBMIT));
-
         verify(formGenerator).generate(caseData, BEARER_TOKEN);
     }
 
     @Test
     void shouldGenerateForm_whenIsLipVLipEnabledStitchingDisabled() {
+        //Given
         given(formGenerator.generate(any(CaseData.class), anyString())).willReturn(FORM);
         CaseData caseData = CaseData.builder().build();
         when(featureToggleService.isLipVLipEnabled()).thenReturn(true);
-        ReflectionTestUtils.setField(handler, "stitchEnabled", false);
 
+        //When
         handler.handle(callbackParamsOf(caseData, ABOUT_TO_SUBMIT));
 
+        //Then
         verify(formGenerator).generate(caseData, BEARER_TOKEN);
     }
 
     @Test
     void shouldGenerateForm_whenIsLipVLipEnabledStitchingEnabled() {
-        when(civilDocumentStitchingService.bundle(ArgumentMatchers.anyList(), anyString(), anyString(), anyString(),
-                any(CaseData.class))).thenReturn(STITCHED_DOC);
+        //Given
+        when(civilDocumentStitchingService.bundle(ArgumentMatchers.anyList(), anyString(), anyString(), anyString(), any(CaseData.class)))
+                .thenReturn(STITCHED_DOC);
         List<Element<CaseDocument>> systemGeneratedCaseDocuments = new ArrayList<>();
         systemGeneratedCaseDocuments.add(element(DIRECTIONS_QUESTIONNAIRE_DOC));
         CaseData caseData = CaseDataBuilder.builder()
@@ -141,9 +148,11 @@ public class GenerateCUIResponseSealedFormCallBackHandlerTest extends BaseCallba
         ReflectionTestUtils.setField(handler, "stitchEnabled", true);
         given(formGenerator.generate(any(CaseData.class), anyString())).willReturn(FORM);
 
+        //When
         var response = (AboutToStartOrSubmitCallbackResponse)handler.handle(callbackParamsOf(caseData, ABOUT_TO_SUBMIT));
         CaseData updatedData = mapper.convertValue(response.getData(), CaseData.class);
 
+        //Then
         assertThat(updatedData.getSystemGeneratedCaseDocuments().stream()
                 .filter(caseDocumentElement -> caseDocumentElement.getValue()
                         .getDocumentName().equals(STITCHED_DOC.getDocumentName())).count()).isEqualTo(1);
