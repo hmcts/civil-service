56c04b00b5a00de2fd0eb6dcfa8006453b83bb90
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-05-17 15:22:52 +0100

    CIV-13703 changes

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateCUIResponseSealedFormCallBackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateCUIResponseSealedFormCallBackHandler.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.civil.handler.callback.camunda.docmosis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
@@ -11,12 +12,19 @@ import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CallbackType;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
+import uk.gov.hmcts.reform.civil.documentmanagement.model.DocumentType;
 import uk.gov.hmcts.reform.civil.enums.DocCategory;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.documents.DocumentMetaData;
 import uk.gov.hmcts.reform.civil.service.SystemGeneratedDocumentService;
 import uk.gov.hmcts.reform.civil.service.docmosis.sealedclaim.SealedClaimLipResponseFormGenerator;
+import uk.gov.hmcts.reform.civil.service.stitching.CivilDocumentStitchingService;
 import uk.gov.hmcts.reform.civil.utils.AssignCategoryId;
+import uk.gov.hmcts.reform.civil.utils.ElementUtils;
+import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -34,6 +42,10 @@ public class GenerateCUIResponseSealedFormCallBackHandler extends CallbackHandle
     private final SealedClaimLipResponseFormGenerator formGenerator;
     private final SystemGeneratedDocumentService systemGeneratedDocumentService;
     private final AssignCategoryId assignCategoryId;
+    private final CivilDocumentStitchingService civilDocumentStitchingService;
+    private final FeatureToggleService featureToggleService;
+    @Value("${stitching.enabled:true}")
+    private boolean stitchEnabled;
 
     private final Map<String, Callback> callbackMap = Map.of(
         callbackKey(CallbackType.ABOUT_TO_SUBMIT), this::prepareSealedForm
@@ -55,16 +67,51 @@ public class GenerateCUIResponseSealedFormCallBackHandler extends CallbackHandle
             caseData,
             callbackParams.getParams().get(BEARER_TOKEN).toString()
         );
-        CaseData updatedCaseData = caseData.toBuilder()
-            .respondent1ClaimResponseDocumentSpec(sealedForm)
-            .systemGeneratedCaseDocuments(systemGeneratedDocumentService.getSystemGeneratedDocumentsWithAddedDocument(
-                sealedForm,
-                caseData
-            ))
-            .build();
+        CaseData.CaseDataBuilder<?, ?> caseDataBuilder = caseData.toBuilder();
         assignCategoryId.assignCategoryIdToCaseDocument(sealedForm, DocCategory.DEF1_DEFENSE_DQ.getValue());
+
+        if (stitchEnabled && featureToggleService.isLipVLipEnabled()) {
+            List<DocumentMetaData> documentMetaDataList = fetchDocumentsToStitch(caseData, sealedForm);
+            CaseDocument stitchedDocument = civilDocumentStitchingService.bundle(
+                    documentMetaDataList,
+                    callbackParams.getParams().get(CallbackParams.Params.BEARER_TOKEN).toString(),
+                    sealedForm.getDocumentName(),
+                    sealedForm.getDocumentName(),
+                    caseData
+            );
+            assignCategoryId.assignCategoryIdToCaseDocument(stitchedDocument, DocCategory.DEF1_DEFENSE_DQ.getValue());
+            caseDataBuilder.respondent1ClaimResponseDocumentSpec(stitchedDocument)
+                    .systemGeneratedCaseDocuments(ElementUtils.wrapElements(stitchedDocument));
+        } else {
+            caseDataBuilder.respondent1ClaimResponseDocumentSpec(sealedForm)
+                    .systemGeneratedCaseDocuments(systemGeneratedDocumentService.getSystemGeneratedDocumentsWithAddedDocument(
+                            sealedForm,
+                            caseData
+                    ));
+        }
         return AboutToStartOrSubmitCallbackResponse.builder()
-            .data(updatedCaseData.toMap(objectMapper))
+            .data(caseDataBuilder.build().toMap(objectMapper))
             .build();
+    }
+
+    private List<DocumentMetaData> fetchDocumentsToStitch(CaseData caseData, CaseDocument sealedForm) {
+        List<DocumentMetaData> documents = new ArrayList<>();
+
+        documents.add(new DocumentMetaData(
+                sealedForm.getDocumentLink(),
+                "Sealed Claim form",
+                LocalDate.now().toString()
+        ));
+        ElementUtils.unwrapElements(caseData.getSystemGeneratedCaseDocuments()).stream()
+                .filter(cd -> DocumentType.DIRECTIONS_QUESTIONNAIRE.equals(cd.getDocumentType()))
+                .map(cd ->
+                        new DocumentMetaData(
+                                cd.getDocumentLink(),
+                                "Directions Questionnaire",
+                                LocalDate.now().toString()
+                        )
+                ).forEach(documents::add);
+
+        return documents;
     }
 }
--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateCUIResponseSealedFormCallBackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateCUIResponseSealedFormCallBackHandlerTest.java
@@ -1,29 +1,44 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.docmosis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
 import uk.gov.hmcts.reform.civil.documentmanagement.model.Document;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.common.Element;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.service.SystemGeneratedDocumentService;
 import uk.gov.hmcts.reform.civil.service.docmosis.sealedclaim.SealedClaimLipResponseFormGenerator;
+import uk.gov.hmcts.reform.civil.service.stitching.CivilDocumentStitchingService;
 import uk.gov.hmcts.reform.civil.utils.AssignCategoryId;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
-import static uk.gov.hmcts.reform.civil.documentmanagement.model.DocumentType.DEFENDANT_DEFENCE;
+import static uk.gov.hmcts.reform.civil.documentmanagement.model.DocumentType.*;
+import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.N1;
+import static uk.gov.hmcts.reform.civil.utils.ElementUtils.element;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {
@@ -43,6 +58,11 @@ public class GenerateCUIResponseSealedFormCallBackHandlerTest extends BaseCallba
     @MockBean
     private SystemGeneratedDocumentService systemGeneratedDocumentService;
     @MockBean
+    private CivilDocumentStitchingService civilDocumentStitchingService;
+    @MockBean
+    private FeatureToggleService featureToggleService;
+
+    @MockBean
     private AssignCategoryId assignCategoryId;
 
     private static final CaseDocument FORM = CaseDocument.builder()
@@ -57,14 +77,77 @@ public class GenerateCUIResponseSealedFormCallBackHandlerTest extends BaseCallba
                           .documentBinaryUrl("binary-url")
                           .build())
         .build();
+    private static final CaseDocument DIRECTIONS_QUESTIONNAIRE_DOC =
+            CaseDocument.builder()
+                    .createdBy("John")
+                    .documentName(String.format(N1.getDocumentTitle(), "000MC001"))
+                    .documentSize(0L)
+                    .documentType(DIRECTIONS_QUESTIONNAIRE)
+                    .createdDatetime(LocalDateTime.now())
+                    .documentLink(Document.builder()
+                            .documentUrl("fake-url")
+                            .documentFileName("file-name")
+                            .documentBinaryUrl("binary-url")
+                            .build())
+                    .build();
+    private static final CaseDocument STITCHED_DOC =
+            CaseDocument.builder()
+                    .createdBy("John")
+                    .documentName("Stitched document")
+                    .documentSize(0L)
+                    .documentType(SEALED_CLAIM)
+                    .createdDatetime(LocalDateTime.now())
+                    .documentLink(Document.builder()
+                            .documentUrl("fake-url")
+                            .documentFileName("file-name")
+                            .documentBinaryUrl("binary-url")
+                            .build())
+                    .build();
     private static final String BEARER_TOKEN = "BEARER_TOKEN";
 
     @Test
     void shouldGenerateForm_whenAboutToSubmitCalled() {
+        when(featureToggleService.isLipVLipEnabled()).thenReturn(false);
+        ReflectionTestUtils.setField(handler, "stitchEnabled", false);
         given(formGenerator.generate(any(CaseData.class), anyString())).willReturn(FORM);
         CaseData caseData = CaseData.builder().build();
 
         handler.handle(callbackParamsOf(caseData, ABOUT_TO_SUBMIT));
+
+        verify(formGenerator).generate(caseData, BEARER_TOKEN);
+    }
+
+    @Test
+    void shouldGenerateForm_whenIsLipVLipEnabledStitchingDisabled() {
+        given(formGenerator.generate(any(CaseData.class), anyString())).willReturn(FORM);
+        CaseData caseData = CaseData.builder().build();
+        when(featureToggleService.isLipVLipEnabled()).thenReturn(true);
+        ReflectionTestUtils.setField(handler, "stitchEnabled", false);
+
+        handler.handle(callbackParamsOf(caseData, ABOUT_TO_SUBMIT));
+
+        verify(formGenerator).generate(caseData, BEARER_TOKEN);
+    }
+
+    @Test
+    void shouldGenerateForm_whenIsLipVLipEnabledStitchingEnabled() {
+        when(civilDocumentStitchingService.bundle(ArgumentMatchers.anyList(), anyString(), anyString(), anyString(),
+                any(CaseData.class))).thenReturn(STITCHED_DOC);
+        List<Element<CaseDocument>> systemGeneratedCaseDocuments = new ArrayList<>();
+        systemGeneratedCaseDocuments.add(element(DIRECTIONS_QUESTIONNAIRE_DOC));
+        CaseData caseData = CaseDataBuilder.builder()
+                .systemGeneratedCaseDocuments(systemGeneratedCaseDocuments).build();
+        when(featureToggleService.isLipVLipEnabled()).thenReturn(true);
+        ReflectionTestUtils.setField(handler, "stitchEnabled", true);
+        given(formGenerator.generate(any(CaseData.class), anyString())).willReturn(FORM);
+
+        var response = (AboutToStartOrSubmitCallbackResponse)handler.handle(callbackParamsOf(caseData, ABOUT_TO_SUBMIT));
+        CaseData updatedData = mapper.convertValue(response.getData(), CaseData.class);
+
+        assertThat(updatedData.getSystemGeneratedCaseDocuments().stream()
+                .filter(caseDocumentElement -> caseDocumentElement.getValue()
+                        .getDocumentName().equals(STITCHED_DOC.getDocumentName())).count()).isEqualTo(1);
+
         verify(formGenerator).generate(caseData, BEARER_TOKEN);
     }
 }
