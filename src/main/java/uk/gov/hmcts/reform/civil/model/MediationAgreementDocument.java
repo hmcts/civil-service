c4651072b117e6e1b8c206d7e11dac1572e0b939
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-05-22 14:52:04 +0100

    added documentUploadedDatetime

--- src/main/java/uk/gov/hmcts/reform/civil/model/MediationAgreementDocument.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/MediationAgreementDocument.java
@@ -8,14 +8,18 @@ import uk.gov.hmcts.reform.civil.documentmanagement.model.Document;
 import uk.gov.hmcts.reform.civil.documentmanagement.model.DocumentType;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Data
-@Builder
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class MediationAgreementDocument {
 
-    private final String name;
-    private final DocumentType documentType;
-    private final Document document;
-    private final LocalDateTime createdDatetime = LocalDateTime.now();
+    private String name;
+    private DocumentType documentType;
+    private Document document;
+    @Builder.Default
+    private LocalDateTime documentUploadedDatetime = LocalDateTime.now(ZoneId.of("Europe/London"));
 
 }
