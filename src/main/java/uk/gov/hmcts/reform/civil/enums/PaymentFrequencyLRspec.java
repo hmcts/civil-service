deb80902450dc9c36e16d8b45945a17eaae3c355
Manish Garg <manish.garg@hmcts.net>
2024-05-22 14:33:50 +0100

    CIV-13987 Fixed checkstyle issues

--- src/main/java/uk/gov/hmcts/reform/civil/enums/PaymentFrequencyLRspec.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/PaymentFrequencyLRspec.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum PaymentFrequencyLRspec {
     ONCE_ONE_WEEK("Paid every week", "every week", "bob wythnos"),
     ONCE_TWO_WEEKS("Paid every 2 weeks", "every 2 weeks", "bob pythefnos"),
-    ONCE_THREE_WEEKS("Paid every 3 weeks", "every 3 weeks","bob tair wythnos"),
+    ONCE_THREE_WEEKS("Paid every 3 weeks", "every 3 weeks", "bob tair wythnos"),
     ONCE_FOUR_WEEKS("Paid every 4 weeks", "every 4 weeks", "bob pedair wythnos"),
     ONCE_ONE_MONTH("Paid every month", "every month", "bob mis");
 
