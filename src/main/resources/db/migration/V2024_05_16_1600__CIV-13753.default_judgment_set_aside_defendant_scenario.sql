8613ba96f8dd834ca629ca7e8039e593c1bc63bb
Omaira-Melo-Hmcts <148855113+Omaira-Melo-Hmcts@users.noreply.github.com>
2024-05-28 15:54:52 +0100

    CIV-13791 Refactor to enable the links for the judgment an view claim

--- src/main/resources/db/migration/V2024_05_16_1600__CIV-13753.default_judgment_set_aside_defendant_scenario.sql
+++ src/main/resources/db/migration/V2024_05_16_1600__CIV-13753.default_judgment_set_aside_defendant_scenario.sql
@@ -23,8 +23,8 @@ VALUES ('Notice.AAA6.JudgmentsOnline.SetAsideError.Defendant',
  */
 INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
                                     scenario_name, task_status_sequence, role, task_order)
-VALUES ('<a>View the response to the claim</a>', 'The response',
-        '<a>View the response to the claim</a>', 'The response',
+VALUES ('<a href={VIEW_RESPONSE_TO_CLAIM} class="govuk-link">View the response to the claim</a>', 'The response',
+        '<a href={VIEW_RESPONSE_TO_CLAIM} class="govuk-link">View the response to the claim</a>', 'The response',
         'Response.View', 'Scenario.AAA6.JudgmentsOnline.SetAsideError.Defendant', '{3, 3}', 'DEFENDANT', 3),
        ('<a>View hearings</a>', 'Hearing',
         '<a>View hearings</a>', 'Hearing',
@@ -46,4 +46,4 @@ VALUES ('<a>View the response to the claim</a>', 'The response',
         'Orders and notices from the court', 'Order.View', 'Scenario.AAA6.JudgmentsOnline.SetAsideError.Defendant', '{3, 3}', 'CLAIMANT', 14),
        ('<a href={VIEW_JUDGEMENT} class="govuk-link">View the judgment</a>', 'Judgments from the court',
         '<a href={VIEW_JUDGEMENT} class="govuk-link">View the judgment</a>', 'Judgments from the court',
-        'Judgment.View', 'Scenario.AAA6.JudgmentsOnline.SetAsideError.Defendant', '{1,1}', 'DEFENDANT', 15);
+        'Judgment.View', 'Scenario.AAA6.JudgmentsOnline.SetAsideError.Defendant', '{3,3}', 'DEFENDANT', 15);
