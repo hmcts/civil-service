5838b85d53ca806c0d4bfce22f5071d7d312eab5
neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
2024-05-30 11:22:12 +0100

    CIV-13418 - Dashboard - Welsh translations - Batch 2B (claimant intention) (#4691)
    * CIV-13448 content changes
    
    * CIV-13448 content changes
    
    * CIV-13448 content changes
    
    * CIV-13448 content changes
    
    * CIV-13448 content changes
    
    * CIV-13574 added config for welsh value for payment service
    
    * CIV-13448 added scenario 8 content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * CIV-13448 added suggested content changes
    
    * draft version - covered 25 scenarios.
    
    * 2nd slot of scenarios.
    
    * CIV-13448 added texts to one paragraph
    
    * added remaining translation.
    
    * added few more translation.
    
    * added 2 more translation.
    
    * CIV-13448 content changes related to 12747
    
    * fixed checkstyle and merging.
    
    * fixed integration test.
    
    * fixed few more integration tests.
    
    * fixed checkstyle error.
    
    * CIV-13418 : Reduce line spacing
    
    * fixed issues seen in QA.
    
    * fixed FT.
    
    ---------
    
    Co-authored-by: jeswanth <jeswanth.reddy@hmcts.net>
    Co-authored-by: jeswanth-hmcts <134285996+jeswanth-hmcts@users.noreply.github.com>
    Co-authored-by: hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
    Co-authored-by: Pablo Ortiz <pablo.gutierrez@version1.com>
    Co-authored-by: Raja Mani <rajakm@gmail.com>
    Co-authored-by: dharmendra kumar <dharmendra.kumar@hmcts.net>
    Co-authored-by: jarekPierchala <118526007+jarekPierchala@users.noreply.github.com>

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimantSettlementAgreementScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimantSettlementAgreementScenarioTest.java
@@ -55,13 +55,14 @@ public class ClaimantSettlementAgreementScenarioTest extends DashboardBaseIntegr
                         "must respond by " + DateUtils.formatDate(respondent1SettlementDeadline) +
                         ".</p><p class=\"govuk-body\">If they do not respond by then, or reject the agreement, " +
                         "you can request a County Court Judgment(CCJ).</p>"),
-                jsonPath("$[0].titleCy").value("Settlement agreement"),
+                jsonPath("$[0].titleCy").value("Cytundeb setlo"),
                 jsonPath("$[0].descriptionCy").value(
-                    "<p class=\"govuk-body\">You have accepted the Mr. Sole Trader offer" +
-                        " and asked them to sign a settlement agreement.</p><p class=\"govuk-body\">The defendant must " +
-                        "respond by " +  DateUtils.formatDateInWelsh(respondent1SettlementDeadline.toLocalDate()) +
-                        ".</p><p class=\"govuk-body\">If they do not respond by then, or reject the agreement, " +
-                        "you can request a County Court Judgment(CCJ).</p>")
+                    "<p class=\"govuk-body\">Rydych wedi derbyn cynnig Mr. Sole Trader ac wedi gofyn " +
+                        "iddynt lofnodi cytundeb setlo.</p><p class=\"govuk-body\">Mae’n rhaid i’r diffynnydd ymateb " +
+                        "erbyn " +
+                        DateUtils.formatDateInWelsh(respondent1SettlementDeadline.toLocalDate()) +
+                        ".</p><p class=\"govuk-body\">Os na fyddant wedi ymateb erbyn hynny, neu os byddant yn gwrthod " +
+                        "y cytundeb, gallwch wneud cais am Ddyfarniad Llys Sifil (CCJ).</p>")
             );
     }
 }
