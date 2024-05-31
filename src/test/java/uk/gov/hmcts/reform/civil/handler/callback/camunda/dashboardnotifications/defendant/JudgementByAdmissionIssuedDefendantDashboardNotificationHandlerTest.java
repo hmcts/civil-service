ae903c3a26897908957eb8ad4fe7cfb20e0a5ebb
GermanAmoros <German.Amoros@HMCTS.NET>
2024-05-31 16:42:37 +0200

    CIV-13792 Fix unit test

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/JudgementByAdmissionIssuedDefendantDashboardNotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/JudgementByAdmissionIssuedDefendantDashboardNotificationHandlerTest.java
@@ -11,6 +11,10 @@ import uk.gov.hmcts.reform.civil.client.DashboardApiClient;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.judgmentonline.JudgmentDetails;
+import uk.gov.hmcts.reform.civil.model.judgmentonline.JudgmentInstalmentDetails;
+import uk.gov.hmcts.reform.civil.model.judgmentonline.JudgmentPaymentPlan;
+import uk.gov.hmcts.reform.civil.model.judgmentonline.PaymentFrequency;
 import uk.gov.hmcts.reform.civil.sampledata.CallbackParamsBuilder;
 import uk.gov.hmcts.reform.civil.service.DashboardNotificationsParamsMapper;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
@@ -26,7 +30,10 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_DASHBOARD_NOTIFICATION_JUDGEMENT_BY_ADMISSION_DEFENDANT;
+import static uk.gov.hmcts.reform.civil.enums.RespondentResponsePartAdmissionPaymentTimeLRspec.SUGGESTION_OF_REPAYMENT_PLAN;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_JUDGEMENTS_ONLINE_ISSUED_CCJ_DEFENDANT;
+import static uk.gov.hmcts.reform.civil.model.judgmentonline.JudgmentState.ISSUED;
+import static uk.gov.hmcts.reform.civil.model.judgmentonline.PaymentPlanSelection.PAY_IN_INSTALMENTS;
 
 @ExtendWith(MockitoExtension.class)
 class JudgementByAdmissionIssuedDefendantDashboardNotificationHandlerTest extends BaseCallbackHandlerTest {
@@ -76,6 +83,17 @@ class JudgementByAdmissionIssuedDefendantDashboardNotificationHandlerTest extend
             .ccdCaseReference(1234L)
             .respondent1ResponseDeadline(LocalDate.of(2020, Month.JANUARY, 18).atStartOfDay())
             .respondent1Represented(YesOrNo.NO)
+            .defenceAdmitPartPaymentTimeRouteRequired(SUGGESTION_OF_REPAYMENT_PLAN)
+            .activeJudgment(JudgmentDetails.builder()
+                                .state(ISSUED)
+                                .paymentPlan(JudgmentPaymentPlan.builder().type(PAY_IN_INSTALMENTS).build())
+                                .orderedAmount("150001")
+                                .instalmentDetails(JudgmentInstalmentDetails.builder()
+                                                       .amount("20001")
+                                                       .paymentFrequency(PaymentFrequency.MONTHLY)
+                                                       .startDate(LocalDate.now())
+                                                       .build())
+                                .build())
             .build();
 
         CallbackParams callbackParams = CallbackParamsBuilder.builder()
