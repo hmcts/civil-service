df57b8ab81b3364527e01eb33f3997e38aea0f19
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2024-05-31 13:37:39 +0100

    CIV-13698 update handler

--- src/main/java/uk/gov/hmcts/reform/civil/callback/OrderCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/callback/OrderCallbackHandler.java
@@ -1,58 +1,20 @@
 package uk.gov.hmcts.reform.civil.callback;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Strings;
-import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
-import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 import uk.gov.hmcts.reform.civil.client.DashboardApiClient;
 import uk.gov.hmcts.reform.civil.enums.AllocatedTrack;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.DashboardNotificationsParamsMapper;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
-import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.HashMap;
 
-import static java.util.Objects.isNull;
-import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
 import static uk.gov.hmcts.reform.civil.enums.AllocatedTrack.SMALL_CLAIM;
 
 public abstract class OrderCallbackHandler extends DashboardWithParamsCallbackHandler {
 
-    private final ObjectMapper objectMapper;
-
     protected OrderCallbackHandler(DashboardApiClient dashboardApiClient, DashboardNotificationsParamsMapper mapper,
-                                   FeatureToggleService featureToggleService, ObjectMapper objectMapper) {
+                                   FeatureToggleService featureToggleService) {
         super(dashboardApiClient, mapper, featureToggleService);
-        this.objectMapper = objectMapper;
-    }
-
-    @Override
-    public CallbackResponse configureDashboardScenario(CallbackParams callbackParams) {
-        CaseData caseData = callbackParams.getCaseData();
-        CaseEvent caseEvent = CaseEvent.valueOf(callbackParams.getRequest().getEventId());
-        CaseData.CaseDataBuilder<?, ?> caseDataBuilder = caseData.toBuilder();
-        HashMap<String, Object> paramsMap = (HashMap<String, Object>) mapper.mapCaseDataToParams(caseData, caseEvent);
-
-        if (isNull(caseData.getRequestForReconsiderationDeadline())) {
-            caseDataBuilder.requestForReconsiderationDeadline(LocalDate.now().plusDays(7).atTime(16, 0));
-        }
-
-        String authToken = callbackParams.getParams().get(BEARER_TOKEN).toString();
-        String scenario = getScenario(caseData, callbackParams);
-        if (!Strings.isNullOrEmpty(scenario) && shouldRecordScenario(caseData)) {
-            dashboardApiClient.recordScenario(
-                caseData.getCcdCaseReference().toString(),
-                scenario,
-                authToken,
-                ScenarioRequestParams.builder().params(paramsMap).build()
-            );
-        }
-
-        return AboutToStartOrSubmitCallbackResponse.builder()
-            .data(caseDataBuilder.build().toMap(objectMapper)).build();
     }
 
     protected boolean isEligibleForReconsideration(CaseData caseData) {
--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/OrderMadeClaimantNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/OrderMadeClaimantNotificationHandler.java
@@ -1,7 +1,10 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.claimant;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.callback.OrderCallbackHandler;
@@ -9,9 +12,14 @@ import uk.gov.hmcts.reform.civil.client.DashboardApiClient;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.DashboardNotificationsParamsMapper;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
+import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 
+import static java.util.Objects.isNull;
+import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_DASHBOARD_NOTIFICATION_DJ_SDO_CLAIMANT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_DASHBOARD_NOTIFICATION_FINAL_ORDER_CLAIMANT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_DASHBOARD_NOTIFICATION_SDO_CLAIMANT;
@@ -25,16 +33,18 @@ import static uk.gov.hmcts.reform.civil.utils.MediationUtils.findMediationUnsucc
 @Service
 public class OrderMadeClaimantNotificationHandler extends OrderCallbackHandler {
 
+    private final ObjectMapper objectMapper;
+
     private static final List<CaseEvent> EVENTS = List.of(CREATE_DASHBOARD_NOTIFICATION_FINAL_ORDER_CLAIMANT,
                                                           CREATE_DASHBOARD_NOTIFICATION_DJ_SDO_CLAIMANT,
                                                           CREATE_DASHBOARD_NOTIFICATION_SDO_CLAIMANT);
     public static final String TASK_ID = "GenerateDashboardNotificationFinalOrderClaimant";
 
     public OrderMadeClaimantNotificationHandler(DashboardApiClient dashboardApiClient,
-                                                 DashboardNotificationsParamsMapper mapper,
-                                                 FeatureToggleService featureToggleService,
-                                                ObjectMapper objectMapper) {
-        super(dashboardApiClient, mapper, featureToggleService, objectMapper);
+                                                DashboardNotificationsParamsMapper mapper,
+                                                FeatureToggleService featureToggleService, ObjectMapper objectMapper) {
+        super(dashboardApiClient, mapper, featureToggleService);
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -45,6 +55,35 @@ public class OrderMadeClaimantNotificationHandler extends OrderCallbackHandler {
     @Override
     public List<CaseEvent> handledEvents() {
         return EVENTS;
+    }
+
+    @Override
+    public CallbackResponse configureDashboardScenario(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        CaseEvent caseEvent = CaseEvent.valueOf(callbackParams.getRequest().getEventId());
+        CaseData.CaseDataBuilder<?, ?> caseDataBuilder = caseData.toBuilder();
+        HashMap<String, Object> paramsMap = (HashMap<String, Object>) mapper.mapCaseDataToParams(caseData, caseEvent);
+
+        if (isNull(caseData.getRequestForReconsiderationDeadline())
+            && isSDOEvent(callbackParams)
+            && isEligibleForReconsideration(caseData)
+            && featureToggleService.isDashboardServiceEnabled()) {
+            caseDataBuilder.requestForReconsiderationDeadline(LocalDate.now().plusDays(7).atTime(16, 0));
+        }
+
+        String authToken = callbackParams.getParams().get(BEARER_TOKEN).toString();
+        String scenario = getScenario(caseData, callbackParams);
+        if (!Strings.isNullOrEmpty(scenario) && shouldRecordScenario(caseData)) {
+            dashboardApiClient.recordScenario(
+                caseData.getCcdCaseReference().toString(),
+                scenario,
+                authToken,
+                ScenarioRequestParams.builder().params(paramsMap).build()
+            );
+        }
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDataBuilder.build().toMap(objectMapper)).build();
     }
 
     @Override
