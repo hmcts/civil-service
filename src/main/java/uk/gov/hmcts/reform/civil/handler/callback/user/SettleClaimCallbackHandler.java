35f4a621950edb9e489b077f28a6bedc9f76a82b
ShwetaTandel-hmcts <shweta.tandel@hmcts.net>
2024-06-03 22:20:22 +0100

    fixes

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/SettleClaimCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/SettleClaimCallbackHandler.java
@@ -8,7 +8,6 @@ import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
-import uk.gov.hmcts.reform.civil.enums.CaseState;
 import uk.gov.hmcts.reform.civil.helpers.SettleClaimHelper;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 
