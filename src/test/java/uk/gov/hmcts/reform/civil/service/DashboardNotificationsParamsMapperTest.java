0421dae4452057b048e0d59fa38b4d939e3fafee
Omaira-Melo-Hmcts <148855113+Omaira-Melo-Hmcts@users.noreply.github.com>
2024-06-03 10:41:43 +0100

    CIV-13791 CheckStyle line fixed

--- src/test/java/uk/gov/hmcts/reform/civil/service/DashboardNotificationsParamsMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/DashboardNotificationsParamsMapperTest.java
@@ -220,7 +220,8 @@ public class DashboardNotificationsParamsMapperTest {
 
         Map<String, Object> result = mapper.mapCaseDataToParams(caseData);
 
-        assertThat(result).extracting("paymentFrecuencyMessage").isEqualTo("You must pay the claim amount of £ 23.00 in biweekly instalments of £ 1.20 The first payment is due on 2022-12-12");
+        assertThat(result).extracting("paymentFrecuencyMessage").isEqualTo("You must pay the claim " +
+                                                                               "amount of £ 23.00 in biweekly instalments of £ 1.20 The first payment is due on 2022-12-12");
     }
 
     @Test
@@ -248,7 +249,8 @@ public class DashboardNotificationsParamsMapperTest {
 
         Map<String, Object> result = mapper.mapCaseDataToParams(caseData);
 
-        assertThat(result).extracting("paymentFrecuencyMessage").isEqualTo("You must pay the claim amount of £ 23.00 in weekly instalments of £ 1.20 The first payment is due on 2022-12-12");
+        assertThat(result).extracting("paymentFrecuencyMessage").isEqualTo("You must pay the claim " +
+                                                                               "amount of £ 23.00 in weekly instalments of £ 1.20 The first payment is due on 2022-12-12");
     }
 
     @Test
