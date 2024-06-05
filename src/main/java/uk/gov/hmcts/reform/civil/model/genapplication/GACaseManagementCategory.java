19f830fa8686191d977afb994237b4feae9d7084
tiagreaney <tiagreaney@gmail.com>
2024-06-05 16:07:55 +0100

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/civil/model/genapplication/GACaseManagementCategory.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/genapplication/GACaseManagementCategory.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.civil.model.genapplication;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
