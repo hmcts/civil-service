4910638fec31adfa098aa7b91c2d83529d258b4e
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2024-06-06 12:09:23 +0100

    Switched generating manual hearing notice to use the casemanagement location external short name instead of venu id

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/hearing/HearingFormGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/hearing/HearingFormGenerator.java
@@ -83,7 +83,7 @@ public class HearingFormGenerator implements TemplateDataGenerator<HearingForm> 
         }
 
         return HearingForm.builder()
-            .courtName(caseManagementLocationDetails.getVenueName())
+            .courtName(caseManagementLocationDetails.getExternalShortName())
             .listingOrRelisting(caseData.getListingOrRelisting().toString())
             .court(caseData.getHearingLocation().getValue().getLabel())
             .caseNumber(caseData.getCcdCaseReference().toString())
