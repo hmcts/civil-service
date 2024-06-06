6b029d07813b57c4f0c60f0b8b7023dea6497306
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2024-06-06 13:11:22 +0100

    Adjusted generating general orders to use the casemanagement location external short name

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/caseprogression/JudgeFinalOrderGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/caseprogression/JudgeFinalOrderGenerator.java
@@ -134,7 +134,7 @@ public class JudgeFinalOrderGenerator implements TemplateDataGenerator<JudgeFina
             .withoutNoticeSelectionDate(nonNull(caseData.getOrderWithoutNotice())
                                             ? caseData.getOrderWithoutNotice().getWithoutNoticeSelectionDate() : null)
             .judgeNameTitle(userDetails.getFullName())
-            .courtName(caseManagementLocationDetails.getVenueName())
+            .courtName(caseManagementLocationDetails.getExternalShortName())
             .courtLocation(getHearingLocationText(caseData, authorisation));
         return freeFormOrderBuilder.build();
     }
@@ -158,7 +158,7 @@ public class JudgeFinalOrderGenerator implements TemplateDataGenerator<JudgeFina
             .defendant2Name(nonNull(caseData.getRespondent2()) ? caseData.getRespondent2().getPartyName() : null)
             .claimantNum(nonNull(caseData.getApplicant2()) ? "Claimant 1" : "Claimant")
             .defendantNum(nonNull(caseData.getRespondent2()) ? "Defendant 1" : "Defendant")
-            .courtName(caseManagementLocationDetails.getVenueName())
+            .courtName(caseManagementLocationDetails.getExternalShortName())
             .finalOrderMadeSelection(caseData.getFinalOrderMadeSelection())
             .orderMadeDate(orderMadeDateBuilder(caseData))
             .courtLocation(getHearingLocationText(caseData, authorisation))
