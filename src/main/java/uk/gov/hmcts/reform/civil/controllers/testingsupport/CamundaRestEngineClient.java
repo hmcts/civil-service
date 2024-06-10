bb9b2f7abca4e25837e031d5b36e4a6a8ea7922a
Mike Lemos <gobii@me.com>
2024-06-10 13:26:49 +0100

    DTSCCI-376 removing local apis when there are external lib clients provided to transfer the contract responsibility

--- src/main/java/uk/gov/hmcts/reform/civil/controllers/testingsupport/CamundaRestEngineClient.java
+++ src/main/java/uk/gov/hmcts/reform/civil/controllers/testingsupport/CamundaRestEngineClient.java
@@ -2,9 +2,11 @@ package uk.gov.hmcts.reform.civil.controllers.testingsupport;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.collections4.CollectionUtils;
+import org.camunda.bpm.engine.exception.NotFoundException;
 import org.camunda.community.rest.client.api.ExternalTaskApiClient;
 import org.camunda.community.rest.client.api.IncidentApiClient;
 import org.camunda.community.rest.client.api.ProcessInstanceApiClient;
+import org.camunda.community.rest.client.model.ActivityInstanceDto;
 import org.camunda.community.rest.client.model.IncidentDto;
 import org.springframework.stereotype.Component;
 
@@ -25,13 +27,15 @@ public class CamundaRestEngineClient {
             .map(response -> response.getBody().getChildActivityInstances())
             .filter(CollectionUtils::isNotEmpty)
             .map(activityInstances -> activityInstances.get(0))
-            .map(activityInstanceDto -> activityInstanceDto.getIncidentIds())
+            .map(ActivityInstanceDto::getIncidentIds)
             .filter(CollectionUtils::isNotEmpty)
             .map(incidentIds -> incidentIds.get(0));
     }
 
     public String getIncidentMessage(String incidentId) {
-        IncidentDto incidentDto = incidentApiClient.getIncident(incidentId).getBody();
+        IncidentDto incidentDto =
+            Optional.ofNullable(incidentApiClient.getIncident(incidentId).getBody())
+                .orElseThrow(NotFoundException::new);
         String externalTaskId = incidentDto.getConfiguration();
 
         return externalTaskApiClient.getExternalTaskErrorDetails(externalTaskId).getBody();
