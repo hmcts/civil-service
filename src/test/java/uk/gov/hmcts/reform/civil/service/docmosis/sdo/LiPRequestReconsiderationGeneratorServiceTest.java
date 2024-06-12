6a40145f99cb165f86be29111438af1110a5087e
meulendijksm <max.meulendijks@hmcts.net>
2024-06-12 12:15:14 +0100

    CIV-13930 - fix checkstyle error

--- src/test/java/uk/gov/hmcts/reform/civil/service/docmosis/sdo/LiPRequestReconsiderationGeneratorServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/docmosis/sdo/LiPRequestReconsiderationGeneratorServiceTest.java
@@ -34,6 +34,7 @@ import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.REQUE
     JacksonAutoConfiguration.class
 })
 public class LiPRequestReconsiderationGeneratorServiceTest {
+    
     private static final String BEARER_TOKEN = "Bearer Token";
     private static final byte[] bytes = {1, 2, 3, 4, 5, 6};
     private static final String claimantFileName = String.format(REQUEST_FOR_RECONSIDERATION.getDocumentTitle(), "claimant");
