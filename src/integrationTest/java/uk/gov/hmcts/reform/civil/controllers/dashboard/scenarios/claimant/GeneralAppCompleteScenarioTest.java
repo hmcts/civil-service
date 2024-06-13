325092c5b13a7fe0214452d333e3492948abd743
paul-pearson <paul.pearson@version1.com>
2024-06-13 13:37:03 +0100

    CIV-9441 Remove unused imports

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/GeneralAppCompleteScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/GeneralAppCompleteScenarioTest.java
@@ -8,7 +8,6 @@ import uk.gov.hmcts.reform.civil.controllers.BaseIntegrationTest;
 import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
