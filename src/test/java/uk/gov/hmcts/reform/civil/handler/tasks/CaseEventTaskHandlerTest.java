e52e5e33db9c0917bc2585b45e56ee54345014d9
sankaviv1 <sankavi.mohanraj@version1.com>
2024-06-14 08:58:17 +0100

    fix unit test

--- src/test/java/uk/gov/hmcts/reform/civil/handler/tasks/CaseEventTaskHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/tasks/CaseEventTaskHandlerTest.java
@@ -664,6 +664,7 @@ class CaseEventTaskHandlerTest {
                 return Map.of("ONE_RESPONDENT_REPRESENTATIVE", true,
                               FlowFlag.GENERAL_APPLICATION_ENABLED.name(), false,
                               FlowFlag.DASHBOARD_SERVICE_ENABLED.name(), false,
+                              FlowFlag.CASE_PROGRESSION_ENABLED.name(), false,
                               FlowFlag.BULK_CLAIM_ENABLED.name(), false,
                               FlowFlag.MINTI_ENABLED.name(), false
                 );
