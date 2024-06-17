56591755b0782cc6171d460384a37aeba6a4fc2c
tiagreaney <tiagreaney@gmail.com>
2024-06-17 10:42:07 +0100

    Code smells

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/BundleCreatedNotificationCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/BundleCreatedNotificationCallbackHandlerTest.java
@@ -72,6 +72,10 @@ class BundleCreatedNotificationCallbackHandlerTest extends BaseCallbackHandlerTe
             //Given: Casedata at hearingScheduled state and callback param with about-to-start event
             CaseData caseData = CaseDataBuilder.builder().atStateHearingDateScheduled().build();
 
+            CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_START);
+
+            handler.handle(params);
+
             //Then: Event should start BUNDLE_CREATION_NOTIFICATION
             assertTrue(handler.handledEvents().contains(BUNDLE_CREATION_NOTIFICATION));
         }
