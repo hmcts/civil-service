4b573a683efe58946fd69fc7ad748ff239ed7e8e
TortolaNavarroD <dario.tortolanavarro@version1.com>
2024-06-19 09:37:19 +0100

    CIV-14094 added line break, removed full stop

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimantRejectRepaymentPlanOrgLtdCoDefendantScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimantRejectRepaymentPlanOrgLtdCoDefendantScenarioTest.java
@@ -54,15 +54,15 @@ public class ClaimantRejectRepaymentPlanOrgLtdCoDefendantScenarioTest extends Da
                 jsonPath("$[0].descriptionEn").value(
                     "<p class=\"govuk-body\">You have rejected the defendant's payment plan, the court will issue a County Court Judgment (CCJ)."
                           +  " If you do not agree with the judgment, you can send in the defendant's financial details and ask for this to be redetermined. "
-                          +  "Your online account will not be updated - any further updates will be by post. Email the details and your claim number"
-                          +  " reference to {cmcCourtEmailId} or send by post to: </p><p class=\"govuk-body\">{cmcCourtAddress}</p>"),
+                          +  "Your online account will not be updated - any further updates will be by post.</p><p class=\"govuk-body\">Email the details and your claim number"
+                          +  " reference to {cmcCourtEmailId} or send by post to: </p><br>{cmcCourtAddress}"),
                 jsonPath("$[0].titleCy").value("Bydd y llys yn adolygu’r manylion ac yn cyhoeddi dyfarniad"),
                 jsonPath("$[0].descriptionCy").value(
                     "<p class=\"govuk-body\">Rydych wedi gwrthod cynllun talu’r diffynnydd a bydd y llys yn cyhoeddi Dyfarniad Llys Sirol (CCJ). " +
                         "Os nad ydych yn cytuno â’r dyfarniad, gallwch anfon manylion ariannol y diffynnydd i’r llys a gofyn am ailbenderfyniad. " +
                         "Ni fydd eich cyfrif ar-lein yn cael ei ddiweddaru - bydd unrhyw ddiweddariadau pellach yn cael eu hanfon drwy’r post." +
-                        " Anfonwch y manylion a rhif eich hawliad reference ar e-bost i {cmcCourtEmailId} neu postiwch yr wybodaeth i: </p>" +
-                        "<p class=\"govuk-body\">{cmcCourtAddress}</p>"));
+                        "</p><p class=\"govuk-body\">Anfonwch y manylion a rhif eich hawliad reference ar e-bost i {cmcCourtEmailId} neu postiwch yr wybodaeth i: </p>" +
+                        "<br>{cmcCourtAddress}"));
     }
 
     @Test
@@ -99,15 +99,15 @@ public class ClaimantRejectRepaymentPlanOrgLtdCoDefendantScenarioTest extends Da
                         jsonPath("$[0].descriptionEn").value(
                                 "<p class=\"govuk-body\">You have rejected the defendant's payment plan, the court will issue a County Court Judgment (CCJ)."
                                         +  " If you do not agree with the judgment, you can send in the defendant's financial details and ask for this to be redetermined. "
-                                        +  "Your online account will not be updated - any further updates will be by post. Email the details and your claim number"
-                                        +  " reference to {cmcCourtEmailId} or send by post to: </p><p class=\"govuk-body\">{cmcCourtAddress}</p>"),
+                                        +  "Your online account will not be updated - any further updates will be by post.</p><p class=\"govuk-body\">Email the details and your claim number"
+                                        +  " reference to {cmcCourtEmailId} or send by post to: </p><br>{cmcCourtAddress}"),
                         jsonPath("$[0].titleCy").value("Bydd y llys yn adolygu’r manylion ac yn cyhoeddi dyfarniad"),
                         jsonPath("$[0].descriptionCy").value(
                                 "<p class=\"govuk-body\">Rydych wedi gwrthod cynllun talu’r diffynnydd a bydd y llys yn cyhoeddi Dyfarniad Llys Sirol (CCJ)." +
                                     " Os nad ydych yn cytuno â’r dyfarniad, gallwch anfon manylion ariannol y diffynnydd i’r llys a gofyn am ailbenderfyniad. " +
-                                    "Ni fydd eich cyfrif ar-lein yn cael ei ddiweddaru - bydd unrhyw ddiweddariadau pellach yn cael eu hanfon drwy’r post. Anfonwch y " +
+                                    "Ni fydd eich cyfrif ar-lein yn cael ei ddiweddaru - bydd unrhyw ddiweddariadau pellach yn cael eu hanfon drwy’r post.</p><p class=\"govuk-body\">Anfonwch y " +
                                     "manylion a rhif eich hawliad reference ar e-bost i {cmcCourtEmailId} neu postiwch yr wybodaeth i: </p>" +
-                                    "<p class=\"govuk-body\">{cmcCourtAddress}</p>"));
+                                    "<br>{cmcCourtAddress}"));
     }
 
 }
