51c266dc0bae2f016a9f769c9e945c7e0852250c
Mike Lemos <gobii@me.com>
2024-06-19 10:55:17 +0100

    DTSCCI-374 replace rest template with feign (#4929)
    * DTSCCI-298 adding contract tests
    
    * DTSCCI-298 contract test tidy up
    
    * DTSCCI-298 contract test tidy up
    
    * DTSCCI-298 contract test tidy up
    
    * DTSCCI-298 contract test tidy up
    
    * DTSCCI-298 contract test tidy up
    
    * DTSCCI-298 contract test tidy up
    
    * DTSCCI-298 contract test tidy up
    
    * DTSCCI-298 contract test tidy up
    
    * DTSCCI-298 contract test tidy up
    
    * DTSCCI-298 contract test tidy up
    
    * DTSCCI-298 contract test tidy up
    
    * DTSCCI-298 contract test tidy up
    
    * DTSCCI-298 contract test tidy up
    
    * DTSCCI-298 contract test tidy up
    
    * DTSCCI-376 removing local apis when there are external lib clients provided to transfer the contract responsibility
    
    * DTSCCI-376 removing local apis when there are external lib clients provided to transfer the contract responsibility
    
    * DTSCCI-376 removing local apis when there are external lib clients provided to transfer the contract responsibility
    
    * DTSCCI-376 removing local apis when there are external lib clients provided to transfer the contract responsibility
    
    * DTSCCI-374 replacing restTemplate calls with feign clients
    
    * DTSCCI-374 replacing restTemplate calls with feign clients
    
    * DTSCCI-374 replacing restTemplate calls with feign clients
    
    * DTSCCI-374 replacing restTemplate calls with feign clients
    
    * DTSCCI-374 replacing restTemplate calls with feign clients
    
    * DTSCCI-374 adding feign clients to restTemplate calls
    
    * DTSCCI-374 adding feign clients to restTemplate calls
    
    * DTSCCI-374 adding feign clients to restTemplate calls
    
    * DTSCCI-374 adding feign clients to restTemplate calls
    
    * DTSCCI-374 adding feign clients to restTemplate calls
    
    * DTSCCI-374 adding feign clients to restTemplate calls
    
    * DTSCCI-374 adding feign clients to restTemplate calls
    
    * DTSCCI-374 adding feign clients to restTemplate calls
    
    * DTSCCI-374 adding feign clients to restTemplate calls
    
    * DTSCCI-374 adding feign clients to restTemplate calls
    
    ---------
    
    Co-authored-by: Ruban <ruban72@hotmail.com>

--- src/main/java/uk/gov/hmcts/reform/civil/service/FeesClientService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/FeesClientService.java
@@ -1,20 +1,20 @@
-package uk.gov.hmcts.reform.fees.client;
+package uk.gov.hmcts.reform.civil.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.civil.client.FeesApiClient;
 import uk.gov.hmcts.reform.civil.enums.AllocatedTrack;
-import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
-import uk.gov.hmcts.reform.fees.client.model.Fee2Dto;
-import uk.gov.hmcts.reform.fees.client.model.FeeLookupResponseDto;
+import uk.gov.hmcts.reform.civil.model.Fee2Dto;
+import uk.gov.hmcts.reform.civil.model.FeeLookupResponseDto;
 
 import java.math.BigDecimal;
 
 @Service
 @ConditionalOnProperty(prefix = "fees.api", name = "url")
 
-public class FeesClient {
+public class FeesClientService {
 
     public static final String EVENT_ISSUE = "issue";
     public static final String EVENT_HEARING = "hearing";
@@ -22,7 +22,7 @@ public class FeesClient {
     public static final String HEARING_SMALL_CLAIMS = "HearingSmallClaims";
     public static final String MONEY_CLAIM = "MoneyClaim";
 
-    private final FeesApi feesApi;
+    private final FeesApiClient feesApiClient;
     private final String service;
     private final String jurisdiction1;
     private final String jurisdiction2;
@@ -30,15 +30,15 @@ public class FeesClient {
     private final FeatureToggleService featureToggleService;
 
     @Autowired
-    public FeesClient(
-        FeesApi feesApi,
+    public FeesClientService(
+        FeesApiClient feesApiClient,
         FeatureToggleService featureToggleService,
         @Value("${fees.api.service:}") String service,
         @Value("${fees.api.jurisdiction1:}") String jurisdiction1,
         @Value("${fees.api.jurisdiction2:}") String jurisdiction2,
         @Value("${fees.api.jurisdiction-fast-track-claim:}") String jurisdictionFastTrackClaim
     ) {
-        this.feesApi = feesApi;
+        this.feesApiClient = feesApiClient;
         this.service = service;
         this.jurisdiction1 = jurisdiction1;
         this.jurisdiction2 = jurisdiction2;
@@ -61,7 +61,7 @@ public class FeesClient {
                 jurisdiction2 = this.jurisdiction2;
             }
 
-            return this.feesApi.lookupFee(
+            return this.feesApiClient.lookupFeeWithAmount(
                 service,
                 jurisdiction1,
                 jurisdiction2,
@@ -72,12 +72,12 @@ public class FeesClient {
             );
 
         } else {
-            return this.feesApi.lookupFeeWithoutKeyword(service, jurisdiction1, jurisdiction2, channel, event, amount);
+            return this.feesApiClient.lookupFeeWithoutKeyword(service, jurisdiction1, jurisdiction2, channel, event, amount);
         }
     }
 
     public Fee2Dto[] findRangeGroup(String channel, String event) {
-        return this.feesApi.findRangeGroup(service, jurisdiction1, jurisdiction2, channel, event);
+        return this.feesApiClient.findRangeGroup(service, jurisdiction1, jurisdiction2, channel, event);
     }
 
     /**
