0c6f2e42ca946a1c10428c10268bb8c632bdd4f5
JamiS <santoshini.jami@hmcts.net>
2024-06-20 10:18:11 +0100

    CIV-14128: Fixed tests

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/DiscontinueClaimClaimantCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/DiscontinueClaimClaimantCallbackHandlerTest.java
@@ -1,20 +1,37 @@
 package uk.gov.hmcts.reform.civil.handler.callback.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.sampledata.CallbackParamsBuilder;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
 
 @SpringBootTest(classes = {
     DiscontinueClaimClaimantCallbackHandler.class,
+    JacksonAutoConfiguration.class
 })
-class DiscontinueClaimClaimantCallbackHandlerTest {
+class DiscontinueClaimClaimantCallbackHandlerTest extends BaseCallbackHandlerTest {
 
-    /*@Autowired
-    private DiscontinueClaimClaimantCallbackHandler handler;*/
+    @Autowired
+    private DiscontinueClaimClaimantCallbackHandler handler;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Nested
     class AboutToStartCallback {
 
-        /*@Test
+        @Test
         void shouldReturnNoError_WhenAboutToStartIsInvoked() {
             CaseData caseData = CaseDataBuilder.builder().atStateClaimIssued().build();
             CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_START, caseData).build();
@@ -23,6 +40,6 @@ class DiscontinueClaimClaimantCallbackHandlerTest {
                 .handle(params);
 
             assertThat(response.getErrors()).isNull();
-        }*/
+        }
     }
 }
