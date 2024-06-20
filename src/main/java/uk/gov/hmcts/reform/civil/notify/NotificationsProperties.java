ed9f78eb1c7b96a1c2a873703998102e571e6c37
Sherlyn Khaw <sherlyn.khaw@evoco.co.uk>
2024-06-20 10:33:48 +0100

    fixed commons and service mismatch issue

--- src/main/java/uk/gov/hmcts/reform/civil/notify/NotificationsProperties.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notify/NotificationsProperties.java
@@ -597,4 +597,7 @@ public class NotificationsProperties {
 
     @NotEmpty
     private String evidenceUploadLipTemplateWelsh;
+
+    @NotEmpty
+    private String notifySettleClaimMarkedPaidInFullDefendantTemplate;
 }
