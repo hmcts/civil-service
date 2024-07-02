92691fb568a100e79599d497f7b695f606dfd8a3
TortolaNavarroD <dario.tortolanavarro@version1.com>
2024-07-02 10:26:50 +0100

    CIV-13828 checkstyle

--- dashboard-notifications/src/main/java/uk/gov/hmcts/reform/dashboard/services/DashboardScenariosService.java
+++ dashboard-notifications/src/main/java/uk/gov/hmcts/reform/dashboard/services/DashboardScenariosService.java
@@ -21,8 +21,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 @Slf4j
 @Service
