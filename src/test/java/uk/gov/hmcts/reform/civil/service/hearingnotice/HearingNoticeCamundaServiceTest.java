98af01526972c1fc2786ddeb78b84cf961223cd1
tiagreaney <tiagreaney@gmail.com>
2024-07-02 14:01:55 +0100

    checkstyleTest fix

--- src/test/java/uk/gov/hmcts/reform/civil/service/hearingnotice/HearingNoticeCamundaServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/hearingnotice/HearingNoticeCamundaServiceTest.java
@@ -18,7 +18,8 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 import static uk.gov.hmcts.reform.civil.enums.CaseState.CASE_PROGRESSION;
 
 @ExtendWith(MockitoExtension.class)
