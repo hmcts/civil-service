db8f41a5ee4babc31103a1c499020bc393d52f72
Manish Garg <manish.garg@hmcts.net>
2024-07-11 09:20:58 +0100

    CIV-14448 Dashboard notifications for claimant and defendant for full defence proceed to NO

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimantEndsClaimFullDefenceFullDisputeScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimantEndsClaimFullDefenceFullDisputeScenarioTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.civil.controllers.dashboard.scenarios.claimant;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
