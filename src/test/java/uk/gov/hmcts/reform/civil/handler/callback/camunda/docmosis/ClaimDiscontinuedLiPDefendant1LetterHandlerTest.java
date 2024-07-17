4b05b7023734297c8192e75f1da56564d9cc919d
Omaira-Melo-Hmcts <148855113+Omaira-Melo-Hmcts@users.noreply.github.com>
2024-07-17 13:42:00 +0100

    CIV-14262 Updating tests

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/ClaimDiscontinuedLiPDefendant1LetterHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/ClaimDiscontinuedLiPDefendant1LetterHandlerTest.java
@@ -4,11 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.Party;
 import uk.gov.hmcts.reform.civil.sampledata.CallbackParamsBuilder;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.SEND_DISCONTINUANCE_LETTER_LIP_DEFENDANT1;
 
 @SpringBootTest(classes = {
@@ -32,6 +39,27 @@ public class ClaimDiscontinuedLiPDefendant1LetterHandlerTest extends BaseCallbac
         assertThat(handler.camundaActivityId(CallbackParamsBuilder.builder().request(CallbackRequest.builder().eventId(
                 SEND_DISCONTINUANCE_LETTER_LIP_DEFENDANT1.name()).build())
                                                  .build())).isEqualTo(TASK_ID_DEFENDANT);
+    }
+
+    @Test
+    void shouldNotifyClaimDiscontinuedDefendant1() {
+        // given
+
+        CaseData caseData = CaseDataBuilder.builder()
+            .atStateClaimIssued1v2AndBothDefendantsDefaultJudgment()
+            .respondent1(Party.builder().partyName("Respondent2 name").type(Party.Type.INDIVIDUAL).build())
+            .addRespondent2(YesOrNo.YES)
+            .respondent1Represented(YesOrNo.YES)
+            .build();
+
+        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+        params.getRequest().setEventId(SEND_DISCONTINUANCE_LETTER_LIP_DEFENDANT1.name());
+
+        // when
+        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+
+        // then
+        assertThat(response.getErrors()).isNull();
     }
 
 }
--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/NotifyDefendant2ClaimDiscontinuedNotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/NotifyDefendant2ClaimDiscontinuedNotificationHandlerTest.java
@@ -9,19 +9,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
-import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
 import uk.gov.hmcts.reform.civil.prd.model.Organisation;
+import uk.gov.hmcts.reform.civil.sampledata.CallbackParamsBuilder;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.service.OrganisationService;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_DISCONTINUANCE_DEFENDANT2;
 
 @SpringBootTest(classes = {
     NotifyDefendant2ClaimDiscontinuedNotificationHandler.class,
@@ -42,6 +44,7 @@ class NotifyDefendant2ClaimDiscontinuedNotificationHandlerTest extends BaseCallb
 
     @Autowired
     private NotifyDefendant2ClaimDiscontinuedNotificationHandler handler;
+    public static final String TASK_ID_DEFENDANT = "NotifyDefendant2ClaimDiscontinued";
 
     @Nested
     class AboutToSubmitCallback {
@@ -52,6 +55,18 @@ class NotifyDefendant2ClaimDiscontinuedNotificationHandlerTest extends BaseCallb
                 TEMPLATE_ID);
             when(organisationService.findOrganisationById(anyString()))
                 .thenReturn(Optional.of(Organisation.builder().name("Test Org Name").build()));
+        }
+
+        @Test
+        void handleEventsReturnsTheExpectedCallbackEvent() {
+            assertThat(handler.handledEvents()).contains(NOTIFY_DISCONTINUANCE_DEFENDANT2);
+        }
+
+        @Test
+        void shouldReturnCorrectCamundaActivityId_whenInvoked() {
+            assertThat(handler.camundaActivityId(CallbackParamsBuilder.builder().request(CallbackRequest.builder().eventId(
+                    NOTIFY_DISCONTINUANCE_DEFENDANT2.name()).build())
+                                                     .build())).isEqualTo(TASK_ID_DEFENDANT);
         }
 
         @Test
@@ -66,7 +81,7 @@ class NotifyDefendant2ClaimDiscontinuedNotificationHandlerTest extends BaseCallb
                 .caseData(caseData)
                 .type(ABOUT_TO_SUBMIT)
                 .request(CallbackRequest.builder()
-                             .eventId(CaseEvent.NOTIFY_DISCONTINUANCE_DEFENDANT1.name())
+                             .eventId(NOTIFY_DISCONTINUANCE_DEFENDANT2.name())
                              .build())
                 .build();
 
