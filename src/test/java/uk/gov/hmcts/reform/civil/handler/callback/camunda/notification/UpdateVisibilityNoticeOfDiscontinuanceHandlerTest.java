913f4ecf098695b4c4fe74302b9d7667423919a2
Sherlyn Khaw <sherlyn.khaw@evoco.co.uk>
2024-07-19 08:49:02 +0100

    CIV-14439 Made notice of discontinuance available

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/UpdateVisibilityNoticeOfDiscontinuanceHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/UpdateVisibilityNoticeOfDiscontinuanceHandlerTest.java
@@ -1,27 +1,36 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.notification;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.camunda.bpm.engine.RuntimeService;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
+import uk.gov.hmcts.reform.civil.enums.DocCategory;
 import uk.gov.hmcts.reform.civil.enums.settlediscontinue.ConfirmOrderGivesPermission;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDocumentBuilder;
+import uk.gov.hmcts.reform.civil.utils.AssignCategoryId;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 
 @SpringBootTest(classes = {
     UpdateVisibilityNoticeOfDiscontinuanceHandler.class,
-    JacksonAutoConfiguration.class
+    JacksonAutoConfiguration.class,
+    AssignCategoryId.class
 })
 class UpdateVisibilityNoticeOfDiscontinuanceHandlerTest extends BaseCallbackHandlerTest {
 
@@ -29,7 +38,13 @@ class UpdateVisibilityNoticeOfDiscontinuanceHandlerTest extends BaseCallbackHand
     private RuntimeService runTimeService;
 
     @Autowired
+    private ObjectMapper mapper;
+
+    @Autowired
     private UpdateVisibilityNoticeOfDiscontinuanceHandler handler;
+
+    @Autowired
+    private AssignCategoryId assignCategoryId;
 
     private static final String processId = "process-id";
 
@@ -56,6 +71,34 @@ class UpdateVisibilityNoticeOfDiscontinuanceHandlerTest extends BaseCallbackHand
             handler.handle(params);
             //Then
             verify(runTimeService).setVariable(processId, "discontinuanceValidationSuccess", toggleState);
+        }
+
+        @Test
+        void shouldCopyNoticeOfDiscontinuance() {
+            CaseDocument noticeOfDiscontinuance = CaseDocumentBuilder.builder().documentName("NOTICE_OF_DISCONTINUANCE").build();
+            CaseData caseData = CaseDataBuilder.builder()
+                .noticeOfDiscontinueCWDoc(noticeOfDiscontinuance)
+                .businessProcess(BusinessProcess.builder().processInstanceId(processId).build())
+                .build();
+            caseData.setConfirmOrderGivesPermission(ConfirmOrderGivesPermission.YES);
+
+            CallbackParams params = CallbackParams.builder()
+                .caseData(caseData)
+                .type(ABOUT_TO_SUBMIT)
+                .request(CallbackRequest.builder()
+                             .eventId(CaseEvent.UPDATE_VISIBILITY_NOTICE_OF_DISCONTINUANCE.name())
+                             .build())
+                .build();
+
+            // When
+            var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+
+            // Then
+            CaseData updatedData = mapper.convertValue(response.getData(), CaseData.class);
+            assertThat(updatedData.getNoticeOfDiscontinueCWDoc()).isNull();
+            assertThat(updatedData.getNoticeOfDiscontinueAllParitiesDoc()).isNotNull();
+            assertThat(updatedData.getNoticeOfDiscontinueAllParitiesDoc().getDocumentLink().getCategoryID())
+                .isEqualTo(DocCategory.NOTICE_OF_DISCONTINUE.getValue());
         }
     }
 
