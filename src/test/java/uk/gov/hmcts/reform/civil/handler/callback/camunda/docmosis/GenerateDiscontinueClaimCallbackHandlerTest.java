a5c8c6b7cede46f3c1bc8ea98d4d5a003849f2d9
Omaira-Melo-Hmcts <148855113+Omaira-Melo-Hmcts@users.noreply.github.com>
2024-07-19 08:30:08 +0100

    CIV-14262 Testing purpose

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateDiscontinueClaimCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateDiscontinueClaimCallbackHandlerTest.java
@@ -102,21 +102,6 @@ class GenerateDiscontinueClaimCallbackHandlerTest extends BaseCallbackHandlerTes
             assertThat(updatedData.getNoticeOfDiscontinueAllParitiesDoc()).isNotNull();
         }
 
-        @ParameterizedTest
-        @ValueSource(booleans = {true, false})
-        void shouldUpdateCamundaVariables_whenInvoked(Boolean toggleState) {
-            //Given
-            CaseData caseData = CaseDataBuilder.builder()
-                    .businessProcess(BusinessProcess.builder().processInstanceId(PROCESS_INSTANCE_ID).build()).build();
-            caseData.setCourtPermissionNeeded(
-                    toggleState ? SettleDiscontinueYesOrNoList.YES : SettleDiscontinueYesOrNoList.NO);
-            CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
-            params.getRequest().setEventId(GEN_NOTICE_OF_DISCONTINUANCE.name());
-            //When
-            handler.handle(params);
-            //Then
-            verify(runTimeService).setVariable(PROCESS_INSTANCE_ID, "JUDGE_ORDER_VERIFICATION_REQUIRED", toggleState);
-        }
     }
 
     @Test
