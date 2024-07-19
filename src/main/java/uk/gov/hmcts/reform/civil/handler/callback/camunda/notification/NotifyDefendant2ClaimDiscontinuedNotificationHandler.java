72b82081da576ad57352d988c51b7457a9b335e8
Omaira-Melo-Hmcts <148855113+Omaira-Melo-Hmcts@users.noreply.github.com>
2024-07-19 09:58:32 +0100

    CIV-14244 Fixing defendant2 notification error

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/NotifyDefendant2ClaimDiscontinuedNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/NotifyDefendant2ClaimDiscontinuedNotificationHandler.java
@@ -2,17 +2,23 @@ package uk.gov.hmcts.reform.civil.handler.callback.camunda.notification;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.notify.NotificationService;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+import uk.gov.hmcts.reform.civil.service.OrganisationService;
 
 import java.util.List;
 import java.util.Map;
 
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_DISCONTINUANCE_DEFENDANT2;
+import static uk.gov.hmcts.reform.civil.utils.NotificationUtils.getRespondentLegalOrganizationName;
 
 @Service
 @RequiredArgsConstructor
@@ -22,11 +28,18 @@ public class NotifyDefendant2ClaimDiscontinuedNotificationHandler extends Callba
     private static final List<CaseEvent> EVENTS = List.of(NOTIFY_DISCONTINUANCE_DEFENDANT2);
     public static final String TASK_ID = "NotifyDefendant2ClaimDiscontinued";
 
+    private static final String REFERENCE_TEMPLATE =
+        "defendant2-claim-discontinued-%s";
+
+    private final NotificationService notificationService;
+    private final NotificationsProperties notificationsProperties;
+    private final OrganisationService organisationService;
+
     @Override
     protected Map<String, Callback> callbacks() {
         return Map.of(
             callbackKey(ABOUT_TO_SUBMIT),
-            this::emptyCallbackResponse
+            this::notifyDefendant2ClaimDiscontinued
         );
     }
 
@@ -40,8 +53,36 @@ public class NotifyDefendant2ClaimDiscontinuedNotificationHandler extends Callba
         return EVENTS;
     }
 
+    private CallbackResponse notifyDefendant2ClaimDiscontinued(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+
+        if (!caseData.isRespondent2LiP()) {
+            notificationService.sendMail(
+                caseData.getRespondentSolicitor2EmailAddress(),
+                getLRTemplate(),
+                addProperties(caseData),
+                getReferenceTemplate(caseData)
+            );
+        }
+
+        return AboutToStartOrSubmitCallbackResponse.builder().build();
+    }
+
+    private String getLRTemplate() {
+        return notificationsProperties.getNotifyClaimDiscontinuedLRTemplate();
+    }
+
+    private String getReferenceTemplate(CaseData caseData) {
+        return String.format(REFERENCE_TEMPLATE, caseData.getLegacyCaseReference());
+    }
+
     @Override
     public Map<String, String> addProperties(CaseData caseData) {
-        return null;
+        return Map.of(
+            CLAIM_REFERENCE_NUMBER,
+            caseData.getLegacyCaseReference(),
+            LEGAL_ORG_NAME,
+            getRespondentLegalOrganizationName(caseData.getRespondent2OrganisationPolicy(), organisationService)
+        );
     }
 }
