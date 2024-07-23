7c0e270905f8a7073e06620a2a926c089f89edaf
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-07-23 16:06:31 +0100

    CIV-14593 changes to send email notification to claimant and defendant when restitching is done

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/NotifyDefendantAmendRestitchBundleHandler.java
@@ -0,0 +1,85 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.notification;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.Callback;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.notify.NotificationService;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+import uk.gov.hmcts.reform.civil.utils.PartyUtils;
+
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Objects.nonNull;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_DEFENDANT_AMEND_RESTITCH_BUNDLE;
+
+@Service
+@RequiredArgsConstructor
+public class NotifyDefendantAmendRestitchBundleHandler extends CallbackHandler implements NotificationData {
+
+    public static final String TASK_ID = "NotifyDefendantAmendRestitchBundle";
+    private static final String REFERENCE_TEMPLATE =
+        "amend-restitch-bundle-defendant-notification-%s";
+    private final NotificationService notificationService;
+    private final NotificationsProperties notificationsProperties;
+    private static final List<CaseEvent> EVENTS = List.of(
+        NOTIFY_DEFENDANT_AMEND_RESTITCH_BUNDLE
+    );
+
+    @Override
+    protected Map<String, Callback> callbacks() {
+        return Map.of(
+            callbackKey(ABOUT_TO_SUBMIT), this::sendNotification
+        );
+    }
+
+    @Override
+    public String camundaActivityId(CallbackParams callbackParams) {
+        return TASK_ID;
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+
+    public CallbackResponse sendNotification(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+
+        if (caseData.isRespondent1LiP() && nonNull(caseData.getRespondent1().getPartyEmail())) {
+            notificationService.sendMail(
+                getRecipientEmail(caseData),
+                getNotificationTemplate(caseData),
+                addProperties(caseData),
+                String.format(REFERENCE_TEMPLATE, caseData.getLegacyCaseReference())
+            );
+        }
+        return AboutToStartOrSubmitCallbackResponse.builder().build();
+    }
+
+    public Map<String, String> addProperties(CaseData caseData) {
+        return Map.of(
+            CLAIM_REFERENCE_NUMBER, caseData.getLegacyCaseReference(),
+            PARTY_NAME, caseData.getRespondent1().getPartyName(),
+            CLAIMANT_V_DEFENDANT, PartyUtils.getAllPartyNames(caseData)
+        );
+    }
+
+    private String getNotificationTemplate(CaseData caseData) {
+        return caseData.isRespondentResponseBilingual()
+            ? notificationsProperties.getNotifyLipUpdateTemplateBilingual()
+            : notificationsProperties.getNotifyLipUpdateTemplate();
+    }
+
+    private String getRecipientEmail(CaseData caseData) {
+        return  caseData.getRespondent1().getPartyEmail();
+    }
+
+}
---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/NotifyDefendantAmendRestitchBundleHandlerTest.java
@@ -0,0 +1,162 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.enums.dq.Language;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.IdamUserDetails;
+import uk.gov.hmcts.reform.civil.model.Party;
+import uk.gov.hmcts.reform.civil.model.citizenui.CaseDataLiP;
+import uk.gov.hmcts.reform.civil.model.citizenui.RespondentLiPResponse;
+import uk.gov.hmcts.reform.civil.notify.NotificationService;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+import uk.gov.hmcts.reform.civil.sampledata.CallbackParamsBuilder;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIMANT_V_DEFENDANT;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIM_REFERENCE_NUMBER;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.PARTY_NAME;
+
+@SpringBootTest(classes = {
+    NotifyDefendantAmendRestitchBundleHandler.class,
+    JacksonAutoConfiguration.class
+})
+class NotifyDefendantAmendRestitchBundleHandlerTest {
+
+    public static final String TEMPLATE_ID = "template-id";
+    public static final String BILINGUAL_TEMPLATE_ID = "bilingual-template-id";
+
+    @MockBean
+    private NotificationService notificationService;
+    @MockBean
+    private NotificationsProperties notificationsProperties;
+    @Autowired
+    NotifyDefendantAmendRestitchBundleHandler handler;
+
+    @Test
+    void shouldSendEmailWhenAllDataIsCorrectAndNotBilingual() {
+
+        CaseData caseData = CaseDataBuilder.builder().atStateClaimDetailsNotified()
+            .claimantUserDetails(IdamUserDetails.builder().email("claimant@hmcts.net").build())
+            .applicant1(Party.builder().individualFirstName("John").individualLastName("Doe")
+                            .type(Party.Type.INDIVIDUAL).build())
+            .applicant1Represented(YesOrNo.NO)
+            .respondent1(Party.builder().individualFirstName("Jack").individualLastName("Jackson")
+                             .type(Party.Type.INDIVIDUAL).partyEmail("respondentLip@example.com").build()).respondent1Represented(YesOrNo.NO).build();
+
+        when(notificationsProperties.getNotifyLipUpdateTemplate()).thenReturn(
+            TEMPLATE_ID);
+
+        CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_SUBMIT, caseData).build();
+
+        handler.handle(params);
+
+        verify(notificationService).sendMail(
+            "respondentLip@example.com",
+            TEMPLATE_ID,
+            getNotificationDataMap(caseData),
+            "amend-restitch-bundle-defendant-notification-000DC001"
+        );
+
+    }
+
+    @Test
+    void shouldSendEmailWhenAllDataIsCorrectAndBilingual() {
+
+        CaseData caseData = CaseDataBuilder.builder().atStateClaimDetailsNotified()
+            .claimantUserDetails(IdamUserDetails.builder().email("claimant@hmcts.net").
+                                     build())
+            .applicant1(Party.builder().individualFirstName("John").individualLastName("Doe")
+                            .type(Party.Type.INDIVIDUAL).build())
+            .applicant1Represented(YesOrNo.NO)
+
+            .respondent1(Party.builder().individualFirstName("Jack").individualLastName("Jackson")
+                             .type(Party.Type.INDIVIDUAL).partyEmail("respondentLip@example.com").build()).respondent1Represented(YesOrNo.NO).build();
+        caseData = caseData.toBuilder().caseDataLiP(CaseDataLiP.builder()
+                                                         .respondent1LiPResponse(RespondentLiPResponse.builder()
+                                                                                     .respondent1ResponseLanguage(Language.BOTH.toString())
+                                                                                     .build())
+                                                         .build()).build();
+
+        when(notificationsProperties.getNotifyLipUpdateTemplateBilingual()).thenReturn(
+            BILINGUAL_TEMPLATE_ID);
+
+        CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_SUBMIT, caseData).build();
+
+        handler.handle(params);
+
+        verify(notificationService).sendMail(
+            "respondentLip@example.com",
+            BILINGUAL_TEMPLATE_ID,
+            getNotificationDataMap(caseData),
+            "amend-restitch-bundle-defendant-notification-000DC001"
+        );
+
+    }
+
+    @Test
+    void shouldNotSendEmailWhenAllDataIsCorrectAndNotLIP() {
+
+        CaseData caseData = CaseDataBuilder.builder().atStateClaimDetailsNotified()
+            .claimantUserDetails(IdamUserDetails.builder().email("claimant@hmcts.net").build())
+            .applicant1(Party.builder().individualFirstName("John").individualLastName("Doe")
+                            .type(Party.Type.INDIVIDUAL).build())
+            .applicant1Represented(YesOrNo.NO)
+            .respondent1(Party.builder().individualFirstName("Jack").individualLastName("Jackson")
+                             .type(Party.Type.INDIVIDUAL).partyEmail("respondentLip@example.com").build()).respondent1Represented(YesOrNo.YES).build();
+
+        when(notificationsProperties.getNotifyLipUpdateTemplate()).thenReturn(
+            TEMPLATE_ID);
+
+        CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_SUBMIT, caseData).build();
+
+        handler.handle(params);
+
+        verify(notificationService, never()).sendMail(any(), any(), any(), any());
+
+    }
+
+    @Test
+    void shouldNotSendEmailWhenDefendantEmailIsNull() {
+
+        CaseData caseData = CaseDataBuilder.builder().atStateClaimDetailsNotified()
+            .claimantUserDetails(IdamUserDetails.builder().email("claimant@hmcts.net").build())
+            .applicant1(Party.builder().individualFirstName("John").individualLastName("Doe")
+                            .type(Party.Type.INDIVIDUAL).build())
+            .applicant1Represented(YesOrNo.NO)
+            .respondent1(Party.builder().individualFirstName("Jack").individualLastName("Jackson")
+                             .type(Party.Type.INDIVIDUAL).partyEmail(null).build()).respondent1Represented(YesOrNo.NO).build();
+
+        when(notificationsProperties.getNotifyLipUpdateTemplate()).thenReturn(
+            TEMPLATE_ID);
+
+        CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_SUBMIT, caseData).build();
+
+        handler.handle(params);
+
+        verify(notificationService, never()).sendMail(any(), any(), any(), any());
+
+    }
+
+    private Map<String, String> getNotificationDataMap(CaseData caseData) {
+        return Map.of(
+            CLAIM_REFERENCE_NUMBER, caseData.getLegacyCaseReference(),
+            PARTY_NAME, "Jack Jackson",
+            CLAIMANT_V_DEFENDANT, "John Doe V Jack Jackson"
+        );
+    }
+
+}
+
