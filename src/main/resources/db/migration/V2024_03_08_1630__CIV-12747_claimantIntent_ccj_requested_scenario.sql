4cd4334585a7294f1627fafc36eb96657d4603ca
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-07-24 17:22:20 +0100

    CIV-13795 notification content updated

--- src/main/resources/db/migration/V2024_03_08_1630__CIV-12747_claimantIntent_ccj_requested_scenario.sql
+++ src/main/resources/db/migration/V2024_03_08_1630__CIV-12747_claimantIntent_ccj_requested_scenario.sql
@@ -16,8 +16,8 @@ VALUES ('Notice.AAA6.ClaimantIntent.CCJ.Requested.Defendant',
         '<p class="govuk-body">${applicant1PartyName} has requested a CCJ against you, because you have not responded to the claim and the response deadline has passed.</p>'
         '<p class="govuk-body">We will post a CCJ to you and ${applicant1PartyName} and explain what to do next.</p>',
         '<p class="govuk-body">Mae ${applicant1PartyName} wedi gwneud cais am CCJ yn eich erbyn oherwydd nid ydych wedi ymateb i’r hawliad ac mae’r terfyn amser ar gyfer ymateb wedi bod.</p>'
-        '<p class="govuk-body">Ni fydd eich cyfrif ar-lein yn cael ei ddiweddaru gyda manylion cynnydd yr hawliad, a bydd unrhyw ddiweddariadau pellach yn cael eu hanfon drwy’r post.</p>'
-        '<p class="govuk-body">Os yw eich terfyn amser wedi pasio, ond nad yw’r CCJ wedi’i gyhoeddi, gallwch dal ymateb. Cysylltwch â Gwasanaeth Llysoedd a Thribiwnlysoedd EF (GLlTEF) ar {civilMoneyClaimsTelephone} os ydych yn Nghymru a Lloegr. ' ||
+          '<p class="govuk-body">Ni fydd eich cyfrif ar-lein yn cael ei ddiweddaru gyda manylion cynnydd yr hawliad, a bydd unrhyw ddiweddariadau pellach yn cael eu hanfon drwy’r post.</p>'
+          '<p class="govuk-body">Os yw eich terfyn amser wedi pasio, ond nad yw’r CCJ wedi’i gyhoeddi, gallwch dal ymateb. Cysylltwch â Gwasanaeth Llysoedd a Thribiwnlysoedd EF (GLlTEF) ar {civilMoneyClaimsTelephone} os ydych yn Nghymru a Lloegr. ' ||
         'Gallwch ffonio rhwng 8.30am a 5pm dydd Llun i ddydd Gwener. <a href="https://www.gov.uk/call-charges" target="_blank" rel="noopener noreferrer" class="govuk-link">Gwybodaeth am gost galwadau (yn agor mewn tab newydd)</a>.</p>'
           '<p class="govuk-body">Os na fyddwch yn cysylltu, byddwn yn anfon CCJ drwy’r post atoch chi a ${applicant1PartyName} ac yn egluro beth i’w wneud nesaf.</p>',
         'DEFENDANT');
