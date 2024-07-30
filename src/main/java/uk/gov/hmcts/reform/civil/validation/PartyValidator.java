023174451e829d433b2673f059f49a5b0acc0bca
ShwetaTandel-hmcts <shweta.tandel@hmcts.net>
2024-07-30 21:03:33 +0100

    fix

--- src/main/java/uk/gov/hmcts/reform/civil/validation/PartyValidator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/validation/PartyValidator.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.model.Address;
 
 import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PartyValidator {
