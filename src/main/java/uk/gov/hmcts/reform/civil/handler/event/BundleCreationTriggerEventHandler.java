0c8085ed06bcf1089e0a981a88beb075be402562
meulendijksm <max.meulendijks@hmcts.net>
2024-07-30 11:30:38 +0100

    CIV-14593 - checkstyle fixes

--- src/main/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/event/BundleCreationTriggerEventHandler.java
@@ -25,6 +25,7 @@ import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CREATE_BUNDLE;
 @Service
 @AllArgsConstructor
 public class BundleCreationTriggerEventHandler {
+
     private final CoreCaseDataService coreCaseDataService;
 
 
