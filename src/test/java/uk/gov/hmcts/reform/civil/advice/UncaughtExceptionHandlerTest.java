98f321df8823b3efe68c4e60a40083b110dd1bdd
EllisD-B <ellis.domengerboyce@hmcts.net>
2024-07-31 10:18:27 +0100

    Fix test

--- src/test/java/uk/gov/hmcts/reform/civil/advice/UncaughtExceptionHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/advice/UncaughtExceptionHandlerTest.java
@@ -9,17 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import uk.gov.hmcts.reform.civil.request.RequestData;
 
-import java.util.UUID;
-import java.util.function.Function;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UncaughtExceptionHandlerTest {
 
-    private static final String CASE_ID = "1234567891234560";
-    private static final String USER_ID = UUID.randomUUID().toString();
+    public static final String NULL_POINTER_EXCEPTION = "Null pointer Exception!!";
 
     @Mock
     private RequestData requestData;
@@ -29,24 +24,12 @@ class UncaughtExceptionHandlerTest {
 
     @Test
     void shouldReturnNotFound_whenCallbackExceptionThrown() {
-        when(requestData.caseId()).thenReturn(CASE_ID);
-        when(requestData.userId()).thenReturn(USER_ID);
-        testTemplate(
-            NullPointerException::new,
-            uncaughtExceptionHandler::runtimeException
-        );
-    }
+        Exception e = new NullPointerException(NULL_POINTER_EXCEPTION);
+        ResponseEntity<?> result = uncaughtExceptionHandler.runtimeException(e);
 
-    private <E extends Throwable> void testTemplate(
-        Function<String, E> exceptionBuilder,
-        Function<E, ResponseEntity<?>> method
-    ) {
-        E exception = exceptionBuilder.apply("Null pointer Exception!!");
-        ResponseEntity<?> result = method.apply(exception);
         assertThat(result.getStatusCode()).isSameAs(HttpStatus.INTERNAL_SERVER_ERROR);
         assertThat(result.getBody()).isNotNull()
-            .extracting(Object::toString).asString().contains("Null pointer Exception!!");
-        assertThat(result.getBody()).extracting(Object::toString).asString().contains(USER_ID);
-        assertThat(result.getBody()).extracting(Object::toString).asString().contains(CASE_ID);
+            .extracting(Object::toString).asString().contains(NULL_POINTER_EXCEPTION);
     }
+
 }
