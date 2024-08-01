e95b02496717c2bfb205c374a5a4e7ee5fd5b25e
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-08-01 11:20:11 +0100

    CIV-14595 reverting case data changes

--- src/main/java/uk/gov/hmcts/reform/civil/model/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/CaseData.java
@@ -40,8 +40,8 @@ import uk.gov.hmcts.reform.civil.enums.TimelineUploadTypeSpec;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.enums.settlediscontinue.ConfirmOrderGivesPermission;
 import uk.gov.hmcts.reform.civil.enums.settlediscontinue.DiscontinuanceTypeList;
-import uk.gov.hmcts.reform.civil.enums.settlediscontinue.SettleDiscontinueYesOrNoList;
 import uk.gov.hmcts.reform.civil.enums.settlediscontinue.MarkPaidConsentList;
+import uk.gov.hmcts.reform.civil.enums.settlediscontinue.SettleDiscontinueYesOrNoList;
 import uk.gov.hmcts.reform.civil.handler.callback.user.spec.show.ResponseOneVOneShowTag;
 import uk.gov.hmcts.reform.civil.model.breathing.BreathingSpaceInfo;
 import uk.gov.hmcts.reform.civil.model.citizenui.CaseDataLiP;
@@ -497,6 +497,7 @@ public class CaseData extends CaseDataParent implements MappableObject {
 
     @Builder.Default
     private final List<IdValue<Bundle>> caseBundles = new ArrayList<>();
+    private final YesOrNo bundleError;
 
     private final Respondent1DebtLRspec specDefendant1Debts;
     private final Respondent1SelfEmploymentLRspec specDefendant1SelfEmploymentDetails;
@@ -687,7 +688,9 @@ public class CaseData extends CaseDataParent implements MappableObject {
     private SettleDiscontinueYesOrNoList isDiscontinuingAgainstBothDefendants;
     private SettlementReason settleReason;
     private final MarkPaidConsentList markPaidConsent;
-
+    private YesOrNo claimantsConsentToDiscontinuance;
+    private CaseDocument noticeOfDiscontinueCWDoc;
+    private CaseDocument noticeOfDiscontinueAllParitiesDoc;
     @JsonUnwrapped
     private FeePaymentOutcomeDetails feePaymentOutcomeDetails;
 
@@ -955,8 +958,8 @@ public class CaseData extends CaseDataParent implements MappableObject {
                 respondent2Represented,
                 specRespondent2Represented
             )
-            .filter(Objects::nonNull)
-            .findFirst().orElse(null));
+                             .filter(Objects::nonNull)
+                             .findFirst().orElse(null));
     }
 
     @JsonIgnore
@@ -1031,8 +1034,8 @@ public class CaseData extends CaseDataParent implements MappableObject {
     @JsonIgnore
     public boolean isPartAdmitImmediatePaymentClaimSettled() {
         return (isPartAdmitClaimSpec()
-                && (Objects.nonNull(getApplicant1AcceptAdmitAmountPaidSpec())
-                && YesOrNo.YES.equals(getApplicant1AcceptAdmitAmountPaidSpec()))
+            && (Objects.nonNull(getApplicant1AcceptAdmitAmountPaidSpec())
+            && YesOrNo.YES.equals(getApplicant1AcceptAdmitAmountPaidSpec()))
             && (Objects.isNull(getApplicant1AcceptPartAdmitPaymentPlanSpec())));
     }
 
@@ -1533,5 +1536,10 @@ public class CaseData extends CaseDataParent implements MappableObject {
     @JsonIgnore
     public boolean nocApplyForLiPDefendant() {
         return isLipvLROneVOne() && getChangeOfRepresentation() != null &&  this.getCcdState() == CaseState.PROCEEDS_IN_HERITAGE_SYSTEM;
+    }
+
+    @JsonIgnore
+    public boolean isJudgeOrderVerificationRequired() {
+        return (this.getCourtPermissionNeeded() != null && SettleDiscontinueYesOrNoList.YES.equals(this.getCourtPermissionNeeded()));
     }
 }
