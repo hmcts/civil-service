c6f04abf15a2baf8ffe66cbd67249d1154e49f87
tiagreaney <tiagreaney@gmail.com>
2024-08-01 18:08:26 +0100

    Remove spring context from ResetPinCUICallbackHandlerTest

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ResetPinCUICallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ResetPinCUICallbackHandlerTest.java
@@ -1,12 +1,12 @@
 package uk.gov.hmcts.reform.civil.handler.callback.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
@@ -16,8 +16,12 @@ import uk.gov.hmcts.reform.civil.model.DefendantPinToPostLRspec;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.service.pininpost.DefendantPinToPostLRspecService;
 
+import java.util.List;
 import java.time.LocalDate;
+import java.util.Map;
+import java.time.format.DateTimeFormatter;
 
+import static com.launchdarkly.shaded.kotlin.jvm.internal.TypeIntrinsics.castToMap;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -25,17 +29,22 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = {
-    ResetPinCUICallbackHandler.class,
-    JacksonAutoConfiguration.class
-})
+@ExtendWith(MockitoExtension.class)
 public class ResetPinCUICallbackHandlerTest extends BaseCallbackHandlerTest {
 
-    @MockBean
+    @Mock
+    private ObjectMapper objectMapper;
+    @Mock
     private DefendantPinToPostLRspecService defendantPinToPostLRspecService;
-    @Autowired
+    @InjectMocks
     private ResetPinCUICallbackHandler handler;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+        handler = new ResetPinCUICallbackHandler(defendantPinToPostLRspecService, objectMapper);
+        objectMapper.registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule());
+    }
 
     @Nested
     class AboutToSubmitCallback {
@@ -67,10 +76,28 @@ public class ResetPinCUICallbackHandlerTest extends BaseCallbackHandlerTest {
 
             var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
 
-            assertThat(response.getData())
-                .extracting("respondent1PinToPostLRspec")
-                .extracting("expiryDate")
-                .isEqualTo(pin.getExpiryDate().toString());
+            Object respondentPinObj = response.getData().get("respondent1PinToPostLRspec");
+            assertThat(respondentPinObj).isNotNull().isInstanceOf(java.util.Map.class);
+
+            @SuppressWarnings("unchecked")
+            Map<String, Object> respondentPinMap = (Map<String, Object>) respondentPinObj;
+
+            Object expiryDateObj = respondentPinMap.get("expiryDate");
+            assertThat(expiryDateObj).isNotNull().isInstanceOf(java.util.List.class);
+
+            List<?> expiryDateList = (List<?>) expiryDateObj;
+
+            List<Integer> dateComponents = expiryDateList.stream()
+                .map(o -> Integer.parseInt(o.toString())).toList();
+
+            LocalDate expiryDate = LocalDate.of(
+                dateComponents.get(0), dateComponents.get(1), dateComponents.get(2)
+            );
+
+            String formattedExpiryDate = expiryDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
+            String expectedExpiryDate = pin.getExpiryDate().format(DateTimeFormatter.ISO_LOCAL_DATE);
+
+            assertThat(formattedExpiryDate).isEqualTo(expectedExpiryDate);
             verify(defendantPinToPostLRspecService, times(1)).resetPinExpiryDate(givenPin);
         }
     }
