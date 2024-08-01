dc3715671fc35747f27cf38a5c529bb9183ca49d
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2024-08-01 13:11:22 +0100

    remove tests

--- src/test/java/uk/gov/hmcts/reform/civil/service/flowstate/TransitionsTestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.civil.service.flowstate;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("uk.gov.hmcts.reform.civil.stateflow.transitions")
-public class TransitionsTestConfiguration {
-}
