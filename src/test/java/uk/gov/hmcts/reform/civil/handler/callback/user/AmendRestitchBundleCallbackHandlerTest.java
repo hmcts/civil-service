c66d57a2a5ee50190f1f6776e778cf17e62ff41b
meulendijksm <max.meulendijks@hmcts.net>
2024-08-01 16:13:56 +0100

    CIV-14585 - remove mid import

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/AmendRestitchBundleCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/AmendRestitchBundleCallbackHandlerTest.java
@@ -42,7 +42,6 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
-import static uk.gov.hmcts.reform.civil.callback.CallbackType.MID;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
 
 @ExtendWith(MockitoExtension.class)
