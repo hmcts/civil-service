9db2b324dcf2f2bbdcb11dda301873064a5a8c7a
Mike Lemos <gobii@me.com>
2024-08-02 14:29:35 +0100

    DTSCCI-694 added config for dev emv - preview

--- bin/utils/idam-lease-user-token.sh
+++ bin/utils/idam-lease-user-token.sh
@@ -9,8 +9,7 @@ IDAM_API_URL=${IDAM_API_BASE_URL:-http://localhost:5000}
 IDAM_URL=${IDAM_STUB_LOCALHOST:-$IDAM_API_URL}
 
 clientSecret=${CCD_API_GATEWAY_IDAM_CLIENT_SECRET:-ccd_gateway_secret}
-redirectUri=${CCD_IDAM_REDIRECT_URL:-https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect}
-echo "redirectUri: $redirectUri"
+redirectUri=${CCD_IDAM_REDIRECT_URL:-http://localhost:3451/oauth2redirect}
 if [ -z "$IDAM_STUB_LOCALHOST" ]; then
   code=$(curl --insecure --fail --show-error --silent -X POST --user "${username}:${password}" "${IDAM_URL}/oauth2/authorize?redirect_uri=${redirectUri}&response_type=code&client_id=ccd_gateway" -d "" | docker run --rm --interactive ghcr.io/jqlang/jq:latest -r .code)
 else
