b6823a3ff3011c54224607cf33e0e1cac625672a
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-08-05 17:03:42 +0100

    CIV-14593 checksytle error

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/AmendRestitchBundleCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/AmendRestitchBundleCallbackHandlerTest.java
@@ -45,7 +45,6 @@ import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.AMEND_RESTITCH_BUNDLE;
 
-
 @ExtendWith(MockitoExtension.class)
 class AmendRestitchBundleCallbackHandlerTest extends BaseCallbackHandlerTest {
 
