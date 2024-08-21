c96ef32ba61699dae93cbfe5f054d33a048f3f63
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2024-08-21 15:37:06 +0100

    CIV-14750 stay case dashboard notification

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/StayCaseClaimantScenarioTest.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.reform.civil.controllers.dashboard.scenarios.claimant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import uk.gov.hmcts.reform.civil.controllers.CaseProgressionDashboardBaseIntegrationTest;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.claimant.StayCaseClaimantNotificationHandler;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class StayCaseClaimantScenarioTest extends CaseProgressionDashboardBaseIntegrationTest {
+
+    @Autowired
+    private StayCaseClaimantNotificationHandler handler;
+
+    @Test
+    void should_create_stay_case_claimant_scenario() throws Exception {
+
+        CaseData caseData = CaseDataBuilder.builder().atStateRespondentPartAdmissionSpec()
+            .applicant1Represented(YesOrNo.NO).build();
+        when(featureToggleService.isLipVLipEnabled()).thenReturn(true);
+        when(featureToggleService.isCaseProgressionEnabled()).thenReturn(true);
+        handler.handle(callbackParams(caseData));
+
+        String caseId = "72014456456456";
+
+        //Verify Notification is created
+        doGet(BEARER_TOKEN, GET_NOTIFICATIONS_URL, caseId, "CLAIMANT")
+            .andExpect(status().isOk())
+            .andExpectAll(
+                status().is(HttpStatus.OK.value()),
+                jsonPath("$[0].titleEn").value("The case has been stayed"),
+                jsonPath("$[0].descriptionEn").value(
+                    "<p class=\"govuk-body\">The case has been stayed as a result of a judge’s order. Any upcoming hearings will be cancelled. <a href=\"{STAY_CASE_DETAILS}\" rel=\"noopener noreferrer\" target=\"_blank\" class=\"govuk-link\">Review the details of the stay order</a>.</p>"),
+                jsonPath("$[0].titleCy").value("Mae’r achos wedi cael ei atal"),
+                jsonPath("$[0].descriptionCy").value(
+                    "<p class=\"govuk-body\">Mae’r achos wedi cael ei atal o ganlyniad i orchymyn gan farnwr. Bydd unrhyw wrandawiadau sydd wedi’u trefnu yn cael eu canslo. <a href=\"{STAY_CASE_DETAILS}\" rel=\"noopener noreferrer\" target=\"_blank\" class=\"govuk-link\">Adolygu manylion y gorchymyn atal</a>.</p>")
+            );
+    }
+}
---
+++ src/main/resources/db/migration/V2024_08_20_0920__CIV-14750_stay_case_scenario.sql
@@ -0,0 +1,47 @@
+/**
+ * Add scenario
+ */
+INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
+VALUES ('Scenario.AAA6.CP.Case.Stayed.Claimant',
+        '{}',
+        '{"Notice.AAA6.CP.Case.Stayed.Claimant" : []}'),
+       ('Scenario.AAA6.CP.Case.Stayed.Defendant',
+        '{}',
+        '{"Notice.AAA6.CP.Case.Stayed.Defendant" : []}');
+
+/**
+ * Add notification template
+ */
+INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
+                                                  ,notification_role)
+VALUES ('Notice.AAA6.CP.Case.Stayed.Claimant', 'The case has been stayed', 'Mae’r achos wedi cael ei atal',
+        '<p class="govuk-body">The case has been stayed as a result of a judge’s order. Any upcoming hearings will be cancelled. <a href="{STAY_CASE_DETAILS}" rel="noopener noreferrer" target="_blank" class="govuk-link">Review the details of the stay order</a>.</p>',
+        '<p class="govuk-body">Mae’r achos wedi cael ei atal o ganlyniad i orchymyn gan farnwr. Bydd unrhyw wrandawiadau sydd wedi’u trefnu yn cael eu canslo. <a href="{STAY_CASE_DETAILS}" rel="noopener noreferrer" target="_blank" class="govuk-link">Adolygu manylion y gorchymyn atal</a>.</p>',
+        'CLAIMANT'),
+       ('Notice.AAA6.CP.Case.Stayed.Defendant', 'The case has been stayed', 'Mae’r achos wedi cael ei atal',
+        '<p class="govuk-body">The case has been stayed as a result of a judge’s order. Any upcoming hearings will be cancelled. <a href="{STAY_CASE_DETAILS}" rel="noopener noreferrer" target="_blank" class="govuk-link">Review the details of the stay order</a>.</p>',
+        '<p class="govuk-body">Mae’r achos wedi cael ei atal o ganlyniad i orchymyn gan farnwr. Bydd unrhyw wrandawiadau sydd wedi’u trefnu yn cael eu canslo. <a href="{STAY_CASE_DETAILS}" rel="noopener noreferrer" target="_blank" class="govuk-link">Adolygu manylion y gorchymyn atal</a>.</p>',
+        'DEFENDANT');
+
+/**
+ * Add task list items
+ */
+INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
+                                    scenario_name, task_status_sequence, role,task_order)
+values ('<a>Pay the hearing fee</a>', 'Hearing' ,'<a>Talu ffi''r gwrandawiad</a>',
+        'Gwrandawiad', 'Hearing.Fee.Pay', 'Scenario.AAA6.CP.Case.Stayed.Claimant', '{2, 2}', 'CLAIMANT', 9),
+       ('<a>Upload hearing documents</a>', 'Hearing' ,'<a>Llwytho dogfennau''r gwrandawiad</a>',
+        'Gwrandawiad', 'Hearing.Document.Upload', 'Scenario.AAA6.CP.Case.Stayed.Claimant', '{2, 2}', 'CLAIMANT', 10),
+       ('<a>Pay the hearing fee</a>', 'Hearing' ,'<a>Talu ffi''r gwrandawiad</a>',
+        'Gwrandawiad', 'Hearing.Fee.Pay', 'Scenario.AAA6.CP.Case.Stayed.Claimant', '{2, 2}', 'CLAIMANT', 9),
+       ('<a>Upload hearing documents</a>', 'Hearing' ,'<a>Llwytho dogfennau''r gwrandawiad</a>',
+        'Gwrandawiad', 'Hearing.Document.Upload', 'Scenario.AAA6.CP.Case.Stayed.Claimant', '{2, 2}', 'CLAIMANT', 10),
+       ('<a>Add the trial arrangements</a>', 'Hearing' ,'<a>Ychwanegu trefniadau''r treial</a>',
+        'Gwrandawiad', 'Hearing.Arrangements.Add', 'Scenario.AAA6.CP.Case.Stayed.Claimant', '{2, 2}', 'CLAIMANT', 11),
+       ('<a>Upload hearing documents</a>', 'Hearing' ,'<a>Llwytho dogfennau''r gwrandawiad</a>',
+        'Gwrandawiad', 'Hearing.Document.Upload', 'Scenario.AAA6.CP.Case.Stayed.Defendant', '{2, 2}', 'DEFENDANT', 10),
+       ('<a>Upload hearing documents</a>', 'Hearing' ,'<a>Llwytho dogfennau''r gwrandawiad</a>',
+        'Gwrandawiad', 'Hearing.Document.Upload', 'Scenario.AAA6.CP.Case.Stayed.Defendant', '{2, 2}', 'DEFENDANT', 10),
+       ('<a>Add the trial arrangements</a>', 'Hearing' ,'<a>Ychwanegu trefniadau''r treial</a>',
+        'Gwrandawiad', 'Hearing.Arrangements.Add', 'Scenario.AAA6.CP.Case.Stayed.Defendant', '{2, 2}', 'DEFENDANT', 11);
+
