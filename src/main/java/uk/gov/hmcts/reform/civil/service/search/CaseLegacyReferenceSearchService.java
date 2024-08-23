3813438d4391341964a60aa7e834c22b84a4bb2b
Velican <duzgunce.vd@gmail.com>
2024-08-23 13:10:59 +0100

    search for ocmc cases separately

--- src/main/java/uk/gov/hmcts/reform/civil/service/search/CaseLegacyReferenceSearchService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/search/CaseLegacyReferenceSearchService.java
@@ -32,17 +32,20 @@ public class CaseLegacyReferenceSearchService {
         return searchResult.getCases().get(0);
     }
 
-    public CaseDetails getCivilOrOcmcCaseData(String legacyReference) {
-        log.info("searching cases with reference {}", legacyReference);
+    public CaseDetails getCivilOrOcmcCaseDataByCaseReference(String caseReference) {
+        log.info("searching cases with reference {}", caseReference);
         Query query = new Query(boolQuery().must(
             boolQuery()
-                .should(matchQuery("data.legacyCaseReference", legacyReference))
-                .should(matchQuery("data.previousServiceCaseReference", legacyReference))
+                .should(matchQuery("data.legacyCaseReference", caseReference))
+                .should(matchQuery("data.previousServiceCaseReference", caseReference))
         ), List.of(), 0);
         SearchResult searchResult = coreCaseDataService.searchCases(query);
         if (searchResult == null || searchResult.getCases().isEmpty()) {
-            log.error("no case found for {}", legacyReference);
-            throw new SearchServiceCaseNotFoundException();
+            searchResult = coreCaseDataService.searchCMCCases(query);
+            if (searchResult == null || searchResult.getCases().isEmpty()) {
+                log.error("no case found for {}", caseReference);
+                throw new SearchServiceCaseNotFoundException();
+            }
         }
         return searchResult.getCases().get(0);
     }
