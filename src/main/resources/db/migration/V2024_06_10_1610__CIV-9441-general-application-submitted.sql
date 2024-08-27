7e6e486fafc85683c330f73ecffd7d779b39deae
paul-pearson <paul.pearson@version1.com>
2024-08-27 14:37:59 +0100

    CIV-15045 Add separate scenario/template for claimant/defendant

--- src/main/resources/db/migration/V2024_06_10_1610__CIV-9441-general-application-submitted.sql
+++ src/main/resources/db/migration/V2024_06_10_1610__CIV-9441-general-application-submitted.sql
@@ -2,15 +2,10 @@
  * Add scenario
  */
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
-VALUES ('Scenario.AAA6.GeneralApplication.Created.Claimant', '{}', '{"Notice.AAA6.GeneralApplication.Fee.Required" : ["applicationFee"]}'),
-       ('Scenario.AAA6.GeneralApplication.Created.Defendant', '{}', '{}');
-
-INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
-                                                  ,notification_role)
-VALUES ('Notice.AAA6.GeneralApplication.Fee.Required', 'You need to pay your Application fee', 'You need to pay your Application fee',
-        '<p class="govuk-body">Your Application has not yet been issued, in order to proceed you must pay the application fee of ${applicationFee}.</p>',
-        '<p class="govuk-body">Your Application has not yet been issued, in order to proceed you must pay the application fee of ${applicationFee}.</p>',
-        'CLAIMANT');
+VALUES ('Scenario.AAA6.GeneralApplication.Created.Claimant', '{}', '{}'),
+       ('Scenario.AAA6.GeneralApplication.Created.Defendant', '{}', '{}'),
+       ('Scenario.AAA6.GeneralApplication.Complete.Claimant', '{}', '{}'),
+       ('Scenario.AAA6.GeneralApplication.Complete.Defendant', '{}', '{}');
 
 /**
  * Add task list items
--- src/main/resources/db/migration/V2024_08_23_1042__CIV-0000-general-application-issued_payment_required_applicant.sql
+++ src/main/resources/db/migration/V2024_08_23_1042__CIV-0000-general-application-issued_payment_required_applicant.sql
@@ -2,11 +2,21 @@
  * Add scenario
  */
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
-VALUES ('Scenario.AAA6.GeneralApplication.Issue.Payment.Required', '{}', '{"Notice.AAA6.GeneralApplication.Fee.Required" : ["applicationFee"]}');
+VALUES ('Scenario.AAA6.GeneralApplication.Issue.Payment.Required.Claimant', '{}', '{"Notice.AAA6.GeneralApplication.Fee.Required.Claimant" : ["applicationFee"]}');
 
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
-VALUES ('Notice.AAA6.GeneralApplication.Fee.Required', 'You need to pay your Application fee', 'You need to pay your Application fee',
-        '<p class="govuk-body">Your Application has not yet been issued, in order to proceed you must pay the application fee of ${applicationFee}.</p>',
-        '<p class="govuk-body">Your Application has not yet been issued, in order to proceed you must pay the application fee of ${applicationFee}.</p>',
+VALUES ('Notice.AAA6.GeneralApplication.Fee.Required.Claimant', 'You need to pay your Application fee', 'You need to pay your Application fee',
+        '<p class="govuk-body">Your Application has not yet been issued, in order to proceed you must pay the application fee of ${applicationFee}. <a href={GENERAL_APPLICATION_FEE_URL} rel="noopener noreferrer" class="govuk-link">Pay the application fee</a></p>',
+        '<p class="govuk-body">Your Application has not yet been issued, in order to proceed you must pay the application fee of ${applicationFee}. <a href={GENERAL_APPLICATION_FEE_URL} rel="noopener noreferrer" class="govuk-link">Pay the application fee</a></p>',
         'CLAIMANT');
+
+INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
+VALUES ('Scenario.AAA6.GeneralApplication.Issue.Payment.Required.Defendant', '{}', '{"Notice.AAA6.GeneralApplication.Fee.Required.Defendant" : ["applicationFee"]}');
+
+INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
+                                                  ,notification_role)
+VALUES ('Notice.AAA6.GeneralApplication.Fee.Required.Defendant', 'You need to pay your Application fee', 'You need to pay your Application fee',
+        '<p class="govuk-body">Your Application has not yet been issued, in order to proceed you must pay the application fee of ${applicationFee}. <a href={GENERAL_APPLICATION_FEE_URL} rel="noopener noreferrer" class="govuk-link">Pay the application fee</a></p>',
+        '<p class="govuk-body">Your Application has not yet been issued, in order to proceed you must pay the application fee of ${applicationFee}. <a href={GENERAL_APPLICATION_FEE_URL} rel="noopener noreferrer" class="govuk-link">Pay the application fee</a></p>',
+        'DEFENDANT');
