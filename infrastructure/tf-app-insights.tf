32526f75284f32d2ec6ef77d9ae2b204435e128e
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2024-08-27 14:43:30 +0100

    Update tf-app-insights.tf

--- infrastructure/tf-app-insights.tf
+++ infrastructure/tf-app-insights.tf
@@ -12,7 +12,7 @@ module "application_insights" {
 }
 
 moved {
-  from = azurerm_application_insights.appinsights
+  from = azurerm_application_insights.web
   to   = module.application_insights.azurerm_application_insights.this
 }
 
