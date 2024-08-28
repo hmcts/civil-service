232a9fb8cbfddd50395f4f6a5e5444e8135fc863
58046 <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
2024-08-28 10:56:18 +0000

    Updating Terraform Formatting

--- infrastructure/tf-app-insights.tf
+++ infrastructure/tf-app-insights.tf
@@ -1,9 +1,9 @@
 module "application_insights" {
   source = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
 
-  env      = var.env
-  product  = var.product
-  name     = "${var.product}-${var.component}"
+  env     = var.env
+  product = var.product
+  name    = "${var.product}-${var.component}"
 
   resource_group_name = azurerm_resource_group.rg.name
 
