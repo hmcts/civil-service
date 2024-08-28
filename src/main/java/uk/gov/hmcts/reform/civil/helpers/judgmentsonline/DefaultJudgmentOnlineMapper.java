80800c0933f2a9b22ede5cb98990c7c45ce25a5a
Omaira-Melo-Hmcts <148855113+Omaira-Melo-Hmcts@users.noreply.github.com>
2024-08-28 17:35:18 +0100

    CIV-14804 Changes to call to CJES for Default Judgment update (#5339)
    * CJES Service : RECORD_JUDGMENT
    
    * link to ccd and camunda
    
    * update testing
    
    * added testing support
    
    * added testing support
    
    * fix send judgment details handler + tests
    
    * revert jenkinsfile
    
    * fix tests
    
    * fix tests
    
    * fix test
    
    * CIV-14804 Changes to send jo Details to CJES
    
    * CIV-14804 Changes to send jo Details to CJES
    
    ---------
    
    Co-authored-by: Sherlyn Khaw <sherlyn.khaw@evoco.co.uk>
    Co-authored-by: Azam <106387766+Azam-Hmcts@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/civil/helpers/judgmentsonline/DefaultJudgmentOnlineMapper.java
+++ src/main/java/uk/gov/hmcts/reform/civil/helpers/judgmentsonline/DefaultJudgmentOnlineMapper.java
@@ -10,6 +10,7 @@ import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.judgmentonline.JudgmentDetails;
 import uk.gov.hmcts.reform.civil.model.judgmentonline.JudgmentInstalmentDetails;
 import uk.gov.hmcts.reform.civil.model.judgmentonline.JudgmentPaymentPlan;
+import uk.gov.hmcts.reform.civil.model.judgmentonline.JudgmentRTLStatus;
 import uk.gov.hmcts.reform.civil.model.judgmentonline.JudgmentState;
 import uk.gov.hmcts.reform.civil.model.judgmentonline.JudgmentType;
 import uk.gov.hmcts.reform.civil.model.judgmentonline.PaymentFrequency;
