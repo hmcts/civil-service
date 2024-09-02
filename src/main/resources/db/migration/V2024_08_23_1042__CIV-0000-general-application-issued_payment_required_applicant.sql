c7c8ff0c4e56e75a5270c6b2d00679fd2bd86069
paul-pearson <paul.pearson@version1.com>
2024-09-02 11:52:44 +0100

    CIV-0000 Spike changes to use applicant/respondent roles

--- src/main/resources/db/migration/V2024_08_23_1042__CIV-0000-general-application-issued_payment_required_applicant.sql
+++ src/main/resources/db/migration/V2024_08_23_1042__CIV-0000-general-application-issued_payment_required_applicant.sql
@@ -2,21 +2,11 @@
  * Add scenario
  */
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
-VALUES ('Scenario.AAA6.GeneralApplication.Issue.Payment.Required.Claimant', '{}', '{"Notice.AAA6.GeneralApplication.Fee.Required.Claimant" : ["applicationFee"]}');
+VALUES ('Scenario.AAA6.GeneralApplication.Issue.Payment.Required.Applicant', '{}', '{"Notice.AAA6.GeneralApplication.Fee.Required.Applicant" : ["applicationFee"]}');
 
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
-VALUES ('Notice.AAA6.GeneralApplication.Fee.Required.Claimant', 'You need to pay your Application fee', 'You need to pay your Application fee',
+VALUES ('Notice.AAA6.GeneralApplication.Fee.Required.Applicant', 'You need to pay your Application fee', 'You need to pay your Application fee',
         '<p class="govuk-body">Your Application has not yet been issued, in order to proceed you must pay the application fee of ${applicationFee}. <a href={GENERAL_APPLICATION_FEE_URL} rel="noopener noreferrer" class="govuk-link">Pay the application fee</a></p>',
         '<p class="govuk-body">Your Application has not yet been issued, in order to proceed you must pay the application fee of ${applicationFee}. <a href={GENERAL_APPLICATION_FEE_URL} rel="noopener noreferrer" class="govuk-link">Pay the application fee</a></p>',
-        'CLAIMANT');
-
-INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
-VALUES ('Scenario.AAA6.GeneralApplication.Issue.Payment.Required.Defendant', '{}', '{"Notice.AAA6.GeneralApplication.Fee.Required.Defendant" : ["applicationFee"]}');
-
-INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
-                                                  ,notification_role)
-VALUES ('Notice.AAA6.GeneralApplication.Fee.Required.Defendant', 'You need to pay your Application fee', 'You need to pay your Application fee',
-        '<p class="govuk-body">Your Application has not yet been issued, in order to proceed you must pay the application fee of ${applicationFee}. <a href={GENERAL_APPLICATION_FEE_URL} rel="noopener noreferrer" class="govuk-link">Pay the application fee</a></p>',
-        '<p class="govuk-body">Your Application has not yet been issued, in order to proceed you must pay the application fee of ${applicationFee}. <a href={GENERAL_APPLICATION_FEE_URL} rel="noopener noreferrer" class="govuk-link">Pay the application fee</a></p>',
-        'DEFENDANT');
+        'APPLICANT');
--- src/main/resources/db/migration/V2024_08_23_1433__CIV-0000-general-application-submitted_applicant.sql
+++ src/main/resources/db/migration/V2024_08_23_1433__CIV-0000-general-application-submitted_applicant.sql
@@ -2,21 +2,11 @@
  * Add scenario
  */
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
-VALUES ('Scenario.AAA6.GeneralApplication.Submitted.Claimant', '{"Notice.AAA6.GeneralApplication.Fee.Required.Claimant"}', '{"Notice.AAA6.GeneralApplication.Submitted.Claimant" : []}');
+VALUES ('Scenario.AAA6.GeneralApplication.Submitted.Applicant', '{"Notice.AAA6.GeneralApplication.Fee.Required.Applicant"}', '{"Notice.AAA6.GeneralApplication.Submitted.Applicant" : []}');
 
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
-VALUES ('Notice.AAA6.GeneralApplication.Submitted.Claimant', 'Your application has been submitted', 'Your application has been submitted',
+VALUES ('Notice.AAA6.GeneralApplication.Submitted.Applicant', 'Your application has been submitted', 'Your application has been submitted',
         '<p class="govuk-body">Your Application has been submitted.</p>',
         '<p class="govuk-body">Your Application has been submitted.</p>',
-        'CLAIMANT');
-
-INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
-VALUES ('Scenario.AAA6.GeneralApplication.Submitted.Defendant', '{"Notice.AAA6.GeneralApplication.Fee.Required.Defendant"}', '{"Notice.AAA6.GeneralApplication.Submitted.Defendant" : []}');
-
-INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
-                                                  ,notification_role)
-VALUES ('Notice.AAA6.GeneralApplication.Submitted.Defendant', 'Your application has been submitted', 'Your application has been submitted',
-        '<p class="govuk-body">Your Application has been submitted.</p>',
-        '<p class="govuk-body">Your Application has been submitted.</p>',
-        'DEFENDANT');
+        'APPLICANT');
