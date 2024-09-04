5f7408a3292a54714a76ca91619998bd694ab9c6
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-09-04 10:30:10 +0100

    fixing lint

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimReconsiderationUpheldClaimantNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/ClaimReconsiderationUpheldClaimantNotificationHandler.java
@@ -8,7 +8,6 @@ import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
-import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.notify.NotificationService;
 import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
@@ -78,6 +77,7 @@ public class ClaimReconsiderationUpheldClaimantNotificationHandler extends Callb
     private String getTemplate() {
         return notificationsProperties.getNotifyClaimReconsiderationLRTemplate();
     }
+
     private String getReferenceTemplate(CaseData caseData) {
         return String.format(REFERENCE_TEMPLATE, caseData.getLegacyCaseReference());
     }
