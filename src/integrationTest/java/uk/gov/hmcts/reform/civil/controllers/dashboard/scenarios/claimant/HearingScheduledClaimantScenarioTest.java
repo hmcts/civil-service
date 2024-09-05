8860d121c2eaac96afe91e9ef4e89efa16513a90
meulendijksm <max.meulendijks@hmcts.net>
2024-09-05 17:02:33 +0100

    CIV-14191 - fix hearing fee hmc test

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/HearingScheduledClaimantScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/HearingScheduledClaimantScenarioTest.java
@@ -287,41 +287,29 @@ public class HearingScheduledClaimantScenarioTest extends CaseProgressionDashboa
             .andExpect(status().isOk())
             .andExpectAll(
                 status().is(HttpStatus.OK.value()),
-                jsonPath("$[0].titleEn").value("A hearing has been scheduled"),
+                jsonPath("$[0].titleEn").value("You must pay the hearing fee"),
                 jsonPath("$[0].descriptionEn").value(
-                    "<p class=\"govuk-body\">Your hearing has been scheduled for 1 April 2024 at "
-                        + "Name. Please keep your contact details and anyone you wish to rely on in court up" +
-                        " to date. You can update contact details by telephoning the court at 0300 123 7050.</p>" +
-                        "<p class=\"govuk-body\"><a href=\"{VIEW_HEARING_NOTICE_CLICK}\" rel=\"noopener noreferrer\" " +
-                        "target=\"_blank\" class=\"govuk-link\">View the hearing notice</a></p>"),
-                jsonPath("$[0].titleCy").value("Mae gwrandawiad wedi'i drefnu"),
+                    "<p class=\"govuk-body\">You must either <a href=\"{PAY_HEARING_FEE_URL_REDIRECT}\" class=\"govuk-link\">pay the hearing fee</a> of £200 "
+                        + "or <a href=\"{APPLY_HELP_WITH_FEES_START}\" class=\"govuk-link\"> apply for help with fees</a>. " +
+                        "You must do this by 1 April 2024. If you do not take one of these actions, your claim will be struck out."),
+                jsonPath("$[0].titleCy").value("Rhaid i chi dalu ffi'r gwrandawiad"),
                 jsonPath("$[0].descriptionCy").value(
-                    "<p class=\"govuk-body\">Mae eich gwrandawiad wedi'i drefnu ar gyfer 1 Ebrill 2024 yn Name. " +
-                        "Cadwch eich manylion cyswllt chi a manylion cyswllt unrhyw un yr hoffech ddibynnu arnynt yn y llys yn gyfredol. " +
-                        "Gallwch ddiweddaru manylion cyswllt drwy ffonio'r llys ar 0300 303 5174.</p>" +
-                        "<p class=\"govuk-body\"><a href=\"{VIEW_HEARING_NOTICE_CLICK}\" rel=\"noopener noreferrer\" target=\"_blank\" " +
-                        "class=\"govuk-link\">Gweld yr hysbysiad o wrandawiad</a></p>")
+                    "<p class=\"govuk-body\">Rhaid i chi naill ai <a href=\"{PAY_HEARING_FEE_URL_REDIRECT}\" class=\"govuk-link\">dalu ffi'r gwrandawiad</a> o £200 neu <a href=\"{APPLY_HELP_WITH_FEES_START}\" class=\"govuk-link\">wneud cais am help i dalu ffioedd</a>. Mae'n rhaid i chi wneud hyn erbyn 1 Ebrill 2024. Os na fyddwch yn cymryd un o'r camau hyn, bydd eich hawliad yn cael ei ddileu.")
             );
 
         //Verify task Item is created
         doGet(BEARER_TOKEN, GET_TASKS_ITEMS_URL, caseId, "CLAIMANT")
             .andExpectAll(
                 status().is(HttpStatus.OK.value()),
-                jsonPath("$[0].reference").value(caseId),
-                jsonPath("$[0].taskNameEn").value(
-                    "<a href={VIEW_HEARINGS}  rel=\"noopener noreferrer\" class=\"govuk-link\">View the hearing</a>"),
-                jsonPath("$[0].currentStatusEn").value(TaskStatus.AVAILABLE.getName()),
-                jsonPath("$[0].taskNameCy").value(
-                    "<a href={VIEW_HEARINGS}  rel=\"noopener noreferrer\" class=\"govuk-link\">Gweld y gwrandawiad</a>"),
-                jsonPath("$[0].currentStatusCy").value(TaskStatus.AVAILABLE.getWelshName()),
                 jsonPath("$[1].reference").value(caseId),
                 jsonPath("$[1].taskNameEn").value(
-                    "<a href={VIEW_ORDERS_AND_NOTICES}  rel=\"noopener noreferrer\" class=\"govuk-link\">View orders and notices</a>"),
-                jsonPath("$[1].currentStatusEn").value(TaskStatus.AVAILABLE.getName()),
+                    "<a href={PAY_HEARING_FEE} class=\"govuk-link\">Pay the hearing fee</a>"),
+                jsonPath("$[1].currentStatusEn").value(TaskStatus.ACTION_NEEDED.getName()),
                 jsonPath("$[1].taskNameCy").value(
-                    "<a href={VIEW_ORDERS_AND_NOTICES}  rel=\"noopener noreferrer\" class=\"govuk-link\">Gweld gorchmynion a rhybuddion</a>"),
-                jsonPath("$[1].currentStatusCy").value(TaskStatus.AVAILABLE.getWelshName())
-
+                    "<a href={PAY_HEARING_FEE} class=\"govuk-link\">Talu ffi'r gwrandawiad</a>"),
+                jsonPath("$[1].currentStatusCy").value(TaskStatus.ACTION_NEEDED.getWelshName()),
+                jsonPath("$[1].hintTextEn").value("Deadline is 12am on 1 April 2024"),
+                jsonPath("$[1].hintTextCy").value("Deadline is 12am on 1 Ebrill 2024")
             );
     }
 }
