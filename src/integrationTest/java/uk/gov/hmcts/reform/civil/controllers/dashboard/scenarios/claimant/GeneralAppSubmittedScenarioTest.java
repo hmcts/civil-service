47f402db753cbb4d49300efbc76591217588b051
Deepthi Doppalapudi <107422736+deepthidoppalapudihmcts@users.noreply.github.com>
2024-09-05 14:53:20 +0100

    CIV-14404 Checkstyle issues corrected

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/GeneralAppSubmittedScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/GeneralAppSubmittedScenarioTest.java
@@ -16,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Testcontainers
 public class GeneralAppSubmittedScenarioTest extends BaseIntegrationTest {
+
     public static final String SCENARIO_GA_SUBMITTED = "Scenario.AAA6.GeneralApps.ApplicationSubmitted.Applicant";
     private static final String DASHBOARD_CREATE_SCENARIO_URL
         = "/dashboard/scenarios/{scenario_ref}/{unique_case_identifier}";
