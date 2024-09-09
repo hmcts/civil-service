a092b1ba96279c36d2ad3a120ade6f38a78f4bcf
meulendijksm <max.meulendijks@hmcts.net>
2024-09-09 09:35:32 +0100

    CIV-14991 - Add hint text removal

--- src/main/resources/db/migration/V2024_06_20_1144__CIV-14169-defendant_decision_outcome_update_task_list_status.sql
+++ src/main/resources/db/migration/V2024_06_20_1144__CIV-14169-defendant_decision_outcome_update_task_list_status.sql
@@ -10,10 +10,10 @@ VALUES ('Scenario.AAA6.Defendant.DecisionOutcome', '{}', '{}'),
  * Add task list items
  */
 INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
-                                    scenario_name, task_status_sequence, role, task_order)
+                                    scenario_name, task_status_sequence, role, task_order, hint_text_en, hint_text_cy)
 VALUES ('<a>Upload hearing documents</a>', 'Hearing' ,'<a>Llwytho dogfennau''r gwrandawiad</a>',
         'Gwrandawiad', 'Hearing.Document.Upload', 'Scenario.AAA6.Defendant.DecisionOutcome', '{2, 2}', 'DEFENDANT', 10),
        ('<a>Upload hearing documents</a>', 'Hearing' ,'<a>Llwytho dogfennau''r gwrandawiad</a>',
         'Gwrandawiad', 'Hearing.Document.Upload', 'Scenario.AAA6.Defendant.TrialReady.DecisionOutcome', '{2, 2}', 'DEFENDANT', 10),
        ('<a>Add the trial arrangements</a>', 'Hearing' ,'<a>Ychwanegu trefniadau''r treial</a>',
-        'Gwrandawiad', 'Hearing.Arrangements.Add', 'Scenario.AAA6.Defendant.DecisionOutcome', '{2, 2}', 'DEFENDANT', 11);
+        'Gwrandawiad', 'Hearing.Arrangements.Add', 'Scenario.AAA6.Defendant.DecisionOutcome', '{2, 2}', 'DEFENDANT', 11, NULL, NULL);
