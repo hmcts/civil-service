74353553c5d4e4640933ee587c3e2556af156680
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2024-09-10 18:11:18 +0100

    CIV-15070 Update unit tests

--- src/main/java/uk/gov/hmcts/reform/civil/model/docmosis/judgmentonline/RecordJudgmentDeterminationOfMeansLiPDefendantLetter.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/docmosis/judgmentonline/RecordJudgmentDeterminationOfMeansLiPDefendantLetter.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import uk.gov.hmcts.reform.civil.model.Fee;
 import uk.gov.hmcts.reform.civil.model.Party;
 import uk.gov.hmcts.reform.civil.model.common.MappableObject;
 
