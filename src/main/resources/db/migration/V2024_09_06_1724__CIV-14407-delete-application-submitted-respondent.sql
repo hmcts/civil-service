efbf26f1b3c2936288496d1c4d11f599e7f18edf
paul-pearson <paul.pearson@version1.com>
2024-09-10 11:38:07 +0100

    CIV-14407 Use single scenario to delete notifications

--- src/main/resources/db/migration/V2024_09_06_1724__CIV-14407-delete-application-submitted-respondent.sql
+++ src/main/resources/db/migration/V2024_09_06_1724__CIV-14407-delete-application-submitted-respondent.sql
@@ -3,8 +3,4 @@
  */
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
 VALUES ('Scenario.AAA6.GeneralApps.ApplicationMade.Delete.Respondent',
-        '{Notice.AAA6.GeneralApps.NonUrgentApplicationMade.Respondent, Notice.AAA6.GeneralApps.UrgentApplicationMade.Respondent}', '{}');
-
-INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
-VALUES ('Scenario.AAA6.GeneralApps.ApplicationUncloaked.Delete.Respondent',
-        '{Notice.AAA6.GeneralApps.NonUrgentApplicationUncloaked.Respondent, Notice.AAA6.GeneralApps.UrgentApplicationUncloaked.Respondent}', '{}');
+        '{Notice.AAA6.GeneralApps.NonUrgentApplicationMade.Respondent, Notice.AAA6.GeneralApps.UrgentApplicationMade.Respondent, Notice.AAA6.GeneralApps.NonUrgentApplicationUncloaked.Respondent, Notice.AAA6.GeneralApps.UrgentApplicationUncloaked.Respondent}', '{}');
