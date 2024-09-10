021171698dd579faa39f606ce6c7cddf71e1eb01
ShwetaTandel-hmcts <shweta.tandel@hmcts.net>
2024-09-10 20:07:08 +0100

    fix check style

--- src/test/java/uk/gov/hmcts/reform/civil/helpers/judgmentsonline/DefaultJudgmentsOnlineMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/helpers/judgmentsonline/DefaultJudgmentsOnlineMapperTest.java
@@ -1,10 +1,7 @@
 package uk.gov.hmcts.reform.civil.helpers.judgmentsonline;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.gov.hmcts.reform.civil.enums.DJPaymentTypeSelection;
