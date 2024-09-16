86c262920b0897a11c51c44690388524e6283bff
paul-pearson <paul.pearson@version1.com>
2024-09-16 16:24:10 +0100

    CIV-15298 Fix tests with email placeholder

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/TellUsSettledDefendantScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/TellUsSettledDefendantScenarioTest.java
@@ -43,12 +43,12 @@ public class TellUsSettledDefendantScenarioTest extends DashboardBaseIntegration
                 jsonPath("$[0].descriptionEn").value(
                     "<p class=\"govuk-body\">The claimant has confirmed that this case was settled on 19 March 2024.</p>"
                         + "<p class=\"govuk-body\">If you do not agree that the case is settled, please outline your objections in"
-                        + " writing within 19 days of the settlement date, to the Civil National Business Centre using the email address at ocmcnton@justice.gov.uk</p>"),
+                        + " writing within 19 days of the settlement date, to the Civil National Business Centre using the email address at {cmcCourtEmailId}</p>"),
                 jsonPath("$[0].titleCy").value("Mae’r hawliad wedi’i setlo"),
                 jsonPath("$[0].descriptionCy").value(
                     "<p class=\"govuk-body\">The claimant has confirmed that this case was settled on 19 March 2024.</p>"
                         + "<p class=\"govuk-body\">If you do not agree that the case is settled, please outline your objections in"
-                        + " writing within 19 days of the settlement date, to the Civil National Business Centre using the email address at ocmcnton@justice.gov.uk</p>")
+                        + " writing within 19 days of the settlement date, to the Civil National Business Centre using the email address at {cmcCourtEmailId}</p>")
             );
     }
 }
