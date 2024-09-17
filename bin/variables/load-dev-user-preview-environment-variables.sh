7e6f1f775492c70c50a43eedb30a302783ce227c
Mike Lemos <gobii@me.com>
2024-09-17 11:08:22 +0100

    DTSCCI-694 upgrading scripts for dev env

--- bin/variables/load-dev-user-preview-environment-variables.sh
+++ bin/variables/load-dev-user-preview-environment-variables.sh
@@ -9,7 +9,7 @@ source .env.local
 export ENVIRONMENT=preview
 # urls
 export URL=$XUI_WEBAPP_URL
-export CIVIL_SERVICE_URL=$TEST_URL
+export CIVIL_SERVICE_URL="https://civil-service-${user}.preview.platform.hmcts.net"
 export SERVICE_AUTH_PROVIDER_API_BASE_URL="http://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
 export IDAM_API_BASE_URL="https://idam-api.aat.platform.hmcts.net"
 export IDAM_API_URL="https://idam-api.aat.platform.hmcts.net"
@@ -22,4 +22,4 @@ export CCD_API_GATEWAY_IDAM_CLIENT_SECRET=$(az keyvault secret show --vault-name
 export CCD_API_GATEWAY_S2S_SECRET=$(az keyvault secret show --vault-name s2s-aat --name microservicekey-ccd-gw --query value -o tsv)
 export S2S_SECRET=$(az keyvault secret show --vault-name civil-aat --name microservicekey-civil-service --query value -o tsv)
 # definition placeholders
-export CCD_DEF_CASE_SERVICE_BASE_URL=$TEST_URL
+export CCD_DEF_CASE_SERVICE_BASE_URL="https://civil-service-${user}.preview.platform.hmcts.net"
