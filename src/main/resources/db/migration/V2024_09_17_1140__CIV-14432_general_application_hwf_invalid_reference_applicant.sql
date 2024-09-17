c1ca7357dfe31278ca5ba4ee259ae58888739278
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2024-09-17 14:32:14 +0100

    CIV-14432 Set up unit test

--- src/main/resources/db/migration/V2024_09_17_1140__CIV-14432_general_application_hwf_invalid_reference_applicant.sql
+++ src/main/resources/db/migration/V2024_09_17_1140__CIV-14432_general_application_hwf_invalid_reference_applicant.sql
@@ -9,8 +9,8 @@ VALUES ('Scenario.AAA6.GeneralApps.HwF.InvalidRef.Applicant',
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
 VALUES ('Notice.AAA6.GeneralApps.HwF.InvalidRef.Applicant',
-        'You’ve provided an invalid help with fees reference number',
+        'You''ve provided an invalid help with fees reference number',
         'Rydych wedi darparu cyfeirnod help i dalu ffioedd annilys',
-        '<p class="govuk-body">You’ve applied for help with the ${applicationFeeTypeEn} fee, but the reference number is invalid.</p><p class="govuk-body">You’ve been sent an email with instructions on what to do next. If you’ve already read the email and taken action, you can disregard this message.</p><p class="govuk-body">You can pay by phone by calling {civilMoneyClaimsTelephone}.</p>',
-        '<p class="govuk-body">Rydych wedi gwneud cais am help i dalu’r ffi gwneud ${applicationFeeTypeCy}, ond mae''r cyfeirnod yn annilys.</p><p class="govuk-body">Anfonwyd e-bost atoch gyda chyfarwyddiadau ar beth i''w wneud nesaf. Os ydych wedi darllen yr e-bost yn barod ac wedi gweithredu, gallwch anwybyddu''r neges hon.</p><p class="govuk-body">Gallwch dalu dros y ffôn drwy ffonio {civilMoneyClaimsTelephone}.</p>',
+        '<p class="govuk-body">You''ve applied for help with the ${applicationFeeTypeEn} fee, but the reference number is invalid.</p><p class="govuk-body">You''ve been sent an email with instructions on what to do next. If you''ve already read the email and taken action, you can disregard this message.</p><p class="govuk-body">You can pay by phone by calling {civilMoneyClaimsTelephone}.</p>',
+        '<p class="govuk-body">Rydych wedi gwneud cais am help i dalu''r ffi gwneud ${applicationFeeTypeCy}, ond mae''r cyfeirnod yn annilys.</p><p class="govuk-body">Anfonwyd e-bost atoch gyda chyfarwyddiadau ar beth i''w wneud nesaf. Os ydych wedi darllen yr e-bost yn barod ac wedi gweithredu, gallwch anwybyddu''r neges hon.</p><p class="govuk-body">Gallwch dalu dros y ffôn drwy ffonio {civilMoneyClaimsTelephone}.</p>',
         'APPLICANT');
