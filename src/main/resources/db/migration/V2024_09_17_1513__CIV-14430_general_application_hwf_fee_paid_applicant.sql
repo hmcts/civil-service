e407f5f9453b000f1c8fee268ad8b8140da54802
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2024-09-19 13:51:29 +0100

    CIV-14437 Dashboard notification for full remission

--- src/main/resources/db/migration/V2024_09_17_1513__CIV-14430_general_application_hwf_fee_paid_applicant.sql
+++ src/main/resources/db/migration/V2024_09_17_1513__CIV-14430_general_application_hwf_fee_paid_applicant.sql
@@ -4,7 +4,7 @@
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
 VALUES ('Scenario.AAA6.GeneralApps.HwF.FeePaid.Applicant',
         '{"Notice.AAA6.GeneralApps.HwF.MoreInfoRequired.Applicant", "Notice.AAA6.GeneralApps.HwF.Updated.Applicant", "Notice.AAA6.GeneralApps.HwF.InvalidRef.Applicant", "Notice.AAA6.GeneralApps.HwF.PartRemission.Applicant", "Notice.AAA6.GeneralApps.HwFRejected.Applicant", "Notice.AAA6.GeneralApps.HwFRequested.Applicant"}',
-        '{"Notice.AAA6.GeneralApps.HwF.FeePaid.Applicant" : ["applicationFee", "applicationFeeTypeEn", "applicationFeeTypeCy"]}');
+        '{"Notice.AAA6.GeneralApps.HwF.FeePaid.Applicant" : ["applicationFeeTypeEn", "applicationFeeTypeCy"]}');
 
 INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
                                                   ,notification_role)
