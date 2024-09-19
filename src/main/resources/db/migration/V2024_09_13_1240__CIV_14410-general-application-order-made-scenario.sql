ea0b712335f353f0bee6509f4e74380a213dccf3
neeta-hmcts <neeta.shinde@hmcts.net>
2024-09-19 14:12:30 +0100

    added unit tests.

--- src/main/resources/db/migration/V2024_09_13_1240__CIV_14410-general-application-order-made-scenario.sql
+++ src/main/resources/db/migration/V2024_09_13_1240__CIV_14410-general-application-order-made-scenario.sql
@@ -29,8 +29,8 @@ INSERT INTO dbs.dashboard_notifications_templates ( template_name, title_En, tit
 VALUES ('Notice.AAA6.GeneralApps.OrderMade.Applicant',
         'An order has been made',
         'Mae gorchymyn wedi’i wneud',
-        '<p class="govuk-body">The judge has made an order related to the application. <a href="{GA_VIEW_APPLICATION_URL}" class="govuk-link">View the order</a> </p>',
-        '<p class="govuk-body">Mae''r barnwr wedi gwneud gorchymyn yn ymwneud â''r cais. <a href="{GA_VIEW_APPLICATION_URL}" class="govuk-link">Gweld y gorchymyn</a> </p>',
+        '<p class="govuk-body">The judge has made an order related to the application. <a href="{GA_VIEW_APPLICATION_URL}" class="govuk-link">View the order</a></p>',
+        '<p class="govuk-body">Mae''r barnwr wedi gwneud gorchymyn yn ymwneud â''r cais. <a href="{GA_VIEW_APPLICATION_URL}" class="govuk-link">Gweld y gorchymyn</a></p>',
         'APPLICANT');
 
 INSERT INTO dbs.dashboard_notifications_templates ( template_name, title_En, title_Cy, description_En, description_Cy
@@ -38,6 +38,6 @@ INSERT INTO dbs.dashboard_notifications_templates ( template_name, title_En, tit
 VALUES ('Notice.AAA6.GeneralApps.OrderMade.Respondent',
         'An order has been made',
         'Mae gorchymyn wedi’i wneud',
-        '<p class="govuk-body">The judge has made an order related to the application. <a href="{GA_RESPONSE_VIEW_APPLICATION_URL}" class="govuk-link">View the order</a> </p>',
-        '<p class="govuk-body">Mae''r barnwr wedi gwneud gorchymyn yn ymwneud â''r cais. <a href="{GA_RESPONSE_VIEW_APPLICATION_URL}" class="govuk-link">Gweld y gorchymyn</a> </p>',
+        '<p class="govuk-body">The judge has made an order related to the application. <a href="{GA_RESPONSE_VIEW_APPLICATION_URL}" class="govuk-link">View the order</a></p>',
+        '<p class="govuk-body">Mae''r barnwr wedi gwneud gorchymyn yn ymwneud â''r cais. <a href="{GA_RESPONSE_VIEW_APPLICATION_URL}" class="govuk-link">Gweld y gorchymyn</a></p>',
         'RESPONDENT');
