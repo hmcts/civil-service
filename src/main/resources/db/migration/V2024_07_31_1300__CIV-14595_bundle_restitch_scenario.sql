a80f121620ba7bb0a53879f8a93f579d1f076a90
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-09-20 11:29:01 +0100

    Moving the sql updates to a new file

--- src/main/resources/db/migration/V2024_07_31_1300__CIV-14595_bundle_restitch_scenario.sql
+++ src/main/resources/db/migration/V2024_07_31_1300__CIV-14595_bundle_restitch_scenario.sql
@@ -7,12 +7,6 @@ VALUES ('Scenario.AAA6.CP.Bundle.Updated.Claimant',
         '{"Notice.AAA6.CP.Bundle.Updated.Claimant": ["bundleRestitchedDateEn", "bundleRestitchedDateCy"]}'),
        ('Scenario.AAA6.CP.Bundle.Updated.Defendant',
         '{"Notice.AAA6.CP.Bundle.Ready.Defendant", "Notice.AAA6.CP.Trial.Arrangements.Finalised.NotifyOtherParty.Defendant", "Notice.AAA6.CP.Trial.Arrangements.Required.Defendant"}',
-        '{"Notice.AAA6.CP.Bundle.Updated.Defendant": ["bundleRestitchedDateEn", "bundleRestitchedDateCy"]}'),
-       ('Scenario.AAA6.CP.Bundle.Updated.TrialReady.Claimant',
-        '{"Notice.AAA6.CP.Bundle.Ready.Claimant", "Notice.AAA6.CP.Trial.Arrangements.Finalised.NotifyOtherParty.Claimant", "Notice.AAA6.CP.Trial.Arrangements.Required.Claimant"}',
-        '{"Notice.AAA6.CP.Bundle.Updated.Claimant": ["bundleRestitchedDateEn", "bundleRestitchedDateCy"]}'),
-       ('Scenario.AAA6.CP.Bundle.Updated.TrialReady.Defendant',
-        '{"Notice.AAA6.CP.Bundle.Ready.Defendant", "Notice.AAA6.CP.Trial.Arrangements.Finalised.NotifyOtherParty.Defendant", "Notice.AAA6.CP.Trial.Arrangements.Required.Defendant"}',
         '{"Notice.AAA6.CP.Bundle.Updated.Defendant": ["bundleRestitchedDateEn", "bundleRestitchedDateCy"]}');
 
 /**
@@ -32,30 +26,3 @@ VALUES ('Notice.AAA6.CP.Bundle.Updated.Claimant',
         '<p class="govuk-body">The case bundle was changed and re-uploaded on ${bundleRestitchedDateEn}. <a href="{VIEW_BUNDLE_REDIRECT}" class="govuk-link">Review the new bundle</a>.</p>',
         '<p class="govuk-body">Cafodd bwndel yr achos ei newid a''i ail-uwchlwytho ar ${bundleRestitchedDateCy}. <a href="{VIEW_BUNDLE_REDIRECT}" class="govuk-link">Adolygu’r bwndel newydd</a>.</p>',
         'DEFENDANT', 'Session');
-
-
-INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
-                                    scenario_name, task_status_sequence, role, task_order)
-values ('<a href={VIEW_BUNDLE} class="govuk-link">View the bundle</a>',
-        'Hearing',
-        '<a href={VIEW_BUNDLE} class="govuk-link">Gweld y bwndel</a>',
-        'Gwrandawiad', 'Hearing.Bundle.View', 'Scenario.AAA6.CP.Bundle.Updated.Claimant', '{3, 3}', 'CLAIMANT', 13),
-       ('<a href={VIEW_BUNDLE} class="govuk-link">View the bundle</a>',
-        'Hearing',
-        '<a href={VIEW_BUNDLE} class="govuk-link">View the bundle</a>',
-        'Gwrandawiad', 'Hearing.Bundle.View', 'Scenario.AAA6.CP.Bundle.Updated.Defendant', '{3, 3}', 'DEFENDANT', 12),
-       ('<a href={VIEW_BUNDLE} class="govuk-link">View the bundle</a>',
-        'Hearing',
-        '<a href={VIEW_BUNDLE} class="govuk-link">Gweld y bwndel</a>',
-        'Gwrandawiad', 'Hearing.Bundle.View', 'Scenario.AAA6.CP.Bundle.Updated.TrialReady.Claimant', '{3, 3}', 'CLAIMANT', 13),
-       ('<a href={VIEW_BUNDLE} class="govuk-link">View the bundle</a>',
-        'Hearing',
-        '<a href={VIEW_BUNDLE} class="govuk-link">Gweld y bwndel</a>',
-        'Gwrandawiad', 'Hearing.Bundle.View', 'Scenario.AAA6.CP.Bundle.Updated.TrialReady.Defendant', '{3, 3}', 'DEFENDANT', 12),
-       ('<a>Add the trial arrangements</a>', 'Hearing' ,'<a>Ychwanegu trefniadau''r treial</a>',
-        'Gwrandawiad', 'Hearing.Arrangements.Add', 'Scenario.AAA6.CP.Bundle.Updated.Claimant', '{2, 2}', 'CLAIMANT', 12),
-       ('<a>Add the trial arrangements</a>', 'Hearing' ,'<a>Ychwanegu trefniadau''r treial</a>',
-        'Gwrandawiad', 'Hearing.Arrangements.Add', 'Scenario.AAA6.CP.Bundle.Updated.Defendant', '{2, 2}', 'DEFENDANT', 11);
-
-
-
