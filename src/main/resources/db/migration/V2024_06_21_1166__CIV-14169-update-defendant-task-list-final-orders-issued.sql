d096abfd99056f193b9188d0fb8481ffdc5aa2b8
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2024-09-20 12:24:26 +0100

    CIV-15258 fix bug

--- src/main/resources/db/migration/V2024_06_21_1166__CIV-14169-update-defendant-task-list-final-orders-issued.sql
+++ src/main/resources/db/migration/V2024_06_21_1166__CIV-14169-update-defendant-task-list-final-orders-issued.sql
@@ -2,7 +2,7 @@
  * Add scenario
  */
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
-VALUES ('Scenario.AAA6.Update.Defendant.TaskList.UploadDocuments.FinalOrders', '{}', '{"Notice.AAA6.CP.OrderMade.Defendant" : ["orderDocument"]}');
+VALUES ('Scenario.AAA6.Update.Defendant.TaskList.UploadDocuments.FinalOrders', '{"Notice.AAA6.CP.Trial.Arrangements.Required.Defendant"}', '{"Notice.AAA6.CP.OrderMade.Defendant" : ["orderDocument"]}');
 
 /**
  * Add task list items claimant
@@ -11,4 +11,6 @@ INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, cat
                                     scenario_name, task_status_sequence, role, task_order)
 VALUES ('<a>Upload hearing documents</a>', 'Hearing',
         '<a>Llwytho dogfennau''r gwrandawiad</a>', 'Gwrandawiad',
-        'Hearing.Document.Upload', 'Scenario.AAA6.Update.Defendant.TaskList.UploadDocuments.FinalOrders', '{2, 2}', 'DEFENDANT', 10);
+        'Hearing.Document.Upload', 'Scenario.AAA6.Update.Defendant.TaskList.UploadDocuments.FinalOrders', '{2, 2}', 'DEFENDANT', 10),
+       ('<a>Add the trial arrangements</a>', 'Hearing' ,'<a>Ychwanegu trefniadau''r treial</a>',
+        'Gwrandawiad', 'Hearing.Arrangements.Add', 'Scenario.AAA6.Update.Defendant.TaskList.UploadDocuments.FinalOrders', '{2, 2}', 'DEFENDANT', 12);
