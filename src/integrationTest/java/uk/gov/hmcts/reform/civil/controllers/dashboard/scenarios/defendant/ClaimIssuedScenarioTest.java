733e191f6b038abef86828da4e24f90cd1af18b2
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-09-24 11:09:01 +0100

    CIV-15072 test case updated

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/ClaimIssuedScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/ClaimIssuedScenarioTest.java
@@ -86,10 +86,12 @@ public class ClaimIssuedScenarioTest extends DashboardBaseIntegrationTest {
                 jsonPath("$[11].currentStatusEn").value("Available"),
                 jsonPath("$[12].taskNameEn").value("<a>View the judgment</a>"),
                 jsonPath("$[12].currentStatusEn").value("Not available yet"),
-                jsonPath("$[13].taskNameEn").value("<a href={GENERAL_APPLICATIONS_INITIATION_PAGE_URL} rel=\"noopener noreferrer\" class=\"govuk-link\">Contact the court to request a change to my case</a>"),
-                jsonPath("$[13].currentStatusEn").value("Optional"),
-                jsonPath("$[14].taskNameEn").value("<a>View applications</a>"),
-                jsonPath("$[14].currentStatusEn").value("Not available yet")
+                jsonPath("$[13].taskNameEn").value("<a>Confirm you've paid a judgment (CCJ) debt</a>"),
+                jsonPath("$[13].currentStatusEn").value("Not available yet"),
+                jsonPath("$[14].taskNameEn").value("<a href={GENERAL_APPLICATIONS_INITIATION_PAGE_URL} rel=\"noopener noreferrer\" class=\"govuk-link\">Contact the court to request a change to my case</a>"),
+                jsonPath("$[14].currentStatusEn").value("Optional"),
+                jsonPath("$[15].taskNameEn").value("<a>View applications</a>"),
+                jsonPath("$[15].currentStatusEn").value("Not available yet")
 
             );
 
