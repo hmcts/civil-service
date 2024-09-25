249acbdd356b3cf29738320d2c514522ae83655c
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2024-09-25 12:07:31 +0100

    CIV-15258 update db migration scripts

--- src/main/resources/db/migration/V2024_09_25_1042__CIV-15258-update-defendant-task-list-final-orders-issued.sql
+++ src/main/resources/db/migration/V2024_09_25_1042__CIV-15258-update-defendant-task-list-final-orders-issued.sql
@@ -1,4 +1,10 @@
 /**
+ * Update scenario with new notifications_to_delete
+ */
+UPDATE dbs.scenario
+SET notifications_to_delete = '{"Notice.AAA6.CP.Trial.Arrangements.Required.Defendant"}'
+WHERE name = 'Scenario.AAA6.Update.Defendant.TaskList.UploadDocuments.FinalOrders';
+/**
  * Add task list items claimant
  */
 INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
