684b05f388bea7831688abba66051e004d63a105
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2024-09-26 16:10:06 +0100

    CIV-15258 update integration tests

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/OrderMadeDefendantScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/OrderMadeDefendantScenarioTest.java
@@ -11,6 +11,7 @@ import uk.gov.hmcts.reform.civil.controllers.DashboardBaseIntegrationTest;
 import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
 import uk.gov.hmcts.reform.civil.documentmanagement.model.Document;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.enums.sdo.ClaimsTrack;
 import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.defendant.OrderMadeDefendantNotificationHandler;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.Mediation;
@@ -156,6 +157,51 @@ public class OrderMadeDefendantScenarioTest extends DashboardBaseIntegrationTest
             .legacyCaseReference("reference")
             .ccdCaseReference(Long.valueOf(caseId))
             .respondent1Represented(YesOrNo.NO)
+            .finalOrderDocumentCollection(List.of(ElementUtils.element(
+                CaseDocument.builder().documentLink(Document.builder().documentBinaryUrl("url").build()).build())))
+            .build();
+
+        when(featureToggleService.isCaseProgressionEnabled()).thenReturn(true);
+        handler.handle(callbackParamsTestFinalOrders(caseData));
+
+        //Verify Notification is created
+        doGet(BEARER_TOKEN, GET_NOTIFICATIONS_URL, caseId, "DEFENDANT")
+            .andExpect(status().isOk())
+            .andExpectAll(
+                status().is(HttpStatus.OK.value()),
+                jsonPath("$[0].titleEn").value("An order has been made"),
+                jsonPath("$[0].descriptionEn").value(
+                    "<p class=\"govuk-body\">The judge has made an order on your claim.</p>" +
+                        "<p class=\"govuk-body\"><a href=\"{VIEW_FINAL_ORDER}\" rel=\"noopener noreferrer\" " +
+                        "target=\"_blank\" class=\"govuk-link\">View the order</a></p>"),
+                jsonPath("$[0].titleCy").value("Mae gorchymyn wedi’i wneud"),
+                jsonPath("$[0].descriptionCy").value(
+                    "<p class=\"govuk-body\">Mae'r Barnwr wedi gwneud gorchymyn ar eich hawliad.</p>" +
+                        "<p class=\"govuk-body\"><a href=\"{VIEW_FINAL_ORDER}\" rel=\"noopener noreferrer\" " +
+                        "target=\"_blank\" class=\"govuk-link\">Gweld y gorchymyn</a></p>")
+            );
+
+        doGet(BEARER_TOKEN, GET_TASKS_ITEMS_URL, caseId, "DEFENDANT")
+            .andExpectAll(
+                status().is(HttpStatus.OK.value()),
+                jsonPath("$[1].reference").value(caseId.toString()),
+                jsonPath("$[1].taskNameEn").value("<a>Upload hearing documents</a>"),
+                jsonPath("$[1].currentStatusEn").value("Inactive")
+            );
+    }
+
+    @Test
+    void should_create_order_made_defendant_all_orders_issued_fast_track_not_ready_trial_scenario() throws Exception {
+
+        String caseId = "725984154548966";
+
+        CaseData caseData = CaseDataBuilder.builder().atStateRespondentPartAdmissionSpec().build()
+            .toBuilder()
+            .legacyCaseReference("reference")
+            .ccdCaseReference(Long.valueOf(caseId))
+            .respondent1Represented(YesOrNo.NO)
+            .claimsTrack(ClaimsTrack.fastTrack)
+            .drawDirectionsOrderRequired(YesOrNo.NO)
             .finalOrderDocumentCollection(List.of(ElementUtils.element(
                 CaseDocument.builder().documentLink(Document.builder().documentBinaryUrl("url").build()).build())))
             .build();
