ba616f70697223d1353655280b14807a778fb91a
laarmada <sergiosalmar@yahoo.es>
2024-09-27 10:48:05 +0100

    add sql update

--- src/main/resources/db/migration/V2024_09_27_1030__CIV-15312_update_hwf_notifications.sql
+++ src/main/resources/db/migration/V2024_09_27_1030__CIV-15312_update_hwf_notifications.sql
@@ -1,14 +1,11 @@
 /**
  * Update scenario
  */
-UPDATE dbs.scenario set notifications_to_delete = replace(notifications_to_delete,
-                                                          '{"Notice.AAA6.ClaimIssue.HWF.Requested","Notice.AAA6.ClaimIssue.HWF.InfoRequired","Notice.AAA6.CP.HearingFee.HWF.InfoRequired"}'
+UPDATE dbs.scenario set notifications_to_delete = '{"Notice.AAA6.ClaimIssue.HWF.Requested","Notice.AAA6.ClaimIssue.HWF.InfoRequired","Notice.AAA6.CP.HearingFee.HWF.InfoRequired"}'
                                                             WHERE name = 'Scenario.AAA6.ClaimIssue.HWF.PartRemission';
 
-UPDATE dbs.scenario set notifications_to_delete = replace(notifications_to_delete,
-                                                          '{"Notice.AAA6.ClaimIssue.HWF.Requested","Notice.AAA6.ClaimIssue.HWF.Updated","Notice.AAA6.ClaimIssue.HWF.InvalidRef","Notice.AAA6.ClaimIssue.HWF.InfoRequired","Notice.AAA6.CP.HearingFee.HWF.InfoRequired"}'
+UPDATE dbs.scenario set notifications_to_delete = '{"Notice.AAA6.ClaimIssue.HWF.Requested","Notice.AAA6.ClaimIssue.HWF.Updated","Notice.AAA6.ClaimIssue.HWF.InvalidRef","Notice.AAA6.ClaimIssue.HWF.InfoRequired","Notice.AAA6.CP.HearingFee.HWF.InfoRequired"}'
                                                             WHERE name = 'Scenario.AAA6.ClaimIssue.HWF.Rejected';
 
-UPDATE dbs.scenario set notifications_to_delete = replace(notifications_to_delete,
-                                                          '{"Notice.AAA6.CP.HearingFee.HWF.AppliedFor.Claimant","Notice.AAA6.ClaimIssue.HWF.InfoRequired","Notice.AAA6.CP.HearingFee.HWF.InfoRequired"}'
+UPDATE dbs.scenario set notifications_to_delete = '{"Notice.AAA6.CP.HearingFee.HWF.AppliedFor.Claimant","Notice.AAA6.ClaimIssue.HWF.InfoRequired","Notice.AAA6.CP.HearingFee.HWF.InfoRequired"}'
                                                             WHERE name = 'Scenario.AAA6.CP.HearingFee.HWF.PartRemission';
