a0edb4fb17998a6672f61eb6663f7a4bfb07b7b4
Velican <duzgunce.vd@gmail.com>
2024-09-27 17:13:43 +0100

    fix lombok "is" prefix being removed

--- src/main/java/uk/gov/hmcts/reform/civil/model/DefendantLinkStatus.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/DefendantLinkStatus.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,6 +8,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class DefendantLinkStatus {
 
+    @JsonProperty("isOcmcCase")
     private boolean isOcmcCase;
     private boolean linked;
 }
