0907a453d15b8cf394239637ff46fc37f10dbc48
Deepthi Doppalapudi <107422736+deepthidoppalapudihmcts@users.noreply.github.com>
2024-09-30 18:42:10 +0100

    CIV-14796 added one more test for coverage

--- src/test/java/uk/gov/hmcts/reform/civil/handler/tasks/UpdateFromGACaseEventTaskHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/tasks/UpdateFromGACaseEventTaskHandlerTest.java
@@ -667,6 +667,47 @@ public class UpdateFromGACaseEventTaskHandlerTest {
     }
 
     @Test
+    void testShouldAddGaDraftApplicationDocument_LipApplicantAndGaDraftsizelessthan1() {
+        when(mockExternalTask.getTopicName()).thenReturn("test");
+        when(featureToggleService.isGaForLipsEnabled()).thenReturn(true);
+        when(mockExternalTask.getAllVariables())
+            .thenReturn(Map.of(
+                "caseId", GENERAL_APP_CASE_ID,
+                "caseEvent", ADD_PDF_TO_MAIN_CASE,
+                "generalAppParentCaseLink", CIVIL_CASE_ID
+            ));
+
+        CaseData caseData = new CaseDataBuilder().atStateClaimDraft()
+            .applicant1Represented(YesOrNo.NO)
+            .businessProcess(BusinessProcess.builder().status(BusinessProcessStatus.READY).build())
+            .build();
+
+        CaseDetails caseDetails = CaseDetailsBuilder.builder().data(caseData).build();
+        StartEventResponse startEventResponse = startEventResponse(caseDetails);
+
+        CaseData generalCaseData = GeneralApplicationDetailsBuilder.builder()
+            .getTestCaseDataWithDraftApplicationPDFDocumentLip(CaseData.builder().build());
+
+        CaseData updatedCaseData = GeneralApplicationDetailsBuilder.builder()
+            .getTestCaseDataWithDraftStaffPDFDocument(CaseData.builder().build());
+
+        when(caseDetailsConverter.toGACaseData(coreCaseDataService.getCase(parseLong(GENERAL_APP_CASE_ID))))
+            .thenReturn(generalCaseData);
+
+        when(coreCaseDataService.startUpdate(CIVIL_CASE_ID, ADD_PDF_TO_MAIN_CASE)).thenReturn(startEventResponse);
+
+        when(caseDetailsConverter.toCaseData(startEventResponse.getCaseDetails())).thenReturn(caseData);
+
+        when(coreCaseDataService.submitUpdate(eq(CIVIL_CASE_ID), any(CaseDataContent.class))).thenReturn(updatedCaseData);
+
+        handler.execute(mockExternalTask, externalTaskService);
+
+        verify(coreCaseDataService).startUpdate(CIVIL_CASE_ID, ADD_PDF_TO_MAIN_CASE);
+        verify(coreCaseDataService).submitUpdate(eq(CIVIL_CASE_ID), any(CaseDataContent.class));
+        verify(externalTaskService).complete(mockExternalTask, null);
+    }
+
+    @Test
     void testShouldAddGaAdditionalDocument() {
         when(mockExternalTask.getTopicName()).thenReturn("test");
 
