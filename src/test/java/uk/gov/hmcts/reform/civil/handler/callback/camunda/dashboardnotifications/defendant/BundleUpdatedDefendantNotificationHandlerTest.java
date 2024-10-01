7c7c34d9109edc60160bf05e09c99481d61a0728
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-10-01 13:26:47 +0100

    CIV-15204 fixing defendant test

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/BundleUpdatedDefendantNotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/BundleUpdatedDefendantNotificationHandlerTest.java
@@ -83,7 +83,7 @@ public class BundleUpdatedDefendantNotificationHandlerTest extends BaseCallbackH
 
             when(dashboardApiClient.recordScenario(any(), any(), anyString(), any())).thenReturn(ResponseEntity.of(
                 Optional.empty()));
-            when(toggleService.isCaseEventsEnabled()).thenReturn(true);
+            when(toggleService.isAmendBundleEnabled()).thenReturn(true);
 
             CaseData caseData = CaseDataBuilder.builder().atStateTrialReadyCheck().build().toBuilder()
                 .respondent1Represented(YesOrNo.NO)
