38662c7949f372bda1033ba898f1af226757fdff
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2024-10-02 11:02:04 +0100

    CIV-15227 stay lift notification

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/AbstractNotifyStayLiftedHandler.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.notification;
+
+import lombok.RequiredArgsConstructor;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.notify.NotificationService;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+public abstract class AbstractNotifyStayLiftedHandler extends CallbackHandler implements NotificationData {
+
+    protected final NotificationService notificationService;
+    protected final NotificationsProperties notificationsProperties;
+
+    public CallbackResponse sendNotification(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+
+        notificationService.sendMail(
+            getRecipient(callbackParams),
+            getNotificationTemplate(caseData),
+            addPropertiesForStayLifted(callbackParams),
+            String.format(getReferenceTemplate(), caseData.getCcdCaseReference())
+        );
+        return AboutToStartOrSubmitCallbackResponse.builder().build();
+    }
+
+    protected abstract String getReferenceTemplate();
+
+    protected abstract String getRecipient(CallbackParams callbackParams);
+
+    protected String getNotificationTemplate(CaseData caseData) {
+        if (isLiP(caseData)) {
+            // TODO: add lip template
+            return null;
+        } else {
+            return notificationsProperties.getNotifyLRStayLifted();
+        }
+    }
+
+    protected abstract boolean isLiP(CaseData caseData);
+
+    protected Map<String, String> addPropertiesForStayLifted(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        return Map.of(
+            CLAIM_REFERENCE_NUMBER, caseData.getCcdCaseReference().toString(),
+            PARTY_NAME, getPartyName(callbackParams)
+        );
+    }
+
+    @Override
+    public Map<String, String> addProperties(CaseData caseData) {
+        return null;
+    }
+
+    protected abstract String getPartyName(CallbackParams callbackParams);
+}
---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/NotifyDefendantStayLiftedHandlerTest.java
@@ -0,0 +1,104 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.notification;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.enums.dq.Language;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.IdamUserDetails;
+import uk.gov.hmcts.reform.civil.model.Party;
+import uk.gov.hmcts.reform.civil.model.citizenui.CaseDataLiP;
+import uk.gov.hmcts.reform.civil.model.citizenui.RespondentLiPResponse;
+import uk.gov.hmcts.reform.civil.notify.NotificationService;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_DEFENDANT2_STAY_LIFTED;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_DEFENDANT_STAY_LIFTED;
+
+@ExtendWith(MockitoExtension.class)
+class NotifyDefendantStayLiftedHandlerTest {
+
+    @Mock
+    private NotificationService notificationService;
+
+    @Mock
+    private NotificationsProperties notificationsProperties;
+
+    @InjectMocks
+    private NotifyDefendantStayLiftedHandler handler;
+
+    private CaseData caseData;
+
+    @BeforeEach
+    void setUp() {
+        caseData = CaseDataBuilder.builder().atStateClaimDetailsNotified()
+            .respondent1(Party.builder().individualFirstName("Jack").individualLastName("Jackson").type(Party.Type.INDIVIDUAL).build())
+            .respondent2(Party.builder().individualFirstName("Jim").individualLastName("Jameson").type(Party.Type.INDIVIDUAL).build())
+            .build();
+    }
+
+    static Stream<Arguments> provideCaseData() {
+        return Stream.of(
+            Arguments.of(false, NOTIFY_DEFENDANT_STAY_LIFTED),
+            Arguments.of(true, NOTIFY_DEFENDANT2_STAY_LIFTED)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCaseData")
+    void sendNotificationShouldSendEmail(boolean isDefendant2, CaseEvent caseEvent) {
+        caseData = caseData.toBuilder()
+            .respondentSolicitor1EmailAddress("defendant@hmcts.net")
+            .respondentSolicitor2EmailAddress("defendant2@hmcts.net")
+            .respondent1Represented(YesOrNo.YES)
+            .respondent2Represented(YesOrNo.YES)
+            .build();
+        CallbackParams params = CallbackParams.builder().caseData(caseData)
+            .request(CallbackRequest.builder().eventId(caseEvent.toString()).build()).build();
+
+        when(notificationsProperties.getNotifyLRStayLifted()).thenReturn("solicitor-template");
+
+        CallbackResponse response = handler.sendNotification(params);
+
+        if (isDefendant2) {
+            verify(notificationService).sendMail(
+                "defendant2@hmcts.net",
+                "solicitor-template",
+                Map.of(
+                    "claimReferenceNumber", "1594901956117591",
+                    "name", "Jim Jameson"
+                ),
+                "stay-lifted-defendant-notification-1594901956117591"
+            );
+        } else {
+            verify(notificationService).sendMail(
+                "defendant@hmcts.net",
+                "solicitor-template",
+                Map.of(
+                    "claimReferenceNumber", "1594901956117591",
+                    "name", "Jack Jackson"
+                ),
+                "stay-lifted-defendant-notification-1594901956117591"
+            );
+        }
+        assertNotNull(response);
+    }
+
+}
