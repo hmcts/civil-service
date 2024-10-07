c5696d2d2bd313241fdb56a559bd2aa815a0fe1f
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-10-07 15:02:38 +0100

    add handlers

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/CoscApplicationAfterPaymentTaskHandler.java
@@ -0,0 +1,78 @@
+package uk.gov.hmcts.reform.civil.handler.tasks;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.camunda.bpm.client.exception.ValueMapperException;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.util.StringUtils;
+import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
+import uk.gov.hmcts.reform.civil.documentmanagement.model.Document;
+import uk.gov.hmcts.reform.civil.exceptions.InvalidCaseDataException;
+import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.common.Element;
+import uk.gov.hmcts.reform.civil.model.genapplication.GADetailsRespondentSol;
+import uk.gov.hmcts.reform.civil.model.genapplication.GeneralApplicationsDetails;
+import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
+import uk.gov.hmcts.reform.civil.service.data.ExternalTaskInput;
+import uk.gov.hmcts.reform.civil.utils.CaseDataContentConverter;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static java.lang.Long.parseLong;
+import static java.util.Objects.isNull;
+import static java.util.Optional.ofNullable;
+
+@RequiredArgsConstructor
+@Component
+public class CoscApplicationAfterPaymentTaskHandler implements BaseExternalTaskHandler {
+
+    private final CoreCaseDataService coreCaseDataService;
+    private final CaseDetailsConverter caseDetailsConverter;
+    private final ObjectMapper mapper;
+
+    private CaseData civilCaseData;
+
+    @Override
+    public void handleTask(ExternalTask externalTask) {
+        try {
+            ExternalTaskInput variables = mapper.convertValue(externalTask.getAllVariables(), ExternalTaskInput.class);
+
+            String civilCaseId =
+                ofNullable(variables.getGeneralAppParentCaseLink())
+                    .orElseThrow(() -> new InvalidCaseDataException(
+                        "General application parent case link not found"));
+
+            StartEventResponse startEventResponse = coreCaseDataService.startUpdate(
+                civilCaseId,
+                variables.getCaseEvent()
+            );
+
+            civilCaseData = caseDetailsConverter.toCaseData(startEventResponse.getCaseDetails());
+
+            coreCaseDataService.submitUpdate(
+                civilCaseId,
+                CaseDataContentConverter.caseDataContentFromStartEventResponse(
+                    startEventResponse,
+                    civilCaseData.toMap(mapper)
+                )
+            );
+        } catch (NumberFormatException ne) {
+            throw new InvalidCaseDataException(
+                "Conversion to long datatype failed for general application for a case ", ne
+            );
+        } catch (IllegalArgumentException | ValueMapperException e) {
+            throw new InvalidCaseDataException("Mapper conversion failed due to incompatible types", e);
+        }
+    }
+
+}
---
+++ src/main/java/uk/gov/hmcts/reform/civil/service/tasklisteners/CoscApplicationAfterPaymentTaskListener.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.civil.service.tasklisteners;
+
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.client.topic.TopicSubscriptionBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.civil.handler.tasks.CoscApplicationAfterPaymentTaskHandler;
+import uk.gov.hmcts.reform.civil.handler.tasks.UpdateFromGACaseEventTaskHandler;
+
+@Component
+public class CoscApplicationAfterPaymentTaskListener {
+
+    private static final String TOPIC = "coscApplicationAfterPayment";
+
+    @Autowired
+    private CoscApplicationAfterPaymentTaskListener(CoscApplicationAfterPaymentTaskHandler coscApplicationAfterPaymentTaskHandler,
+                                                    ExternalTaskClient client) {
+        TopicSubscriptionBuilder subscriptionBuilder = client.subscribe(TOPIC);
+        subscriptionBuilder.handler(coscApplicationAfterPaymentTaskHandler).open();
+    }
+}
