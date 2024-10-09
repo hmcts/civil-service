bd86820d4063019b7af2920b0c3423616c3bc349
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2024-10-09 10:16:00 +0100

    Added process cosc application logic, events and handlers

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/CoscApplicationProcessorHandler.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.reform.civil.handler.tasks;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.civil.event.CoscApplicationProcessorEvent;
+import uk.gov.hmcts.reform.civil.service.search.CoscApplicationSearchService;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class CoscApplicationProcessorHandler implements BaseExternalTaskHandler {
+
+    private final CoscApplicationSearchService coscApplicationSearchService;
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    @Override
+    public void handleTask(ExternalTask externalTask) {
+        List<CaseDetails> cases = coscApplicationSearchService.getCases();
+        log.info("COSC Application Processor Job '{}' found {} case(s)", externalTask.getTopicName(), cases.size());
+
+        cases.forEach(caseDetails -> {
+            try {
+                log.info("Processing case caseId '{}'", caseDetails.getId());
+                applicationEventPublisher.publishEvent(new CoscApplicationProcessorEvent(caseDetails.getId()));
+            } catch (Exception e) {
+                log.error("COSC Application Processor Job failed to process case with id: '{}'", caseDetails.getId(), e);
+            }
+        });
+    }
+}
