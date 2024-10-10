08d1d2524e6888e5f3d3de4cf38d84f2aa93597e
sankaviv1 <sankavi.mohanraj@version1.com>
2024-10-10 11:01:17 +0100

    build fix

--- src/main/java/uk/gov/hmcts/reform/civil/model/dq/DocumentsToBeConsidered.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/dq/DocumentsToBeConsidered.java
@@ -1,13 +1,13 @@
 package uk.gov.hmcts.reform.civil.model.dq;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 
-@Builder(toBuilder = true)
+@SuperBuilder(toBuilder = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
