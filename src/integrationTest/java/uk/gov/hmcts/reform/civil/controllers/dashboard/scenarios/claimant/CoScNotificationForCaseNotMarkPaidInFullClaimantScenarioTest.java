a9a80513d0de8ecb66e750b6130596bffc91101d
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-10-14 09:40:11 +0100

    CIV-15082 changes

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/CoScNotificationForCaseNotMarkPaidInFullClaimantScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/CoScNotificationForCaseNotMarkPaidInFullClaimantScenarioTest.java
@@ -45,8 +45,8 @@ public class CoScNotificationForCaseNotMarkPaidInFullClaimantScenarioTest extend
                 jsonPath("$[0].descriptionEn").value(
                     "<p class=\"govuk-body\">The defendant who owed you money has told us that they paid in full on " + DateUtils.formatDate(
                         coscFullPaymentDate) +
-                        ". You should<a href=\"{CONFIRM_YOU_HAVE_BEEN_PAID_URL}\" class=\"govuk-link\">confirm if you've been paid</a>." +
-                        "<br>If you have not been paid, you will need to write to the court where the judgment was issued within a month from " + DateUtils.formatDate(
+                        ". You should <a href=\"{CONFIRM_YOU_HAVE_BEEN_PAID_URL}\" class=\"govuk-link\">confirm if you've been paid</a>." +
+                        "<br><br>If you have not been paid, you will need to write to the court where the judgment was issued within a month from " + DateUtils.formatDate(
                         coscFullPaymentDate) +
                         " so the application can be reviewed by a district judge. You can find the name of the court on the top right of the judgment." +
                         " You can then <u>search for the contact details of the court</u> to get the address.</p>"),
@@ -54,8 +54,8 @@ public class CoScNotificationForCaseNotMarkPaidInFullClaimantScenarioTest extend
                 jsonPath("$[0].descriptionCy").value(
                     "<p class=\"govuk-body\">Mae diffynnydd a oedd arnynt arian ichi wedi dweud wrthym eu bod wedi talu’n llawn ar " + DateUtils.formatDateInWelsh(
                         coscFullPaymentDate) +
-                        ". Dylech<a href=\"{CONFIRM_YOU_HAVE_BEEN_PAID_URL}\" class=\"govuk-link\">gadarnhau os ydych wedi cael eich talu</a>." +
-                        "<br>Os nad ydych wedi cael eich talu, bydd angen i chi ysgrifennu at y llys lle cyhoeddwyd y dyfarniad o fewn mis i " + DateUtils.formatDateInWelsh(
+                        ". Dylech <a href=\"{CONFIRM_YOU_HAVE_BEEN_PAID_URL}\" class=\"govuk-link\">gadarnhau os ydych wedi cael eich talu</a>." +
+                        "<br><br>Os nad ydych wedi cael eich talu, bydd angen i chi ysgrifennu at y llys lle cyhoeddwyd y dyfarniad o fewn mis i " + DateUtils.formatDateInWelsh(
                         coscFullPaymentDate) +
                         " fel y gall barnwr rhanbarth adolygu’r cais. Gallwch ddod o hyd i enw’r llys ar ochr dde uchaf y dyfarniad." +
                         " Yna gallwch <u>chwilio am fanylion cyswllt y llys</u> i gael y cyfeiriad.</p>"));
