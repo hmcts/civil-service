bfb200c43bdf6cf21eaffe5d291ca4abb449c855
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-10-15 09:02:56 +0100

    CIV-15080 change

--- src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/CoscApplicationAfterPaymentTaskHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/CoscApplicationAfterPaymentTaskHandler.java
@@ -4,14 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.client.exception.ValueMapperException;
 import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.exceptions.InvalidCaseDataException;
+import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 import uk.gov.hmcts.reform.civil.service.data.ExternalTaskInput;
+import uk.gov.hmcts.reform.civil.service.flowstate.IStateFlowEngine;
 
 import java.util.Map;
+import java.util.Objects;
 
 import static java.util.Optional.ofNullable;
 import static uk.gov.hmcts.reform.civil.utils.CaseDataContentConverter.caseDataContentFromStartEventResponse;
@@ -22,6 +27,8 @@ public class CoscApplicationAfterPaymentTaskHandler implements BaseExternalTaskH
 
     private final CoreCaseDataService coreCaseDataService;
     private final ObjectMapper mapper;
+    private final IStateFlowEngine stateFlowEngine;
+    private CaseData data;
 
     @Override
     public void handleTask(ExternalTask externalTask) {
@@ -37,7 +44,7 @@ public class CoscApplicationAfterPaymentTaskHandler implements BaseExternalTaskH
 
             StartEventResponse startEventResponse = coreCaseDataService.startUpdate(civilCaseId, caseEvent);
 
-            coreCaseDataService.submitUpdate(civilCaseId, caseDataContentFromStartEventResponse(startEventResponse, Map.of()));
+            data = coreCaseDataService.submitUpdate(civilCaseId, caseDataContentFromStartEventResponse(startEventResponse, Map.of()));
 
         } catch (NumberFormatException ne) {
             throw new InvalidCaseDataException(
@@ -46,5 +53,19 @@ public class CoscApplicationAfterPaymentTaskHandler implements BaseExternalTaskH
         } catch (IllegalArgumentException | ValueMapperException e) {
             throw new InvalidCaseDataException("Mapper conversion failed due to incompatible types", e);
         }
+    }
+
+    @Override
+    public VariableMap getVariableMap() {
+        VariableMap variables = Variables.createVariables();
+        var stateFlow = stateFlowEngine.evaluate(data);
+        variables.putValue(FLOW_STATE, stateFlow.getState().getName());
+        variables.putValue(FLOW_FLAGS, stateFlow.getFlags());
+        variables.putValue("isJudgmentMarkedPaidInFull", checkMarkPaidInFull(data));
+        return variables;
+    }
+
+    private boolean checkMarkPaidInFull(CaseData data) {
+        return (Objects.nonNull(data.getActiveJudgment()) && (data.getActiveJudgment().getFullyPaymentMadeDate() != null));
     }
 }
