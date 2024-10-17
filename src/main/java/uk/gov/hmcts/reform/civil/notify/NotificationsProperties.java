ebdfcac29516a317f49d7db7be41b6a03d86ace2
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2024-10-17 12:04:33 +0100

    CIV-15231 add properties

--- src/main/java/uk/gov/hmcts/reform/civil/notify/NotificationsProperties.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notify/NotificationsProperties.java
@@ -642,4 +642,10 @@ public class NotificationsProperties {
 
     @NotEmpty
     private String notifyLRStayUpdateRequested;
+
+    @NotEmpty
+    private String notifyLipStayUpdateRequested;
+
+    @NotEmpty
+    private String notifyLipBilingualStayUpdateRequested;
 }
