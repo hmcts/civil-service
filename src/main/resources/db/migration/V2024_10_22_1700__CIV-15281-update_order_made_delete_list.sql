835db0e3d4c897cfbbaac501e9e84c720c2b68d1
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-10-23 14:15:39 +0100

    CIV-15281 updating sql

--- src/main/resources/db/migration/V2024_10_22_1700__CIV-15281-update_order_made_delete_list.sql
+++ src/main/resources/db/migration/V2024_10_22_1700__CIV-15281-update_order_made_delete_list.sql
@@ -1,22 +1,6 @@
-/**
- * Add scenario
- */
-INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
-VALUES ('Scenario.AAA6.CP.OrderMade.Claimant', '{}', '{"Notice.AAA6.CP.OrderMade.Claimant" : ["orderDocument"]}');
-
-/**
- * Add notification template
- */
-INSERT INTO dbs.dashboard_notifications_templates (template_name, title_En, title_Cy, description_En, description_Cy
-                                                  ,notification_role, time_to_live)
-VALUES ('Notice.AAA6.CP.OrderMade.Claimant', 'An order has been made', 'Mae gorchymyn wedi’i wneud',
-        '<p class="govuk-body">The judge has made an order on your claim.</p><p class="govuk-body"><a href="{VIEW_FINAL_ORDER}" rel="noopener noreferrer" target="_blank" class="govuk-link">View the order</a></p>',
-        '<p class="govuk-body">Mae''r Barnwr wedi gwneud gorchymyn ar eich hawliad.</p><p class="govuk-body"><a href="{VIEW_FINAL_ORDER}" rel="noopener noreferrer" target="_blank" class="govuk-link">Gweld y gorchymyn</a></p>',
-        'CLAIMANT', 'Session');
-
 
 update dbs.scenario set notifications_to_delete = '{"Notice.AAA6.CP.ReviewOrderRequestedbyOtherParty.Claimant", "Notice.AAA6.CP.SDOMadebyLA.Claimant", "Notice.AAA6.CP.ReviewOrderRequestedbyRecipient.Recipient.Claimant", ""}'
                     where name = 'Scenario.AAA6.CP.OrderMade.Claimant';
 
 update dbs.scenario set notifications_to_delete = '{"Notice.AAA6.CP.ReviewOrderRequestedbyOtherParty.Defendant", "Notice.AAA6.CP.SDOMadebyLA.Defendant", "Notice.AAA6.CP.ReviewOrderRequestedbyRecipient.Recipient.Defendant"}'
-where name = 'Scenario.AAA6.CP.OrderMade.Claimant';
+where name = 'Scenario.AAA6.CP.OrderMade.Defendant';
