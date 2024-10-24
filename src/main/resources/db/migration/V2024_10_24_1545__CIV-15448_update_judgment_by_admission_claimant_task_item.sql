76da7b86c08e93a4332511e115b6a0fcbdbb5ac7
kalachandrasekar1 <114995593+kalachandrasekar1@users.noreply.github.com>
2024-10-24 17:23:18 +0100

    CIV-15448 CoSC - Add guidelines link (#5700)
    * CIV-15301 deleting all the previous notifications when case goes offline
    
    * CIV-15301 updating tests
    
    * CIV-15301 rename method
    
    * CIV-15301 case proceeds in caseman change to delete notifications and update task lists
    
    * CIV-15448 remove cosc task item for claimant
    
    * CIV-15448 - db script name update
    
    * CIV-15448 merge issue fixed
    
    ---------
    
    Co-authored-by: VijayaKumarK <kannan.vijayakumar@hmcts.net>
    Co-authored-by: kannan-v-hmcts <148557022+kannan-v-hmcts@users.noreply.github.com>
    Co-authored-by: mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>

---
+++ src/main/resources/db/migration/V2024_10_24_1545__CIV-15448_update_judgment_by_admission_claimant_task_item.sql
@@ -0,0 +1,6 @@
+
+/**
+ * Update task item template
+ */
+delete from dbs.task_item_template where scenario_name = 'Scenario.AAA6.JudgmentsOnline.IssuedCCJ.Claimant'
+  and template_name = 'Judgment.Cosc';
