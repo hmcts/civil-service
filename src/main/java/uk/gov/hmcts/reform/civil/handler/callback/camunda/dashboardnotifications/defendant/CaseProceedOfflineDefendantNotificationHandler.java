83c9b18586e9618a8ad2fdf6f473f1cb018c3266
kalachandrasekar1 <114995593+kalachandrasekar1@users.noreply.github.com>
2024-10-28 15:55:32 +0000

    CIV-15278 checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/CaseProceedOfflineDefendantNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/CaseProceedOfflineDefendantNotificationHandler.java
@@ -52,7 +52,7 @@ public class CaseProceedOfflineDefendantNotificationHandler extends DashboardCal
     @Override
     public String getScenario(CaseData caseData) {
         if (featureToggleService.isCoSCEnabled() && caseData.getActiveJudgment() != null) {
-            if(caseData.isFastTrackClaim()) {
+            if (caseData.isFastTrackClaim()) {
                 return SCENARIO_AAA6_UPDATE_CASE_PROCEED_IN_CASE_MAN_DEFENDANT_FAST_TRACK.getScenario();
             }
             return SCENARIO_AAA6_UPDATE_CASE_PROCEED_IN_CASE_MAN_DEFENDANT.getScenario();
