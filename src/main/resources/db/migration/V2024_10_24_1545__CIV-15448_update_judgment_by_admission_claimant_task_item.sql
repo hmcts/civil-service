aa67e6a8b1441ae9fba3df9103a63984e0acb6bc
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-10-29 09:26:53 +0000

    DB script updated

--- src/main/resources/db/migration/V2024_10_24_1545__CIV-15448_update_judgment_by_admission_claimant_task_item.sql
+++ src/main/resources/db/migration/V2024_10_24_1545__CIV-15448_update_judgment_by_admission_claimant_task_item.sql
@@ -1,6 +1,15 @@
+/**
+* Delete task list references
+*/
+DELETE FROM dbs.task_list
+WHERE task_item_template_id IN (
+  SELECT id FROM dbs.task_item_template
+            WHERE (template_name = 'Judgment.Cosc' AND
+                   scenario_name = 'Scenario.AAA6.JudgmentsOnline.IssuedCCJ.Claimant')
+  );
 
 /**
- * Update task item template
+ * Delete task item
  */
-delete from dbs.task_item_template where scenario_name = 'Scenario.AAA6.JudgmentsOnline.IssuedCCJ.Claimant'
-  and template_name = 'Judgment.Cosc';
+DELETE FROM dbs.task_item_template WHERE scenario_name = 'Scenario.AAA6.JudgmentsOnline.IssuedCCJ.Claimant'
+  AND template_name = 'Judgment.Cosc';
