95b30aa04796b97ca2ccc78149c1497633a5735e
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-11-11 11:37:42 +0000

    CIV-15647 updating tests

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/CreateSDORespondent1NotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/CreateSDORespondent1NotificationHandlerTest.java
@@ -25,6 +25,7 @@ import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.sampledata.PartyBuilder;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.service.OrganisationService;
+import uk.gov.hmcts.reform.civil.utils.PartyUtils;
 
 import java.util.Map;
 import java.util.Optional;
@@ -34,10 +35,11 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIMANT_V_DEFENDANT;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIM_LEGAL_ORG_NAME_SPEC;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIM_REFERENCE_NUMBER;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.PARTY_NAME;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.PARTY_REFERENCES;
-import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.RESPONDENT_NAME;
 
 @ExtendWith(MockitoExtension.class)
 class CreateSDORespondent1NotificationHandlerTest extends BaseCallbackHandlerTest {
@@ -205,7 +207,8 @@ class CreateSDORespondent1NotificationHandlerTest extends BaseCallbackHandlerTes
         private Map<String, String> getNotificationDataLipMap(CaseData caseData) {
             return Map.of(
                 CLAIM_REFERENCE_NUMBER, CASE_ID.toString(),
-                RESPONDENT_NAME, caseData.getRespondent1().getPartyName()
+                PARTY_NAME, caseData.getRespondent1().getPartyName(),
+                CLAIMANT_V_DEFENDANT, PartyUtils.getAllPartyNames(caseData)
             );
         }
 
@@ -213,8 +216,8 @@ class CreateSDORespondent1NotificationHandlerTest extends BaseCallbackHandlerTes
         private Map<String, String> getNotificationDataLipMap1(CaseData caseData) {
             return Map.of(
                 CLAIM_REFERENCE_NUMBER, CASE_ID.toString(),
-                CLAIM_LEGAL_ORG_NAME_SPEC, caseData.getRespondent1().getPartyName(),
-                PARTY_REFERENCES, "Claimant reference: 12345 - Defendant reference: 6789"
+                PARTY_NAME, caseData.getRespondent1().getPartyName(),
+                CLAIMANT_V_DEFENDANT, PartyUtils.getAllPartyNames(caseData)
             );
         }
     }
