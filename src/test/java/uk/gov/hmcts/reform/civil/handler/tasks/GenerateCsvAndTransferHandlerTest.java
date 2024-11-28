4dbe8fcc72cb131837389012ee40cb4ad44c90a0
sankaviv1 <sankavi.mohanraj@version1.com>
2024-11-28 11:08:19 +0000

    fix tests

--- src/test/java/uk/gov/hmcts/reform/civil/handler/tasks/GenerateCsvAndTransferHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/tasks/GenerateCsvAndTransferHandlerTest.java
@@ -29,6 +29,7 @@ import java.util.Map;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
