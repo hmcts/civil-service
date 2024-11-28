852ef46ac257f0bb3f15d5706ff0a40041088738
LiaoP <pijian.liao@hmcts.net>
2024-11-28 19:16:13 +0000

    CIV-15958 update test

--- src/test/java/uk/gov/hmcts/reform/civil/stateflow/transitions/ClaimSubmittedTransitionBuilderTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/stateflow/transitions/ClaimSubmittedTransitionBuilderTest.java
@@ -11,10 +11,14 @@ import uk.gov.hmcts.reform.civil.model.citizenui.CaseDataLiP;
 import uk.gov.hmcts.reform.civil.model.citizenui.HelpWithFees;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
+import uk.gov.hmcts.reform.civil.service.flowstate.FlowFlag;
 import uk.gov.hmcts.reform.civil.stateflow.model.Transition;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static java.util.Map.entry;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -66,6 +70,23 @@ public class ClaimSubmittedTransitionBuilderTest {
         assertTransition(result.get(3), "MAIN.CLAIM_SUBMITTED", "MAIN.PENDING_CLAIM_ISSUED_UNREPRESENTED_DEFENDANT_ONE_V_ONE_SPEC");
         assertTransition(result.get(4), "MAIN.CLAIM_SUBMITTED", "MAIN.PENDING_CLAIM_ISSUED_UNREPRESENTED_DEFENDANT_ONE_V_ONE_SPEC");
         assertTransition(result.get(5), "MAIN.CLAIM_SUBMITTED", "MAIN.PENDING_CLAIM_ISSUED_UNREPRESENTED_DEFENDANT_ONE_V_ONE_SPEC");
+    }
+
+    @Test
+    void shouldGoNocRoute_whenDefendantNoCOnline() {
+        when(mockFeatureToggleService.isDefendantNoCOnline()).thenReturn(true);
+        when(mockFeatureToggleService.isPinInPostEnabled()).thenReturn(true);
+        ClaimSubmittedTransitionBuilder claimSubmittedTransitionBuilder = new ClaimSubmittedTransitionBuilder(
+            mockFeatureToggleService);
+        result = claimSubmittedTransitionBuilder.buildTransitions();
+
+        CaseData caseData = CaseDataBuilder.builder().atStateClaimIssuedPaymentFailed().build();
+        assertTrue(paymentFailed.test(caseData));
+        assertThat(getCaseFlags(result.get(3), caseData)).hasSize(3).contains(
+            entry(FlowFlag.LIP_CASE.name(), true),
+            entry(FlowFlag.UNREPRESENTED_DEFENDANT_ONE.name(), true),
+            entry(FlowFlag.PIP_ENABLED.name(), true)
+        );
     }
 
     @Test
@@ -145,5 +166,11 @@ public class ClaimSubmittedTransitionBuilderTest {
     private void assertTransition(Transition transition, String sourceState, String targetState) {
         assertThat(transition.getSourceState()).isEqualTo(sourceState);
         assertThat(transition.getTargetState()).isEqualTo(targetState);
+    }
+
+    private Map<String, Boolean> getCaseFlags(Transition result, CaseData caseData) {
+        Map<String, Boolean> flags = new HashMap<>();
+        result.getDynamicFlags().accept(caseData, flags);
+        return flags;
     }
 }
