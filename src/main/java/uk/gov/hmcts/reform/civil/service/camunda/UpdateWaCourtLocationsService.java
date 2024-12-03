c7673cc0bc677313a695fc191b116bb759f0bc2d
drummondjm <mark.drummond@version1.com>
2024-12-03 18:10:35 +0000

    Add conditional property for UpdateWaCourtLocationsService

--- src/main/java/uk/gov/hmcts/reform/civil/service/camunda/UpdateWaCourtLocationsService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/camunda/UpdateWaCourtLocationsService.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 
