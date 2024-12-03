5c9a1f76ddd2c40a492580f9db008a9a9c9fa32a
Anirudha <anirudha.com@gmail.com>
2024-12-03 09:03:21 +0000

    DTSCCI-1250: dedupe multiple WA tasks

--- src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/CvpJoinLinkSchedulerHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/CvpJoinLinkSchedulerHandler.java
@@ -10,7 +10,8 @@ import uk.gov.hmcts.reform.civil.event.CvpJoinLinkEvent;
 import uk.gov.hmcts.reform.civil.model.ExternalTaskData;
 import uk.gov.hmcts.reform.civil.service.search.CaseHearingDateSearchService;
 
-import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,9 +23,8 @@ public class CvpJoinLinkSchedulerHandler extends BaseExternalTaskHandler {
 
     @Override
     public ExternalTaskData handleTask(ExternalTask externalTask) {
-        List<CaseDetails> cases = searchService.getCases();
-        List<Long> ids = cases.stream().map(CaseDetails::getId).sorted().toList();
-        log.info("CVP Join Link Scheduler job '{}' found {} case(s) with ids {}", externalTask.getTopicName(), cases.size(), ids);
+        Set<CaseDetails> cases = searchService.getCases();
+        log.info("CVP Join Link Scheduler job '{}' found {} case(s)", externalTask.getTopicName(), cases.size());
 
         cases.forEach(caseDetails -> {
             try {
