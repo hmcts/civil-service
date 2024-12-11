fcb236f7e20155b89439be924d20158fc46c9d65
neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
2024-12-11 16:36:03 +0000

    Added update script to delete the notification.

---
+++ src/main/resources/db/migration/V2024_12_11_1205__CIV-16312-update_respondent_response_submit_delete_list_1.sql
@@ -0,0 +1,3 @@
+
+update dbs.scenario set notifications_to_delete = '{"Notice.AAA6.GeneralApps.NonUrgentApplicationUncloaked.Respondent"}'
+                    where name = 'Scenario.AAA6.GeneralApps.RespondentResponseSubmitted.Respondent';
