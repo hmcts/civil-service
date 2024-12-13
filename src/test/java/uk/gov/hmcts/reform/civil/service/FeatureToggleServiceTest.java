59c1dc93d6e888737680b11e9b36909dab7a7b87
paul-pearson <paul.pearson@version1.com>
2024-12-13 14:10:13 +0000

    CIV-15956 Remove unused imports

--- src/test/java/uk/gov/hmcts/reform/civil/service/FeatureToggleServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/FeatureToggleServiceTest.java
@@ -13,7 +13,6 @@ import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
--- src/test/java/uk/gov/hmcts/reform/civil/stateflow/transitions/ClaimSubmittedTransitionBuilderTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/stateflow/transitions/ClaimSubmittedTransitionBuilderTest.java
@@ -24,7 +24,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.stateflow.transitions.ClaimSubmittedTransitionBuilder.claimIssueBilingual;
 import static uk.gov.hmcts.reform.civil.stateflow.transitions.ClaimSubmittedTransitionBuilder.claimIssueHwF;
