a3ca05b1ba99f7f956b5a43971abab04dec22b36
rishikrsharma <rishikrsharma@users.noreply.github.com>
2024-12-18 14:02:28 +0000

    Revert "DTSCCI-1183: cover letter - amend stitch process (#5917)" (#5938)
    This reverts commit e49e6f90a4d2f900cd761664e9d6abd675eca93a.

--- src/main/java/uk/gov/hmcts/reform/civil/service/SendFinalOrderBulkPrintService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/SendFinalOrderBulkPrintService.java
@@ -12,7 +12,6 @@ import uk.gov.hmcts.reform.civil.service.docmosis.CoverLetterAppendService;
 import java.util.List;
 
 import static java.util.Objects.nonNull;
-import static uk.gov.hmcts.reform.civil.documentmanagement.model.DocumentType.JUDGE_FINAL_ORDER;
 import static uk.gov.hmcts.reform.civil.documentmanagement.model.DocumentType.ORDER_NOTICE_TRANSLATED_DOCUMENT;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.caseevents.SendFinalOrderToLiPCallbackHandler.TASK_ID_DEFENDANT;
 
@@ -47,7 +46,7 @@ public class SendFinalOrderBulkPrintService {
         byte[] letterContent;
         Party recipient = isDefendantPrint(task) ? caseData.getRespondent1() : caseData.getApplicant1();
 
-        letterContent = coverLetterAppendService.makeDocumentMailable(caseData, authorisation, recipient, JUDGE_FINAL_ORDER, caseDocument);
+        letterContent = coverLetterAppendService.makeDocumentMailable(caseData, authorisation, recipient, caseDocument);
 
         List<String> recipients = List.of(recipient.getPartyName());
         bulkPrintService.printLetter(letterContent, caseData.getLegacyCaseReference(),
