8e06d4f6bbc47d6a34e145842355fbc5de9e3551
Azam <muhammad.ghulam@hmcts.net>
2025-01-02 15:54:18 +0000

    CIV-15686 removed initialaser

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateClaimCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateClaimCallbackHandler.java
@@ -52,7 +52,6 @@ import uk.gov.hmcts.reform.civil.utils.AssignCategoryId;
 import uk.gov.hmcts.reform.civil.utils.CaseFlagsInitialiser;
 import uk.gov.hmcts.reform.civil.utils.CaseNameUtils;
 import uk.gov.hmcts.reform.civil.utils.CourtLocationUtils;
-import uk.gov.hmcts.reform.civil.utils.CaseQueriesInitializer;
 import uk.gov.hmcts.reform.civil.utils.OrgPolicyUtils;
 import uk.gov.hmcts.reform.civil.validation.DateOfBirthValidator;
 import uk.gov.hmcts.reform.civil.validation.OrgPolicyValidator;
@@ -145,7 +144,6 @@ public class CreateClaimCallbackHandler extends CallbackHandler implements Parti
     private final CourtLocationUtils courtLocationUtils;
     private final AssignCategoryId assignCategoryId;
     private final CaseFlagsInitialiser caseFlagInitialiser;
-    private final CaseQueriesInitializer caseQueriesInitializer;
     private final ToggleConfiguration toggleConfiguration;
     private final String caseDocLocation = "/cases/case-details/%s#CaseDocuments";
     private final PartyValidator partyValidator;
@@ -564,7 +562,6 @@ public class CreateClaimCallbackHandler extends CallbackHandler implements Parti
         dataBuilder.caseNamePublic(CaseNameUtils.buildCaseName(caseData));
 
         caseFlagInitialiser.initialiseCaseFlags(CREATE_CLAIM, dataBuilder);
-        caseQueriesInitializer.initialiseCaseQueries(dataBuilder);
 
         dataBuilder.ccdState(CaseState.PENDING_CASE_ISSUED);
 
