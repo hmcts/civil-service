5f8e12f9c6c5e5a6fc5d7557fd8a620a4e36cd0d
kannan-v-hmcts <148557022+kannan-v-hmcts@users.noreply.github.com>
2025-01-07 09:21:46 +0000

    CIV-15265 Order Review Obligation Check (#5887)
    * add confirm order review
    * add confirm order review
    * add toggle
    * fix sonar coverage
    ---------
    Co-authored-by: laarmada <sergiosalmar@yahoo.es>
    Co-authored-by: mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
    Co-authored-by: meulendijksm <max.meulendijks@hmcts.net>
    Co-authored-by: m-meulendijks-v1 <107135537+m-meulendijks-v1@users.noreply.github.com>
    Co-authored-by: miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
    Co-authored-by: Miguel Molina <93722947+miguelMolina3691@users.noreply.github.com>
    Co-authored-by: Harry H <33700332+HarryH96@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/reform/civil/service/search/OrderReviewObligationSearchService.java
@@ -0,0 +1,73 @@
+package uk.gov.hmcts.reform.civil.service.search;
+
+import org.elasticsearch.index.query.QueryBuilder;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.civil.enums.CaseState;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.model.search.Query;
+import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.existsQuery;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static org.elasticsearch.index.query.QueryBuilders.rangeQuery;
+import static org.elasticsearch.index.query.QueryBuilders.termQuery;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.AWAITING_APPLICANT_INTENTION;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.AWAITING_RESPONDENT_ACKNOWLEDGEMENT;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.All_FINAL_ORDERS_ISSUED;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.CASE_PROGRESSION;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.DECISION_OUTCOME;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.HEARING_READINESS;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.IN_MEDIATION;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.JUDICIAL_REFERRAL;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.PREPARE_FOR_HEARING_CONDUCT_HEARING;
+
+@Service
+public class OrderReviewObligationSearchService extends ElasticSearchService {
+
+    public OrderReviewObligationSearchService(CoreCaseDataService coreCaseDataService) {
+        super(coreCaseDataService);
+    }
+
+    public Query query(int startIndex) {
+        return new Query(
+            boolQuery()
+                .must(boolQuery()
+                          .must(existsQuery("data.storedObligationData"))
+                          .must(rangeQuery("data.storedObligationData.value.obligationDate")
+                                    .lte(LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE)))
+                          .must(termQuery("data.storedObligationData.value.obligationWATaskRaised", YesOrNo.NO))
+                          .must(boolQuery()
+                                    .minimumShouldMatch(1)
+                                    .should(beState(PREPARE_FOR_HEARING_CONDUCT_HEARING))
+                                    .should(beState(HEARING_READINESS))
+                                    .should(beState(AWAITING_APPLICANT_INTENTION))
+                                    .should(beState(AWAITING_RESPONDENT_ACKNOWLEDGEMENT))
+                                    .should(beState(DECISION_OUTCOME))
+                                    .should(beState(IN_MEDIATION))
+                                    .should(beState(All_FINAL_ORDERS_ISSUED))
+                                    .should(beState(JUDICIAL_REFERRAL))
+                                    .should(beState(CASE_PROGRESSION))
+                          )
+                ),
+            List.of("reference"),
+            startIndex
+        );
+    }
+
+    @Override
+    Query queryInMediationCases(int startIndex, LocalDate claimMovedDate, boolean carmEnabled, boolean initialSearch,
+                                String searchAfterValue) {
+        return null;
+    }
+
+    private QueryBuilder beState(CaseState caseState) {
+        return boolQuery()
+            .must(matchQuery("state", caseState.toString()));
+    }
+
+}
