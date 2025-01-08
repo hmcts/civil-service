acfc98fee4cd4176860b72462f529f54029d8f21
Mike Lemos <gobii@me.com>
2025-01-08 16:48:36 +0000

    DTSCCI-1053 removing requestData from global exception handling

--- src/main/java/uk/gov/hmcts/reform/civil/utils/CaseDataRequestUtil.java
+++ src/main/java/uk/gov/hmcts/reform/civil/utils/CaseDataRequestUtil.java
@@ -2,12 +2,19 @@ package uk.gov.hmcts.reform.civil.utils;
 
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
+import java.nio.charset.StandardCharsets;
+
+import static uk.gov.hmcts.reform.civil.utils.JsonUtil.getValueByKey;
+
 public class CaseDataRequestUtil {
 
     private CaseDataRequestUtil() {
     }
 
     public static String getCaseId(ContentCachingRequestWrapper requestBody) {
-        return "";
+        return requestBody != null ? getValueByKey(new String(
+            requestBody.getContentAsByteArray(),
+            StandardCharsets.UTF_8
+        ), "id") : "";
     }
 }
