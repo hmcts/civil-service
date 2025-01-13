91fb9ded1121a8c828e12b26d14fcaf9ee15e8ec
rishisharma <rishi.sharma@hmcts.net>
2025-01-13 13:00:27 +0000

    DTSCCI-650: update unspec claim default court location to cnbc

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateClaimCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateClaimCallbackHandlerTest.java
@@ -1972,7 +1972,7 @@ class CreateClaimCallbackHandlerTest extends BaseCallbackHandlerTest {
                 assertThat(response.getData())
                     .extracting("caseManagementLocation")
                     .extracting("region", "baseLocation")
-                    .containsExactly("4", "192280");
+                    .containsExactly("2", "420219");
             }
         }
 
