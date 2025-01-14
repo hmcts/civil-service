74b2f96d78e5cff9d8a4c68be9f4bbce6500a6fd
EllisD-B <ellis.domengerboyce@hmcts.net>
2025-01-14 09:46:58 +0000

    message handler

--- src/main/java/uk/gov/hmcts/reform/civil/handler/message/AddCaseNoteMessageHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/message/AddCaseNoteMessageHandler.java
@@ -3,15 +3,21 @@ package uk.gov.hmcts.reform.civil.handler.message;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.model.message.CcdEventMessage;
+import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.ADD_CASE_NOTE;
-
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_RPA_ON_CONTINUOUS_FEED;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class AddCaseNoteMessageHandler implements CcdEventMessageHandler {
+
+    private static final CaseEvent NOTIFY_ROBOTICS = NOTIFY_RPA_ON_CONTINUOUS_FEED;
+
+    private final CoreCaseDataService coreCaseDataService;
 
     @Override
     public boolean canHandle(String caseEvent) {
@@ -20,6 +26,10 @@ public class AddCaseNoteMessageHandler implements CcdEventMessageHandler {
 
     @Override
     public void handle(CcdEventMessage message) {
+        try {
+            coreCaseDataService.triggerEvent(Long.parseLong(message.getCaseId()), NOTIFY_ROBOTICS);
+        } catch (Exception e) {
 
+        }
     }
 }
