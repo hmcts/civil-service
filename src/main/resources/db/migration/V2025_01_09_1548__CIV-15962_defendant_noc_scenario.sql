1dd6bdafbc9904549f40809e218cb943f808a3d5
paul-pearson <paul.pearson@version1.com>
2025-01-15 11:04:45 +0000

    CIV-15962 Fix issues found in testing

--- src/main/resources/db/migration/V2025_01_09_1548__CIV-15962_defendant_noc_scenario.sql
+++ src/main/resources/db/migration/V2025_01_09_1548__CIV-15962_defendant_noc_scenario.sql
@@ -7,7 +7,7 @@ VALUES ('Scenario.AAA6.DefendantNoticeOfChange.ClaimMovesOffline.Claimant',
           "Notice.AAA6.DefResponse.FullAdmit.PayImmediately.Claimant",
           "Notice.AAA6.DefResponse.PartAdmit.PayImmediately.Claimant",
           "Notice.AAA6.DefResponse.FullOrPartAdmit.PayBySetDate.Claimant",
-          "Notice.AAA6.DefResponse.FullOrPartAdmit.PayByInstallments.Claimant",
+          "Notice.AAA6.DefResponse.FullOrPartAdmit.PayByInstalments.Claimant",
           "Notice.AAA6.DefResponse.OrgOrLtdCompany.FullOrPartAdmit.PayBySetDate.Claimant",
           "Notice.AAA6.DefResponse.PartAdmit.AlreadyPaid.Claimant",
           "Notice.AAA6.DefResponse.FullDefence.AlreadyPaid.Claimant",
@@ -32,10 +32,10 @@ VALUES ('Notice.AAA6.DefendantNoticeOfChange.ClaimMovesOffline.Claimant',
         'Mae ${respondent1PartyName} wedi gofyn i gynrychiolydd cyfreithiol weithredu ar eu rhanh',
         '<p class="govuk-body">${respondent1PartyName} has asked for a legal representative to act on their behalf. From now on you will need to liaise with their representative.<br>'
         '<a href="{VIEW_INFO_ABOUT_DEFENDANT}" class="govuk-link">View the contact details of the defendant’s legal representative.</a><br>'
-        'This claim will now move offline and you must submit your intention to proceed by using form <a href="https://www.gov.uk/government/publications/form-n225-request-for-judgment-and-reply-to-admission-specified-amount" class="govuk-link">N225</a> (for a full admission) or <a href="https://www.gov.uk/government/publications/form-n225a-notice-of-part-admission-specified-amount" class="govuk-link">N225A</a> (for a partial admission) by ${applicant1ResponseDeadlineEn}.</p>',
+        'This claim will now move offline and you must submit your intention to proceed by using form <a href="https://www.gov.uk/government/publications/form-n225-request-for-judgment-and-reply-to-admission-specified-amount" target="_blank" class="govuk-link">N225</a> (for a full admission) or <a href="https://www.gov.uk/government/publications/form-n225a-notice-of-part-admission-specified-amount" target="_blank" class="govuk-link">N225A</a> (for a partial admission) by ${applicant1ResponseDeadlineEn}.</p>',
         '<p class="govuk-body">Mae ${respondent1PartyName} wedi gofyn i gynrychiolydd cyfreithiol weithredu ar eu rhan.  O hyn ymlaen bydd angen i chi gysylltu â''u cynrychiolydd.<br>'
         '<a href="{VIEW_INFO_ABOUT_DEFENDANT}" class="govuk-link">Gweld manylion cyswllt cynrychiolydd cyfreithiol y diffynnydd.</a><br>'
-        'Bydd yr hawliad hwn bellach yn symud all-lein ac mae''n rhaid i chi gyflwyno eich bwriad i fwrw ymlaen trwy ddefnyddio ffurflen <a href="https://www.gov.uk/government/publications/form-n225-request-for-judgment-and-reply-to-admission-specified-amount" class="govuk-link">N225</a> (ar gyfer addefiad llawn) neu <a href="https://www.gov.uk/government/publications/form-n225a-notice-of-part-admission-specified-amount" class="govuk-link">N225A</a> (ar gyfer addefiad rhannol) erbyn ${applicant1ResponseDeadlineCy}.</p>',
+        'Bydd yr hawliad hwn bellach yn symud all-lein ac mae''n rhaid i chi gyflwyno eich bwriad i fwrw ymlaen trwy ddefnyddio ffurflen <a href="https://www.gov.uk/government/publications/form-n225-request-for-judgment-and-reply-to-admission-specified-amount" target="_blank" class="govuk-link">N225</a> (ar gyfer addefiad llawn) neu <a href="https://www.gov.uk/government/publications/form-n225a-notice-of-part-admission-specified-amount" target="_blank" class="govuk-link">N225A</a> (ar gyfer addefiad rhannol) erbyn ${applicant1ResponseDeadlineCy}.</p>',
         'CLAIMANT');
 
 /**
@@ -45,4 +45,9 @@ INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, cat
                                     scenario_name, task_status_sequence, role, task_order)
 values ('<a href={VIEW_RESPONSE_TO_CLAIM} rel="noopener noreferrer" class="govuk-link">View the response to the claim</a>', 'The response',
         '<a href={VIEW_RESPONSE_TO_CLAIM} rel="noopener noreferrer" class="govuk-link">Gweld yr ymateb i''r hawliad</a>',
-        'Yr ymateb', 'Response.View', 'Scenario.AAA6.DefendantNoticeOfChange.ClaimMovesOffline.Claimant', '{3, 3}', 'CLAIMANT', 3);
+        'Yr ymateb', 'Response.View', 'Scenario.AAA6.DefendantNoticeOfChange.ClaimMovesOffline.Claimant', '{3, 3}', 'CLAIMANT', 3),
+       ('<a>Contact the court to request a change to my case</a>', 'Applications',
+        '<a>Contact the court to request a change to my case</a>',
+        'Ceisiadau', 'Application.Create', 'Scenario.AAA6.DefendantNoticeOfChange.ClaimMovesOffline.Claimant', '{2, 2}', 'CLAIMANT', 16),
+       ('<a>View applications</a>', 'Applications' ,'<a>Gweld y cais i gyd</a>',
+        'Ceisiadau', 'Application.View', 'Scenario.AAA6.DefendantNoticeOfChange.ClaimMovesOffline.Claimant', '{2, 2}', 'CLAIMANT', 17);
