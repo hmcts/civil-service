9e38e83fd3c787971d54a9cb7c4831a46173225d
gmmagruder <gabrielle.magruder@gmail.com>
2025-01-21 15:24:09 +0000

    Remove unused imports

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/NotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/NotificationHandler.java
@@ -3,15 +3,9 @@ package uk.gov.hmcts.reform.civil.handler.callback.camunda.notification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
-import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
-import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.notify.NotificationService;
-
-import java.util.List;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
