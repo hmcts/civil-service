d9ff27a887d6ba16e5f48ebb2b8beb492c2c7512
Azam <106387766+Azam-Hmcts@users.noreply.github.com>
2025-01-22 11:29:38 +0000

    CIV-15686 Query Management (#5963)
    * CIV-15686 Added callback handler
    * CIV-15686 Whitelisted event for raising query
    * CIV-15686 Added model for queryManagement
    * CIV-15686 Added new query case initializers
    * CIV-15686 Added QueryInitializer in claim creation
    * CIV-15686 added caseMessage as field in case date
    * Implemented storing latest query raised by user
    ---------
    
    Co-authored-by: GarethLancaster <31533575+Gareth40342@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/reform/civil/model/querymanagement/LatestQuery.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.civil.model.querymanagement;
+
+import lombok.Builder;
+import lombok.Data;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+
+@Data
+@Builder(toBuilder = true)
+public class LatestQuery {
+
+    private final String queryId;
+    private final YesOrNo isHearingRelated;
+
+}
