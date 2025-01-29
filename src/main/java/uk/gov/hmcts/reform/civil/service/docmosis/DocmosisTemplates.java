4be74057ab2ab34db392da5de3fc8b644e5b1d0f
ShwetaTandel-hmcts <shweta.tandel@hmcts.net>
2025-01-29 21:22:39 +0000

    The delete commit for Revert CIV-12873

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/DocmosisTemplates.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/DocmosisTemplates.java
@@ -93,7 +93,6 @@ public enum DocmosisTemplates {
     GENERATE_LIP_CLAIMANT_CLAIM_FORM("CV-SPC-CLM-ENG-CLAIMANT-CLAIM-FORM.docx", "%s-claim-form-claimant-copy.pdf"),
     GENERATE_LIP_DEFENDANT_CLAIM_FORM("CV-SPC-CLM-ENG-DEFENDANT-CLAIM-FORM.docx", "%s-sealed-claim-form.pdf"),
     RECONSIDERATION_UPHELD_DECISION_OUTPUT_PDF("CV-UNS-DEC-ENG-00001.docx", "Decision_on_reconsideration_request_%s.pdf"),
-    DEFAULT_JUDGMENT_COVER_LETTER_DEFENDANT_LEGAL_ORG("CV-CMC-LET-ENG-LR-JO0002.docx", "cover_letter.pdf"),
     JUDGMENT_BY_ADMISSION_CLAIMANT("CV-SPC-DEC-ENG-00916.docx", "Judgment_by_admission_claimant.pdf"),
     JUDGMENT_BY_ADMISSION_DEFENDANT("CV-SPC-DEC-ENG-00915.docx", "Judgment_by_admission_defendant.pdf"),
     JUDGMENT_BY_DETERMINATION_CLAIMANT("CV-SPC-DEC-ENG-00982.docx", "Judgment_by_determination_claimant.pdf"),
--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/dj/nondivergent/NonDivergentSpecDefaultJudgmentFormGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/dj/nondivergent/NonDivergentSpecDefaultJudgmentFormGenerator.java
@@ -18,7 +18,6 @@ import uk.gov.hmcts.reform.civil.utils.AssignCategoryId;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.GEN_DJ_FORM_NON_DIVERGENT_SPEC_CLAIMANT;
@@ -123,20 +122,20 @@ public class NonDivergentSpecDefaultJudgmentFormGenerator {
         );
     }
 
-    private List<DocumentMetaData> appendWelshDocToDocument(CaseDocument englishDoc, CaseDocument... welshDocuments) {
+    private List<DocumentMetaData> appendWelshDocToDocument(CaseDocument englishDoc, CaseDocument welshDocument) {
         List<DocumentMetaData> documentMetaDataList = new ArrayList<>();
 
         documentMetaDataList.add(new DocumentMetaData(
             englishDoc.getDocumentLink(),
-            "Welsh Document",
+            "English Document",
             LocalDate.now().toString()
         ));
 
-        Arrays.stream(welshDocuments).forEach(caseDocument -> documentMetaDataList.add(new DocumentMetaData(
-            caseDocument.getDocumentLink(),
+        documentMetaDataList.add(new DocumentMetaData(
+            welshDocument.getDocumentLink(),
             "Welsh Doc to attach",
             LocalDate.now().toString()
-        )));
+        ));
 
         return documentMetaDataList;
     }
