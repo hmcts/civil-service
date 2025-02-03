a119e27e0b43753c6f0fc7003003d7fa0366064d
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2025-02-03 12:03:26 +0000

    CIV-16559 confirm order review delete all notifications

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/ConfirmOrderReviewClaimantNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/ConfirmOrderReviewClaimantNotificationHandler.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.claimant;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
@@ -8,14 +10,20 @@ import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.callback.CaseEventsDashboardCallbackHandler;
 import uk.gov.hmcts.reform.civil.client.DashboardApiClient;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.helpers.sdo.SdoHelper;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.DashboardNotificationsParamsMapper;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
+import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
 
+import java.util.HashMap;
 import java.util.List;
 
+import static java.util.Objects.isNull;
 import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UPDATE_TASK_LIST_CONFIRM_ORDER_REVIEW_CLAIMANT;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_UPDATE_DASHBOARD_CLAIMANT_TASK_LIST_UPLOAD_DOCUMENTS_FINAL_ORDERS;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_UPDATE_TASK_LIST_TRIAL_READY_FINALS_ORDERS_CLAIMANT;
 
 @Service
 public class ConfirmOrderReviewClaimantNotificationHandler extends CaseEventsDashboardCallbackHandler {
@@ -24,21 +32,19 @@ public class ConfirmOrderReviewClaimantNotificationHandler extends CaseEventsDas
         List.of(UPDATE_TASK_LIST_CONFIRM_ORDER_REVIEW_CLAIMANT);
     public static final String TASK_ID = "UpdateTaskListConfirmOrderReviewClaimant";
     public static final String GA = "Applications";
+    private final ObjectMapper objectMapper;
 
     public ConfirmOrderReviewClaimantNotificationHandler(DashboardApiClient dashboardApiClient,
                                                          DashboardNotificationsParamsMapper mapper,
-                                                         FeatureToggleService featureToggleService) {
+                                                         FeatureToggleService featureToggleService,
+                                                         ObjectMapper objectMapper) {
         super(dashboardApiClient, mapper, featureToggleService);
+        this.objectMapper = objectMapper;
     }
 
     @Override
     public String camundaActivityId(CallbackParams callbackParams) {
         return TASK_ID;
-    }
-
-    @Override
-    public String getScenario(CaseData caseData) {
-        return null;
     }
 
     @Override
@@ -56,6 +62,7 @@ public class ConfirmOrderReviewClaimantNotificationHandler extends CaseEventsDas
     public CallbackResponse configureDashboardScenario(CallbackParams callbackParams) {
         CaseData caseData = callbackParams.getCaseData();
         String authToken = callbackParams.getParams().get(BEARER_TOKEN).toString();
+        CaseEvent caseEvent = CaseEvent.valueOf(callbackParams.getRequest().getEventId());
 
         if (shouldRecordScenario(caseData)) {
             dashboardApiClient.makeProgressAbleTasksInactiveForCaseIdentifierAndRole(
@@ -64,8 +71,35 @@ public class ConfirmOrderReviewClaimantNotificationHandler extends CaseEventsDas
                 GA,
                 authToken
             );
-        }
+            dashboardApiClient.deleteNotificationsForCaseIdentifierAndRole(
+                caseData.getCcdCaseReference().toString(),
+                "CLAIMANT",
+                authToken
+            );
+            HashMap<String, Object> paramsMap = (HashMap<String, Object>) mapper.mapCaseDataToParams(caseData, caseEvent);
 
-        return AboutToStartOrSubmitCallbackResponse.builder().build();
+            String scenario = getScenario(caseData);
+            if (!Strings.isNullOrEmpty(scenario)) {
+                dashboardApiClient.recordScenario(
+                    caseData.getCcdCaseReference().toString(),
+                    scenario,
+                    authToken,
+                    ScenarioRequestParams.builder().params(paramsMap).build()
+                );
+            }
+        }
+        return AboutToStartOrSubmitCallbackResponse.builder().data(caseData.toMap(objectMapper)).build();
+    }
+
+    @Override
+    protected String getScenario(CaseData caseData) {
+        if (isOrderMadeFastTrackTrialNotResponded(caseData)) {
+            return SCENARIO_AAA6_UPDATE_TASK_LIST_TRIAL_READY_FINALS_ORDERS_CLAIMANT.getScenario();
+        }
+        return SCENARIO_AAA6_UPDATE_DASHBOARD_CLAIMANT_TASK_LIST_UPLOAD_DOCUMENTS_FINAL_ORDERS.getScenario();
+    }
+
+    private boolean isOrderMadeFastTrackTrialNotResponded(CaseData caseData) {
+        return SdoHelper.isFastTrack(caseData) && isNull(caseData.getTrialReadyApplicant());
     }
 }
--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/ConfirmOrderReviewDefendantNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/ConfirmOrderReviewDefendantNotificationHandler.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.defendant;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
@@ -8,14 +10,20 @@ import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.callback.CaseEventsDashboardCallbackHandler;
 import uk.gov.hmcts.reform.civil.client.DashboardApiClient;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.helpers.sdo.SdoHelper;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.DashboardNotificationsParamsMapper;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
+import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
 
+import java.util.HashMap;
 import java.util.List;
 
+import static java.util.Objects.isNull;
 import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UPDATE_TASK_LIST_CONFIRM_ORDER_REVIEW_DEFENDANT;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_UPDATE_DASHBOARD_DEFENDANT_TASK_LIST_UPLOAD_DOCUMENTS_FINAL_ORDERS;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_UPDATE_TASK_LIST_TRIAL_READY_FINALS_ORDERS_DEFENDANT;
 
 @Service
 public class ConfirmOrderReviewDefendantNotificationHandler extends CaseEventsDashboardCallbackHandler {
@@ -24,11 +32,14 @@ public class ConfirmOrderReviewDefendantNotificationHandler extends CaseEventsDa
         List.of(UPDATE_TASK_LIST_CONFIRM_ORDER_REVIEW_DEFENDANT);
     public static final String TASK_ID = "UpdateTaskListConfirmOrderReviewDefendant";
     public static final String GA = "Applications";
+    private final ObjectMapper objectMapper;
 
     public ConfirmOrderReviewDefendantNotificationHandler(DashboardApiClient dashboardApiClient,
                                                           DashboardNotificationsParamsMapper mapper,
-                                                          FeatureToggleService featureToggleService) {
+                                                          FeatureToggleService featureToggleService,
+                                                          ObjectMapper objectMapper) {
         super(dashboardApiClient, mapper, featureToggleService);
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -38,7 +49,10 @@ public class ConfirmOrderReviewDefendantNotificationHandler extends CaseEventsDa
 
     @Override
     public String getScenario(CaseData caseData) {
-        return null;
+        if (isOrderMadeFastTrackTrialNotResponded(caseData)) {
+            return SCENARIO_AAA6_UPDATE_TASK_LIST_TRIAL_READY_FINALS_ORDERS_DEFENDANT.getScenario();
+        }
+        return SCENARIO_AAA6_UPDATE_DASHBOARD_DEFENDANT_TASK_LIST_UPLOAD_DOCUMENTS_FINAL_ORDERS.getScenario();
     }
 
     @Override
@@ -56,6 +70,7 @@ public class ConfirmOrderReviewDefendantNotificationHandler extends CaseEventsDa
     public CallbackResponse configureDashboardScenario(CallbackParams callbackParams) {
         CaseData caseData = callbackParams.getCaseData();
         String authToken = callbackParams.getParams().get(BEARER_TOKEN).toString();
+        CaseEvent caseEvent = CaseEvent.valueOf(callbackParams.getRequest().getEventId());
 
         if (shouldRecordScenario(caseData)) {
             dashboardApiClient.makeProgressAbleTasksInactiveForCaseIdentifierAndRole(
@@ -64,7 +79,28 @@ public class ConfirmOrderReviewDefendantNotificationHandler extends CaseEventsDa
                 GA,
                 authToken
             );
+            dashboardApiClient.deleteNotificationsForCaseIdentifierAndRole(
+                caseData.getCcdCaseReference().toString(),
+                "DEFENDANT",
+                authToken
+            );
+
+            HashMap<String, Object> paramsMap = (HashMap<String, Object>) mapper.mapCaseDataToParams(caseData, caseEvent);
+
+            String scenario = getScenario(caseData);
+            if (!Strings.isNullOrEmpty(scenario)) {
+                dashboardApiClient.recordScenario(
+                    caseData.getCcdCaseReference().toString(),
+                    scenario,
+                    authToken,
+                    ScenarioRequestParams.builder().params(paramsMap).build()
+                );
+            }
         }
-        return AboutToStartOrSubmitCallbackResponse.builder().build();
+        return AboutToStartOrSubmitCallbackResponse.builder().data(caseData.toMap(objectMapper)).build();
+    }
+
+    private boolean isOrderMadeFastTrackTrialNotResponded(CaseData caseData) {
+        return SdoHelper.isFastTrack(caseData) && isNull(caseData.getTrialReadyRespondent1());
     }
 }
