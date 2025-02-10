112fc89dff4d95710ed8b6993986ff14d37a699d
dharmendra kumar <dharmendra.kumar@hmcts.net>
2025-02-10 16:36:14 +0000

    DTSCCI-1592: Add indexes on dashboard tables

---
+++ src/main/resources/db/migration/V2025_02_10_1604__DTSCCI-1592_add_indexes_on_dashboard_tables.sql
@@ -0,0 +1,10 @@
+create index idx_scenario_name on dbs.scenario(name);
+
+create index idx_dashboard_notifications_templates_template_name on dbs.dashboard_notifications_templates(template_name);
+create index idx_dashboard_notifications_reference_citizen_role on dbs.dashboard_notifications(reference, citizen_role);
+
+
+create index idx_task_item_template_scenario_name on dbs.task_item_template(scenario_name);
+create index idx_task_item_template_template_name_role on dbs.task_item_template(template_name, role);
+
+create index idx_task_list_reference on dbs.task_list(reference);
