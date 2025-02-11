5a20d527ec8c6bb7692a4048645686d4f5d1368d
Velican <duzgunce.vd@gmail.com>
2025-02-11 15:00:57 +0000

    fix test

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/fees/FeesControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/fees/FeesControllerTest.java
@@ -15,6 +15,7 @@ import uk.gov.hmcts.reform.civil.model.Fee2Dto;
 import uk.gov.hmcts.reform.civil.model.FeeVersionDto;
 import uk.gov.hmcts.reform.civil.model.FlatAmountDto;
 import uk.gov.hmcts.reform.civil.model.citizenui.GeneralApplicationFeeRequest;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.service.FeesService;
 import uk.gov.hmcts.reform.civil.service.GeneralAppFeesService;
 import uk.gov.hmcts.reform.civil.utils.InterestCalculator;
@@ -59,7 +60,7 @@ public class FeesControllerTest extends BaseIntegrationTest {
     public void shouldReturnClaimInterestToDate() {
         CaseData caseData = CaseData.builder().build();
         when(interestCalculator.calculateInterest(caseData)).thenReturn(new BigDecimal("0.1"));
-        doGet(BEARER_TOKEN, FEES_CLAIM_CALCULATE_INTEREST_URL, caseData)
+        doPost(BEARER_TOKEN, CaseDataBuilder.builder().build(), FEES_CLAIM_CALCULATE_INTEREST_URL, caseData)
             .andExpect(content().json(toJson("0.1")))
             .andExpect(status().isOk());
     }
