ff80fcea0e34ca32904cc1d9edaa1346816baa76
drummondjm <mark.drummond@version.com>
2025-02-19 16:14:18 +0000

    add in https://github.com/hmcts/civil-service/pull/6157 https://github.com/hmcts/civil-service/pull/6169

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ConfirmListingCompletedCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ConfirmListingCompletedCallbackHandlerTest.java
@@ -5,10 +5,14 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.enums.AllocatedTrack;
+import uk.gov.hmcts.reform.civil.enums.CaseCategory;
 import uk.gov.hmcts.reform.civil.enums.caseprogression.ConfirmListingTickBox;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
@@ -18,6 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.MID;
 import static uk.gov.hmcts.reform.civil.enums.caseprogression.ConfirmListingTickBox.CONFIRM_LISTING;
@@ -29,11 +34,46 @@ class ConfirmListingCompletedCallbackHandlerTest extends BaseCallbackHandlerTest
     @InjectMocks
     private ConfirmListingCompletedCallbackHandler handler;
 
+    private ObjectMapper objectMapper;
+
     @BeforeEach
     void setUp() {
-        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         handler = new ConfirmListingCompletedCallbackHandler(objectMapper);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "INTERMEDIATE_CLAIM, 4",
+        "MULTI_CLAIM, 5"
+    })
+    void shouldPopulateHearingListingTypeUnspec_whenInvoked(String claimType, String  expectedSize) {
+        CaseData caseData = CaseDataBuilder.builder().atStateNotificationAcknowledged().build().toBuilder()
+            .allocatedTrack(AllocatedTrack.valueOf(claimType))
+            .build();
+        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_START);
+        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+        CaseData updatedData = objectMapper.convertValue(response.getData(), CaseData.class);
+
+        assertThat(updatedData.getHearingListedDynamicList().getListItems().size()).isEqualTo(Integer.parseInt(expectedSize));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "INTERMEDIATE_CLAIM, 4",
+        "MULTI_CLAIM, 5"
+    })
+    void shouldPopulateHearingListingTypeSpec_whenInvoked(String claimType, String  expectedSize) {
+        CaseData caseData = CaseDataBuilder.builder().atStateNotificationAcknowledged().build().toBuilder()
+            .caseAccessCategory(CaseCategory.SPEC_CLAIM)
+            .responseClaimTrack(claimType)
+            .build();
+        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_START);
+        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+        CaseData updatedData = objectMapper.convertValue(response.getData(), CaseData.class);
+
+        assertThat(updatedData.getHearingListedDynamicList().getListItems().size()).isEqualTo(Integer.parseInt(expectedSize));
     }
 
     @Test
