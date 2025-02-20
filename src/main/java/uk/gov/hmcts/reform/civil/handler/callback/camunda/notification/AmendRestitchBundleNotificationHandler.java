9d31471265e6c82e1e3b75244fa352a4b87698a2
gmmagruder <gabrielle.magruder@gmail.com>
2025-02-20 10:55:47 +0000

    Add new handler for AmendRestitchBundle email notifications

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/AmendRestitchBundleNotificationHandler.java
@@ -0,0 +1,52 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.notification;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.Callback;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.notification.handlers.AmendRestitchBundleNotifier;
+
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.NOTIFY_AMEND_RESTITCH_BUNDLE;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AmendRestitchBundleNotificationHandler extends CallbackHandler {
+
+    private static final List<CaseEvent> EVENTS = List.of(NOTIFY_AMEND_RESTITCH_BUNDLE);
+
+    public static final String TASK_ID = "AmendRestitchBundleNotifier";
+
+    private final AmendRestitchBundleNotifier amendRestitchBundleNotifier;
+
+    @Override
+    protected Map<String, Callback> callbacks() {
+        return Map.of(
+            callbackKey(ABOUT_TO_SUBMIT), this::notifyForAmendRestitchBundle
+        );
+    }
+
+    @Override
+    public String camundaActivityId(CallbackParams callbackParams) {
+        return TASK_ID;
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+
+    private CallbackResponse notifyForAmendRestitchBundle(CallbackParams callbackParams) {
+        amendRestitchBundleNotifier.notifyParties(callbackParams.getCaseData());
+        return AboutToStartOrSubmitCallbackResponse.builder().build();
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/notification/AmendRestitchBundleNotificationHandlerTest.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.notification;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.notification.handlers.AmendRestitchBundleNotifier;
+import uk.gov.hmcts.reform.civil.sampledata.CallbackParamsBuilder;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+
+@ExtendWith(MockitoExtension.class)
+public class AmendRestitchBundleNotificationHandlerTest {
+
+    @Mock
+    private AmendRestitchBundleNotifier amendRestitchBundleNotifier;
+    @InjectMocks
+    private AmendRestitchBundleNotificationHandler amendRestitchBundleNotificationHandler;
+
+    @Test
+    void shouldNotifyParties_whenInvoked() {
+        CaseData caseData = CaseDataBuilder.builder().atStateClaimDetailsNotified().build();
+        CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_SUBMIT, caseData).build();
+
+        amendRestitchBundleNotificationHandler.handle(params);
+        verify(amendRestitchBundleNotifier).notifyParties(caseData);
+    }
+}
