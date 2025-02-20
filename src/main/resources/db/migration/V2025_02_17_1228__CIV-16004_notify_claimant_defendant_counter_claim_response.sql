bf224b96c63ad43c8ce79243acc620eb66678b5a
jeswanth <jeswanth.reddy@hmcts.net>
2025-02-20 17:42:38 +0000

    CIV-16004 added changes to show dashboard activites

--- src/main/resources/db/migration/V2025_02_17_1228__CIV-16004_notify_claimant_defendant_counter_claim_response.sql
+++ src/main/resources/db/migration/V2025_02_17_1228__CIV-16004_notify_claimant_defendant_counter_claim_response.sql
@@ -17,3 +17,14 @@ VALUES ('Notice.AAA6.DefLRResponse.FullDefence.Counterclaim.Claimant',
         '<p class="govuk-body">The defendant must file the response by the deadline. If they do not respond by their deadline then you can request a county court judgment by using form <a href="https://www.gov.uk/government/publications/form-n225-request-for-judgment-and-reply-to-admission-specified-amount" target="_blank" class="govuk-link">N225</a>. This claim will now proceed offline.</p>',
         '<p class="govuk-body">Rhaid i''r diffynnydd gyflwyno''r ymateb erbyn y dyddiad cau. Os na fyddant yn ymateb erbyn y dyddiad cau, gallwch ofyn am ddyfarniad llys sirol drwy ddefnyddio ffurflen <a href="https://www.gov.uk/government/publications/form-n225-request-for-judgment-and-reply-to-admission-specified-amount" target="_blank" class="govuk-link">N225</a>. Bydd yr hawliad hwn yn mynd rhagddo all-lein yn awr.</p>',
         'CLAIMANT');
+
+/**
+ * Add task list items
+ */
+INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
+                                    scenario_name, task_status_sequence, role, task_order)
+values ('<a href={VIEW_RESPONSE_TO_CLAIM} rel="noopener noreferrer" class="govuk-link">View the response to the claim</a>',
+        'The response',
+        '<a href={VIEW_RESPONSE_TO_CLAIM} rel="noopener noreferrer" class="govuk-link">Gweld yr ymateb i''r hawliad</a>',
+        'Yr ymateb', 'Response.View', 'Scenario.Notice.AAA6.DefLRResponse.FullDefence.Counterclaim.Claimant', '{3, 3}',
+        'CLAIMANT', 3);
