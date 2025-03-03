75db037f7f4bdfc5b82a45c7421be1a1b542dbde
Ruban <ruban72@hotmail.com>
2025-03-03 11:26:09 +0000

    DTSCCI-1556: Remove JWT from security config

--- src/main/java/uk/gov/hmcts/reform/civil/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/SecurityConfiguration.java
@@ -1,23 +1,14 @@
 package uk.gov.hmcts.reform.civil.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
-import org.springframework.security.oauth2.core.OAuth2TokenValidator;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.JwtIssuerValidator;
-import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import uk.gov.hmcts.reform.civil.security.JwtGrantedAuthoritiesConverter;
+import uk.gov.hmcts.reform.auth.checker.core.RequestAuthorizer;
+import uk.gov.hmcts.reform.auth.checker.core.user.User;
+import uk.gov.hmcts.reform.auth.checker.spring.useronly.AuthCheckerUserOnlyFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -56,19 +47,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         "/testing-support/flowstate"
     };
 
-    @Value("${spring.security.oauth2.client.provider.oidc.issuer-uri}")
-    private String issuerUri;
+    private final RequestAuthorizer<User> userRequestAuthorizer;
+    private final AuthenticationManager authenticationManager;
 
-    @Value("${oidc.issuer}")
-    private String issuerOverride;
-
-    private final JwtAuthenticationConverter jwtAuthenticationConverter;
-
-    @Autowired
-    public SecurityConfiguration(final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter) {
-        jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+    public SecurityConfiguration(
+        RequestAuthorizer<User> userRequestAuthorizer,
+        AuthenticationManager authenticationManager
+    ) {
+        this.userRequestAuthorizer = userRequestAuthorizer;
+        this.authenticationManager = authenticationManager;
     }
+
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -77,6 +66,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        AuthCheckerUserOnlyFilter<User> authCheckerUserOnlyFilter =
+            new AuthCheckerUserOnlyFilter<>(userRequestAuthorizer);
+
+        authCheckerUserOnlyFilter.setAuthenticationManager(authenticationManager);
+        // @formatter:off
         http
             .sessionManagement().sessionCreationPolicy(STATELESS).and()
             .csrf().disable()
@@ -90,23 +84,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .authenticated()
             .and()
             .oauth2ResourceServer()
-            .jwt()
-            .jwtAuthenticationConverter(jwtAuthenticationConverter)
-            .and()
             .and()
             .oauth2Client();
     }
 
-    @Bean
-    JwtDecoder jwtDecoder() {
-        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuerUri);
-
-        // We are using issuerOverride instead of issuerUri as SIDAM has the wrong issuer at the moment
-        OAuth2TokenValidator<Jwt> withTimestamp = new JwtTimestampValidator();
-        OAuth2TokenValidator<Jwt> withIssuer = new JwtIssuerValidator(issuerOverride);
-        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp, withIssuer);
-
-        jwtDecoder.setJwtValidator(validator);
-        return jwtDecoder;
-    }
 }
