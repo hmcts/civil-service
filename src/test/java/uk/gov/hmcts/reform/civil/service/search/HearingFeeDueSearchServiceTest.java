fc9f1bf0c3fb13503db7611bd883d98b54cf1fe4
drummondjm <mark.drummond@version1.com>
2025-03-03 17:34:50 +0000

    checkstyle

--- src/test/java/uk/gov/hmcts/reform/civil/service/search/HearingFeeDueSearchServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/search/HearingFeeDueSearchServiceTest.java
@@ -3,20 +3,14 @@ package uk.gov.hmcts.reform.civil.service.search;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import uk.gov.hmcts.reform.civil.model.search.Query;
-import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
 import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
-import static org.elasticsearch.index.query.QueryBuilders.rangeQuery;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
 
 class HearingFeeDueSearchServiceTest extends ElasticSearchServiceTest {
 
@@ -34,15 +28,12 @@ class HearingFeeDueSearchServiceTest extends ElasticSearchServiceTest {
         return new Query(query, List.of("reference"), fromValue);
     }
 
-
     @Test
     void testQuery() {
         Query expectedQuery = buildQuery(0);
         String queryString = expectedQuery.toString();
         assertFalse(queryString.contains("data.hearingDueDate"));
     }
-
-
 
     @Override
     protected Query buildQueryInMediation(int fromValue, LocalDate date, boolean carmEnabled,
