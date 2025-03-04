90d1c355755725894b1bc7292adb651abcccca7c
Deepthi Doppalapudi <107422736+deepthidoppalapudihmcts@users.noreply.github.com>
2025-03-04 12:33:53 +0000

    CIV-16691 corrected name of the table in the script

--- src/main/resources/db/migration/V2025_02_27_1007__CIV_16691_update_general_application_proceed_offline_applicant.sql
+++ src/main/resources/db/migration/V2025_02_27_1007__CIV_16691_update_general_application_proceed_offline_applicant.sql
@@ -1,5 +1,5 @@
 /**
  * Update scenario for applicant & respondent
  */
-UPDATE dbs.dashboard_notifications_   templates SET notification_role = replace(notification_role, 'APPLICANT') WHERE template_name  = 'Notice.AAA6.GeneralApps.ApplicationProceedsOffline.Applicant' ;
-UPDATE dbs.dashboard_notifications_   templates SET notification_role = replace(notification_role, 'RESPONDENT') WHERE template_name  = 'Notice.AAA6.GeneralApps.ApplicationProceedsOffline.Respondent';
+UPDATE dbs.dashboard_notifications_templates SET notification_role = replace(notification_role, 'APPLICANT') WHERE template_name  = 'Notice.AAA6.GeneralApps.ApplicationProceedsOffline.Applicant' ;
+UPDATE dbs.dashboard_notifications_templates SET notification_role = replace(notification_role, 'RESPONDENT') WHERE template_name  = 'Notice.AAA6.GeneralApps.ApplicationProceedsOffline.Respondent';
