020a2a8b5329e99a854d623c481df0bf2ff3275a
Deepthi Doppalapudi <107422736+deepthidoppalapudihmcts@users.noreply.github.com>
2025-03-04 12:06:43 +0000

    civ-16691 dashboard notification when application proceeds offline

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/generalapplications/ApplicationsProceedOfflineNotificationCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/generalapplications/ApplicationsProceedOfflineNotificationCallbackHandlerTest.java
@@ -24,6 +24,7 @@ import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.service.DashboardNotificationsParamsMapper;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
+import uk.gov.hmcts.reform.dashboard.services.DashboardNotificationService;
 import uk.gov.hmcts.reform.dashboard.services.DashboardScenariosService;
 
 import java.util.ArrayList;
@@ -37,7 +38,7 @@ import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.enums.CaseState.CASE_ISSUED;
 import static uk.gov.hmcts.reform.civil.enums.CaseState.PROCEEDS_IN_HERITAGE_SYSTEM;
-import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_UPDATE_CASE_PROCEED_IN_CASE_MAN_CLAIMANT;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_CASE_PROCEED_IN_CASE_MAN_CLAIMANT;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_UPDATE_CASE_PROCEED_IN_CASE_MAN_DEFENDANT;
 import static uk.gov.hmcts.reform.civil.utils.ElementUtils.wrapElements;
 
@@ -54,9 +55,13 @@ class ApplicationsProceedOfflineNotificationCallbackHandlerTest extends BaseCall
     @Mock
     private DashboardScenariosService dashboardScenariosService;
     @Mock
+    private DashboardNotificationService dashboardNotificationService;
+    @Mock
     private DashboardNotificationsParamsMapper mapper;
     @Mock
     private FeatureToggleService toggleService;
+    private static final String CLAIMANT = "Claimant";
+    private static final String DEFENDANT = "Defendant";
 
     @Nested
     class AboutToSubmitCallback {
@@ -289,9 +294,11 @@ class ApplicationsProceedOfflineNotificationCallbackHandlerTest extends BaseCall
                 .build();
             // THEN
             handler.handle(callbackParams);
+            verify(dashboardNotificationService).deleteByReferenceAndCitizenRole(caseData.getCcdCaseReference().toString(),
+                                                                                 CLAIMANT);
             verify(dashboardScenariosService).recordScenarios(
                 "BEARER_TOKEN",
-                SCENARIO_AAA6_UPDATE_CASE_PROCEED_IN_CASE_MAN_CLAIMANT.getScenario(),
+                SCENARIO_AAA6_CASE_PROCEED_IN_CASE_MAN_CLAIMANT.getScenario(),
                 caseData.getCcdCaseReference().toString(),
                 ScenarioRequestParams.builder().params(new HashMap<>()).build()
             );
@@ -372,6 +379,8 @@ class ApplicationsProceedOfflineNotificationCallbackHandlerTest extends BaseCall
                 .build();
             // THEN
             handler.handle(callbackParams);
+            verify(dashboardNotificationService).deleteByReferenceAndCitizenRole(caseData.getCcdCaseReference().toString(),
+                                                                                 DEFENDANT);
             verify(dashboardScenariosService).recordScenarios(
                 "BEARER_TOKEN",
                 SCENARIO_AAA6_UPDATE_CASE_PROCEED_IN_CASE_MAN_DEFENDANT.getScenario(),
