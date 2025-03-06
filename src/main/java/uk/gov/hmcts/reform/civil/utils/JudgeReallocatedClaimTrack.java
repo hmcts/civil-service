596c8c6e308ea5ffcf62940f64e0d95fb8053d7d
drummondjm <mark.drummond@version1.com>
2025-03-06 14:03:10 +0000

    fix for when small fats claims are reallocated to minti type track by judge in make an order

--- src/main/java/uk/gov/hmcts/reform/civil/utils/JudgeReallocatedClaimTrack.java
+++ src/main/java/uk/gov/hmcts/reform/civil/utils/JudgeReallocatedClaimTrack.java
@@ -1,9 +1,13 @@
 package uk.gov.hmcts.reform.civil.utils;
-import uk.gov.hmcts.reform.civil.model.CaseData;
 
+import uk.gov.hmcts.reform.civil.model.CaseData;
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.YES;
 
 public class JudgeReallocatedClaimTrack {
+
+    private JudgeReallocatedClaimTrack() {
+        //No op
+    }
 
     public static Boolean hasJudgeReallocatedTrack(CaseData caseData) {
         return caseData.getFinalOrderAllocateToTrack() != null && caseData.getFinalOrderAllocateToTrack().equals(YES);
