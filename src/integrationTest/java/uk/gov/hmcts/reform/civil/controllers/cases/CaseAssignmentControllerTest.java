5636d9b83829ef8f2eadd6a055f6724b982845a5
Ruban <ruban72@hotmail.com>
2025-03-06 14:11:08 +0000

    DTSCCI-1556: Fix integration tests

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/cases/CaseAssignmentControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/cases/CaseAssignmentControllerTest.java
@@ -10,10 +10,13 @@ import uk.gov.hmcts.reform.civil.service.AssignCaseService;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 import uk.gov.hmcts.reform.civil.service.citizen.defendant.LipDefendantCaseAssignmentService;
 import uk.gov.hmcts.reform.civil.service.pininpost.DefendantPinToPostLRspecService;
+import uk.gov.hmcts.reform.civil.service.pininpost.exception.PinNotMatchException;
 import uk.gov.hmcts.reform.civil.service.search.CaseLegacyReferenceSearchService;
+import uk.gov.hmcts.reform.civil.service.search.exceptions.SearchServiceCaseNotFoundException;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -47,6 +50,42 @@ public class CaseAssignmentControllerTest extends BaseIntegrationTest {
         doPost("", "123", VALIDATE_PIN_URL, "123")
             .andExpect(content().json(toJson(caseDetails)))
             .andExpect(status().isOk());
+    }
+
+    @Test
+    @SneakyThrows
+    void givenIncorrectReference_whenValidateCaseAndPin_shouldReturnUnauthorized() {
+        when(caseByLegacyReferenceSearchService
+                 .getCaseDataByLegacyReference(any())).thenThrow(new SearchServiceCaseNotFoundException());
+
+        doPost("", "123", VALIDATE_PIN_URL, "123")
+            .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @SneakyThrows
+    void givenIncorrectPin_whenValidateCaseAndPin_shouldReturnBadRequest() {
+        givenCaseIsFound();
+        doThrow(new PinNotMatchException()).when(defendantPinToPostLRspecService).validatePin(
+            any(CaseDetails.class),
+            anyString()
+        );
+
+        doPost("", "123", VALIDATE_PIN_URL, "123")
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @SneakyThrows
+    void givenIncorrectPin_whenValidateOcmcPin_shouldReturnBadRequest() {
+        givenCaseIsFound();
+        doThrow(new PinNotMatchException()).when(defendantPinToPostLRspecService).validateOcmcPin(
+            anyString(),
+            anyString()
+        );
+
+        doPost("", "123", VALIDATE_OCMC_PIN_URL, "123")
+            .andExpect(status().isBadRequest());
     }
 
     @Test
