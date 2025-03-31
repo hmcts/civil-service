bf9fde683a52c80dcc5245079cf751217c30a883
EllisD-B <ellis.domengerboyce@hmcts.net>
2025-03-31 09:25:55 +0100

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/civil/handler/message/CcdEventMessageHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/message/CcdEventMessageHandler.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.civil.handler.message;
 
 import uk.gov.hmcts.reform.civil.model.Result;
-import uk.gov.hmcts.reform.civil.model.message.CcdEventMessage;
 
 import java.util.List;
 
--- src/main/java/uk/gov/hmcts/reform/civil/model/Result.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/Result.java
@@ -3,5 +3,6 @@ package uk.gov.hmcts.reform.civil.model;
 public sealed interface Result {
 
     record Success() implements Result { }
+
     record Error(ExceptionRecord exceptionRecord) implements Result { }
 }
