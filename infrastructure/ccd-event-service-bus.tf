701067aba7737db893e526a83db1b8b7dea3a20f
Ieuan Byers <ieuan.byers@justice.gov.uk>
2025-03-31 09:20:50 +0100

    Updating servicebus subscription module to use new branch

--- infrastructure/ccd-event-service-bus.tf
+++ infrastructure/ccd-event-service-bus.tf
@@ -1,9 +1,13 @@
-module "servicebus-subscription-ccd-events" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=DTSPO-25025-add-status-flag"
-  name                = "civil-ccd-case-events-sub-${var.env}"
-  namespace_name      = "ccd-servicebus-${var.env}"
-  topic_name          = "ccd-case-events-${var.env}"
+data "azurerm_servicebus_namespace" "ccd-servicebus" {
+  name                = "ccd-servicebus-${var.env}"
   resource_group_name = "ccd-shared-${var.env}"
+}
+
+module "servicebus-subscription-ccd-events" {
+  source       = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=DTSPO-25025-add-status-flag"
+  name         = "civil-ccd-case-events-sub-${var.env}"
+  namespace_id = data.azurerm_servicebus_namespace.ccd-servicebus.id
+  topic_name   = "ccd-case-events-${var.env}"
 }
 
 resource "azurerm_servicebus_subscription_rule" "civil_ccd_jurisdiction_rule" {
