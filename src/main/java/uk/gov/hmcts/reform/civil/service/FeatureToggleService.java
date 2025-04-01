600c414570d7c5b60b4b757873dac3f2709ac4e7
paul-pearson <paul.pearson@version1.com>
2025-04-01 10:42:41 +0100

    CIV-16952 Add feature toggle for Welsh GA

--- src/main/java/uk/gov/hmcts/reform/civil/service/FeatureToggleService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/FeatureToggleService.java
@@ -160,4 +160,8 @@ public class FeatureToggleService {
     public boolean isQueryManagementLRsEnabled() {
         return featureToggleApi.isFeatureEnabled("query-management");
     }
+
+    public boolean isGaForWelshEnabled() {
+        return featureToggleApi.isFeatureEnabled("generalApplicationsForWelshParty");
+    }
 }
