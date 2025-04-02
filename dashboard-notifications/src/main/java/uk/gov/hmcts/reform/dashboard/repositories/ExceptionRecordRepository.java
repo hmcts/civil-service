14af2393567adb1a36588f25bfb9974ff0ae20c7
EllisD-B <ellis.domengerboyce@hmcts.net>
2025-04-02 14:06:44 +0100

    debug mode

--- dashboard-notifications/src/main/java/uk/gov/hmcts/reform/dashboard/repositories/ExceptionRecordRepository.java
+++ dashboard-notifications/src/main/java/uk/gov/hmcts/reform/dashboard/repositories/ExceptionRecordRepository.java
@@ -1,16 +1,13 @@
 package uk.gov.hmcts.reform.dashboard.repositories;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.dashboard.entities.ExceptionRecordEntity;
 
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 @Repository
-@Transactional
 public interface ExceptionRecordRepository extends CrudRepository<ExceptionRecordEntity, UUID> {
 
     Optional<ExceptionRecordEntity> findByIdempotencyKey(String idempotencyKey);
