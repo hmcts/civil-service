201ad925e28da33da8bfcfe25d72ec6b1318d7d8
paul-pearson <paul.pearson@version1.com>
2025-04-03 17:44:37 +0100

    CIV-16846 Caseworker event change language preference

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/ChangeLanguagePreferenceCallbackHandler.java
@@ -0,0 +1,117 @@
+package uk.gov.hmcts.reform.civil.handler.callback.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.Callback;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.citizenui.CaseDataLiP;
+import uk.gov.hmcts.reform.civil.model.welshenhancements.ChangeLanguagePreference;
+import uk.gov.hmcts.reform.civil.model.welshenhancements.PreferredLanguage;
+import uk.gov.hmcts.reform.civil.model.welshenhancements.UserType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CHANGE_LANGUAGE_PREFERENCE;
+
+@Service
+@RequiredArgsConstructor
+public class ChangeLanguagePreferenceCallbackHandler extends CallbackHandler {
+
+    public static final String LIP_PARTY_REQUIRED = "At least one party on the claim must be unrepresented.";
+    public static final String SELECTED_PARTY_LIP_REQUIRED = "The selected party must be unrepresented.";
+
+    public static final String DEFENDANT_RESPONSE_REQUIRED = "The defendant must have already responded in order to change their language preference.";
+    private static final List<CaseEvent> EVENTS = List.of(CHANGE_LANGUAGE_PREFERENCE);
+
+    private final ObjectMapper objectMapper;
+
+    private final Map<String, Callback> callbackMap = Map.of(callbackKey(ABOUT_TO_START), this::validateChangeLanguagePreference,
+                                                             callbackKey(ABOUT_TO_SUBMIT), this::changeLanguagePreference,
+                                                             callbackKey(SUBMITTED), this::emptySubmittedCallbackResponse);
+
+    @Override
+    protected Map<String, Callback> callbacks() {
+        return callbackMap;
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+
+    private CallbackResponse validateChangeLanguagePreference(CallbackParams callbackParams) {
+        var caseData = callbackParams.getCaseData();
+        ArrayList<String> errors = new ArrayList<>();
+        if (!caseData.isApplicantLiP() && !caseData.isRespondent1LiP()) {
+            errors.add(LIP_PARTY_REQUIRED);
+        }
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .errors(errors)
+            .build();
+    }
+
+    private CallbackResponse changeLanguagePreference(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        CaseData.CaseDataBuilder<?, ?> builder = caseData.toBuilder();
+        List<String> errors = new ArrayList<>();
+        PreferredLanguage preferredLanguage = Optional.ofNullable(caseData.getChangeLanguagePreference())
+            .map(ChangeLanguagePreference::getPreferredLanguage)
+            .orElseThrow(() -> new IllegalArgumentException("Preferred language not found"));
+        String revisedBilingualPreference = switch (preferredLanguage) {
+            case ENGLISH -> "ENGLISH";
+            case WELSH -> "WELSH";
+            case ENGLISH_AND_WELSH -> "BOTH";
+        };
+        UserType userType = Optional.ofNullable(caseData.getChangeLanguagePreference())
+            .map(ChangeLanguagePreference::getUserType)
+            .orElseThrow(() -> new IllegalArgumentException("User type not found"));
+        switch (userType) {
+            case CLAIMANT -> {
+                if (!caseData.isApplicantLiP()) {
+                    errors.add(SELECTED_PARTY_LIP_REQUIRED);
+                } else {
+                    builder.claimantBilingualLanguagePreference(revisedBilingualPreference);
+                }
+            }
+            case DEFENDANT -> {
+                if (!caseData.isRespondent1LiP()) {
+                    errors.add(SELECTED_PARTY_LIP_REQUIRED);
+                } else if (caseData.getCaseDataLiP() == null || caseData.getCaseDataLiP().getRespondent1LiPResponse() == null) {
+                    errors.add(DEFENDANT_RESPONSE_REQUIRED);
+                } else {
+                    setRespondentResponseBilingualLanguagePreference(caseData, builder, revisedBilingualPreference);
+                }
+            }
+        }
+        builder.changeLanguagePreference(null);
+        CaseData updatedCaseData = builder.build();
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(updatedCaseData.toMap(objectMapper))
+            .errors(errors)
+            .build();
+    }
+
+    private void setRespondentResponseBilingualLanguagePreference(CaseData caseData,
+                                                                  CaseData.CaseDataBuilder<?,?> builder,
+                                                                  String revisedBilingualPreference) {
+        CaseDataLiP caseDataLiP = caseData.getCaseDataLiP();
+        builder.caseDataLiP(caseDataLiP.toBuilder()
+                                .respondent1LiPResponse(caseDataLiP.getRespondent1LiPResponse().toBuilder()
+                                                            .respondent1ResponseLanguage(revisedBilingualPreference).build())
+                                .build());
+    }
+
+
+}
---
+++ src/main/java/uk/gov/hmcts/reform/civil/model/welshenhancements/UserType.java
@@ -0,0 +1,6 @@
+package uk.gov.hmcts.reform.civil.model.welshenhancements;
+
+public enum UserType {
+    CLAIMANT,
+    DEFENDANT
+}
