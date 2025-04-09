fbf53421e77d13b7302f3c80f6e0cd2e92a7e9f1
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-04-09 13:25:22 +0100

    run functional tests using a shell script from civil-def instead

---
+++ bin/run-functional-tests.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+cd ${PWD%/*}/civil-ccd-definition
+
+echo "Running Functional tests on ${ENVIRONMENT} env"
+
+if [ "$ENVIRONMENT" = "aat" ] || [ -z "$PR_FT_GROUPS" ]; then
+  yarn test:api-nonprod
+else
+  command="yarn test:api-nonprod --grep "
+  pr_ft_groups=$(echo "$PR_FT_GROUPS" | awk '{print tolower($0)}')
+  
+  regex_pattern=""
+
+  IFS=',' read -ra ft_groups_array <<< "$pr_ft_groups"
+
+  for ft_group in "${ft_groups_array[@]}"; do
+      if [ -n "$regex_pattern" ]; then
+          regex_pattern+="|"
+      fi
+      regex_pattern+="@$ft_group"
+  done
+
+  command+="'$regex_pattern'"
+  echo "Executing: $command"
+  eval "$command"
+fi
+
+echo "Current directory: $(pwd)"
