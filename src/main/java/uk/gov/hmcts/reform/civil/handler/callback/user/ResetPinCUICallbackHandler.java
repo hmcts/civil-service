79917f9441ca44a716675f545bbaf84acac1a1da
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2025-04-14 16:19:15 +0100

    CIV-15925 defendant lip pip notification added

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/ResetPinCUICallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/ResetPinCUICallbackHandler.java
@@ -10,7 +10,11 @@ import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.config.PinInPostConfiguration;
+import uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.notify.NotificationService;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
 import uk.gov.hmcts.reform.civil.service.pininpost.DefendantPinToPostLRspecService;
 
 import java.util.List;
@@ -20,14 +24,21 @@ import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.RESET_PIN;
+import static uk.gov.hmcts.reform.civil.helpers.DateFormatHelper.DATE;
+import static uk.gov.hmcts.reform.civil.helpers.DateFormatHelper.formatLocalDate;
+import static uk.gov.hmcts.reform.civil.utils.PartyUtils.getPartyNameBasedOnType;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class ResetPinCUICallbackHandler extends CallbackHandler {
+public class ResetPinCUICallbackHandler extends CallbackHandler implements NotificationData {
 
     private final DefendantPinToPostLRspecService defendantPinToPostLRspecService;
+    private final NotificationService notificationService;
+    private final NotificationsProperties notificationsProperties;
+    private final PinInPostConfiguration pipInPostConfiguration;
     private final ObjectMapper objectMapper;
+    private static final String REFERENCE_TEMPLATE = "claim-continuing-online-notification-%s";
 
     @Override
     protected Map<String, Callback> callbacks() {
@@ -44,15 +55,45 @@ public class ResetPinCUICallbackHandler extends CallbackHandler {
     }
 
     private CallbackResponse resetPinExpiryDate(CallbackParams callbackParams) {
-        CaseData updatedCase = callbackParams.getCaseData().toBuilder()
+        CaseData caseData = callbackParams.getCaseData();
+        CaseData updatedCase = caseData.toBuilder()
             .respondent1PinToPostLRspec(defendantPinToPostLRspecService
-                                            .resetPinExpiryDate(callbackParams
-                                                                    .getCaseData()
-                                                                    .getRespondent1PinToPostLRspec()))
+                                            .resetPinExpiryDate(caseData.getRespondent1PinToPostLRspec()))
             .build();
 
+        if (caseData.isRespondent1LiP()
+            && caseData.getRespondent1() != null
+            && caseData.getRespondent1().getPartyEmail() != null) {
+            log.info("Start PIP email notification to defendantLip for case id {}", caseData.getLegacyCaseReference());
+            generateDefendantLipPIPEmail(caseData);
+        }
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(updatedCase.toMap(objectMapper))
             .build();
+    }
+
+    @Override
+    public Map<String, String> addProperties(CaseData caseData) {
+        return Map.of(
+            RESPONDENT_NAME, getPartyNameBasedOnType(caseData.getRespondent1()),
+            CLAIMANT_NAME, getPartyNameBasedOnType(caseData.getApplicant1()),
+            ISSUED_ON, formatLocalDate(caseData.getIssueDate(), DATE),
+            RESPOND_URL, pipInPostConfiguration.getRespondToClaimUrl(),
+            CLAIM_REFERENCE_NUMBER, caseData.getLegacyCaseReference(),
+            CLAIM_16_DIGIT_NUMBER, caseData.getCcdCaseReference().toString(),
+            PIN, caseData.getRespondent1PinToPostLRspec().getAccessCode(),
+            RESPONSE_DEADLINE, formatLocalDate(caseData.getRespondent1ResponseDeadline()
+                                                   .toLocalDate(), DATE),
+            FRONTEND_URL, pipInPostConfiguration.getCuiFrontEndUrl()
+        );
+    }
+
+    private void generateDefendantLipPIPEmail(CaseData caseData) {
+        notificationService.sendMail(
+            caseData.getRespondent1().getPartyEmail(),
+            notificationsProperties.getRespondentDefendantResponseForSpec(),
+            addProperties(caseData),
+            String.format(REFERENCE_TEMPLATE, caseData.getLegacyCaseReference())
+        );
     }
 }
--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ResetPinCUICallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ResetPinCUICallbackHandlerTest.java
@@ -10,20 +10,33 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.config.PinInPostConfiguration;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.DefendantPinToPostLRspec;
+import uk.gov.hmcts.reform.civil.notify.NotificationService;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+import uk.gov.hmcts.reform.civil.sampledata.PartyBuilder;
 import uk.gov.hmcts.reform.civil.service.pininpost.DefendantPinToPostLRspecService;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.never;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.*;
+import static uk.gov.hmcts.reform.civil.helpers.DateFormatHelper.DATE;
+import static uk.gov.hmcts.reform.civil.helpers.DateFormatHelper.formatLocalDate;
+import static uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder.LEGACY_CASE_REFERENCE;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {
@@ -36,6 +49,12 @@ public class ResetPinCUICallbackHandlerTest extends BaseCallbackHandlerTest {
     private DefendantPinToPostLRspecService defendantPinToPostLRspecService;
     @Autowired
     private ResetPinCUICallbackHandler handler;
+    @MockBean
+    private NotificationService notificationService;
+    @MockBean
+    private NotificationsProperties notificationsProperties;
+    @MockBean
+    private PinInPostConfiguration pinInPostConfiguration;
 
     @Nested
     class AboutToSubmitCallback {
@@ -73,6 +92,76 @@ public class ResetPinCUICallbackHandlerTest extends BaseCallbackHandlerTest {
                 .isEqualTo(pin.getExpiryDate().toString());
             verify(defendantPinToPostLRspecService, times(1)).resetPinExpiryDate(givenPin);
         }
+
+        @Test
+        void shouldNotifyDefendantLip_whenInvoked() {
+            // Given
+            CaseData caseData = CaseDataBuilder.builder().atStateClaimIssued1v1LiP()
+                .respondent1(PartyBuilder.builder().soleTrader().build().toBuilder()
+                                 .build())
+                .addRespondent1PinToPostLRspec(DefendantPinToPostLRspec.builder()
+                                                   .accessCode("TEST1234")
+                                                   .expiryDate(LocalDate.now().plusDays(180))
+                                                   .build())
+                .claimDetailsNotificationDate(LocalDateTime.now())
+                .respondent1ResponseDeadline(LocalDateTime.now())
+                .build();
+            when(notificationsProperties.getRespondentDefendantResponseForSpec())
+                .thenReturn("template-id");
+            when(pinInPostConfiguration.getRespondToClaimUrl()).thenReturn("dummy_respond_to_claim_url");
+            when(pinInPostConfiguration.getCuiFrontEndUrl()).thenReturn("dummy_cui_front_end_url");
+
+            CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+
+            // When
+            AboutToStartOrSubmitCallbackResponse response = (AboutToStartOrSubmitCallbackResponse) handler
+                .handle(params);
+
+            // Then
+            verify(notificationService).sendMail(
+                "sole.trader@email.com",
+                "template-id",
+                getNotificationDataMap(caseData),
+                "claim-continuing-online-notification-000DC001"
+            );
+        }
+
+        @Test
+        void shouldNotNotifyDefendantLip_whenNoEmailIsEntered() {
+            // Given
+            CaseData caseData = CaseDataBuilder.builder()
+                .respondent1(PartyBuilder.builder().soleTrader().build().toBuilder()
+                                 .partyEmail(null)
+                                 .build())
+                .addRespondent1PinToPostLRspec(DefendantPinToPostLRspec.builder()
+                                                   .accessCode("TEST1234")
+                                                   .expiryDate(LocalDate.now().plusDays(180))
+                                                   .build())
+               .build();
+            caseData.toBuilder().respondent1Represented(YesOrNo.NO).specRespondent1Represented(YesOrNo.NO).build();
+            CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+
+            // When
+            handler.handle(params);
+
+            // Then
+            verify(notificationService, never()).sendMail(any(), any(), any(), any());
+        }
     }
 
+    private Map<String, String> getNotificationDataMap(CaseData caseData) {
+        return Map.of(
+            RESPONDENT_NAME, "Mr. Sole Trader",
+            CLAIMANT_NAME, "Mr. John Rambo",
+            ISSUED_ON, formatLocalDate(LocalDate.now(), DATE),
+            RESPOND_URL, "dummy_respond_to_claim_url",
+            CLAIM_REFERENCE_NUMBER, LEGACY_CASE_REFERENCE,
+            CLAIM_16_DIGIT_NUMBER, CASE_ID.toString(),
+            PIN, "TEST1234",
+            RESPONSE_DEADLINE, formatLocalDate(
+                caseData.getRespondent1ResponseDeadline()
+                    .toLocalDate(), DATE),
+            FRONTEND_URL, "dummy_cui_front_end_url"
+        );
+    }
 }
