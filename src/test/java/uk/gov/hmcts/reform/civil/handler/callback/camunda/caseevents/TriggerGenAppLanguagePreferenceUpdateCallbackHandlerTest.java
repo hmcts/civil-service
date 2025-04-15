98cf5bd969f193c20471a75d88fce8219de6e2dc
paul-pearson <paul.pearson@version1.com>
2025-04-15 09:02:36 +0100

    CIV-16847 Fix test

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/caseevents/TriggerGenAppLanguagePreferenceUpdateCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/caseevents/TriggerGenAppLanguagePreferenceUpdateCallbackHandlerTest.java
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
-import static uk.gov.hmcts.reform.civil.callback.CaseEvent.TRIGGER_LOCATION_UPDATE;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.TRIGGER_GA_LANGUAGE_UPDATE;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UPDATE_GA_LANGUAGE_PREFERENCE;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,7 +49,7 @@ public class TriggerGenAppLanguagePreferenceUpdateCallbackHandlerTest {
                 .build();
             handler.handle(params);
 
-            verify(helperService, times(1)).triggerEvent(caseData, TRIGGER_LOCATION_UPDATE);
+            verify(helperService, times(1)).triggerEvent(caseData, TRIGGER_GA_LANGUAGE_UPDATE);
             verifyNoMoreInteractions(helperService);
         }
 
