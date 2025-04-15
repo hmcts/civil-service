6c10a9157fe0ff6425d89b36b19c4bfbc8a8fabe
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2025-04-15 11:58:02 +0100

    CIV-15925 checkstyle fix

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ResetPinCUICallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ResetPinCUICallbackHandlerTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.config.PinInPostConfiguration;
-import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.DefendantPinToPostLRspec;
