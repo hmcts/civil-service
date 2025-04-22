44460700b133a6c456a96a72e2b3996aa9c74cd7
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2025-04-22 12:11:06 +0100

    Revert "CIV-16777 Part admit LR Carm (#6387)"
    This reverts commit 4711ab1c92251b4e18c24f60218baf19d1a392a9.

--- src/main/java/uk/gov/hmcts/reform/civil/utils/CaseStateUtils.java
+++ src/main/java/uk/gov/hmcts/reform/civil/utils/CaseStateUtils.java
@@ -4,6 +4,7 @@ import uk.gov.hmcts.reform.civil.constants.SpecJourneyConstantLRSpec;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
+import static uk.gov.hmcts.reform.civil.enums.YesOrNo.YES;
 
 public class CaseStateUtils {
 
@@ -13,7 +14,8 @@ public class CaseStateUtils {
 
     public static boolean shouldMoveToInMediationState(CaseData caseData, boolean carmEnabled) {
         if (carmEnabled && SpecJourneyConstantLRSpec.SMALL_CLAIM.equals(caseData.getResponseClaimTrack())) {
-            return caseData.hasApplicantProceededWithClaim()
+            return YES.equals(caseData.getApplicant1ProceedWithClaim())
+                || YES.equals(caseData.getApplicant1ProceedWithClaimSpec2v1())
                 || (caseData.getCaseDataLiP() != null
                 && NO.equals(caseData.getCaseDataLiP().getApplicant1SettleClaim()));
         }
