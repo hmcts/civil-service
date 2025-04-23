e053acb427b82fe8227dd2c255dce2a45abd05b7
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2025-04-23 12:05:59 +0100

    DTSCCI-1461  Add Lip v Lip, LR v Lip, Lip v LR email generators & replace state flow engine checks (#6449)
    * Add Lip v Lip email generators
    
    * Add NocLipDefendant changes
    
    * Update respondent1Email
    
    ---------
    
    Co-authored-by: mounikaammineni <Ayra@23082019>

---
+++ src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/DefendantEmailDTOGenerator.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.reform.civil.notification.handlers;
+
+import lombok.AllArgsConstructor;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.Party;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+public abstract class DefendantEmailDTOGenerator extends EmailDTOGenerator {
+
+    @Override
+    public String getEmailAddress(CaseData caseData) {
+        return caseData.getRespondent1PartyEmail();
+    }
+}
