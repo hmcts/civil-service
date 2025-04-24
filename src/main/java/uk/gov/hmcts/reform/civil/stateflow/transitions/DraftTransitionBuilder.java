98a7212ba24b3eb385aa88584d438349bfae3090
paul-pearson <paul.pearson@version1.com>
2025-04-24 17:02:08 +0100

    CIV-16832 Hide sdo when bilingual party

--- src/main/java/uk/gov/hmcts/reform/civil/stateflow/transitions/DraftTransitionBuilder.java
+++ src/main/java/uk/gov/hmcts/reform/civil/stateflow/transitions/DraftTransitionBuilder.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.civil.stateflow.transitions;
 
 import uk.gov.hmcts.reform.civil.enums.CaseState;
+import uk.gov.hmcts.reform.civil.enums.dq.Language;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.service.flowstate.FlowFlag;
@@ -13,6 +14,7 @@ import java.util.function.Predicate;
 
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.YES;
+import static uk.gov.hmcts.reform.civil.service.flowstate.FlowFlag.BILINGUAL_DOCS;
 import static uk.gov.hmcts.reform.civil.service.flowstate.FlowFlag.BULK_CLAIM_ENABLED;
 import static uk.gov.hmcts.reform.civil.service.flowstate.FlowFlag.CASE_PROGRESSION_ENABLED;
 import static uk.gov.hmcts.reform.civil.service.flowstate.FlowFlag.CLAIM_STATE_DURING_NOC;
@@ -21,6 +23,7 @@ import static uk.gov.hmcts.reform.civil.service.flowstate.FlowFlag.DEFENDANT_NOC
 import static uk.gov.hmcts.reform.civil.service.flowstate.FlowFlag.GENERAL_APPLICATION_ENABLED;
 import static uk.gov.hmcts.reform.civil.service.flowstate.FlowFlag.IS_JO_LIVE_FEED_ACTIVE;
 import static uk.gov.hmcts.reform.civil.service.flowstate.FlowFlag.JO_ONLINE_LIVE_ENABLED;
+import static uk.gov.hmcts.reform.civil.service.flowstate.FlowFlag.WELSH_ENABLED;
 import static uk.gov.hmcts.reform.civil.service.flowstate.FlowPredicate.caseContainsLiP;
 import static uk.gov.hmcts.reform.civil.service.flowstate.FlowState.Main.CLAIM_SUBMITTED;
 
@@ -37,16 +40,18 @@ public abstract class DraftTransitionBuilder extends TransitionBuilder {
             .set((c, flags) -> flags.putAll(
                 // Do not set UNREPRESENTED_DEFENDANT_ONE or UNREPRESENTED_DEFENDANT_TWO to false here unless
                 // camunda diagram for TAKE_CASE_OFFLINE is changed
-                Map.of(
-                    FlowFlag.ONE_RESPONDENT_REPRESENTATIVE.name(), true,
-                    GENERAL_APPLICATION_ENABLED.name(), switchTheGAFlagIfLipExists(c),
-                    DASHBOARD_SERVICE_ENABLED.name(), isDashBoardEnabledForCase(c),
-                    CASE_PROGRESSION_ENABLED.name(), featureToggleService.isCaseProgressionEnabled(),
-                    BULK_CLAIM_ENABLED.name(), featureToggleService.isBulkClaimEnabled(),
-                    JO_ONLINE_LIVE_ENABLED.name(), featureToggleService.isJudgmentOnlineLive(),
-                    IS_JO_LIVE_FEED_ACTIVE.name(), featureToggleService.isJOLiveFeedActive(),
-                    DEFENDANT_NOC_ONLINE.name(), featureToggleService.isDefendantNoCOnlineForCase(c),
-                    CLAIM_STATE_DURING_NOC.name(), getMainClaimCcdState(c)
+                Map.ofEntries(
+                    Map.entry(FlowFlag.ONE_RESPONDENT_REPRESENTATIVE.name(), true),
+                    Map.entry(GENERAL_APPLICATION_ENABLED.name(), switchTheGAFlagIfLipExists(c)),
+                    Map.entry(DASHBOARD_SERVICE_ENABLED.name(), isDashBoardEnabledForCase(c)),
+                    Map.entry(CASE_PROGRESSION_ENABLED.name(), featureToggleService.isCaseProgressionEnabled()),
+                    Map.entry(BULK_CLAIM_ENABLED.name(), featureToggleService.isBulkClaimEnabled()),
+                    Map.entry(JO_ONLINE_LIVE_ENABLED.name(), featureToggleService.isJudgmentOnlineLive()),
+                    Map.entry(IS_JO_LIVE_FEED_ACTIVE.name(), featureToggleService.isJOLiveFeedActive()),
+                    Map.entry(DEFENDANT_NOC_ONLINE.name(), featureToggleService.isDefendantNoCOnlineForCase(c)),
+                    Map.entry(CLAIM_STATE_DURING_NOC.name(), getMainClaimCcdState(c)),
+                    Map.entry(WELSH_ENABLED.name(), featureToggleService.isGaForWelshEnabled()),
+                    Map.entry(BILINGUAL_DOCS.name(), lipPartySpecifiedBilingualDocs(c))
                 )), transitions)
             .moveTo(CLAIM_SUBMITTED, transitions)
             .onlyWhen(claimSubmittedTwoRegisteredRespondentRepresentatives
@@ -55,32 +60,36 @@ public abstract class DraftTransitionBuilder extends TransitionBuilder {
             .set((c, flags) -> flags.putAll(
                 // Do not set UNREPRESENTED_DEFENDANT_ONE or UNREPRESENTED_DEFENDANT_TWO to false here unless
                 // camunda diagram for TAKE_CASE_OFFLINE is changed
-                Map.of(
-                    FlowFlag.ONE_RESPONDENT_REPRESENTATIVE.name(), false,
-                    FlowFlag.TWO_RESPONDENT_REPRESENTATIVES.name(), true,
-                    GENERAL_APPLICATION_ENABLED.name(), switchTheGAFlagIfLipExists(c),
-                    DASHBOARD_SERVICE_ENABLED.name(), isDashBoardEnabledForCase(c),
-                    CASE_PROGRESSION_ENABLED.name(), featureToggleService.isCaseProgressionEnabled(),
-                    BULK_CLAIM_ENABLED.name(), featureToggleService.isBulkClaimEnabled(),
-                    JO_ONLINE_LIVE_ENABLED.name(), featureToggleService.isJudgmentOnlineLive(),
-                    IS_JO_LIVE_FEED_ACTIVE.name(), featureToggleService.isJOLiveFeedActive(),
-                    DEFENDANT_NOC_ONLINE.name(), featureToggleService.isDefendantNoCOnlineForCase(c),
-                    CLAIM_STATE_DURING_NOC.name(), getMainClaimCcdState(c)
+                Map.ofEntries(
+                    Map.entry(FlowFlag.ONE_RESPONDENT_REPRESENTATIVE.name(), false),
+                    Map.entry(FlowFlag.TWO_RESPONDENT_REPRESENTATIVES.name(), true),
+                    Map.entry(GENERAL_APPLICATION_ENABLED.name(), switchTheGAFlagIfLipExists(c)),
+                    Map.entry(DASHBOARD_SERVICE_ENABLED.name(), isDashBoardEnabledForCase(c)),
+                    Map.entry(CASE_PROGRESSION_ENABLED.name(), featureToggleService.isCaseProgressionEnabled()),
+                    Map.entry(BULK_CLAIM_ENABLED.name(), featureToggleService.isBulkClaimEnabled()),
+                    Map.entry(JO_ONLINE_LIVE_ENABLED.name(), featureToggleService.isJudgmentOnlineLive()),
+                    Map.entry(IS_JO_LIVE_FEED_ACTIVE.name(), featureToggleService.isJOLiveFeedActive()),
+                    Map.entry(DEFENDANT_NOC_ONLINE.name(), featureToggleService.isDefendantNoCOnlineForCase(c)),
+                    Map.entry(CLAIM_STATE_DURING_NOC.name(), getMainClaimCcdState(c)),
+                    Map.entry(WELSH_ENABLED.name(), featureToggleService.isGaForWelshEnabled()),
+                    Map.entry(BILINGUAL_DOCS.name(), lipPartySpecifiedBilingualDocs(c))
                 )), transitions)
             // Only one unrepresented defendant
             .moveTo(CLAIM_SUBMITTED, transitions)
             .onlyWhen(claimSubmittedOneUnrepresentedDefendantOnly, transitions)
             .set((c, flags) -> flags.putAll(
-                Map.of(
-                    FlowFlag.UNREPRESENTED_DEFENDANT_ONE.name(), true,
-                    GENERAL_APPLICATION_ENABLED.name(), switchTheGAFlagIfLipExists(c),
-                    DASHBOARD_SERVICE_ENABLED.name(), isDashBoardEnabledForCase(c),
-                    CASE_PROGRESSION_ENABLED.name(), featureToggleService.isCaseProgressionEnabled(),
-                    BULK_CLAIM_ENABLED.name(), featureToggleService.isBulkClaimEnabled(),
-                    JO_ONLINE_LIVE_ENABLED.name(), featureToggleService.isJudgmentOnlineLive(),
-                    IS_JO_LIVE_FEED_ACTIVE.name(), featureToggleService.isJOLiveFeedActive(),
-                    DEFENDANT_NOC_ONLINE.name(), featureToggleService.isDefendantNoCOnlineForCase(c),
-                    CLAIM_STATE_DURING_NOC.name(), getMainClaimCcdState(c)
+                Map.ofEntries(
+                    Map.entry(FlowFlag.UNREPRESENTED_DEFENDANT_ONE.name(), true),
+                    Map.entry(GENERAL_APPLICATION_ENABLED.name(), switchTheGAFlagIfLipExists(c)),
+                    Map.entry(DASHBOARD_SERVICE_ENABLED.name(), isDashBoardEnabledForCase(c)),
+                    Map.entry(CASE_PROGRESSION_ENABLED.name(), featureToggleService.isCaseProgressionEnabled()),
+                    Map.entry(BULK_CLAIM_ENABLED.name(), featureToggleService.isBulkClaimEnabled()),
+                    Map.entry(JO_ONLINE_LIVE_ENABLED.name(), featureToggleService.isJudgmentOnlineLive()),
+                    Map.entry(IS_JO_LIVE_FEED_ACTIVE.name(), featureToggleService.isJOLiveFeedActive()),
+                    Map.entry(DEFENDANT_NOC_ONLINE.name(), featureToggleService.isDefendantNoCOnlineForCase(c)),
+                    Map.entry(CLAIM_STATE_DURING_NOC.name(), getMainClaimCcdState(c)),
+                    Map.entry(WELSH_ENABLED.name(), featureToggleService.isGaForWelshEnabled()),
+                    Map.entry(BILINGUAL_DOCS.name(), lipPartySpecifiedBilingualDocs(c))
                 )), transitions)
             // Unrepresented defendant 1
             .moveTo(CLAIM_SUBMITTED, transitions)
@@ -88,51 +97,57 @@ public abstract class DraftTransitionBuilder extends TransitionBuilder {
                 .and(claimSubmittedOneUnrepresentedDefendantOnly.negate())
                 .and(claimSubmittedRespondent2Unrepresented.negate()), transitions)
             .set((c, flags) -> flags.putAll(
-                Map.of(
-                    FlowFlag.UNREPRESENTED_DEFENDANT_ONE.name(), true,
-                    FlowFlag.UNREPRESENTED_DEFENDANT_TWO.name(), false,
-                    GENERAL_APPLICATION_ENABLED.name(), switchTheGAFlagIfLipExists(c),
-                    DASHBOARD_SERVICE_ENABLED.name(), isDashBoardEnabledForCase(c),
-                    CASE_PROGRESSION_ENABLED.name(), featureToggleService.isCaseProgressionEnabled(),
-                    BULK_CLAIM_ENABLED.name(), featureToggleService.isBulkClaimEnabled(),
-                    JO_ONLINE_LIVE_ENABLED.name(), featureToggleService.isJudgmentOnlineLive(),
-                    IS_JO_LIVE_FEED_ACTIVE.name(), featureToggleService.isJOLiveFeedActive(),
-                    DEFENDANT_NOC_ONLINE.name(), featureToggleService.isDefendantNoCOnlineForCase(c),
-                    CLAIM_STATE_DURING_NOC.name(), getMainClaimCcdState(c)
+                Map.ofEntries(
+                    Map.entry(FlowFlag.UNREPRESENTED_DEFENDANT_ONE.name(), true),
+                    Map.entry(FlowFlag.UNREPRESENTED_DEFENDANT_TWO.name(), false),
+                    Map.entry(GENERAL_APPLICATION_ENABLED.name(), switchTheGAFlagIfLipExists(c)),
+                    Map.entry(DASHBOARD_SERVICE_ENABLED.name(), isDashBoardEnabledForCase(c)),
+                    Map.entry(CASE_PROGRESSION_ENABLED.name(), featureToggleService.isCaseProgressionEnabled()),
+                    Map.entry(BULK_CLAIM_ENABLED.name(), featureToggleService.isBulkClaimEnabled()),
+                    Map.entry(JO_ONLINE_LIVE_ENABLED.name(), featureToggleService.isJudgmentOnlineLive()),
+                    Map.entry(IS_JO_LIVE_FEED_ACTIVE.name(), featureToggleService.isJOLiveFeedActive()),
+                    Map.entry(DEFENDANT_NOC_ONLINE.name(), featureToggleService.isDefendantNoCOnlineForCase(c)),
+                    Map.entry(CLAIM_STATE_DURING_NOC.name(), getMainClaimCcdState(c)),
+                    Map.entry(WELSH_ENABLED.name(), featureToggleService.isGaForWelshEnabled()),
+                    Map.entry(BILINGUAL_DOCS.name(), lipPartySpecifiedBilingualDocs(c))
                 )), transitions)
             // Unrepresented defendant 2
             .moveTo(CLAIM_SUBMITTED, transitions)
             .onlyWhen(claimSubmittedRespondent2Unrepresented
                 .and(claimSubmittedRespondent1Unrepresented.negate()), transitions)
             .set((c, flags) -> flags.putAll(
-                Map.of(
-                    FlowFlag.UNREPRESENTED_DEFENDANT_ONE.name(), false,
-                    FlowFlag.UNREPRESENTED_DEFENDANT_TWO.name(), true,
-                    GENERAL_APPLICATION_ENABLED.name(), switchTheGAFlagIfLipExists(c),
-                    DASHBOARD_SERVICE_ENABLED.name(), isDashBoardEnabledForCase(c),
-                    CASE_PROGRESSION_ENABLED.name(), featureToggleService.isCaseProgressionEnabled(),
-                    BULK_CLAIM_ENABLED.name(), featureToggleService.isBulkClaimEnabled(),
-                    JO_ONLINE_LIVE_ENABLED.name(), featureToggleService.isJudgmentOnlineLive(),
-                    IS_JO_LIVE_FEED_ACTIVE.name(), featureToggleService.isJOLiveFeedActive(),
-                    DEFENDANT_NOC_ONLINE.name(), featureToggleService.isDefendantNoCOnlineForCase(c),
-                    CLAIM_STATE_DURING_NOC.name(), getMainClaimCcdState(c)
+                Map.ofEntries(
+                    Map.entry(FlowFlag.UNREPRESENTED_DEFENDANT_ONE.name(), false),
+                    Map.entry(FlowFlag.UNREPRESENTED_DEFENDANT_TWO.name(), true),
+                    Map.entry(GENERAL_APPLICATION_ENABLED.name(), switchTheGAFlagIfLipExists(c)),
+                    Map.entry(DASHBOARD_SERVICE_ENABLED.name(), isDashBoardEnabledForCase(c)),
+                    Map.entry(CASE_PROGRESSION_ENABLED.name(), featureToggleService.isCaseProgressionEnabled()),
+                    Map.entry(BULK_CLAIM_ENABLED.name(), featureToggleService.isBulkClaimEnabled()),
+                    Map.entry(JO_ONLINE_LIVE_ENABLED.name(), featureToggleService.isJudgmentOnlineLive()),
+                    Map.entry(IS_JO_LIVE_FEED_ACTIVE.name(), featureToggleService.isJOLiveFeedActive()),
+                    Map.entry(DEFENDANT_NOC_ONLINE.name(), featureToggleService.isDefendantNoCOnlineForCase(c)),
+                    Map.entry(CLAIM_STATE_DURING_NOC.name(), getMainClaimCcdState(c)),
+                    Map.entry(WELSH_ENABLED.name(), featureToggleService.isGaForWelshEnabled()),
+                    Map.entry(BILINGUAL_DOCS.name(), lipPartySpecifiedBilingualDocs(c))
                 )), transitions)
             // Unrepresented defendants
             .moveTo(CLAIM_SUBMITTED, transitions)
             .onlyWhen(claimSubmittedRespondent1Unrepresented.and(
                 claimSubmittedRespondent2Unrepresented), transitions)
             .set((c, flags) -> flags.putAll(
-                Map.of(
-                    FlowFlag.UNREPRESENTED_DEFENDANT_ONE.name(), true,
-                    FlowFlag.UNREPRESENTED_DEFENDANT_TWO.name(), true,
-                    GENERAL_APPLICATION_ENABLED.name(), switchTheGAFlagIfLipExists(c),
-                    DASHBOARD_SERVICE_ENABLED.name(), isDashBoardEnabledForCase(c),
-                    CASE_PROGRESSION_ENABLED.name(), featureToggleService.isCaseProgressionEnabled(),
-                    BULK_CLAIM_ENABLED.name(), featureToggleService.isBulkClaimEnabled(),
-                    JO_ONLINE_LIVE_ENABLED.name(), featureToggleService.isJudgmentOnlineLive(),
-                    IS_JO_LIVE_FEED_ACTIVE.name(), featureToggleService.isJOLiveFeedActive(),
-                    DEFENDANT_NOC_ONLINE.name(), featureToggleService.isDefendantNoCOnlineForCase(c),
-                    CLAIM_STATE_DURING_NOC.name(), getMainClaimCcdState(c)
+                Map.ofEntries(
+                    Map.entry(FlowFlag.UNREPRESENTED_DEFENDANT_ONE.name(), true),
+                    Map.entry(FlowFlag.UNREPRESENTED_DEFENDANT_TWO.name(), true),
+                    Map.entry(GENERAL_APPLICATION_ENABLED.name(), switchTheGAFlagIfLipExists(c)),
+                    Map.entry(DASHBOARD_SERVICE_ENABLED.name(), isDashBoardEnabledForCase(c)),
+                    Map.entry(CASE_PROGRESSION_ENABLED.name(), featureToggleService.isCaseProgressionEnabled()),
+                    Map.entry(BULK_CLAIM_ENABLED.name(), featureToggleService.isBulkClaimEnabled()),
+                    Map.entry(JO_ONLINE_LIVE_ENABLED.name(), featureToggleService.isJudgmentOnlineLive()),
+                    Map.entry(IS_JO_LIVE_FEED_ACTIVE.name(), featureToggleService.isJOLiveFeedActive()),
+                    Map.entry(DEFENDANT_NOC_ONLINE.name(), featureToggleService.isDefendantNoCOnlineForCase(c)),
+                    Map.entry(CLAIM_STATE_DURING_NOC.name(), getMainClaimCcdState(c)),
+                    Map.entry(WELSH_ENABLED.name(), featureToggleService.isGaForWelshEnabled()),
+                    Map.entry(BILINGUAL_DOCS.name(), lipPartySpecifiedBilingualDocs(c))
                 )), transitions);
     }
 
@@ -188,6 +203,10 @@ public abstract class DraftTransitionBuilder extends TransitionBuilder {
             && caseData.getRespondent2OrgRegistered() == NO
             && (caseData.getRespondent2SameLegalRepresentative() == NO
             || caseData.getRespondent2SameLegalRepresentative() == null);
+
+    public boolean lipPartySpecifiedBilingualDocs(CaseData caseData) {
+        return (caseData.isLipClaimantSpecifiedBilingualDocuments() || caseData.isLipDefendantSpecifiedBilingualDocuments());
+    }
 
     public boolean isDashBoardEnabledForCase(CaseData caseData) {
         return featureToggleService.isDashboardEnabledForCase(caseData) && caseContainsLiP.test(caseData);
