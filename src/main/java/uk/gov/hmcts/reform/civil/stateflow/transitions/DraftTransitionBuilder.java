76d8d16216699f2c11ce235cd0b44274c37dea6e
paul-pearson <paul.pearson@version1.com>
2025-04-24 17:15:10 +0100

    CIV-16832 Remove unused import

--- src/main/java/uk/gov/hmcts/reform/civil/stateflow/transitions/DraftTransitionBuilder.java
+++ src/main/java/uk/gov/hmcts/reform/civil/stateflow/transitions/DraftTransitionBuilder.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.civil.stateflow.transitions;
 
 import uk.gov.hmcts.reform.civil.enums.CaseState;
-import uk.gov.hmcts.reform.civil.enums.dq.Language;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.service.flowstate.FlowFlag;
