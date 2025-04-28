276962fc65dae7ff7bb0e27610b0de15d82b977c
gmmagruder <gabrielle.magruder@gmail.com>
2025-04-28 21:46:45 +0100

    Add tests for ClaimantResponseNotAgreedRepayment

---
+++ src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/claimantresponsenotagreedrepayment/ClaimantResponseNotAgreedRepaymentClaimantEmailDTOGeneratorTest.java
@@ -0,0 +1,68 @@
+package uk.gov.hmcts.reform.civil.notification.handlers.claimantresponsenotagreedrepayment;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+import uk.gov.hmcts.reform.civil.utils.PartyUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIMANT_NAME;
+import static uk.gov.hmcts.reform.civil.utils.PartyUtils.getPartyNameBasedOnType;
+
+public class ClaimantResponseNotAgreedRepaymentClaimantEmailDTOGeneratorTest {
+
+    @Mock
+    private NotificationsProperties notificationsProperties;
+
+    @InjectMocks
+    private ClaimantResponseNotAgreedRepaymentClaimantEmailDTOGenerator emailDTOGenerator;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void shouldReturnCorrectEmailTemplateId() {
+        CaseData caseData = CaseData.builder().build();
+        String expectedTemplateId = "template-id";
+        when(notificationsProperties.getNotifyClaimantLipTemplateManualDetermination()).thenReturn(expectedTemplateId);
+
+        String actualTemplateId = emailDTOGenerator.getEmailTemplateId(caseData);
+
+        assertThat(actualTemplateId).isEqualTo(expectedTemplateId);
+    }
+
+    @Test
+    void shouldReturnCorrectReferenceTemplate() {
+        String referenceTemplate = emailDTOGenerator.getReferenceTemplate();
+
+        assertThat(referenceTemplate).isEqualTo("claimant-reject-repayment-respondent-notification-%s");
+    }
+
+    @Test
+    void shouldAddCustomProperties() {
+        CaseData caseData = CaseData.builder().build();
+        MockedStatic<PartyUtils> partyUtilsMockedStatic = Mockito.mockStatic(PartyUtils.class);
+
+        String partyName = "party name";
+        partyUtilsMockedStatic.when(() -> getPartyNameBasedOnType(any())).thenReturn(partyName);
+
+        Map<String, String> properties = new HashMap<>();
+        Map<String, String> updatedProperties = emailDTOGenerator.addCustomProperties(properties, caseData);
+
+        assertThat(updatedProperties.size()).isEqualTo(1);
+        updatedProperties.put(CLAIMANT_NAME, partyName);
+    }
+}
