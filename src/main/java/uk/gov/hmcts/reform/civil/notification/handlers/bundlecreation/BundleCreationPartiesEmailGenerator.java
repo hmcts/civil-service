3cbe3abc16eac27f8291d31b0036773c2b3a12e0
gmmagruder <gabrielle.magruder@gmail.com>
2025-04-30 15:08:30 +0100

    refactor package name

--- src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/bundlecreation/BundleCreationPartiesEmailGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/bundlecreation/BundleCreationPartiesEmailGenerator.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.civil.notification.handlers.bundlecreationn;
+package uk.gov.hmcts.reform.civil.notification.handlers.bundlecreation;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
