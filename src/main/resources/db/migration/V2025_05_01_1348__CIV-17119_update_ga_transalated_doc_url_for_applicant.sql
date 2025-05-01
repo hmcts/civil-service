251df75fed1c7238f339ab1192cb17265073f60f
Deepthi Doppalapudi <107422736+deepthidoppalapudihmcts@users.noreply.github.com>
2025-05-01 13:50:11 +0100

    CIV-17119 updated script translated document for applicant dashboard notification.

---
+++ src/main/resources/db/migration/V2025_05_01_1348__CIV-17119_update_ga_transalated_doc_url_for_applicant.sql
@@ -0,0 +1,7 @@
+/**
+ * Removing full stop and new line from link
+ */
+UPDATE dbs.dashboard_notifications_templates SET description_En = replace(description_En, '<p class="govuk-body"><a href="{GA_VIEW_APPLICATION_URL}" class="govuk-link">View translated application documents.</a></p>')
+                                             WHERE template_name in ('Notice.AAA6.GeneralApps.TranslatedDocumentUploaded.Applicant');
+UPDATE dbs.dashboard_notifications_templates SET description_Cy = replace(description_Cy, '<p class="govuk-body"><a href="{GA_VIEW_APPLICATION_URL}" class="govuk-link">Gweld dogfennau’r cais a gyfieithwyd.</a></p>')
+                                             WHERE template_name in ('Notice.AAA6.GeneralApps.TranslatedDocumentUploaded.Applicant');
