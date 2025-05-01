43f2da59ee5811904273e2772e8d2e235da0fb62
Deepthi Doppalapudi <107422736+deepthidoppalapudihmcts@users.noreply.github.com>
2025-05-01 14:32:06 +0100

    CIV-17119 updated script

--- src/main/resources/db/migration/V2025_05_01_1348__CIV-17119_update_ga_transalated_doc_url_for_applicant.sql
+++ src/main/resources/db/migration/V2025_05_01_1348__CIV-17119_update_ga_transalated_doc_url_for_applicant.sql
@@ -1,7 +1,7 @@
 /**
- * Removing full stop and new line from link
+ * update link on the notification url
  */
-UPDATE dbs.dashboard_notifications_templates SET description_En = replace(description_En, '<p class="govuk-body"><a href="{GA_VIEW_APPLICATION_URL}" class="govuk-link">View translated application documents.</a></p>')
+UPDATE dbs.dashboard_notifications_templates SET description_En = replace(description_En, '<p class="govuk-body"><a href="{GA_RESPONSE_VIEW_APPLICATION_URL}" class="govuk-link">View translated application documents.</a></p>', '<p class="govuk-body"><a href="{GA_VIEW_APPLICATION_URL}" class="govuk-link">View translated application documents.</a></p>')
                                              WHERE template_name in ('Notice.AAA6.GeneralApps.TranslatedDocumentUploaded.Applicant');
-UPDATE dbs.dashboard_notifications_templates SET description_Cy = replace(description_Cy, '<p class="govuk-body"><a href="{GA_VIEW_APPLICATION_URL}" class="govuk-link">Gweld dogfennau’r cais a gyfieithwyd.</a></p>')
+UPDATE dbs.dashboard_notifications_templates SET description_Cy = replace(description_Cy, '<p class="govuk-body"><a href="{GA_RESPONSE_VIEW_APPLICATION_URL}" class="govuk-link">Gweld dogfennau’r cais a gyfieithwyd.</a></p>', '<p class="govuk-body"><a href="{GA_VIEW_APPLICATION_URL}" class="govuk-link">Gweld dogfennau’r cais a gyfieithwyd.</a></p>')
                                              WHERE template_name in ('Notice.AAA6.GeneralApps.TranslatedDocumentUploaded.Applicant');
