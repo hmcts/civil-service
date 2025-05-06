3a5cd77eb36fdaff8a65e07c553f8778405e0792
Ruban <ruban72@hotmail.com>
2025-05-06 10:00:39 +0100

    DTSCCI-2079: Update servlet libraries

--- src/main/java/uk/gov/hmcts/reform/civil/filters/RequestFilter.java
+++ src/main/java/uk/gov/hmcts/reform/civil/filters/RequestFilter.java
@@ -1,24 +1,21 @@
 package uk.gov.hmcts.reform.civil.filters;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
-import javax.servlet.FilterChain;
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.validation.constraints.NotNull;
 
 @Component
 public class RequestFilter extends OncePerRequestFilter {
-
     @Override
-    protected void doFilterInternal(@NotNull HttpServletRequest request,
-                                    @NotNull HttpServletResponse response,
-                                    @NotNull FilterChain filterChain) throws ServletException, IOException {
-        ContentCachingRequestWrapper cachedBodyHttpServletRequest =
+    protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response,
+                                    javax.servlet.FilterChain filterChain) throws ServletException, IOException {
+                ContentCachingRequestWrapper cachedBodyHttpServletRequest =
             new ContentCachingRequestWrapper(request);
 
         filterChain.doFilter(cachedBodyHttpServletRequest, response);
