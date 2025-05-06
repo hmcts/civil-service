41a713f3ecda6a00117a3f0f2dee51db272d50c4
Ruban <ruban72@hotmail.com>
2025-05-06 12:00:47 +0100

    DTSCCI-2079: Tomcat suppressions

--- src/main/java/uk/gov/hmcts/reform/civil/filters/RequestFilter.java
+++ src/main/java/uk/gov/hmcts/reform/civil/filters/RequestFilter.java
@@ -1,26 +1,23 @@
 package uk.gov.hmcts.reform.civil.filters;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
-import java.io.IOException;
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.constraints.NotNull;
+import java.io.IOException;
 
 @Component
 public class RequestFilter extends OncePerRequestFilter {
 
-    /**
-     * This filter caches the request body for logging purposes.
-     * It wraps the request in a ContentCachingRequestWrapper to allow
-     * the request body to be read multiple times.
-     */
     @Override
-    protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response,
-                                    javax.servlet.FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NotNull HttpServletRequest request,
+                                    @NotNull HttpServletResponse response,
+                                    @NotNull FilterChain filterChain) throws ServletException, IOException {
         ContentCachingRequestWrapper cachedBodyHttpServletRequest =
             new ContentCachingRequestWrapper(request);
 
