405979b34ac232a23a66a8bd13010548f03ddf2e
mounikaammineni <Ayra@23082019>
2025-05-09 11:35:32 +0100

    Update

--- src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/changeofrepresentation/AllNoCPartiesEmailGeneratorTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/changeofrepresentation/AllNoCPartiesEmailGeneratorTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class AllNoCPartiesEmailGeneratorTest {
+    
     private NoCFormerSolicitorEmailDTOGenerator formerSolicitorEmailDTOGenerator;
     private NoCOtherSolicitorOneEmailDTOGenerator otherSolicitorOneEmailDTOGenerator;
     private NoCOtherSolicitorTwoEmailDTOGenerator otherSolicitorTwoEmailDTOGenerator;
