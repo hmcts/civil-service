cb1b981f1d38735de641e2ec6e21c975a5486e64
sankaviv1 <sankavi.mohanraj@version1.com>
2025-05-13 10:41:14 +0100

    add time to live delete notif on click

--- src/main/resources/db/migration/V2025_05_08_1029__CIV_16922_court_response_to_query.sql
+++ src/main/resources/db/migration/V2025_05_08_1029__CIV_16922_court_response_to_query.sql
@@ -24,23 +24,23 @@ VALUES ('Scenario.AAA6.LiPQM.QueryResponded.Claimant',
  * Add notification template claimant
  */
 INSERT INTO dbs.dashboard_notifications_templates ( template_name, title_En, title_Cy, description_En, description_Cy
-                                                  , notification_role)
+                                                  , notification_role, time_to_live)
 VALUES ('Notice.AAA6.LiPQM.QueryResponded.Claimant',
         'The court has responded to your message',
         'Mae’r llys wedi ymateb i’ch neges',
         '<p class="govuk-body">The court has responded to the message you sent. <br> <a href="{QM_VIEW_MESSAGES_URL}" target="_blank" rel="noopener noreferrer" class="govuk-link">View the message</a></p>',
         '<p class="govuk-body">Mae''r llys wedi ymateb i''r neges a anfonwyd gennych. <br> <a href="{QM_VIEW_MESSAGES_URL}" target="_blank" rel="noopener noreferrer" class="govuk-link">Gweld y neges</a></p>',
-        'CLAIMANT');
+        'CLAIMANT', 'Click');
 
 /**
  * Add notification template defendant
  */
 INSERT INTO dbs.dashboard_notifications_templates ( template_name, title_En, title_Cy, description_En, description_Cy
-                                                  , notification_role)
+                                                  , notification_role, time_to_live)
 VALUES ('Notice.AAA6.LiPQM.QueryResponded.Defendant',
         'The court has responded to your message',
         'Mae’r llys wedi ymateb i’ch neges',
         '<p class="govuk-body">The court has responded to the message you sent. <br> <a href="{QM_VIEW_MESSAGES_URL}" target="_blank" rel="noopener noreferrer" class="govuk-link">View the message</a></p>',
         '<p class="govuk-body">Mae''r llys wedi ymateb i''r neges a anfonwyd gennych. <br> <a href="{QM_VIEW_MESSAGES_URL}" target="_blank" rel="noopener noreferrer" class="govuk-link">Gweld y neges</a></p>',
-        'DEFENDANT');
+        'DEFENDANT', 'Click');
 
