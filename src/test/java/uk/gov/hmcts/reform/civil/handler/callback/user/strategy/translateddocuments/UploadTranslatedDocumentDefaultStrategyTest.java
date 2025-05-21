49cb7453a8a0ad2a2a33c26d8babf3d17deb1acf
Neeta Shinde <neeta.shinde@hmcts.net>
2025-05-21 18:00:35 +0100

    fixed build issue

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/strategy/translateddocuments/UploadTranslatedDocumentDefaultStrategyTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/strategy/translateddocuments/UploadTranslatedDocumentDefaultStrategyTest.java
@@ -35,7 +35,7 @@ import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.model.citizenui.TranslatedDocumentType.CLAIMANT_INTENTION;
 import static uk.gov.hmcts.reform.civil.model.citizenui.TranslatedDocumentType.ORDER_NOTICE;
 import static uk.gov.hmcts.reform.civil.model.citizenui.TranslatedDocumentType.INTERLOCUTORY_JUDGMENT;
-import static uk.gov.hmcts.reform.civil.model.citizenui.TranslatedDocumentType.ORDER_NOTICE;
+import static uk.gov.hmcts.reform.civil.model.citizenui.TranslatedDocumentType.DEFENDANT_RESPONSE;
 import static uk.gov.hmcts.reform.civil.model.citizenui.TranslatedDocumentType.SETTLEMENT_AGREEMENT;
 import static uk.gov.hmcts.reform.civil.model.citizenui.TranslatedDocumentType.MANUAL_DETERMINATION;
 import static uk.gov.hmcts.reform.civil.model.citizenui.TranslatedDocumentType.CLAIM_ISSUE;
@@ -445,7 +445,7 @@ class UploadTranslatedDocumentDefaultStrategyTest {
             .extracting("camundaEvent")
             .isEqualTo(CaseEvent.UPLOAD_TRANSLATED_DOCUMENT_SETTLEMENT_AGREEMENT.name());
     }
-  
+
     @Test
     void shouldNotCopySealedClaimForm() {
         //Given
@@ -488,5 +488,5 @@ class UploadTranslatedDocumentDefaultStrategyTest {
             .extracting("businessProcess")
             .extracting("camundaEvent")
             .isEqualTo(CaseEvent.UPLOAD_TRANSLATED_DOCUMENT_CLAIM_ISSUE.name());
-    }  
+    }
 }
