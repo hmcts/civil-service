e5cdba39aaeb5ee8d9a273b90fffc4e87e5c1587
paul-pearson <paul.pearson@version1.com>
2025-05-22 16:52:17 +0100

    CIV-16880 Remove unused import

--- src/main/java/uk/gov/hmcts/reform/civil/service/SystemGeneratedDocumentService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/SystemGeneratedDocumentService.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.civil.service;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.citizenui.TranslatedDocument;
