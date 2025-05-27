335f1c51fe19a67367b243c2d459072f98cf5f46
gmmagruder <gabrielle.magruder@gmail.com>
2025-05-27 14:34:41 +0100

    Update generator

--- src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/amendrestitchbundle/AmendRestitchBundleAllPartiesEmailGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/amendrestitchbundle/AmendRestitchBundleAllPartiesEmailGenerator.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.reform.civil.notification.handlers.amendrestitchbundle;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.civil.notification.handlers.AllPartiesEmailGenerator;
 
+import java.util.List;
+
 @Component
 public class AmendRestitchBundleAllPartiesEmailGenerator extends AllPartiesEmailGenerator {
 
@@ -13,12 +15,10 @@ public class AmendRestitchBundleAllPartiesEmailGenerator extends AllPartiesEmail
         AmendRestitchBundleClaimantEmailDTOGenerator claimantEmailDTOGenerator,
         AmendRestitchBundleDefendantEmailDTOGenerator defendantEmailDTOGenerator
     ) {
-        super(
-            appSolOneEmailGenerator,
+        super(List.of(appSolOneEmailGenerator,
             respSolOneEmailGenerator,
             respSolTwoEmailGenerator,
             claimantEmailDTOGenerator,
-            defendantEmailDTOGenerator
-        );
+            defendantEmailDTOGenerator));
     }
 }
