79293351f0b2c11eb45dbe395c4c723a92f9f60c
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-05-27 08:11:56 +0100

    update run nightly api tests

--- bin/run-nightly-api-tests.sh
+++ bin/run-nightly-api-tests.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -ex
 
-yarn playwright install
-
-echo "Running API tests on ${ENVIRONMENT} env"
+echo "Running Functional tests on ${ENVIRONMENT} env"
 
 if [ "$FORCE_GREEN_BUILD" == "true" ]; then
   echo "Manually forced green build, no functional tests will be run."
@@ -11,45 +9,49 @@ if [ "$FORCE_GREEN_BUILD" == "true" ]; then
 fi
 
 
-# Define path to failedTestFiles.json
-FAILED_TEST_FILES_JSON="./test-results/functional/failedTestFiles.json"
-NOT_EXECUTED_TEST_FILES_JSON="./test-results/functional/notExecutedTestFiles.json"
-
-# Check if RUN_FAILED_AND_NOT_EXECUTED_TEST_FILES is set to "true"
-if [ "$RUN_FAILED_AND_NOT_EXECUTED_TEST_FILES" != "true" ]; then
+# Check if RUN_FAILED_AND_PREV_NOT_EXECUTED_TEST_FILES is set to "true"
+if [ "$RUN_PREV_FAILED_AND_NOT_EXECUTED_TEST_FILES" != "true" ]; then
+  yarn playwright install
   yarn test:api-nightly-prod
+else 
+  # Define path to failedTestFiles.json
+  TEST_FILES_REPORT="test-results/functional/testFilesReport.json"
+  PREV_TEST_FILES_REPORT="test-results/functional/prevTestFilesReport.json"
 
-# Check if failedTestFiles.json exists and is non-empty
-elif [ ! -f "$FAILED_TEST_FILES_JSON" ] || [ ! -s "$FAILED_TEST_FILES_JSON" ]; then
-  echo "failedTestFiles.json not found or is empty."
-  exit 1
+  if [ "$CI" != "true" ]; then
+    [ -f "$TEST_FILES_REPORT" ] && mv "$TEST_FILES_REPORT" "$PREV_TEST_FILES_REPORT"
+  fi
 
-# Check if the JSON array inside failedTestFiles.json is empty
-elif [ "$(jq 'length' "$FAILED_TEST_FILES_JSON")" -eq 0 ]; then
-  echo "failedTestFiles.json contains an empty array."
-  exit 1
-
-else
-  # Collect array elements into a comma-separated string
-  FAILED_TEST_FILES=$(jq -r '.[]' "$FAILED_TEST_FILES_JSON" | paste -sd "," -)
-
-  # If no failed tests, set FAILED_TEST_FILES to empty string
-  FAILED_TEST_FILES=${FAILED_TEST_FILES:-""}
-
-  # Collect array elements into a comma-separated string
-  NOT_EXECUTED_TEST_FILES=$(jq -r '.[]' "$NOT_EXECUTED_TEST_FILES_JSON" | paste -sd "," -)
-
-  # If no failed tests, set NOT_EXECUTED_TEST_FILES to empty string
-  NOT_EXECUTED_TEST_FILES=${NOT_EXECUTED_TEST_FILES:-""}
-
-  if [ -z "$FAILED_TEST_FILES" ]; then
-    echo "No failed tests found."
+  # Check if prevTestFilesReport.json exists and is non-empty
+  if [ ! -f "$PREV_TEST_FILES_REPORT" ] || [ ! -s "$PREV_TEST_FILES_REPORT" ]; then
+    echo "prevTestFilesReport.json not found or is empty."
     exit 1
-  else
-    # Export as environment variable
-    export FAILED_TEST_FILES
-    export NOT_EXECUTED_TEST_FILES
 
-    yarn test:api-nightly-prod
+  # Check if the JSON array inside prevTestFilesReport.json is empty
+  elif [ "$(jq '.failedTestFiles | length' "$PREV_TEST_FILES_REPORT")" -eq 0 ]; then
+    echo "failedTestFiles in prevTestFilesReport.json contains an empty array."
+    exit 1
+
+  else
+    # Collect array elements into a comma-separated string
+    PREV_FAILED_TEST_FILES=$(jq -r '.failedTestFiles[]' "$PREV_TEST_FILES_REPORT" | paste -sd "," -)
+
+    # Collect array elements into a comma-separated string
+    PREV_NOT_EXECUTED_TEST_FILES=$(jq -r '.notExecutedTestFiles[]' "$PREV_TEST_FILES_REPORT" | paste -sd "," -)
+
+    if [ -z "$PREV_FAILED_TEST_FILES" ]; then
+      echo "No failed tests found."
+      exit 1
+    else
+      # Export as environment variable
+      export PREV_FAILED_TEST_FILES="$PREV_FAILED_TEST_FILES"
+      export PREV_NOT_EXECUTED_TEST_FILES="$PREV_NOT_EXECUTED_TEST_FILES"
+      
+      yarn playwright install
+      yarn test:api-nightly-prod
+    fi
   fi
 fi
+
+
+
