d7a94d087facc517d5a9c608c1092f0088ae2949
jeswanth <jeswanth.reddy@hmcts.net>
2025-05-27 17:11:30 +0100

    CIV-17027 added changes to disable default judgement request if defendant response submitted before deadline

--- src/test/java/uk/gov/hmcts/reform/civil/service/search/DefendantResponseDeadlineCheckSearchServiceWithWelshFlagOnTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/search/DefendantResponseDeadlineCheckSearchServiceWithWelshFlagOnTest.java
@@ -44,8 +44,7 @@ class DefendantResponseDeadlineCheckSearchServiceWithWelshFlagOnTest extends Ela
 
         return new Query(query, List.of("reference"), fromValue);
     }
-
-
+    
     @Override
     protected Query buildQueryInMediation(int fromValue, LocalDate date, boolean carmEnabled,
                                           boolean initialSearch,
