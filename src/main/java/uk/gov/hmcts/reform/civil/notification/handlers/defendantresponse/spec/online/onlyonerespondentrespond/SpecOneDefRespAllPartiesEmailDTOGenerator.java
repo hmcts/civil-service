2f6623ddfb972a4b2a18069765ea2eae7e565a24
mounikaammineni <Ayra@23082019>
2025-05-28 12:51:37 +0100

    update

--- src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/defendantresponse/spec/online/onlyonerespondentrespond/SpecOneDefRespAllPartiesEmailDTOGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/defendantresponse/spec/online/onlyonerespondentrespond/SpecOneDefRespAllPartiesEmailDTOGenerator.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.reform.civil.notification.handlers.defendantresponse.spec.online.onlyonerespondentrespond;
 
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.civil.notification.handlers.AllPartiesEmailGenerator;
 import uk.gov.hmcts.reform.civil.notification.handlers.defendantresponse.spec.common.SpecDefRespAppSolOneEmailDTOGenerator;
 
 import java.util.List;
 
+@Component
 public class SpecOneDefRespAllPartiesEmailDTOGenerator extends AllPartiesEmailGenerator {
 
     public SpecOneDefRespAllPartiesEmailDTOGenerator(
