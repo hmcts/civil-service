8ebe4f53219d36cc7d185f94045e899589f4523e
mounikaammineni <Ayra@23082019>
2025-05-28 13:04:34 +0100

    update

--- src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/defendantresponse/spec/online/onlyonerespondentrespond/SpecOneDefRespRespSolTwoEmailDTOGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/defendantresponse/spec/online/onlyonerespondentrespond/SpecOneDefRespRespSolTwoEmailDTOGenerator.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.reform.civil.notification.handlers.defendantresponse.spec.online.onlyonerespondentrespond;
 
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.notification.handlers.defendantresponse.spec.common.SpecDefRespEmailHelper;
 import uk.gov.hmcts.reform.civil.notification.handlers.defendantresponse.spec.common.SpecDefRespRespSolTwoEmailDTOGenerator;
 import uk.gov.hmcts.reform.civil.service.OrganisationService;
 
+@Component
 public class SpecOneDefRespRespSolTwoEmailDTOGenerator extends SpecDefRespRespSolTwoEmailDTOGenerator {
 
     public SpecOneDefRespRespSolTwoEmailDTOGenerator(SpecDefRespEmailHelper specRespEmailHelper,
