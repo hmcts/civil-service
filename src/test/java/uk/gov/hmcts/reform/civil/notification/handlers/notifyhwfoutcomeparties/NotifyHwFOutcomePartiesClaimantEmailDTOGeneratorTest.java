55d16d763fea0722eb1c5cd8a3e69e1846e1a465
devind-ra <devinrrathod@gmail.com>
2025-05-28 13:24:13 +0100

    fixing test files

--- src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/notifyhwfoutcomeparties/NotifyHwFOutcomePartiesClaimantEmailDTOGeneratorTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/notifyhwfoutcomeparties/NotifyHwFOutcomePartiesClaimantEmailDTOGeneratorTest.java
@@ -8,7 +8,8 @@ import uk.gov.hmcts.reform.civil.model.CaseData;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class NotifyHwFOutcomePartiesClaimantEmailDTOGeneratorTest {
 
