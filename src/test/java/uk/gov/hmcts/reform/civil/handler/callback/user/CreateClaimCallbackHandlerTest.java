e59d140e513f16a4bf445ed53f3bd83d11486264
rishikrsharma <rishikrsharma@users.noreply.github.com>
2025-05-29 13:07:37 +0100

    Revert "DTSCCI-650: update unspec claim default court location to cnbc (#6003)"
    This reverts commit 8cd1afd0b0e8615b014b2996e15df7bc354ed8ad.

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateClaimCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/CreateClaimCallbackHandlerTest.java
@@ -1957,7 +1957,7 @@ class CreateClaimCallbackHandlerTest extends BaseCallbackHandlerTest {
                 assertThat(response.getData())
                     .extracting("caseManagementLocation")
                     .extracting("region", "baseLocation")
-                    .containsExactly("2", "420219");
+                    .containsExactly("4", "192280");
             }
         }
 
