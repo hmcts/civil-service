cbe30326f69ab13c6753a2bf52bedfc0fd1f0b4e
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2025-05-30 19:09:42 +0100

    WIP

--- src/main/resources/db/migration/V2025_05_30_1454__CIV_0000_update_status_tag_for_view_other_messages.sql
+++ src/main/resources/db/migration/V2025_05_30_1454__CIV_0000_update_status_tag_for_view_other_messages.sql
@@ -5,7 +5,7 @@ INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, cat
                                     scenario_name, task_status_sequence, role, task_order)
 VALUES ('<a href="{QM_VIEW_OTHER_MESSAGES_URL}" rel="noopener noreferrer" class="govuk-link">View messages from other parties</a>',
         'Applications and messages to the court',
-        '<a href="{QM_VIEW_MESSAGES_URL}" rel="noopener noreferrer" class="govuk-link">Gweld negeseuon gan bartïon eraill</a>',
+        '<a href="{QM_VIEW_OTHER_MESSAGES_URL}" rel="noopener noreferrer" class="govuk-link">Gweld negeseuon gan bartïon eraill</a>',
         'Ceisiadau a negeseuon i’r llys', 'View.Messages', 'Scenario.AAA6.ViewMessages.Available.Defendant',
         '{3, 3}', 'CLAIMANT', 19),
        ('<a href="{QM_VIEW_OTHER_MESSAGES}" rel="noopener noreferrer" class="govuk-link">View messages from other parties</a>',
