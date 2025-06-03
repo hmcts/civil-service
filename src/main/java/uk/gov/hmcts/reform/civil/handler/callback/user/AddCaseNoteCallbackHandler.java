36a88b50f56724c1908628311d1df496d98041d8
drummondjm <mark.drummond@version1.com>
2025-06-03 12:46:43 +0100

    testing change

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/AddCaseNoteCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/AddCaseNoteCallbackHandler.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.civil.handler.callback.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
@@ -15,6 +16,7 @@ import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.CaseNote;
 import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.service.CaseNoteService;
+import uk.gov.hmcts.reform.civil.service.flowstate.SimpleStateFlowEngine;
 
 import java.util.Collections;
 import java.util.List;
@@ -28,12 +30,14 @@ import static uk.gov.hmcts.reform.civil.callback.CaseEvent.ADD_CASE_NOTE;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AddCaseNoteCallbackHandler extends CallbackHandler {
 
     private static final List<CaseEvent> EVENTS = Collections.singletonList(ADD_CASE_NOTE);
 
     private final CaseNoteService caseNoteService;
     private final ObjectMapper objectMapper;
+    private final SimpleStateFlowEngine stateFlowEngine;
 
     @Value("${azure.service-bus.ccd-events-topic.enabled:false}")
     private boolean ccdEventsServiceBusEnabled;
@@ -43,8 +47,15 @@ public class AddCaseNoteCallbackHandler extends CallbackHandler {
         return Map.of(
             callbackKey(ABOUT_TO_START), this::emptyCallbackResponse,
             callbackKey(ABOUT_TO_SUBMIT), this::moveNotesIntoList,
-            callbackKey(SUBMITTED), this::emptySubmittedCallbackResponse
+            callbackKey(SUBMITTED), this::test
         );
+    }
+
+    private CallbackResponse test(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        log.info("STATE HISTORY: {}", stateFlowEngine.evaluate(caseData).getStateHistory().toString());
+
+        return AboutToStartOrSubmitCallbackResponse.builder().build();
     }
 
     @Override
