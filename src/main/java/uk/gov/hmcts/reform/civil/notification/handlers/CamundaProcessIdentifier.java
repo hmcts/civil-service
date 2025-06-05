0279e0821cb5109be33737ab2c7feec597ad287c
AbuSalamHMCTS <138011124+AbuSalamHMCTS@users.noreply.github.com>
2025-06-05 15:29:16 +0100

    Fix merge conflicts

--- src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/CamundaProcessIdentifier.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/CamundaProcessIdentifier.java
@@ -10,5 +10,6 @@ public enum CamundaProcessIdentifier {
     AcknowledgeClaimUnspecNotifyParties,
     GenerateOrderNotifyPartiesCourtOfficerOrder,
     GenerateOrderNotifyParties,
+    ClaimantLipHelpWithFeesNotifier,
     DismissCaseNotify
 }
