8bd1e65b306b8e7c379675b44bc20ce50c55cb9d
drummondjm <mark.drummond@version1.com>
2025-06-09 12:04:09 +0100

    update state claimant intention post translation

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/task/respondtodefencespeccallbackhandlertask/DetermineNextState.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/task/respondtodefencespeccallbackhandlertask/DetermineNextState.java
@@ -1,24 +1,35 @@
 package uk.gov.hmcts.reform.civil.handler.callback.user.task.respondtodefencespeccallbackhandlertask;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.Callback;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.enums.CaseCategory;
 import uk.gov.hmcts.reform.civil.enums.CaseState;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.helpers.judgmentsonline.JudgmentByAdmissionOnlineMapper;
 import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.service.DirectionsQuestionnairePreparer;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.service.flowstate.FlowState;
 import uk.gov.hmcts.reform.civil.service.flowstate.FlowStateAllowedEventService;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CallbackVersion.V_2;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.CLAIMANT_RESPONSE_SPEC;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.JUDGEMENT_BY_ADMISSION_NON_DIVERGENT_SPEC;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UPDATE_CLAIM_STATE_AFTER_DOC_UPLOADED;
 import static uk.gov.hmcts.reform.civil.enums.AllocatedTrack.INTERMEDIATE_CLAIM;
 import static uk.gov.hmcts.reform.civil.enums.AllocatedTrack.MULTI_CLAIM;
 import static uk.gov.hmcts.reform.civil.enums.CaseState.AWAITING_APPLICANT_INTENTION;
@@ -34,12 +45,75 @@ import static uk.gov.hmcts.reform.civil.utils.CaseStateUtils.shouldMoveToInMedia
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class DetermineNextState  {
+public class DetermineNextState extends CallbackHandler {
 
+    private final List<CaseEvent> EVENTS = Collections.singletonList(UPDATE_CLAIM_STATE_AFTER_DOC_UPLOADED);
+    private Map<String, Callback> callbackMap = Map.of(callbackKey(ABOUT_TO_SUBMIT), this::updateClaimStatePostTranslation);
+    private static final String TASK_ID = "UpdateClaimStateAfterClaimantIntentionLrTranslatedDocUploaded";
+    private final ObjectMapper objectMapper;
     private final FeatureToggleService featureToggleService;
     private final JudgmentByAdmissionOnlineMapper judgmentByAdmissionOnlineMapper;
     private final FlowStateAllowedEventService flowStateAllowedEventService;
-    private final DirectionsQuestionnairePreparer directionsQuestionnairePreparer;
+
+    @Override
+    protected Map<String, Callback> callbacks() {
+        return callbackMap;
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+
+    @Override
+    public String camundaActivityId(CallbackParams callbackParams) {
+        return TASK_ID;
+    }
+
+    private CallbackResponse updateClaimStatePostTranslation(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        CaseData.CaseDataBuilder<?, ?> caseDataBuilder = caseData.toBuilder();
+        String nextState = determineNextStatePostTranslation(caseData, callbackParams);
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDataBuilder.build().toMap(objectMapper))
+            .state(nextState)
+            .build();
+    }
+
+    public String determineNextStatePostTranslation(CaseData caseData, CallbackParams callbackParams) {
+
+        String nextState = "";
+        log.info("Determining next state (post translation) for Case: {}", caseData.getCcdCaseReference());
+
+        if (V_2.equals(callbackParams.getVersion())
+            && featureToggleService.isPinInPostEnabled()
+            && isOneVOne(caseData)) {
+            log.debug("Pin in Post enabled for Case: {}", caseData.getCcdCaseReference());
+            if (caseData.hasClaimantAgreedToFreeMediation()) {
+                nextState = CaseState.IN_MEDIATION.name();
+            } else if (isDefenceAdmitPayImmediately(caseData)) {
+                nextState = getNextState(caseData);
+            } else if (caseData.hasApplicantRejectedRepaymentPlan()) {
+                nextState = CaseState.PROCEEDS_IN_HERITAGE_SYSTEM.name();
+            } else if (isClaimNotSettled(caseData)) {
+                nextState = CaseState.JUDICIAL_REFERRAL.name();
+            } else if (caseData.isPartAdmitClaimSettled()) {
+                nextState = CaseState.CASE_SETTLED.name();
+            } else if (isLipVLipOneVOne(caseData)) {
+                nextState = CaseState.CASE_STAYED.name();
+            }
+        }
+
+        if (shouldMoveToInMediationState(
+            caseData, featureToggleService.isCarmEnabledForCase(caseData))) {
+            nextState = CaseState.IN_MEDIATION.name();
+        }
+        if (shouldNotChangeStateMinti(caseData)) {
+            nextState = AWAITING_APPLICANT_INTENTION.name();
+        }
+
+        return nextState;
+    }
 
     public String determineNextState(CaseData caseData,
                                      CallbackParams callbackParams,
