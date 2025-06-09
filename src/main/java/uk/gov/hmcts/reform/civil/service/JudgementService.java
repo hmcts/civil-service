c4f05b72a8065923a590724c5b386ed3bf9acd9f
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2025-06-09 12:29:55 +0100

    CIV-17095-JO add flag

--- src/main/java/uk/gov/hmcts/reform/civil/service/JudgementService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/JudgementService.java
@@ -160,7 +160,8 @@ public class JudgementService {
     }
 
     private YesOrNo checkFixedCostOption(CaseData caseData) {
-        if ((nonNull(caseData.getFixedCosts()) && YesOrNo.YES.equals(caseData.getFixedCosts().getClaimFixedCosts()))
+        if (isLrFullAdmitRepaymentPlan(caseData)
+            && (nonNull(caseData.getFixedCosts()) && YesOrNo.YES.equals(caseData.getFixedCosts().getClaimFixedCosts()))
             || YesOrNo.YES.equals(caseData.getCcjPaymentDetails().getCcjJudgmentFixedCostOption())) {
             return YesOrNo.YES;
         }
