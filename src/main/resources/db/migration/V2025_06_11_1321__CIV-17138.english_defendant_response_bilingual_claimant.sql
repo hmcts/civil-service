d03d675914facfc37eded06adb35ceee9a67ea2a
paul-pearson <paul.pearson@version1.com>
2025-06-11 17:27:22 +0100

    CIV-17138 Fix migration script

--- src/main/resources/db/migration/V2025_06_11_1321__CIV-17138.english_defendant_response_bilingual_claimant.sql
+++ src/main/resources/db/migration/V2025_06_11_1321__CIV-17138.english_defendant_response_bilingual_claimant.sql
@@ -1,12 +1,13 @@
 /**
  * Add scenario for claimant - delete notifications but do not create new one
  */
-INSERT INTO dbs.scenario (name, notifications_to_delete)
+INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
 VALUES ('Scenario.AAA6.EnglishDefResponse.BilingualFlagSet.Claimant',
         '{"Notice.AAA6.ClaimIssue.Response.Await",
           "Notice.AAA6.DefResponse.MoretimeRequested.Claimant",
           "Notice.AAA6.DefResponse.ResponseTimeElapsed.Claimant",
-          "Notice.AAA6.ClaimIssue.HWF.PhonePayment"}');
+          "Notice.AAA6.ClaimIssue.HWF.PhonePayment"}',
+        '{}');
 
 /**
  * Add task list items
