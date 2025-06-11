df615c2a436c9797c2e8a5ee2bc910e52538c5f6
AbuSalam22 <136581242+AbuSalam22@users.noreply.github.com>
2025-06-11 15:43:52 +0100

    DTSCCI-2281

---
+++ src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/takecaseoffline/CaseTakenOfflineRespSolTwoEmailDTOGenerator.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.reform.civil.notification.handlers.takecaseoffline;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.notification.handlers.RespSolOneEmailDTOGenerator;
+import uk.gov.hmcts.reform.civil.notification.handlers.RespSolTwoEmailDTOGenerator;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+import uk.gov.hmcts.reform.civil.service.OrganisationService;
+
+
+@Component
+public class CaseTakenOfflineRespSolTwoEmailDTOGenerator extends RespSolTwoEmailDTOGenerator {
+
+    private static final String REFERENCE_TEMPLATE = "case-taken-offline-respondent-notification-%s";
+    private final NotificationsProperties notificationsProperties;
+
+    public CaseTakenOfflineRespSolTwoEmailDTOGenerator(OrganisationService organisationService, NotificationsProperties notificationsProperties) {
+        super(organisationService);
+        this.notificationsProperties = notificationsProperties;
+    }
+
+    @Override
+    protected String getEmailTemplateId(CaseData caseData) {
+        return notificationsProperties.getSolicitorCaseTakenOffline();
+    }
+
+    @Override
+    protected String getReferenceTemplate() {
+        return REFERENCE_TEMPLATE;
+    }
+}
