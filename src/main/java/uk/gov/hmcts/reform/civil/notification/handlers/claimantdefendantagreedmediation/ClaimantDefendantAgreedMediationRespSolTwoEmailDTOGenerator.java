f3c06229ccb2659f7381898a5bedc83a55e6e9ba
gmmagruder <gabrielle.magruder@gmail.com>
2025-06-11 14:52:42 +0100

    Refactor ClaimantDefendantAgreedMediationPartiesEmailGenerator to use All parties generator

--- src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/claimantdefendantagreedmediation/ClaimantDefendantAgreedMediationRespSolTwoEmailDTOGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/claimantdefendantagreedmediation/ClaimantDefendantAgreedMediationRespSolTwoEmailDTOGenerator.java
@@ -4,16 +4,23 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.notification.handlers.RespSolTwoEmailDTOGenerator;
 import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.service.OrganisationService;
+
+import static uk.gov.hmcts.reform.civil.utils.NotificationUtils.shouldSendMediationNotificationDefendant2LRCarm;
 
 @Component
 public class ClaimantDefendantAgreedMediationRespSolTwoEmailDTOGenerator extends RespSolTwoEmailDTOGenerator {
 
     private final NotificationsProperties notificationsProperties;
 
-    public ClaimantDefendantAgreedMediationRespSolTwoEmailDTOGenerator(OrganisationService organisationService, NotificationsProperties notificationsProperties) {
+    private final FeatureToggleService featureToggleService;
+
+    public ClaimantDefendantAgreedMediationRespSolTwoEmailDTOGenerator(OrganisationService organisationService, NotificationsProperties notificationsProperties,
+                                                                       FeatureToggleService featureToggleService) {
         super(organisationService);
         this.notificationsProperties = notificationsProperties;
+        this.featureToggleService = featureToggleService;
     }
 
     @Override
@@ -24,5 +31,11 @@ public class ClaimantDefendantAgreedMediationRespSolTwoEmailDTOGenerator extends
     @Override
     protected String getReferenceTemplate() {
         return "mediation-agreement-respondent-notification-%s";
+    }
+
+    @Override
+    public Boolean getShouldNotify(CaseData caseData) {
+        boolean carmEnabled = featureToggleService.isCarmEnabledForCase(caseData);
+        return shouldSendMediationNotificationDefendant2LRCarm(caseData, carmEnabled);
     }
 }
