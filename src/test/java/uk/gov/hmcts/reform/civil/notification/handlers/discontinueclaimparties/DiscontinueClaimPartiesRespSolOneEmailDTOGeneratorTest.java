1c73d1e223544ac447fd460417c02b445fdc1b35
devind-ra <devinrrathod@gmail.com>
2025-06-11 15:22:29 +0100

    adding test files for event

---
+++ src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/discontinueclaimparties/DiscontinueClaimPartiesRespSolOneEmailDTOGeneratorTest.java
@@ -0,0 +1,147 @@
+package uk.gov.hmcts.reform.civil.notification.handlers.discontinueclaimparties;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.MockitoAnnotations;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.Party;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+import uk.gov.hmcts.reform.civil.service.OrganisationService;
+import uk.gov.hmcts.reform.civil.utils.NotificationUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIM_REFERENCE_NUMBER;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.LEGAL_ORG_NAME;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.PARTY_REFERENCES;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CASEMAN_REF;
+
+class DiscontinueClaimPartiesRespSolOneEmailDTOGeneratorTest {
+    private static final String TEMPLATE_ID = "template-id";
+    private static final String REFERENCE_NUMBER = "8372942374";
+    protected static final String APPLICANT_LEGAL_ORG_NAME = "Test Legal Org";
+
+    @Mock
+    private NotificationsProperties notificationsProperties;
+
+    @Mock
+    private OrganisationService organisationService;
+
+    private DiscontinueClaimPartiesRespSolOneEmailDTOGenerator generator;
+
+    private MockedStatic<NotificationUtils> notificationUtilsMockedStatic;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        generator = new DiscontinueClaimPartiesRespSolOneEmailDTOGenerator(notificationsProperties, organisationService);
+        notificationUtilsMockedStatic = mockStatic(NotificationUtils.class);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (notificationUtilsMockedStatic != null) {
+            notificationUtilsMockedStatic.close();
+        }
+    }
+
+    @Test
+    void shouldReturnEmailTemplateId() {
+        when(notificationsProperties.getNotifyClaimDiscontinuedLRTemplate()).thenReturn(TEMPLATE_ID);
+
+        CaseData caseData = CaseData.builder().build();
+        String result = generator.getEmailTemplateId(caseData);
+
+        assertEquals(TEMPLATE_ID, result);
+    }
+
+    @Test
+    void shouldReturnReferenceTemplate() {
+        String result = generator.getReferenceTemplate();
+
+        assertEquals("defendant-claim-discontinued-%s", result);
+    }
+
+    @Test
+    void shouldAddCustomProperties() {
+        Map<String, String> properties = new HashMap<>();
+        CaseData caseData = mock(CaseData.class);
+
+        notificationUtilsMockedStatic.when(() -> NotificationUtils.getRespondentLegalOrganizationName(caseData.getRespondent1OrganisationPolicy(), organisationService))
+            .thenReturn(APPLICANT_LEGAL_ORG_NAME);
+
+        when(caseData.getLegacyCaseReference()).thenReturn(REFERENCE_NUMBER);
+        when(caseData.getCcdCaseReference()).thenReturn(12345L);
+
+        Map<String, String> result = generator.addCustomProperties(properties, caseData);
+
+        assertThat(result.get(LEGAL_ORG_NAME)).isEqualTo(APPLICANT_LEGAL_ORG_NAME);
+        assertThat(result.get(CLAIM_REFERENCE_NUMBER)).isEqualTo("12345");
+        assertThat(result.get(PARTY_REFERENCES)).isEqualTo(NotificationUtils.buildPartiesReferencesEmailSubject(caseData));
+        assertThat(result.get(CASEMAN_REF)).isEqualTo(REFERENCE_NUMBER);
+    }
+
+    @Test
+    void shouldReturnRespondentSolicitorEmailAddressWhenShouldNotifyIsTrue() {
+        CaseData caseData = CaseData.builder()
+            .respondentSolicitor1EmailAddress("solicitor@example.com")
+            .respondent1Represented(YesOrNo.YES)
+            .build();
+
+        String result = generator.getEmailAddress(caseData);
+
+        assertEquals("solicitor@example.com", result);
+    }
+
+    @Test
+    void shouldReturnRespondentPartyEmailWhenShouldNotifyIsFalse() {
+        Party respondent1 = mock(Party.class);
+        when(respondent1.getPartyEmail()).thenReturn("party@example.com");
+
+        CaseData caseData = CaseData.builder()
+            .respondent1(respondent1)
+            .respondent1Represented(YesOrNo.NO)
+            .build();
+
+        String result = generator.getEmailAddress(caseData);
+
+        assertEquals("party@example.com", result);
+    }
+
+    @Test
+    void shouldReturnLipEmailTemplateIdWhenShouldNotifyIsFalse() {
+        CaseData caseData = CaseData.builder()
+            .respondent1Represented(YesOrNo.NO)
+            .build();
+
+        when(notificationsProperties.getNotifyClaimDiscontinuedLipTemplate()).thenReturn(TEMPLATE_ID);
+
+        String result = generator.getEmailTemplateId(caseData);
+
+        assertEquals(TEMPLATE_ID, result);
+    }
+
+    @Test
+    void shouldReturnLRTemplateIdWhenShouldNotifyIsTrue() {
+        CaseData caseData = CaseData.builder()
+            .respondent1Represented(YesOrNo.YES)
+            .build();
+
+        when(notificationsProperties.getNotifyClaimDiscontinuedLRTemplate()).thenReturn(TEMPLATE_ID);
+
+        String result = generator.getEmailTemplateId(caseData);
+
+        assertEquals(TEMPLATE_ID, result);
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/discontinueclaimparties/DiscontinueClaimPartiesRespSolTwoEmailDTOGeneratorTest.java
@@ -0,0 +1,104 @@
+package uk.gov.hmcts.reform.civil.notification.handlers.discontinueclaimparties;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.MockitoAnnotations;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+import uk.gov.hmcts.reform.civil.service.OrganisationService;
+import uk.gov.hmcts.reform.civil.utils.NotificationUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.*;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CASEMAN_REF;
+
+class DiscontinueClaimPartiesRespSolTwoEmailDTOGeneratorTest {
+    private static final String TEMPLATE_ID = "template-id";
+    private static final String REFERENCE_NUMBER = "8372942374";
+    protected static final String APPLICANT_LEGAL_ORG_NAME = "Test Legal Org";
+
+    @Mock
+    private NotificationsProperties notificationsProperties;
+
+    @Mock
+    private OrganisationService organisationService;
+
+    private DiscontinueClaimPartiesRespSolTwoEmailDTOGenerator generator;
+
+    private MockedStatic<NotificationUtils> notificationUtilsMockedStatic;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        generator = new DiscontinueClaimPartiesRespSolTwoEmailDTOGenerator(notificationsProperties, organisationService);
+        notificationUtilsMockedStatic = mockStatic(NotificationUtils.class);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (notificationUtilsMockedStatic != null) {
+            notificationUtilsMockedStatic.close();
+        }
+    }
+
+    @Test
+    void shouldReturnEmailTemplateId() {
+        when(notificationsProperties.getNotifyClaimDiscontinuedLRTemplate()).thenReturn(TEMPLATE_ID);
+
+        CaseData caseData = CaseData.builder().build();
+        String result = generator.getEmailTemplateId(caseData);
+
+        assertEquals(TEMPLATE_ID, result);
+    }
+
+    @Test
+    void shouldReturnReferenceTemplate() {
+        String result = generator.getReferenceTemplate();
+
+        assertEquals("defendant2-claim-discontinued-%s", result);
+    }
+
+    @Test
+    void shouldAddCustomProperties() {
+        Map<String, String> properties = new HashMap<>();
+        CaseData caseData = mock(CaseData.class);
+
+        notificationUtilsMockedStatic.when(() -> NotificationUtils.getRespondentLegalOrganizationName(caseData.getRespondent2OrganisationPolicy(), organisationService))
+            .thenReturn(APPLICANT_LEGAL_ORG_NAME);
+
+        when(caseData.getLegacyCaseReference()).thenReturn(REFERENCE_NUMBER);
+        when(caseData.getCcdCaseReference()).thenReturn(12345L);
+
+        Map<String, String> result = generator.addCustomProperties(properties, caseData);
+
+        assertThat(result.get(LEGAL_ORG_NAME)).isEqualTo(APPLICANT_LEGAL_ORG_NAME);
+        assertThat(result.get(CLAIM_REFERENCE_NUMBER)).isEqualTo("12345");
+        assertThat(result.get(PARTY_REFERENCES)).isEqualTo(NotificationUtils.buildPartiesReferencesEmailSubject(caseData));
+        assertThat(result.get(CASEMAN_REF)).isEqualTo(REFERENCE_NUMBER);
+    }
+
+    @Test
+    void shouldReturnLRTemplateIdWhenShouldNotifyIsTrue() {
+        CaseData caseData = CaseData.builder()
+            .respondent1Represented(YesOrNo.YES)
+            .build();
+
+        when(notificationsProperties.getNotifyClaimDiscontinuedLRTemplate()).thenReturn(TEMPLATE_ID);
+
+        String result = generator.getEmailTemplateId(caseData);
+
+        assertEquals(TEMPLATE_ID, result);
+    }
+
+}
