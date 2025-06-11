f71f3b4eda408452f1453ceb70a12d61d46327be
gmmagruder <gabrielle.magruder@gmail.com>
2025-06-11 14:56:46 +0100

    Remove unused imports

--- src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/claimantdefendantagreedmediation/ClaimantDefendantAgreedMediationPartiesEmailGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/claimantdefendantagreedmediation/ClaimantDefendantAgreedMediationPartiesEmailGenerator.java
@@ -1,18 +1,8 @@
 package uk.gov.hmcts.reform.civil.notification.handlers.claimantdefendantagreedmediation;
 
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.notification.handlers.AllLegalRepsEmailGenerator;
 import uk.gov.hmcts.reform.civil.notification.handlers.AllPartiesEmailGenerator;
-import uk.gov.hmcts.reform.civil.notification.handlers.EmailDTO;
-import uk.gov.hmcts.reform.civil.notification.handlers.EmailDTOGenerator;
-import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
-
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
-import static uk.gov.hmcts.reform.civil.utils.NotificationUtils.shouldSendMediationNotificationDefendant2LRCarm;
 
 @Component
 public class ClaimantDefendantAgreedMediationPartiesEmailGenerator extends AllPartiesEmailGenerator {
