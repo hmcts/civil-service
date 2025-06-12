94ef95c759653c9377edf1e2fb4e8728551c4220
Ruban <ruban72@hotmail.com>
2025-06-12 17:17:44 +0100

    DTSCCI-2168: Add fixed costs to def amount when full admission

--- src/main/java/uk/gov/hmcts/reform/civil/service/citizenui/responsedeadline/DeadlineExtensionCalculatorService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/citizenui/responsedeadline/DeadlineExtensionCalculatorService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.bankholidays.WorkingDayIndicator;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static java.util.Objects.requireNonNull;
 
@@ -25,10 +26,24 @@ public class DeadlineExtensionCalculatorService {
         return calculateWorkingDays(responseDate.plusDays(1), plusDays);
     }
 
+    public LocalDate calculateExtendedDeadline(LocalDateTime responseDate, int plusDays) {
+        workingDaysCounter = 0;
+        requireNonNull(responseDate);
+        if(is4pmOrAfter(responseDate)) {
+            responseDate = responseDate.plusDays(1);
+        }
+        return calculateWorkingDays(responseDate.toLocalDate().plusDays(1), plusDays);
+    }
+
     public LocalDate calculateWorkingDays(LocalDate responseDate, int plusDays) {
         if (workingDayIndicator.isWorkingDay(responseDate)) {
             workingDaysCounter++;
         }
         return workingDaysCounter == plusDays ? responseDate : calculateWorkingDays(responseDate.plusDays(1), plusDays);
+    }
+
+
+    private boolean is4pmOrAfter(LocalDateTime dateOfService) {
+        return dateOfService.getHour() >= 16;
     }
 }
