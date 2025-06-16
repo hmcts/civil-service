bd0a7892dd9a6b318a60f96f33fcc33456119793
Ruban <ruban72@hotmail.com>
2025-06-16 09:20:08 +0100

    DTSCCI-2168: Update time zone to cater for BST

--- src/main/java/uk/gov/hmcts/reform/civil/service/citizenui/responsedeadline/DeadlineExtensionCalculatorService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/citizenui/responsedeadline/DeadlineExtensionCalculatorService.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.civil.service.citizenui.responsedeadline;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.bankholidays.WorkingDayIndicator;
 
