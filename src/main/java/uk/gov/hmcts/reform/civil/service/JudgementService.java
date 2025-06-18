d04a7bcedf012e00bf30874238bee75bde4efdb2
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2025-06-18 16:22:25 +0100

    CIV-17247 changes

--- src/main/java/uk/gov/hmcts/reform/civil/service/JudgementService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/JudgementService.java
@@ -100,7 +100,7 @@ public class JudgementService {
     }
 
     public BigDecimal ccjJudgementSubTotal(CaseData caseData) {
-        if (isLrFullAdmitRepaymentPlan(caseData) || isLRPartAdmitRepaymentPlan(caseData)) {
+        if (isLrFullAdmitRepaymentPlan(caseData) || isLRPartAdmitRepaymentPlan(caseData) || isLrPayImmediatelyPlan(caseData)) {
             return ccjJudgmentClaimAmount(caseData)
                 .add(ccjJudgmentClaimFee(caseData))
                 .add(ccjJudgmentFixedCost(caseData));
