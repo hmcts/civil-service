696b2b0a7ca9b49525d58a26400de9d30b73eef7
Neeta Shinde <neeta.shinde@hmcts.net>
2025-06-20 17:25:31 +0100

    fixed typo

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateDirectionQuestionnaireLipCallBackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateDirectionQuestionnaireLipCallBackHandler.java
@@ -56,7 +56,7 @@ public class GenerateDirectionQuestionnaireLipCallBackHandler extends CallbackHa
 
     private CallbackResponse prepareDirectionsQuestionnaire(CallbackParams callbackParams) {
         CaseData caseData = callbackParams.getCaseData();
-        if ( caseData.isFullAdmitClaimSpec()
+        if (caseData.isFullAdmitClaimSpec()
             || caseData.isClaimantAcceptedClaimAmount()) {
             return AboutToStartOrSubmitCallbackResponse.builder()
                 .build();
