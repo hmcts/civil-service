f4054e666857613a04eb0d0560abb16fe0794755
gmmagruder <gabrielle.magruder@gmail.com>
2025-06-24 08:12:14 +0100

    Refactor CreateSDOPartiesEmailGenerator to use AllPartiesGenerator

--- src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/createsdo/CreateSDOPartiesEmailGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/createsdo/CreateSDOPartiesEmailGenerator.java
@@ -1,60 +1,18 @@
 package uk.gov.hmcts.reform.civil.notification.handlers.createsdo;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.notification.handlers.EmailDTO;
-import uk.gov.hmcts.reform.civil.notification.handlers.PartiesEmailGenerator;
+import uk.gov.hmcts.reform.civil.notification.handlers.AllPartiesEmailGenerator;
+import java.util.List;
 
-import java.util.HashSet;
-import java.util.Set;
-
-@AllArgsConstructor
-@Slf4j
 @Component
-public class CreateSDOPartiesEmailGenerator implements PartiesEmailGenerator {
+public class CreateSDOPartiesEmailGenerator extends AllPartiesEmailGenerator {
 
-    protected final CreateSDOAppSolOneEmailDTOGenerator appSolOneEmailDTOGenerator;
-
-    protected final CreateSDOClaimantEmailDTOGenerator claimantEmailDTOGenerator;
-
-    protected final CreateSDORespSolOneEmailDTOGenerator respSolOneEmailDTOGenerator;
-
-    protected final CreateSDORespSolTwoEmailDTOGenerator respSolTwoEmailDTOGenerator;
-
-    protected final CreateSDODefendantEmailDTOGenerator defendantEmailDTOGenerator;
-
-    @Override
-    public Set<EmailDTO> getPartiesToNotify(CaseData caseData, String taskId) {
-        Set<EmailDTO> partiesToEmail = new HashSet<>();
-        log.info("Generating email for case ID: {}", caseData.getCcdCaseReference());
-        partiesToEmail.add(getApplicant(caseData, taskId));
-        partiesToEmail.addAll(getRespondents(caseData, taskId));
-        return partiesToEmail;
-    }
-
-    private EmailDTO getApplicant(CaseData caseData, String taskId) {
-        return caseData.isApplicantLiP() ? claimantEmailDTOGenerator.buildEmailDTO(caseData, taskId) : appSolOneEmailDTOGenerator.buildEmailDTO(caseData, taskId);
-    }
-
-    private Set<EmailDTO> getRespondents(CaseData caseData, String taskId) {
-        Set<EmailDTO> recipients = new HashSet<>();
-
-        if (caseData.isRespondent1LiP()) {
-            recipients.add(defendantEmailDTOGenerator.buildEmailDTO(caseData, taskId));
-        } else {
-            recipients.add(respSolOneEmailDTOGenerator.buildEmailDTO(caseData, taskId));
-        }
-
-        if (caseData.getRespondent2() != null) {
-            if (caseData.isRespondent2LiP()) {
-                recipients.add(defendantTwoEmailDTOGenerator.buildEmailDTO(caseData, taskId));
-            } else {
-                recipients.add(respSolTwoEmailDTOGenerator.buildEmailDTO(caseData, taskId));
-            }
-        }
-
-        return recipients;
+    public CreateSDOPartiesEmailGenerator(CreateSDOAppSolOneEmailDTOGenerator appSolOneEmailDTOGenerator,
+                                          CreateSDORespSolOneEmailDTOGenerator respSolOneEmailDTOGenerator,
+                                          CreateSDORespSolTwoEmailDTOGenerator respSolTwoEmailDTOGenerator,
+                                          CreateSDOClaimantEmailDTOGenerator claimantEmailDTOGenerator,
+                                          CreateSDODefendantEmailDTOGenerator defendantEmailDTOGenerator) {
+        super(List.of(appSolOneEmailDTOGenerator, respSolOneEmailDTOGenerator, respSolTwoEmailDTOGenerator, claimantEmailDTOGenerator, defendantEmailDTOGenerator));
     }
 }
