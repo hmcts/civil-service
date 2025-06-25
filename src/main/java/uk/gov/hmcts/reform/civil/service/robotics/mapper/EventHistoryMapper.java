fbe275e8f42207430319b79b1c54d621639eb4c8
drummondjm <mark.drummond@version.com>
2025-06-25 14:57:29 +0100

    Change/update query collection for RPA

--- src/main/java/uk/gov/hmcts/reform/civil/service/robotics/mapper/EventHistoryMapper.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/robotics/mapper/EventHistoryMapper.java
@@ -1122,7 +1122,7 @@ public class EventHistoryMapper {
     }
 
     private void buildQueriesEvent(EventHistory.EventHistoryBuilder builder, CaseData caseData) {
-        if (!isCaseOffline(caseData) || !featureToggleService.isQueryManagementLRsEnabled()) {
+        if (!isCaseOffline(caseData)) {
             return;
         }
         if (!hasActiveQueries(caseData)) {
@@ -1145,9 +1145,13 @@ public class EventHistoryMapper {
     }
 
     private boolean hasActiveQueries(CaseData caseData) {
-        return caseData.getQmApplicantSolicitorQueries() != null
-            || caseData.getQmRespondentSolicitor1Queries() != null
-            || caseData.getQmRespondentSolicitor2Queries() != null;
+        if (featureToggleService.isPublicQueryManagementEnabled(caseData)) {
+            return caseData.getQueries() != null;
+        } else {
+            return caseData.getQmApplicantSolicitorQueries() != null
+                || caseData.getQmRespondentSolicitor1Queries() != null
+                || caseData.getQmRespondentSolicitor2Queries() != null;
+        }
     }
 
     private boolean isCaseOffline(CaseData caseData) {
