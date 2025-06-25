5aee2a99c177a458984dd6fc7470dba97d4ab4d8
drummondjm <mark.drummond@version1.com>
2025-06-25 09:41:54 +0100

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/civil/service/DirectionsQuestionnairePreparer.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/DirectionsQuestionnairePreparer.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.civil.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
 import uk.gov.hmcts.reform.civil.enums.CaseState;
