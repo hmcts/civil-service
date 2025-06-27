b57393d809961adcef6071a089ed755a6ef2fedd
sankaviv1 <sankavi.mohanraj@version1.com>
2025-06-27 09:40:14 +0100

    fix unit test

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/ClaimIssueNotificationsHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendant/ClaimIssueNotificationsHandlerTest.java
@@ -130,7 +130,6 @@ class ClaimIssueNotificationsHandlerTest extends BaseCallbackHandlerTest {
     void createDashboardNotifications_queryManagement() {
         when(dashboardNotificationsParamsMapper.mapCaseDataToParams(any())).thenReturn(params);
         when(featureToggleService.isLipVLipEnabled()).thenReturn(true);
-        when(featureToggleService.isLipQueryManagementEnabled(any())).thenReturn(true);
 
         LocalDateTime dateTime = LocalDate.of(2020, Month.JANUARY, 18).atStartOfDay();
 
