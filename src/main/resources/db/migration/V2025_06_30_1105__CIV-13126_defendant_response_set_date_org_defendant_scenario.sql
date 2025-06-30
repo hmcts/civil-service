4c0eb69ea8cdbb1efb460304644db0830954f2ca
rishisharma <rishi.sharma@hmcts.net>
2025-06-30 14:07:53 +0100

    DTSCCI-2168: update migration script

--- src/main/resources/db/migration/V2025_06_30_1105__CIV-13126_defendant_response_set_date_org_defendant_scenario.sql
+++ src/main/resources/db/migration/V2025_06_30_1105__CIV-13126_defendant_response_set_date_org_defendant_scenario.sql
@@ -3,7 +3,7 @@
  */
 UPDATE dbs.scenario
 SET notifications_to_create = '{"Notice.AAA6.DefResponse.OrgOrLtdCompany.FullOrPartAdmit.PayBySetDate.Defendant" : ["applicant1PartyName", "defendantAdmittedAmount", "defendantAmountIncludesTextEn", "defendantAmountIncludesTextCy", "respondent1AdmittedAmountPaymentDeadlineEn", "respondent1AdmittedAmountPaymentDeadlineCy"]}'
-WHERE name = 'Scenario.AAA6.DefResponse.OrgOrLtdCompany.FullOrPartAdmit.PayBySetDate.Defendant';
+where name = 'Scenario.AAA6.DefResponse.OrgOrLtdCompany.FullOrPartAdmit.PayBySetDate.Defendant';
 
 
 /**
@@ -12,4 +12,4 @@ WHERE name = 'Scenario.AAA6.DefResponse.OrgOrLtdCompany.FullOrPartAdmit.PayBySet
 UPDATE dbs.dashboard_notifications_templates
 SET description_En ='<p class="govuk-body">You have offered to pay ${applicant1PartyName} ${defendantAdmittedAmount} ${defendantAmountIncludesTextEn} by ${respondent1AdmittedAmountPaymentDeadlineEn}. You need to send the claimant your financial details. The court will contact you when they respond.</p><p class="govuk-body"><a href="{VIEW_RESPONSE_TO_CLAIM}" class="govuk-link">View your response</a></p>',
     description_Cy ='<p class="govuk-body">Rydych wedi cynnig talu ${defendantAdmittedAmount} ${defendantAmountIncludesTextCy} i ${applicant1PartyName} erbyn ${respondent1AdmittedAmountPaymentDeadlineCy}. Mae angen i chi anfon eich manylion ariannol at yr hawlydd. Bydd y llys yn cysylltu â chi pan fyddant yn ymateb.</p><p class="govuk-body"><a href="{VIEW_RESPONSE_TO_CLAIM}" class="govuk-link">Gweld eich ymateb</a></p>'
-WHERE template_name = 'Notice.AAA6.DefResponse.OrgOrLtdCompany.FullOrPartAdmit.PayBySetDate.Defendant';
+where template_name = 'Notice.AAA6.DefResponse.OrgOrLtdCompany.FullOrPartAdmit.PayBySetDate.Defendant';
