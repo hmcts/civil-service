f516f2b3f451f56e281a4533be187dca928b279d
mounikaammineni <Ayra@23082019>
2025-07-04 14:15:58 +0100

    Add test properties

--- src/test/java/uk/gov/hmcts/reform/civil/service/search/DefendantResponseDeadlineCheckSearchServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/search/DefendantResponseDeadlineCheckSearchServiceTest.java
@@ -6,6 +6,7 @@ import org.mockito.Mock;
 import uk.gov.hmcts.reform.civil.model.search.Query;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
@@ -27,10 +28,11 @@ class DefendantResponseDeadlineCheckSearchServiceTest extends ElasticSearchServi
 
     @Override
     protected Query buildQuery(int fromValue) {
+        String fixedNow = getSchedulerStartTimeFromThreadLocal();
         BoolQueryBuilder query = boolQuery()
             .minimumShouldMatch(1)
             .should(boolQuery()
-                        .must(rangeQuery("data.respondent1ResponseDeadline").lt("now"))
+                        .must(rangeQuery("data.respondent1ResponseDeadline").lt(fixedNow))
                         .mustNot(matchQuery("data.respondent1ResponseDeadlineChecked", "Yes"))
                         .must(boolQuery().must(matchQuery("state", "AWAITING_RESPONDENT_ACKNOWLEDGEMENT")))
                         .must(boolQuery()
@@ -41,5 +43,17 @@ class DefendantResponseDeadlineCheckSearchServiceTest extends ElasticSearchServi
             );
 
         return new Query(query, List.of("reference"), fromValue);
+    }
+
+    @SuppressWarnings("unchecked")
+    private String getSchedulerStartTimeFromThreadLocal() {
+        try {
+            Field fixedNowField = ElasticSearchService.class.getDeclaredField("schedulerStartTime");
+            fixedNowField.setAccessible(true);
+            ThreadLocal<String> fixedNow = (ThreadLocal<String>) fixedNowField.get(searchService);
+            return fixedNow.get();
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to retrieve fixed 'now' time from ThreadLocal", e);
+        }
     }
 }
