b9b13b6386f08c31fb0728f55abc8fddaa428c1e
Ruban <ruban72@hotmail.com>
2025-07-08 15:15:16 +0100

    DTSCCI-2409: Delete existing notifications for ccj

---
+++ src/main/resources/db/migration/V2025_07_08_1500__CIV-13792_update_judgment_by_admission_issued_defendant_scenario.sql
@@ -0,0 +1,12 @@
+/**
+ * Update scenario
+ */
+UPDATE dbs.scenario
+SET notifications_to_delete = '{"Notice.AAA6.ClaimantIntent.CCJ.Requested.Defendant",
+          "Notice.AAA6.ClaimantIntent.RequestedCCJ.ClaimantAcceptedDefendantPlan.Defendant",
+          "Notice.AAA6.ClaimantIntent.RequestCCJ.ClaimantAcceptOrRejectPlan.SettlementRequested.NoDefResponse.Defendant",
+          "Notice.AAA6.ClaimantIntent.RequestCCJ.ClaimantAcceptOrRejectPlan.SettlementRequested.DefPaymentMissed.Defendant",
+          "Notice.AAA6.DefResponse.FullOrPartAdmit.PayBySetDate.Defendant,
+          "Notice.AAA6.DefResponse.FullOrPartAdmit.PayByInstallments.Defendant,
+          "Notice.AAA6.DefResponse.FullOrPartAdmit.PayImmediately.Defendant"}'
+WHERE name = 'Scenario.AAA6.JudgmentsOnline.IssuedCCJ.Defendant';
