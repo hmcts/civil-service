c846859e70d47820a6b4c6b2a63ef37c57ab880e
Ruban <ruban72@hotmail.com>
2025-07-09 15:14:21 +0100

    DTSCCI-2409: Clear out PartAdmit notification

--- src/main/resources/db/migration/V2025_07_09_1500__CIV-13792_update_judgment_by_admission_issued_defendant_scenario.sql
+++ src/main/resources/db/migration/V2025_07_09_1500__CIV-13792_update_judgment_by_admission_issued_defendant_scenario.sql
@@ -9,5 +9,5 @@ SET notifications_to_delete = '{"Notice.AAA6.ClaimantIntent.CCJ.Requested.Defend
           "Notice.AAA6.DefResponse.FullOrPartAdmit.PayBySetDate.Defendant",
           "Notice.AAA6.DefResponse.FullOrPartAdmit.PayByInstallments.Defendant",
           "Notice.AAA6.DefResponse.FullOrPartAdmit.PayImmediately.Defendant",
-          "Notice.AAA6.ClaimantIntent.PartAdmit.Defendant"}',
+          "Notice.AAA6.ClaimantIntent.PartAdmit.Defendant"}'
 WHERE name = 'Scenario.AAA6.JudgmentsOnline.IssuedCCJ.Defendant';
