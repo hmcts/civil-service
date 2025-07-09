f256d171b55a186fbf4fd7fb2ced1e8cad02b234
Gabrielle Magruder <51362727+gmmagruder@users.noreply.github.com>
2025-07-09 11:17:50 +0100

    DTSCCI-2315: Refactor defendant response cui to notify all parties in the same event (#6672)
    * Add new notifier for defendant response cui
    
    * Add tests for new notifier
    
    * Change isApplicantRespresented() to !isApplicantLip
    
    ---------
    
    Co-authored-by: mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/CamundaProcessIdentifier.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/CamundaProcessIdentifier.java
@@ -25,6 +25,7 @@ public enum CamundaProcessIdentifier {
     JudgmentVariedDeterminationOfMeansNotifyParties,
     ClaimSubmittedApplicantNotifier,
     ClaimantLipHelpWithFeesNotifier,
+    DefendantResponseCUINotify,
     TakeCaseOfflineNotifier,
     UnpaidHearingFeeNotifier,
     ClaimantResponseConfirmsNotToProceedNotify,
