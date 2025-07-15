b1da6f93c7e974ef6cc7dd71f1b1e5d368e59415
drummondjm <mark.drummond@version.com>
2025-07-15 16:01:24 +0100

    lip cases which are multi or intermediate should not be taken offline via scheduler
    Also remove redundant logs from past changes

--- src/test/java/uk/gov/hmcts/reform/civil/service/search/TakeCaseOfflineSearchServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/search/TakeCaseOfflineSearchServiceTest.java
@@ -1,7 +1,9 @@
 package uk.gov.hmcts.reform.civil.service.search;
 
 import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.junit.jupiter.api.BeforeEach;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.model.search.Query;
 
 import java.util.List;
@@ -9,6 +11,7 @@ import java.util.List;
 import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
 import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 import static org.elasticsearch.index.query.QueryBuilders.rangeQuery;
+import static org.elasticsearch.index.query.QueryBuilders.termQuery;
 
 class TakeCaseOfflineSearchServiceTest extends ElasticSearchServiceTest {
 
@@ -19,11 +22,26 @@ class TakeCaseOfflineSearchServiceTest extends ElasticSearchServiceTest {
 
     @Override
     protected Query buildQuery(int fromValue) {
+        String allocatedTrackPath = "data.allocatedTrack";
+        String responseTrackPath = "data.responseClaimTrack";
+        String isLipCase = "data.isLipOnCase";
+
+        QueryBuilder lipOnCaseMultiOrIntermediateTrack = boolQuery()
+            .must(termQuery(isLipCase, YesOrNo.YES.name()))
+            .must(
+                boolQuery()
+                    .should(matchQuery(allocatedTrackPath, "MULTI_CLAIM"))
+                    .should(matchQuery(responseTrackPath, "MULTI_CLAIM"))
+                    .should(matchQuery(allocatedTrackPath, "INTERMEDIATE_CLAIM"))
+                    .should(matchQuery(responseTrackPath, "INTERMEDIATE_CLAIM"))
+            );
+
         BoolQueryBuilder query = boolQuery()
             .minimumShouldMatch(1)
             .should(boolQuery()
                         .must(rangeQuery("data.applicant1ResponseDeadline").lt("now"))
-                        .must(boolQuery().must(matchQuery("state", "AWAITING_APPLICANT_INTENTION"))))
+                        .must(boolQuery().must(matchQuery("state", "AWAITING_APPLICANT_INTENTION")))
+                        .mustNot(lipOnCaseMultiOrIntermediateTrack))
             .should(boolQuery()
                         .must(rangeQuery("data.addLegalRepDeadlineRes1").lt("now"))
                         .must(boolQuery().must(matchQuery("state", "AWAITING_RESPONDENT_ACKNOWLEDGEMENT"))))
