d2b80a18f56187b3c7f88fb6825c68dc53a12ae5
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2025-07-18 17:09:07 +0100

    TEST ONLY DO NOT MERGE

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/sealedclaim/SealedClaimResponseFormGeneratorForSpec.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/sealedclaim/SealedClaimResponseFormGeneratorForSpec.java
@@ -45,8 +45,6 @@ import static uk.gov.hmcts.reform.civil.enums.MultiPartyScenario.getMultiPartySc
 import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.DEFENDANT_RESPONSE_SPEC_SEALED_1V1_INSTALLMENTS;
 import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.DEFENDANT_RESPONSE_SPEC_SEALED_1V1;
 import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.DEFENDANT_RESPONSE_SPEC_SEALED_1V2;
-import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.DEFENDANT_RESPONSE_SPEC_SEALED_1V2_LR_ADMISSION_BULK;
-import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.DEFENDANT_RESPONSE_SPEC_SEALED_1V1_INSTALLMENTS_LR_ADMISSION_BULK;
 
 @Slf4j
 @Service
@@ -429,17 +427,12 @@ public class SealedClaimResponseFormGeneratorForSpec implements TemplateDataGene
     private DocmosisTemplates getTemplate(CaseData caseData) {
         log.info("Testing DTSCCI-2300");
         if (caseData.getRespondent2() != null && YesOrNo.YES.equals(caseData.getRespondentResponseIsSame())) {
-            return featureToggleService.isLrAdmissionBulkEnabled()
-                    ? DEFENDANT_RESPONSE_SPEC_SEALED_1V2_LR_ADMISSION_BULK
-                    : DEFENDANT_RESPONSE_SPEC_SEALED_1V2;
+            return DEFENDANT_RESPONSE_SPEC_SEALED_1V2;
         }
         return getDocmosisTemplateForSingleParty();
     }
 
     private DocmosisTemplates getDocmosisTemplateForSingleParty() {
-        if (featureToggleService.isLrAdmissionBulkEnabled()) {
-            return DEFENDANT_RESPONSE_SPEC_SEALED_1V1_INSTALLMENTS_LR_ADMISSION_BULK;
-        }
         return featureToggleService.isPinInPostEnabled()
                 ? DEFENDANT_RESPONSE_SPEC_SEALED_1V1_INSTALLMENTS
                 : DEFENDANT_RESPONSE_SPEC_SEALED_1V1;
