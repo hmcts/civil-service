34aa9b33511804f2628f8ae94a58cd97271c4ab5
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2025-07-24 11:57:02 +0100

    CIV-17507 changes

--- src/main/java/uk/gov/hmcts/reform/civil/service/JudgementService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/JudgementService.java
@@ -60,7 +60,7 @@ public class JudgementService {
         BigDecimal claimAmount = caseData.getTotalClaimAmount();
         if (isLrFullAdmitRepaymentPlan(caseData)
             || isLrFullAdmitPayImmediately(caseData)) {
-            BigDecimal interest = interestCalculator.calculateInterest(caseData);
+            BigDecimal interest = getLatestInterest(caseData);
             claimAmount = claimAmount.add(interest);
         } else {
             if (caseData.isPartAdmitClaimSpec()) {
@@ -175,7 +175,7 @@ public class JudgementService {
         return caseData.getCcjPaymentDetails().getCcjJudgmentFixedCostOption();
     }
 
-    public boolean isLipVLipFullAdmitSetDate(CaseData caseData) {
-        return caseData.isLipvLipOneVOne() && caseData.isPayBySetDate() && caseData.isFullAdmitClaimSpec();
+    public BigDecimal getLatestInterest(CaseData caseData) {
+        return interestCalculator.calculateInterest(caseData);
     }
 }
