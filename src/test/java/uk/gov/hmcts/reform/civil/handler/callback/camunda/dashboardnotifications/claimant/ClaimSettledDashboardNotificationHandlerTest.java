37da6a3c08d0586f217a62b70c199b459eec4672
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2025-07-28 09:51:00 +0100

    clean up QM LR code - remove test QM when is offline

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/ClaimSettledDashboardNotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimant/ClaimSettledDashboardNotificationHandlerTest.java
@@ -77,41 +77,6 @@ class ClaimSettledDashboardNotificationHandlerTest  extends BaseCallbackHandlerT
         }
 
         @Test
-        void shouldRecordScenarioQmLrIsOff_whenInvoked() {
-            CaseData caseData = CaseDataBuilder.builder().atStateClaimSubmittedSmallClaim()
-                .applicant1Represented(YesOrNo.NO)
-                .caseDataLip(CaseDataLiP.builder().applicant1SettleClaim(YesOrNo.YES)
-                                 .applicant1ClaimSettledDate(
-                                     LocalDate.now()).build()).build();
-
-            HashMap<String, Object> scenarioParams = new HashMap<>();
-            scenarioParams.put("applicant1ClaimSettledDateEn", caseData.getApplicant1ClaimSettleDate());
-            scenarioParams.put("applicant1ClaimSettledDateCy", caseData.getApplicant1ClaimSettleDate());
-
-            when(mapper.mapCaseDataToParams(any())).thenReturn(scenarioParams);
-
-            CallbackParams params = CallbackParamsBuilder.builder().of(ABOUT_TO_SUBMIT, caseData).request(
-                CallbackRequest.builder().eventId(CREATE_DASHBOARD_NOTIFICATION_FOR_CLAIM_SETTLED_FOR_CLAIMANT1.name()).build()
-            ).build();
-
-            handler.handle(params);
-            verify(dashboardNotificationService).deleteByReferenceAndCitizenRole(
-                caseData.getCcdCaseReference().toString(),
-                "CLAIMANT");
-            verify(taskListService).makeProgressAbleTasksInactiveForCaseIdentifierAndRoleExcludingTemplate(
-                caseData.getCcdCaseReference().toString(),
-                "CLAIMANT",
-                "Application.View");
-
-            verify(dashboardScenariosService).recordScenarios(
-                "BEARER_TOKEN",
-                "Scenario.AAA6.ClaimantIntent.ClaimSettledEvent.Claimant",
-                caseData.getCcdCaseReference().toString(),
-                ScenarioRequestParams.builder().params(scenarioParams).build()
-            );
-        }
-
-        @Test
         void shouldRecordScenarioQmLrIsOnAndIsNonEaCourt_whenInvoked() {
             CaseData caseData = CaseDataBuilder.builder()
                 .atStateClaimSubmittedSmallClaim()
