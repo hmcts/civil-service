63c73489696a52b9ac5072ce1f53a1c3a877f296
Velican <duzgunce.vd@gmail.com>
2025-08-07 14:30:46 +0100

    clean up to use static text

--- src/main/resources/db/migration/V2025_08_06_1600__DTSCCI-2381_claimant_intent_part_admit_defendant_scenario.sql
+++ src/main/resources/db/migration/V2025_08_06_1600__DTSCCI-2381_claimant_intent_part_admit_defendant_scenario.sql
@@ -1,15 +1,8 @@
 /**
- * Update scenario
- */
-UPDATE dbs.scenario
-SET notifications_to_create = '{"Notice.AAA6.ClaimantIntent.PartAdmit.Defendant" : ["defendantAdmittedAmount", "amountIncludesTextEn", "amountIncludesTextCy", "respondent1AdmittedAmountPaymentDeadlineEn", "respondent1AdmittedAmountPaymentDeadlineCy", "applicant1PartyName"]}'
-WHERE name ='Scenario.AAA6.ClaimantIntent.PartAdmit.Defendant';
-
-/**
  * Update notification template
  */
 UPDATE dbs.dashboard_notifications_templates
-SET description_En = '<p class="govuk-body">${applicant1PartyName} has accepted your offer to pay ${defendantAdmittedAmount}${amountIncludesTextEn}, immediately in full and final settlement of the claim. Funds must be received in <a href={VIEW_INFO_ABOUT_CLAIMANT} class="govuk-link">their account</a> by ${respondent1AdmittedAmountPaymentDeadlineEn}.</p><p class="govuk-body">If they don''t receive the money by then, they can request a County Court Judgment (CCJ).</p>',
-    description_Cy = '<p class="govuk-body">Mae ${applicant1PartyName} wedi derbyn eich cynnig i dalu ${defendantAdmittedAmount}${amountIncludesTextCy}, yn llawn ar unwaith fel setliad llawn a therfynol o’r hawliad.  Rhaid i’r arian fod yn <a href={VIEW_INFO_ABOUT_CLAIMANT} class="govuk-link">ei g/chyfrif</a> erbyn ${respondent1AdmittedAmountPaymentDeadlineCy}.</p><p class="govuk-body"> Os na fydd yr arian wedi cyrraedd erbyn hynny, gallant ofyn am Ddyfarniad Llys Sirol (CCJ).</p>'
+SET description_En = '<p class="govuk-body">${applicant1PartyName} has accepted your offer to pay ${defendantAdmittedAmount}, plus the claim fee and any fixed costs claimed, immediately in full and final settlement of the claim. Funds must be received in <a href={VIEW_INFO_ABOUT_CLAIMANT} class="govuk-link">their account</a> by ${respondent1AdmittedAmountPaymentDeadlineEn}.</p><p class="govuk-body">If they don''t receive the money by then, they can request a County Court Judgment (CCJ).</p>',
+    description_Cy = '<p class="govuk-body">Mae ${applicant1PartyName} wedi derbyn eich cynnig i dalu ${defendantAdmittedAmount}, ynghyd â ffi’r hawliad ac unrhyw gostau sefydlog a hawlir, yn llawn ar unwaith fel setliad llawn a therfynol o’r hawliad.  Rhaid i’r arian fod yn <a href={VIEW_INFO_ABOUT_CLAIMANT} class="govuk-link">ei g/chyfrif</a> erbyn ${respondent1AdmittedAmountPaymentDeadlineCy}.</p><p class="govuk-body"> Os na fydd yr arian wedi cyrraedd erbyn hynny, gallant ofyn am Ddyfarniad Llys Sirol (CCJ).</p>'
 WHERE template_name = 'Notice.AAA6.ClaimantIntent.PartAdmit.Defendant' and notification_role = 'DEFENDANT';
 
