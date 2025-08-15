38894d97c08cca7fc53c8d0a4333e111adcda2ef
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2025-08-15 10:10:57 +0100

    CIV-17604 changes

--- src/main/resources/db/migration/V2025_08_15_1030__CIV-17604_update_pay_by_installments_ccj_scenario.sql
+++ src/main/resources/db/migration/V2025_08_15_1030__CIV-17604_update_pay_by_installments_ccj_scenario.sql
@@ -13,3 +13,15 @@ update dbs.scenario set notifications_to_delete = '{"Notice.AAA6.ClaimantIntent.
           "Notice.AAA6.DefResponse.PartAdmit.PayImmediately.Claimant",
           "Notice.AAA6.ClaimantIntent.FullAdmit.Claimant"}'
 where name = 'Scenario.AAA6.Update.JudgmentsOnline.IssuedCCJ.Claimant';
+
+
+update dbs.scenario set notifications_to_delete = '{"Notice.AAA6.ClaimantIntent.CCJ.Requested.Defendant",
+          "Notice.AAA6.ClaimantIntent.RequestedCCJ.ClaimantAcceptedDefendantPlan.Defendant",
+          "Notice.AAA6.ClaimantIntent.RequestCCJ.ClaimantAcceptOrRejectPlan.SettlementRequested.NoDefResponse.Defendant",
+          "Notice.AAA6.ClaimantIntent.RequestCCJ.ClaimantAcceptOrRejectPlan.SettlementRequested.DefPaymentMissed.Defendant",
+          "Notice.AAA6.DefResponse.OrgOrLtdCompany.FullOrPartAdmit.PayBySetDate.Defendant",
+          "Notice.AAA6.DefResponse.OrgOrLtdCompany.FullOrPartAdmit.PayByInstallments.Defendant",
+          "Notice.AAA6.DefResponse.FullOrPartAdmit.PayBySetDate.Defendant",
+          "Notice.AAA6.DefResponse.FullOrPartAdmit.PayByInstallments.Defendant",
+          "Notice.AAA6.DefResponse.FullOrPartAdmit.PayImmediately.Defendant"}'
+where name = 'Scenario.AAA6.JudgmentsOnline.IssuedCCJ.Defendant';
