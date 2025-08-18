a6f7e1d2dc82ea9213f67f2ff73a8d7394b6f525
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-08-18 13:43:25 +0100

    DTSCCI-1817 checkstyle

--- src/test/java/uk/gov/hmcts/reform/civil/service/robotics/mapper/EventHistoryMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/robotics/mapper/EventHistoryMapperTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.civil.service.robotics.mapper;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
