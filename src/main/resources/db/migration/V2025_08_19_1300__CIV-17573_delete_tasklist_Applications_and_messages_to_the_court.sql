e479bcdaa843732139908891ccd8313ff5642702
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2025-08-21 10:38:41 +0100

    Create V2025_08_19_1300__CIV-17573_delete_tasklist_Applications_and_messages_to_the_court.sql

---
+++ src/main/resources/db/migration/V2025_08_19_1300__CIV-17573_delete_tasklist_Applications_and_messages_to_the_court.sql
@@ -0,0 +1,19 @@
+/**
+ * delete all the task items from task item template where category = 'Applications and messages to the court'
+  forcing it to fail
+ */
+delete
+from dbs.task_list
+WHERE EXISTS (
+  SELECT -1
+  FROM dbs.task_item_template
+  WHERE dbs.task_item_template.id = dbs.task_list.task_item_template_id
+    and dbs.task_item_template.category_en = 'Applications and messages to the court'
+);
+
+/**
+ * delete all item template where category = 'Applications and messages to the court'
+ */
+delete
+from dbs.task_item_template
+where category_en = 'Applications and messages to the courtss'
