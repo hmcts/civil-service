58dd802e674a6c5d8bb7d0c1d44eede7eacfd64b
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-08-21 10:40:02 +0100

    DTSCCI-2371 changes

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ClaimantResponseCuiCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ClaimantResponseCuiCallbackHandlerTest.java
@@ -82,6 +82,7 @@ import static uk.gov.hmcts.reform.civil.enums.RespondentResponsePartAdmissionPay
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.YES;
 import static uk.gov.hmcts.reform.civil.enums.dq.HearingLength.ONE_DAY;
+import static uk.gov.hmcts.reform.civil.service.PaymentDateService.DART_FORMATTER;
 import static uk.gov.hmcts.reform.civil.utils.ElementUtils.wrapElements;
 
 @ExtendWith(MockitoExtension.class)
@@ -688,7 +689,8 @@ class ClaimantResponseCuiCallbackHandlerTest extends BaseCallbackHandlerTest {
 
         @Test
         void shouldUpdateDefendantPaymentDeadlineForPartAdmitImmediateWhenClaimantAcceptedRepaymentPlan() {
-            when(paymentDateService.getFormattedPaymentDate(any())).thenReturn("2021-01-01");
+            LocalDate paymentDeadline = LocalDate.now().plusDays(1);
+            when(paymentDateService.calculatePaymentDeadline()).thenReturn(paymentDeadline);
             CaseData caseData = CaseDataBuilder.builder()
                 .applicant1ResponseDate(LocalDateTime.now())
                 .applicant1(Party.builder().type(Party.Type.INDIVIDUAL).partyName("CLAIMANT_NAME").build())
@@ -706,7 +708,7 @@ class ClaimantResponseCuiCallbackHandlerTest extends BaseCallbackHandlerTest {
             CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
             var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
             CaseData updatedCaseData = getCaseData(response);
-            assertThat(updatedCaseData.getWhenToBePaidText()).isEqualTo("2021-01-01");
+            assertThat(updatedCaseData.getWhenToBePaidText()).isEqualTo(paymentDeadline.format(DART_FORMATTER));
         }
     }
 
