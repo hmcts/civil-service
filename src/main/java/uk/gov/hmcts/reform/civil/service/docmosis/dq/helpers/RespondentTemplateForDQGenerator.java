8bda9dd7c030c306057a722888dd370dd845f4aa
mounikaammineni <Ayra@23082019>
2025-08-22 15:20:09 +0100

    Add override

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/dq/helpers/RespondentTemplateForDQGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/dq/helpers/RespondentTemplateForDQGenerator.java
@@ -34,7 +34,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
-import java.util.stream.IntStream;
 
 import static java.util.Objects.nonNull;
 import static java.util.Optional.ofNullable;
