2e147ebf33f3a769a5da8a3dc32cf6b67e5f5e0e
Ruban <ruban72@hotmail.com>
2025-09-03 15:50:39 +0100

    DTSCCI-2572: Fix CS issue

--- src/test/java/uk/gov/hmcts/reform/civil/utils/CaseMigrationEncryptionUtilTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/utils/CaseMigrationEncryptionUtilTest.java
@@ -12,7 +12,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 class CaseMigrationEncryptionUtilTest {
 
     @Test
