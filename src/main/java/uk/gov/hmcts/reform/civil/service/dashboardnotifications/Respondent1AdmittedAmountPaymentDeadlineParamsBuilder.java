ddc0e26a3ae97e1e38327eda900c78ec5c803605
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-09-11 12:06:40 +0100

    DTSCCI-2369 changes

--- src/main/java/uk/gov/hmcts/reform/civil/service/dashboardnotifications/Respondent1AdmittedAmountPaymentDeadlineParamsBuilder.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/dashboardnotifications/Respondent1AdmittedAmountPaymentDeadlineParamsBuilder.java
@@ -20,14 +20,14 @@ public class Respondent1AdmittedAmountPaymentDeadlineParamsBuilder extends Dashb
             var amountIncludesTextEn = DateUtils.formatDate(whenWillThisAmountBePaid);
             var amountIncludesTextCy = DateUtils.formatDateInWelsh(whenWillThisAmountBePaid, false);
 
-            var descriptionEn = String.format("<p class=\"govuk-body\">You've said you owe £%s plus the claim fee and " +
+            var descriptionEn = String.format("<p class=\"govuk-body\">You've said you owe %s plus the claim fee and " +
                                                   "any fixed costs claimed and offered to pay %s immediately. " +
                                                   "We will contact you when the claimant responds." +
                                                   "</p>",
                                               defendantAdmittedAmount,
                                               amountIncludesTextEn);
 
-            var descriptionCy = String.format("<p class=\"govuk-body\">Rydych chi wedi dweud bod £%s yn ddyledus gennych, " +
+            var descriptionCy = String.format("<p class=\"govuk-body\">Rydych chi wedi dweud bod %s yn ddyledus gennych, " +
                                                   "a ffi’r hawliad ac unrhyw gostau sefydlog a hawlir ac rydych wedi cynnig i dalu %s ar unwaith. " +
                                                   "Byddwn yn cysylltu â chi pan fydd yr hawlydd yn ymateb.</p>",
                                               defendantAdmittedAmount,
@@ -48,14 +48,15 @@ public class Respondent1AdmittedAmountPaymentDeadlineParamsBuilder extends Dashb
             var amountIncludesTextEn = params.get("amountIncludesTextEn") != null ? params.get("amountIncludesTextEn").toString() : "";
             var amountIncludesTextCy = params.get("amountIncludesTextCy") != null ? params.get("amountIncludesTextCy").toString() : "";
             var applicant1PartyName = params.get("applicant1PartyName") != null ? params.get("applicant1PartyName").toString() : "";
-            var descriptionEn = String.format("<p class=\"govuk-body\">You have offered to pay %s by %s%s. The payment must be received in %s''s account by then, " +
+            var descriptionEn = String.format("<p class=\"govuk-body\">You have offered to pay %s by %s%s. The payment must be received in %s's account by then, " +
                                                   "if not they can request a county court judgment.</p><p class=\"govuk-body\"><a href=\"{VIEW_RESPONSE_TO_CLAIM}\" class=\"govuk-link\">View your response</a></p>",
                                               defendantAdmittedAmount,
                                               respondent1AdmittedAmountPaymentDeadlineEn,
                                               amountIncludesTextEn,
                                               applicant1PartyName);
 
-            var descriptionCy = String.format("<p class=\"govuk-body\">Rydych wedi cynnig talu %s erbyn %s%s. Rhaid i’r taliad fod yng nghyfrif %s erbyn y dyddiad hwnnw. Os nad yw, yna gallant wneud cais am ddyfarniad llys sirol.</p><p class=\"govuk-body\"><a href=\"{VIEW_RESPONSE_TO_CLAIM}\" class=\"govuk-link\">Gweld eich ymateb</a></p>",
+            var descriptionCy = String.format("<p class=\"govuk-body\">Rydych wedi cynnig talu %s erbyn %s%s. Rhaid i’r taliad fod yng nghyfrif %s erbyn y dyddiad hwnnw. Os nad yw, " +
+                                                  "yna gallant wneud cais am ddyfarniad llys sirol.</p><p class=\"govuk-body\"><a href=\"{VIEW_RESPONSE_TO_CLAIM}\" class=\"govuk-link\">Gweld eich ymateb</a></p>",
                                               defendantAdmittedAmount,
                                               respondent1AdmittedAmountPaymentDeadlineCy,
                                               amountIncludesTextCy,
