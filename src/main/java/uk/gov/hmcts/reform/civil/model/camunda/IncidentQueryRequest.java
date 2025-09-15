c9d38ae12f2e104bc220c2a4562713342cf35a5c
mounikaammineni <Ayra@23082019>
2025-09-15 11:09:41 +0100

    update

--- src/main/java/uk/gov/hmcts/reform/civil/model/camunda/IncidentQueryRequest.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/camunda/IncidentQueryRequest.java
@@ -1,6 +1,10 @@
 package uk.gov.hmcts.reform.civil.model.camunda;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.OffsetDateTime;
 import java.util.List;
