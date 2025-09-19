cfee5427316fda2a5fff54bc38e8eb9b3e0da6d0
jeswanth <jeswanth.reddy@hmcts.net>
2025-09-19 14:15:07 +0100

    CIV-17723 release clean up

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/caseevents/SendHearingToLiPCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/caseevents/SendHearingToLiPCallbackHandlerTest.java
@@ -11,11 +11,8 @@ import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
 import uk.gov.hmcts.reform.civil.documentmanagement.model.Document;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
-import uk.gov.hmcts.reform.civil.enums.dq.Language;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.citizenui.CaseDataLiP;
-import uk.gov.hmcts.reform.civil.model.citizenui.RespondentLiPResponse;
 import uk.gov.hmcts.reform.civil.sampledata.CallbackParamsBuilder;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
@@ -31,18 +28,19 @@ import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.SEND_HEARING_TO_LIP_CLAIMANT;
-import static uk.gov.hmcts.reform.civil.callback.CaseEvent.SEND_HEARING_TO_LIP_CLAIMANT_HMC;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.SEND_HEARING_TO_LIP_DEFENDANT;
-import static uk.gov.hmcts.reform.civil.callback.CaseEvent.SEND_HEARING_TO_LIP_DEFENDANT_HMC;
 import static uk.gov.hmcts.reform.civil.documentmanagement.model.DocumentType.HEARING_FORM;
-import static uk.gov.hmcts.reform.civil.documentmanagement.model.DocumentType.HEARING_FORM_WELSH;
-import static uk.gov.hmcts.reform.civil.handler.callback.camunda.caseevents.SendHearingToLiPCallbackHandler.TASK_ID_CLAIMANT_HMC;
-import static uk.gov.hmcts.reform.civil.handler.callback.camunda.caseevents.SendHearingToLiPCallbackHandler.TASK_ID_DEFENDANT_HMC;
 import static uk.gov.hmcts.reform.civil.utils.ElementUtils.wrapElements;
 
 @ExtendWith(MockitoExtension.class)
 class SendHearingToLiPCallbackHandlerTest extends BaseCallbackHandlerTest {
 
+    public static final String TASK_ID_DEFENDANT = "SendHearingToDefendantLIP";
+    public static final String TASK_ID_CLAIMANT = "SendHearingToClaimantLIP";
+    private static final String TEST = "test";
+    private static final String UPLOAD_TIMESTAMP = "14 Apr 2024 00:00:00";
+    private static final Document DOCUMENT_LINK = new Document("document/url", TEST, TEST, TEST, TEST,
+            UPLOAD_TIMESTAMP);
     @InjectMocks
     private SendHearingToLiPCallbackHandler handler;
     @Mock
@@ -54,19 +52,13 @@ class SendHearingToLiPCallbackHandlerTest extends BaseCallbackHandlerTest {
     @Mock
     private DashboardScenariosService dashboardScenariosService;
 
-    public static final String TASK_ID_DEFENDANT = "SendHearingToDefendantLIP";
-    public static final String TASK_ID_CLAIMANT = "SendHearingToClaimantLIP";
-    private static final String TEST = "test";
-    private static final String UPLOAD_TIMESTAMP = "14 Apr 2024 00:00:00";
-    private static final Document DOCUMENT_LINK = new Document("document/url", TEST, TEST, TEST, TEST, UPLOAD_TIMESTAMP);
-
     @Test
     void shouldNotCallRecordScenario_whenCaseProgressionIsDisabled() {
         when(featureToggleService.isCaseProgressionEnabled()).thenReturn(false);
 
         CallbackParams callbackParams = CallbackParamsBuilder.builder()
-            .of(ABOUT_TO_SUBMIT, CaseData.builder().build())
-            .build();
+                .of(ABOUT_TO_SUBMIT, CaseData.builder().build())
+                .build();
 
         handler.handle(callbackParams);
         verifyNoInteractions(dashboardScenariosService);
@@ -82,18 +74,18 @@ class SendHearingToLiPCallbackHandlerTest extends BaseCallbackHandlerTest {
     void shouldReturnCorrectCamundaActivityId_whenInvoked() {
         assertThat(handler.camundaActivityId(CallbackParamsBuilder.builder().request(CallbackRequest.builder().eventId(
                 SEND_HEARING_TO_LIP_DEFENDANT.name()).build())
-                                                 .build())).isEqualTo(TASK_ID_DEFENDANT);
+                .build())).isEqualTo(TASK_ID_DEFENDANT);
         assertThat(handler.camundaActivityId(CallbackParamsBuilder.builder().request(CallbackRequest.builder().eventId(
                 SEND_HEARING_TO_LIP_CLAIMANT.name()).build())
-                                                 .build())).isEqualTo(TASK_ID_CLAIMANT);
+                .build())).isEqualTo(TASK_ID_CLAIMANT);
     }
 
     @Test
     void shouldDownloadDocumentAndPrintLetterSuccessfully() {
         // given
         CaseData caseData = CaseDataBuilder.builder()
-            .systemGeneratedCaseDocuments(wrapElements(CaseDocument.builder().documentType(HEARING_FORM).build()))
-            .respondent1Represented(YesOrNo.NO).build();
+                .systemGeneratedCaseDocuments(wrapElements(CaseDocument.builder().documentType(HEARING_FORM).build()))
+                .respondent1Represented(YesOrNo.NO).build();
         CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
         params.getRequest().setEventId(SEND_HEARING_TO_LIP_DEFENDANT.name());
         when(featureToggleService.isCaseProgressionEnabled()).thenReturn(true);
@@ -109,8 +101,8 @@ class SendHearingToLiPCallbackHandlerTest extends BaseCallbackHandlerTest {
     void shouldDownloadDocumentAndPrintLetterSuccessfullyWhenIsClaimant() {
         // given
         CaseData caseData = CaseDataBuilder.builder()
-            .systemGeneratedCaseDocuments(wrapElements(CaseDocument.builder().documentType(HEARING_FORM).build()))
-            .applicant1Represented(YesOrNo.NO).build();
+                .systemGeneratedCaseDocuments(wrapElements(CaseDocument.builder().documentType(HEARING_FORM).build()))
+                .applicant1Represented(YesOrNo.NO).build();
         CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
         params.getRequest().setEventId(SEND_HEARING_TO_LIP_CLAIMANT.name());
         // when
@@ -122,46 +114,50 @@ class SendHearingToLiPCallbackHandlerTest extends BaseCallbackHandlerTest {
         verify(sendHearingBulkPrintService).sendHearingToLIP(any(), any(), eq(TASK_ID_CLAIMANT), eq(false));
     }
 
-//    @Test
-//    void shouldDownloadDocumentAndPrintWelshLetterSuccessfully() {
-//        // given
-//        CaseDataLiP caseDataLiP = CaseDataLiP.builder()
-//            .respondent1LiPResponse(RespondentLiPResponse.builder().respondent1ResponseLanguage("BOTH").build()).build();
-//        CaseData caseData = CaseDataBuilder.builder().atStateRespondentFullDefence().build().toBuilder()
-//            .caseDataLiP(caseDataLiP)
-//            .systemGeneratedCaseDocuments(wrapElements(CaseDocument.builder().documentType(HEARING_FORM).build()))
-//            .respondent1Represented(YesOrNo.NO).build();
-//;
-//        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
-//        params.getRequest().setEventId(SEND_HEARING_TO_LIP_DEFENDANT_HMC.name());
-//        when(featureToggleService.isCaseProgressionEnabled()).thenReturn(true);
-//        when(featureToggleService.isHmcForLipEnabled()).thenReturn(true);
-//        // when
-//        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
-//
-//        // then
-//        assertThat(response.getErrors()).isNull();
-//        verify(sendHearingBulkPrintService).sendHearingToLIP(any(), any(), eq(TASK_ID_DEFENDANT_HMC), eq(true));
-//    }
+    // @Test
+    // void shouldDownloadDocumentAndPrintWelshLetterSuccessfully() {
+    // // given
+    // CaseDataLiP caseDataLiP = CaseDataLiP.builder()
+    // .respondent1LiPResponse(RespondentLiPResponse.builder().respondent1ResponseLanguage("BOTH").build()).build();
+    // CaseData caseData =
+    // CaseDataBuilder.builder().atStateRespondentFullDefence().build().toBuilder()
+    // .caseDataLiP(caseDataLiP)
+    // .systemGeneratedCaseDocuments(wrapElements(CaseDocument.builder().documentType(HEARING_FORM).build()))
+    // .respondent1Represented(YesOrNo.NO).build();
+    // ;
+    // CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+    // params.getRequest().setEventId(SEND_HEARING_TO_LIP_DEFENDANT_HMC.name());
+    // when(featureToggleService.isCaseProgressionEnabled()).thenReturn(true);
+    // when(featureToggleService.isHmcForLipEnabled()).thenReturn(true);
+    // // when
+    // var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+    //
+    // // then
+    // assertThat(response.getErrors()).isNull();
+    // verify(sendHearingBulkPrintService).sendHearingToLIP(any(), any(),
+    // eq(TASK_ID_DEFENDANT_HMC), eq(true));
+    // }
 
-//    @Test
-//    void shouldDownloadDocumentAndPrintLetterWelshSuccessfullyWhenIsClaimant() {
-//        // given
-//        CaseDocument caseDocument = CaseDocument.builder().documentType(HEARING_FORM_WELSH).documentLink(DOCUMENT_LINK).build();
-//        CaseData caseData = CaseDataBuilder.builder()
-//            .systemGeneratedCaseDocuments(wrapElements(CaseDocument.builder().documentType(HEARING_FORM).build()))
-//            .applicant1Represented(YesOrNo.NO)
-//            .claimantBilingualLanguagePreference(Language.BOTH.toString()).build().toBuilder()
-//            .hearingDocumentsWelsh(wrapElements(caseDocument)).build();
-//        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
-//        params.getRequest().setEventId(SEND_HEARING_TO_LIP_CLAIMANT_HMC.name());
-//        // when
-//        when(featureToggleService.isCaseProgressionEnabled()).thenReturn(true);
-//        when(featureToggleService.isHmcForLipEnabled()).thenReturn(true);
-//        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
-//
-//        // then
-//        assertThat(response.getErrors()).isNull();
-//        verify(sendHearingBulkPrintService).sendHearingToLIP(any(), any(), eq(TASK_ID_CLAIMANT_HMC), eq(true));
-//    }
+    // @Test
+    // void shouldDownloadDocumentAndPrintLetterWelshSuccessfullyWhenIsClaimant() {
+    // // given
+    // CaseDocument caseDocument =
+    // CaseDocument.builder().documentType(HEARING_FORM_WELSH).documentLink(DOCUMENT_LINK).build();
+    // CaseData caseData = CaseDataBuilder.builder()
+    // .systemGeneratedCaseDocuments(wrapElements(CaseDocument.builder().documentType(HEARING_FORM).build()))
+    // .applicant1Represented(YesOrNo.NO)
+    // .claimantBilingualLanguagePreference(Language.BOTH.toString()).build().toBuilder()
+    // .hearingDocumentsWelsh(wrapElements(caseDocument)).build();
+    // CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+    // params.getRequest().setEventId(SEND_HEARING_TO_LIP_CLAIMANT_HMC.name());
+    // // when
+    // when(featureToggleService.isCaseProgressionEnabled()).thenReturn(true);
+    // when(featureToggleService.isHmcForLipEnabled()).thenReturn(true);
+    // var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+    //
+    // // then
+    // assertThat(response.getErrors()).isNull();
+    // verify(sendHearingBulkPrintService).sendHearingToLIP(any(), any(),
+    // eq(TASK_ID_CLAIMANT_HMC), eq(true));
+    // }
 }
