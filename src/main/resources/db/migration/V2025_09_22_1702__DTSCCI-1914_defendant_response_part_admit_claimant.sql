c99023cb94cd543dbdb4d23590fef11c64e9e8a5
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-09-22 16:35:08 +0100

    DTSCCI-1914 Update notification

---
+++ src/main/resources/db/migration/V2025_09_22_1702__DTSCCI-1914_defendant_response_part_admit_claimant.sql
@@ -0,0 +1,6 @@
+/**
+ * Update scenario
+ */
+update dbs.scenario
+set notifications_to_delete = '{"Notice.AAA6.DefResponse.ResponseTimeElapsed.Claimant","Notice.AAA6.DefResponse.PartAdmit.PayImmediately.Claimant", "Notice.AAA6.DefResponse.FullOrPartAdmit.PayBySetDate.Claimant", "Notice.AAA6.DefResponse.FullOrPartAdmit.PayByInstalments.Claimant", "Notice.AAA6.DefResponse.OrgOrLtdCompany.FullOrPartAdmit.PayBySetDate.Claimant", "Notice.AAA6.DefResponse.OrgOrLtdCompany.FullOrPartAdmit.PayByInstallments.Claimant", "Notice.AAA6.DefResponse.PartAdmit.AlreadyPaid.Claimant", "Notice.AAA6.DefResponse.FullDefence.AlreadyPaid.Claimant", "Notice.AAA6.DefResponse.FullDefence.FullDispute.RefusedMediation.Claimant", "Notice.AAA6.DefResponse.FullDefence.FullDispute.SuggestedMediation.Claimant", "Notice.AAA6.DefResponse.FullDefence.FullDispute.Multi.Int.Fast.Claimant"}'
+where name = 'Scenario.AAA6.DefResponse.PartAdmit.PayImmediately.Claimant';
