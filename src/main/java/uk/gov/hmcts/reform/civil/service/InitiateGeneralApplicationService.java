a6270a0f446d45cc4b3fc7e84444a922b6817600
rishikrsharma <rishikrsharma@users.noreply.github.com>
2025-09-22 13:39:47 +0100

    DTSCCI-2673: adding log messages (#7029)

--- src/main/java/uk/gov/hmcts/reform/civil/service/InitiateGeneralApplicationService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/InitiateGeneralApplicationService.java
@@ -154,6 +154,7 @@ public class InitiateGeneralApplicationService {
     }
 
     private boolean isLipCase(CaseData caseData, CaseAssignmentUserRolesResource userRoles) {
+        log.info("Checking isLipCase for case: {} and application feature is enabled {}", caseData.getCcdCaseReference(), featureToggleService.isGaForLipsEnabled());
         if (featureToggleService.isGaForLipsEnabled() && (caseData.isRespondent1LiP() || caseData.isRespondent2LiP()
             || caseData.isApplicantNotRepresented())) {
 
