42ffa32cd614c008e8d2db53ca3a0896736ba632
jeswanth <jeswanth.reddy@hmcts.net>
2025-09-22 12:26:59 +0100

    CIV-17723 release clean up

--- src/test/java/uk/gov/hmcts/reform/civil/model/citizenui/DashboardClaimStatusFactoryTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/model/citizenui/DashboardClaimStatusFactoryTest.java
@@ -38,7 +38,6 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static uk.gov.hmcts.reform.civil.utils.ElementUtils.element;
 
 class DashboardClaimStatusFactoryTest {
@@ -309,32 +308,29 @@ class DashboardClaimStatusFactoryTest {
 
     private CaseData fastClaim(List<CaseEventDetail> eventHistory, FeatureToggleService toggleService) {
         eventHistory.add(CaseEventDetail.builder()
-                             .createdDate(LocalDateTime.now())
-                             .id(CaseEvent.CREATE_SDO.name())
-                             .build());
+                .createdDate(LocalDateTime.now())
+                .id(CaseEvent.CREATE_SDO.name())
+                .build());
         CaseDocument sdoDocument = CaseDocument.builder()
-            .documentType(DocumentType.SDO_ORDER)
-            .createdDatetime(LocalDateTime.now())
-            .build();
+                .documentType(DocumentType.SDO_ORDER)
+                .createdDatetime(LocalDateTime.now())
+                .build();
         DynamicListElement selectedCourt = DynamicListElement.builder()
-            .code("00002").label("court 2 - 2 address - Y02 7RB").build();
+                .code("00002").label("court 2 - 2 address - Y02 7RB").build();
 
         CaseData caseData = CaseData.builder()
-            .ccdState(CaseState.CASE_PROGRESSION)
-            .responseClaimTrack(AllocatedTrack.FAST_CLAIM.name())
-            .totalClaimAmount(BigDecimal.valueOf(1000))
-            .caseManagementLocation(CaseLocationCivil.builder().baseLocation(selectedCourt.getCode()).build())
-            .systemGeneratedCaseDocuments(List.of(Element.<CaseDocument>builder()
-                                                      .value(sdoDocument).build()))
-            .drawDirectionsOrderRequired(YesOrNo.NO)
-            .claimsTrack(ClaimsTrack.fastTrack)
-            .build();
-            checkStatus(caseData, eventHistory,
-                        DashboardClaimStatus.SDO_ORDER_CREATED_CP,
-                        DashboardClaimStatus.SDO_ORDER_CREATED_CP
-            );
-
-
+                .ccdState(CaseState.CASE_PROGRESSION)
+                .responseClaimTrack(AllocatedTrack.FAST_CLAIM.name())
+                .totalClaimAmount(BigDecimal.valueOf(1000))
+                .caseManagementLocation(CaseLocationCivil.builder().baseLocation(selectedCourt.getCode()).build())
+                .systemGeneratedCaseDocuments(List.of(Element.<CaseDocument>builder()
+                        .value(sdoDocument).build()))
+                .drawDirectionsOrderRequired(YesOrNo.NO)
+                .claimsTrack(ClaimsTrack.fastTrack)
+                .build();
+        checkStatus(caseData, eventHistory,
+                DashboardClaimStatus.SDO_ORDER_CREATED_CP,
+                DashboardClaimStatus.SDO_ORDER_CREATED_CP);
         return caseData;
     }
 
@@ -539,23 +535,22 @@ class DashboardClaimStatusFactoryTest {
 
     private CaseData smallClaim(List<CaseEventDetail> eventHistory, FeatureToggleService toggleService) {
         CaseDocument sdoDocument = CaseDocument.builder()
-            .documentType(DocumentType.SDO_ORDER)
-            .createdDatetime(LocalDateTime.now())
-            .build();
+                .documentType(DocumentType.SDO_ORDER)
+                .createdDatetime(LocalDateTime.now())
+                .build();
         DynamicListElement selectedCourt = DynamicListElement.builder()
-            .code("00002").label("court 2 - 2 address - Y02 7RB").build();
+                .code("00002").label("court 2 - 2 address - Y02 7RB").build();
         CaseData caseData = CaseData.builder()
-            .ccdState(CaseState.CASE_PROGRESSION)
-            .responseClaimTrack(AllocatedTrack.SMALL_CLAIM.name())
-            .totalClaimAmount(BigDecimal.valueOf(999))
-            .caseManagementLocation(CaseLocationCivil.builder().baseLocation(selectedCourt.getCode()).build())
-            .systemGeneratedCaseDocuments(List.of(Element.<CaseDocument>builder()
-                                                      .value(sdoDocument).build()))
-            .build();
-            checkStatus(caseData, eventHistory,
-                        DashboardClaimStatus.SDO_ORDER_LEGAL_ADVISER_CREATED,
-                        DashboardClaimStatus.SDO_ORDER_LEGAL_ADVISER_CREATED
-            );
+                .ccdState(CaseState.CASE_PROGRESSION)
+                .responseClaimTrack(AllocatedTrack.SMALL_CLAIM.name())
+                .totalClaimAmount(BigDecimal.valueOf(999))
+                .caseManagementLocation(CaseLocationCivil.builder().baseLocation(selectedCourt.getCode()).build())
+                .systemGeneratedCaseDocuments(List.of(Element.<CaseDocument>builder()
+                        .value(sdoDocument).build()))
+                .build();
+        checkStatus(caseData, eventHistory,
+                DashboardClaimStatus.SDO_ORDER_LEGAL_ADVISER_CREATED,
+                DashboardClaimStatus.SDO_ORDER_LEGAL_ADVISER_CREATED);
 
         return caseData;
     }
