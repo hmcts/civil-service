2362cc7526e6ae6f86cf2c6321433d7b2ef95a14
bharatkumarpatel1 <Bharatkumar.Patel1@HMCTS.NET>
2025-09-23 16:39:35 +0100

    DTSCCI-1914 - updated query to fix notification

--- src/main/resources/db/migration/V2025_09_22_1702__DTSCCI-1914_defendant_response_part_admit_claimant.sql
+++ src/main/resources/db/migration/V2025_09_22_1702__DTSCCI-1914_defendant_response_part_admit_claimant.sql
@@ -1,18 +1,13 @@
 /**
  * Update scenario
  */
-update
-	dbs.scenario
-set
-	notifications_to_delete = '{"Notice.AAA6.DefResponse.ResponseTimeElapsed.Claimant",
-								"Notice.AAA6.DefResponse.FullOrPartAdmit.PayBySetDate.Claimant",
-								"Notice.AAA6.DefResponse.FullOrPartAdmit.PayByInstalments.Claimant",
-								"Notice.AAA6.DefResponse.OrgOrLtdCompany.FullOrPartAdmit.PayBySetDate.Claimant",
-								"Notice.AAA6.DefResponse.OrgOrLtdCompany.FullOrPartAdmit.PayByInstallments.Claimant",
-								"Notice.AAA6.DefResponse.PartAdmit.AlreadyPaid.Claimant",
-								"Notice.AAA6.DefResponse.FullDefence.AlreadyPaid.Claimant",
-								"Notice.AAA6.DefResponse.FullDefence.FullDispute.RefusedMediation.Claimant",
-								"Notice.AAA6.DefResponse.FullDefence.FullDispute.SuggestedMediation.Claimant",
-								"Notice.AAA6.DefResponse.FullDefence.FullDispute.Multi.Int.Fast.Claimant"}'
-where
-	name = 'Scenario.AAA6.DefResponse.PartAdmit.PayImmediately.Claimant';
+UPDATE dbs.scenario
+SET
+  notifications_to_delete = '{ "Notice.AAA6.ClaimIssue.Response.Await",
+                               "Notice.AAA6.DefResponse.MoreTimeRequested.Claimant",
+                               "Notice.AAA6.DefResponse.MoreTimeRequested.Claimant",
+                               "Notice.AAA6.ClaimIssue.HWF.PhonePayment",
+                               "Notice.AAA6.DefResponse.BilingualFlagSet.Claimant",
+                               "Notice.AAA6.DefResponse.ResponseTimeElapsed.Claimant" }'
+WHERE
+  name = 'Scenario.AAA6.DefResponse.PartAdmit.PayImmediately.Claimant';
