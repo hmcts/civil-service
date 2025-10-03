87b8d523d883d1b7bba1ff079be1300d6503ea5f
mounikaammineni <Ayra@23082019>
2025-10-03 16:27:43 +0100

    update

--- src/main/java/uk/gov/hmcts/reform/civil/bulkupdate/csv/DashboardScenarioCaseReference.java
+++ src/main/java/uk/gov/hmcts/reform/civil/bulkupdate/csv/DashboardScenarioCaseReference.java
@@ -12,6 +12,7 @@ import lombok.experimental.SuperBuilder;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @SuperBuilder
 @Data
+@EqualsAndHashCode(callSuper = true)
 @JsonPropertyOrder(value = {"caseReference"})
 @AllArgsConstructor
 @NoArgsConstructor
