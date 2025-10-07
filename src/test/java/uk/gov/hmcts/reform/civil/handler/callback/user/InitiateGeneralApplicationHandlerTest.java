78dda64cf9ad9c0de8fd3dbcc25595d7e22da2b5
AbuSalam22 <136581242+AbuSalam22@users.noreply.github.com>
2025-10-07 15:10:07 +0100

    DTSCCI-2712 fix

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/InitiateGeneralApplicationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/InitiateGeneralApplicationHandlerTest.java
@@ -13,6 +13,7 @@ import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.reform.civil.bankholidays.WorkingDayIndicator;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.enums.BusinessProcessStatus;
 import uk.gov.hmcts.reform.civil.enums.CaseCategory;
 import uk.gov.hmcts.reform.civil.enums.CaseRole;
 import uk.gov.hmcts.reform.civil.enums.CaseState;
@@ -20,6 +21,7 @@ import uk.gov.hmcts.reform.civil.enums.dq.GeneralApplicationTypes;
 import uk.gov.hmcts.reform.civil.enums.dq.GeneralApplicationTypesLR;
 import uk.gov.hmcts.reform.civil.enums.dq.Language;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
+import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.Fee;
 import uk.gov.hmcts.reform.civil.model.IdamUserDetails;
@@ -31,9 +33,11 @@ import uk.gov.hmcts.reform.civil.model.genapplication.GAApplicationType;
 import uk.gov.hmcts.reform.civil.model.genapplication.GAApplicationTypeLR;
 import uk.gov.hmcts.reform.civil.model.genapplication.GAHearingDateGAspec;
 import uk.gov.hmcts.reform.civil.model.genapplication.GAHearingDetails;
+import uk.gov.hmcts.reform.civil.model.genapplication.GAInformOtherParty;
 import uk.gov.hmcts.reform.civil.model.genapplication.GAPbaDetails;
 import uk.gov.hmcts.reform.civil.model.genapplication.GARespondentOrderAgreement;
 import uk.gov.hmcts.reform.civil.model.genapplication.GAUnavailabilityDates;
+import uk.gov.hmcts.reform.civil.model.genapplication.GAUrgencyRequirement;
 import uk.gov.hmcts.reform.civil.model.genapplication.GeneralApplication;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.sampledata.GeneralApplicationDetailsBuilder;
@@ -51,8 +55,11 @@ import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
+import static java.lang.String.format;
+import static java.time.LocalDate.EPOCH;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -80,6 +87,7 @@ import static uk.gov.hmcts.reform.civil.enums.dq.GeneralApplicationTypes.SUMMARY
 import static uk.gov.hmcts.reform.civil.enums.dq.GeneralApplicationTypes.VARY_ORDER;
 import static uk.gov.hmcts.reform.civil.enums.dq.GeneralApplicationTypes.VARY_PAYMENT_TERMS_OF_JUDGMENT;
 import static uk.gov.hmcts.reform.civil.handler.callback.user.InitiateGeneralApplicationHandler.NOT_ALLOWED_SETTLE_DISCONTINUE;
+import static uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder.CUSTOMER_REFERENCE;
 import static uk.gov.hmcts.reform.civil.service.InitiateGeneralApplicationServiceConstants.INVALID_SETTLE_BY_CONSENT;
 import static uk.gov.hmcts.reform.civil.service.InitiateGeneralApplicationServiceConstants.INVALID_UNAVAILABILITY_RANGE;
 import static uk.gov.hmcts.reform.civil.service.validation.GeneralApplicationValidator.INVALID_TRIAL_DATE_RANGE;
@@ -134,6 +142,88 @@ class InitiateGeneralApplicationHandlerTest extends BaseCallbackHandlerTest {
     private final BigDecimal fee275 = new BigDecimal("27500");
     private static final String FEE_CODE = "test_fee_code";
     private static final String FEE_VERSION = "1";
+
+    private static final String STRING_CONSTANT = "this is a string";
+    private static final LocalDate APP_DATE_EPOCH = EPOCH;
+    private static final String CONFIRMATION_BODY_FREE = "<br/> <p> The court will make a decision"
+            + " on this application."
+            + "<br/> <p>  The other party's legal representative has been notified that you have"
+            + " submitted this application";
+    private static final Fee FEE275 = Fee.builder().calculatedAmountInPence(
+            BigDecimal.valueOf(27500)).code("FEE0444").version("1").build();
+
+    private CaseData getEmptyTestCase(CaseData caseData) {
+        return caseData.toBuilder()
+                .build();
+    }
+
+    private CaseData getReadyTestCaseData(CaseData caseData, boolean multipleGenAppTypes) {
+        GAInformOtherParty withOrWithoutNotice = GAInformOtherParty.builder()
+                .isWithNotice(YES)
+                .reasonsForWithoutNotice(STRING_CONSTANT)
+                .build();
+        GARespondentOrderAgreement withOrWithoutConsent = GARespondentOrderAgreement.builder()
+                .hasAgreed(NO).build();
+
+        return getReadyTestCaseData(caseData, multipleGenAppTypes, withOrWithoutConsent, withOrWithoutNotice);
+    }
+
+    private CaseData getReadyTestCaseData(CaseData caseData,
+                                          boolean multipleGenAppTypes,
+                                          GARespondentOrderAgreement hasAgreed,
+                                          GAInformOtherParty withOrWithoutNotice) {
+        GeneralApplication.GeneralApplicationBuilder builder = GeneralApplication.builder();
+        if (multipleGenAppTypes) {
+            builder.generalAppType(GAApplicationType.builder()
+                    .types(Arrays.asList(EXTEND_TIME, SUMMARY_JUDGEMENT))
+                    .build());
+        } else {
+            builder.generalAppType(GAApplicationType.builder()
+                    .types(singletonList(EXTEND_TIME))
+                    .build());
+        }
+        GeneralApplication application = builder
+
+                .generalAppInformOtherParty(withOrWithoutNotice)
+                .generalAppRespondentAgreement(hasAgreed)
+                .generalAppPBADetails(
+                        GAPbaDetails.builder()
+                                .fee(FEE275)
+                                .serviceReqReference(CUSTOMER_REFERENCE).build())
+                .generalAppUrgencyRequirement(GAUrgencyRequirement.builder()
+                        .generalAppUrgency(YES)
+                        .reasonsForUrgency(STRING_CONSTANT)
+                        .urgentAppConsiderationDate(APP_DATE_EPOCH)
+                        .build())
+                .isMultiParty(NO)
+                .businessProcess(BusinessProcess.builder()
+                        .status(BusinessProcessStatus.READY)
+                        .build())
+                .build();
+        return getEmptyTestCase(caseData)
+                .toBuilder()
+                .generalApplications(wrapElements(application))
+                .build();
+    }
+
+    private String confirmationBodyBasedOnToggle(Boolean isGaForLipsEnabled) {
+        StringBuilder bodyConfirmation = new StringBuilder();
+        bodyConfirmation.append("<br/>");
+        bodyConfirmation.append("<p class=\"govuk-body govuk-!-font-weight-bold\"> Your application fee of £%s"
+                + " is now due for payment. Your application will not be processed further"
+                + " until this fee is paid.</p>");
+        bodyConfirmation.append("%n%n To pay this fee, click the link below, or else open your application from the"
+                + " Applications tab of this case listing and then click on the service request tab.");
+
+        if (isGaForLipsEnabled) {
+            bodyConfirmation.append("%n%n If necessary, all documents relating to this application, "
+                    + "including any response from the court, will be translated."
+                    + " You will be notified when these are available.");
+        }
+
+        bodyConfirmation.append("%n%n <a href=\"%s\" target=\"_blank\">Pay your application fee </a> %n");
+        return bodyConfirmation.toString();
+    }
 
     @BeforeEach
     void setup() {
@@ -1575,14 +1665,83 @@ class InitiateGeneralApplicationHandlerTest extends BaseCallbackHandlerTest {
     class SubmittedCallback {
 
         @Test
-        void shouldReturnEmptyResponse_whenInvoked() {
-            CaseData caseData = CaseDataBuilder.builder().atStateClaimIssued()
-                .build();
+        void handleEventsReturnsTheExpectedCallbackEvent() {
+            assertThat(handler.handledEvents()).contains(INITIATE_GENERAL_APPLICATION);
+        }
+
+        @Test
+        void shouldReturnExpectedSubmittedCallbackResponse_whenRespondentsDoesNotHaveRepresentation() {
+            CaseData caseData = getReadyTestCaseData(CaseDataBuilder.builder().ccdCaseReference(CASE_ID).build(), true);
+            CallbackParams params = callbackParamsOf(caseData, SUBMITTED);
+            GeneralApplication genapp = caseData.getGeneralApplications().get(0).getValue();
+            when(feesService.isFreeGa(any())).thenReturn(false);
+            String body = format(
+                    confirmationBodyBasedOnToggle(false),
+                    genapp.getGeneralAppPBADetails().getFee().toPounds(),
+                    format("/cases/case-details/%s#Applications", CASE_ID));
+
+            var response = (SubmittedCallbackResponse) handler.handle(params);
+            assertThat(response).isNotNull();
+            assertThat(response).usingRecursiveComparison().isEqualTo(
+                    SubmittedCallbackResponse.builder()
+                            .confirmationHeader(
+                                    "# You have submitted an application")
+                            .confirmationBody(body)
+                            .build());
+            assertThat(response).isNotNull();
+            assertThat(response.getConfirmationBody()).isEqualTo(body);
+        }
+
+        @Test
+        void shouldReturnExpectedSubmittedCallbackResponse_whengaLips_is_enable() {
+            CaseData caseData = getReadyTestCaseData(CaseDataBuilder.builder().ccdCaseReference(CASE_ID).build(), true);
+            CallbackParams params = callbackParamsOf(caseData, SUBMITTED);
+            GeneralApplication genapp = caseData.getGeneralApplications().get(0).getValue();
+            when(feesService.isFreeGa(any())).thenReturn(false);
+            when(featureToggleService.isGaForLipsEnabled()).thenReturn(true);
+            String body = format(
+                    confirmationBodyBasedOnToggle(true),
+                    genapp.getGeneralAppPBADetails().getFee().toPounds(),
+                    format("/cases/case-details/%s#Applications", CASE_ID));
+
+            var response = (SubmittedCallbackResponse) handler.handle(params);
+            assertThat(response).isNotNull();
+            assertThat(response).usingRecursiveComparison().isEqualTo(
+                    SubmittedCallbackResponse.builder()
+                            .confirmationHeader(
+                                    "# You have submitted an application")
+                            .confirmationBody(body)
+                            .build());
+            assertThat(response).isNotNull();
+            assertThat(response.getConfirmationBody()).isEqualTo(body);
+        }
+
+        @Test
+        void shouldReturnFreeGAConfirmationBodyBody_whenFreeGA() {
+            CaseData caseData = getReadyTestCaseData(CaseDataBuilder.builder().ccdCaseReference(CASE_ID).build(), true);
+            CallbackParams params = callbackParamsOf(caseData, SUBMITTED);
+            when(feesService.isFreeGa(any())).thenReturn(true);
+
+            var response = (SubmittedCallbackResponse) handler.handle(params);
+            assertThat(response).isNotNull();
+            assertThat(response).usingRecursiveComparison().isEqualTo(
+                    SubmittedCallbackResponse.builder()
+                            .confirmationHeader(
+                                    "# You have submitted an application")
+                            .confirmationBody(CONFIRMATION_BODY_FREE)
+                            .build());
+            assertThat(response).isNotNull();
+            assertThat(response.getConfirmationBody()).isEqualTo(CONFIRMATION_BODY_FREE);
+        }
+
+        @Test
+        void shouldNotReturnBuildConfirmationIfGeneralApplicationIsEmpty() {
+            CaseData caseData = getEmptyTestCase(CaseDataBuilder.builder().build());
             CallbackParams params = callbackParamsOf(caseData, SUBMITTED);
 
-            SubmittedCallbackResponse response = (SubmittedCallbackResponse) handler.handle(params);
-
-            assertThat(response).isEqualTo(SubmittedCallbackResponse.builder().confirmationHeader("# You have submitted an application").build());
+            var response = (SubmittedCallbackResponse) handler.handle(params);
+            assertThat(response).isNotNull();
+            assertThat(response.getConfirmationBody()).isNull();
         }
     }
 }
