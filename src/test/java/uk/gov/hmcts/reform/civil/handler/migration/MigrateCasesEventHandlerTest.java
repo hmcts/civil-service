a552a45e231660c4abf9d48e21f3492e49b173ec
mounikaammineni <Ayra@23082019>
2025-10-07 10:28:12 +0100

    update

--- src/test/java/uk/gov/hmcts/reform/civil/handler/migration/MigrateCasesEventHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/migration/MigrateCasesEventHandlerTest.java
@@ -23,12 +23,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.*;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class MigrateCasesEventHandlerTest {
