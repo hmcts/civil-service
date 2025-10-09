7c4049157e39572c147e49c68a6317e8336bc6b0
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2025-10-09 16:26:11 +0100

    DTSCCI-2747 Generic State update via migration task (#7064)
    * update
    
    * update
    
    * update
    
    * udpate
    
    * fix inident retry
    
    * fix inident retry
    
    * fix inident retry
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update
    
    * Update
    
    * Update
    
    * Change name
    
    ---------
    
    Co-authored-by: mounikaammineni <Ayra@23082019>

---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/migration/UpdateCaseStateCallbackHandlerTest.java
@@ -0,0 +1,77 @@
+package uk.gov.hmcts.reform.civil.handler.callback.migration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CallbackType;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class UpdateCaseStateCallbackHandlerTest {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private UpdateCaseStateCallbackHandler handler;
+
+    @BeforeEach
+    void setUp() {
+        handler = new UpdateCaseStateCallbackHandler(objectMapper);
+    }
+
+    @Test
+    void shouldReturnResponseWithState_whenNextStateIsPresent() {
+        // Given
+        String nextState = "AWAITING_APPLICANT_INTENTION";
+        CaseData caseData = CaseData.builder()
+            .nextState(nextState)
+            .build();
+
+        CallbackParams params = mock(CallbackParams.class);
+        when(params.getCaseData()).thenReturn(caseData);
+        when(params.getType()).thenReturn(CallbackType.ABOUT_TO_SUBMIT);
+        when(params.getCaseData()).thenReturn(caseData);
+
+        // When
+        AboutToStartOrSubmitCallbackResponse response =
+            (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+
+        // Then
+        assertThat(response.getState()).isEqualTo(nextState);
+        assertThat(response.getData()).doesNotContainEntry("nextState", nextState);
+    }
+
+    @Test
+    void shouldReturnResponseWithoutState_whenNextStateIsNull() {
+        // Given
+        CaseData caseData = CaseData.builder()
+            .nextState(null)
+            .build();
+
+        CallbackParams params = mock(CallbackParams.class);
+        when(params.getCaseData()).thenReturn(caseData);
+        when(params.getType()).thenReturn(CallbackType.ABOUT_TO_SUBMIT);
+        when(params.getCaseData()).thenReturn(caseData);
+
+        // When
+        AboutToStartOrSubmitCallbackResponse response =
+            (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+
+        // Then
+        assertThat(response.getState()).isNull();
+        assertThat(response.getData().get("nextState")).isNull();
+    }
+
+    @Test
+    void shouldHandleOnlyUpdateCaseDataEvent() {
+        // Then
+        assertThat(handler.handledEvents())
+            .hasSize(1)
+            .first()
+            .extracting(Enum::name)
+            .isEqualTo("UPDATE_CASE_DATA");
+    }
+}
