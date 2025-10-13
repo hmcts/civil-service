f6cbc682da467f0afb818bc4ec76a18a10eb2cef
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2025-10-13 13:29:47 +0100

    Revert "DTSCCI-938 Revert removing presisting party flags issue workaround (#7052)"
    This reverts commit 4455e97d

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/task/respondtodefencespeccallbackhandlertask/AboutToSubmitRespondToDefenceTask.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/task/respondtodefencespeccallbackhandlertask/AboutToSubmitRespondToDefenceTask.java
@@ -58,7 +58,6 @@ import static uk.gov.hmcts.reform.civil.service.PaymentDateService.DATE_FORMATTE
 import static uk.gov.hmcts.reform.civil.utils.ElementUtils.wrapElements;
 import static uk.gov.hmcts.reform.civil.utils.ExpertUtils.addEventAndDateAddedToApplicantExperts;
 import static uk.gov.hmcts.reform.civil.utils.PartyUtils.populateDQPartyIds;
-import static uk.gov.hmcts.reform.civil.utils.PersistDataUtils.persistFlagsForParties;
 import static uk.gov.hmcts.reform.civil.utils.PersistDataUtils.persistPartyAddress;
 import static uk.gov.hmcts.reform.civil.utils.WitnessUtils.addEventAndDateAddedToApplicantWitnesses;
 
@@ -90,7 +89,6 @@ public class AboutToSubmitRespondToDefenceTask implements CaseTask {
         CaseData caseData = persistPartyAddress(oldCaseData, callbackParams.getCaseData());
         CaseData.CaseDataBuilder<?, ?> builder = caseData.toBuilder().applicant1ResponseDate(time.now());
 
-        persistFlagsForParties(oldCaseData, caseData, builder);
         setResponseDocumentNull(builder);
         updateCaselocationDetails(callbackParams, caseData, builder);
         updateApplicant1DQ(callbackParams, caseData, builder);
