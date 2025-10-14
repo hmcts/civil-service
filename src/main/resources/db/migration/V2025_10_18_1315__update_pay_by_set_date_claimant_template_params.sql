70424a8d52f1598094bba1c4343f01d84b7743bc
Ruban <ruban72@hotmail.com>
2025-10-14 10:14:52 +0100

    DTSCCI-2385: Fix integration tests

--- src/main/resources/db/migration/V2025_10_18_1315__update_pay_by_set_date_claimant_template_params.sql
+++
@@ -1,3 +0,0 @@
-UPDATE dbs.scenario
-SET notifications_to_create = '{"Notice.AAA6.DefResponse.FullOrPartAdmit.PayBySetDate.Claimant" : ["respondent1PartyName", "defendantAdmittedAmount", "defendantAmountIncludesTextEn", "defendantAmountIncludesTextCy", "respondent1AdmittedAmountPaymentDeadlineEn", "respondent1AdmittedAmountPaymentDeadlineCy"]}'
-WHERE name = 'Scenario.AAA6.DefResponse.FullOrPartAdmit.PayBySetDate.Claimant';
