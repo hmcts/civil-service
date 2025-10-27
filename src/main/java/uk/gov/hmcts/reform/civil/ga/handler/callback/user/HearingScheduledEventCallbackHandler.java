50f2fffb48aae2d63016f7b649b0146814896890
Shaun Francis <shaun.francis@hmcts.net>
2025-10-27 10:45:40 +0000

    Add general application callback handlers nad tests

---
+++ src/main/java/uk/gov/hmcts/reform/civil/ga/handler/callback/user/HearingScheduledEventCallbackHandler.java
@@ -0,0 +1,162 @@
+package uk.gov.hmcts.reform.civil.ga.handler.callback.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.Callback;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.enums.CaseState;
+import uk.gov.hmcts.reform.civil.ga.callback.GeneralApplicationCallbackHandler;
+import uk.gov.hmcts.reform.civil.ga.model.GeneralApplicationCaseData;
+import uk.gov.hmcts.reform.civil.ga.model.genapplication.GAHearingNoticeDetail;
+import uk.gov.hmcts.reform.civil.ga.service.GeneralAppLocationRefDataService;
+import uk.gov.hmcts.reform.civil.model.BusinessProcess;
+import uk.gov.hmcts.reform.civil.model.common.DynamicList;
+import uk.gov.hmcts.reform.civil.model.common.DynamicListElement;
+import uk.gov.hmcts.reform.civil.referencedata.model.LocationRefData;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+import static java.lang.String.format;
+import static java.util.Objects.nonNull;
+import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.MID;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.HEARING_SCHEDULED_GA;
+import static uk.gov.hmcts.reform.civil.model.common.DynamicList.fromList;
+
+@Service
+@RequiredArgsConstructor
+public class HearingScheduledEventCallbackHandler extends CallbackHandler implements GeneralApplicationCallbackHandler {
+
+    public static final String HEARING_CONFIRMATION_HEADER = """
+        # Hearing notice created
+        ##### You may need to complete other tasks for the
+        ##### hearing for example, book an interpreter.<br/>%s""";
+    private static final List<CaseEvent> EVENTS = Collections.singletonList(HEARING_SCHEDULED_GA);
+    private final GeneralAppLocationRefDataService locationRefDataService;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected Map<String, Callback> callbacks() {
+        return Map.of(
+            callbackKey(ABOUT_TO_START), this::locationList,
+            callbackKey(MID, "hearing-check-date"), this::checkFutureDate,
+            callbackKey(ABOUT_TO_SUBMIT), this::validateHearingScheduledProcess,
+            callbackKey(SUBMITTED), this::buildConfirmation
+        );
+    }
+
+    private CallbackResponse locationList(CallbackParams callbackParams) {
+        GeneralApplicationCaseData caseData = (GeneralApplicationCaseData) callbackParams.getBaseCaseData();
+        GeneralApplicationCaseData.GeneralApplicationCaseDataBuilder<?, ?> caseDataBuilder = caseData.toBuilder();
+
+        String authToken = callbackParams.getParams().get(BEARER_TOKEN).toString();
+        DynamicList dynamicLocationList = getLocationsFromList(locationRefDataService.getCourtLocations(authToken));
+        if (!caseData.getCcdState().equals(CaseState.ORDER_MADE)
+            && Objects.nonNull(caseData.getJudicialListForHearing())
+            && Objects.nonNull(caseData.getJudicialListForHearing().getHearingPreferredLocation())
+            && Objects.nonNull(caseData.getJudicialListForHearing().getHearingPreferredLocation().getValue())
+        ) {
+            String preLabel = caseData.getJudicialListForHearing().getHearingPreferredLocation().getValue().getLabel();
+            Optional<DynamicListElement> first = dynamicLocationList.getListItems().stream()
+                .filter(l -> l.getLabel().equals(preLabel)).findFirst();
+            first.ifPresent(dynamicLocationList::setValue);
+        }
+        caseDataBuilder.gaHearingNoticeDetail(GAHearingNoticeDetail
+                                                  .builder().hearingLocation(dynamicLocationList).build());
+
+        if (caseData.getCcdState().equals(CaseState.ORDER_MADE)) {
+            caseDataBuilder.gaHearingNoticeApplication(null)
+                .gaHearingNoticeInformation(null);
+        }
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDataBuilder.build().toMap(objectMapper))
+            .build();
+    }
+
+    private DynamicList getLocationsFromList(final List<LocationRefData> locations) {
+        return fromList(locations.stream().map(location -> new StringBuilder().append(location.getSiteName())
+                .append(" - ").append(location.getCourtAddress())
+                .append(" - ").append(location.getPostcode()).toString())
+                            .toList());
+    }
+
+    private CallbackResponse checkFutureDate(CallbackParams callbackParams) {
+        List<String> errors = new ArrayList<>();
+        LocalDateTime hearingDateTime = null;
+        GeneralApplicationCaseData caseData = (GeneralApplicationCaseData) callbackParams.getBaseCaseData();
+        GeneralApplicationCaseData.GeneralApplicationCaseDataBuilder<?, ?> caseDataBuilder = caseData.toBuilder();
+        LocalDate date = caseData.getGaHearingNoticeDetail().getHearingDate();
+        String hourMinute = caseData.getGaHearingNoticeDetail().getHearingTimeHourMinute();
+        if (hourMinute != null) {
+            int hours = Integer.parseInt(hourMinute.substring(0, 2));
+            int minutes = Integer.parseInt(hourMinute.substring(2, 4));
+            LocalTime time = LocalTime.of(hours, minutes, 0);
+            hearingDateTime = LocalDateTime.of(date, time);
+        } else {
+            errors.add("Time is required");
+        }
+
+        errors = (Objects.isNull(hearingDateTime)) ? null :
+            isFutureDate(hearingDateTime);
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDataBuilder.build().toMap(objectMapper))
+            .errors(errors)
+            .build();
+    }
+
+    private List<String> isFutureDate(LocalDateTime hearingDateTime) {
+        List<String> errors = new ArrayList<>();
+        if (!checkFutureDateValidation(hearingDateTime)) {
+            errors.add("Hearing date must be in the future");
+        }
+        return errors;
+    }
+
+    private boolean checkFutureDateValidation(LocalDateTime localDateTime) {
+        return localDateTime != null && localDateTime.isAfter(LocalDateTime.now());
+    }
+
+    private CallbackResponse validateHearingScheduledProcess(CallbackParams callbackParams) {
+        GeneralApplicationCaseData caseData = (GeneralApplicationCaseData) callbackParams.getBaseCaseData();
+        if (nonNull(caseData.getGaHearingNoticeDetail().getHearingLocation())) {
+            caseData.getGaHearingNoticeDetail().getHearingLocation().setListItems(null);
+        }
+        GeneralApplicationCaseData.GeneralApplicationCaseDataBuilder<?, ?> dataBuilder = caseData.toBuilder();
+        dataBuilder.businessProcess(BusinessProcess.ready(HEARING_SCHEDULED_GA)).build();
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(dataBuilder.build().toMap(objectMapper))
+            .build();
+    }
+
+    private SubmittedCallbackResponse buildConfirmation(CallbackParams callbackParams) {
+        String confirmationHeader = format(HEARING_CONFIRMATION_HEADER, "<br/>");
+        String confirmationBody = "<br/> <br/>";
+        return SubmittedCallbackResponse.builder()
+            .confirmationHeader(confirmationHeader)
+            .confirmationBody(confirmationBody)
+            .build();
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/civil/ga/handler/callback/user/GeneralApplicationAfterPaymentCallbackHandlerTest.java
@@ -0,0 +1,169 @@
+package uk.gov.hmcts.reform.civil.ga.handler.callback.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.enums.PaymentStatus;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.enums.dq.GeneralApplicationTypes;
+import uk.gov.hmcts.reform.civil.ga.handler.GeneralApplicationBaseCallbackHandlerTest;
+import uk.gov.hmcts.reform.civil.ga.model.GeneralApplicationCaseData;
+import uk.gov.hmcts.reform.civil.model.Fee;
+import uk.gov.hmcts.reform.civil.ga.model.GeneralAppParentCaseLink;
+import uk.gov.hmcts.reform.civil.model.PaymentDetails;
+import uk.gov.hmcts.reform.civil.model.genapplication.GAApplicationType;
+import uk.gov.hmcts.reform.civil.model.genapplication.GAHearingDetails;
+import uk.gov.hmcts.reform.civil.model.genapplication.GAInformOtherParty;
+import uk.gov.hmcts.reform.civil.model.genapplication.GAPbaDetails;
+import uk.gov.hmcts.reform.civil.model.genapplication.GARespondentOrderAgreement;
+import uk.gov.hmcts.reform.civil.model.genapplication.GASolicitorDetailsGAspec;
+import uk.gov.hmcts.reform.civil.model.genapplication.GAStatementOfTruth;
+import uk.gov.hmcts.reform.civil.model.genapplication.GAUrgencyRequirement;
+import uk.gov.hmcts.reform.civil.model.genapplication.GeneralApplication;
+import uk.gov.hmcts.reform.civil.ga.service.GaForLipService;
+import uk.gov.hmcts.reform.civil.sampledata.GeneralApplicationCaseDataBuilder;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.INITIATE_COSC_APPLICATION_AFTER_PAYMENT;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.INITIATE_GENERAL_APPLICATION_AFTER_PAYMENT;
+import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
+import static uk.gov.hmcts.reform.civil.enums.YesOrNo.YES;
+import static uk.gov.hmcts.reform.civil.enums.dq.GeneralApplicationTypes.RELIEF_FROM_SANCTIONS;
+import static uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder.CUSTOMER_REFERENCE;
+import static uk.gov.hmcts.reform.civil.utils.ElementUtils.wrapElements;
+
+@SpringBootTest(classes = {
+    GeneralApplicationAfterPaymentCallbackHandler.class,
+    JacksonAutoConfiguration.class,
+})
+public class GeneralApplicationAfterPaymentCallbackHandlerTest extends GeneralApplicationBaseCallbackHandlerTest {
+
+    @Autowired
+    private GeneralApplicationAfterPaymentCallbackHandler handler;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private GaForLipService gaForLipService;
+
+    private static final String STRING_CONSTANT = "STRING_CONSTANT";
+    private static final Long CHILD_CCD_REF = 1646003133062762L;
+    private static final Long PARENT_CCD_REF = 1645779506193000L;
+
+    @Test
+    void shouldTriggerCoscBusinessProcess() {
+        GeneralApplicationCaseData caseData = getSampleGeneralApplicationCaseData(NO, YES);
+        caseData = addGeneralAppType(caseData, GeneralApplicationTypes.CONFIRM_CCJ_DEBT_PAID);
+        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+        when(gaForLipService.isLipApp(any(GeneralApplicationCaseData.class))).thenReturn(false);
+        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+        GeneralApplicationCaseData responseCaseData = objectMapper.convertValue(response.getData(), GeneralApplicationCaseData.class);
+        assertThat(responseCaseData.getBusinessProcess().getCamundaEvent()).isEqualTo(INITIATE_COSC_APPLICATION_AFTER_PAYMENT.name());
+    }
+
+    @Test
+    void shouldTriggerTheEventAndAboutToSubmit() {
+        GeneralApplicationCaseData caseData = getSampleGeneralApplicationCaseData(NO, YES);
+        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+        when(gaForLipService.isLipApp(any(GeneralApplicationCaseData.class))).thenReturn(false);
+        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+        GeneralApplicationCaseData responseCaseData = objectMapper.convertValue(response.getData(), GeneralApplicationCaseData.class);
+        assertThat(responseCaseData.getBusinessProcess().getCamundaEvent()).isEqualTo(INITIATE_GENERAL_APPLICATION_AFTER_PAYMENT.name());
+    }
+
+    @Test
+    void shouldTriggerTheEventAndAboutToSubmitWithoutBusinessProcess() {
+        GeneralApplicationCaseData caseData = getSampleGeneralApplicationCaseData(NO, YES);
+        caseData = addPaymentStatusToGAPbaDetails(caseData, PaymentStatus.FAILED);
+
+        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+        when(gaForLipService.isLipApp(any(GeneralApplicationCaseData.class))).thenReturn(true);
+        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+        GeneralApplicationCaseData responseCaseData = objectMapper.convertValue(response.getData(), GeneralApplicationCaseData.class);
+        assertThat(responseCaseData.getBusinessProcess()).isNull();
+    }
+
+    @Test
+    void shouldTriggerTheEventAndAboutToSubmitWithBusinessProcess() {
+        GeneralApplicationCaseData caseData = getSampleGeneralApplicationCaseData(NO, YES);
+        caseData = addPaymentStatusToGAPbaDetails(caseData, PaymentStatus.SUCCESS);
+
+        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+        when(gaForLipService.isLipApp(any(GeneralApplicationCaseData.class))).thenReturn(true);
+        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+        GeneralApplicationCaseData responseCaseData = objectMapper.convertValue(response.getData(), GeneralApplicationCaseData.class);
+        assertThat(responseCaseData.getBusinessProcess().getCamundaEvent()).isEqualTo(INITIATE_GENERAL_APPLICATION_AFTER_PAYMENT.name());
+    }
+
+    @Test
+    void handleEventsReturnsTheExpectedCallbackEvent() {
+        assertThat(handler.handledEvents()).contains(
+            INITIATE_GENERAL_APPLICATION_AFTER_PAYMENT);
+    }
+
+    private GeneralApplicationCaseData getSampleGeneralApplicationCaseData(YesOrNo isConsented, YesOrNo isTobeNotified) {
+        return GeneralApplicationCaseDataBuilder.builder().buildCaseDateBaseOnGeneralApplication(
+                getGeneralApplicationBeforePayment(isConsented, isTobeNotified))
+            .toBuilder().ccdCaseReference(CHILD_CCD_REF).build();
+    }
+
+    private GeneralApplication getGeneralApplicationBeforePayment(YesOrNo isConsented, YesOrNo isTobeNotified) {
+        return GeneralApplication.builder()
+            .generalAppType(GAApplicationType.builder().types(List.of(RELIEF_FROM_SANCTIONS)).build())
+            .generalAppRespondentAgreement(GARespondentOrderAgreement.builder().hasAgreed(isConsented).build())
+            .generalAppInformOtherParty(GAInformOtherParty.builder().isWithNotice(isTobeNotified).build())
+            .generalAppPBADetails(
+                GAPbaDetails.builder()
+                    .fee(
+                        Fee.builder()
+                            .code("FE203")
+                            .calculatedAmountInPence(BigDecimal.valueOf(27500))
+                            .version("1")
+                            .build())
+                    .serviceReqReference(CUSTOMER_REFERENCE).build())
+            .generalAppDetailsOfOrder(STRING_CONSTANT)
+            .generalAppReasonsOfOrder(STRING_CONSTANT)
+            .generalAppUrgencyRequirement(GAUrgencyRequirement.builder().generalAppUrgency(NO).build())
+            .generalAppStatementOfTruth(GAStatementOfTruth.builder().build())
+            .generalAppHearingDetails(GAHearingDetails.builder().build())
+            .generalAppRespondentSolicitors(wrapElements(GASolicitorDetailsGAspec.builder()
+                                                             .email("abc@gmail.com").build()))
+            .isMultiParty(NO)
+            .parentClaimantIsApplicant(YES)
+            .generalAppParentCaseLink(GeneralAppParentCaseLink.builder()
+                                          .caseReference(PARENT_CCD_REF.toString()).build())
+            .build();
+    }
+
+    private GeneralApplicationCaseData addPaymentStatusToGAPbaDetails(GeneralApplicationCaseData caseData, PaymentStatus status) {
+        GAPbaDetails pbaDetails = caseData.getGeneralAppPBADetails();
+        GAPbaDetails.GAPbaDetailsBuilder pbaDetailsBuilder;
+        pbaDetailsBuilder = pbaDetails == null ? GAPbaDetails.builder() : pbaDetails.toBuilder();
+
+        PaymentDetails paymentDetails = PaymentDetails.builder()
+            .status(status)
+            .build();
+        pbaDetails = pbaDetailsBuilder.paymentDetails(paymentDetails).build();
+        return caseData.toBuilder()
+            .generalAppPBADetails(pbaDetails)
+            .build();
+    }
+
+    private GeneralApplicationCaseData addGeneralAppType(GeneralApplicationCaseData caseData, GeneralApplicationTypes generalApplicationTypes) {
+        return caseData.toBuilder().generalAppType(
+                GAApplicationType.builder().types(List.of(generalApplicationTypes))
+                    .build())
+            .build();
+    }
+}
+
