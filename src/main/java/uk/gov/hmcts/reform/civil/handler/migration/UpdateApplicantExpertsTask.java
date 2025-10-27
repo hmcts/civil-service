6c8b7838451d09f0b859dcb209d914352591b877
mounikaammineni <Ayra@23082019>
2025-10-27 16:16:06 +0000

    Update

--- src/main/java/uk/gov/hmcts/reform/civil/handler/migration/UpdateApplicantExpertsTask.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/migration/UpdateApplicantExpertsTask.java
@@ -7,12 +7,13 @@ import uk.gov.hmcts.reform.civil.model.PartyFlagStructure;
 import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.model.dq.Applicant1DQ;
 import uk.gov.hmcts.reform.civil.model.dq.Applicant2DQ;
+import uk.gov.hmcts.reform.civil.model.dq.Expert;
 import uk.gov.hmcts.reform.civil.model.dq.Experts;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 @Component
 public class UpdateApplicantExpertsTask extends MigrationTask<CaseReference> {
@@ -42,59 +43,63 @@ public class UpdateApplicantExpertsTask extends MigrationTask<CaseReference> {
             throw new IllegalArgumentException("CaseReference fields must not be null");
         }
 
-        // --- Update Applicant Experts ---
+        UnaryOperator<PartyFlagStructure> updatePartyFlag = expert -> expert.toBuilder()
+            .firstName(defaultIfNull(expert.getFirstName()))
+            .lastName(defaultIfNull(expert.getLastName()))
+            .build();
+
+        UnaryOperator<Expert> updateExpert = expert -> expert.toBuilder()
+            .firstName(defaultIfNull(expert.getFirstName()))
+            .lastName(defaultIfNull(expert.getLastName()))
+            .build();
+
+        // Update applicant experts
         List<Element<PartyFlagStructure>> updatedApplicantExperts =
-            updateElements(caseData.getApplicantExperts(), expert -> expert.toBuilder()
-                .firstName(expert.getFirstName() == null ? "TBC" : expert.getFirstName())
-                .lastName(expert.getLastName() == null ? "TBC" : expert.getLastName())
-                .build()
-            );
+            updateElements(caseData.getApplicantExperts(), updatePartyFlag);
 
-        Applicant1DQ applicant1DQ = caseData.getApplicant1DQ();
-        Applicant2DQ applicant2DQ = caseData.getApplicant2DQ();
+        // Update DQ experts separately
+        Applicant1DQ updatedApplicant1DQ = updateApplicant1DQExperts(caseData.getApplicant1DQ(), updateExpert);
+        Applicant2DQ updatedApplicant2DQ = updateApplicant2DQExperts(caseData.getApplicant2DQ(), updateExpert);
 
-        // --- Update Applicant 1 DQ Experts ---
-        if (applicant1DQ != null && applicant1DQ.getApplicant1DQExperts() != null) {
-            Experts updatedExperts = applicant1DQ.getApplicant1DQExperts().toBuilder()
-                .details(updateElements(applicant1DQ.getApplicant1DQExperts().getDetails(),
-                                        expert -> expert.toBuilder()
-                                            .firstName(expert.getFirstName() == null ? "TBC" : expert.getFirstName())
-                                            .lastName(expert.getLastName() == null ? "TBC" : expert.getLastName())
-                                            .build()
-                ))
-                .build();
-            applicant1DQ = applicant1DQ.toBuilder()
-                .applicant1DQExperts(updatedExperts)
-                .build();
-        }
-
-        // --- Update Applicant 2 DQ Experts ---
-        if (applicant2DQ != null && applicant2DQ.getApplicant2DQExperts() != null) {
-            Experts updatedExperts = applicant2DQ.getApplicant2DQExperts().toBuilder()
-                .details(updateElements(applicant2DQ.getApplicant2DQExperts().getDetails(),
-                                        expert -> expert.toBuilder()
-                                            .firstName(expert.getFirstName() == null ? "TBC" : expert.getFirstName())
-                                            .lastName(expert.getLastName() == null ? "TBC" : expert.getLastName())
-                                            .build()
-                ))
-                .build();
-            applicant2DQ = applicant2DQ.toBuilder()
-                .applicant2DQExperts(updatedExperts)
-                .build();
-        }
-
-        // --- Rebuild and return updated CaseData ---
         return caseData.toBuilder()
             .applicantExperts(updatedApplicantExperts)
-            .applicant1DQ(applicant1DQ)
-            .applicant2DQ(applicant2DQ)
+            .applicant1DQ(updatedApplicant1DQ)
+            .applicant2DQ(updatedApplicant2DQ)
             .build();
     }
 
-    /**
-     * Generic helper method to update elements using a provided transformer function.
-     */
-    private <T> List<Element<T>> updateElements(List<Element<T>> elements, Function<T, T> transformer) {
+    /** Update Applicant1DQ experts */
+    private Applicant1DQ updateApplicant1DQExperts(Applicant1DQ dq, UnaryOperator<Expert> transformer) {
+        if (dq == null || dq.getApplicant1DQExperts() == null) {
+            return dq;
+        }
+
+        Experts updatedExperts = dq.getApplicant1DQExperts().toBuilder()
+            .details(updateElements(dq.getApplicant1DQExperts().getDetails(), transformer))
+            .build();
+
+        return dq.toBuilder()
+            .applicant1DQExperts(updatedExperts)
+            .build();
+    }
+
+    /** Update Applicant2DQ experts */
+    private Applicant2DQ updateApplicant2DQExperts(Applicant2DQ dq, UnaryOperator<Expert> transformer) {
+        if (dq == null || dq.getApplicant2DQExperts() == null) {
+            return dq;
+        }
+
+        Experts updatedExperts = dq.getApplicant2DQExperts().toBuilder()
+            .details(updateElements(dq.getApplicant2DQExperts().getDetails(), transformer))
+            .build();
+
+        return dq.toBuilder()
+            .applicant2DQExperts(updatedExperts)
+            .build();
+    }
+
+    /** Update a list of elements using a UnaryOperator */
+    private <T> List<Element<T>> updateElements(List<Element<T>> elements, UnaryOperator<T> transformer) {
         return Optional.ofNullable(elements)
             .orElse(Collections.emptyList())
             .stream()
@@ -103,5 +108,10 @@ public class UpdateApplicantExpertsTask extends MigrationTask<CaseReference> {
                 .value(transformer.apply(element.getValue()))
                 .build())
             .toList();
+    }
+
+    /** Return "TBC" if null */
+    private String defaultIfNull(String value) {
+        return value == null ? "TBC" : value;
     }
 }
