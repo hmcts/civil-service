dd01b535192588e7b447952cfd5b7e5b644aab24
mounikaammineni <Ayra@23082019>
2025-10-28 15:34:07 +0000

    Update

--- src/test/java/uk/gov/hmcts/reform/civil/handler/migration/UpdatePartyExpertsTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/migration/UpdatePartyExpertsTaskTest.java
@@ -19,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UpdatePartyExpertsTaskTest {
+
     private UpdatePartyExpertsTask task;
 
     @BeforeEach
