24747938c2c92c51dba614ed57463353423357c8
rishisharma <rishi.sharma@hmcts.net>
2025-10-29 12:44:14 +0000

    DTSCCI-3173: capture error when party notified fails

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/automatedhearingnotice/UpdateHmcPartiesNotifiedHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/automatedhearingnotice/UpdateHmcPartiesNotifiedHandlerTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.automatedhearingnotice;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,24 +7,26 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.service.hearingnotice.HearingNoticeCamundaService;
 import uk.gov.hmcts.reform.civil.service.hearingnotice.HearingNoticeVariables;
 import uk.gov.hmcts.reform.hmc.model.unnotifiedhearings.HearingDay;
 import uk.gov.hmcts.reform.hmc.model.unnotifiedhearings.PartiesNotified;
-import uk.gov.hmcts.reform.hmc.model.unnotifiedhearings.PartiesNotifiedServiceData;
 import uk.gov.hmcts.reform.hmc.service.HearingsService;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
@@ -42,100 +43,100 @@ class UpdateHmcPartiesNotifiedHandlerTest extends BaseCallbackHandlerTest {
     @Mock
     private HearingsService hearingsService;
 
-    @BeforeEach
-    void setUp() {
-        when(hearingNoticeCamundaService.getProcessVariables(any()))
-            .thenReturn(HearingNoticeVariables.builder()
-                            .hearingId("HER1234")
-                            .hearingLocationEpims("12345")
-                            .days(List.of(HearingDay.builder()
-                                      .hearingStartDateTime(LocalDateTime.of(2023, 1, 1, 0, 0, 0))
-                                      .hearingEndDateTime(LocalDateTime.of(2023, 1, 1, 12, 0, 0))
-                                      .build()))
-                            .hearingStartDateTime(LocalDateTime.of(2022, 11, 7, 15, 15))
-                            .requestVersion(10L)
-                            .responseDateTime(LocalDateTime.of(2022, 10, 10, 15, 15))
-                            .build());
+    private CallbackParams buildCallbackParams(CaseData caseData) {
+        return CallbackParams.builder()
+            .type(ABOUT_TO_SUBMIT)
+            .caseData(caseData)
+            .params(Map.of(CallbackParams.Params.BEARER_TOKEN, "BEARER_TOKEN"))
+            .build();
+    }
+
+    private HearingNoticeVariables sampleCamundaVars() {
+        return HearingNoticeVariables.builder()
+            .hearingId("HER1234")
+            .hearingLocationEpims("12345")
+            .days(List.of(HearingDay.builder()
+                              .hearingStartDateTime(LocalDateTime.of(2023, 1, 1, 0, 0, 0))
+                              .hearingEndDateTime(LocalDateTime.of(2023, 1, 1, 12, 0, 0))
+                              .build()))
+            .hearingStartDateTime(LocalDateTime.of(2022, 11, 7, 15, 15))
+            .requestVersion(10L)
+            .responseDateTime(LocalDateTime.of(2022, 10, 10, 15, 15))
+            .build();
     }
 
     @Test
-    void shouldReturnExpectedCallbackResponseOnAboutToSubmit() {
-        when(hearingsService.updatePartiesNotifiedResponse(anyString(), anyString(), anyInt(), any(), any()))
-            .thenReturn(new ResponseEntity<>("Ok", HttpStatus.ACCEPTED));
-        CaseData caseData = CaseDataBuilder.builder().atStateClaimDetailsNotified().build().toBuilder()
+    void shouldSuccessfullyUpdatePartiesNotified_whenServiceReturns200() {
+        // Given
+        CaseData caseData = CaseData.builder().ccdCaseReference(12345L).build().toBuilder()
             .businessProcess(BusinessProcess.builder().processInstanceId("").build())
             .build();
-        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+        CallbackParams params = buildCallbackParams(caseData);
+        HearingNoticeVariables camundaVars = sampleCamundaVars();
 
-        LocalDateTime receivedDate = LocalDateTime.of(2022, 10, 10, 15, 15);
-        List<HearingDay> hearingDays = List.of(HearingDay.builder()
-                                             .hearingStartDateTime(LocalDateTime.of(2023, 1, 1, 0, 0, 0))
-                                             .hearingEndDateTime(LocalDateTime.of(2023, 1, 1, 12, 0, 0))
-                                             .build());
-        PartiesNotified partiesNotified = PartiesNotified.builder()
-            .serviceData(PartiesNotifiedServiceData.builder()
-                             .days(hearingDays)
-                             .hearingNoticeGenerated(true)
-                             .hearingLocation("12345")
-                             .build())
-            .build();
+        when(hearingNoticeCamundaService.getProcessVariables(any())).thenReturn(camundaVars);
+        when(hearingsService.updatePartiesNotifiedResponse(anyString(), anyString(), anyInt(), any(), any(PartiesNotified.class)))
+            .thenReturn(new ResponseEntity<>(HttpStatus.OK));
 
-        handler.handle(params);
+        // When
+        var response = handler.handle(params);
 
-        verify(hearingsService).updatePartiesNotifiedResponse("BEARER_TOKEN", "HER1234", 10, receivedDate, partiesNotified);
+        // Then
+        assertNotNull(response);
+        verify(hearingsService).updatePartiesNotifiedResponse(eq("BEARER_TOKEN"), eq("HER1234"), anyInt(), any(), any(PartiesNotified.class));
     }
 
     @Test
-    void shouldReturnExpectedCallbackResponseOnAboutToSubmitV1() {
+    void shouldLogWarning_whenServiceReturnsNon2xx() {
+        CaseData caseData = CaseData.builder().ccdCaseReference(12345L).build().toBuilder()
+            .businessProcess(BusinessProcess.builder().processInstanceId("").build())
+            .build();
+        CallbackParams params = buildCallbackParams(caseData);
+        HearingNoticeVariables camundaVars = sampleCamundaVars();
+
+        when(hearingNoticeCamundaService.getProcessVariables(any())).thenReturn(camundaVars);
+        when(hearingsService.updatePartiesNotifiedResponse(anyString(), anyString(), anyInt(), any(), any()))
+            .thenReturn(new ResponseEntity<>("Error", HttpStatus.BAD_REQUEST));
+
+        var response = handler.handle(params);
+
+        assertNotNull(response);
+        verify(hearingsService).updatePartiesNotifiedResponse(eq("BEARER_TOKEN"), eq("HER1234"), anyInt(), any(), any(PartiesNotified.class));
+    }
+
+    @Test
+    void shouldHandleNullResponseGracefully() {
+        CaseData caseData = CaseData.builder().ccdCaseReference(12345L).build().toBuilder()
+            .businessProcess(BusinessProcess.builder().processInstanceId("").build())
+            .build();
+        CallbackParams params = buildCallbackParams(caseData);
+        HearingNoticeVariables camundaVars = sampleCamundaVars();
+
+        when(hearingNoticeCamundaService.getProcessVariables(any())).thenReturn(camundaVars);
         when(hearingsService.updatePartiesNotifiedResponse(anyString(), anyString(), anyInt(), any(), any()))
             .thenReturn(null);
-        CaseData caseData = CaseDataBuilder.builder().atStateClaimDetailsNotified().build().toBuilder()
-            .businessProcess(BusinessProcess.builder().processInstanceId("").build())
-            .build();
-        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
 
-        LocalDateTime receivedDate = LocalDateTime.of(2022, 10, 10, 15, 15);
-        List<HearingDay> hearingDays = List.of(HearingDay.builder()
-                                                   .hearingStartDateTime(LocalDateTime.of(2023, 1, 1, 0, 0, 0))
-                                                   .hearingEndDateTime(LocalDateTime.of(2023, 1, 1, 12, 0, 0))
-                                                   .build());
-        PartiesNotified partiesNotified = PartiesNotified.builder()
-            .serviceData(PartiesNotifiedServiceData.builder()
-                             .days(hearingDays)
-                             .hearingNoticeGenerated(true)
-                             .hearingLocation("12345")
-                             .build())
-            .build();
+        var response = handler.handle(params);
 
-        handler.handle(params);
-
-        verify(hearingsService).updatePartiesNotifiedResponse("BEARER_TOKEN", "HER1234", 10, receivedDate, partiesNotified);
+        assertNotNull(response);
+        verify(hearingsService).updatePartiesNotifiedResponse(anyString(), anyString(), anyInt(), any(), any());
     }
 
     @Test
-    void shouldNotReturnExpectedCallbackResponseOnAboutToSubmitWhenHearingsServiceReturns400() {
-        when(hearingsService.updatePartiesNotifiedResponse(anyString(), anyString(), anyInt(), any(), any()))
-            .thenReturn(new ResponseEntity<>("Bad Request", HttpStatus.BAD_REQUEST));
-        CaseData caseData = CaseDataBuilder.builder().atStateClaimDetailsNotified().build().toBuilder()
+    void shouldCatchRestClientException() {
+        CaseData caseData = CaseData.builder().ccdCaseReference(12345L).build().toBuilder()
             .businessProcess(BusinessProcess.builder().processInstanceId("").build())
             .build();
-        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+        CallbackParams params = buildCallbackParams(caseData);
+        HearingNoticeVariables camundaVars = sampleCamundaVars();
 
-        LocalDateTime receivedDate = LocalDateTime.of(2022, 10, 10, 15, 15);
-        List<HearingDay> hearingDays = List.of(HearingDay.builder()
-                                                   .hearingStartDateTime(LocalDateTime.of(2023, 1, 1, 0, 0, 0))
-                                                   .hearingEndDateTime(LocalDateTime.of(2023, 1, 1, 12, 0, 0))
-                                                   .build());
-        PartiesNotified partiesNotified = PartiesNotified.builder()
-            .serviceData(PartiesNotifiedServiceData.builder()
-                             .days(hearingDays)
-                             .hearingNoticeGenerated(true)
-                             .hearingLocation("12345")
-                             .build())
-            .build();
+        when(hearingNoticeCamundaService.getProcessVariables(any())).thenReturn(camundaVars);
+        when(hearingsService.updatePartiesNotifiedResponse(anyString(), anyString(), anyInt(), any(), any()))
+            .thenThrow(new RestClientException("Connection refused"));
 
-        handler.handle(params);
+        var response = handler.handle(params);
 
-        verify(hearingsService).updatePartiesNotifiedResponse("BEARER_TOKEN", "HER1234", 10, receivedDate, partiesNotified);
+        assertNotNull(response);
+        verify(hearingsService).updatePartiesNotifiedResponse(anyString(), anyString(), anyInt(), any(), any());
     }
 }
