40dd2df4eb84be31fbb0d71d32a737d996c4382b
Shaun Francis <shaun.francis@hmcts.net>
2025-11-03 08:45:54 +0000

    Pull and import both civil and gen apps CCD definitions

---
+++ bin/pull-latest-gen-apps-ccd-files.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+ccdRepoName="civil-general-apps-ccd-definition"
+branchName=$1
+directoryName="gen-apps-test"
+
+#Checkout specific branch of CCD definitions
+git clone https://github.com/hmcts/${ccdRepoName}.git
+cd ${ccdRepoName}
+
+echo "Switch to ${branchName} branch on ${ccdRepoName}"
+git checkout ${branchName}
+cd ..
+
+#Copy ccd definition files  to civil-ccd-def which contains ccd def files
+cp -r ./${ccdRepoName}/ga-ccd-definition .
+#mv ./${ccdRepoName}/e2e ./gen-apps-e2e
+#cp -r ./${ccdRepoName}/package.json ./${directoryName}
+#cp -r ./${ccdRepoName}/yarn.lock ./${directoryName}
+#cp -r ./${ccdRepoName}/codecept.conf.js ./${directoryName}
+#cp -r ./${ccdRepoName}/saucelabs.conf.js ./${directoryName}
+echo *
+rm -rf ./${ccdRepoName}
