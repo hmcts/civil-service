0e301b7a970a3d5e4b10db091e5ad22c69faa736
Shaun Francis <shaun.francis@hmcts.net>
2025-11-04 16:50:40 +0000

    Revert codecept.conf.js and try different approach

--- bin/pull-latest-civil-ccd-files.sh
+++ bin/pull-latest-civil-ccd-files.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-ccdRepoName="civil-general-apps-ccd-definition"
+ccdRepoName="civil-ccd-definition"
 branchName=$1
-directoryName="gen-apps-test"
+directoryName="civil-test"
 
 #Checkout specific branch of CCD definitions
 git clone https://github.com/hmcts/${ccdRepoName}.git
@@ -13,9 +13,8 @@ git checkout ${branchName}
 cd ..
 
 #Copy ccd definition files  to civil-ccd-def which contains ccd def files
-cp -r ./${ccdRepoName}/ga-ccd-definition .
+cp -r ./${ccdRepoName}/ccd-definition .
 mkdir -p ${directoryName}/e2e && mv ./${ccdRepoName}/e2e ./${directoryName}/e2e
-#cp -r ./${ccdRepoName}/e2e ./${directoryName}/gen-apps-e2e
 cp -r ./${ccdRepoName}/package.json ./${directoryName}
 cp -r ./${ccdRepoName}/yarn.lock ./${directoryName}
 cp -r ./${ccdRepoName}/codecept.conf.js ./${directoryName}
