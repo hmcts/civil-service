5add17f80837d6d4022cc7241abb1a408d00e530
matt-goodwin-solirius <matt.goodwin@solirius.com>
2025-11-04 15:31:49 +0000

    Ensure system generated case documents are included

--- src/test/java/uk/gov/hmcts/reform/civil/helpers/bundle/BundleRequestMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/helpers/bundle/BundleRequestMapperTest.java
@@ -37,6 +37,10 @@ import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 
 import java.time.LocalDate;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -126,6 +130,46 @@ class BundleRequestMapperTest {
     }
 
     @Test
+    void shouldIncludeDefendantDefenceWhenOnlyInSystemGeneratedDocs() {
+        given(featureToggleService.isCaseProgressionEnabled()).willReturn(false);
+        given(featureToggleService.isAmendBundleEnabled()).willReturn(false);
+
+        CaseDocument defendantDefence = CaseDocument.builder()
+            .documentType(DocumentType.DEFENDANT_DEFENCE)
+            .createdBy("Defendant")
+            .documentLink(Document.builder()
+                             .documentUrl(TEST_URL)
+                             .documentBinaryUrl(TEST_URL)
+                             .documentFileName(TEST_FILE_NAME)
+                             .build())
+            .createdDatetime(LocalDateTime.of(2023, 2, 10, 2, 2, 2))
+            .build();
+
+        CaseData caseData = CaseData.builder()
+            .ccdCaseReference(12345L)
+            .applicant1(Party.builder().type(Party.Type.COMPANY).partyName("Claimant Ltd").build())
+            .respondent1(Party.builder().type(Party.Type.COMPANY).partyName("Defendant Ltd").build())
+            .hearingDate(LocalDate.now())
+            .submittedDate(LocalDateTime.of(2023, 2, 10, 2, 2, 2))
+            .systemGeneratedCaseDocuments(ElementUtils.wrapElements(defendantDefence))
+            .build();
+
+        BundleCreateRequest bundleCreateRequest = bundleRequestMapper.mapCaseDataToBundleCreateRequest(
+            caseData,
+            "sample.yaml",
+            "JUR",
+            "CASE"
+        );
+
+        assertTrue(
+            bundleCreateRequest.getCaseDetails().getCaseData().getStatementsOfCaseDocuments()
+                .stream()
+                .anyMatch(doc -> DocumentType.DEFENDANT_DEFENCE.name().equals(doc.getValue().getDocumentType())),
+            "Defendant defence document should be included when only present in system generated docs"
+        );
+    }
+
+    @Test
     void shouldHandleCaseDataWithNoCategoryId() {
         CaseData caseData = getCaseDataWithNoId();
         given(featureToggleService.isAmendBundleEnabled()).willReturn(false);
@@ -143,8 +187,134 @@ class BundleRequestMapperTest {
 
         BundleCreateRequest result = mapCaseData(caseData);
 
-        assertBundleCreateRequestIsValid(result);
-        assertDirectionsQuestionnaires(result);
+    private List<Element<CaseDocument>> setupSystemGeneratedCaseDocsUnbundledFolderId() {
+        List<Element<CaseDocument>> systemGeneratedCaseDocuments = new ArrayList<>();
+        CaseDocument caseDocumentDQDef1 =
+            CaseDocument.builder()
+                .documentType(DocumentType.DIRECTIONS_QUESTIONNAIRE)
+                .documentLink(Document.builder().documentUrl(TEST_URL)
+                                  .documentFileName("ONE").categoryID("UnbundledFolder").build())
+                .createdDatetime(LocalDateTime.of(2023, 2, 10, 2,
+                                                  2, 2)).build();
+        CaseDocument caseDocumentDQApp1 =
+            CaseDocument.builder()
+                .documentType(DocumentType.DIRECTIONS_QUESTIONNAIRE)
+                .documentLink(Document.builder().documentUrl(TEST_URL)
+                                  .documentFileName("TWO").categoryID("UnbundledFolder").build())
+                .createdDatetime(LocalDateTime.of(2023, 3, 10, 2,
+                                                  2, 2)).build();
+        CaseDocument caseDocumentDQDef22 =
+            CaseDocument.builder()
+                .documentType(DocumentType.DIRECTIONS_QUESTIONNAIRE)
+                .documentLink(Document.builder().documentUrl(TEST_URL)
+                                  .documentFileName("THREE").categoryID("UnbundledFolder").build())
+                .createdDatetime(LocalDateTime.of(2023, 4, 11, 2,
+                                                  2, 2)).build();
+        CaseDocument caseDocumentDQDef21 =
+            CaseDocument.builder()
+                .documentType(DocumentType.DIRECTIONS_QUESTIONNAIRE)
+                .documentLink(Document.builder().documentUrl(TEST_URL)
+                                  .documentFileName("FOUR").categoryID("UnbundledFolder").build())
+                .createdDatetime(LocalDateTime.of(2023, 5, 10, 2,
+                                                  2, 2)).build();
+        CaseDocument caseDocumentDQDef23 =
+            CaseDocument.builder()
+                .documentType(DocumentType.DIRECTIONS_QUESTIONNAIRE)
+                .documentLink(Document.builder().documentUrl(TEST_URL)
+                                  .documentFileName("FIVE").build())
+                .createdDatetime(LocalDateTime.of(2023, 6, 10, 2,
+                                                  2, 2)).build();
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentDQDef1));
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentDQApp1));
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentDQDef22));
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentDQDef21));
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentDQDef23));
+        return systemGeneratedCaseDocuments;
+    }
+
+    private List<Element<CaseDocument>> setupSystemGeneratedCaseDocs() {
+        List<Element<CaseDocument>> systemGeneratedCaseDocuments = new ArrayList<>();
+        CaseDocument caseDocumentClaim =
+            CaseDocument.builder().documentType(DocumentType.SEALED_CLAIM).documentLink(Document.builder().documentUrl(
+                TEST_URL).documentFileName(TEST_FILE_NAME).categoryID("detailsOfClaim").build()).createdDatetime(LocalDateTime.of(2023, 2, 10, 2,
+                 2, 2)).build();
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentClaim));
+        CaseDocument caseDocumentDQDef1 =
+                CaseDocument.builder()
+                        .documentType(DocumentType.DIRECTIONS_QUESTIONNAIRE)
+                        .documentLink(Document.builder().documentUrl(TEST_URL)
+                                .categoryID(DocCategory.DEF1_DEFENSE_DQ.getValue())
+                                .documentFileName(TEST_FILE_NAME).build())
+                        .createdDatetime(LocalDateTime.of(2023, 2, 10, 2,
+                                2, 2)).build();
+        CaseDocument caseDocumentDQApp1 =
+            CaseDocument.builder()
+                .documentType(DocumentType.DIRECTIONS_QUESTIONNAIRE)
+                .documentLink(Document.builder().documentUrl(TEST_URL).categoryID(DocCategory.APP1_DQ.getValue())
+                        .documentFileName(TEST_FILE_NAME).build())
+                .createdDatetime(LocalDateTime.of(2023, 2, 10, 2,
+                                                  2, 2)).build();
+        CaseDocument caseDocumentDQDef22 =
+                CaseDocument.builder()
+                        .documentType(DocumentType.DIRECTIONS_QUESTIONNAIRE)
+                        .documentLink(Document.builder().documentUrl(TEST_URL)
+                                .categoryID(DocCategory.DEF2_DEFENSE_DQ.getValue())
+                                .documentFileName(TEST_FILE_NAME).build())
+                        .createdDatetime(LocalDateTime.of(2023, 2, 11, 2,
+                                2, 2)).build();
+        CaseDocument caseDocumentDQDef21 =
+                CaseDocument.builder()
+                        .documentType(DocumentType.DIRECTIONS_QUESTIONNAIRE)
+                        .documentLink(Document.builder().documentUrl(TEST_URL)
+                                .categoryID(DocCategory.DEF2_DEFENSE_DQ.getValue())
+                                .documentFileName(TEST_FILE_NAME).build())
+                        .createdDatetime(LocalDateTime.of(2023, 2, 10, 2,
+                                2, 2)).build();
+        CaseDocument caseDocumentDQDef23 =
+                CaseDocument.builder()
+                        .documentType(DocumentType.DIRECTIONS_QUESTIONNAIRE)
+                        .documentLink(Document.builder().documentUrl(TEST_URL)
+                                .categoryID(DocCategory.DEF2_DEFENSE_DQ.getValue())
+                                .documentFileName(TEST_FILE_NAME).build())
+                        .createdDatetime(LocalDateTime.of(2023, 3, 10, 2,
+                                2, 2)).build();
+        CaseDocument caseDocumentDQNoId =
+            CaseDocument.builder()
+                .documentType(DocumentType.DIRECTIONS_QUESTIONNAIRE)
+                .documentLink(Document.builder().documentUrl(TEST_URL)
+                                  .documentFileName("DQ_NO_CATEGORY_ID").build())
+                .createdDatetime(LocalDateTime.of(2023, 2, 10, 2,
+                                                  2, 2)).build();
+        CaseDocument caseDocumentDQApp1LiP =
+            CaseDocument.builder()
+                .documentType(DocumentType.DIRECTIONS_QUESTIONNAIRE)
+                .documentLink(Document.builder().documentUrl(TEST_URL).categoryID(DocCategory.DQ_APP1.getValue())
+                                  .documentFileName(TEST_FILE_NAME).build())
+                .createdDatetime(LocalDateTime.of(2023, 3, 11, 2,
+                                                  2, 2)).build();
+        CaseDocument caseDocumentDQDef1LiP =
+            CaseDocument.builder()
+                .documentType(DocumentType.DIRECTIONS_QUESTIONNAIRE)
+                .documentLink(Document.builder().documentUrl(TEST_URL).categoryID(DocCategory.DQ_DEF1.getValue())
+                                  .documentFileName(TEST_FILE_NAME).build())
+                .createdDatetime(LocalDateTime.of(2023, 3, 12, 2,
+                                                  2, 2)).build();
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentDQDef1));
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentDQApp1));
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentDQDef22));
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentDQDef21));
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentDQDef23));
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentDQNoId));
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentDQApp1LiP));
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentDQDef1LiP));
+        CaseDocument caseDocumentDJ =
+            CaseDocument.builder()
+                .documentType(DocumentType.DEFAULT_JUDGMENT_SDO_ORDER)
+                .documentLink(Document.builder().documentUrl(TEST_URL).documentFileName(TEST_FILE_NAME).build())
+                .createdDatetime(LocalDateTime.of(2023, 2, 10, 2,
+                                                  2, 2)).build();
+        systemGeneratedCaseDocuments.add(ElementUtils.element(caseDocumentDJ));
+        return systemGeneratedCaseDocuments;
     }
 
     @Test
