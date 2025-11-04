69f41c45cf28dfc9aba6bbbc974e44e9e4ea4098
AbuSalam22 <136581242+AbuSalam22@users.noreply.github.com>
2025-11-04 17:12:14 +0000

    DTSCCI-3134

---
+++ src/main/java/uk/gov/hmcts/reform/civil/service/sdo/SdoSubmissionService.java
@@ -0,0 +1,163 @@
+package uk.gov.hmcts.reform.civil.service.sdo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
+import uk.gov.hmcts.reform.civil.enums.CaseCategory;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.CaseData.CaseDataBuilder;
+import uk.gov.hmcts.reform.civil.model.BusinessProcess;
+import uk.gov.hmcts.reform.civil.model.sdo.SdoR2SmallClaimsHearing;
+import uk.gov.hmcts.reform.civil.model.sdo.SdoR2Trial;
+import uk.gov.hmcts.reform.civil.utils.ElementUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static uk.gov.hmcts.reform.civil.enums.AllocatedTrack.FAST_CLAIM;
+import static uk.gov.hmcts.reform.civil.enums.AllocatedTrack.SMALL_CLAIM;
+import static uk.gov.hmcts.reform.civil.utils.HearingUtils.getHearingNotes;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class SdoSubmissionService {
+
+    private final SdoFeatureToggleService featureToggleService;
+    private final SdoLocationService sdoLocationService;
+    private final SdoCaseClassificationService caseClassificationService;
+
+    public CaseData prepareSubmission(CaseData caseData, String authToken) {
+        CaseDataBuilder<?, ?> builder = caseData.toBuilder()
+            .businessProcess(BusinessProcess.ready(CaseEvent.CREATE_SDO))
+            .hearingNotes(getHearingNotes(caseData));
+
+        moveGeneratedDocument(caseData, builder);
+        updateEaCourtLocation(caseData, builder);
+        trimMethodLocations(caseData, builder);
+        updateSmallClaimsHearing(caseData, builder);
+        updateClaimsTrack(caseData, builder);
+        updateTrialLocations(caseData, builder);
+
+        return builder.build();
+    }
+
+    private void moveGeneratedDocument(CaseData caseData, CaseDataBuilder<?, ?> builder) {
+        CaseDocument document = caseData.getSdoOrderDocument();
+        if (document == null) {
+            return;
+        }
+
+        if (featureToggleService.isWelshJourneyEnabled(caseData)) {
+            List<uk.gov.hmcts.reform.civil.model.common.Element<CaseDocument>> preTranslation =
+                new ArrayList<>(Optional.ofNullable(caseData.getPreTranslationDocuments()).orElseGet(ArrayList::new));
+            preTranslation.add(ElementUtils.element(document));
+            builder.preTranslationDocuments(preTranslation);
+        } else {
+            List<uk.gov.hmcts.reform.civil.model.common.Element<CaseDocument>> generatedDocuments =
+                new ArrayList<>(Optional.ofNullable(caseData.getSystemGeneratedCaseDocuments()).orElseGet(ArrayList::new));
+            generatedDocuments.add(ElementUtils.element(document));
+            builder.systemGeneratedCaseDocuments(generatedDocuments);
+        }
+
+        builder.sdoOrderDocument(null);
+    }
+
+    private void updateEaCourtLocation(CaseData caseData, CaseDataBuilder<?, ?> builder) {
+        if (!CaseCategory.SPEC_CLAIM.equals(caseData.getCaseAccessCategory())) {
+            return;
+        }
+
+        boolean isLipCase = caseData.isApplicantLiP() || caseData.isRespondent1LiP() || caseData.isRespondent2LiP();
+        if (featureToggleService.isWelshEnabledForMainCase()) {
+            builder.eaCourtLocation(YesOrNo.YES);
+        } else if (!isLipCase) {
+            log.info("Case {} is whitelisted for case progression.", caseData.getCcdCaseReference());
+            builder.eaCourtLocation(YesOrNo.YES);
+        } else {
+            boolean isWhitelisted = isLipCaseWithProgressionEnabledAndCourtWhiteListed(caseData);
+            builder.eaCourtLocation(isWhitelisted ? YesOrNo.YES : YesOrNo.NO);
+        }
+    }
+
+    private boolean isLipCaseWithProgressionEnabledAndCourtWhiteListed(CaseData caseData) {
+        return (caseData.isLipvLipOneVOne() || caseData.isLRvLipOneVOne()
+            || (caseData.isLipvLROneVOne() && featureToggleService.isDefendantNoCOnlineForCase(caseData)))
+            && featureToggleService
+            .isCaseProgressionEnabledAndLocationWhiteListed(caseData.getCaseManagementLocation().getBaseLocation());
+    }
+
+    private void trimMethodLocations(CaseData caseData, CaseDataBuilder<?, ?> builder) {
+        Optional.ofNullable(caseData.getDisposalHearingMethodInPerson())
+            .map(sdoLocationService::trimListItems)
+            .ifPresent(builder::disposalHearingMethodInPerson);
+        Optional.ofNullable(caseData.getFastTrackMethodInPerson())
+            .map(sdoLocationService::trimListItems)
+            .ifPresent(builder::fastTrackMethodInPerson);
+        Optional.ofNullable(caseData.getSmallClaimsMethodInPerson())
+            .map(sdoLocationService::trimListItems)
+            .ifPresent(builder::smallClaimsMethodInPerson);
+    }
+
+    private void updateSmallClaimsHearing(CaseData caseData, CaseDataBuilder<?, ?> builder) {
+        if (!caseClassificationService.isDrhSmallClaim(caseData) || caseData.getSdoR2SmallClaimsHearing() == null) {
+            return;
+        }
+
+        SdoR2SmallClaimsHearing hearing = caseData.getSdoR2SmallClaimsHearing();
+        SdoR2SmallClaimsHearing.SdoR2SmallClaimsHearingBuilder hearingBuilder = hearing.toBuilder();
+
+        Optional.ofNullable(hearing.getHearingCourtLocationList())
+            .map(sdoLocationService::trimListItems)
+            .ifPresent(hearingBuilder::hearingCourtLocationList);
+        Optional.ofNullable(hearing.getAltHearingCourtLocationList())
+            .map(sdoLocationService::trimListItems)
+            .ifPresent(hearingBuilder::altHearingCourtLocationList);
+
+        builder.sdoR2SmallClaimsHearing(hearingBuilder.build());
+    }
+
+    private void updateClaimsTrack(CaseData caseData, CaseDataBuilder<?, ?> builder) {
+        CaseCategory caseCategory = caseData.getCaseAccessCategory();
+        switch (caseCategory) {
+            case UNSPEC_CLAIM:
+                if (caseClassificationService.isSmallClaimsTrack(caseData)) {
+                    builder.allocatedTrack(SMALL_CLAIM);
+                } else if (caseClassificationService.isFastTrack(caseData)) {
+                    builder.allocatedTrack(FAST_CLAIM);
+                }
+                break;
+            case SPEC_CLAIM:
+                if (caseClassificationService.isSmallClaimsTrack(caseData)) {
+                    builder.responseClaimTrack(SMALL_CLAIM.name());
+                } else if (caseClassificationService.isFastTrack(caseData)) {
+                    builder.responseClaimTrack(FAST_CLAIM.name());
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void updateTrialLocations(CaseData caseData, CaseDataBuilder<?, ?> builder) {
+        if (caseData.getSdoR2Trial() == null) {
+            return;
+        }
+
+        SdoR2Trial trial = caseData.getSdoR2Trial();
+        SdoR2Trial.SdoR2TrialBuilder trialBuilder = trial.toBuilder();
+
+        Optional.ofNullable(trial.getHearingCourtLocationList())
+            .map(sdoLocationService::trimListItems)
+            .ifPresent(trialBuilder::hearingCourtLocationList);
+        Optional.ofNullable(trial.getAltHearingCourtLocationList())
+            .map(sdoLocationService::trimListItems)
+            .ifPresent(trialBuilder::altHearingCourtLocationList);
+
+        builder.sdoR2Trial(trialBuilder.build());
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/civil/service/sdo/SdoSubmissionServiceTest.java
@@ -0,0 +1,193 @@
+package uk.gov.hmcts.reform.civil.service.sdo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
+import uk.gov.hmcts.reform.civil.enums.CaseCategory;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.enums.BusinessProcessStatus;
+import uk.gov.hmcts.reform.civil.model.common.DynamicList;
+import uk.gov.hmcts.reform.civil.model.common.DynamicListElement;
+import uk.gov.hmcts.reform.civil.model.common.Element;
+import uk.gov.hmcts.reform.civil.model.defaultjudgment.CaseLocationCivil;
+import uk.gov.hmcts.reform.civil.model.sdo.SdoR2SmallClaimsHearing;
+import uk.gov.hmcts.reform.civil.model.sdo.SdoR2Trial;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDocumentBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class SdoSubmissionServiceTest {
+
+    private static final String AUTH_TOKEN = "auth-token";
+
+    @Mock
+    private SdoFeatureToggleService featureToggleService;
+
+    @Mock
+    private SdoLocationService locationService;
+
+    @Mock
+    private SdoCaseClassificationService classificationService;
+
+    private SdoSubmissionService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new SdoSubmissionService(featureToggleService, locationService, classificationService);
+    }
+
+    @Test
+    void shouldMoveDocumentToSystemGeneratedWhenEnglishJourney() {
+        CaseDocument document = CaseDocumentBuilder.builder().documentName("sdo.pdf").build();
+        List<Element<CaseDocument>> generatedDocs = new ArrayList<>();
+        CaseData caseData = CaseData.builder()
+            .caseAccessCategory(CaseCategory.SPEC_CLAIM)
+            .sdoOrderDocument(document)
+            .systemGeneratedCaseDocuments(generatedDocs)
+            .build();
+
+
+        CaseData result = service.prepareSubmission(caseData, AUTH_TOKEN);
+
+        assertThat(result.getSystemGeneratedCaseDocuments()).hasSize(1);
+        assertThat(result.getSystemGeneratedCaseDocuments().get(0).getValue()).isEqualTo(document);
+        assertThat(result.getSdoOrderDocument()).isNull();
+        assertThat(result.getBusinessProcess()).isNotNull();
+        assertThat(result.getBusinessProcess().getCamundaEvent()).isEqualTo(CaseEvent.CREATE_SDO.name());
+        assertThat(result.getBusinessProcess().getStatus()).isEqualTo(BusinessProcessStatus.READY);
+    }
+
+    @Test
+    void shouldMoveDocumentToPreTranslationForWelshJourney() {
+        CaseDocument document = CaseDocumentBuilder.builder().documentName("sdo.pdf").build();
+        List<Element<CaseDocument>> preTranslationDocs = new ArrayList<>();
+        CaseData caseData = CaseData.builder()
+            .caseAccessCategory(CaseCategory.SPEC_CLAIM)
+            .sdoOrderDocument(document)
+            .preTranslationDocuments(preTranslationDocs)
+            .build();
+
+        when(featureToggleService.isWelshJourneyEnabled(caseData)).thenReturn(true);
+
+        CaseData result = service.prepareSubmission(caseData, AUTH_TOKEN);
+
+        assertThat(result.getPreTranslationDocuments()).hasSize(1);
+        assertThat(result.getPreTranslationDocuments().get(0).getValue()).isEqualTo(document);
+        assertThat(result.getSdoOrderDocument()).isNull();
+    }
+
+    @Test
+    void shouldSetEaCourtLocationForSpecCaseWhenNotLip() {
+        CaseData caseData = CaseData.builder()
+            .caseAccessCategory(CaseCategory.SPEC_CLAIM)
+            .applicant1Represented(YesOrNo.YES)
+            .respondent1Represented(YesOrNo.YES)
+            .caseManagementLocation(CaseLocationCivil.builder().baseLocation("123").build())
+            .build();
+
+        when(featureToggleService.isWelshEnabledForMainCase()).thenReturn(false);
+
+        CaseData result = service.prepareSubmission(caseData, AUTH_TOKEN);
+
+        assertThat(result.getEaCourtLocation()).isEqualTo(YesOrNo.YES);
+    }
+
+    @Test
+    void shouldRespectLipCaseWhenCourtNotWhiteListed() {
+        CaseData caseData = CaseData.builder()
+            .caseAccessCategory(CaseCategory.SPEC_CLAIM)
+            .applicant1Represented(YesOrNo.NO)
+            .respondent1Represented(YesOrNo.NO)
+            .caseManagementLocation(CaseLocationCivil.builder().baseLocation("123").build())
+            .build();
+
+        when(featureToggleService.isWelshEnabledForMainCase()).thenReturn(false);
+
+        CaseData result = service.prepareSubmission(caseData, AUTH_TOKEN);
+
+        assertThat(result.getEaCourtLocation()).isEqualTo(YesOrNo.NO);
+    }
+
+    @Test
+    void shouldTrimDynamicListsAndSmallClaimsHearingLocations() {
+        DynamicListElement selected = DynamicListElement.builder().code("loc").label("Loc").build();
+        DynamicList originalList = DynamicList.builder().value(selected).build();
+        DynamicList trimmedList = DynamicList.builder().value(selected).listItems(List.of()).build();
+
+        when(locationService.trimListItems(originalList)).thenReturn(trimmedList);
+        when(locationService.trimListItems(originalList)).thenReturn(trimmedList);
+        when(classificationService.isDrhSmallClaim(any())).thenReturn(true);
+
+        SdoR2SmallClaimsHearing hearing = SdoR2SmallClaimsHearing.builder()
+            .hearingCourtLocationList(originalList)
+            .altHearingCourtLocationList(originalList)
+            .build();
+        SdoR2Trial trial = SdoR2Trial.builder()
+            .hearingCourtLocationList(originalList)
+            .altHearingCourtLocationList(originalList)
+            .build();
+
+        CaseData caseData = CaseData.builder()
+            .caseAccessCategory(CaseCategory.UNSPEC_CLAIM)
+            .disposalHearingMethodInPerson(originalList)
+            .fastTrackMethodInPerson(originalList)
+            .smallClaimsMethodInPerson(originalList)
+            .sdoR2SmallClaimsHearing(hearing)
+            .sdoR2Trial(trial)
+            .build();
+
+        when(locationService.trimListItems(originalList)).thenReturn(trimmedList);
+
+        CaseData result = service.prepareSubmission(caseData, AUTH_TOKEN);
+
+        assertThat(result.getDisposalHearingMethodInPerson()).isEqualTo(trimmedList);
+        assertThat(result.getFastTrackMethodInPerson()).isEqualTo(trimmedList);
+        assertThat(result.getSmallClaimsMethodInPerson()).isEqualTo(trimmedList);
+        assertThat(result.getSdoR2SmallClaimsHearing().getHearingCourtLocationList()).isEqualTo(trimmedList);
+        assertThat(result.getSdoR2SmallClaimsHearing().getAltHearingCourtLocationList()).isEqualTo(trimmedList);
+        assertThat(result.getSdoR2Trial().getHearingCourtLocationList()).isEqualTo(trimmedList);
+        assertThat(result.getSdoR2Trial().getAltHearingCourtLocationList()).isEqualTo(trimmedList);
+    }
+
+    @Test
+    void shouldUpdateClaimsTrackForUnspecSmallClaims() {
+        CaseData caseData = CaseData.builder()
+            .caseAccessCategory(CaseCategory.UNSPEC_CLAIM)
+            .build();
+
+        when(classificationService.isSmallClaimsTrack(caseData)).thenReturn(true);
+
+        CaseData result = service.prepareSubmission(caseData, AUTH_TOKEN);
+
+        assertThat(result.getAllocatedTrack()).isEqualTo(uk.gov.hmcts.reform.civil.enums.AllocatedTrack.SMALL_CLAIM);
+    }
+
+    @Test
+    void shouldUpdateClaimsTrackForSpecFastTrack() {
+        CaseData caseData = CaseData.builder()
+            .caseAccessCategory(CaseCategory.SPEC_CLAIM)
+            .build();
+
+        when(featureToggleService.isWelshEnabledForMainCase()).thenReturn(false);
+        when(classificationService.isSmallClaimsTrack(caseData)).thenReturn(false);
+        when(classificationService.isFastTrack(caseData)).thenReturn(true);
+
+        CaseData result = service.prepareSubmission(caseData, AUTH_TOKEN);
+
+        assertThat(result.getResponseClaimTrack()).isEqualTo("FAST_CLAIM");
+    }
+
+}
