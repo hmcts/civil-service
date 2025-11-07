67d3086108b451be3a2e3e1e32e926a9d8ef7ba7
mounikaammineni <Ayra@23082019>
2025-11-07 13:46:38 +0000

    Update

--- src/test/java/uk/gov/hmcts/reform/civil/bulkupdate/csv/PartyDetailsCaseReferenceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bulkupdate/csv/PartyDetailsCaseReferenceTest.java
@@ -25,7 +25,7 @@ class PartyDetailsCaseReferenceTest {
 
         caseReference.fromExcelRow(rowValues);
 
-        assertThat(caseReference.getCaseReference()).isEqualTo("\"CASE-123\"");
+        assertThat(caseReference.getCaseReference()).isEqualTo("CASE-123");
     }
 
     @Test
@@ -121,7 +121,7 @@ class PartyDetailsCaseReferenceTest {
 
         caseReference.fromExcelRow(rowValues);
 
-        assertThat(caseReference.getCaseReference()).isEqualTo("\"CASE-999\"");
+        assertThat(caseReference.getCaseReference()).isEqualTo("CASE-999");
         assertThat(caseReference.getDataObject()).isNull();
         assertThat(caseReference.getParty()).isNull();
         assertThat(caseReference.isApplicant1()).isFalse();
