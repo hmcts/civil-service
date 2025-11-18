4927ba28a7fd0399c067556b20e6d1599535c332
Ruban <ruban72@hotmail.com>
2025-11-18 14:03:38 +0000

    DTSCCI-3259: Fix CS issues

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardTaskContribution.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardTaskContribution.java
@@ -3,18 +3,17 @@ package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotification
 import java.util.List;
 
 /**
- * Represents a group of dashboard tasks that should be executed together
- * when a specific Camunda dashboard task runs.
+ * Represents a group of dashboard tasks that should be executed together when a Camunda dashboard task runs.
  */
 public interface DashboardTaskContribution {
 
     /**
-     * @return the Camunda activity id that this contribution is associated with
+     * Returns the Camunda activity id that this contribution is associated with.
      */
     String taskId();
 
     /**
-     * @return the ordered dashboard tasks that need to be triggered for the activity id
+     * Returns the ordered dashboard tasks that need to be triggered for the activity id.
      */
     List<DashboardWorkflowTask> dashboardTasks();
 }
