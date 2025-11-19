97294b5211abcf1915b7f5388de05778dc61e362
rishisharma <rishi.sharma@hmcts.net>
2025-11-19 17:16:16 +0000

    DTSCCI-3280: fix supplementary data

--- src/main/java/uk/gov/hmcts/reform/civil/handler/RetriggerCasesEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/RetriggerCasesEventHandler.java
@@ -1,28 +1,20 @@
 package uk.gov.hmcts.reform.civil.handler;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.client.task.ExternalTask;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
-import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
 import uk.gov.hmcts.reform.civil.handler.tasks.BaseExternalTaskHandler;
-import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.model.ExternalTaskData;
-import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import static java.lang.Long.parseLong;
+import static java.util.Collections.singletonMap;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,40 +22,41 @@ import static java.lang.Long.parseLong;
 public class RetriggerCasesEventHandler extends BaseExternalTaskHandler {
 
     private final CoreCaseDataService coreCaseDataService;
-    private final CaseDetailsConverter caseDetailsConverter;
     private final ObjectMapper mapper;
 
     @Override
     public ExternalTaskData handleTask(ExternalTask externalTask) {
-        assert externalTask.getVariable("caseEvent") != null;
-        assert externalTask.getVariable("caseIds") != null;
-
+        if (externalTask.getVariable("caseEvent") == null) {
+            throw new IllegalArgumentException("CaseEvent can not be null");
+        }
+        if (externalTask.getVariable("caseIds") == null) {
+            throw new IllegalArgumentException("CaseIds can not be null");
+        }
         String caseIds = externalTask.getVariable("caseIds");
         CaseEvent caseEvent = CaseEvent.valueOf(externalTask.getVariable("caseEvent"));
         String eventSummary = "Re-trigger of " + caseEvent.name();
         String eventDescription = "Process ID: %s".formatted(externalTask.getProcessInstanceId());
         Map<String, Object> caseData = getCaseData(externalTask);
-        String documentJson = externalTask.getVariable("document");
+        String orgId = externalTask.getVariable("orgId");
 
         for (String caseId : caseIds.split(",")) {
-            if (documentJson != null) {
-                handleDocumentUpdateFinalOrders(caseId, caseEvent, documentJson, externalTask.getProcessInstanceId());
-                break;
-            }
-            try {
-                log.info("" +
-                    "Retrigger CaseId: {} started", caseId);
-                externalTask.getAllVariables().put("caseId", caseId);
-                coreCaseDataService.triggerEvent(
-                    parseLong(caseId.trim()),
-                    caseEvent,
-                    caseData,
-                    eventSummary,
-                    eventDescription
-                );
-                log.info("Retrigger CaseId: {} finished. Case data: {}", caseId, caseData);
-            } catch (Exception e) {
-                log.error("ERROR Retrigger CaseId: {}. Case data: {},  {}", caseId, caseData, e.getMessage(), e);
+            if (orgId != null) {
+                setSupplementaryData(parseLong(caseId.trim()), orgId);
+            } else {
+                try {
+                    log.info("Retrigger CaseId: {} started", caseId);
+                    externalTask.getAllVariables().put("caseId", caseId);
+                    coreCaseDataService.triggerEvent(
+                        parseLong(caseId.trim()),
+                        caseEvent,
+                        caseData,
+                        eventSummary,
+                        eventDescription
+                    );
+                    log.info("Retrigger CaseId: {} finished. Case data: {}", caseId, caseData);
+                } catch (Exception e) {
+                    log.error("ERROR Retrigger CaseId: {}. Case data: {},  {}", caseId, caseData, e.getMessage(), e);
+                }
             }
         }
         return ExternalTaskData.builder().build();
@@ -85,44 +78,13 @@ public class RetriggerCasesEventHandler extends BaseExternalTaskHandler {
         }
     }
 
-    private void handleDocumentUpdateFinalOrders(String caseId,
-                                                 CaseEvent caseEvent,
-                                                 String documentJson,
-                                                 String processInstanceId) {
-        CaseDocument document = null;
-
-        try {
-            document = mapper.readValue(documentJson, CaseDocument.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(String.format("Adding Final order document to case %s with error %s", caseId, e));
-        }
-
-        CaseDetails caseDetails = coreCaseDataService.getCase(Long.parseLong(caseId));
-        Map<String, Object> data = caseDetails.getData();
-
-        JavaType listType = mapper.getTypeFactory()
-            .constructCollectionType(List.class,
-                                     mapper.getTypeFactory().constructParametricType(Element.class, CaseDocument.class));
-
-        List<Element<CaseDocument>> finalOrderDocumentCollection = data.get("finalOrderDocumentCollection") != null
-            ? mapper.convertValue(data.get("finalOrderDocumentCollection"), listType)
-            : new ArrayList<>();
-
-        finalOrderDocumentCollection.add(Element.<CaseDocument>builder().id(UUID.randomUUID()).value(document).build());
-
-        Map<String, Object> updatedData = new HashMap<>();
-
-        updatedData.put("finalOrderDocumentCollection", finalOrderDocumentCollection);
-
-        String eventSummary = "Re-trigger of " + caseEvent.name();
-        String eventDescription = "Process ID: %s".formatted(processInstanceId);
-
-        coreCaseDataService.triggerEvent(
-            parseLong(caseId.trim()),
-            caseEvent,
-            updatedData,
-            eventSummary,
-            eventDescription
+    private void setSupplementaryData(Long caseId, String orgId) {
+        Map<String, Map<String, Map<String, Object>>> supplementaryDataCivil = new HashMap<>();
+        String orgObj = "{" + orgId + ": 1}";
+        supplementaryDataCivil.put(
+            "supplementary_data_updates",
+            singletonMap("$set", singletonMap("orgs_assigned_users", orgObj))
         );
+        coreCaseDataService.setSupplementaryData(caseId, supplementaryDataCivil);
     }
 }
--- src/test/java/uk/gov/hmcts/reform/civil/handler/RetriggerCasesEventHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/RetriggerCasesEventHandlerTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.civil.handler;
 
-import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.camunda.bpm.client.task.ExternalTask;
@@ -12,29 +11,21 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
-import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
-import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
-import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
-import static java.util.Collections.emptyList;
-import static org.assertj.core.api.Assertions.assertThat;
+import static java.util.Collections.singletonMap;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UPDATE_CASE_DATA;
 
 @ExtendWith(MockitoExtension.class)
 class RetriggerCasesEventHandlerTest {
@@ -49,9 +40,6 @@ class RetriggerCasesEventHandlerTest {
 
     @Spy
     private ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
-
-    @Spy
-    private CaseDetailsConverter caseDetailsConverter = new CaseDetailsConverter(mapper);
 
     @InjectMocks
     private RetriggerCasesEventHandler handler;
@@ -139,7 +127,7 @@ class RetriggerCasesEventHandlerTest {
         ExternalTask externalTask = mock(ExternalTask.class);
         when(externalTask.getVariable("caseEvent")).thenReturn(null);
 
-        assertThrows(AssertionError.class, () -> handler.handleTask(externalTask));
+        assertThrows(IllegalArgumentException.class, () -> handler.handleTask(externalTask));
     }
 
     @Test
@@ -148,7 +136,7 @@ class RetriggerCasesEventHandlerTest {
         when(externalTask.getVariable("caseEvent")).thenReturn("CASE_EVENT");
         when(externalTask.getVariable("caseIds")).thenReturn(null);
 
-        assertThrows(AssertionError.class, () -> handler.handleTask(externalTask));
+        assertThrows(IllegalArgumentException.class, () -> handler.handleTask(externalTask));
     }
 
     @Test
@@ -208,81 +196,38 @@ class RetriggerCasesEventHandlerTest {
     }
 
     @Test
-    void testDocumentUpdateFinalOrders() {
+    void handleTask_RetriggerCasesForSuppementaryData() {
         ExternalTask externalTask = mock(ExternalTask.class);
-        CaseDetails caseDetails = CaseDetails.builder().data(Map.of("finalOrderDocumentCollection", List.of(Element.<CaseDocument>builder().id(
-            UUID.randomUUID()).value(CaseDocument.builder().documentSize(123L).build()).build()))).build();
-
-        when(externalTask.getVariable("caseEvent")).thenReturn("UPDATE_CASE_DATA");
-        when(externalTask.getVariable("caseIds")).thenReturn("1");
+        String orgId = "XXXXXX";
+        Map<String, Map<String, Map<String, Object>>> supplementaryDataCivil = new HashMap<>();
+        String orgObj = "{" + orgId + ": 1}";
+        supplementaryDataCivil.put(
+            "supplementary_data_updates",
+            singletonMap("$set", singletonMap("orgs_assigned_users", orgObj))
+        );
+        System.out.println(supplementaryDataCivil);
+        when(externalTask.getVariable("caseEvent")).thenReturn("RETRIGGER_CASES");
+        when(externalTask.getVariable("caseIds")).thenReturn(" 1, 2 ");
         when(externalTask.getVariable("caseData")).thenReturn(null);
-        when(externalTask.getVariable("document")).thenReturn("{\n" +
-                                                                  "      \"createdBy\" : \"Civil\",\n" +
-                                                                  "      \"documentLink\" : {\n" +
-                                                                  "        \"category_id\" : \"caseManagementOrders\",\n" +
-                                                                  "        \"document_url\" : \"url\",\n" +
-                                                                  "        \"upload_timestamp\" : \"2025-05-30T17:36:30.513000\",\n" +
-                                                                  "        \"document_filename\" : \"Order_30052025.pdf\",\n" +
-                                                                  "        \"document_binary_url\" : \"binary\"\n" +
-                                                                  "      },\n" +
-                                                                  "      \"documentName\" : \"Order_30052025.pdf\",\n" +
-                                                                  "      \"documentSize\" : 43927,\n" +
-                                                                  "      \"documentType\" : \"JUDGE_FINAL_ORDER\",\n" +
-                                                                  "      \"createdDatetime\" : \"2025-05-30T17:36:30.513000\"\n" +
-                                                                  "    }");
+        when(externalTask.getVariable("orgId")).thenReturn(orgId);
         when(externalTask.getProcessInstanceId()).thenReturn("1");
-        when(coreCaseDataService.getCase(any())).thenReturn(caseDetails);
 
         handler.handleTask(externalTask);
-
-        verify(coreCaseDataService).triggerEvent(eq(1L), eq(UPDATE_CASE_DATA), caseDataMap.capture(), any(), any());
-
-        JavaType listType = mapper.getTypeFactory()
-            .constructCollectionType(List.class,
-                                     mapper.getTypeFactory().constructParametricType(Element.class, CaseDocument.class));
-        List<Element<CaseDocument>> finalOrderDocumentCollection = caseDataMap.getValue().get("finalOrderDocumentCollection") != null
-            ? mapper.convertValue(caseDataMap.getValue().get("finalOrderDocumentCollection"), listType)
-            : emptyList();
-
-        assertThat(finalOrderDocumentCollection.get(1).getValue().getDocumentName()).isEqualTo("Order_30052025.pdf");
-    }
-
-    @Test
-    void testDocumentUpdateFinalOrders_CurrentCollectionEmpty() {
-        ExternalTask externalTask = mock(ExternalTask.class);
-        CaseDetails caseDetails = CaseDetails.builder().data(new HashMap<>()).build();
-
-        when(externalTask.getVariable("caseEvent")).thenReturn("UPDATE_CASE_DATA");
-        when(externalTask.getVariable("caseIds")).thenReturn("1");
-        when(externalTask.getVariable("caseData")).thenReturn(null);
-        when(externalTask.getVariable("document")).thenReturn("{\n" +
-                                                                  "      \"createdBy\" : \"Civil\",\n" +
-                                                                  "      \"documentLink\" : {\n" +
-                                                                  "        \"category_id\" : \"caseManagementOrders\",\n" +
-                                                                  "        \"document_url\" : \"url\",\n" +
-                                                                  "        \"upload_timestamp\" : \"2025-05-30T17:36:30.513000\",\n" +
-                                                                  "        \"document_filename\" : \"Order_30052025.pdf\",\n" +
-                                                                  "        \"document_binary_url\" : \"binary\"\n" +
-                                                                  "      },\n" +
-                                                                  "      \"documentName\" : \"Order_30052025.pdf\",\n" +
-                                                                  "      \"documentSize\" : 43927,\n" +
-                                                                  "      \"documentType\" : \"JUDGE_FINAL_ORDER\",\n" +
-                                                                  "      \"createdDatetime\" : \"2025-05-30T17:36:30.513000\"\n" +
-                                                                  "    }");
-        when(externalTask.getProcessInstanceId()).thenReturn("1");
-        when(coreCaseDataService.getCase(any())).thenReturn(caseDetails);
-
-        handler.handleTask(externalTask);
-
-        verify(coreCaseDataService).triggerEvent(eq(1L), eq(UPDATE_CASE_DATA), caseDataMap.capture(), any(), any());
-
-        JavaType listType = mapper.getTypeFactory()
-            .constructCollectionType(List.class,
-                                     mapper.getTypeFactory().constructParametricType(Element.class, CaseDocument.class));
-        List<Element<CaseDocument>> finalOrderDocumentCollection = caseDataMap.getValue().get("finalOrderDocumentCollection") != null
-            ? mapper.convertValue(caseDataMap.getValue().get("finalOrderDocumentCollection"), listType)
-            : emptyList();
-
-        assertThat(finalOrderDocumentCollection.get(0).getValue().getDocumentName()).isEqualTo("Order_30052025.pdf");
+        verify(coreCaseDataService).setSupplementaryData(1L, supplementaryDataCivil);
+        verify(coreCaseDataService).setSupplementaryData(2L, supplementaryDataCivil);
+        verify(coreCaseDataService, never()).triggerEvent(
+            eq(1L),
+            eq(CaseEvent.RETRIGGER_CASES),
+            anyMap(),
+            eq("Re-trigger of RETRIGGER_CASES"),
+            eq(EVENT_DESCRIPTION)
+        );
+        verify(coreCaseDataService, never()).triggerEvent(
+            eq(2L),
+            eq(CaseEvent.RETRIGGER_CASES),
+            anyMap(),
+            eq("Re-trigger of RETRIGGER_CASES"),
+            eq(EVENT_DESCRIPTION)
+        );
     }
 }
