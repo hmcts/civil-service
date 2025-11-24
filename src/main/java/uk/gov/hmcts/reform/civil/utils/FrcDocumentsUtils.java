04cddcff6563eca5a7cca42a4b1ced32873c85cf
Rakesh Chiluka <rakesh.chiluka2@hmcts.net>
2025-11-24 10:48:03 +0000

    dtscci- converted caseData builder to setter lombok

--- src/main/java/uk/gov/hmcts/reform/civil/utils/FrcDocumentsUtils.java
+++ src/main/java/uk/gov/hmcts/reform/civil/utils/FrcDocumentsUtils.java
@@ -32,7 +32,7 @@ public class FrcDocumentsUtils {
             .map(FixedRecoverableCosts::getFrcSupportingDocument).isPresent()) {
 
             Document respondent1FrcSupportingDocument = caseData
-                .getRespondent1DQ().getFixedRecoverableCostsIntermediate().getFrcSupportingDocument();
+                .getRespondent1DQ().getRespondent1DQFixedRecoverableCostsIntermediate().getFrcSupportingDocument();
 
             assignCategoryId.assignCategoryIdToDocument(respondent1FrcSupportingDocument,
                                                         DocCategory.DQ_DEF1.getValue());
@@ -43,7 +43,7 @@ public class FrcDocumentsUtils {
             .map(FixedRecoverableCosts::getFrcSupportingDocument).isPresent()) {
 
             Document respondent2FrcSupportingDocument = caseData
-                .getRespondent2DQ().getFixedRecoverableCostsIntermediate().getFrcSupportingDocument();
+                .getRespondent2DQ().getRespondent2DQFixedRecoverableCostsIntermediate().getFrcSupportingDocument();
 
             assignCategoryId.assignCategoryIdToDocument(respondent2FrcSupportingDocument,
                                                         DocCategory.DQ_DEF2.getValue());
