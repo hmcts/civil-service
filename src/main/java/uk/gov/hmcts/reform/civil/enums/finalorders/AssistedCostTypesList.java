fad4f708f89edd091b0fcd6f0fb5167b4b12defe
Shaun Francis <shaun.francis@hmcts.net>
2025-11-24 11:51:21 +0000

    Update enums for general application

--- src/main/java/uk/gov/hmcts/reform/civil/enums/finalorders/AssistedCostTypesList.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/finalorders/AssistedCostTypesList.java
@@ -1,9 +1,17 @@
 package uk.gov.hmcts.reform.civil.enums.finalorders;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum AssistedCostTypesList {
-    COSTS_IN_THE_CASE,
-    NO_ORDER_TO_COST,
-    COSTS_RESERVED,
-    MAKE_AN_ORDER_FOR_DETAILED_COSTS,
-    BESPOKE_COSTS_ORDER
+    COSTS_IN_CASE("Costs in the case"),
+    NO_ORDER_TO_COST("No order as to costs"),
+    COSTS_RESERVED("Costs reserved"),
+    MAKE_AN_ORDER_FOR_DETAILED_COSTS("Make an Order for detailed costs"),
+    BESPOKE_COSTS_ORDER("Bespoke costs order"),
+    REFUSED("refused");
+
+    private final String displayedValue;
 }
