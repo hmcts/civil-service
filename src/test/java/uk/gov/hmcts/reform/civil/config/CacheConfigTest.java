58586120538bf19796750e2efa5af4cf323139a5
mounikaammineni <Ayra@23082019>
2025-11-24 10:23:32 +0000

    Update

---
+++ src/test/java/uk/gov/hmcts/reform/civil/config/CacheConfigTest.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.civil.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class CacheConfigUnitTest {
+
+    @Test
+    void shouldCreateAllCaches() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CacheConfig.class);
+        CacheManager cacheManager = context.getBean(CacheManager.class);
+
+        assertNotNull(cacheManager.getCache("userInfoCache"));
+        assertNotNull(cacheManager.getCache("accessTokenCache"));
+        assertNotNull(cacheManager.getCache("courtVenueCache"));
+
+        context.close();
+    }
+}
