3b02164f62f5dcf67a3f24268c13d3c5d63c3f48
philip-milne <philip.milne@hmcts.net>
2025-11-25 10:08:25 +0000

    DTSCCI-3131 Predicate utility & annotation

---
+++ src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/predicate/annotations/BusinessRule.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.civil.service.flowstate.predicate.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface BusinessRule {
+    String summary();                   // Short readable name
+    String description();               // detailed explanation for BAs
+    String group() default "General";   // To group in the report (e.g., "Payment")
+}
