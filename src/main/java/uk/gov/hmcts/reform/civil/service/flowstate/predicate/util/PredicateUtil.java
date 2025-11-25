2a3eb8d89b3770fb140e41b38cd421292ed72948
philip-milne <philip.milne@hmcts.net>
2025-11-25 10:08:25 +0000

    DTSCCI-3131 Predicate utility & annotation

---
+++ src/main/java/uk/gov/hmcts/reform/civil/service/flowstate/predicate/util/PredicateUtil.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.civil.service.flowstate.predicate.util;
+
+import java.util.function.Predicate;
+
+public class PredicateUtil {
+
+    private PredicateUtil() {
+
+    }
+
+    public static <T> Predicate<T> nullSafe(Predicate<T> predicate) {
+        // A wrapper that returns false if the path throws NPE
+        return t -> {
+            try {
+                return predicate.test(t);
+            } catch (NullPointerException e) {
+                return false;
+            }
+        };
+    }
+}
