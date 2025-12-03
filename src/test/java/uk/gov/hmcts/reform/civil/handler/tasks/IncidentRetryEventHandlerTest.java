cee8d60e7023a6f1481e3feb6dcc01c14db24a30
mounikaammineni <mounika.ammineni1@hmcts.net>
2025-12-03 11:03:39 +0000

    Add completion

--- src/test/java/uk/gov/hmcts/reform/civil/handler/tasks/IncidentRetryEventHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/tasks/IncidentRetryEventHandlerTest.java
@@ -18,7 +18,13 @@ import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
