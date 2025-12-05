70d8dee07157b13e77a4ed05065896f39b9ca90c
philip-milne <philip.milne@hmcts.net>
2025-12-05 17:12:25 +0000

    DTSCCI-3423 Initial commit - replaced CaseData with DismissedPredicate & TakenOfflinePredicate for ClaimDetails Notified Time Ext Transition

--- src/main/java/uk/gov/hmcts/reform/civil/stateflow/transitions/ClaimDetailsNotifiedTimeExtensionTransitionBuilder.java
+++ src/main/java/uk/gov/hmcts/reform/civil/stateflow/transitions/ClaimDetailsNotifiedTimeExtensionTransitionBuilder.java
@@ -1,17 +1,15 @@
 package uk.gov.hmcts.reform.civil.stateflow.transitions;
 
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.service.flowstate.FlowState;
+import uk.gov.hmcts.reform.civil.service.flowstate.predicate.DismissedPredicate;
+import uk.gov.hmcts.reform.civil.service.flowstate.predicate.TakenOfflinePredicate;
 import uk.gov.hmcts.reform.civil.stateflow.model.Transition;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.function.Predicate;
 
 import static java.util.function.Predicate.not;
 import static uk.gov.hmcts.reform.civil.service.flowstate.FlowPredicate.allResponsesReceived;
@@ -44,37 +42,16 @@ public class ClaimDetailsNotifiedTimeExtensionTransitionBuilder extends MidTrans
             .moveTo(ALL_RESPONSES_RECEIVED, transitions).onlyWhen((respondentTimeExtension).and(allResponsesReceived), transitions)
             .moveTo(AWAITING_RESPONSES_FULL_DEFENCE_RECEIVED, transitions)
             .onlyWhen((awaitingResponsesFullDefenceReceived).and(respondentTimeExtension)
-                .and(not(caseDismissedAfterDetailNotifiedExtension)), transitions)
+                .and(not(DismissedPredicate.afterClaimNotifiedExtension)), transitions)
             .moveTo(AWAITING_RESPONSES_FULL_ADMIT_RECEIVED, transitions)
             .onlyWhen((awaitingResponsesFullAdmitReceived).and(respondentTimeExtension), transitions)
             .moveTo(AWAITING_RESPONSES_NOT_FULL_DEFENCE_OR_FULL_ADMIT_RECEIVED, transitions)
             .onlyWhen((awaitingResponsesNonFullDefenceOrFullAdmitReceived).and(respondentTimeExtension), transitions)
-            .moveTo(TAKEN_OFFLINE_BY_STAFF, transitions).onlyWhen(takenOfflineByStaffAfterClaimDetailsNotifiedExtension, transitions)
+            .moveTo(TAKEN_OFFLINE_BY_STAFF, transitions)
+            .onlyWhen(TakenOfflinePredicate.byStaff.and(TakenOfflinePredicate.afterClaimNotifiedExtension), transitions)
             .moveTo(PAST_CLAIM_DISMISSED_DEADLINE_AWAITING_CAMUNDA, transitions)
-            .onlyWhen(caseDismissedAfterDetailNotifiedExtension, transitions)
-            .moveTo(TAKEN_OFFLINE_SDO_NOT_DRAWN, transitions).onlyWhen(takenOfflineSDONotDrawnAfterClaimDetailsNotifiedExtension, transitions);
+            .onlyWhen(DismissedPredicate.afterClaimNotifiedExtension, transitions)
+            .moveTo(TAKEN_OFFLINE_SDO_NOT_DRAWN, transitions)
+            .onlyWhen(TakenOfflinePredicate.sdoNotDrawn.and(TakenOfflinePredicate.afterClaimNotifiedExtension), transitions);
     }
-
-    public static final Predicate<CaseData> takenOfflineSDONotDrawnAfterClaimDetailsNotifiedExtension = caseData ->
-        caseData.getReasonNotSuitableSDO() != null
-            && StringUtils.isNotBlank(caseData.getReasonNotSuitableSDO().getInput())
-            && caseData.getTakenOfflineDate() != null
-            && caseData.getRespondent1AcknowledgeNotificationDate() == null
-            && caseData.getRespondent1TimeExtensionDate() != null
-            && caseData.getRespondent1ResponseDate() == null;
-
-    public static final Predicate<CaseData> takenOfflineByStaffAfterClaimDetailsNotifiedExtension = caseData ->
-        caseData.getTakenOfflineByStaffDate() != null
-            && caseData.getRespondent1AcknowledgeNotificationDate() == null
-            && caseData.getRespondent1TimeExtensionDate() != null
-            && caseData.getRespondent1ResponseDate() == null;
-
-    public static final Predicate<CaseData> caseDismissedAfterDetailNotifiedExtension = caseData ->
-        caseData.getClaimDismissedDeadline().isBefore(LocalDateTime.now())
-            && ((caseData.getRespondent1AcknowledgeNotificationDate() == null
-            && caseData.getRespondent1TimeExtensionDate() != null)
-            || (caseData.getRespondent2AcknowledgeNotificationDate() == null
-            && caseData.getRespondent2TimeExtensionDate() != null))
-            && caseData.getRespondent1ClaimResponseIntentionType() == null
-            && caseData.getRespondent2ClaimResponseIntentionType() == null;
 }
