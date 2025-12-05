71c6d0af7d718908b2a8e0131eb93f1f8f3ba852
philip-milne <philip.milne@hmcts.net>
2025-12-05 17:16:10 +0000

    DTSCCI-3423 Redundant calls to FlowPredicate delegate removed for ClaimDetails Notified Time Ext Transition

--- src/main/java/uk/gov/hmcts/reform/civil/stateflow/transitions/ClaimDetailsNotifiedTimeExtensionTransitionBuilder.java
+++ src/main/java/uk/gov/hmcts/reform/civil/stateflow/transitions/ClaimDetailsNotifiedTimeExtensionTransitionBuilder.java
@@ -6,18 +6,13 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.service.flowstate.FlowState;
 import uk.gov.hmcts.reform.civil.service.flowstate.predicate.DismissedPredicate;
+import uk.gov.hmcts.reform.civil.service.flowstate.predicate.ResponsePredicate;
 import uk.gov.hmcts.reform.civil.service.flowstate.predicate.TakenOfflinePredicate;
 import uk.gov.hmcts.reform.civil.stateflow.model.Transition;
 
 import java.util.List;
 
 import static java.util.function.Predicate.not;
-import static uk.gov.hmcts.reform.civil.service.flowstate.FlowPredicate.allResponsesReceived;
-import static uk.gov.hmcts.reform.civil.service.flowstate.FlowPredicate.awaitingResponsesFullAdmitReceived;
-import static uk.gov.hmcts.reform.civil.service.flowstate.FlowPredicate.awaitingResponsesFullDefenceReceived;
-import static uk.gov.hmcts.reform.civil.service.flowstate.FlowPredicate.awaitingResponsesNonFullDefenceOrFullAdmitReceived;
-import static uk.gov.hmcts.reform.civil.service.flowstate.FlowPredicate.notificationAcknowledged;
-import static uk.gov.hmcts.reform.civil.service.flowstate.FlowPredicate.respondentTimeExtension;
 import static uk.gov.hmcts.reform.civil.service.flowstate.FlowState.Main.ALL_RESPONSES_RECEIVED;
 import static uk.gov.hmcts.reform.civil.service.flowstate.FlowState.Main.AWAITING_RESPONSES_FULL_ADMIT_RECEIVED;
 import static uk.gov.hmcts.reform.civil.service.flowstate.FlowState.Main.AWAITING_RESPONSES_FULL_DEFENCE_RECEIVED;
@@ -38,20 +33,35 @@ public class ClaimDetailsNotifiedTimeExtensionTransitionBuilder extends MidTrans
 
     @Override
     void setUpTransitions(List<Transition> transitions) {
-        this.moveTo(NOTIFICATION_ACKNOWLEDGED, transitions).onlyWhen(notificationAcknowledged, transitions)
-            .moveTo(ALL_RESPONSES_RECEIVED, transitions).onlyWhen((respondentTimeExtension).and(allResponsesReceived), transitions)
+        this.moveTo(NOTIFICATION_ACKNOWLEDGED, transitions)
+            .onlyWhen(ResponsePredicate.notificationAcknowledged, transitions)
+            .moveTo(ALL_RESPONSES_RECEIVED, transitions)
+            .onlyWhen(
+                (ResponsePredicate.respondentTimeExtension)
+                    .and(ResponsePredicate.allResponsesReceived), transitions
+            )
             .moveTo(AWAITING_RESPONSES_FULL_DEFENCE_RECEIVED, transitions)
-            .onlyWhen((awaitingResponsesFullDefenceReceived).and(respondentTimeExtension)
-                .and(not(DismissedPredicate.afterClaimNotifiedExtension)), transitions)
+            .onlyWhen(
+                (ResponsePredicate.awaitingResponsesFullDefenceReceived).and(ResponsePredicate.respondentTimeExtension)
+                    .and(not(DismissedPredicate.afterClaimNotifiedExtension)), transitions
+            )
             .moveTo(AWAITING_RESPONSES_FULL_ADMIT_RECEIVED, transitions)
-            .onlyWhen((awaitingResponsesFullAdmitReceived).and(respondentTimeExtension), transitions)
+            .onlyWhen(
+                (ResponsePredicate.awaitingResponsesFullAdmitReceived)
+                    .and(ResponsePredicate.respondentTimeExtension), transitions
+            )
             .moveTo(AWAITING_RESPONSES_NOT_FULL_DEFENCE_OR_FULL_ADMIT_RECEIVED, transitions)
-            .onlyWhen((awaitingResponsesNonFullDefenceOrFullAdmitReceived).and(respondentTimeExtension), transitions)
+            .onlyWhen(
+                (ResponsePredicate.awaitingResponsesNonFullDefenceOrFullAdmitReceived)
+                    .and(ResponsePredicate.respondentTimeExtension), transitions
+            )
             .moveTo(TAKEN_OFFLINE_BY_STAFF, transitions)
             .onlyWhen(TakenOfflinePredicate.byStaff.and(TakenOfflinePredicate.afterClaimNotifiedExtension), transitions)
             .moveTo(PAST_CLAIM_DISMISSED_DEADLINE_AWAITING_CAMUNDA, transitions)
             .onlyWhen(DismissedPredicate.afterClaimNotifiedExtension, transitions)
             .moveTo(TAKEN_OFFLINE_SDO_NOT_DRAWN, transitions)
-            .onlyWhen(TakenOfflinePredicate.sdoNotDrawn.and(TakenOfflinePredicate.afterClaimNotifiedExtension), transitions);
+            .onlyWhen(
+                TakenOfflinePredicate.sdoNotDrawn.and(TakenOfflinePredicate.afterClaimNotifiedExtension), transitions
+            );
     }
 }
