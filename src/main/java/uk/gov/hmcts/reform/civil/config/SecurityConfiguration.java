7a3f8de82c4c2491dac45d5768cafb11632e1e70
Ruban <ruban72@hotmail.com>
2025-12-10 09:28:43 +0000

    DTSCCI-3141: Fix Additional security flag issue (#7125)
    * DTSCCI-3141: Fix Additional security flag issue
    
    
    ---------
    
    Co-authored-by: stateflow-diagram-bot <civil-service@users.noreply.github.com>
    Co-authored-by: DaneF-hmcts <Dane.Faulconbridge@HMCTS.NET>

--- src/main/java/uk/gov/hmcts/reform/civil/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/SecurityConfiguration.java
@@ -36,9 +36,7 @@ public class SecurityConfiguration {
         "/loggers/**", "/assignment/**", "/service-request-update",
         "/service-request-update-claim-issued", "/case/document/downloadDocument/**",
         "/fees/claim/calculate-interest",
-        "/testing-support/flowstate",
-        "/testing-support/rpaJson",
-        "/testing-support/rpaJsonSpec"
+        "/testing-support/**"
     };
 
     private final RequestAuthorizer<User> userRequestAuthorizer;
