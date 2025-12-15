24e39fd0ceefe0033a465df9a60be3c75b6921fb
rishisharma <rishi.sharma@hmcts.net>
2025-12-15 19:33:26 +0000

    DTSCCI-3388: category preserve issue

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/ManageDocumentsHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/ManageDocumentsHandler.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.civil.handler.callback.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
@@ -9,6 +10,9 @@ import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.citizenui.ManageDocument;
+import uk.gov.hmcts.reform.civil.model.common.Element;
 
 import java.util.List;
 import java.util.Map;
@@ -17,6 +21,7 @@ import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.MANAGE_DOCUMENTS;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ManageDocumentsHandler extends CallbackHandler {
@@ -40,6 +45,22 @@ public class ManageDocumentsHandler extends CallbackHandler {
     }
 
     private CallbackResponse uploadManageDocuments(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        List<Element<ManageDocument>> manageDocumentsList = caseData.getManageDocumentsList();
+
+        if (manageDocumentsList != null) {
+            log.info("submit data callback called {}", caseData.getCcdCaseReference());
+            for (Element<ManageDocument> element : manageDocumentsList) {
+                ManageDocument manageDocument = element.getValue();
+                log.info("Document Name: {}", manageDocument.getDocumentName());
+                log.info("Document Type: {}", manageDocument.getDocumentType());
+                log.info("DocumentName: {}", manageDocument.getDocumentLink().getDocumentFileName());
+                log.info("DocumentURL: {}", manageDocument.getDocumentLink().getDocumentUrl());
+                log.info("DocumentBinary: {}", manageDocument.getDocumentLink().getDocumentBinaryUrl());
+                log.info("DocumentUploadedTimeStamp: {}", manageDocument.getDocumentLink().getUploadTimestamp());
+                log.info("DocumentCategoryID: {}", manageDocument.getDocumentLink().getCategoryID());
+            }
+        }
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(callbackParams.getCaseData().toMap(objectMapper))
             .build();
