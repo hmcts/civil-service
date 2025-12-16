1f8b2ae12b51d47f538098a5799e51131b29d54a
rishisharma <rishi.sharma@hmcts.net>
2025-12-16 14:31:16 +0000

    DTSCCI-3388: category preserve issue

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/ManageDocumentsHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/ManageDocumentsHandler.java
@@ -10,16 +10,19 @@ import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.documentmanagement.model.Document;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.citizenui.ManageDocument;
 import uk.gov.hmcts.reform.civil.model.common.Element;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.MANAGE_DOCUMENTS;
+import static uk.gov.hmcts.reform.civil.utils.ElementUtils.element;
 
 @Slf4j
 @Service
@@ -47,22 +50,46 @@ public class ManageDocumentsHandler extends CallbackHandler {
     private CallbackResponse uploadManageDocuments(CallbackParams callbackParams) {
         CaseData caseData = callbackParams.getCaseData();
         List<Element<ManageDocument>> manageDocumentsList = caseData.getManageDocumentsList();
-
-        if (manageDocumentsList != null) {
+        List<Element<ManageDocument>> manageDocumentsNewList = new ArrayList<>();
+        if (!manageDocumentsList.isEmpty()) {
             log.info("submit data callback called {}", caseData.getCcdCaseReference());
             for (Element<ManageDocument> element : manageDocumentsList) {
                 ManageDocument manageDocument = element.getValue();
-                log.info("Document Name: {}", manageDocument.getDocumentName());
-                log.info("Document Type: {}", manageDocument.getDocumentType());
-                log.info("DocumentName: {}", manageDocument.getDocumentLink().getDocumentFileName());
-                log.info("DocumentURL: {}", manageDocument.getDocumentLink().getDocumentUrl());
-                log.info("DocumentBinary: {}", manageDocument.getDocumentLink().getDocumentBinaryUrl());
-                log.info("DocumentUploadedTimeStamp: {}", manageDocument.getDocumentLink().getUploadTimestamp());
-                log.info("DocumentCategoryID: {}", manageDocument.getDocumentLink().getCategoryID());
+                ManageDocument manageDocumentNew = new ManageDocument();
+                manageDocumentNew.setDocumentLink(preserveCategoryId(callbackParams, manageDocument.getDocumentLink()));
+                manageDocumentNew.setDocumentName(manageDocument.getDocumentName());
+                manageDocumentNew.setDocumentType(manageDocument.getDocumentType());
+                manageDocumentNew.setDocumentTypeOther(manageDocument.getDocumentTypeOther());
+                manageDocumentNew.setCreatedDatetime(manageDocument.getCreatedDatetime());
+                manageDocumentsNewList.add(element(manageDocument));
             }
+            caseData.setManageDocuments(manageDocumentsNewList);
         }
+
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(callbackParams.getCaseData().toMap(objectMapper))
             .build();
+    }
+
+    private Document preserveCategoryId(CallbackParams callbackParams, Document documentLink) {
+        CaseData caseDataBefore = callbackParams.getCaseDataBefore();
+        if (caseDataBefore != null) {
+            List<Element<ManageDocument>> manageDocumentsListBefore = caseDataBefore.getManageDocumentsList();
+            if (!manageDocumentsListBefore.isEmpty()) {
+                for (Element<ManageDocument> element : manageDocumentsListBefore) {
+                    ManageDocument manageDocument = element.getValue();
+                    if (manageDocument.getDocumentLink().getDocumentBinaryUrl().equals(documentLink.getDocumentBinaryUrl())) {
+                        Document document = new Document();
+                        document.setDocumentFileName(documentLink.getDocumentFileName());
+                        document.setUploadTimestamp(documentLink.getUploadTimestamp());
+                        document.setDocumentUrl(documentLink.getDocumentUrl());
+                        document.setDocumentBinaryUrl(documentLink.getDocumentBinaryUrl());
+                        document.setCategoryID(documentLink.getCategoryID());
+                        return document;
+                    }
+                }
+            }
+        }
+        return  documentLink;
     }
 }
