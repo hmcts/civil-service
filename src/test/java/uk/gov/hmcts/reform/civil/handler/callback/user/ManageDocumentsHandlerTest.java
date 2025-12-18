77bf833760be02dd099912dd39c8a624e6fa3158
rishikrsharma <rishikrsharma@users.noreply.github.com>
2025-12-18 13:20:24 +0000

    DTSCCI-3388: category preserve issue (#7241)
    * DTSCCI-3388: category preserve issue
    
    * DTSCCI-3388: category preserve issue
    
    * DTSCCI-3388: category preserve issue
    
    * DTSCCI-3388: category preserve issue
    
    * DTSCCI-3388: category preserve issue
    
    * DTSCCI-3388: category preserve issue
    
    * DTSCCI-3388: category preserve issue
    
    * DTSCCI-3388: category preserve issue
    
    * DTSCCI-3388: category preserve issue
    
    * DTSCCI-3388: category preserve issue
    
    * DTSCCI-3388: ManageDocumentsHandler patch
    
    ---------
    
    Co-authored-by: pats-john <13101669+pats-john@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ManageDocumentsHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/ManageDocumentsHandlerTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.handler.callback.user;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,16 +23,19 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.MANAGE_DOCUMENTS;
 
 @ExtendWith(MockitoExtension.class)
-public class ManageDocumentsHandlerTest extends BaseCallbackHandlerTest {
+class ManageDocumentsHandlerTest extends BaseCallbackHandlerTest {
 
     private ManageDocumentsHandler handler;
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setup() {
-        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         handler = new ManageDocumentsHandler(objectMapper);
     }
@@ -52,17 +56,53 @@ public class ManageDocumentsHandlerTest extends BaseCallbackHandlerTest {
                 ManageDocument.builder()
                     .documentType(ManageDocumentType.N9A_PAPER_ADMISSION_FULL_OR_PART)
                     .documentName("defendant")
-                    .documentLink(Document.builder().build())
+                    .documentLink(Document.builder()
+                                      .documentUrl("http://test.com")
+                                      .documentBinaryUrl("http://test.com/binary")
+                                      .documentFileName("document")
+                                      .categoryID("ApplicantTestCategory")
+                                      .build())
                     .build()
             );
-            CaseData caseData = CaseDataBuilder.builder().build().toBuilder()
+
+            Element<ManageDocument> document2 = new Element<>(
+                UUID.fromString("5fc03087-d265-11e7-b8c6-83e29cd24f45"),
+                ManageDocument.builder()
+                    .documentType(ManageDocumentType.N9A_PAPER_ADMISSION_FULL_OR_PART)
+                    .documentName("defendant")
+                    .documentLink(Document.builder()
+                                      .documentUrl("http://test2.com")
+                                      .documentBinaryUrl("http://test2.com/binary")
+                                      .documentFileName("document2")
+                                      .build())
+                    .build()
+            );
+
+            CaseData caseDataBefore = CaseDataBuilder.builder().build().toBuilder()
                 .manageDocuments(List.of(document))
                 .build();
-            CallbackParams params = callbackParamsOf(caseData, CallbackType.ABOUT_TO_SUBMIT);
+
+            CaseData caseData = CaseDataBuilder.builder().build().toBuilder()
+                .manageDocuments(List.of(document, document2))
+                .build();
+            CallbackParams params = callbackParamsOf(caseData, caseDataBefore, CallbackType.ABOUT_TO_SUBMIT);
             //When
             var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+            List<Element<ManageDocument>> manageDocuments = convertToMap(response.getData().get("manageDocuments"));
+
+            assertEquals("http://test.com", manageDocuments.get(0).getValue().getDocumentLink().getDocumentUrl());
+            assertEquals("ApplicantTestCategory", manageDocuments.get(0).getValue().getDocumentLink().getCategoryID());
+
+            assertEquals("http://test2.com", manageDocuments.get(1).getValue().getDocumentLink().getDocumentUrl());
+            assertNull(manageDocuments.get(1).getValue().getDocumentLink().getCategoryID());
+
             //Then
             assertThat(response.getData()).extracting("manageDocuments").isNotNull();
         }
+    }
+
+    public List<Element<ManageDocument>> convertToMap(Object object) {
+        return objectMapper.convertValue(object, new TypeReference<>() {
+        });
     }
 }
