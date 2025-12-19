0c200643b4305a973bedb4e2e06414d9e88da1d8
Sabah Irfan <sabah.irfan@gmail.com>
2025-12-19 08:26:53 +0000

    Get Rid of Case Data Builder in Civil Service 17

--- src/main/java/uk/gov/hmcts/reform/civil/model/genapplication/CaseLocationCivil.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/genapplication/CaseLocationCivil.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
 @Builder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Accessors(chain = true)
 public class CaseLocationCivil {
 
     private String region;
