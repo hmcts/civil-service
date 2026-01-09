3ce33d7364740c00b0592f1f2ec68f970578dd43
Sabah u Din Irfan <sabah.irfan@gmail.com>
2026-01-09 11:59:17 +0000

    DTSCCI-3539 Remove @SuperBuilder, @Builder from civil.model package (#7298)

--- src/main/java/uk/gov/hmcts/reform/civil/model/bundle/Bundle.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/bundle/Bundle.java
@@ -3,14 +3,12 @@ package uk.gov.hmcts.reform.civil.model.bundle;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Value;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Value
-@Builder(toBuilder = true)
 @NoArgsConstructor(force = true)
 @Data
 public class Bundle {
