d47a633901481bcbfac55326ff88a49f23e048eb
manohar.nunna <manohar.nunna@hmcts.net>
2026-01-13 12:03:40 +0000

    DTSPO-29292: Fix AppInsightsConfiguration for ApplicationInsights 3.x API
    Remove usage of deprecated TelemetryConfiguration.getActive() which is not public in 3.x.
    The connection string should be configured via APPLICATIONINSIGHTS_CONNECTION_STRING
    environment variable which is the standard approach in ApplicationInsights 3.x.

--- src/main/java/uk/gov/hmcts/reform/civil/config/AppInsightsConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/AppInsightsConfiguration.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.civil.config;
 
 import com.microsoft.applicationinsights.TelemetryClient;
-import com.microsoft.applicationinsights.TelemetryConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +12,8 @@ public class AppInsightsConfiguration {
     public TelemetryClient telemetryClient(
         @Value("${appinsights-connection-string:}") String connectionString
     ) {
-        if (connectionString != null && !connectionString.isBlank()) {
-            TelemetryConfiguration.getActive().setConnectionString(connectionString);
-        }
+        // Note: Connection string is typically configured via APPLICATIONINSIGHTS_CONNECTION_STRING
+        // environment variable or applicationinsights.json config file in ApplicationInsights 3.x
         return new TelemetryClient();
     }
 }
