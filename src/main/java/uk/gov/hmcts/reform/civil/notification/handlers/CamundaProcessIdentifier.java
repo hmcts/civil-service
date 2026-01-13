8d921bbf3dc043bb115cbe4c53d13e43e6106c5e
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-01-13 17:24:10 +0000

    Cleaned-up CamundaProcessIdentifier

--- src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/CamundaProcessIdentifier.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/CamundaProcessIdentifier.java
@@ -59,5 +59,5 @@ public enum CamundaProcessIdentifier {
     BundleCreationNotify,
     CreateSDONotify,
     RaiseQueryNotifier,
-   OtherPartyQueryRaisedNotifier;
+    OtherPartyQueryRaisedNotifier;
 }
