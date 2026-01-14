03b9428ca6b50007046e0025726cbab8e570bcbe
Gajanan <gajanan.deshpande1@hmcts.net>
2026-01-14 16:35:56 +0000

    DTSCCI-3668 Dashboard Notification Handlers Refactor - hearing_fee_unpaid.bpmn

---
+++ src/main/java/uk/gov/hmcts/reform/civil/service/dashboardnotifications/hearingfeeunpaid/HearingFeeUnpaidDefendantNotificationService.java
@@ -0,0 +1,55 @@
+package uk.gov.hmcts.reform.civil.service.dashboardnotifications.hearingfeeunpaid;
+
+import static java.util.Objects.isNull;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_HEARING_FEE_UNPAID_DEFENDANT;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_HEARING_FEE_UNPAID_TRIAL_READY_DEFENDANT;
+
+import uk.gov.hmcts.reform.civil.helpers.sdo.SdoHelper;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.service.dashboardnotifications.DashboardNotificationsParamsMapper;
+import uk.gov.hmcts.reform.civil.service.dashboardnotifications.DashboardScenarioService;
+import uk.gov.hmcts.reform.dashboard.services.DashboardNotificationService;
+import uk.gov.hmcts.reform.dashboard.services.DashboardScenariosService;
+import uk.gov.hmcts.reform.dashboard.services.TaskListService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HearingFeeUnpaidDefendantNotificationService extends DashboardScenarioService {
+
+    private final DashboardNotificationService dashboardNotificationService;
+    private final TaskListService taskListService;
+
+    public HearingFeeUnpaidDefendantNotificationService(DashboardScenariosService dashboardScenariosService,
+                                                        DashboardNotificationService dashboardNotificationService,
+                                                        DashboardNotificationsParamsMapper mapper,
+                                                        TaskListService taskListService) {
+        super(dashboardScenariosService, mapper);
+        this.dashboardNotificationService = dashboardNotificationService;
+        this.taskListService = taskListService;
+    }
+
+    public void notifyHearingFeeUnpaid(CaseData caseData, String authToken) {
+        recordScenario(caseData, authToken);
+    }
+
+    @Override
+    public String getScenario(CaseData caseData) {
+        return isNull(caseData.getTrialReadyRespondent1())  && SdoHelper.isFastTrack(caseData)
+            ? SCENARIO_AAA6_HEARING_FEE_UNPAID_DEFENDANT.getScenario()
+            : SCENARIO_AAA6_HEARING_FEE_UNPAID_TRIAL_READY_DEFENDANT.getScenario();
+    }
+
+    @Override
+    public boolean shouldRecordScenario(CaseData caseData) {
+        return caseData.isRespondent1NotRepresented();
+    }
+
+    @Override
+    protected void beforeRecordScenario(CaseData caseData, String authToken) {
+        dashboardNotificationService.deleteByReferenceAndCitizenRole(
+            String.valueOf(caseData.getCcdCaseReference()),
+            DEFENDANT_ROLE
+        );
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/civil/service/dashboardnotifications/hearingfeeunpaid/HearingFeeUnpaidDefendantNotificationServiceTest.java
@@ -0,0 +1,125 @@
+package uk.gov.hmcts.reform.civil.service.dashboardnotifications.hearingfeeunpaid;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.enums.sdo.ClaimsTrack;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+import uk.gov.hmcts.reform.civil.service.dashboardnotifications.DashboardNotificationsParamsMapper;
+import uk.gov.hmcts.reform.dashboard.data.ScenarioRequestParams;
+import uk.gov.hmcts.reform.dashboard.services.DashboardNotificationService;
+import uk.gov.hmcts.reform.dashboard.services.DashboardScenariosService;
+
+import java.util.HashMap;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_HEARING_FEE_UNPAID_DEFENDANT;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_HEARING_FEE_UNPAID_TRIAL_READY_DEFENDANT;
+
+@ExtendWith(MockitoExtension.class)
+class HearingFeeUnpaidDefendantNotificationServiceTest {
+
+    public static final String CCD_REF = "1234";
+    private static final String AUTH_TOKEN = "BEARER";
+    public static final String DEFENDANT = "DEFENDANT";
+
+    @Mock
+    private DashboardScenariosService dashboardScenariosService;
+    @Mock
+    private DashboardNotificationService dashboardNotificationService;
+    @Mock
+    private DashboardNotificationsParamsMapper mapper;
+
+    @InjectMocks
+    private HearingFeeUnpaidDefendantNotificationService service;
+
+    @BeforeEach
+    void setup() {
+        when(mapper.mapCaseDataToParams(any())).thenReturn(new HashMap<>());
+    }
+
+    @Test
+    void shouldRecordFastTrackScenarioWhenTrialReadyRespondentIsNull() {
+        CaseData caseData = CaseDataBuilder.builder().build();
+        caseData.setRespondent1Represented(YesOrNo.NO);
+        caseData.setCcdCaseReference(1234L);
+        caseData.setDrawDirectionsOrderRequired(YesOrNo.NO);
+        caseData.setClaimsTrack(ClaimsTrack.fastTrack);
+        caseData.setTrialReadyRespondent1(null);
+
+        service.notifyHearingFeeUnpaid(caseData, AUTH_TOKEN);
+
+        verify(dashboardNotificationService).deleteByReferenceAndCitizenRole(CCD_REF, DEFENDANT);
+        verify(dashboardScenariosService).recordScenarios(
+            AUTH_TOKEN,
+            SCENARIO_AAA6_HEARING_FEE_UNPAID_DEFENDANT.getScenario(),
+            CCD_REF,
+            ScenarioRequestParams.builder().params(new HashMap<>()).build()
+        );
+    }
+
+    @Test
+    void shouldRecordTrialReadyScenarioWhenTrialReadyRespondentIsNotNull() {
+        CaseData caseData = CaseDataBuilder.builder().build();
+        caseData.setRespondent1Represented(YesOrNo.NO);
+        caseData.setCcdCaseReference(1234L);
+        caseData.setDrawDirectionsOrderRequired(YesOrNo.NO);
+        caseData.setClaimsTrack(ClaimsTrack.fastTrack);
+        caseData.setTrialReadyRespondent1(YesOrNo.YES);
+
+        service.notifyHearingFeeUnpaid(caseData, AUTH_TOKEN);
+
+        verify(dashboardNotificationService).deleteByReferenceAndCitizenRole(CCD_REF, DEFENDANT);
+        verify(dashboardScenariosService).recordScenarios(
+            AUTH_TOKEN,
+            SCENARIO_AAA6_HEARING_FEE_UNPAID_TRIAL_READY_DEFENDANT.getScenario(),
+            CCD_REF,
+            ScenarioRequestParams.builder().params(new HashMap<>()).build()
+        );
+    }
+
+    @Test
+    void shouldRecordTrialReadyScenarioWhenNotFastTrack() {
+        CaseData caseData = CaseDataBuilder.builder().build();
+        caseData.setRespondent1Represented(YesOrNo.NO);
+        caseData.setDrawDirectionsOrderRequired(YesOrNo.YES);
+        caseData.setDrawDirectionsOrderSmallClaims(YesOrNo.NO);
+        caseData.setTrialReadyRespondent1(null);
+        caseData.setCcdCaseReference(1234L);
+
+        service.notifyHearingFeeUnpaid(caseData, AUTH_TOKEN);
+
+        verify(dashboardNotificationService).deleteByReferenceAndCitizenRole(CCD_REF, DEFENDANT);
+        verify(dashboardScenariosService).recordScenarios(
+            AUTH_TOKEN,
+            SCENARIO_AAA6_HEARING_FEE_UNPAID_TRIAL_READY_DEFENDANT.getScenario(),
+            CCD_REF,
+            ScenarioRequestParams.builder().params(new HashMap<>()).build()
+        );
+    }
+
+    @Test
+    void shouldNotRecordScenarioWhenDefendantRepresented() {
+        CaseData caseData = CaseDataBuilder.builder().build();
+        caseData.setRespondent1Represented(YesOrNo.YES);
+        caseData.setCcdCaseReference(1234L);
+        caseData.setDrawDirectionsOrderRequired(YesOrNo.NO);
+        caseData.setClaimsTrack(ClaimsTrack.fastTrack);
+        caseData.setTrialReadyRespondent1(null);
+
+        service.notifyHearingFeeUnpaid(caseData, AUTH_TOKEN);
+
+        verify(dashboardNotificationService, never()).deleteByReferenceAndCitizenRole(any(), any());
+        verify(dashboardScenariosService, never()).recordScenarios(any(), any(), any(), any());
+    }
+}
+
+
