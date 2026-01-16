6dd81b06c107fd1c704a675daca36dbc85ab2929
Gajanan <gajanan.deshpande1@hmcts.net>
2026-01-16 10:48:17 +0000

    DTSCCI-3676 Dashboard Notification Handlers Refactor - mediation_successful.bpmn

---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/mediationsuccessful/MediationSuccessfulDashboardTaskTest.java
@@ -0,0 +1,53 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.mediationsuccessful;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardTaskContext;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.service.dashboardnotifications.mediationsuccessful.MediationSuccessfulClaimantDashboardService;
+import uk.gov.hmcts.reform.civil.service.dashboardnotifications.mediationsuccessful.MediationSuccessfulDefendantDashboardService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class MediationSuccessfulDashboardTaskTest {
+
+    private static final String AUTH_TOKEN = "Bearer token";
+    @Mock
+    private DashboardTaskContext context;
+    @Mock
+    private MediationSuccessfulClaimantDashboardService claimantDashboardService;
+    @Mock
+    private MediationSuccessfulDefendantDashboardService defendantDashboardService;
+
+    private final CaseData caseData = CaseData.builder().ccdCaseReference(1L).build();
+
+    @BeforeEach
+    void setupContext() {
+        when(context.caseData()).thenReturn(caseData);
+        when(context.authToken()).thenReturn(AUTH_TOKEN);
+    }
+
+    @Test
+    void claimantTaskShouldDelegateToDashboardService() {
+        MediationSuccessfulClaimantDashboardTask task = new MediationSuccessfulClaimantDashboardTask(claimantDashboardService);
+
+        task.execute(context);
+
+        verify(claimantDashboardService).notifyMediationSuccessful(caseData, AUTH_TOKEN);
+    }
+
+    @Test
+    void defendantTaskShouldDelegateToDashboardService() {
+        MediationSuccessfulDefendantDashboardTask task = new MediationSuccessfulDefendantDashboardTask(defendantDashboardService);
+
+        task.execute(context);
+
+        verify(defendantDashboardService).notifyMediationSuccessful(caseData, AUTH_TOKEN);
+    }
+}
