f64505614cb0b26d92599b05b6aa7a4fe8cc60bf
Gajanan <gajanan.deshpande1@hmcts.net>
2026-01-19 14:03:47 +0000

    DTSCCI-3676 Dashboard Notification Handlers Refactor - mediation_successful.bpmn

--- src/test/java/uk/gov/hmcts/reform/civil/service/dashboardnotifications/DashboardMapperTestConfiguration.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/dashboardnotifications/DashboardMapperTestConfiguration.java
@@ -16,7 +16,8 @@ import org.springframework.context.annotation.FilterType;
         @Filter(type = FilterType.REGEX,
             pattern = "uk\\.gov\\.hmcts\\.reform\\.civil\\.service\\.dashboardnotifications\\.evidenceuploaded.*"),
         @Filter(type = FilterType.REGEX,
-            pattern = "uk\\.gov\\.hmcts\\.reform\\.civil\\.service\\.dashboardnotifications\\.mediationsuccessful.*")
+            pattern = "uk\\.gov\\.hmcts\\.reform\\.civil\\.service\\.dashboardnotifications\\.mediationsuccessful.*"),
+        @Filter(type = FilterType.REGEX,
             pattern = "uk\\.gov\\.hmcts\\.reform\\.civil\\.service\\.dashboardnotifications\\.trailreadycheck.*")
     }
 )
