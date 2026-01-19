996eaa2ea3fe37d926ed442ce1438d37346a0c3e
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-01-19 11:19:53 +0000

    DTSCCI-3612 Dashboard Notification Handlers Refactor - defendant_response_deadline_check.bpmn

---
+++ src/main/java/uk/gov/hmcts/reform/civil/service/dashboardnotifications/defendantresponsedeadlinecheck/DefendantResponseDeadlineCheckClaimantDashboardService.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.civil.service.dashboardnotifications.defendantresponsedeadlinecheck;
+
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.service.dashboardnotifications.DashboardNotificationsParamsMapper;
+import uk.gov.hmcts.reform.civil.service.dashboardnotifications.DashboardScenarioService;
+import uk.gov.hmcts.reform.dashboard.services.DashboardScenariosService;
+
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_DEFENDANT_RESPONSE_DEADLINE_PASSED_CLAIMANT;
+
+public class DefendantResponseDeadlineCheckClaimantDashboardService extends DashboardScenarioService {
+
+    public DefendantResponseDeadlineCheckClaimantDashboardService(DashboardScenariosService dashboardScenariosService, DashboardNotificationsParamsMapper mapper) {
+        super(dashboardScenariosService, mapper);
+    }
+
+    public void notifyDefendantResponseDeadlineCheck(CaseData caseData, String authToken) {
+        recordScenario(caseData, authToken);
+    }
+
+    @Override
+    public String getScenario(CaseData caseData) {
+        return SCENARIO_AAA6_DEFENDANT_RESPONSE_DEADLINE_PASSED_CLAIMANT.getScenario();
+    }
+
+    @Override
+    public boolean shouldRecordScenario(CaseData caseData) {
+        return caseData.isApplicant1NotRepresented();
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendantresponsedeadlinecheck/DefendantResponseDeadlineCheckDashboardTaskTest.java
@@ -0,0 +1,56 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.defendantresponsedeadlinecheck;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardTaskContext;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+import uk.gov.hmcts.reform.civil.service.dashboardnotifications.defendantresponsedeadlinecheck.DefendantResponseDeadlineCheckClaimantDashboardService;
+import uk.gov.hmcts.reform.civil.service.dashboardnotifications.defendantresponsedeadlinecheck.DefendantResponseDeadlineCheckDefendantDashboardService;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DefendantResponseDeadlineCheckDashboardTaskTest {
+
+    private static final String AUTH_TOKEN = "Bearer token";
+
+    @Mock
+    private DefendantResponseDeadlineCheckClaimantDashboardService claimantDashboardService;
+    @Mock
+    private DefendantResponseDeadlineCheckDefendantDashboardService defendantDashboardService;
+    @Mock
+    private DashboardTaskContext context;
+
+    private final CaseData caseData = new CaseDataBuilder().caseReference(1L).build();
+
+    @BeforeEach
+    void setupContext() {
+        when(context.caseData()).thenReturn(caseData);
+        when(context.authToken()).thenReturn(AUTH_TOKEN);
+    }
+
+    @Test
+    void claimantTaskShouldDelegateToDashboardService() {
+        DefendantResponseDeadlineCheckClaimantDashboardTask task =
+            new DefendantResponseDeadlineCheckClaimantDashboardTask(claimantDashboardService);
+
+        task.execute(context);
+
+        verify(claimantDashboardService).notifyDefendantResponseDeadlineCheck(caseData, AUTH_TOKEN);
+    }
+
+    @Test
+    void defendantTaskShouldDelegateToDashboardService() {
+        DefendantResponseDeadlineCheckDefendantDashboardTask task =
+            new DefendantResponseDeadlineCheckDefendantDashboardTask(defendantDashboardService);
+
+        task.execute(context);
+
+        verify(defendantDashboardService).notifyDefendantResponseDeadlineCheck(caseData, AUTH_TOKEN);
+    }
+}
