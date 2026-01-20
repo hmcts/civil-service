002d6ce868bfa1e934beeebc4be11f0c2b7e3ee4
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-01-20 16:32:57 +0000

    Missing @Component annotation

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendantresponse/DefendantResponseDashboardTaskContributor.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/defendantresponse/DefendantResponseDashboardTaskContributor.java
@@ -1,8 +1,10 @@
 package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.defendantresponse;
 
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardTaskContributor;
 import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardTaskIds;
 
+@Component
 public class DefendantResponseDashboardTaskContributor extends DashboardTaskContributor {
 
     public DefendantResponseDashboardTaskContributor(DefendantResponseClaimantDashboardTask claimantTask,
