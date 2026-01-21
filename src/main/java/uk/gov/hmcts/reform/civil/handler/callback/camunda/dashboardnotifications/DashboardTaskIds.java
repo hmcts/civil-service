d36529b68f964b5d87e3e7478fe11b89d21a063e
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-01-21 11:44:51 +0000

    DTSCCI-3615 Dashboard Notification Handlers Refactor - discontinue_claim_claimant.bpmn, validate_discontinue_claim_claimant.bpmn, upload_translated_discontinuance_doc.bpmn

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardTaskIds.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardTaskIds.java
@@ -30,6 +30,7 @@ public final class DashboardTaskIds {
     public static final String HEARING_FEE_UNPAID = "GenerateDashboardNotificationsHearingFeeUnpaid";
     public static final String CITIZEN_HEARING_FEE_PAYMENT = "GenerateDashboardNotificationsCitizenHearingFeePayment";
     public static final String DEFENDANT_SIGN_SETTLEMENT_AGREEMENT = "GenerateDashboardNotificationsSignSettlementAgreement";
+    public static final String DISCONTINUE_CLAIM_CLAIMANT = "GenerateDashboardNotificationsDiscontinueClaimClaimant";
     public static final String HEARING_SCHEDULED_HMC = "GenerateDashboardNotificationsHearingScheduledHmc";
     public static final String HEARING_SCHEDULED = "GenerateDashboardNotificationsHearingScheduled";
     public static final String TRIAL_READY_CHECK = "GenerateDashboardNotificationsTrialReadyCheck";
