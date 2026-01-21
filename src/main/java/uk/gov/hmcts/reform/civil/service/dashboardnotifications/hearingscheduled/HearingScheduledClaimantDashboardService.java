6ae25445594c78c1f7ebc832d3bad6304b10df42
philip-milne <philip.milne@hmcts.net>
2026-01-21 08:35:14 +0000

    DTSCCI-3620 Refactor hearing scheduled services to use abstract base class for SonarQube

--- src/main/java/uk/gov/hmcts/reform/civil/service/dashboardnotifications/hearingscheduled/HearingScheduledClaimantDashboardService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/dashboardnotifications/hearingscheduled/HearingScheduledClaimantDashboardService.java
@@ -4,51 +4,26 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.enums.AllocatedTrack;
 import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.referencedata.model.LocationRefData;
 import uk.gov.hmcts.reform.civil.service.dashboardnotifications.DashboardNotificationsParamsMapper;
-import uk.gov.hmcts.reform.civil.service.dashboardnotifications.DashboardScenarioService;
 import uk.gov.hmcts.reform.civil.service.referencedata.LocationReferenceDataService;
 import uk.gov.hmcts.reform.civil.utils.CourtLocationUtils;
 import uk.gov.hmcts.reform.dashboard.services.DashboardScenariosService;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static java.util.Objects.isNull;
-import static java.util.Objects.nonNull;
 import static uk.gov.hmcts.reform.civil.enums.CaseState.HEARING_READINESS;
 import static uk.gov.hmcts.reform.civil.enums.hearing.ListingOrRelisting.LISTING;
 
 @Service("hearingScheduledClaimantDashboardService")
-public class HearingScheduledClaimantDashboardService extends DashboardScenarioService {
-
-    private final LocationReferenceDataService locationRefDataService;
-    private final CourtLocationUtils courtLocationUtils;
+public class HearingScheduledClaimantDashboardService extends AbstractHearingScheduledDashboardService {
 
     public HearingScheduledClaimantDashboardService(DashboardScenariosService dashboardScenariosService,
                                                     DashboardNotificationsParamsMapper mapper,
                                                     LocationReferenceDataService locationRefDataService,
                                                     CourtLocationUtils courtLocationUtils) {
-        super(dashboardScenariosService, mapper);
-        this.locationRefDataService = locationRefDataService;
-        this.courtLocationUtils = courtLocationUtils;
-    }
-
-    public void notifyHearingScheduled(CaseData caseData, String authToken) {
-        populateCourtName(caseData, authToken);
-        recordScenario(caseData, authToken);
-    }
-
-    private void populateCourtName(CaseData caseData, String authToken) {
-        List<LocationRefData> locations = locationRefDataService.getHearingCourtLocations(authToken);
-        LocationRefData locationRefData = courtLocationUtils.fillPreferredLocationData(
-            locations,
-            caseData.getHearingLocation()
-        );
-        if (nonNull(locationRefData)) {
-            caseData.setHearingLocationCourtName(locationRefData.getSiteName());
-        }
+        super(dashboardScenariosService, mapper, locationRefDataService, courtLocationUtils);
     }
 
     @Override
