0cf49a9392ade63b02183e8147e72928d709f20f
AbuSalam22 <136581242+AbuSalam22@users.noreply.github.com>
2026-01-22 14:41:40 +0000

    DTSCCI-3601 fix

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimantresponse/ClaimantResponseCuiDashboardTaskContributor.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimantresponse/ClaimantResponseCuiDashboardTaskContributor.java
@@ -3,32 +3,16 @@ package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotification
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardTaskContributor;
 import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardTaskIds;
-import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.caseproceedsoffline.ApplicationsProceedOfflineClaimantDashboardTask;
-import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.caseproceedsoffline.ApplicationsProceedOfflineDefendantDashboardTask;
 
 @Component
 public class ClaimantResponseCuiDashboardTaskContributor extends DashboardTaskContributor {
 
     public ClaimantResponseCuiDashboardTaskContributor(
-        ClaimantResponseClaimantDashboardTask claimantTask,
-        ClaimantResponseDefendantDashboardTask defendantTask,
-        ClaimantCcjResponseClaimantDashboardTask claimantCcjTask,
-        ClaimantCcjResponseDefendantDashboardTask defendantCcjTask,
-        ApplicationsProceedOfflineClaimantDashboardTask claimantOfflineTask,
-        ApplicationsProceedOfflineDefendantDashboardTask defendantOfflineTask,
-        JudgmentByAdmissionIssuedClaimantDashboardTask judgmentByAdmissionClaimantTask,
-        JudgmentByAdmissionIssuedDefendantDashboardTask judgmentByAdmissionDefendantTask
+        ClaimantResponseCuiDashboardNotificationsTask notificationsTask
     ) {
         super(
             DashboardTaskIds.CLAIMANT_RESPONSE_CUI,
-            claimantTask,
-            defendantTask,
-            claimantOfflineTask,
-            defendantOfflineTask,
-            claimantCcjTask,
-            defendantCcjTask,
-            judgmentByAdmissionClaimantTask,
-            judgmentByAdmissionDefendantTask
+            notificationsTask
         );
     }
 }
--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimantresponse/ClaimantResponseCuiDashboardTaskContributorTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/claimantresponse/ClaimantResponseCuiDashboardTaskContributorTest.java
@@ -6,8 +6,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardTaskIds;
 import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardWorkflowTask;
-import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.caseproceedsoffline.ApplicationsProceedOfflineClaimantDashboardTask;
-import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.caseproceedsoffline.ApplicationsProceedOfflineDefendantDashboardTask;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -16,50 +14,20 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ClaimantResponseCuiDashboardTaskContributorTest {
 
     @Mock
-    private ClaimantResponseClaimantDashboardTask claimantTask;
-    @Mock
-    private ClaimantResponseDefendantDashboardTask defendantTask;
-    @Mock
-    private ClaimantCcjResponseClaimantDashboardTask claimantCcjTask;
-    @Mock
-    private ClaimantCcjResponseDefendantDashboardTask defendantCcjTask;
-    @Mock
-    private ApplicationsProceedOfflineClaimantDashboardTask claimantOfflineTask;
-    @Mock
-    private ApplicationsProceedOfflineDefendantDashboardTask defendantOfflineTask;
-    @Mock
-    private JudgmentByAdmissionIssuedClaimantDashboardTask judgmentByAdmissionClaimantTask;
-    @Mock
-    private JudgmentByAdmissionIssuedDefendantDashboardTask judgmentByAdmissionDefendantTask;
+    private ClaimantResponseCuiDashboardNotificationsTask notificationsTask;
 
     @Test
     void shouldExposeTaskIdAndDashboardTasks() {
         ClaimantResponseCuiDashboardTaskContributor contributor = new ClaimantResponseCuiDashboardTaskContributor(
-            claimantTask,
-            defendantTask,
-            claimantCcjTask,
-            defendantCcjTask,
-            claimantOfflineTask,
-            defendantOfflineTask,
-            judgmentByAdmissionClaimantTask,
-            judgmentByAdmissionDefendantTask
+            notificationsTask
         );
 
         assertThat(contributor.taskId()).isEqualTo(DashboardTaskIds.CLAIMANT_RESPONSE_CUI);
-        assertThat(contributor.dashboardTasks()).containsExactly(
-            claimantTask,
-            defendantTask,
-            claimantOfflineTask,
-            defendantOfflineTask,
-            claimantCcjTask,
-            defendantCcjTask,
-            judgmentByAdmissionClaimantTask,
-            judgmentByAdmissionDefendantTask
-        );
+        assertThat(contributor.dashboardTasks()).containsExactly(notificationsTask);
         assertListIsImmutable(contributor.dashboardTasks());
     }
 
     private void assertListIsImmutable(java.util.List<DashboardWorkflowTask> tasks) {
-        assertThatThrownBy(() -> tasks.add(claimantTask)).isInstanceOf(UnsupportedOperationException.class);
+        assertThatThrownBy(() -> tasks.add(notificationsTask)).isInstanceOf(UnsupportedOperationException.class);
     }
 }
