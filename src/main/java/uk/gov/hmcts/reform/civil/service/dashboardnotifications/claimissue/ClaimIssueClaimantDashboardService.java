26063af71e83197cbef8402446fcc6a73b6be352
philip-milne <philip.milne@hmcts.net>
2026-01-22 17:22:03 +0000

    DTSCCI-3768: Claim Issue Claimant and Defendant

--- src/main/java/uk/gov/hmcts/reform/civil/service/dashboardnotifications/claimissue/ClaimIssueClaimantDashboardService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/dashboardnotifications/claimissue/ClaimIssueClaimantDashboardService.java
@@ -1,29 +1,25 @@
 package uk.gov.hmcts.reform.civil.service.dashboardnotifications.claimissue;
 
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.service.dashboardnotifications.DashboardNotificationsParamsMapper;
 import uk.gov.hmcts.reform.civil.service.dashboardnotifications.DashboardScenarioService;
-import uk.gov.hmcts.reform.dashboard.services.DashboardNotificationService;
 import uk.gov.hmcts.reform.dashboard.services.DashboardScenariosService;
-import uk.gov.hmcts.reform.dashboard.services.TaskListService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ClaimIssueClaimantDashboardService extends DashboardScenarioService {
 
-    private final DashboardNotificationService dashboardNotificationService;
-    private final TaskListService taskListService;
     private final FeatureToggleService featureToggleService;
 
     public ClaimIssueClaimantDashboardService(DashboardScenariosService dashboardScenariosService,
                                                DashboardNotificationsParamsMapper mapper,
-                                               FeatureToggleService featureToggleService,
-                                               DashboardNotificationService dashboardNotificationService,
-                                               TaskListService taskListService) {
+                                               FeatureToggleService featureToggleService) {
         super(dashboardScenariosService, mapper);
-        this.dashboardNotificationService = dashboardNotificationService;
-        this.taskListService = taskListService;
         this.featureToggleService = featureToggleService;
     }
 
@@ -33,7 +29,28 @@ public class ClaimIssueClaimantDashboardService extends DashboardScenarioService
 
 
     @Override
+    protected boolean shouldRecordScenario(CaseData caseData) {
+        return featureToggleService.isLipVLipEnabled();
+    }
+
+    @Override
     protected String getScenario(CaseData caseData) {
-        return "";
+        return DashboardScenarios.SCENARIO_AAA6_CLAIM_ISSUE_RESPONSE_AWAIT.getScenario();
+    }
+
+    @Override
+    protected Map<String, Boolean> getScenarios(CaseData caseData) {
+        Map<String, Boolean> scenarios = new HashMap<>();
+        if(featureToggleService.isLipVLipEnabled()) {
+            if (featureToggleService.isLipQueryManagementEnabled(caseData)) {
+                scenarios.put(DashboardScenarios.SCENARIO_AAA6_APPLICATIONS_TO_THE_COURT.getScenario(), true);
+                scenarios.put(DashboardScenarios.SCENARIO_AAA6_MESSAGES_TO_THE_COURT.getScenario(), true);
+            }
+
+            if (caseData.isHWFTypeClaimIssued() && caseData.claimIssueFullRemissionNotGrantedHWF()) {
+                scenarios.put(DashboardScenarios.SCENARIO_AAA6_CLAIM_ISSUE_HWF_PHONE_PAYMENT.getScenario(), true);
+            }
+        }
+        return scenarios;
     }
 }
--- src/main/java/uk/gov/hmcts/reform/civil/service/dashboardnotifications/claimissue/ClaimIssueDefendantDashboardService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/dashboardnotifications/claimissue/ClaimIssueDefendantDashboardService.java
@@ -1,29 +1,26 @@
 package uk.gov.hmcts.reform.civil.service.dashboardnotifications.claimissue;
 
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.civil.enums.AllocatedTrack;
+import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.service.dashboardnotifications.DashboardNotificationsParamsMapper;
 import uk.gov.hmcts.reform.civil.service.dashboardnotifications.DashboardScenarioService;
-import uk.gov.hmcts.reform.dashboard.services.DashboardNotificationService;
 import uk.gov.hmcts.reform.dashboard.services.DashboardScenariosService;
-import uk.gov.hmcts.reform.dashboard.services.TaskListService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ClaimIssueDefendantDashboardService extends DashboardScenarioService {
 
-    private final DashboardNotificationService dashboardNotificationService;
-    private final TaskListService taskListService;
     private final FeatureToggleService featureToggleService;
 
     public ClaimIssueDefendantDashboardService(DashboardScenariosService dashboardScenariosService,
                                                 DashboardNotificationsParamsMapper mapper,
-                                                FeatureToggleService featureToggleService,
-                                                DashboardNotificationService dashboardNotificationService,
-                                                TaskListService taskListService) {
+                                                FeatureToggleService featureToggleService) {
         super(dashboardScenariosService, mapper);
-        this.dashboardNotificationService = dashboardNotificationService;
-        this.taskListService = taskListService;
         this.featureToggleService = featureToggleService;
     }
 
@@ -31,9 +28,31 @@ public class ClaimIssueDefendantDashboardService extends DashboardScenarioServic
         recordScenario(caseData, authToken);
     }
 
-
     @Override
     protected String getScenario(CaseData caseData) {
-        return "";
+        return null;
     }
+
+    @Override
+    protected Map<String, Boolean> getScenarios(CaseData caseData) {
+        Map<String, Boolean> scenarios = new HashMap<>();
+        if (featureToggleService.isLipVLipEnabled()) {
+            boolean isUnrepresented = caseData.isRespondent1NotRepresented();
+            if (isUnrepresented) {
+                scenarios.put(DashboardScenarios.SCENARIO_AAA6_CLAIM_ISSUE_RESPONSE_REQUIRED.getScenario(), true);
+
+                AllocatedTrack allocatedTrack = AllocatedTrack.getAllocatedTrack(caseData.getTotalClaimAmount(), null, null);
+                if (AllocatedTrack.FAST_CLAIM.equals(allocatedTrack)) {
+                    scenarios.put(DashboardScenarios.SCENARIO_AAA6_CP_CLAIM_ISSUE_FAST_TRACK_DEFENDANT.getScenario(), true);
+                }
+            }
+
+            if (featureToggleService.isLipQueryManagementEnabled(caseData)) {
+                scenarios.put(DashboardScenarios.SCENARIO_AAA6_APPLICATIONS_TO_THE_COURT.getScenario(), true);
+                scenarios.put(DashboardScenarios.SCENARIO_AAA6_MESSAGES_TO_THE_COURT.getScenario(), true);
+            }
+        }
+        return scenarios;
+    }
+
 }
