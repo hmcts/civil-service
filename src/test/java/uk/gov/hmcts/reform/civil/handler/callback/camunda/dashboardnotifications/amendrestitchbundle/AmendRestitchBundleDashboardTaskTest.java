f522bd6fdfd66104bbe9732c1b6fa1d507d33640
Ruban <ruban72@hotmail.com>
2026-02-02 09:22:12 +0000

    DTSCCI-3589: Amend restitch bundle dashboard refactor (#7275)
    * DTSCCI-3589: Amend restitch bundle, bundle creation, case settled dashboard refactor
    
    ---------
    
    Co-authored-by: krishnanuthalapati <32389208+krishnanuthalapati@users.noreply.github.com>
    Co-authored-by: pats-john <13101669+pats-john@users.noreply.github.com>

---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/amendrestitchbundle/AmendRestitchBundleDashboardTaskTest.java
@@ -0,0 +1,53 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.amendrestitchbundle;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardTaskContext;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.service.dashboardnotifications.amendrestitchbundle.AmendRestitchBundleClaimantDashboardService;
+import uk.gov.hmcts.reform.civil.service.dashboardnotifications.amendrestitchbundle.AmendRestitchBundleDefendantDashboardService;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AmendRestitchBundleDashboardTaskTest {
+
+    private static final String AUTH_TOKEN = "Bearer token";
+
+    @Mock
+    private AmendRestitchBundleClaimantDashboardService claimantDashboardService;
+    @Mock
+    private AmendRestitchBundleDefendantDashboardService defendantDashboardService;
+    @Mock
+    private DashboardTaskContext context;
+
+    private final CaseData caseData = CaseData.builder().ccdCaseReference(1L).build();
+
+    @BeforeEach
+    void setupContext() {
+        when(context.caseData()).thenReturn(caseData);
+        when(context.authToken()).thenReturn(AUTH_TOKEN);
+    }
+
+    @Test
+    void claimantTaskShouldDelegateToDashboardService() {
+        AmendRestitchBundleClaimantDashboardTask task = new AmendRestitchBundleClaimantDashboardTask(claimantDashboardService);
+
+        task.execute(context);
+
+        verify(claimantDashboardService).notifyBundleUpdated(caseData, AUTH_TOKEN);
+    }
+
+    @Test
+    void defendantTaskShouldDelegateToDashboardService() {
+        AmendRestitchBundleDefendantDashboardTask task = new AmendRestitchBundleDefendantDashboardTask(defendantDashboardService);
+
+        task.execute(context);
+
+        verify(defendantDashboardService).notifyBundleUpdated(caseData, AUTH_TOKEN);
+    }
+}
