7ca0ac85c3fc91d8a2d99495c80a8e9ec2b9a65f
philip-milne <philip.milne@hmcts.net>
2026-02-06 18:50:31 +0000

    DTSCCI-3751 initiate_general_application.bpmn & DTSCCI-3769 upload_translated_document_free_fee_application.bpmn
    # Conflicts:
    #	src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardTaskIds.java
    
    # Conflicts:
    #	src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardTaskIds.java

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardTaskIds.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardTaskIds.java
@@ -26,4 +26,5 @@ public final class DashboardTaskIds {
     public static final String HEARING_FEE_UNPAID = "GenerateDashboardNotificationsHearingFeeUnpaid";
     public static final String HEARING_SCHEDULED_HMC = "GenerateDashboardNotificationsHearingScheduledHmc";
     public static final String HEARING_SCHEDULED = "GenerateDashboardNotificationsHearingScheduled";
+    public static final String GA_APPLICATION_ISSUED = "GenerateDashboardNotificationsGaApplicationIssued";
 }
