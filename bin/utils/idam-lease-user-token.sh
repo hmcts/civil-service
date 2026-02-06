b4c74bfbff34f3cceff8cfa7a044eb4f53bac0e6
Mike Lemos <michael.lemos1@hmcts.net>
2026-02-06 18:11:19 +0000

    DTSCCI-3660 adding config to use HMC in preview

--- bin/utils/idam-lease-user-token.sh
+++ bin/utils/idam-lease-user-token.sh
@@ -8,7 +8,7 @@ password=${2}
 IDAM_API_URL=${IDAM_API_BASE_URL:-http://localhost:5000}
 IDAM_URL=${IDAM_STUB_LOCALHOST:-$IDAM_API_URL}
 
-clientSecret=${CCD_API_GATEWAY_OAUTH2_CLIENT_SECRET:-ccd_gateway_secret}
+clientSecret=${CCD_API_GATEWAY_IDAM_CLIENT_SECRET:-ccd_gateway_secret}
 redirectUri=${CCD_IDAM_REDIRECT_URL:-http://localhost:3451/oauth2redirect}
 if [ -z "$IDAM_STUB_LOCALHOST" ]; then
   code=$(curl --insecure --fail --show-error --silent -X POST --user "${username}:${password}" "${IDAM_URL}/oauth2/authorize?redirect_uri=${redirectUri}&response_type=code&client_id=ccd_gateway" -d "" | docker run --rm --interactive ghcr.io/jqlang/jq:latest -r .code)
--- bin/variables/load-dev-user-preview-environment-variables.sh
+++ bin/variables/load-dev-user-preview-environment-variables.sh
@@ -18,7 +18,7 @@ export CCD_DEFINITION_STORE_API_BASE_URL=$CCD_DEFINITION_STORE_URL
 export CAMUNDA_BASE_URL=$CAMUNDA_URL
 export CCD_CONFIGURER_IMPORTER_USERNAME=$(az keyvault secret show --vault-name civil-aat --name ccd-importer-username --query value -o tsv)
 export CCD_CONFIGURER_IMPORTER_PASSWORD=$(az keyvault secret show --vault-name civil-aat --name ccd-importer-password --query value -o tsv)
-export CCD_API_GATEWAY_OAUTH2_CLIENT_SECRET=$(az keyvault secret show --vault-name ccd-aat --name ccd-api-gateway-oauth2-client-secret --query value -o tsv)
+export CCD_API_GATEWAY_IDAM_CLIENT_SECRET=$(az keyvault secret show --vault-name ccd-aat --name ccd-api-gateway-oauth2-client-secret --query value -o tsv)
 export CCD_API_GATEWAY_S2S_SECRET=$(az keyvault secret show --vault-name s2s-aat --name microservicekey-ccd-gw --query value -o tsv)
 export S2S_SECRET=$(az keyvault secret show --vault-name civil-aat --name microservicekey-civil-service --query value -o tsv)
 # definition placeholders
