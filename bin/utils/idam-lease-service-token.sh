73a48524cd9980f3d17f1cc218c7375999acbea0
Mike Lemos <michael.lemos1@hmcts.net>
2026-02-06 20:07:00 +0000

    DTSCCI-3660 adding config to use HMC in preview

--- bin/utils/idam-lease-service-token.sh
+++ bin/utils/idam-lease-service-token.sh
@@ -5,8 +5,6 @@ set -eu
 microservice=${1}
 oneTimePassword=${2}
 
-echo "/n/ndebugging!!!!! SERVICE_AUTH_PROVIDER_API_BASE_URL- ${SERVICE_AUTH_PROVIDER_API_BASE_URL}"
-
 curl --insecure --fail --show-error --silent -X POST \
   ${SERVICE_AUTH_PROVIDER_API_BASE_URL:-http://localhost:4502}/lease \
   -H "Content-Type: application/json" \
