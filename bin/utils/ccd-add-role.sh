06ed1e62cfa91a8206d14ff63719f79d1426e742
Mike Lemos <michael.lemos1@hmcts.net>
2026-02-06 18:09:54 +0000

    DTSCCI-3660 adding config to use HMC in preview

--- bin/utils/ccd-add-role.sh
+++ bin/utils/ccd-add-role.sh
@@ -6,10 +6,17 @@ dir=$(dirname ${0})
 
 role=${1}
 
-userToken=$(${dir}/idam-lease-user-token.sh ${CCD_CONFIGURER_IMPORTER_USERNAME:-ccd.docker.default@hmcts.net} ${CCD_CONFIGURER_IMPORTER_PASSWORD:-Password12!})
-serviceToken=$(${dir}/idam-lease-service-token.sh ccd_gw $(docker run --rm hmctspublic.azurecr.io/imported/toolbelt/oathtool --totp -b ${CCD_API_GATEWAY_S2S_SECRET:-AAAAAAAAAAAAAAAC}))
+echo "/n/ndebugging!!!!! CCD_CONFIGURER_IMPORTER_USERNAME- ${CCD_CONFIGURER_IMPORTER_USERNAME}"
+echo "/n/ndebugging!!!!! CCD_CONFIGURER_IMPORTER_PASSWORD- ${CCD_CONFIGURER_IMPORTER_PASSWORD}"
 
-echo "Creating CCD role: ${role}"
+userToken=$(${dir}/idam-lease-user-token.sh ${CCD_CONFIGURER_IMPORTER_USERNAME:-ccd.docker.default@hmcts.net} ${CCD_CONFIGURER_IMPORTER_PASSWORD:-Password12!})
+echo "/n/ndebugging!!!!! userToken- ${userToken}"
+echo "/n/CCD_API_GATEWAY_S2S_SECRET!!!!! userToken- ${CCD_API_GATEWAY_S2S_SECRET}"
+
+serviceToken=$(${dir}/idam-lease-service-token.sh ccd_gw $(docker run --rm hmctspublic.azurecr.io/imported/toolbelt/oathtool --totp -b ${CCD_API_GATEWAY_S2S_SECRET:-AAAAAAAAAAAAAAAC}))
+echo "/n/ndebugging!!!!! serviceToken- ${serviceToken}"
+
+echo "Creating CCD role: ${role} using ${CCD_DEFINITION_STORE_API_BASE_URL}"
 
 curl --insecure --fail --show-error --silent --output /dev/null -X PUT \
   ${CCD_DEFINITION_STORE_API_BASE_URL:-http://localhost:4451}/api/user-role \
