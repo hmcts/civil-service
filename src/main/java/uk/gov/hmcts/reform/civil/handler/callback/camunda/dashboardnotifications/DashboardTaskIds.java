63605d54f180bf0c9ba1af97293f1815abe11adb
philip-milne <philip.milne@hmcts.net>
2026-02-06 07:51:51 +0000

    DTSCCI-3620 HEARING_SCHEDULED typo

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardTaskIds.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardTaskIds.java
@@ -25,5 +25,5 @@ public final class DashboardTaskIds {
     public static final String CLAIM_ISSUE = "GenerateDashboardNotificationsClaimIssue";
     public static final String HEARING_FEE_UNPAID = "GenerateDashboardNotificationsHearingFeeUnpaid";
     public static final String HEARING_SCHEDULED_HMC = "GenerateDashboardNotificationsHearingScheduledHmc";
-    public static final String HEARING_SCHEDULED = "GenerateDashboardNotificationHearingScheduled";
+    public static final String HEARING_SCHEDULED = "GenerateDashboardNotificationsHearingScheduled";
 }
