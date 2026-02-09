654eeb13847003a2f1a195f86325b8000b9016a3
Sumit Bagga <sumit.bagga2@hmcts.net>
2026-02-09 16:02:03 +0000

    ppi future date fix

--- src/main/java/uk/gov/hmcts/reform/civil/model/sdo/PPI.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/sdo/PPI.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
+import jakarta.validation.constraints.Future;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PPI {
 
+    @Future(message = "The date entered must be in the future")
     private LocalDate ppiDate;
     private String text;
 }
