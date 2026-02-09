4d1bc81af7cdaaaad7f6cda1da06bc933b27fe02
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2026-02-09 14:21:16 +0000

    Update NotifyClaimAppSolEmailDTOGenerator.java

--- src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/notifyclaimandclaimdetails/notifyunspecclaim/NotifyClaimAppSolEmailDTOGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/notifyclaimandclaimdetails/notifyunspecclaim/NotifyClaimAppSolEmailDTOGenerator.java
@@ -7,6 +7,8 @@ import uk.gov.hmcts.reform.civil.service.OrganisationService;
 
 import java.util.Map;
 
+import static uk.gov.hmcts.reform.civil.utils.PartyUtils.getPartyNameBasedOnType;
+
 @Component
 public class NotifyClaimAppSolEmailDTOGenerator extends AppSolOneEmailDTOGenerator {
 
