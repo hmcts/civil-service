341de99ee203964ab73d44852b8409dececd8477
Sabah Irfan <sabah.irfan@gmail.com>
2026-02-09 15:53:43 +0000

    Remove auto configuration exclusion

--- src/main/java/uk/gov/hmcts/reform/civil/Application.java
+++ src/main/java/uk/gov/hmcts/reform/civil/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import uk.gov.hmcts.reform.fees.client.config.FeesClientAutoConfiguration;
 
 @SpringBootApplication
 @EnableCamundaRestClient
