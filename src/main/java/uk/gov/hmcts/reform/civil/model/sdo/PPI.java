f1644dc87328516c38dbe66197e975d6630b522b
Sumit Bagga <sumit.bagga2@hmcts.net>
2026-02-10 17:22:49 +0000

    update the date validation as per requirements

--- src/main/java/uk/gov/hmcts/reform/civil/model/sdo/PPI.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/sdo/PPI.java
@@ -4,14 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
-import jakarta.validation.constraints.Future;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PPI {
 
-    @Future(message = "The date entered must be in the future")
     private LocalDate ppiDate;
     private String text;
 }
