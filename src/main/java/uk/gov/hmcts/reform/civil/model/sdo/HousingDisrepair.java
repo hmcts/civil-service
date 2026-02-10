54e0c51172a51ced609ac048f6908122b001a2d9
Sumit Bagga <sumit.bagga2@hmcts.net>
2026-02-10 17:21:37 +0000

    Revert "ppi future date fix"
    This reverts commit 03f37bd6557a948674139636b0408450faac29b3.

--- src/main/java/uk/gov/hmcts/reform/civil/model/sdo/HousingDisrepair.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/sdo/HousingDisrepair.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.model.sdo;
 
+import jakarta.validation.constraints.Future;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +14,10 @@ public class HousingDisrepair {
 
     private String clauseA;
     private String clauseB;
+    @Future(message = "The date entered must be in the future")
     private LocalDate firstReportDateBy;
     private String clauseCBeforeDate;
+    @Future(message = "The date entered must be in the future")
     private LocalDate jointStatementDateBy;
     private String clauseCAfterDate;
     private String clauseD;
