5a21893cd1c7d326610fe9a7a094f836cbabd86f
rishisharma <rishi.sharma@hmcts.net>
2026-02-18 15:19:17 +0000

    DTSCCI-000: fix it

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/config/OpenAPIPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/config/OpenAPIPublisherTest.java
@@ -4,12 +4,9 @@ import com.microsoft.applicationinsights.TelemetryClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -26,20 +23,19 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
  * Each travis run on master should automatically save and upload (if updated) documentation.
  */
 
-@SpringJUnitWebConfig
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest
 @ActiveProfiles("integration-test")
 class OpenAPIPublisherTest {
 
-    @Autowired
     private MockMvc mvc;
-
-    @Autowired
-    private WebApplicationContext wac;
+    private final WebApplicationContext wac;
 
     @MockBean
     private TelemetryClient telemetryClient;
+
+    OpenAPIPublisherTest(WebApplicationContext wac) {
+        this.wac = wac;
+    }
 
     @BeforeEach
     void setUp() {
