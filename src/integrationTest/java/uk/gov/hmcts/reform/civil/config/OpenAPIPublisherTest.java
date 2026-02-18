432fd994e52a3f3307670bff445c240263136a84
Shaun Francis <shaun.francis@hmcts.net>
2026-02-18 14:29:20 +0000

    Remove OpenAPIPublisherTest annotation

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/config/OpenAPIPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/config/OpenAPIPublisherTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
