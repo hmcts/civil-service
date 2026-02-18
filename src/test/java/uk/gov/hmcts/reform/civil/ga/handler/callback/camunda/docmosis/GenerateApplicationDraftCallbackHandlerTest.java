3e63ff75f1bd94dd6547df00b296b86d5532844a
philip-milne <philip.milne@hmcts.net>
2026-02-18 19:48:33 +0000

    DTSCCI-3858 GenerateApplicationDraftCallbackHandler cognitive complexity reduction

--- src/test/java/uk/gov/hmcts/reform/civil/ga/handler/callback/camunda/docmosis/GenerateApplicationDraftCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/ga/handler/callback/camunda/docmosis/GenerateApplicationDraftCallbackHandlerTest.java
@@ -424,6 +424,45 @@ class GenerateApplicationDraftCallbackHandlerTest extends GeneralApplicationBase
     }
 
     @Test
+    void shouldNotGenerateApplicationDraftDocument_whenJudgeHasMadeDecision_LR() {
+        when(gaForLipService.isGaForLip(any())).thenReturn(false);
+        GeneralApplicationCaseData caseData = getSampleGeneralApplicationCaseData(YES, NO, YES);
+        caseData = caseData.toBuilder()
+            .judicialDecision(uk.gov.hmcts.reform.civil.ga.model.genapplication.GAJudicialDecision.builder().build())
+            .generalAppPBADetails(GeneralApplicationPbaDetails.builder()
+                                      .paymentDetails(PaymentDetails.builder()
+                                                          .status(PaymentStatus.SUCCESS).build())
+                                      .fee(Fee.builder().code("FREE").build()).build()).build();
+        when(generalAppFeesService.isFreeApplication(any())).thenReturn(true);
+        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+
+        handler.handle(params);
+
+        verifyNoInteractions(generalApplicationDraftGenerator);
+    }
+
+    @Test
+    void shouldNotGenerateApplicationDraftDocument_whenNotLip_AndNoConditionMet_LR() {
+        when(gaForLipService.isGaForLip(any())).thenReturn(false);
+        // Non-urgent, With Notice, Paid - but maybe we want to test when NONE of the 4 conditions match.
+        // 1. Urgent and Free: NO (Not urgent)
+        // 2. Non-urgent, without notice, paid: NO (With notice)
+        // 3. Urgent and paid: NO (Not urgent)
+        // 4. RespondentsResponseSatisfied: NO (Assume false by default for this data)
+        GeneralApplicationCaseData caseData = getSampleGeneralApplicationCaseData(YES, YES, NO);
+        caseData = caseData.toBuilder()
+            .generalAppPBADetails(GeneralApplicationPbaDetails.builder()
+                                      .paymentDetails(PaymentDetails.builder()
+                                                          .status(PaymentStatus.SUCCESS).build())
+                                      .fee(Fee.builder().code("NotFree").build()).build()).build();
+        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+
+        handler.handle(params);
+
+        verifyNoInteractions(generalApplicationDraftGenerator);
+    }
+
+    @Test
     void shouldNotGenerateApplicationDraftDocument_whenAboutToSubmitEventIsCalledAndFreeApp_Lip() {
         GeneralApplicationCaseData caseData = getSampleGeneralApplicationCaseDataLip(YES, NO, NO);
         when(gaForLipService.isGaForLip(any())).thenReturn(true);
