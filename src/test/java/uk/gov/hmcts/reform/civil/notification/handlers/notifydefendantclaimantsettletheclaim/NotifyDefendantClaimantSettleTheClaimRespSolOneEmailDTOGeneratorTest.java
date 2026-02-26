42fcb2dee04105fc99a74b69311ac0ef73926de8
Rakesh Chiluka <rakesh.chiluka2@hmcts.net>
2026-02-26 10:31:16 +0000

    Dtscci-4025 fixed sonar issues and added tests

--- src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/notifydefendantclaimantsettletheclaim/NotifyDefendantClaimantSettleTheClaimRespSolOneEmailDTOGeneratorTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/notifydefendantclaimantsettletheclaim/NotifyDefendantClaimantSettleTheClaimRespSolOneEmailDTOGeneratorTest.java
@@ -4,13 +4,28 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import uk.gov.hmcts.reform.ccd.model.OrganisationPolicy;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.Party;
+import uk.gov.hmcts.reform.civil.model.SolicitorReferences;
 import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
 import uk.gov.hmcts.reform.civil.service.OrganisationService;
+import uk.gov.hmcts.reform.civil.utils.NotificationUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIM_16_DIGIT_NUMBER;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIM_LEGAL_ORG_NAME_SPEC;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIMANT_NAME;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.DEFENDANT_REFERENCE_NUMBER;
 
 public class NotifyDefendantClaimantSettleTheClaimRespSolOneEmailDTOGeneratorTest {
 
@@ -45,5 +60,67 @@ public class NotifyDefendantClaimantSettleTheClaimRespSolOneEmailDTOGeneratorTes
         String referenceTemplate = emailDTOGenerator.getReferenceTemplate();
 
         assertThat(referenceTemplate).isEqualTo("notify-defendant-lr-claimant-settle-the-claim-notification-%s");
+    }
+
+    @Test
+    void shouldAddCustomPropertiesWithDefendantReference() {
+        String claimantName = "Claimant Company Ltd";
+        String defendantReference = "DEF-REF-123";
+        Long ccdCaseReference = 1234567890123456L;
+        String legalOrgName = "Test Legal Org";
+
+        OrganisationPolicy organisationPolicy = new OrganisationPolicy();
+        SolicitorReferences solicitorReferences = new SolicitorReferences();
+        solicitorReferences.setRespondentSolicitor1Reference(defendantReference);
+
+        CaseData caseData = CaseData.builder()
+            .applicant1(Party.builder().type(Party.Type.COMPANY).companyName(claimantName).build())
+            .ccdCaseReference(ccdCaseReference)
+            .solicitorReferences(solicitorReferences)
+            .respondent1OrganisationPolicy(organisationPolicy)
+            .build();
+
+        MockedStatic<NotificationUtils> notificationUtilsMockedStatic = Mockito.mockStatic(NotificationUtils.class);
+        notificationUtilsMockedStatic.when(() -> NotificationUtils.getLegalOrganizationNameForRespondent(any(), anyBoolean(), any()))
+            .thenReturn(legalOrgName);
+
+        Map<String, String> properties = new HashMap<>();
+        Map<String, String> updatedProperties = emailDTOGenerator.addCustomProperties(properties, caseData);
+
+        notificationUtilsMockedStatic.close();
+
+        assertThat(updatedProperties).containsEntry(CLAIM_LEGAL_ORG_NAME_SPEC, legalOrgName);
+        assertThat(updatedProperties).containsEntry(CLAIMANT_NAME, claimantName);
+        assertThat(updatedProperties).containsEntry(CLAIM_16_DIGIT_NUMBER, ccdCaseReference.toString());
+        assertThat(updatedProperties).containsEntry(DEFENDANT_REFERENCE_NUMBER, defendantReference);
+    }
+
+    @Test
+    void shouldAddCustomPropertiesWithoutDefendantReference() {
+        String claimantName = "Claimant Company Ltd";
+        Long ccdCaseReference = 1234567890123456L;
+        String legalOrgName = "Test Legal Org";
+
+        OrganisationPolicy organisationPolicy = new OrganisationPolicy();
+
+        CaseData caseData = CaseData.builder()
+            .applicant1(Party.builder().type(Party.Type.COMPANY).companyName(claimantName).build())
+            .ccdCaseReference(ccdCaseReference)
+            .respondent1OrganisationPolicy(organisationPolicy)
+            .build();
+
+        MockedStatic<NotificationUtils> notificationUtilsMockedStatic = Mockito.mockStatic(NotificationUtils.class);
+        notificationUtilsMockedStatic.when(() -> NotificationUtils.getLegalOrganizationNameForRespondent(any(), anyBoolean(), any()))
+            .thenReturn(legalOrgName);
+
+        Map<String, String> properties = new HashMap<>();
+        Map<String, String> updatedProperties = emailDTOGenerator.addCustomProperties(properties, caseData);
+
+        notificationUtilsMockedStatic.close();
+
+        assertThat(updatedProperties).containsEntry(CLAIM_LEGAL_ORG_NAME_SPEC, legalOrgName);
+        assertThat(updatedProperties).containsEntry(CLAIMANT_NAME, claimantName);
+        assertThat(updatedProperties).containsEntry(CLAIM_16_DIGIT_NUMBER, ccdCaseReference.toString());
+        assertThat(updatedProperties).containsEntry(DEFENDANT_REFERENCE_NUMBER, "Not provided");
     }
 }
