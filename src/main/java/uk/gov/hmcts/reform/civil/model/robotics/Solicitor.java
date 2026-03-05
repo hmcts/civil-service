1ff285cde0be4e37e7bc9712deb1af4e25a3ccd0
Sabah Irfan <sabah.irfan@gmail.com>
2026-03-05 07:16:23 +0000

    revert lombok upgrade

--- src/main/java/uk/gov/hmcts/reform/civil/model/robotics/Solicitor.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/robotics/Solicitor.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.civil.model.robotics;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 @Data
@@ -23,7 +22,6 @@ public class Solicitor {
     private String contactEmailAddress;
     private String preferredMethodOfCommunication;
     private String reference;
-    @Getter(onMethod_ = {@JsonProperty("isPayee")})
     @JsonProperty("isPayee")
     private boolean isPayee;
 
