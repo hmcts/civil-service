0f1553e6e954053a645ffe39a5eb619b1ed514eb
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-03-05 12:22:16 +0000

    DTSCCI-4097 Refactor notify_decision_on_reconsideration_request.bpmn to use generic notify event

---
+++ src/main/java/uk/gov/hmcts/reform/civil/notification/handlers/notifydecisiononreconsiderationrequest/NotifyDecisionOnReconsiderationRequestRespSolTwoEmailDTOGenerator.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.reform.civil.notification.handlers.notifydecisiononreconsiderationrequest;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.notification.handlers.RespSolTwoEmailDTOGenerator;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+import uk.gov.hmcts.reform.civil.service.OrganisationService;
+
+import java.util.Map;
+
+import static java.util.Objects.nonNull;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIMANT_V_DEFENDANT;
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.PARTY_NAME;
+import static uk.gov.hmcts.reform.civil.utils.HearingUtils.getClaimantVDefendant;
+
+@Component
+public class NotifyDecisionOnReconsiderationRequestRespSolTwoEmailDTOGenerator extends RespSolTwoEmailDTOGenerator {
+
+    private static final String REFERENCE_TEMPLATE = "reconsideration-upheld-applicant-notification-%s";
+    private final NotificationsProperties notificationsProperties;
+
+    public NotifyDecisionOnReconsiderationRequestRespSolTwoEmailDTOGenerator(OrganisationService organisationService,
+                                                                             NotificationsProperties notificationsProperties) {
+        super(organisationService);
+        this.notificationsProperties = notificationsProperties;
+    }
+
+    @Override
+    protected String getEmailTemplateId(CaseData caseData) {
+        return notificationsProperties.getNotifyClaimReconsiderationLRTemplate();
+    }
+
+    @Override
+    protected String getReferenceTemplate() {
+        return REFERENCE_TEMPLATE;
+    }
+
+    @Override
+    protected String getEmailAddress(CaseData caseData) {
+        return caseData.getRespondentSolicitor2EmailAddress() != null
+            ? caseData.getRespondentSolicitor2EmailAddress()
+            : caseData.getRespondentSolicitor1EmailAddress();
+    }
+
+    @Override
+    public Boolean getShouldNotify(CaseData caseData) {
+        return caseData.getRespondent2() != null
+            && nonNull(caseData.getRespondent2().getPartyName())
+            && !caseData.getRespondent2().getPartyName().isEmpty()
+            && (caseData.getRespondentSolicitor1EmailAddress() != null
+            || caseData.getRespondentSolicitor2EmailAddress() != null);
+    }
+
+    @Override
+    protected Map<String, String> addCustomProperties(Map<String, String> properties, CaseData caseData) {
+        Map<String, String> result = super.addCustomProperties(properties, caseData);
+        result.put(CLAIMANT_V_DEFENDANT, getClaimantVDefendant(caseData));
+        result.put(PARTY_NAME, caseData.getRespondent2().getPartyName());
+        return result;
+    }
+}
