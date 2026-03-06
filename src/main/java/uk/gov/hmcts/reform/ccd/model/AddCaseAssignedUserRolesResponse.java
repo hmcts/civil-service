5dda01da68dd52c413b79aa8d7f56c1b6e342635
Mike Lemos <gobii@me.com>
2026-03-06 11:08:32 +0000

    DTSCCI-3888 Upgrade to springboot 4

--- src/main/java/uk/gov/hmcts/reform/ccd/model/AddCaseAssignedUserRolesResponse.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/model/AddCaseAssignedUserRolesResponse.java
@@ -1,9 +1,15 @@
 package uk.gov.hmcts.reform.ccd.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AddCaseAssignedUserRolesResponse {
 
     private String status;
+
+    @JsonProperty("status_message")
+    private String statusMessage;
 }
