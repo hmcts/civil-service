27008ea592c5759de5325cfa2cc80175f1a50fd6
Mike Lemos <gobii@me.com>
2026-03-09 09:46:06 +0000

    DTSCCI-3888 Upgrade to springboot 4

--- src/test/java/uk/gov/hmcts/reform/civil/config/TestJacksonAutoConfiguration.java
+++ src/test/java/uk/gov/hmcts/reform/civil/config/TestJacksonAutoConfiguration.java
@@ -1,9 +1,25 @@
 package uk.gov.hmcts.reform.civil.config;
 
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import java.util.List;
 
 @Configuration
 @Import(JacksonConfiguration.class)
 public class TestJacksonAutoConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper(ObjectProvider<List<Module>> modulesProvider) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.findAndRegisterModules();
+        modulesProvider.getIfAvailable(List::of).forEach(mapper::registerModule);
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        return mapper;
+    }
 }
