d0e4adda170b6c76f36a8243d35759223b3a480c
Mike Lemos <gobii@me.com>
2026-03-09 10:18:56 +0000

    DTSCCI-3888 Upgrade to springboot 4

--- src/main/java/uk/gov/hmcts/reform/civil/config/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/JacksonConfiguration.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.reform.civil.config;
 
 import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Locale;
 
 @Configuration
@@ -26,5 +30,14 @@ public class JacksonConfiguration {
         module.addSerializer(new LocalDateSerializer(DATE_FORMATTER));
         module.addSerializer(new LocalDateTimeSerializer(DATE_TIME_FORMATTER));
         return module;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(ObjectProvider<List<Module>> modulesProvider) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.findAndRegisterModules();
+        modulesProvider.getIfAvailable(List::of).forEach(mapper::registerModule);
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        return mapper;
     }
 }
