134056fed3032ebe38d5736930ed07f301ed3141
rishisharma <rishi.sharma@hmcts.net>
2026-03-11 17:03:56 +0000

    DTSCCI-3779: Dashboard notification for mark paid in full

--- src/main/resources/db/migration/V2026_03_10_1042__create_clamant_and_defendant_mark_paid_in_full_notifications.sql
+++ src/main/resources/db/migration/V2026_03_10_1042__create_clamant_and_defendant_mark_paid_in_full_notifications.sql
@@ -1,7 +1,7 @@
-UPDATE dbs.scenario SET notifications_to_delete = '{"Notice.AAA6.JudgmentsOnline.IssuedCCJ.Claimant"}',
+UPDATE dbs.scenario SET notifications_to_delete = '{"Notice.AAA6.JudgmentsOnline.IssuedCCJ.Claimant", "Notice.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Claimant"}',
                         notifications_to_create = '{"Notice.AAA6.JudgmentsOnline.PaidInFull.Claimant": []}'
 WHERE name = 'Scenario.AAA6.ProofOfDebtPayment.Confirmation.Claimant';
 
-UPDATE dbs.scenario SET notifications_to_delete = '{"Notice.AAA6.JudgmentsOnline.IssuedCCJ.Defendant"}',
+UPDATE dbs.scenario SET notifications_to_delete = '{"Notice.AAA6.JudgmentsOnline.IssuedCCJ.Defendant","Notice.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Defendant"}',
                         notifications_to_create = '{"Notice.AAA6.JudgmentsOnline.PaidInFull.Defendant": []}'
 WHERE name = 'Scenario.AAA6.ProofOfDebtPayment.Confirmation.Defendant';
