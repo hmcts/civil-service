871b3ff8788fa096a755eef51e9441032f7ffa41
mounikaammineni <mounika.ammineni1@hmcts.net>
2026-03-11 08:39:04 +0000

    Add unassign case role messages

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/migration/UnAssignUserWithACaseRole.java
@@ -0,0 +1,46 @@
+package uk.gov.hmcts.reform.civil.handler.migration;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.civil.bulkupdate.csv.UnAssignCaseReference;
+import uk.gov.hmcts.reform.civil.enums.CaseRole;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.service.CoreCaseUserService;
+
+@Component
+public class UnAssignUserWithACaseRole extends MigrationTask<UnAssignCaseReference> {
+
+    public final CoreCaseUserService coreCaseUserService;
+
+    public UnAssignUserWithACaseRole(CoreCaseUserService coreCaseUserService1) {
+        super(UnAssignCaseReference.class);
+        this.coreCaseUserService = coreCaseUserService1;
+    }
+
+    @Override
+    protected String getEventSummary() {
+        return "UnAssign a case role to a case";
+    }
+
+    protected CaseData migrateCaseData(CaseData caseData, UnAssignCaseReference unassignCaseReference) {
+        if (caseData == null || unassignCaseReference.getCaseReference() == null) {
+            throw new IllegalArgumentException("CaseData and CaseReference must not be null");
+        }
+
+        coreCaseUserService.unassignCase(unassignCaseReference.getCaseReference(),
+                                         unassignCaseReference.getUserId(),
+                                         unassignCaseReference.getOrganisationId(),
+                                       CaseRole.valueOf(unassignCaseReference.getCaseRole())
+        );
+        return caseData;
+    }
+
+    @Override
+    protected String getEventDescription() {
+        return "This task is used to unassign a case role to a case";
+    }
+
+    @Override
+    protected String getTaskName() {
+        return "UnAssignUserWithACaseRole";
+    }
+}
