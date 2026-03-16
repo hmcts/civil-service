de01d9c8becbf797c0a17863fa552adecb58968d
Mike Lemos <gobii@me.com>
2026-03-16 08:36:50 +0000

    adding security.md

--- src/main/java/uk/gov/hmcts/reform/civil/config/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/JacksonConfiguration.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.reform.civil.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import uk.gov.hmcts.reform.civil.handler.callback.user.spec.show.DefendantResponseShowTag;
+import uk.gov.hmcts.reform.civil.model.CaseDataParent;
 
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Set;
 
 @Configuration
 public class JacksonConfiguration {
@@ -25,6 +29,12 @@ public class JacksonConfiguration {
             builder.simpleDateFormat(DATE_TIME_FORMAT);
             builder.serializers(new LocalDateSerializer(DATE_FORMATTER));
             builder.serializers(new LocalDateTimeSerializer(DATE_TIME_FORMATTER));
+            builder.mixIn(CaseDataParent.class, CaseDataParentMixin.class);
         };
+    }
+
+    private abstract static class CaseDataParentMixin {
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        abstract Set<DefendantResponseShowTag> getShowConditionFlags();
     }
 }
