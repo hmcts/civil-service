e73e49773f57b97edfa3554a5117a9b565033f20
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-03-24 11:38:28 +0000

    DTSCCI-4317 Refactor set_aside_judgement_request.bpmn to use generic notify event

---
+++ src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/setasidejudgementrequest/SetAsideJudgementRequestClaimantEmailDTOGeneratorTest.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.reform.civil.notification.handlers.setasidejudgementrequest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class SetAsideJudgementRequestClaimantEmailDTOGeneratorTest {
+
+    @Mock
+    private NotificationsProperties notificationsProperties;
+
+    @InjectMocks
+    private SetAsideJudgementRequestClaimantEmailDTOGenerator generator;
+
+    @Test
+    void shouldReturnLipTemplate() {
+        CaseData caseData = CaseData.builder().build();
+        when(notificationsProperties.getNotifyUpdateTemplate()).thenReturn("lip-template");
+
+        assertThat(generator.getEmailTemplateId(caseData)).isEqualTo("lip-template");
+        assertThat(generator.getReferenceTemplate()).isEqualTo("set-aside-judgment-applicant-notification-lip-%s");
+    }
+}
