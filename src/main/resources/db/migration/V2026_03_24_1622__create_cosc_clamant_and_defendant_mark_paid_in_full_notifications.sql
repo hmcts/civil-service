d0307d585540298a1e6a6f570a579803a797cbf0
rishisharma <rishi.sharma@hmcts.net>
2026-03-25 07:16:13 +0000

    DTSCCI-3902: Update COSC Generation logic

--- src/main/resources/db/migration/V2026_03_24_1622__create_cosc_clamant_and_defendant_mark_paid_in_full_notifications.sql
+++
@@ -1,6 +0,0 @@
-UPDATE dbs.scenario SET notifications_to_delete = '{"Notice.AAA6.ProofofDebtPayment.Application.Claimant"}'
-WHERE name = 'Scenario.AAA6.ProofOfDebtPayment.Confirmation.Claimant';
-
-UPDATE dbs.scenario SET notifications_to_delete = '{"Notice.AAA6.ProofofDebtPayment.Application.Defendant"}',
-                        notifications_to_create = '{"Notice.AAA6.ProofofDebtPayment.ApplicationProcessed.Defendant": []}'
-WHERE name = 'Scenario.AAA6.ProofOfDebtPayment.Confirmation.Defendant';
