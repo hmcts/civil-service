ce32398132b096fbaa07bf610d2b7d382074045b
rishisharma <rishi.sharma@hmcts.net>
2026-03-25 12:48:36 +0000

    DTSCCI-3902: Update COSC Generation logic

--- src/main/resources/db/migration/V2026_03_25_1118__update_mark_paid_in_full_notifications.sql
+++ src/main/resources/db/migration/V2026_03_25_1118__update_mark_paid_in_full_notifications.sql
@@ -1,6 +1,10 @@
-UPDATE dbs.scenario SET notifications_to_delete = '{"Notice.AAA6.ProofofDebtPayment.Application.Claimant"}'
+UPDATE dbs.scenario SET notifications_to_delete = '{"Notice.AAA6.ProofofDebtPayment.Application.Claimant","Notice.AAA6.JudgmentsOnline.IssuedCCJ.Claimant"}'
 WHERE name = 'Scenario.AAA6.ProofOfDebtPayment.Confirmation.Claimant';
 
-UPDATE dbs.scenario SET notifications_to_delete = '{"Notice.AAA6.ProofofDebtPayment.Application.Defendant"}',
-                        notifications_to_create = '{"Notice.AAA6.ProofofDebtPayment.ApplicationProcessed.Defendant": []}'
+UPDATE dbs.scenario SET notifications_to_delete = '{"Notice.AAA6.ProofofDebtPayment.Application.Defendant", "Notice.AAA6.JudgmentsOnline.IssuedCCJ.Defendant"}'
 WHERE name = 'Scenario.AAA6.ProofOfDebtPayment.Confirmation.Defendant';
+
+INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
+VALUES ('Scenario.AAA6.ProofOfDebtPayment.Confirmation.Defendant',
+        '{}',
+        '{"Notice.AAA6.ProofofDebtPayment.ApplicationProcessed.Defendant": []}');
