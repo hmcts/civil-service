59088904cd7e1db89a86f5272e98e4d2b7a5a0c3
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-03-30 14:20:13 +0100

    Maintain JSON properties after enum cleanup

--- src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/FastTrackMethodVideoConferenceHearing.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/FastTrackMethodVideoConferenceHearing.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.reform.civil.enums.sdo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public enum FastTrackMethodVideoConferenceHearing {
+    @JsonProperty("videoTheClaimant")
     VIDEO_THE_CLAIMANT("the claimant"),
+    @JsonProperty("videoTheDefendant")
     VIDEO_THE_DEFENDANT("the defendant"),
+    @JsonProperty("videoTheCourt")
     VIDEO_THE_COURT("the court");
 
     private final String label;
--- src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/SmallClaimsMethod.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/sdo/SmallClaimsMethod.java
@@ -1,7 +1,12 @@
 package uk.gov.hmcts.reform.civil.enums.sdo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum SmallClaimsMethod {
+    @JsonProperty("smallClaimsMethodInPerson")
     SMALL_CLAIMS_METHOD_IN_PERSON,
+    @JsonProperty("smallClaimsMethodVideoConferenceHearing")
     SMALL_CLAIMS_METHOD_VIDEO_CONFERENCE_HEARING,
+    @JsonProperty("smallClaimsMethodTelephoneHearing")
     SMALL_CLAIMS_METHOD_TELEPHONE_HEARING
 }
