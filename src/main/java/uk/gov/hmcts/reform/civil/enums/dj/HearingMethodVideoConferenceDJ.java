226ee54c847ee1b377ac5c69b3f4269e5ce71d07
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-03-31 10:14:44 +0100

    DTSCCI-3856 High severity constant naming cleanup in DJ enums

--- src/main/java/uk/gov/hmcts/reform/civil/enums/dj/HearingMethodVideoConferenceDJ.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/dj/HearingMethodVideoConferenceDJ.java
@@ -1,18 +1,23 @@
 package uk.gov.hmcts.reform.civil.enums.dj;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public enum HearingMethodVideoConferenceDJ {
-    videoTheClaimant("the claimant"),
-    videoTheDefendant("the defendant"),
-    videoTheCourt("the court");
+    @JsonProperty("videoTheClaimant")
+    VIDEO_THE_CLAIMANT("the claimant"),
 
-    private String label;
+    @JsonProperty("videoTheDefendant")
+    VIDEO_THE_DEFENDANT("the defendant"),
+
+    @JsonProperty("videoTheCourt")
+    VIDEO_THE_COURT("the court");
+
+    private final String label;
 
     HearingMethodVideoConferenceDJ(String value) {
         this.label = value;
     }
 
 }
-
