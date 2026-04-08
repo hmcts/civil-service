f9a7678e16a8afc6cd57d9d1d65c57108c831834
Mike Lemos <gobii@me.com>
2026-04-08 11:34:21 +0100

    DTSCCI-5038 import defs and add roles done through the correct pipeline stage now.

--- bin/setup-devuser-preview-env.sh
+++ bin/setup-devuser-preview-env.sh
@@ -4,16 +4,22 @@ camundaBranch=${2:-master}
 echo "export ENVIRONMENT=devuser-preview"
 echo "Loading Environment Variables"
 source ./bin/variables/load-dev-user-preview-environment-variables.sh
+
 echo "Importing Roles to the CCD pod"
 ./bin/add-roles.sh
+
+echo "Preparing CCD definitions"
+./bin/pull-latest-civil-ccd-files.sh ${ccdBranch}
+./bin/build-release-ccd-definition.sh preview
+
+echo "Running high-level CCD data setup"
+./gradlew --no-daemon highLevelDataSetup
+
 echo "Importing Camunda definitions"
 ./bin/pull-latest-camunda-files.sh ${camundaBranch}
-echo "Importing CCD definitions"
-./bin/pull-latest-civil-ccd-files.sh ${ccdBranch}
-./bin/import-ccd-definition.sh "-e *-prod.json,*HNL-nonprod.json,AuthorisationCaseType-shuttered.json"
 
 rm -rf $(pwd)/ccd-definition
-rm -rf $(pwd)/build/ccd-development-config
+rm -rf $(pwd)/build/ccd-release-config
 rm -rf $(pwd)/camunda
 
 echo "ENV variables set for devuser-preview environment."
