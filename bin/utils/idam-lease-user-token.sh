6e754d85e0a3e994baeb22d6a1116640caf33205
Ruban <ruban72@hotmail.com>
2026-04-08 10:54:35 +0100

    DTSCCI-5034: Update Idam endpoints

--- bin/utils/idam-lease-user-token.sh
+++ bin/utils/idam-lease-user-token.sh
@@ -10,10 +10,12 @@ IDAM_URL=${IDAM_STUB_LOCALHOST:-$IDAM_API_URL}
 
 clientSecret=${CCD_API_GATEWAY_IDAM_CLIENT_SECRET:-ccd_gateway_secret}
 redirectUri=${CCD_IDAM_REDIRECT_URL:-http://localhost:3451/oauth2redirect}
-if [ -z "$IDAM_STUB_LOCALHOST" ]; then
-  code=$(curl --insecure --fail --show-error --silent -X POST --user "${username}:${password}" "${IDAM_URL}/oauth2/authorize?redirect_uri=${redirectUri}&response_type=code&client_id=ccd_gateway" -d "" | docker run --rm --interactive ghcr.io/jqlang/jq:latest -r .code)
-else
-  code=stubbed-value
-fi
 
-curl --insecure --fail --show-error --silent -X POST -H "Content-Type: application/x-www-form-urlencoded" --user "ccd_gateway:${clientSecret}" "${IDAM_URL}/oauth2/token?code=${code}&redirect_uri=${redirectUri}&grant_type=authorization_code" -d "" | docker run --rm --interactive ghcr.io/jqlang/jq:latest -r .access_token
+if [ -z "$IDAM_STUB_LOCALHOST" ]; then
+  curl --insecure --fail --show-error --silent -X POST \
+    -H "Content-Type: application/x-www-form-urlencoded" \
+    -d "grant_type=password&username=${username}&password=${password}&client_id=ccd_gateway&client_secret=${clientSecret}&redirect_uri=${redirectUri}&scope=openid%20profile%20roles" \
+    "${IDAM_URL}/o/token" | docker run --rm --interactive ghcr.io/jqlang/jq:latest -r .access_token
+else
+  curl --insecure --fail --show-error --silent -X POST -H "Content-Type: application/x-www-form-urlencoded" --user "ccd_gateway:${clientSecret}" "${IDAM_URL}/oauth2/token?code=stubbed-value&redirect_uri=${redirectUri}&grant_type=authorization_code" -d "" | docker run --rm --interactive ghcr.io/jqlang/jq:latest -r .access_token
+fi
