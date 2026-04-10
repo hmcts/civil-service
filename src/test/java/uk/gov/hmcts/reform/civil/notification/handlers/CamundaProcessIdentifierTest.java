3456cf6e56b502e1d20daade6f9a7c422e7ec64e
Rakesh Chiluka <rakesh.chiluka2@hmcts.net>
2026-04-10 17:27:30 +0100

    Dtscci-4276 fixed the failure test to pass the build

--- src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/CamundaProcessIdentifierTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/CamundaProcessIdentifierTest.java
@@ -11,7 +11,7 @@ class CamundaProcessIdentifierTest {
     void shouldContainAllExpectedEnumValues() {
         CamundaProcessIdentifier[] values = CamundaProcessIdentifier.values();
 
-        assertThat(values).hasSize(70)
+        assertThat(values).hasSize(71)
             .contains(
                 CamundaProcessIdentifier.ClaimDismissedNotifyParties,
                 CamundaProcessIdentifier.ClaimantConfirmProceedNotifyParties,
@@ -79,7 +79,8 @@ class CamundaProcessIdentifierTest {
                 CamundaProcessIdentifier.NotifyDefendantClaimantSettleTheClaimNotify,
                 CamundaProcessIdentifier.TranslatedDocumentUploadedNotifyParties,
                 CamundaProcessIdentifier.DJ_NON_DIVERGENT_NOTIFIER,
-                CamundaProcessIdentifier.STANDARD_DIRECTION_ORDER_DJ_NOTIFY_PARTIES
+                CamundaProcessIdentifier.STANDARD_DIRECTION_ORDER_DJ_NOTIFY_PARTIES,
+                CamundaProcessIdentifier.TranslatedOrderNoticeUploadedNotifyParties
         );
     }
 
@@ -110,6 +111,6 @@ class CamundaProcessIdentifierTest {
 
         assertThat(values)
             .satisfies(v -> assertThat(v[0]).isEqualTo(CamundaProcessIdentifier.ClaimDismissedNotifyParties))
-            .satisfies(v -> assertThat(v[69]).isEqualTo(CamundaProcessIdentifier.STANDARD_DIRECTION_ORDER_DJ_NOTIFY_PARTIES));
+            .satisfies(v -> assertThat(v[70]).isEqualTo(CamundaProcessIdentifier.TranslatedOrderNoticeUploadedNotifyParties));
     }
 }
