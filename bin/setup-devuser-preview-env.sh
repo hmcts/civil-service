652da505147f622eedae0a79c2a7b278903fa595
Mike Lemos <michael.lemos1@hmcts.net>
2026-04-13 13:51:33 +0100

    DTSCCI-5038 Refactor import and add roles scripts

--- bin/setup-devuser-preview-env.sh
+++ bin/setup-devuser-preview-env.sh
@@ -1,21 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu
+
 ccdBranch=${1:-master}
 camundaBranch=${2:-master}
 
 echo "export ENVIRONMENT=devuser-preview"
 echo "Loading Environment Variables"
-source ./bin/variables/load-dev-user-preview-environment-variables.sh
+source ./bin/devuser/load-preview-environment-variables.sh
 . ./bin/utils/idam-get-tokens.sh
 echo "Importing Roles to the CCD pod"
-./bin/add-roles.sh
+./bin/devuser/add-roles.sh
 echo "Importing Camunda definitions"
-./bin/pull-latest-camunda-files.sh ${camundaBranch}
+./bin/pull-latest-camunda-files.sh "${camundaBranch}"
+./bin/import-bpmn-diagram.sh .
 echo "Importing CCD definitions"
-./bin/pull-latest-civil-ccd-files.sh ${ccdBranch}
-./bin/import-ccd-definition.sh "-e *-prod.json,*HNL-nonprod.json,AuthorisationCaseType-shuttered.json"
+./bin/pull-latest-civil-ccd-files.sh "${ccdBranch}"
+./bin/devuser/import-ccd-definition.sh "-e *-prod.json,*HNL-nonprod.json,AuthorisationCaseType-shuttered.json"
 
-rm -rf $(pwd)/ccd-definition
-rm -rf $(pwd)/build/ccd-development-config
-rm -rf $(pwd)/camunda
+rm -rf "$(pwd)/ccd-definition"
+rm -rf "$(pwd)/build/ccd-development-config"
+rm -rf "$(pwd)/camunda"
 
 echo "ENV variables set for devuser-preview environment."
 echo "XUI_URL: $XUI_WEBAPP_URL"
