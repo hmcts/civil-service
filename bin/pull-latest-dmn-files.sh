0673b2349650af70a7a2622d4e16337bb5be0f2e
Mike Lemos <gobii@me.com>
2026-04-16 08:54:55 +0100

    Revert "Dtscci-5038 (#7728)"
    This reverts commit c473e1f7f1a171c66f95b3576b72c6a0c1dee72c.

--- bin/pull-latest-dmn-files.sh
+++ bin/pull-latest-dmn-files.sh
@@ -3,15 +3,17 @@
 branchName=$1
 
 #Checkout specific branch pf  civil camunda bpmn definition
-rm -rf ./civil-wa-task-configuration ./wa-dmn
 git clone https://github.com/hmcts/civil-wa-task-configuration.git
 cd civil-wa-task-configuration
 
 echo "Switch to ${branchName} branch on civil-wa-task-configuration"
-git checkout "${branchName}"
+git checkout ${branchName}
 cd ..
 mkdir wa-dmn
 
 #Copy camunda folder to civil-ccd-def which contians dmn files
 cp -r ./civil-wa-task-configuration/src/main/resources ./wa-dmn/.
 rm -rf ./civil-wa-task-configuration
+
+./bin/import-dmn-diagram.sh ./wa-dmn/. civil civil
+rm -rf ./wa-dmn
