dc3675fd5b2535862a2c5de6e7776603c2b5a1eb
rishisharma <rishi.sharma@hmcts.net>
2026-04-16 16:44:13 +0100

    DTSCCI-3902: Cosc application

--- src/main/resources/db/migration/V2026_04_16_1612__update_claimant_notifications.sql
+++ src/main/resources/db/migration/V2026_04_16_1612__update_claimant_notifications.sql
@@ -1,3 +1,3 @@
 UPDATE dbs.scenario SET notifications_to_delete = '{"Notice.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Claimant"}',
                         notifications_to_create = '{}'
-WHERE name = 'Notice.AAA6.ProofofDebtPayment.Application.Claimant';
+WHERE name = 'Scenario.AAA6.ProofofDebtPayment.Application.Claimant';
