eb1d4b2e0ba41d0326c940174d3df10a54222f37
AbuSalamHMCTS <abu.salam@HMCTS.NET>
2026-04-16 15:50:41 +0100

    DTSCCI-5110 Checkstyle

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/StayLiftedDefendantScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/defendant/StayLiftedDefendantScenarioTest.java
@@ -13,7 +13,6 @@ import uk.gov.hmcts.reform.dashboard.data.TaskStatus;
 
 import java.time.LocalDateTime;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
