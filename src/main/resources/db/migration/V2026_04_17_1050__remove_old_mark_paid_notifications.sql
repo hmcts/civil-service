778a61ae2ab727b2ef3943ed9f52bbadae54d2a1
rishisharma <rishi.sharma@hmcts.net>
2026-04-17 11:52:16 +0100

    DTSCCI-3902: Cosc application

--- src/main/resources/db/migration/V2026_04_17_1050__remove_old_mark_paid_notifications.sql
+++ src/main/resources/db/migration/V2026_04_17_1050__remove_old_mark_paid_notifications.sql
@@ -2,5 +2,5 @@
  * Notifications to be deleted after DJ journey until Claimant confirms payment.
  */
 
-UPDATE dbs.scenario SET notifications_to_delete = '{"Notice.AAA6.JudgmentsOnline.IssuedCCJ.Claimant"}'
+UPDATE dbs.scenario SET notifications_to_delete = '{"Notice.AAA6.JudgmentsOnline.IssuedCCJ.Claimant","Notice.AAA6.JudgmentsOnline.DefaultJudgmentIssued.Claimant"}'
 WHERE name = 'Scenario.AAA6.ProofofDebtPayment.Application.Claimant';
