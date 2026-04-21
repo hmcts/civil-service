34430049dba6c77ca2fb98d7d98e7498bda15b5f
Mike Lemos <gobii@me.com>
2026-04-21 15:50:33 +0100

    Revert "Dtscci 5038 (#7744)"
    This reverts commit 651613a14da08a84cf08d0dba39e35ae0ae9586c.

---
+++ bin/variables/load-staging-environment-variables.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eu
+
+echo 'export ENVIRONMENT=aat'
+
+# urls
+echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
+echo "export IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net"
+echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect"
+echo "export CCD_DEFINITION_STORE_API_BASE_URL=https://civil-service-definition-store-staging-aat.service.core-compute-aat.internal"
+echo "export CAMUNDA_BASE_URL=https://civil-service-camunda-staging-aat.service.core-compute-aat.internal"
+
+# definition placeholders
+echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-service-staging-java"
+echo "export CCD_DEF_GEN_APP_SERVICE_BASE_URL=http://civil-service-staging-java"
