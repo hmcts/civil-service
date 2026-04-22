0453f653ae0e19bf8a87e46ff1240618a6a1d391
Mike Lemos <michael.lemos1@hmcts.net>
2026-04-22 19:59:44 +0100

    DTSCCI-5038 Adding BPMN config and skipping AAT HLD

--- src/main/java/uk/gov/hmcts/reform/civil/setup/CamundaHighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/reform/civil/setup/CamundaHighLevelDataSetupApp.java
@@ -38,20 +38,22 @@ public final class CamundaHighLevelDataSetupApp {
     }
 
     private static void configureRelaxedSsl() throws GeneralSecurityException {
-        TrustManager[] trustAllManagers = new TrustManager[]{new X509TrustManager() {
-            @Override
-            public void checkClientTrusted(X509Certificate[] chain, String authType) {
-            }
+        TrustManager[] trustAllManagers = new TrustManager[] {
+            new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                }
 
-            @Override
-            public void checkServerTrusted(X509Certificate[] chain, String authType) {
-            }
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                }
 
-            @Override
-            public X509Certificate[] getAcceptedIssuers() {
-                return new X509Certificate[0];
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
             }
-        }};
+        };
 
         SSLContext sslContext = SSLContext.getInstance("TLS");
         sslContext.init(null, trustAllManagers, new SecureRandom());
