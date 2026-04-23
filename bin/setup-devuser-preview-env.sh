f83b8636e6336617979de92a7584de4d31064b55
Mike Lemos <gobii@me.com>
2026-04-23 09:00:36 +0100

    Revert "Dtscci 5038 (#7751)"
    This reverts commit d728633342171c10f6e0a088ee047892aea99fb2.

---
+++ bin/setup-devuser-preview-env.sh
@@ -0,0 +1,21 @@
+ccdBranch=${1:-master}
+camundaBranch=${2:-master}
+
+echo "export ENVIRONMENT=devuser-preview"
+echo "Loading Environment Variables"
+source ./bin/variables/load-dev-user-preview-environment-variables.sh
+. ./bin/shared/idam-get-tokens.sh
+echo "Importing Roles to the CCD pod"
+./bin/add-roles.sh
+echo "Importing Camunda definitions"
+./bin/pull-latest-camunda-files.sh ${camundaBranch}
+echo "Importing CCD definitions"
+./bin/pull-latest-civil-ccd-files.sh ${ccdBranch}
+./bin/import-ccd-definition.sh "-e *-prod.json,*HNL-nonprod.json,AuthorisationCaseType-shuttered.json"
+
+rm -rf $(pwd)/ccd-definition
+rm -rf $(pwd)/build/ccd-development-config
+rm -rf $(pwd)/camunda
+
+echo "ENV variables set for devuser-preview environment."
+echo "XUI_URL: $XUI_WEBAPP_URL"
