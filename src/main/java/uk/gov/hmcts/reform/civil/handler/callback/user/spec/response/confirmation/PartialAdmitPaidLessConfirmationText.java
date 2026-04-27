d4124abc4eccefd7e1cd9c1c17e777abc6ec5507
Gajanan <gajanan.deshpande1@hmcts.net>
2026-04-27 12:01:47 +0100

    DTSCCI-133 - 1v2 different solicitors defendant response forms incorrectly generated - 2

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/response/confirmation/PartialAdmitPaidLessConfirmationText.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/spec/response/confirmation/PartialAdmitPaidLessConfirmationText.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.civil.enums.RespondentResponseTypeSpec;
 import uk.gov.hmcts.reform.civil.handler.callback.user.spec.RespondToClaimConfirmationTextSpecGenerator;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.RespondToClaim;
 import uk.gov.hmcts.reform.civil.service.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.utils.MonetaryConversions;
 
@@ -22,8 +21,7 @@ public class PartialAdmitPaidLessConfirmationText implements RespondToClaimConfi
             || NO.equals(caseData.getSpecDefenceAdmittedRequired())) {
             return Optional.empty();
         }
-        BigDecimal howMuchWasPaid = Optional.ofNullable(caseData.getRespondToAdmittedClaim())
-            .map(RespondToClaim::getHowMuchWasPaid).orElse(null);
+        BigDecimal howMuchWasPaid = (caseData.getResponseToClaim() != null ? caseData.getResponseToClaim().getHowMuchWasPaid() : null);
         BigDecimal totalClaimAmount = caseData.getTotalClaimAmount();
 
         if (howMuchWasPaid == null || totalClaimAmount == null) {
