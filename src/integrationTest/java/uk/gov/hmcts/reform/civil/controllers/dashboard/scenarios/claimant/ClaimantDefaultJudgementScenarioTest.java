9c68a75ad202ea0f66ffa88bd79ff73dd44c6fae
AbuSalamHMCTS <abu.salam@HMCTS.NET>
2026-05-06 09:01:28 +0100

    DTSCCI-5097

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimantDefaultJudgementScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimantDefaultJudgementScenarioTest.java
@@ -40,12 +40,12 @@ public class ClaimantDefaultJudgementScenarioTest extends DashboardBaseIntegrati
             .andExpect(status().isOk())
             .andExpectAll(
                 status().is(HttpStatus.OK.value()),
-                jsonPath("$[0].titleEn").value("A judgment against the defendant has been made"),
+                jsonPath("$[0].titleEn").value("A judgment against the defendant has been now been entered"),
                 jsonPath("$[0].descriptionEn")
                     .value("<p class=\"govuk-body\">The defendant should now pay you according to the terms " +
-                               "of the judgment. <br> Once they do, you should <a href=\"{CONFIRM_YOU_HAVE_BEEN_PAID_URL}\" class=\"govuk-link\">" +
-                               "confirm that they’ve paid you the full amount that you’re owed</a>.<br>If they do not pay you by the date on the judgment, " +
-                               "you can <u>ask for enforcement action to be taken against them</u>. <br>If you need to change the terms of payment within the judgment, " +
+                               "of the judgment.<br>Once they do, you should <a href=\"{CONFIRM_YOU_HAVE_BEEN_PAID_URL}\" class=\"govuk-link\">" +
+                               "confirm that they've paid you the full amount that you're owed</a>.<br>If they do not pay you by the date on the judgment, " +
+                               "you can <u>ask for enforcement action to be taken against them</u>.<br>If you need to change the terms of payment within the judgment, " +
                                "such as the instalments you had previously agreed, you can <a href=\"{GENERAL_APPLICATIONS_INITIATION_PAGE_URL}\" class=\"govuk-link\">" +
                                "make an application to vary the judgment</a>.</p>")
             );
@@ -71,13 +71,13 @@ public class ClaimantDefaultJudgementScenarioTest extends DashboardBaseIntegrati
             .andExpect(status().isOk())
             .andExpectAll(
                 status().is(HttpStatus.OK.value()),
-                jsonPath("$[0].titleCy").value("Mae dyfarniad wedi’i wneud yn erbyn y diffynnydd"),
+                jsonPath("$[0].titleCy").value("Mae dyfarniad yn erbyn y diffynnydd wedi’i wneud"),
                 jsonPath("$[0].descriptionCy").value(
-                    "<p class=\"govuk-body\">Dylai’r diffynnydd eich talu yn unol â thelerau’r dyfarniad. " +
-                        "<br> Unwaith y byddant yn gwneud hynny, dylech <a href=\"{CONFIRM_YOU_HAVE_BEEN_PAID_URL}\" class=\"govuk-link\">" +
-                        "gadarnhau eu bod wedi talu’r swm llawn sy’n ddyledus i chi</a>.<br>Os na fyddant yn eich talu erbyn y dyddiad ar y dyfarniad, gallwch " +
-                        "<u>ofyn am gymryd camau  gorfodi yn eu herbyn</u>." +
-                        " <br>Os oes arnoch angen newid y telerau talu o fewn y dyfarniad, fel y rhandaliadau roeddech wedi cytuno arnynt yn flaenorol, gallwch " +
+                    "<p class=\"govuk-body\">Dylai'r diffynnydd nawr eich talu yn unol â thelerau'r dyfarniad." +
+                        "<br>Unwaith y byddant yn gwneud hynny, dylech <a href=\"{CONFIRM_YOU_HAVE_BEEN_PAID_URL}\" class=\"govuk-link\">" +
+                        "gadarnhau eu bod wedi talu'r swm llawn sy'n ddyledus i chi</a>.<br>Os na fyddant yn eich talu erbyn y dyddiad ar y dyfarniad, gallwch " +
+                        "<u>ofyn am gymryd camau gorfodi yn eu herbyn</u>." +
+                        "<br>Os oes angen i chi newid y telerau talu o fewn y dyfarniad, fel y rhandaliadau yr oeddech wedi cytuno arnynt yn flaenorol, gallwch " +
                         "<a href=\"{GENERAL_APPLICATIONS_INITIATION_PAGE_URL}\" class=\"govuk-link\">wneud cais i amrywio’r dyfarniad</a>.</p>")
             );
     }
