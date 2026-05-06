d3f3b3a58259029c68b0fa8fbce6b96a69f6b403
rishikrsharma <rishikrsharma@users.noreply.github.com>
2026-05-06 12:54:11 +0100

    Revert "Create scheduler framework with examples and tests (#7803)"
    This reverts commit 9791cc601abaa71ce80bdfd5fae7a89aed256056.

--- src/main/java/uk/gov/hmcts/reform/civil/scheduler/common/ScheduledTask.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.civil.scheduler.common;
-
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-
-import java.util.function.Consumer;
-
-public interface ScheduledTask extends Consumer<CaseDetails> {
-}
