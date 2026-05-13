4224b41f48f3ba06e77da7c824bef3b9a8d47226
philip-milne <philip.milne@hmcts.net>
2026-05-13 18:07:28 +0100

    Introduce `ExternalTaskCompletionService` to centralize external task completion logic and update handlers to use this service. Include relevant unit tests for coverage.

--- src/test/java/uk/gov/hmcts/reform/civil/handler/tasks/DecisionOutcomeHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/tasks/DecisionOutcomeHandlerTest.java
@@ -13,14 +13,12 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.civil.event.DecisionOutcomeEvent;
-import uk.gov.hmcts.reform.civil.exceptions.CompleteTaskException;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDetailsBuilder;
 import uk.gov.hmcts.reform.civil.service.search.DecisionOutcomeSearchService;
 
 import java.util.Map;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -108,9 +106,7 @@ class DecisionOutcomeHandlerTest {
         doThrow(new NotFoundException(errorMessage, new RestException("", "", 404)))
             .when(externalTaskService).complete(mockTask, null);
 
-        assertThrows(
-            CompleteTaskException.class,
-            () -> handler.execute(mockTask, externalTaskService));
+        handler.execute(mockTask, externalTaskService);
 
         verify(externalTaskService, never()).handleFailure(
             any(ExternalTask.class),
@@ -135,7 +131,7 @@ class DecisionOutcomeHandlerTest {
         String errorMessage = "there was an error";
 
         doThrow(new NullPointerException(errorMessage))
-            .when(applicationEventPublisher).publishEvent(eq(new DecisionOutcomeEvent(caseId)));
+            .when(applicationEventPublisher).publishEvent(new DecisionOutcomeEvent(caseId));
 
         handler.execute(mockTask, externalTaskService);
 
