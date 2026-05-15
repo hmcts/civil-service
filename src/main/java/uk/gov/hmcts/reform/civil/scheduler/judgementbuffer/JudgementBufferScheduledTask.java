dd3fd67625d892bf785294db52a719b595f94ee7
Shaun Francis <shaun.francis@hmcts.net>
2026-05-15 10:32:52 +0100

    Simplify JudgementBufferScheduledTask

--- src/main/java/uk/gov/hmcts/reform/civil/scheduler/judgementbuffer/JudgementBufferScheduledTask.java
+++ src/main/java/uk/gov/hmcts/reform/civil/scheduler/judgementbuffer/JudgementBufferScheduledTask.java
@@ -1,15 +1,10 @@
 package uk.gov.hmcts.reform.civil.scheduler.judgementbuffer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
-import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
-import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.scheduler.common.ScheduledTask;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 
@@ -19,27 +14,11 @@ import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 public class JudgementBufferScheduledTask implements ScheduledTask {
 
     private final CoreCaseDataService coreCaseDataService;
-    private final CaseDetailsConverter caseDetailsConverter;
-    private final ObjectMapper objectMapper;
 
     @Override
     public void accept(CaseDetails caseDetails) {
         Long caseId = caseDetails.getId();
         log.info("JudgementBufferScheduledTask::accept case {}", caseId);
-
-        StartEventResponse startEventResponse = coreCaseDataService.startUpdate(
-            String.valueOf(caseId),
-            CaseEvent.DEFAULT_JUDGEMENT_GRANTED_SPEC
-        );
-
-        CaseData caseData = caseDetailsConverter.toCaseData(startEventResponse.getCaseDetails());
-
-        CaseDataContent caseDataContent = coreCaseDataService.caseDataContentFromStartEventResponse(
-            startEventResponse,
-            caseData.toMap(objectMapper)
-        );
-        caseDataContent.getEvent().setSummary("Issue Judgement");
-        caseDataContent.getEvent().setDescription("Issue Judgement after Judgement Buffer");
-        coreCaseDataService.submitUpdate(String.valueOf(caseId), caseDataContent);
+        coreCaseDataService.triggerEvent(caseId, CaseEvent.DEFAULT_JUDGEMENT_GRANTED_SPEC);
     }
 }
--- src/test/java/uk/gov/hmcts/reform/civil/scheduler/judgementbuffer/JudgementBufferScheduledTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/scheduler/judgementbuffer/JudgementBufferScheduledTaskTest.java
@@ -1,61 +1,32 @@
 package uk.gov.hmcts.reform.civil.scheduler.judgementbuffer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.ccd.client.model.Event;
-import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
-import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
-import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 
-import java.time.LocalDateTime;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class JudgementBufferScheduledTaskTest {
 
     @Mock
     private CoreCaseDataService coreCaseDataService;
-    @Mock
-    private CaseDetailsConverter caseDetailsConverter;
-    @Mock
-    private ObjectMapper objectMapper;
 
     @InjectMocks
     private JudgementBufferScheduledTask task;
 
     @Test
     void shouldTriggerUpdateWhenCaseIsEligibleForDefaultJudgement() {
-        CaseDetails caseDetails = CaseDetails.builder().id(1L).build();
-        StartEventResponse startEventResponse = StartEventResponse.builder()
-            .caseDetails(caseDetails)
-            .build();
-
-        CaseData caseData = CaseData.builder()
-            .respondent1ResponseDeadline(LocalDateTime.now().minusDays(1))
-            .build();
-
-        CaseDataContent caseDataContent = CaseDataContent.builder()
-            .event(Event.builder().build())
-            .build();
-
-        when(coreCaseDataService.startUpdate("1", CaseEvent.DEFAULT_JUDGEMENT_GRANTED_SPEC)).thenReturn(startEventResponse);
-        when(caseDetailsConverter.toCaseData(any(CaseDetails.class))).thenReturn(caseData);
-        when(coreCaseDataService.caseDataContentFromStartEventResponse(eq(startEventResponse), any())).thenReturn(caseDataContent);
+        Long caseId = 123L;
+        CaseDetails caseDetails = CaseDetails.builder().id(caseId).build();
 
         task.accept(caseDetails);
 
-        verify(coreCaseDataService).submitUpdate("1", caseDataContent);
+        verify(coreCaseDataService).triggerEvent(caseId, CaseEvent.DEFAULT_JUDGEMENT_GRANTED_SPEC);
     }
 }
