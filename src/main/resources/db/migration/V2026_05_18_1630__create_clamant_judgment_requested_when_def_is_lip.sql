5937359a98bd2004720e7b42cbccf3f5070737cf
mounikaammineni <mounilka.ammineni1@hmcts.net>
2026-05-18 21:57:29 +0100

    DTSCCI-5096  changes

--- src/main/resources/db/migration/V2026_05_18_1630__create_clamant_judgment_requested_when_def_is_lip.sql
+++ src/main/resources/db/migration/V2026_05_18_1630__create_clamant_judgment_requested_when_def_is_lip.sql
@@ -3,5 +3,5 @@
  */
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
 VALUES ('Scenario.AAA6.DefIsLip.Judgment.Requested.Claimant',
-        '{}',
+        '{"Notice.AAA6.DefResponse.ResponseTimeElapsed.Claimant"}',
         '{"Notice.AAA6.DefLip.Judgment.Requested.Claimant": []}');
