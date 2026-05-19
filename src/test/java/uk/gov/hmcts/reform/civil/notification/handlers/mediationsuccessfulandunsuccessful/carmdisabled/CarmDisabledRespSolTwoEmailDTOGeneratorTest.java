033d171a3f7c89e9efaf826f3fe0b44489f6ddc4
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-05-19 11:48:36 +0100

    Added further coverage, excluded generator for duplication

--- src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/mediationsuccessfulandunsuccessful/carmdisabled/CarmDisabledRespSolTwoEmailDTOGeneratorTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/mediationsuccessfulandunsuccessful/carmdisabled/CarmDisabledRespSolTwoEmailDTOGeneratorTest.java
@@ -10,15 +10,18 @@ import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.Party;
 import uk.gov.hmcts.reform.civil.notify.NotificationsProperties;
 import uk.gov.hmcts.reform.civil.service.OrganisationService;
+import uk.gov.hmcts.reform.civil.utils.MediationUtils;
 import uk.gov.hmcts.reform.civil.utils.NotificationUtils;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.enums.mediation.MediationUnsuccessfulReason.NOT_CONTACTABLE_DEFENDANT_TWO;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIMANT_NAME;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.CLAIM_LEGAL_ORG_NAME_SPEC;
 import static uk.gov.hmcts.reform.civil.handler.callback.camunda.notification.NotificationData.PARTY_NAME;
@@ -26,6 +29,10 @@ import static uk.gov.hmcts.reform.civil.notification.handlers.CamundaProcessIden
 
 @ExtendWith(MockitoExtension.class)
 class CarmDisabledRespSolTwoEmailDTOGeneratorTest {
+
+    private static final String SUCCESS_TEMPLATE = "success-template";
+    private static final String NO_ATTENDANCE_TEMPLATE = "no-attendance-template";
+    private static final String UNSUCCESSFUL_TEMPLATE = "unsuccessful-template";
 
     @Mock
     private OrganisationService organisationService;
@@ -39,11 +46,41 @@ class CarmDisabledRespSolTwoEmailDTOGeneratorTest {
     @Test
     void shouldReturnSuccessfulTemplateForSecondRepresentedDefendant() {
         CaseData caseData = mock(CaseData.class);
-        when(notificationsProperties.getNotifyLrDefendantSuccessfulMediation()).thenReturn("success-template");
+        when(notificationsProperties.getNotifyLrDefendantSuccessfulMediation()).thenReturn(SUCCESS_TEMPLATE);
 
         String templateId = generator.getEmailTemplateId(caseData, MediationSuccessfulNotifyParties.toString());
 
-        assertThat(templateId).isEqualTo("success-template");
+        assertThat(templateId).isEqualTo(SUCCESS_TEMPLATE);
+    }
+
+    @Test
+    void shouldReturnNoAttendanceTemplateWhenDefendantTwoNotContactable() {
+        CaseData caseData = mock(CaseData.class);
+        when(notificationsProperties.getMediationUnsuccessfulNoAttendanceLRTemplate()).thenReturn(NO_ATTENDANCE_TEMPLATE);
+
+        try (MockedStatic<MediationUtils> mediationUtils = mockStatic(MediationUtils.class)) {
+            mediationUtils.when(() -> MediationUtils.findMediationUnsuccessfulReason(caseData, List.of(NOT_CONTACTABLE_DEFENDANT_TWO)))
+                .thenReturn(true);
+
+            String templateId = generator.getEmailTemplateId(caseData, "other-task");
+
+            assertThat(templateId).isEqualTo(NO_ATTENDANCE_TEMPLATE);
+        }
+    }
+
+    @Test
+    void shouldReturnUnsuccessfulTemplateWhenDefendantTwoReasonDoesNotMatch() {
+        CaseData caseData = mock(CaseData.class);
+        when(notificationsProperties.getMediationUnsuccessfulLRTemplate()).thenReturn(UNSUCCESSFUL_TEMPLATE);
+
+        try (MockedStatic<MediationUtils> mediationUtils = mockStatic(MediationUtils.class)) {
+            mediationUtils.when(() -> MediationUtils.findMediationUnsuccessfulReason(caseData, List.of(NOT_CONTACTABLE_DEFENDANT_TWO)))
+                .thenReturn(false);
+
+            String templateId = generator.getEmailTemplateId(caseData);
+
+            assertThat(templateId).isEqualTo(UNSUCCESSFUL_TEMPLATE);
+        }
     }
 
     @Test
@@ -66,5 +103,10 @@ class CarmDisabledRespSolTwoEmailDTOGeneratorTest {
                 .containsEntry(CLAIMANT_NAME, "Alice Smith")
                 .containsEntry(PARTY_NAME, "Alice Smith's claim against you");
         }
+    }
+
+    @Test
+    void shouldReturnReferenceTemplate() {
+        assertThat(generator.getReferenceTemplate()).isEqualTo("mediation-update-defendant-notification-LR-%s");
     }
 }
