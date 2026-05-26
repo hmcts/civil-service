cd4fce235da21c1341fd83e5a577ffa73227224f
mounikaammineni <mounilka.ammineni1@hmcts.net>
2026-05-26 20:42:44 +0100

    DTSCCI-5108  changes

---
+++ src/main/resources/db/migration/V2026_05_25_1403__DTSCCI-5107_case_discontinued.sql
@@ -0,0 +1,11 @@
+/**
+ * Update scenario
+ */
+UPDATE dbs.scenario
+SET
+  notifications_to_delete = '{ "Notice.AAA6.DefResponse.MoreTimeRequested.Claimant",
+                               "Notice.AAA6.ClaimIssue.Response.Required",
+                               "Notice.AAA6.DefResponse.BilingualFlagSet.Claimant",
+                               "Notice.AAA6.DefResponse.ResponseTimeElapsed.Claimant" }'
+WHERE
+  name = 'Scenario.AAA6.Discontinue.NoticeOfDiscontinuanceIssued.Defendant';
