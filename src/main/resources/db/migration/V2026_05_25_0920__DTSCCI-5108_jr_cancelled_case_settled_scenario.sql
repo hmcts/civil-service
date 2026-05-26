8d77b213ac7fa7f0456f0a5fdfd94014b367b49c
mounikaammineni <mounilka.ammineni1@hmcts.net>
2026-05-26 14:20:42 +0100

    DTSCCI-5107  changes

--- src/main/resources/db/migration/V2026_05_25_0920__DTSCCI-5108_jr_cancelled_case_settled_scenario.sql
+++ src/main/resources/db/migration/V2026_05_25_0920__DTSCCI-5108_jr_cancelled_case_settled_scenario.sql
@@ -3,5 +3,5 @@
  */
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
 VALUES ('Scenario.AAA6.JR.Cancelled.Case.Settled.Claimant',
-        '{}',
+        '{"Notice.AAA6.DefLip.Judgment.Requested.Claimant"}',
         '{"Notice.AAA6.JR.Cancelled.Case.Settled.Claimant" : []}');
