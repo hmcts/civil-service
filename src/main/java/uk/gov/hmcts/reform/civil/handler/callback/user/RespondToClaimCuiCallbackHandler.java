183b2d91a92c837db26ea72a09a52c940ac7f99e
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2026-05-27 15:34:34 +0100

    DTSCCI-5059 Add judgment requested intermediate state with LD flag - judgment-buffer  (#7760)

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToClaimCuiCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToClaimCuiCallbackHandler.java
@@ -14,7 +14,9 @@ import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.documentmanagement.model.CaseDocument;
 import uk.gov.hmcts.reform.civil.enums.AllocatedTrack;
 import uk.gov.hmcts.reform.civil.enums.CaseState;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.enums.dq.Language;
+import uk.gov.hmcts.reform.civil.helpers.judgmentsonline.JudgmentsOnlineHelper;
 import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.Party;
@@ -109,6 +111,11 @@ public class RespondToClaimCuiCallbackHandler extends CallbackHandler {
                 LocalDate.now()
             ));
 
+        boolean isJoRequested = featureToggleService.isJudgmentBufferEnabled()
+            && YesOrNo.YES.equals(caseData.getIsJoRequested());
+        if (isJoRequested) {
+            JudgmentsOnlineHelper.clearJOCaseData(caseData);
+        }
         AboutToStartOrSubmitCallbackResponse.AboutToStartOrSubmitCallbackResponseBuilder responseBuilder =
             AboutToStartOrSubmitCallbackResponse.builder()
                 .data(caseData.toMap(objectMapper));
@@ -119,6 +126,9 @@ public class RespondToClaimCuiCallbackHandler extends CallbackHandler {
 
         if (!needsTranslating) {
             responseBuilder.state(CaseState.AWAITING_APPLICANT_INTENTION.name());
+        } else if (isJoRequested) {
+            // Keep translated judgment-requested cases in the respondent acknowledgement stage.
+            responseBuilder.state(CaseState.AWAITING_RESPONDENT_ACKNOWLEDGEMENT.name());
         }
 
         if (caseFlagsLoggingEnabled) {
