c4102936e0180275be8b1f0b58d47f87cdf48555
mounikaammineni <mounilka.ammineni1@hmcts.net>
2026-05-27 12:11:13 +0100

    DTSCCI-5105  changes

--- src/main/resources/db/migration/V2026_05_19_1026__DTSCCI-5105_create_claimant_cancel_jr_defresponse_extension_granted.sql
+++ src/main/resources/db/migration/V2026_05_19_1026__DTSCCI-5105_create_claimant_cancel_jr_defresponse_extension_granted.sql
@@ -6,3 +6,10 @@ VALUES ('Scenario.AAA6.DefResponse.MoreTimeRequested.JR.Cancelled.Claimant',
         '{"Notice.AAA6.DefResponse.ResponseTimeElapsed.Defendant",
         "Notice.AAA6.DefLip.Judgment.Requested.Claimant"}',
         '{"Notice.AAA6.DefResponse.MoreTimeRequested.JR.Cancelled.Claimant": []}');
+
+
+/**
+ * Update notification template
+ */
+update dbs.scenario set notifications_to_delete = '{"Notice.AAA6.DefResponse.ResponseTimeElapsed.Defendant"}'
+where name = 'Scenario.AAA6.DefResponse.MoreTimeRequested.Defendant';
