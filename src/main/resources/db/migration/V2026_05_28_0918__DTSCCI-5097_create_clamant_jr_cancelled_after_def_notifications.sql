076f03de64ed5ab22534fd2b7be240f9b605416e
AbuSalamHMCTS <abu.salam@HMCTS.NET>
2026-05-28 09:45:24 +0100

    DTSCCI-5097 fix

--- src/main/resources/db/migration/V2026_05_28_0918__DTSCCI-5097_create_clamant_jr_cancelled_after_def_notifications.sql
+++
@@ -1,7 +0,0 @@
-/**
- * Add scenario for claimant
- */
-INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
-VALUES ('Scenario.AAA6.DefResponse.JR.Cancelled.Claimant',
-        '{}',
-        '{"Notice.AAA6.DefResponse.JR.Cancelled.Claimant": []}');
