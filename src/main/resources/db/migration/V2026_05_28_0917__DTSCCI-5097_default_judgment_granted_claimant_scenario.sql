197dab1aacfb46c3afde51beaa75e4169535759a
AbuSalamHMCTS <abu.salam@HMCTS.NET>
2026-06-01 16:24:35 +0100

    DTSCCI-5097 Notification delete fix

--- src/main/resources/db/migration/V2026_05_28_0917__DTSCCI-5097_default_judgment_granted_claimant_scenario.sql
+++ src/main/resources/db/migration/V2026_05_28_0917__DTSCCI-5097_default_judgment_granted_claimant_scenario.sql
@@ -1,6 +1,9 @@
 INSERT INTO dbs.scenario (name, notifications_to_delete, notifications_to_create)
 VALUES ('Scenario.AAA6.JudgmentsOnline.DefaultJudgmentGranted.Claimant',
-        '{"Notice.AAA6.ClaimantIntent.CCJ.Requested.Claimant"}',
+        '{"Notice.AAA6.ClaimantIntent.CCJ.Requested.Claimant",
+          "Notice.AAA6.DefResponse.ResponseTimeElapsed.Claimant",
+          "Notice.AAA6.ClaimIssue.Response.Await",
+          "Notice.AAA6.ClaimIssue.Response.Required"}',
         '{"Notice.AAA6.JudgmentsOnline.DefaultJudgmentGranted.Claimant": ["djClaimantNotificationMessage", "djClaimantNotificationMessageCy"]}');
 
 INSERT INTO dbs.task_item_template (task_name_en, category_en, task_name_cy, category_cy, template_name,
