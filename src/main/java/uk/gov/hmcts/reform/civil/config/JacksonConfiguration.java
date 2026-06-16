7879ea795f6dcdeab8c567db2374031fe6b80f23
Mike Lemos <michael.lemos1@hmcts.net>
2026-06-16 17:06:40 +0100

    DTSCCI-5599 restore integration mapper date support

--- src/main/java/uk/gov/hmcts/reform/civil/config/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/JacksonConfiguration.java
@@ -1,10 +1,15 @@
 package uk.gov.hmcts.reform.civil.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
@@ -22,9 +27,20 @@ public class JacksonConfiguration {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jsonDateTimeFormatCustomizer() {
         return builder -> {
+            builder.modulesToInstall(JavaTimeModule.class);
+            builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
             builder.simpleDateFormat(DATE_TIME_FORMAT);
             builder.serializers(new LocalDateSerializer(DATE_FORMATTER));
             builder.serializers(new LocalDateTimeSerializer(DATE_TIME_FORMATTER));
         };
+    }
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder.createXmlMapper(false)
+            .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .modulesToInstall(JavaTimeModule.class)
+            .build();
     }
 }
