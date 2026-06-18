0f5958f9cfe2eafa1f52e6b92b4e3c474f554dce
AbuSalamHMCTS <abu.salam@HMCTS.NET>
2026-06-18 10:34:31 +0100

    DTSCCI-5097

--- src/main/java/uk/gov/hmcts/reform/civil/service/search/JudgementBufferExpiredSearchService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/search/JudgementBufferExpiredSearchService.java
@@ -2,17 +2,13 @@ package uk.gov.hmcts.reform.civil.service.search;
 
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.enums.CaseState;
+import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.model.search.Query;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
-import uk.gov.hmcts.reform.civil.service.search.calculator.SearchDateTimeCalculator;
 
 import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-
-import static uk.gov.hmcts.reform.civil.helpers.LocalDateTimeHelper.LOCAL_ZONE;
 
 import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
 import static org.elasticsearch.index.query.QueryBuilders.existsQuery;
@@ -23,24 +19,19 @@ import static org.elasticsearch.index.query.QueryBuilders.rangeQuery;
 @Slf4j
 public class JudgementBufferExpiredSearchService extends ElasticSearchService {
 
-    private final SearchDateTimeCalculator dateTimeCalculator;
-
-    public JudgementBufferExpiredSearchService(CoreCaseDataService coreCaseDataService,
-                                               SearchDateTimeCalculator dateTimeCalculator) {
+    public JudgementBufferExpiredSearchService(CoreCaseDataService coreCaseDataService) {
         super(coreCaseDataService);
-        this.dateTimeCalculator = dateTimeCalculator;
     }
 
     @Override
     public Query query(int startIndex, String timeNow) {
         log.info("Call to JudgementBufferExpiredSearchService query with index {} and timeNow {}", startIndex, timeNow);
-        ZonedDateTime now = ZonedDateTime.parse(timeNow).withZoneSameInstant(LOCAL_ZONE);
-        ZonedDateTime timeMinus48WorkingHours = dateTimeCalculator.minusWorkingHours(now, 48);
+        ZonedDateTime timeMinus48Hours = ZonedDateTime.parse(timeNow).minusHours(48);
         return new Query(
             boolQuery()
                 .minimumShouldMatch(1)
                 .should(boolQuery()
-                            .must(rangeQuery("data.joDJCreatedDate").lte(formatToIsoLocal(timeMinus48WorkingHours)))
+                            .must(rangeQuery("data.joDJCreatedDate").lte(timeMinus48Hours))
                             .must(beState(CaseState.JUDGMENT_REQUESTED))
                             .must(haveNoOngoingBusinessProcess())
                 ),
@@ -61,9 +52,5 @@ public class JudgementBufferExpiredSearchService extends ElasticSearchService {
             .should(boolQuery().mustNot(existsQuery("data.businessProcess")))
             .should(boolQuery().mustNot(existsQuery("data.businessProcess.status")))
             .should(boolQuery().must(matchQuery("data.businessProcess.status", "FINISHED")));
-    }
-
-    private String formatToIsoLocal(ZonedDateTime timeMinus48WorkingHours) {
-        return timeMinus48WorkingHours.toLocalDateTime().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
     }
 }
--- src/test/java/uk/gov/hmcts/reform/civil/service/search/JudgementBufferExpiredSearchServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/search/JudgementBufferExpiredSearchServiceTest.java
@@ -4,17 +4,12 @@ import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 import uk.gov.hmcts.reform.civil.enums.CaseState;
 import uk.gov.hmcts.reform.civil.model.search.Query;
-import uk.gov.hmcts.reform.civil.service.search.calculator.SearchDateTimeCalculator;
 
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-
-import static uk.gov.hmcts.reform.civil.helpers.LocalDateTimeHelper.LOCAL_ZONE;
 
 import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
 import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
@@ -25,15 +20,11 @@ import static org.mockito.Mockito.mockStatic;
 class JudgementBufferExpiredSearchServiceTest extends ElasticSearchServiceTest {
 
     private static final ZonedDateTime FIXED_NOW = ZonedDateTime.of(2025, 7, 8, 10, 0, 0, 0, ZoneOffset.UTC);
-    private static final ZonedDateTime FIXED_NOW_LONDON = FIXED_NOW.withZoneSameInstant(LOCAL_ZONE);
     private MockedStatic<ZonedDateTime> zonedDateTimeMock;
 
     @BeforeEach
     void setup() {
-        SearchDateTimeCalculator dateTimeCalculator = Mockito.mock(SearchDateTimeCalculator.class);
-        Mockito.when(dateTimeCalculator.minusWorkingHours(FIXED_NOW_LONDON, 48))
-            .thenReturn(FIXED_NOW_LONDON.minusDays(2));
-        searchService = new JudgementBufferExpiredSearchService(coreCaseDataService, dateTimeCalculator);
+        searchService = new JudgementBufferExpiredSearchService(coreCaseDataService);
         zonedDateTimeMock = mockStatic(ZonedDateTime.class, CALLS_REAL_METHODS);
         zonedDateTimeMock.when(() -> ZonedDateTime.now(ZoneOffset.UTC)).thenReturn(FIXED_NOW);
     }
@@ -45,11 +36,11 @@ class JudgementBufferExpiredSearchServiceTest extends ElasticSearchServiceTest {
 
     @Override
     protected Query buildQuery(int fromValue) {
-        ZonedDateTime timeMinus48WorkingHours = FIXED_NOW_LONDON.minusDays(2);
+        ZonedDateTime timeMinus48Hours = FIXED_NOW.minusHours(48);
         BoolQueryBuilder query = boolQuery()
             .minimumShouldMatch(1)
             .should(boolQuery()
-                        .must(rangeQuery("data.joDJCreatedDate").lte(timeMinus48WorkingHours.toLocalDateTime().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)))
+                        .must(rangeQuery("data.joDJCreatedDate").lte(timeMinus48Hours))
                         .must(boolQuery().must(matchQuery("state", CaseState.JUDGMENT_REQUESTED.toString())))
                         .must(((JudgementBufferExpiredSearchService) searchService).haveNoOngoingBusinessProcess()));
         return new Query(query, List.of("reference"), fromValue, true);
