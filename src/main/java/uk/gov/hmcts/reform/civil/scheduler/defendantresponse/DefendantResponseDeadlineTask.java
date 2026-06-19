a657aef6eb7b84702ad664d6fa9bb5f860220b70
Shaun Francis <shaun.francis@hmcts.net>
2026-06-19 13:57:00 +0100

    Add Spring scheduler for defendant response deadline check and remove Camunda scheduler process

--- src/main/java/uk/gov/hmcts/reform/civil/scheduler/defendantresponse/DefendantResponseDeadlineTask.java
+++ src/main/java/uk/gov/hmcts/reform/civil/scheduler/defendantresponse/DefendantResponseDeadlineTask.java
@@ -5,15 +5,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.civil.scheduler.common.ScheduledTask;
+import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
+
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.DEFENDANT_RESPONSE_DEADLINE_CHECK;
 
 @Component
 @AllArgsConstructor
 @Slf4j
 public class DefendantResponseDeadlineTask implements ScheduledTask {
 
+    private final CoreCaseDataService coreCaseDataService;
+
     @Override
     public void accept(CaseDetails caseDetails) {
-        log.info("DefendantResponseDeadlineTask::accept case {}", caseDetails.getId());
-        //Add logic to publish event for defendant response deadline
+        Long caseId = caseDetails.getId();
+        log.info("DefendantResponseDeadlineTask::accept case {}", caseId);
+        coreCaseDataService.triggerEvent(caseId, DEFENDANT_RESPONSE_DEADLINE_CHECK);
     }
 }
