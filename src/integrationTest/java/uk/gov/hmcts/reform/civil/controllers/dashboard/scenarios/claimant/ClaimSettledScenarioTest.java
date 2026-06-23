b17e7f7dab6dd93b7a975b575a644fc4fdd81c46
Sumit Bagga <sumit.bagga2@hmcts.net>
2026-06-23 14:51:50 +0100

    DTSCCI-5567 DTSCCI-5568 DTSCCI-5569
    Temp translation used for testing

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimSettledScenarioTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/dashboard/scenarios/claimant/ClaimSettledScenarioTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.civil.controllers.dashboard.scenarios.claimant;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+
 import uk.gov.hmcts.reform.civil.controllers.DashboardBaseIntegrationTest;
 import uk.gov.hmcts.reform.civil.enums.CaseState;
 import uk.gov.hmcts.reform.civil.enums.RespondentResponseTypeSpec;
@@ -33,7 +34,7 @@ public class ClaimSettledScenarioTest extends DashboardBaseIntegrationTest {
         LocalDateTime respondent1SettlementDeadline = LocalDateTime.now().plusDays(7);
         CaseDataLiP caseDataLiP = new CaseDataLiP()
             .setApplicant1LiPResponse(new ClaimantLiPResponse()
-                                       .setApplicant1SignedSettlementAgreement(YesOrNo.YES));
+                                          .setApplicant1SignedSettlementAgreement(YesOrNo.YES));
 
         CaseData caseData = CaseDataBuilder.builder().atStateClaimantFullDefence().build()
             .toBuilder()
@@ -47,7 +48,7 @@ public class ClaimSettledScenarioTest extends DashboardBaseIntegrationTest {
             .respondToClaim(new RespondToClaim()
                                 .setHowMuchWasPaid(BigDecimal.valueOf(300000))
                                 .setWhenWasThisAmountPaid(LocalDate.of(2024, 3, 16))
-                                )
+            )
             .respondent1RespondToSettlementAgreementDeadline(respondent1SettlementDeadline)
             .build();
 
