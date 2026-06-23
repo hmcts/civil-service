10f04bdbfdfa1fffada978caba3a27caab580968
58046 <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
2026-06-23 07:06:46 +0000

    Updating Terraform Formatting

--- infrastructure/redis.tf
+++ infrastructure/redis.tf
@@ -5,17 +5,17 @@ data "azurerm_subnet" "core_infra_redis_subnet" {
 }
 
 module "civil-service-cache" {
-  source                          = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product                         = "${var.product}-${var.component}-cache"
-  location                        = var.location
-  env                             = var.env
-  subnetid                        = data.azurerm_subnet.core_infra_redis_subnet.id
-  common_tags                     = var.common_tags
-  redis_version                   = 6
-  private_endpoint_enabled        = true
-  business_area                   = "cft"
-  public_network_access_enabled   = false
-  sku_name                        = var.redis_sku_name
-  family                          = var.redis_family
-  capacity                        = var.redis_capacity
+  source                        = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product                       = "${var.product}-${var.component}-cache"
+  location                      = var.location
+  env                           = var.env
+  subnetid                      = data.azurerm_subnet.core_infra_redis_subnet.id
+  common_tags                   = var.common_tags
+  redis_version                 = 6
+  private_endpoint_enabled      = true
+  business_area                 = "cft"
+  public_network_access_enabled = false
+  sku_name                      = var.redis_sku_name
+  family                        = var.redis_family
+  capacity                      = var.redis_capacity
 }
