f0aa5f97821f9b2f1a8003272889103069985f53
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-06-24 14:55:08 +0100

    DTSCCI-5722 Create throttle service to be used by Spring scheduler framework

--- src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/BaseExternalTaskHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/BaseExternalTaskHandler.java
@@ -12,6 +12,7 @@ import uk.gov.hmcts.reform.civil.exceptions.NotRetryableException;
 import uk.gov.hmcts.reform.civil.helpers.ExternalTaskExceptionHelper;
 import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.ExternalTaskData;
+import uk.gov.hmcts.reform.civil.scheduler.common.SchedulerThrottleService;
 import uk.gov.hmcts.reform.civil.service.ExternalTaskCompletionService;
 
 import java.util.Objects;
@@ -24,16 +25,17 @@ public abstract class BaseExternalTaskHandler implements ExternalTaskHandler {
 
     public static final String FLOW_STATE = "flowState";
     public static final String FLOW_FLAGS = "flowFlags";
-    public static final int SMALL_BATCH = 25;
 
     protected final Logger log = LoggerFactory.getLogger(BaseExternalTaskHandler.class);
 
     private final ExternalTaskCompletionService externalTaskCompletionService;
     private final EventProperties eventProperties;
+    private final SchedulerThrottleService schedulerThrottleService;
 
     protected BaseExternalTaskHandler(ExternalTaskCompletionService externalTaskCompletionService, EventProperties eventProperties) {
         this.externalTaskCompletionService = externalTaskCompletionService;
         this.eventProperties = eventProperties;
+        this.schedulerThrottleService = new SchedulerThrottleService(eventProperties);
     }
 
     /**
@@ -135,51 +137,15 @@ public abstract class BaseExternalTaskHandler implements ExternalTaskHandler {
     protected abstract ExternalTaskData handleTask(ExternalTask externalTask);
 
     protected void throttle(long count) {
-        long delay = eventProperties.getDispatchDelay();
-        throttle(count, delay);
+        schedulerThrottleService.throttle(count);
     }
 
     protected void throttle(long count, long delay) {
-        long lock = eventProperties.getLockDuration();
-        throttle(count, delay, lock);
+        schedulerThrottleService.throttle(count, delay);
     }
 
     protected void throttle(long count, long delay, long lock) {
-        long effectiveDelay = calculateEffectiveDelay(count, lock, delay);
-        if (effectiveDelay == 0) {
-            return;
-        }
-        try {
-            Thread.sleep(effectiveDelay);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-    }
-
-    /**
-     * Calculates the effective delay for a batch processing task based on the total found items,
-     * the lock duration, and the desired delay. Ensures the delay does not surpass the maximum
-     * permissible delay derived from the lock duration and batch size.
-     *
-     * @param count the total number of items found in the batch; if less than or equal to 25, no delay is applied.
-     * @param lock  the duration for which the task is locked in milliseconds.
-     * @param delay the desired delay in milliseconds between task executions.
-     * @return the calculated effective delay in milliseconds. Returns 0 if count is less than or equal to 25.
-     */
-    private long calculateEffectiveDelay(long count, long lock, long delay) {
-        if (count <= 1 || delay <= 0 || lock <= 0) {
-            // skip no-op or invalid delays
-            return 0;
-        }
-
-        if (count <= SMALL_BATCH && delay < 2000L) {
-            // skip for small & fast batches
-            return 0;
-        }
-
-        long maxExecutionTimeMs = (long) (lock * 0.8);
-        long maxDelay = maxExecutionTimeMs / count;
-        return Math.min(maxDelay, delay);
+        schedulerThrottleService.throttle(count, delay, lock);
     }
 
     /**
