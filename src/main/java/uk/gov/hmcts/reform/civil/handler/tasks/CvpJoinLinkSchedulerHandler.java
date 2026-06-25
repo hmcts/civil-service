cafb2b130926071c8924fbe788e76c056de6d5f8
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-06-25 09:29:06 +0100

    Revert legacy scheduler file removal

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/CvpJoinLinkSchedulerHandler.java
@@ -0,0 +1,53 @@
+package uk.gov.hmcts.reform.civil.handler.tasks;
+
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.civil.event.CvpJoinLinkEvent;
+import uk.gov.hmcts.reform.civil.model.ExternalTaskData;
+import uk.gov.hmcts.reform.civil.service.search.CaseHearingDateSearchService;
+
+import java.util.Set;
+import uk.gov.hmcts.reform.civil.config.properties.EventProperties;
+
+@Slf4j
+@Component
+public class CvpJoinLinkSchedulerHandler extends BaseExternalTaskHandler {
+
+    private final CaseHearingDateSearchService searchService;
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    public CvpJoinLinkSchedulerHandler(
+        EventProperties eventProperties,
+        CaseHearingDateSearchService searchService,
+        ApplicationEventPublisher applicationEventPublisher
+    ) {
+        super(eventProperties);
+        this.searchService = searchService;
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    @Override
+    public ExternalTaskData handleTask(ExternalTask externalTask) {
+        Set<CaseDetails> cases = searchService.getCases();
+        log.info("CVP Join Link Scheduler job '{}' found {} case(s)", externalTask.getTopicName(), cases.size());
+
+        cases.forEach(caseDetails -> {
+            try {
+                log.info("Publishing event for case id: '{}'", caseDetails.getId());
+                applicationEventPublisher.publishEvent(new CvpJoinLinkEvent(caseDetails.getId()));
+                throttle(cases.size());
+            } catch (Exception e) {
+                log.error("Publishing 'CvpJoinLinkEvent' event for case id: '{}' failed", caseDetails.getId(), e);
+            }
+        });
+        return new ExternalTaskData();
+    }
+
+    @Override
+    public int getMaxAttempts() {
+        return 1;
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/tasks/CvpJoinLinkSchedulerHandlerTest.java
@@ -0,0 +1,175 @@
+package uk.gov.hmcts.reform.civil.handler.tasks;
+
+import org.camunda.bpm.client.exception.NotFoundException;
+import org.camunda.bpm.client.exception.RestException;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.ExternalTaskService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.civil.config.properties.AsyncHandlerProperties;
+import uk.gov.hmcts.reform.civil.event.CvpJoinLinkEvent;
+import uk.gov.hmcts.reform.civil.exceptions.CompleteTaskException;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDetailsBuilder;
+import uk.gov.hmcts.reform.civil.service.search.CaseHearingDateSearchService;
+
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+import org.mockito.Spy;
+import uk.gov.hmcts.reform.civil.config.properties.EventProperties;
+
+@ExtendWith(SpringExtension.class)
+class CvpJoinLinkSchedulerHandlerTest {
+
+    @Mock
+    private ExternalTask mockTask;
+
+    @Mock
+    private ExternalTaskService externalTaskService;
+
+    @Mock
+    private CaseHearingDateSearchService searchService;
+
+    @Mock
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @Mock
+    private AsyncHandlerProperties asyncHandlerProperties;
+    @Spy
+    private EventProperties eventProperties = configuredEventProperties();
+
+    @InjectMocks
+    private CvpJoinLinkSchedulerHandler handler;
+
+    @BeforeEach
+    void init() {
+        when(mockTask.getTopicName()).thenReturn("test");
+        when(mockTask.getWorkerId()).thenReturn("worker");
+    }
+
+    @Test
+    void shouldEmitCvpJoinLinkEvent_whenCasesFound() {
+        long caseId = 1L;
+        Map<String, Object> data = Map.of("data", "some data");
+        Set<CaseDetails> caseDetails = Set.of(new CaseDetailsBuilder().id(caseId).data(data).build());
+
+        when(searchService.getCases()).thenReturn(caseDetails);
+
+        handler.execute(mockTask, externalTaskService);
+
+        verify(applicationEventPublisher).publishEvent(new CvpJoinLinkEvent(caseId));
+        verify(externalTaskService).complete(mockTask, null);
+    }
+
+    @Test
+    void shouldNotEmitCvpJoinLinkEvent_WhenNoCasesFound() {
+        when(searchService.getCases()).thenReturn(Set.of());
+
+        handler.execute(mockTask, externalTaskService);
+
+        verifyNoInteractions(applicationEventPublisher);
+    }
+
+    @Test
+    void shouldCallHandleFailureMethod_whenExceptionFromBusinessLogic() {
+        String errorMessage = "there was an error";
+
+        when(mockTask.getRetries()).thenReturn(null);
+        when(searchService.getCases()).thenAnswer(invocation -> {
+            throw new Exception(errorMessage);
+        });
+
+        handler.execute(mockTask, externalTaskService);
+
+        verify(externalTaskService, never()).complete(mockTask);
+        verify(externalTaskService).handleFailure(
+            eq(mockTask),
+            eq(errorMessage),
+            anyString(),
+            eq(0),
+            anyLong()
+        );
+    }
+
+    @Test
+    void shouldNotCallHandleFailureMethod_whenExceptionOnCompleteCall() {
+        String errorMessage = "there was an error";
+
+        doThrow(new NotFoundException(errorMessage, new RestException("", "", 404)))
+            .when(externalTaskService).complete(mockTask, null);
+
+        assertThrows(
+            CompleteTaskException.class,
+            () -> handler.execute(mockTask, externalTaskService));
+
+        verify(externalTaskService, never()).handleFailure(
+            any(ExternalTask.class),
+            anyString(),
+            anyString(),
+            anyInt(),
+            anyLong()
+        );
+    }
+
+    @Test
+    void shouldHandleExceptionAndContinue_whenOneCaseErrors() {
+        long caseId = 1L;
+        long otherId = 2L;
+        Map<String, Object> data = Map.of("data", "some data");
+        Set<CaseDetails> caseDetails = Set.of(
+            new CaseDetailsBuilder().id(caseId).data(data).build(),
+            new CaseDetailsBuilder().id(otherId).data(data).build());
+
+        when(searchService.getCases()).thenReturn(caseDetails);
+
+        String errorMessage = "there was an error";
+
+        doThrow(new NullPointerException(errorMessage))
+            .when(applicationEventPublisher).publishEvent(eq(new CvpJoinLinkEvent(caseId)));
+
+        handler.execute(mockTask, externalTaskService);
+
+        verify(externalTaskService, never()).handleFailure(
+            any(ExternalTask.class),
+            anyString(),
+            anyString(),
+            anyInt(),
+            anyLong()
+        );
+
+        verify(applicationEventPublisher, times(2)).publishEvent(any(CvpJoinLinkEvent.class));
+        verify(applicationEventPublisher).publishEvent(new CvpJoinLinkEvent(caseId));
+        verify(applicationEventPublisher).publishEvent(new CvpJoinLinkEvent(otherId));
+    }
+
+    @Test
+    void shouldOnlyAttemptOnce() {
+        assertEquals(1, handler.getMaxAttempts());
+    }
+
+    private static EventProperties configuredEventProperties() {
+        EventProperties properties = new EventProperties();
+        properties.setRetryCount(3);
+        return properties;
+    }
+
+}
