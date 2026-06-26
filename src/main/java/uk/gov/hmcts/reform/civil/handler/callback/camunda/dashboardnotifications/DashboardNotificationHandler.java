26cb6944719a4f0af2c85bae0a3704ae0c9faa3b
Mike Lemos <gobii@me.com>
2026-06-26 10:57:18 +0100

    Revert "DTSCCI-5479 Remove dashboard notifications from CCD events (#7899)" (#8048)
    This reverts commit 70d47f40ff06260ed91a9f7bccd236666a213db7.

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardNotificationHandler.java
@@ -24,7 +24,7 @@ public class DashboardNotificationHandler extends CallbackHandler implements Mul
 
     private static final List<CaseEvent> EVENTS = List.of(DASHBOARD_NOTIFICATION_EVENT);
 
-    private final DashboardNotificationDispatcher dispatcher;
+    private final DashboardNotificationRegistry registry;
 
     @Override
     protected Map<String, Callback> callbacks() {
@@ -47,7 +47,14 @@ public class DashboardNotificationHandler extends CallbackHandler implements Mul
     private CallbackResponse dispatchNotifications(CallbackParams callbackParams) {
         String activityId = camundaActivityId(callbackParams);
         DashboardTaskContext context = DashboardTaskContext.from(callbackParams);
-        dispatcher.dispatch(activityId, context);
+        List<DashboardWorkflowTask> workflows = registry.workflowsFor(activityId, context.caseType());
+
+        if (workflows.isEmpty()) {
+            log.warn("No dashboard notification handlers registered for activity {}", activityId);
+            return AboutToStartOrSubmitCallbackResponse.builder().build();
+        }
+
+        workflows.forEach(task -> task.execute(context));
         return AboutToStartOrSubmitCallbackResponse.builder().build();
     }
 }
