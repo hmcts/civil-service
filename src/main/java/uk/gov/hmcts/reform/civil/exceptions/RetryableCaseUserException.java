e5e5c4b82625f57f1769f32f349425a0d5f0d6a7
philip-milne <philip.milne@hmcts.net>
2026-06-29 12:19:01 +0100

    DTSCCI-5489: Add RetryableCaseUserException for  retryable GET case user

---
+++ src/main/java/uk/gov/hmcts/reform/civil/exceptions/RetryableCaseUserException.java
@@ -0,0 +1,12 @@
+package uk.gov.hmcts.reform.civil.exceptions;
+
+public class RetryableCaseUserException extends PaymentsApiException {
+
+    public RetryableCaseUserException(String message) {
+        super(message);
+    }
+
+    public RetryableCaseUserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
