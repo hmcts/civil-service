c220a6ed493cf6618a84969f8d07cdf5c3ceb3d9
toyin-hmcts <bait-toyin.ajani@hmcts.net>
2026-07-07 09:49:24 +0100

    adding indexes to draft store tables

--- src/main/resources/db/migration/V2026_07_06_1417__DTSCCI-5548-create-draft_store_tables.sql
+++ src/main/resources/db/migration/V2026_07_06_1417__DTSCCI-5548-create-draft_store_tables.sql
@@ -23,11 +23,6 @@ CREATE TABLE draft_store
   FOREIGN KEY (DRAFT_TYPE_ID) REFERENCES dbs.draft_type(id)
 );
 
--- Index on foreign key (Crucial for joins and deletions on draft_type)
-CREATE INDEX idx_draft_store_draft_type_id ON draft_store(DRAFT_TYPE_ID);
-
--- Index for user lookups (Assuming you'll query "get drafts for this user")
-CREATE INDEX idx_draft_store_user_id ON draft_store(USER_ID);
-
--- Index for cleanup cron jobs (Assuming you'll delete expired drafts regularly)
-CREATE INDEX idx_draft_store_expires_at ON draft_store(EXPIRES_AT);
+CREATE INDEX idx_draft_store_draft_type_id ON dbs.draft_store(DRAFT_TYPE_ID);
+CREATE INDEX idx_draft_store_user_id ON dbs.draft_store(USER_ID);
+CREATE INDEX idx_draft_store_expires_at ON dbs.draft_store(EXPIRES_AT);
