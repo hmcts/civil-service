029ed99b8cb6eb28da5e1ffbc1350a864a2fde35
Amy Breslin <amy.breslin@solirius.com>
2026-07-31 16:08:53 +0100

    DTSCCI-5548 - implemented a 30 day fallback in draftType, cleaned up integration tests to update removing draft_created_at field, implemented an integration test for 30 day fallback and updated create integration test to verify the expiresAt is equal to 180

--- src/main/java/uk/gov/hmcts/reform/draftstore/DraftType.java
+++ src/main/java/uk/gov/hmcts/reform/draftstore/DraftType.java
@@ -9,12 +9,28 @@ import java.time.OffsetDateTime;
 @RequiredArgsConstructor
 public enum DraftType {
 
-    DRAFT_CLAIM(1, 180);
+    DRAFT_CLAIM(1, 180),
+    UNDEFINED_DRAFT(2, 30);
 
     private final int id;
     private final long retentionDays;
 
     public OffsetDateTime calculateExpiry(OffsetDateTime createdAt) {
+        if (createdAt == null) {
+            return null;
+        }
         return createdAt.plusDays(retentionDays);
+    }
+
+    public static DraftType getExpiryOrDefault(Integer draftTypeId) {
+        if (draftTypeId == null) {
+            return UNDEFINED_DRAFT;
+        }
+        for (DraftType type : values()) {
+            if (type.getId() == draftTypeId) {
+                return type;
+            }
+        }
+        return UNDEFINED_DRAFT;
     }
 }
