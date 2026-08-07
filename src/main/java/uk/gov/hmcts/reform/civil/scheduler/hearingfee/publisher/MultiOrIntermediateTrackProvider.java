d5f5a61ad8e8928278603fba20e854af082f81a9
Shaun Francis <Shaun.Francis@hmcts.net>
2026-08-07 10:04:09 +0100

    Dtscci 5652 migrate hearing fee check (#8046)
    * Add Hearing Fee Scheduler and related components
    
    * Update HearingFeeScheduler for scheduler framework changes and add integration tests
    
    * Tidy up tests
    
    * Add documentation for HearingFeeScheduler
    
    * Remove CaseData.builder() and use CaseDataBuilder
    
    * Fix sonar issues
    
    * Fix sonar issue
    
    ---------
    
    Co-authored-by: Raja Mani <rajakm@gmail.com>
    Co-authored-by: Dane-Faulconbridge <Dane.Faulconbridge@HMCTS.NET>

---
+++ src/main/java/uk/gov/hmcts/reform/civil/scheduler/hearingfee/publisher/MultiOrIntermediateTrackProvider.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.civil.scheduler.hearingfee.publisher;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.scheduler.hearingfee.publisher.strategy.HearingFeeEventStrategy;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+@Component
+@AllArgsConstructor
+public class MultiOrIntermediateTrackProvider implements HearingFeeLogging {
+
+    private static final String PUBLISHING = "Publishing ";
+
+    private final HearingFeeEventPublisher hearingFeeEventPublisher;
+    private final List<HearingFeeEventStrategy> strategies;
+
+    public Consumer<Long> getPublisher(CaseData caseData) {
+        return strategies.stream()
+            .filter(strategy -> strategy.supports(caseData))
+            .findFirst()
+            .map(strategy -> hearingFeeEventPublisher.createPublisher(
+                getLogMessage(PUBLISHING, strategy.getEventName(), caseData),
+                strategy.getEventFactory()
+            ))
+            .orElseThrow(() -> new IllegalStateException("Hearing fee payment details are not set for case: " + caseData.getCcdCaseReference()));
+    }
+}
