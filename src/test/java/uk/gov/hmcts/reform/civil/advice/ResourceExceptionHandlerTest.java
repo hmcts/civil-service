a966b29ff177dea86d2a40243745851babb7f274
Shaun Francis <shaun.francis@hmcts.net>
2026-08-21 17:58:06 +0100

    Fix merge conflict

--- src/test/java/uk/gov/hmcts/reform/civil/advice/ResourceExceptionHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/advice/ResourceExceptionHandlerTest.java
@@ -20,7 +20,6 @@ import uk.gov.hmcts.reform.civil.documentmanagement.DocumentAccessException;
 import uk.gov.hmcts.reform.civil.documentmanagement.DocumentNotFoundException;
 import uk.gov.hmcts.reform.civil.documentmanagement.InvalidDocumentLinkException;
 import uk.gov.hmcts.reform.civil.model.CallbackErrorResponse;
-import uk.gov.hmcts.reform.civil.exceptions.CaseAccessDataStoreUnavailableException;
 import uk.gov.hmcts.reform.civil.exceptions.UpstreamIdamException;
 import uk.gov.hmcts.reform.civil.service.robotics.exception.JsonSchemaValidationException;
 import uk.gov.hmcts.reform.civil.stateflow.exception.StateFlowException;
