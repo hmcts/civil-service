a819ac826219af59caafc3659418a954560b03c8
mounikaammineni <mounilka.ammineni1@hmcts.net>
2026-08-27 11:04:37 +0100

    Fix CVEs

--- src/main/java/uk/gov/hmcts/reform/cmc/model/ClaimEvent.java
+++ src/main/java/uk/gov/hmcts/reform/cmc/model/ClaimEvent.java
@@ -1,15 +1,15 @@
-package uk.gov.hmcts.reform.civil.callback;
+package uk.gov.hmcts.reform.cmc.model;
 
 import lombok.Getter;
 
 @Getter
-public enum CmcCaseEvent {
+public enum ClaimEvent {
 
     MIGRATE_CASE("migrateCase");
 
     private final String eventName;
 
-    CmcCaseEvent(String eventName) {
+    ClaimEvent(String eventName) {
         this.eventName = eventName;
     }
 
