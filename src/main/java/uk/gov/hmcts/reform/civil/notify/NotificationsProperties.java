97b193cade55121a7e380bdbfe1bc303d56c2f8a
sbagga-moj <sumit.bagga2@hmcts.net>
2026-09-01 12:36:59 +0100

    OCCC-288 (#8188)
    * OCCC-288 Breathing Space (CUI) - LiP Breathing Space Email Notifications

--- src/main/java/uk/gov/hmcts/reform/civil/notify/NotificationsProperties.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notify/NotificationsProperties.java
@@ -425,10 +425,16 @@ public class NotificationsProperties {
     private String notifyApplicant1EnteredBreathingSpaceLip;
 
     @NotEmpty
+    private String notifyApplicant1EnteredBreathingSpaceLipWelsh;
+
+    @NotEmpty
     private String notifyLiPClaimantClaimSubmittedAndHelpWithFeeTemplate;
 
     @NotEmpty
     private String notifyEnteredBreathingSpaceForDefendantLip;
+
+    @NotEmpty
+    private String notifyEnteredBreathingSpaceForDefendantLipWelsh;
 
     @NotEmpty
     private String notifyDefendantLrTemplate;
