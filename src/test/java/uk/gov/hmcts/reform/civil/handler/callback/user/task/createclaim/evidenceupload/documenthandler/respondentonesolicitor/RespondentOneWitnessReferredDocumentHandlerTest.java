f4dce943f8f3bac9001fea343d80db11acd51558
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-09-08 11:01:42 +0100

    Increase coverage

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/task/createclaim/evidenceupload/documenthandler/respondentonesolicitor/RespondentOneWitnessReferredDocumentHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/task/createclaim/evidenceupload/documenthandler/respondentonesolicitor/RespondentOneWitnessReferredDocumentHandlerTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.handler.callback.user.task.createclaim.evidenceupload.documenthandler.respondentonesolicitor;
 
+import uk.gov.hmcts.reform.civil.documentmanagement.model.Document;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,9 +8,17 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.civil.handler.callback.user.task.createclaim.evidenceupload.documenthandler.BaseDocumentHandlerTest;
 import uk.gov.hmcts.reform.civil.handler.callback.user.task.evidenceupload.documenthandler.respondentonesolicitor.RespondentOneWitnessReferredDocumentHandler;
+import uk.gov.hmcts.reform.civil.model.caseprogression.UploadEvidenceDocumentType;
+import uk.gov.hmcts.reform.civil.model.common.Element;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static uk.gov.hmcts.reform.civil.handler.callback.user.task.createclaim.evidenceupload.documenthandler.BaseDocumentHandlerTest.DomainConstants.RESPONDENT;
+import static uk.gov.hmcts.reform.civil.handler.callback.user.task.createclaim.evidenceupload.documenthandler.BaseDocumentHandlerTest.DomainConstants.ORIGINAL_FILE_NAME;
 
 @ExtendWith(MockitoExtension.class)
 class RespondentOneWitnessReferredDocumentHandlerTest extends BaseDocumentHandlerTest {
@@ -36,5 +45,29 @@ class RespondentOneWitnessReferredDocumentHandlerTest extends BaseDocumentHandle
 
         assertEquals("typeOfDocument referred to in the statement of witnessName 10-02-2022.pdf",
                 caseData.getDocumentReferredInStatementRes().get(0).getValue().getDocumentUpload().getDocumentFileName());
+    }
+
+    @Test
+    void shouldRenameDocumentsWithoutIssueDate() {
+        Document localDocument = new Document();
+        localDocument.setDocumentFileName(ORIGINAL_FILE_NAME);
+
+        UploadEvidenceDocumentType uploadEvidenceDocumentType = new UploadEvidenceDocumentType();
+        uploadEvidenceDocumentType.setDocumentIssuedDate(null);
+        uploadEvidenceDocumentType.setTypeOfDocument("typeOfDocument");
+        uploadEvidenceDocumentType.setWitnessOptionName("witnessName");
+        uploadEvidenceDocumentType.setDocumentUpload(localDocument);
+
+        Element<UploadEvidenceDocumentType> element = new Element<>();
+        element.setValue(uploadEvidenceDocumentType);
+
+        caseData = CaseDataBuilder.builder().build();
+        caseData.setDocumentReferredInStatementRes(List.of(element));
+
+        StringBuilder notificationBuilder = new StringBuilder();
+
+        assertDoesNotThrow(() -> handler.handleDocuments(caseData, RESPONDENT, notificationBuilder));
+        assertEquals("typeOfDocument referred to in the statement of witnessName.pdf",
+            caseData.getDocumentReferredInStatementRes().get(0).getValue().getDocumentUpload().getDocumentFileName());
     }
 }
