b6a5a7274312beb2bb886f70902cbb41e6fc8173
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-09-08 11:13:40 +0100

    Removed unused import

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/task/createclaim/evidenceupload/documenthandler/respondentonesolicitor/RespondentOneWitnessReferredDocumentHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/task/createclaim/evidenceupload/documenthandler/respondentonesolicitor/RespondentOneWitnessReferredDocumentHandlerTest.java
@@ -12,7 +12,6 @@ import uk.gov.hmcts.reform.civil.model.caseprogression.UploadEvidenceDocumentTyp
 import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
